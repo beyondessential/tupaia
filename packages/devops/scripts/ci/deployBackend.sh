80fb9dc1a3b637c4659a8143ec0176650bec85ce
Edwin <edmofro@gmail.com>
2020-01-03 10:41:09 +1300

    Run yarn scripts from central location

--- packages/devops/scripts/ci/deployBackend.sh
+++ packages/devops/scripts/ci/deployBackend.sh
@@ -6,7 +6,7 @@ fi
 
 if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
   echo "Deployment for ${CI_BRANCH} exists, updating with latest changes"
-  ssh ubuntu@api.tupaia.org 'cd ${CI_PACKAGE}; git fetch; git checkout ${CI_BRANCH}; git pull; yarn; yarn migrate; pm2 restart ${CI_PACKAGE};'
+  ssh ubuntu@api.tupaia.org 'cd tupaia; git fetch; git checkout ${CI_BRANCH}; git pull; yarn; yarn migrate-${CI_PACKAGE}; pm2 restart ${CI_PACKAGE};'
 else
   echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
