ab64c3a4ccb518d96fb52f3506e5848d7a4667a5
Edwin <edmofro@gmail.com>
2020-01-02 17:44:17 +1300

    Turn off strict host checking

--- packages/devops/scripts/ci/deployFrontend.sh
+++ packages/devops/scripts/ci/deployFrontend.sh
@@ -6,7 +6,7 @@ fi
 
 if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
   echo "Deployment for ${CI_BRANCH} exists, updating with latest changes"
-  ssh ubuntu@$DEPLOYMENT_URL 'cd tupaia/packages/${CI_PACKAGE}; git fetch; git checkout ${CI_BRANCH}; git pull; yarn; yarn build;'
+  ssh -o StrictHostKeyChecking=no ubuntu@$DEPLOYMENT_URL 'cd tupaia/packages/${CI_PACKAGE}; git fetch; git checkout ${CI_BRANCH}; git pull; yarn; yarn build;'
 else
   echo "No deployment exists for ${CI_BRANCH}, cancelling update"
   echo $DEPLOYMENT_URL
