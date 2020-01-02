0a95043c3a5481fab53f3e8df9c30ee843be4db7
Edwin <edmofro@gmail.com>
2020-01-03 10:43:09 +1300

    Use double quotes so variables are evaluated

--- packages/devops/scripts/ci/deployFrontend.sh
+++ packages/devops/scripts/ci/deployFrontend.sh
@@ -7,7 +7,7 @@ fi
 if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
   echo "Deployment for ${CI_BRANCH} exists, updating with latest changes"
   /bin/bash -c "ssh-keyscan -H ${DEPLOYMENT_URL} >> /root/.ssh/known_hosts"
-  ssh ubuntu@$DEPLOYMENT_URL 'cd tupaia; git fetch; git checkout ${CI_BRANCH}; git pull; yarn; yarn build-${CI_PACKAGE};'
+  ssh ubuntu@$DEPLOYMENT_URL "cd tupaia; git fetch; git checkout ${CI_BRANCH}; git pull; yarn; yarn build-${CI_PACKAGE};"
 else
   echo "No deployment exists for ${CI_BRANCH}, cancelling update"
   echo $DEPLOYMENT_URL
