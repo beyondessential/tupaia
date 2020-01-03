b8ad235252e9699608447fe09afdf949c03b0be0
Edwin <edmofro@gmail.com>
2020-01-03 16:33:29 +1300

    Setup db connection

--- packages/devops/scripts/ci/setupTestData.sh
+++ packages/devops/scripts/ci/setupTestData.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 DIR=`dirname "$0"`
 ${DIR}/waitForPostgres.sh
-psql -h db -p 5432 -U postgres -d test -f src/tests/testData/testDataDump.sql
+psql -h $DB_URL -p 5432 -U postgres -d postgres "CREATE USER $DB_USER WITH PASSWORD $DB_PASSWORD"
+psql -h $DB_URL -p 5432 -U postgres -d postgres "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
+PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p 5432 -U $DB_USER -d $DB_NAME -f src/tests/testData/testDataDump.sql
