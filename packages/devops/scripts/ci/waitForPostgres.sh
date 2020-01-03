e326986acdbe6c7149e8c2a8a073de7bf793a50a
Edwin <edmofro@gmail.com>
2020-01-03 17:24:29 +1300

    Allow any point release of postgis

--- packages/devops/scripts/ci/waitForPostgres.sh
+++ packages/devops/scripts/ci/waitForPostgres.sh
@@ -8,7 +8,7 @@ args=(
 	--quiet --no-align --tuples-only
 )
 
-until select="$(echo 'SELECT PostGIS_full_version()' | psql "${args[@]}")" && [[ "$select" == *"2.5.1"* ]] || [ $RETRIES -eq 0 ]; do
+until select="$(echo 'SELECT PostGIS_full_version()' | psql "${args[@]}")" && [[ "$select" == *"2.5."* ]] || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
 	echo $select
   sleep 1
