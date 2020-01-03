4c84d0d03fdc9126b7e1ed4e38ab47f2b0507251
Edwin <edmofro@gmail.com>
2020-01-03 18:11:55 +1300

    Set db connection details using test env during testing

--- packages/devops/scripts/ci/testBackend.sh
+++ packages/devops/scripts/ci/testBackend.sh
@@ -2,5 +2,9 @@
 DIR=`dirname "$0"`
 ${DIR}/setupTestData.sh
 cd ./packages/${CI_PACKAGE}
+export DB_NAME=$CI_TEST_DB_NAME
+export DB_PASSWORD=$CI_TEST_DB_PASSWORD
+export DB_URL=$CI_TEST_DB_URL
+export DB_USER=$CI_TEST_DB_USER
 yarn migrate
 yarn test
