688b2afe8b79d8d39c80350e6211f2a5ed4938d2
Edwin <edmofro@gmail.com>
2020-01-03 17:27:34 +1300

    Create role rather than user

--- packages/devops/scripts/ci/setupTestData.sh
+++ packages/devops/scripts/ci/setupTestData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR=`dirname "$0"`
 ${DIR}/waitForPostgres.sh
-psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE USER $DB_USER SUPERUSER WITH PASSWORD '$DB_PASSWORD'"
+psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE ROLE $DB_USER LOGIN SUPERUSER PASSWORD '$DB_PASSWORD'"
 psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p 5432 -U $DB_USER -d $DB_NAME -f ./packages/meditrak-server/src/tests/testData/testDataDump.sql
