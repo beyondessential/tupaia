ba6eaf26858946860195fccf1f22c69499e4ea01
Edwin <edmofro@gmail.com>
2020-01-03 16:37:46 +1300

    Remove double quotes from meditrak-server env file

--- packages/devops/scripts/ci/setupTestData.sh
+++ packages/devops/scripts/ci/setupTestData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR=`dirname "$0"`
 ${DIR}/waitForPostgres.sh
-psql -h $DB_URL -p 5432 -U postgres -d postgres "CREATE USER $DB_USER WITH PASSWORD $DB_PASSWORD"
-psql -h $DB_URL -p 5432 -U postgres -d postgres "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
+psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE USER $DB_USER WITH PASSWORD $DB_PASSWORD"
+psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p 5432 -U $DB_USER -d $DB_NAME -f src/tests/testData/testDataDump.sql
