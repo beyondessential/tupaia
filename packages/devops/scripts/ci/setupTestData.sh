8829f6d2cf59ef3fd80f385ea2cfcf05f88463a6
Edwin <edmofro@gmail.com>
2020-01-03 16:42:26 +1300

    Specify test file correctly

--- packages/devops/scripts/ci/setupTestData.sh
+++ packages/devops/scripts/ci/setupTestData.sh
@@ -3,4 +3,4 @@ DIR=`dirname "$0"`
 ${DIR}/waitForPostgres.sh
 psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD'"
 psql -h $DB_URL -p 5432 -U postgres -d postgres -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
-PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p 5432 -U $DB_USER -d $DB_NAME -f src/tests/testData/testDataDump.sql
+PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p 5432 -U $DB_USER -d $DB_NAME -f tupaia/packages/meditrak-server/src/tests/testData/testDataDump.sql
