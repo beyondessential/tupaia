c5a5476fde2b8a616b3b61d7eaa6bd509dc83c7e
Edwin <edmofro@gmail.com>
2020-01-06 09:45:50 +1300

    Debugging user creation

--- packages/meditrak-server/src/dataAccessors/createUser.js
+++ packages/meditrak-server/src/dataAccessors/createUser.js
@@ -43,6 +43,8 @@ export const createUser = async (
         ...hashAndSaltPassword(password),
         ...restOfUser,
       });
+      console.log('*************************');
+      console.log(user.first_name);
 
       await transactingModels.userCountryPermission.findOrCreate({
         user_id: user.id,
