8d31fa1f96781e5df934225b20c044e7f1f9bab0
Edwin <edmofro@gmail.com>
2020-01-07 11:41:55 +1300

    Build common package without istanbul coverage

--- packages/common/dist/database/index.js
+++ packages/common/dist/database/index.js
@@ -1,31 +1,5 @@
 'use strict';
 
-var cov_1ddty7y6f = function () {
-  var path = '/Users/Edwin/Documents/Git/tupaia/packages/common/src/database/index.js',
-      hash = '692ae4b0976d59733c25ecc85c82b2fc64dca2b6',
-      Function = function () {}.constructor,
-      global = new Function('return this')(),
-      gcv = '__coverage__',
-      coverageData = {
-    path: '/Users/Edwin/Documents/Git/tupaia/packages/common/src/database/index.js',
-    statementMap: {},
-    fnMap: {},
-    branchMap: {},
-    s: {},
-    f: {},
-    b: {},
-    _coverageSchema: '332fd63041d2c1bcb487cc26dd0d5f7d97098a6c'
-  },
-      coverage = global[gcv] || (global[gcv] = {});
-
-  if (coverage[path] && coverage[path].hash === hash) {
-    return coverage[path];
-  }
-
-  coverageData.hash = hash;
-  return coverage[path] = coverageData;
-}();
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
--- packages/common/dist/index.js
+++ packages/common/dist/index.js
@@ -1,31 +1,5 @@
 'use strict';
 
-var cov_140ao1z4y7 = function () {
-  var path = '/Users/Edwin/Documents/Git/tupaia/packages/common/src/index.js',
-      hash = '8b44232c5b3c56217c0b861c6448ca5c920f24a0',
-      Function = function () {}.constructor,
-      global = new Function('return this')(),
-      gcv = '__coverage__',
-      coverageData = {
-    path: '/Users/Edwin/Documents/Git/tupaia/packages/common/src/index.js',
-    statementMap: {},
-    fnMap: {},
-    branchMap: {},
-    s: {},
-    f: {},
-    b: {},
-    _coverageSchema: '332fd63041d2c1bcb487cc26dd0d5f7d97098a6c'
-  },
-      coverage = global[gcv] || (global[gcv] = {});
-
-  if (coverage[path] && coverage[path].hash === hash) {
-    return coverage[path];
-  }
-
-  coverageData.hash = hash;
-  return coverage[path] = coverageData;
-}();
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
