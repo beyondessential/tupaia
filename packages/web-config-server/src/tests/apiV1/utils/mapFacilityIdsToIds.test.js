a820f63ede4f88b2a6def6d564dd6e83a7e498d9
Rohan Port <rohan@beyondessential.com.au>
2020-01-08 16:35:50 +1100

    fix/tupaia-web/1085: Added unit tests for mapFacilityIdsToIds and mapFacilityIdsToGroupCodes

---
+++ packages/web-config-server/src/tests/apiV1/utils/mapFacilityIdsToIds.test.js
@@ -0,0 +1,48 @@
+/**
+ * Tupaia MediTrak
+ * Copyright (c) 2020 Beyond Essential Systems Pty Ltd
+ */
+import { expect } from 'chai';
+import { it, describe } from 'mocha';
+
+import { mapFacilityIdsToIds } from '/apiV1/utils/mapFacilityIdsToIds';
+
+const organisationUnits = [
+    { code: 'SB_Guadalcanal Province',
+    name: 'Guadalcanal Province',
+    id: 'As8RCJJNVGC',
+    description: '{"level":"District"}',
+    comment: '[[-10.59505396,158.59292748800002],[-9.181143,161.338592912]]',
+    children:
+     [ { code: 'SB_10503', level: 4, id: 'IYRU3RH79ti' },
+       { code: 'SB_10203', level: 4, id: 'GqumUN45VC8' },
+       { code: 'SB_10601', level: 4, id: 'HxGTP4vnkfn' } ], 
+    },
+    { 
+        code: 'SB_Honiara',
+        name: 'Honiara',
+        id: 'xeNth7JPCGf',
+        description: '{"level":"District"}',
+        children:
+        [ { code: 'SB_90201', level: 4, id: 'ODdaT8ncTzt' },
+        { code: 'SB_90301', level: 4, id: 'yLzHFimWVVF' },
+        { code: 'SB_90305', level: 4, id: 'UbnIMaXikNf' },
+        { code: 'SB_90205', level: 4, id: 'iyd21zEg9jS' } ], 
+    },
+];
+
+describe('mapFacilityIdsToIds', () => {
+  it('should map facility ids to ids', () => {
+    try {
+      const result = mapFacilityIdsToIds(organisationUnits);
+      expect(result['As8RCJJNVGC']).to.equal('As8RCJJNVGC');
+      expect(result['IYRU3RH79ti']).to.equal('As8RCJJNVGC');
+      expect(result['GqumUN45VC8']).to.equal('As8RCJJNVGC');
+      expect(result['xeNth7JPCGf']).to.equal('xeNth7JPCGf');
+      expect(result['UbnIMaXikNf']).to.equal('xeNth7JPCGf');
+      return Promise.resolve();
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  });
+});
