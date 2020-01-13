ed347d8ce6368650c5f5619e91d61b2162373270
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-01-13 13:54:36 +1100

    Rename helper method

--- packages/utils/src/tests/request.test.js
+++ packages/utils/src/tests/request.test.js
@@ -10,7 +10,7 @@ describe('request', () => {
   const BASE_URL = 'https://test-api.org';
   const ENDPOINT = 'reports';
 
-  const assertUrlIsExpected = (queryParams, expectedUrl) => {
+  const assertUrlIsCorrect = (queryParams, expectedUrl) => {
     expect(stringifyQuery(BASE_URL, ENDPOINT, queryParams)).to.equal(expectedUrl);
   };
 
@@ -19,47 +19,47 @@ describe('request', () => {
       const expectedUrl = 'https://test-api.org/reports';
 
       expect(stringifyQuery(BASE_URL, ENDPOINT)).to.equal(expectedUrl);
-      [undefined, null, {}].forEach(queryParams => assertUrlIsExpected(queryParams, expectedUrl));
+      [undefined, null, {}].forEach(queryParams => assertUrlIsCorrect(queryParams, expectedUrl));
     });
 
     it('`undefined` params', () => {
-      assertUrlIsExpected({ a: undefined }, 'https://test-api.org/reports');
-      assertUrlIsExpected({ a: 1, b: undefined }, 'https://test-api.org/reports?a=1');
-      assertUrlIsExpected({ a: undefined, b: 2 }, 'https://test-api.org/reports?b=2');
+      assertUrlIsCorrect({ a: undefined }, 'https://test-api.org/reports');
+      assertUrlIsCorrect({ a: 1, b: undefined }, 'https://test-api.org/reports?a=1');
+      assertUrlIsCorrect({ a: undefined, b: 2 }, 'https://test-api.org/reports?b=2');
     });
 
     it('`null` params', () => {
-      assertUrlIsExpected({ a: null }, 'https://test-api.org/reports');
-      assertUrlIsExpected({ a: 1, b: null }, 'https://test-api.org/reports?a=1');
-      assertUrlIsExpected({ a: null, b: 2 }, 'https://test-api.org/reports?b=2');
+      assertUrlIsCorrect({ a: null }, 'https://test-api.org/reports');
+      assertUrlIsCorrect({ a: 1, b: null }, 'https://test-api.org/reports?a=1');
+      assertUrlIsCorrect({ a: null, b: 2 }, 'https://test-api.org/reports?b=2');
     });
 
     it('one query param', () => {
-      assertUrlIsExpected({ a: 1 }, 'https://test-api.org/reports?a=1');
+      assertUrlIsCorrect({ a: 1 }, 'https://test-api.org/reports?a=1');
     });
 
     it('two query params', () => {
-      assertUrlIsExpected({ a: 1, b: 2 }, 'https://test-api.org/reports?a=1&b=2');
+      assertUrlIsCorrect({ a: 1, b: 2 }, 'https://test-api.org/reports?a=1&b=2');
     });
 
     it('special characters', () => {
-      assertUrlIsExpected({ a: '=test' }, 'https://test-api.org/reports?a=%3Dtest');
-      assertUrlIsExpected({ '%value': 0.3 }, 'https://test-api.org/reports?%25value=0.3');
-      assertUrlIsExpected({ '%value': '=test' }, 'https://test-api.org/reports?%25value=%3Dtest');
+      assertUrlIsCorrect({ a: '=test' }, 'https://test-api.org/reports?a=%3Dtest');
+      assertUrlIsCorrect({ '%value': 0.3 }, 'https://test-api.org/reports?%25value=0.3');
+      assertUrlIsCorrect({ '%value': '=test' }, 'https://test-api.org/reports?%25value=%3Dtest');
     });
 
     it('array query param', () => {
-      assertUrlIsExpected({ a: [1] }, 'https://test-api.org/reports?a=1');
-      assertUrlIsExpected({ a: [1, 2] }, 'https://test-api.org/reports?a=1&a=2');
+      assertUrlIsCorrect({ a: [1] }, 'https://test-api.org/reports?a=1');
+      assertUrlIsCorrect({ a: [1, 2] }, 'https://test-api.org/reports?a=1&a=2');
     });
 
     it('array query param with special characters', () => {
-      assertUrlIsExpected({ a: [1, '=test'] }, 'https://test-api.org/reports?a=1&a=%3Dtest');
-      assertUrlIsExpected(
+      assertUrlIsCorrect({ a: [1, '=test'] }, 'https://test-api.org/reports?a=1&a=%3Dtest');
+      assertUrlIsCorrect(
         { '%value': [0.63, 1] },
         'https://test-api.org/reports?%25value=0.63&%25value=1',
       );
-      assertUrlIsExpected(
+      assertUrlIsCorrect(
         { '%value': [1, '=test'] },
         'https://test-api.org/reports?%25value=1&%25value=%3Dtest',
       );
