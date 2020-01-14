73fd31ec112b6a3652c2121f906f78d88f58b7b4
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-01-15 10:08:53 +1100

    Build rather than mutate analytics query

--- packages/dhis-api/src/buildAnalyticsQuery.js
+++ packages/dhis-api/src/buildAnalyticsQuery.js
@@ -1,54 +1,32 @@
-/**
- * Removes redundant fields from an analytics query to prevent the following DHIS errors:
- * 1. `Periods and start and end dates cannot be specified simultaneously`
- * 2. `Request-URI Too Large`
- */
-const removeRedundantFields = queryIn => {
-  const query = { ...queryIn };
+const getDxString = query => {
+  const { dataElementCodes, dataElementGroupCodes, dataElementGroupCode } = query;
 
-  if (queryIn.period) {
-    // [`startDate`, `endDate`] and `period` cannot be used at the same data value analytic query
-    delete query.startDate;
-    delete query.endDate;
+  if (dataElementCodes) {
+    return dataElementCodes.join(';');
   }
-
-  // Remove redundant properties which can significantly increase the request uri
-  delete query.period;
-  delete query.dataElementCodes;
-  delete query.dataElementGroupCode;
-  delete query.dataElementGroupCodes;
-  delete query.organisationUnitCodes;
-
-  return query;
+  return (dataElementGroupCodes || [dataElementGroupCode])
+    .map(groupCode => `DE_GROUP-${groupCode}`)
+    .join(';');
 };
 
+/**
+ * @param {AnalyticsQueryInput} queryIn
+ * @returns {Object<string, string>}
+ */
 export const buildAnalyticsQuery = queryIn => {
-  const query = { ...queryIn };
-
-  // Add common defaults for input and output id schemes
-  query.inputIdScheme = 'code';
-  query.outputIdScheme = query.outputIdScheme || 'uid';
-
-  // Transform the easily understood 'dataElementCodes', 'period', etc. to DHIS2 language
-  const {
-    dataElementCodes,
-    dataElementGroupCode,
-    dataElementGroupCodes,
-    organisationUnitCode,
-    period,
-  } = query;
-  const getDxString = () => {
-    if (dataElementCodes) {
-      return dataElementCodes.join(';');
-    }
-    return (dataElementGroupCodes || [dataElementGroupCode])
-      .map(groupCode => `DE_GROUP-${groupCode}`)
-      .join(';');
+  const query = {
+    inputIdScheme: 'code',
+    outputIdScheme: queryIn.outputIdScheme || 'uid',
+    includeMetadataDetails: true,
+    dimension: [`dx:${getDxString(queryIn)}`, `ou:${queryIn.organisationUnitCode}`],
   };
-  const dxString = getDxString();
 
-  query.dimension = [`dx:${dxString}`, `pe:${period}`, `ou:${organisationUnitCode}`];
-  query.includeMetadataDetails = true;
+  if (queryIn.period) {
+    query.dimension.push(`pe:${queryIn.period}`);
+  } else {
+    query.startDate = queryIn.startDate;
+    query.endDate = queryIn.endDate;
+  }
 
-  return removeRedundantFields(query);
+  return query;
 };
