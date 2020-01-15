9be7acec3eb6d4adf42ddcadef9e4262ce9a5dc8
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-01-15 17:02:49 +1100

    Use CI_BRANCH

--- packages/devops/scripts/ci/validateBranchName.sh
+++ packages/devops/scripts/ci/validateBranchName.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-branch=$(git symbolic-ref --short -q HEAD)
 reserved_endings=(api config export mobile admin www)
 
 for reserved_ending in ${reserved_endings[@]}
 do
-    if [[ "$branch" == *$reserved_ending ]]; then
-        echo "Invalid branch name, must not end in '$reserved_ending'";
+    if [[ "$CI_BRANCH" == *$reserved_ending ]]; then
+        echo "Invalid branch name, must not end in '$reserved_ending'"
         exit 1;
     fi
 done
