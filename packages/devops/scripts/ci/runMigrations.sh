2fc31f804fa70212e0f21ead2630330d821366e1
Bastien Goepfert <bas.goepfert@gmail.com>
2020-01-22 12:12:37 +1100

    move deployment migration step to single pre deploy step

--- packages/devops/scripts/ci/runMigrations.sh
+++ packages/devops/scripts/ci/runMigrations.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-PACKAGE=$1
 DIR=`dirname "$0"`
 DEPLOYMENT_URL=$(${DIR}/determineDeploymentUrl.sh)
 if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
-  echo "Deployment for ${CI_BRANCH} exists, updating with latest changes"
-  ssh ubuntu@$DEPLOYMENT_URL "cd tupaia; pm2 restart ${PACKAGE};"
+  echo "Deployment for ${CI_BRANCH} exists, running migrations"
+  ssh ubuntu@$DEPLOYMENT_URL "cd tupaia; yarn migrate;"
 else
   echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
