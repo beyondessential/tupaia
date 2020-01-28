b91a59cd0f9f77fd68575cf00cda2e2007ed3835
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-01-28 15:00:54 +1100

    Refactor getDhisApiInstance in @tupaia/data-broker

--- packages/data-broker/src/index.js
+++ packages/data-broker/src/index.js
@@ -3,8 +3,4 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-// other packages currently use getDhisApiInstance directly, but this should be removed at some point
-// in future when they do everything via the data-broker
-export { getDhisApiInstance } from './services/dhis/getDhisApiInstance';
-
 export { DataBroker } from './DataBroker';
--- packages/data-broker/src/services/dhis/getDhisApiInstance.js
+++ packages/data-broker/src/services/dhis/getDhisApiInstance.js
@@ -1,53 +1,12 @@
-/**
- * Tupaia Config Server
- * Copyright (c) 2018 Beyond Essential Systems Pty Ltd
- */
-import { kebab as convertToKebabCase } from 'case';
 import { DhisApi } from '@tupaia/dhis-api';
-import { getCountryNameFromCode } from '@tupaia/utils';
+import { getDhisConfig } from '@tupaia/utils';
 
-const dhisApiInstances = {};
+const instances = {};
 
-const REGIONAL_SERVER_NAME = 'regional';
-const SERVER_NAMES = new Set([REGIONAL_SERVER_NAME, 'tonga']);
-
-const getServerUrlFromName = serverName => {
-  const isProduction = process.env.IS_PRODUCTION_ENVIRONMENT === 'true';
-  const devPrefix = isProduction ? '' : 'dev-';
-  const specificServerPrefix = '' || serverName === REGIONAL_SERVER_NAME ? '' : `${serverName}-`;
-  return `https://${devPrefix}${specificServerPrefix}aggregation.tupaia.org`;
-};
-
-const getServerNameFromCountryCode = countryCode =>
-  convertToKebabCase(getCountryNameFromCode(countryCode));
-
-export const getServerName = (entityCode, isDataRegional) => {
-  if (isDataRegional) return REGIONAL_SERVER_NAME;
-  const countryCode = entityCode.substring(0, 2); // All organisation unit codes start with the two letter country code
-  const countrySpecificServerName = getServerNameFromCountryCode(countryCode);
-  return SERVER_NAMES.has(countrySpecificServerName)
-    ? countrySpecificServerName
-    : REGIONAL_SERVER_NAME; // If the country does not have a dhis2 server, use the regional server
-};
-
-/**
- * Returns an api instance connected to the dhis server for the country containing the given
- * entityCode, or the regional dhis server if either none is passed in, or the data
- * is regional. Can also pass the `serverName` directly, to specify exactly which instance to use.
- * @param {Object}  options
- * @param {string}  options.entityCode      Along with isDataRegional, determines which dhis instance to use
- * @param {boolean} options.isDataRegional  Along with entityCode, determines which dhis instance to use
- * @param {string}  options.serverName      If provided, the server name will take this value rather
- *                                          being derived from the other options
- */
-export const getDhisApiInstance = ({
-  serverName: providedServerName,
-  entityCode = '',
-  isDataRegional = true,
-}) => {
-  const serverName = providedServerName || getServerName(entityCode, isDataRegional);
-  if (!dhisApiInstances[serverName]) {
-    dhisApiInstances[serverName] = new DhisApi(serverName, getServerUrlFromName(serverName));
+export const getDhisApiInstance = options => {
+  const { serverName, serverUrl } = getDhisConfig(options);
+  if (!instances[serverName]) {
+    instances[serverName] = new DhisApi(serverName, serverUrl);
   }
-  return dhisApiInstances[serverName];
+  return instances[serverName];
 };
