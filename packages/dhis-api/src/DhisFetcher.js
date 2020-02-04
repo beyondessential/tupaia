469802910b61192707c09387874cd97fadc31c23
Edwin <edmofro@gmail.com>
2020-02-05 09:25:19 +1300

    Longer timeout on dhis fetches

--- packages/dhis-api/src/DhisFetcher.js
+++ packages/dhis-api/src/DhisFetcher.js
@@ -29,6 +29,8 @@ const CACHEABLE_ENDPOINTS = [
 const getIsCacheable = (endpoint, httpMethod) =>
   CACHEABLE_ENDPOINTS.includes(endpoint) && httpMethod === 'GET';
 
+const MAX_FETCH_WAIT_TIME = 120 * 1000; // 120 seconds allows longer running DHIS fetches to succeed
+
 /**
  * Handles auth, fetching, and caching the more stable responses from dhis2 endpoints
  */
@@ -102,7 +104,7 @@ export class DhisFetcher {
     if (cachedResponse) {
       return cachedResponse;
     }
-    const response = await fetchWithTimeout(url, fetchConfig);
+    const response = await fetchWithTimeout(url, fetchConfig, MAX_FETCH_WAIT_TIME);
 
     if (response.ok) {
       try {
