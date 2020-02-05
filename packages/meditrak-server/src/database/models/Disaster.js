8238bc4cc1f23f38b0f07bdbd58c2ca0b937e15b
Ralph Ward <ralph.ward@gmail.com>
2020-02-06 09:20:42 +1100

    Update Disaster.js

--- packages/meditrak-server/src/database/models/Disaster.js
+++ packages/meditrak-server/src/database/models/Disaster.js
@@ -3,9 +3,7 @@
  * Copyright (c) 2019 Beyond Essential Systems Pty Ltd
  **/
 
-import { DatabaseType } from '../DatabaseType';
-import { TYPES } from '..';
-import { DatabaseModel } from '../DatabaseModel';
+import { DatabaseModel, DatabaseType, TYPES } from '@tupaia/database';
 
 class DisasterType extends DatabaseType {
   static databaseType = TYPES.DISASTER;
