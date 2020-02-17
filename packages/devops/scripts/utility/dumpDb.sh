014c1636623385815ff168e169f33c69dae5fbaf
Edwin <edmofro@gmail.com>
2020-02-18 11:29:59 +1300

    Don't use postgres user during gzip

--- packages/devops/scripts/utility/dumpDb.sh
+++ packages/devops/scripts/utility/dumpDb.sh
@@ -45,8 +45,12 @@ dump_file_path="/var/lib/postgresql/$DUMP_FILE_NAME"
 
 echo "Creating dump file in $domain..."
 ssh -o StrictHostKeyChecking=no -i $identity_file $host \
-  "sudo -u postgres bash -c \"pg_dump tupaia > $dump_file_path\" \
-    && sudo gzip $dump_file_path"
+  "sudo -u postgres bash -c \"pg_dump tupaia > $dump_file_path\""
+
+
+echo "Compressing dump file"
+ssh -o StrictHostKeyChecking=no -i $identity_file $host \
+  "sudo gzip -f $dump_file_path"
 
 target_path="$(cd "$target_dir"; pwd)/$DUMP_FILE_NAME.gz"
 echo "Downloading dump file into '$target_path'..."
