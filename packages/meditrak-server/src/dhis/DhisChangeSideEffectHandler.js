5b211396b995251b04f1b8724dd929c76a7957bc
Edwin <edmofro@gmail.com>
2020-02-20 14:18:15 +1300

    Add temporary debug logging

--- packages/meditrak-server/src/dhis/DhisChangeSideEffectHandler.js
+++ packages/meditrak-server/src/dhis/DhisChangeSideEffectHandler.js
@@ -10,10 +10,12 @@ export class DhisChangeSideEffectHandler extends ChangeSideEffectHandler {
     // If an event based answer is added, updated, or deleted, trigger an update to its parent
     // survey response, as event based answers sync to dhis2 as a whole response
     const allAnswerIds = this.getIdsFromChangesForModel(changes, this.models.answer);
+    console.log('Triggering for', allAnswerIds.length);
     const responsesToMarkAsChangedById = {};
     const batchSize = this.models.database.maxBindingsPerQuery;
     for (let i = 0; i < allAnswerIds.length; i += batchSize) {
       const batchOfAnswerIds = allAnswerIds.slice(i, i + batchSize);
+      console.log('Looking up event based responses for', batchOfAnswerIds.length);
       const batchOfResponses = await this.models.database.executeSql(
         `
           SELECT DISTINCT survey_response.*
@@ -26,11 +28,22 @@ export class DhisChangeSideEffectHandler extends ChangeSideEffectHandler {
         `,
         batchOfAnswerIds,
       );
+      console.log('Found', batchOfResponses.length);
       batchOfResponses.forEach(r => {
         responsesToMarkAsChangedById[r.id] = r;
       });
+      console.log('Added to object');
     }
     const responsesToMarkAsChanged = Object.values(responsesToMarkAsChangedById);
+    console.log(
+      'Marking as changed',
+      responsesToMarkAsChanged.length,
+      'from',
+      allAnswerIds.length,
+      'answers of',
+      changes.length,
+      'changes',
+    );
     await this.models.surveyResponse.markRecordsAsChanged(responsesToMarkAsChanged);
   };
 }
