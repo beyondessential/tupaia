158813c6725b588f8acf71d9589b32b3c5b830c3
Ralph Ward <ralph.ward@gmail.com>
2020-02-20 13:30:32 +1100

    Update 20200218010903-CreateProjectHierarchy.js

--- packages/database/src/migrations/20200218010903-CreateProjectHierarchy.js
+++ packages/database/src/migrations/20200218010903-CreateProjectHierarchy.js
@@ -16,8 +16,8 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  db.runSql(`
+exports.up = async function(db) {
+  await db.runSql(`
       ALTER TABLE entity_relation ALTER COLUMN entity_relation_type_code drop not null;
       ALTER TABLE entity_relation DROP CONSTRAINT entity_relation_entity_relation_type_code_fkey;
       ALTER TABLE project drop column entity_ids;
@@ -25,13 +25,13 @@ exports.up = function(db) {
       ALTER TABLE project add column entity_id text;
     `);
 
-  updateProject(db, 'unfpa', 'UNFPA', `'WS','MH','TO','FM'`, 'country');
-  updateProject(db, 'imms', 'Immunization Module', `'VU','SB'`, 'country');
-  updateProject(db, 'fanafana', 'Fanafana Ola', `'TO'`, 'country');
-  updateProject(db, 'disaster', 'Disaster Response', `'Wo'`, 'world');
-  updateProject(db, 'wish', 'WISH Fiji', `'FJ'`, 'country');
-  updateProject(db, 'strive', 'STRIVE PNG', `'PG'`, 'country');
-  updateProject(db, 'explore', 'General', `'Wo'`, 'world');
+  await updateProject(db, 'unfpa', 'UNFPA', `'WS','MH','TO','FM'`, 'country');
+  await updateProject(db, 'imms', 'Immunization Module', `'VU','SB'`, 'country');
+  await updateProject(db, 'fanafana', 'Fanafana Ola', `'TO'`, 'country');
+  await updateProject(db, 'disaster', 'Disaster Response', `'Wo'`, 'world');
+  await updateProject(db, 'wish', 'WISH Fiji', `'FJ'`, 'country');
+  await updateProject(db, 'strive', 'STRIVE PNG', `'PG'`, 'country');
+  await updateProject(db, 'explore', 'General', `'Wo'`, 'world');
   return null;
 };
 
@@ -39,18 +39,18 @@ const updateProject = (db, projectCode, projectDescription, countryCodes, type) 
   const projectId = generateId();
 
   return db.runSql(`
-    insert into entity (id, code, parent_id, "name", type ) values ('${projectId}', '${projectCode}', '5d3f8844a72aa231bf71977f', '${projectDescription}', 'project');
-    insert into entity_relation (
+    insert into "entity" ("id", "code", "parent_id", "name", "type" ) values ('${projectId}', '${projectCode}', '5d3f8844a72aa231bf71977f', '${projectDescription}', 'project');
+    insert into "entity_relation" (
           select
               '${generateId().slice(
                 0,
                 -1,
               )}' || LPAD(row_number() OVER()::text, 1, '0'), '${projectId}',  id, ''
-            from entity
+            from "entity"
             where country_code in (${countryCodes})
              and  type in ('${type}'));
 
-    update project set entity_id = '${projectId}' where code ='${projectCode}';
+    update "project" set "entity_id" = '${projectId}' where "code" ='${projectCode}';
   `);
 };
 
