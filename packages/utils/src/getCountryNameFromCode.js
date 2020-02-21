e559a2bebd3a04881e7eecf55cd2fe18ab9109d0
Edwin <edmofro@gmail.com>
2020-02-21 14:10:03 +1300

    Only import countrynames when it is actually used

--- packages/utils/src/getCountryNameFromCode.js
+++ packages/utils/src/getCountryNameFromCode.js
@@ -1,7 +1,10 @@
-import { getName } from 'countrynames';
 import { capital as capitaliseFirstLetters } from 'case';
 
 export const getCountryNameFromCode = countryCode => {
+  // only require countrynames when the function is actually run, otherwise it breaks front end
+  // builds as the top level export from utils/index.js means that it is loaded
+  // TODO get better at only requiring used code from internal dependencies
+  const { getName } = require('countrynames');
   if (countryCode === 'DL') {
     return 'Demo Land';
   }
