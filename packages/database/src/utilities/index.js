4b567a8a07fc2ccd8eb454280cba5d0a8adc88b9
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-02-27 12:53:02 +1100

    Fix insertObject util

---
+++ packages/database/src/utilities/index.js
@@ -0,0 +1,8 @@
+export { generateId, getHighestPossibleIdForGivenTime } from './generateId';
+export {
+  insertObject,
+  arrayToDbString,
+  updateValues,
+  removeArrayValue,
+  replaceArrayValue,
+} from './migration';
