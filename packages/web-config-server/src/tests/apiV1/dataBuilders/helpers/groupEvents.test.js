81436bfeceb42517693820a87744b606b7f500a6
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-02-28 17:00:48 +1100

    Add groupBy functionality in countEvents

---
+++ packages/web-config-server/src/tests/apiV1/dataBuilders/helpers/groupEvents.test.js
@@ -0,0 +1,67 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { groupEvents } from '/apiV1/dataBuilders/helpers/groupEvents';
+import * as Models from '/models/Entity';
+
+const EVENTS = [
+  { orgUnit: 'TO_Tongatapu', orgUnitName: 'Tongatapu', dataValues: [] },
+  { orgUnit: 'TO_Niuas', orgUnitName: 'Niuas', dataValues: [] },
+];
+const PARENT_ORG_UNIT = {
+  code: 'TO',
+  name: 'Tonga',
+  getDescendantsOfType: type =>
+    type === 'district'
+      ? [
+          { code: 'TO_Tongatapu', name: 'Tongatapu' },
+          { code: 'TO_Niuas', name: 'Niuas' },
+          { code: 'TO_Haapai', name: 'Haapai' },
+        ]
+      : [],
+};
+
+const stubEntity = () => {
+  sinon
+    .stub(Models.Entity, 'findOne')
+    .callsFake(({ code }) => (code === PARENT_ORG_UNIT.code ? PARENT_ORG_UNIT : null));
+};
+
+const restoreEntity = () => {
+  Models.Entity.findOne.restore();
+};
+
+describe('groupEvents()', () => {
+  before(() => {
+    stubEntity();
+  });
+
+  after(() => {
+    restoreEntity();
+  });
+
+  it('allOrgUnitNames', () =>
+    expect(
+      groupEvents(EVENTS, {
+        type: 'allOrgUnitNames',
+        options: { parentCode: 'TO', type: 'district' },
+      }),
+    ).to.eventually.deep.equal({
+      Tongatapu: [{ orgUnit: 'TO_Tongatapu', orgUnitName: 'Tongatapu', dataValues: [] }],
+      Niuas: [{ orgUnit: 'TO_Niuas', orgUnitName: 'Niuas', dataValues: [] }],
+      Haapai: [],
+    }));
+
+  it('unknown groupBy type', async () => {
+    const assertCorrectErrorIsThrown = groupBySpecs =>
+      expect(groupEvents(EVENTS, groupBySpecs)).to.eventually.be.rejectedWith(
+        /not a supported groupBy type/,
+      );
+
+    return Promise.all([{ type: 'unknownType' }, { type: '' }, {}].map(assertCorrectErrorIsThrown));
+  });
+});
