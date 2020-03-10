c46f940ca356bc00424a7a08ac162bfa3909e252
Ralph Ward <ralph.ward@gmail.com>
2020-03-10 15:07:20 +1100

    Update two more tickets

--- packages/database/src/migrations/20200310020604-MatrixTableNameChange.js
+++ packages/database/src/migrations/20200310020604-MatrixTableNameChange.js
@@ -18,14 +18,24 @@ exports.up = function(db) {
   return db.runSql(`
     update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"JEE (Pacific Island Countries)"') where id = 'WHO_IHR_JEE_WPRO';
     update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"SPAR (Pacific Island Countries)"') where id = 'WHO_IHR_SPAR_WPRO';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"Total number of Pacific States Parties submitting the IHR self-assessment annual report since 2010on"') where id = 'WHO_SURVEY';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{entityHeader}', '""') where id = 'WHO_SURVEY';
+    update "dashboardReport" set "viewJson" = "viewJson" || '{"presentationOptions": {"hideAverage": true}}' where id = 'WHO_SURVEY';
+    update "dashboardReport" set "viewJson" = "viewJson" || '{"entityHeader": "Pacific territories"}' where id = 'WHO_IHR_SPAR_NST';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"IHR self-assessments"') where id = 'WHO_IHR_SPAR_NST';
   `);
 };
 
 exports.down = function(db) {
   return db.runSql(`
-  update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"JEE (WPRO Countries)"') where id = 'WHO_IHR_JEE_WPRO';
-  update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"SPAR (WPRO Countries)"') where id = 'WHO_IHR_SPAR_WPRO';
-`);
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"JEE (WPRO Countries)"') where id = 'WHO_IHR_JEE_WPRO';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"SPAR (WPRO Countries)"') where id = 'WHO_IHR_SPAR_WPRO';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"WHO SPAR Reporting Countries"') where id = 'WHO_SURVEY';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{entityHeader}', '"Western Pacific Region"') where id = 'WHO_SURVEY';
+    update "dashboardReport" SET "viewJson" = "viewJson" - 'presentationOptions' where id = 'WHO_SURVEY';  
+    update "dashboardReport" set "viewJson" = "viewJson" - 'entityHeader' where id = 'WHO_IHR_SPAR_NST';
+    update "dashboardReport" set "viewJson" = jsonb_set("viewJson", '{name}', '"SPAR (Non-sovereign territories)"') where id = 'WHO_IHR_SPAR_NST';
+  `);
 };
 
 exports._meta = {
