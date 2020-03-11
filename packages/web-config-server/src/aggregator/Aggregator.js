16c0825ea5f6b1bae9fafd9de32382149baf9b01
Edwin <edmofro@gmail.com>
2020-03-12 10:02:21 +1300

    Include event replacements in query building
    Addressing https://github.com/beyondessential/tupaia-backlog/issues/199

--- packages/web-config-server/src/aggregator/Aggregator.js
+++ packages/web-config-server/src/aggregator/Aggregator.js
@@ -4,7 +4,7 @@ import { QueryBuilder } from '/dhis/QueryBuilder';
 export class Aggregator extends BaseAggregator {
   async fetchAnalytics(dataElementCodes, originalQuery, replacementValues, ...otherParams) {
     const queryBuilder = new QueryBuilder(originalQuery, replacementValues);
-    const query = queryBuilder.makeDimensionReplacements();
+    const query = queryBuilder.build();
 
     return super.fetchAnalytics(dataElementCodes, query, ...otherParams);
   }
