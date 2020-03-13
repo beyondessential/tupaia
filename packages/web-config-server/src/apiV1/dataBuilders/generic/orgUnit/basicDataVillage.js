180f328eda4a410136d5c12df927850ef66fdcf5
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-03-13 14:02:24 +1100

    Add basicDataVillage data builder

---
+++ packages/web-config-server/src/apiV1/dataBuilders/generic/orgUnit/basicDataVillage.js
@@ -0,0 +1,26 @@
+import { DataBuilder } from '/apiV1/dataBuilders/DataBuilder';
+
+export class BasicDataVillageBuilder extends DataBuilder {
+  async build() {
+    const facility = await this.entity.parent();
+    const facilityName = facility ? facility.name : DataBuilder.NO_DATA_AVAILABLE;
+
+    const data = [
+      { name: 'Type', value: 'Village/Hamlet' },
+      { name: 'Nearest Health Facility', value: facilityName },
+    ];
+
+    return { data };
+  }
+}
+
+export const basicDataVillage = ({ dataBuilderConfig, query, entity }, aggregator, dhisApi) => {
+  const builder = new BasicDataVillageBuilder(
+    aggregator,
+    dhisApi,
+    dataBuilderConfig,
+    query,
+    entity,
+  );
+  return builder.build();
+};
