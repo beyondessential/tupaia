fdbc9fd75f99882e330c8fa60ef30ebf2669d4c5
Edwin <edmofro@gmail.com>
2020-03-19 14:25:28 +1300

    Add changes to RouteHandler

--- packages/web-config-server/src/apiV1/RouteHandler.js
+++ packages/web-config-server/src/apiV1/RouteHandler.js
@@ -1,7 +1,6 @@
 import { respond } from '@tupaia/utils';
-import { getDhisApiInstance } from '/dhis';
-import { PermissionsChecker } from './utils/PermissionsChecker';
 import { Entity } from '/models';
+import { PermissionsError } from '@tupaia/utils';
 
 /**
  * Interface class for handling permission checked endpoints
@@ -11,22 +10,36 @@ export class RouteHandler {
   constructor(req, res) {
     this.req = req;
     this.res = res;
+    this.entity = null;
+  }
+
+  // can be overridden by subclasses with specific permissions checks
+  async checkPermissions() {
+    const PermissionsChecker = this.prototype.PermissionsChecker;
+    if (!PermissionsChecker) {
+      throw new Error(
+        'Each RouteHandler must explicitly specify a permissions checker to ensure permissions have been considered',
+      );
+    }
+    try {
+      const { query, userHasAccess } = this.req;
+      const checker = new PermissionsChecker(query, userHasAccess, this.entity);
+      await checker.checkPermissions();
+    } catch (e) {
+      throw new PermissionsError(e.message);
+    }
   }
 
   async handleRequest() {
     // Fetch permissions
-    const regionalDhisApi = getDhisApiInstance();
     const { organisationUnitCode } = this.req.query;
     this.entity = await Entity.findOne({ code: organisationUnitCode });
-    const permissionsChecker = new PermissionsChecker(this.req, regionalDhisApi, this.entity);
-    const permissionJson = await permissionsChecker.getPermissionsOrThrowError();
-    if (permissionJson) {
-      // Join userLevel and overlays to this
-      const { userLevel, overlays } = permissionJson;
-      this.userLevel = userLevel;
-      this.overlays = overlays;
+    await this.checkPermissions();
+    // if a 'buildResponse' is defined by the subclass, run it and respond, otherwise assume the
+    // subclass will override handleRequest directly and respond itself
+    if (this.buildResponse) {
+      const response = await this.buildResponse(this.req);
+      respond(this.res, response);
     }
-    const response = await this.buildResponse(this.req);
-    respond(this.res, response);
   }
 }
