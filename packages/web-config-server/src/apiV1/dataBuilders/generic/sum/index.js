d899c31c596254e61a57dbdfecca88e201e1cc5d
monty-wanty <lockealastair@gmail.com>
2020-03-25 12:11:12 +1100

    241 Add COVID Cases By State report Australia

--- packages/web-config-server/src/apiV1/dataBuilders/generic/sum/index.js
+++ packages/web-config-server/src/apiV1/dataBuilders/generic/sum/index.js
@@ -3,3 +3,4 @@ export { sumLatestPerSeries } from './sumPerSeries';
 export { sumLatest } from './sum';
 export { sumPerDataGroupPerMonth } from './sumPerDataGroupPerMonth';
 export { sumPerWeek, sumPerMonth } from './sumPerPeriod';
+export { sumByOrgUnit } from './sumByOrgUnit';
---
+++ packages/web-config-server/src/apiV1/dataBuilders/generic/sum/sumByOrgUnit.js
@@ -0,0 +1,49 @@
+/**
+ * Tupaia Config Server
+ * Copyright (c) 2019 Beyond Essential Systems Pty Ltd
+ */
+
+import { DataBuilder } from '/apiV1/dataBuilders/DataBuilder';
+
+class SumByOrgUnitBuilder extends DataBuilder {
+  /**
+   * @returns {SumAggregateSeriesOutput}
+   */
+  async build() {
+    const { dataElementCodes, labels } = this.config;
+
+    const { results } = await this.fetchAnalytics(dataElementCodes);
+    console.log(labels);
+
+    const dataByOrgUnit = {};
+    results.forEach(({ organisationUnit, value }) => {
+      dataByOrgUnit[organisationUnit] = (dataByOrgUnit[organisationUnit] || 0) + value;
+    });
+    const returnData = Object.keys(dataByOrgUnit)
+      .sort()
+      .map(organisationUnit => ({
+        name: labels && labels[organisationUnit] ? labels[organisationUnit] : organisationUnit,
+        value: dataByOrgUnit[organisationUnit],
+      }));
+    return {
+      data: returnData,
+    };
+  }
+}
+
+export const sumByOrgUnit = async (
+  { dataBuilderConfig, query, entity },
+  aggregator,
+  dhisApi,
+  aggregationType,
+) => {
+  const builder = new SumByOrgUnitBuilder(
+    aggregator,
+    dhisApi,
+    dataBuilderConfig,
+    query,
+    entity,
+    aggregationType,
+  );
+  return builder.build();
+};
