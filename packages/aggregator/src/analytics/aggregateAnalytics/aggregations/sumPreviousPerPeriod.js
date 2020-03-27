4aacc6073a4ac5b664af1d73c6528ebfe1946be5
Rohan Port <rohan@beyondessential.com.au>
2020-03-27 11:58:32 +1100

    277: Added SUM_PREVIOUS_EACH_DAY aggregation type - Created aggregations/utils directory for common utils functions

---
+++ packages/aggregator/src/analytics/aggregateAnalytics/aggregations/sumPreviousPerPeriod.js
@@ -0,0 +1,40 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+import { convertToPeriod } from '@tupaia/dhis-api';
+import { getContinuousPeriodsForAnalytics } from './utils';
+
+export const sumPreviousPerPeriod = (analytics, aggregationPeriod) => {
+  const periods = getContinuousPeriodsForAnalytics(analytics, aggregationPeriod);
+
+  const periodGroupedAnalytics = periods.reduce((summedAnalytics, period) => {
+    const analyticsForPeriod = analytics.filter(
+      analytic => convertToPeriod(analytic.period, aggregationPeriod) === period,
+    );
+
+    if (!summedAnalytics.length) {
+      return [analyticsForPeriod]; //First period, just add analytics
+    }
+
+    const previousPeriodAnalytics = summedAnalytics[summedAnalytics.length - 1];
+    previousPeriodAnalytics.forEach(previousAnalytic => {
+      const indexOfEquivalentAnalytic = analyticsForPeriod.findIndex(
+        currentAnalytic =>
+          previousAnalytic.dataElement === currentAnalytic.dataElement &&
+          previousAnalytic.organisationUnit === currentAnalytic.organisationUnit,
+      );
+      // If there are no matching response elements already being returned, add it
+      if (indexOfEquivalentAnalytic < 0) {
+        analyticsForPeriod.push({ ...previousAnalytic, period });
+      } else {
+        analyticsForPeriod[indexOfEquivalentAnalytic].value += previousAnalytic.value;
+      }
+    });
+
+    return [...summedAnalytics, analyticsForPeriod];
+  }, []);
+
+  return [].concat(...periodGroupedAnalytics); //Flatten array
+};
