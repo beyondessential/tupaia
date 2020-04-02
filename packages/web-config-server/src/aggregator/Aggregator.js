5f297adeadd67849dea1af5a6579fbb84c5b33f3
monty-wanty <lockealastair@gmail.com>
2020-04-03 10:17:21 +1100

    310 Fix periodFromAnalytics

--- packages/web-config-server/src/aggregator/Aggregator.js
+++ packages/web-config-server/src/aggregator/Aggregator.js
@@ -5,8 +5,6 @@ export class Aggregator extends BaseAggregator {
   async fetchAnalytics(dataElementCodes, originalQuery, replacementValues, ...otherParams) {
     const queryBuilder = new QueryBuilder(originalQuery, replacementValues);
     const query = queryBuilder.build();
-    const ans = await super.fetchAnalytics(dataElementCodes, query, ...otherParams);
-    console.log(ans);
-    return ans;
+    return super.fetchAnalytics(dataElementCodes, query, ...otherParams);
   }
 }
