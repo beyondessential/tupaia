1bf6eba31b1922ac846cd20c50fab0c57abe3563
Tom Caiger <caigertom@gmail.com>
2020-04-09 11:27:30 +1200

    Update custom icons

--- packages/ui-components/src/stories/icon.stories.js
+++ packages/ui-components/src/stories/icon.stories.js
@@ -5,52 +5,24 @@
 
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import {
-  AddBoxOutlined,
-  IndeterminateCheckBox,
-  Autorenew,
-  ChevronRight,
-  ChevronLeft,
-} from '@material-ui/icons';
-import {
-  Alerts,
-  Calendar,
-  Cases,
-  Dashboard,
-  Expand,
-  Export,
-  Home,
-  Minimize,
-  Outbreaks,
-  Sort,
-  Submit,
-  Warning,
-} from '../components/Icons';
+import { Autorenew, Alarm, CalendarToday, ChevronRight, ChevronLeft } from '@material-ui/icons';
+import { Alerts, Cases, Dashboard, Home, Outbreaks } from '../components/Icons';
 
 export default {
   title: 'Icon',
 };
 
 export const icons = () => (
-  <>
-    <Box p={4}>
-      <AddBoxOutlined />
-      <Alerts />
-      <Calendar />
-      <Cases />
-      <Dashboard />
-      <Expand />
-      <Export />
-      <Home />
-      <Minimize />
-      <Outbreaks />
-      <Sort />
-      <Submit />
-      <Warning />
-      <IndeterminateCheckBox />
-      <Autorenew />
-      <ChevronRight />
-      <ChevronLeft />
-    </Box>
-  </>
+  <Box p={4}>
+    <Alerts />
+    <Cases />
+    <Dashboard />
+    <Home />
+    <Outbreaks />
+    <Alarm />
+    <CalendarToday />
+    <Autorenew />
+    <ChevronRight />
+    <ChevronLeft />
+  </Box>
 );
