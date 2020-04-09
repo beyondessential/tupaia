9a739d9c784c6e102239cfb26e28e11fcddf63a1
Tom Caiger <caigertom@gmail.com>
2020-04-09 12:31:27 +1200

    wip: update modals

--- packages/ui-components/src/components/Modal.js
+++ packages/ui-components/src/components/Modal.js
@@ -4,72 +4,34 @@
  */
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
+import PropTypes from 'prop-types';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import { Button } from '../components/Button';
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
-
-export const SimpleModal = () => {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
+export const Modal = (props) => {
+  const { onClose, open } = props;
 
   const handleClose = () => {
-    setOpen(false);
+    onClose();
   };
 
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      <SimpleModal />
-    </div>
-  );
-
   return (
-    <div>
-      <button type="button" onClick={handleOpen}>
-        Open Modal
-      </button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
-    </div>
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">Archive Alert</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          You can set my maximum width and whether to adapt or not.
+        </DialogContentText>
+      </DialogContent>
+      <Button onClick={handleClose}>Close</Button>
+    </Dialog>
   );
-}
+};
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+};
--- packages/ui-components/src/stories/modal.stories.js
+++ packages/ui-components/src/stories/modal.stories.js
@@ -4,12 +4,58 @@
  */
 
 import React from 'react';
-import { SimpleModal } from '../components/Modal';
+import { Modal } from '../components/Modal';
+import { Button } from '../components/Button';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import * as COLORS from '../theme/colors';
 
 export default {
   title: 'Modal',
 };
 
-export const Modal = () => (
-  <SimpleModal />
-);
+const Container = styled.div`
+  max-width: 1200px;
+  width: 100vw;
+  height: 100vh;
+  padding: 3rem;
+  background-color: ${COLORS.LIGHTGREY};
+  
+  .MuiCard-root {
+      max-width: 330px;
+  }
+`;
+
+export const modal = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Container>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h4" gutterBottom>
+            Heading
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate
+            numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+          </Typography>
+          <br/>
+          <Button onClick={handleClickOpen}>Open modal</Button>
+        </CardContent>
+      </Card>
+      <Modal open={open} onClose={handleClose} />
+    </Container>
+  );
+};
