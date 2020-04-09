381876165f99ece5de3b1403186e769d3a17f71d
Bastien Goepfert <bas.goepfert@gmail.com>
2020-04-09 14:48:49 +1000

    add tests per capita migration

--- packages/database/src/migrations/20200409013211-AddAddCovidTestsPerCapitaReport.js
+++ packages/database/src/migrations/20200409013211-AddAddCovidTestsPerCapitaReport.js
@@ -4,6 +4,8 @@ var dbm;
 var type;
 var seed;
 
+import { AGGREGATION_TYPES } from '@tupaia/aggregator/src/aggregationTypes';
+
 /**
  * We receive the dbmigrate dependency from dbmigrate initially.
  * This enables us to not have to rely on NODE_PATH.
@@ -15,11 +17,41 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return null;
+  return db.runSql(`
+    insert into "dashboardReport" ("id", "dataBuilder", "dataBuilderConfig", "viewJson")
+    VALUES (
+      'COVID_Tests_Per_Capita',
+      'percentagesByNominatedPairs',
+      '{
+        "pairs": {"dailysurvey006": "AU_POP002"},
+        "aggregationTypes": {
+          "numerator": "${AGGREGATION_TYPES.SUM}",
+          "denominator": "${AGGREGATION_TYPES.FINAL_EACH_DAY_FILL_EMPTY_DAYS}"
+        }
+      }',
+      '{
+        "name":"COVID-19 Tests per capita",
+        "type": "chart",
+        "chartType": "line",
+        "valueType": "percentage",
+        "periodGranularity": "day"
+      }'
+    );
+
+    update "dashboardGroup"
+    set "dashboardReports" = "dashboardReports" || '{COVID_Tests_Per_Capita}'
+    where code = 'AU_Covid_Country';
+  `);
 };
 
 exports.down = function(db) {
-  return null;
+  return db.runSql(`
+    delete from "dashboardReport" where id = 'COVID_Tests_Per_Capita';
+
+    update "dashboardGroup"
+    set "dashboardReports" = array_remove("dashboardReports", '"COVID_Tests_Per_Capita"')
+    where code = 'AU_Covid_Country';
+  `);
 };
 
 exports._meta = {
