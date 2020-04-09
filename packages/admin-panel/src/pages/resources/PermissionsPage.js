f394109ba4ebaf6f8b68453088507b2352497877
Edwin <edmofro@gmail.com>
2020-04-10 08:16:41 +1200

    Admin panel matches single user_entity_permissions table

--- packages/admin-panel/src/pages/resources/PermissionsPage.js
+++ packages/admin-panel/src/pages/resources/PermissionsPage.js
@@ -6,56 +6,10 @@
 import React from 'react';
 
 import { ResourcePage } from './ResourcePage';
-import { TabsPage } from '../TabsPage';
 
-const COUNTRY = 'COUNTRY';
-const GEOGRAPHICAL_AREA = 'GEOGRAPHICAL_AREA';
-const FACILITY = 'FACILITY';
-
-const TABS = {
-  [COUNTRY]: {
-    title: 'Country level',
-    endpoint: 'userCountryPermissions',
-    fields: [
-      {
-        Header: 'Country',
-        source: 'country.name',
-        editConfig: {
-          optionsEndpoint: 'countries',
-        },
-      },
-    ],
-  },
-  [GEOGRAPHICAL_AREA]: {
-    title: 'Geographical Area level',
-    endpoint: 'userGeographicalAreaPermissions',
-    fields: [
-      {
-        Header: 'Geographical Area',
-        source: 'geographical_area.name',
-        editConfig: {
-          optionsEndpoint: 'geographical_area',
-        },
-      },
-    ],
-  },
-  [FACILITY]: {
-    title: 'Facility level',
-    endpoint: 'userFacilityPermissions',
-    fields: [
-      {
-        // TODO: this has to moved over to entities
-        Header: 'Facility',
-        source: 'clinic.name',
-        editConfig: {
-          optionsEndpoint: 'facilities',
-        },
-      },
-    ],
-  },
-};
-
-const BASE_FIELDS = [
+// export for use on users page
+export const PERMISSIONS_ENDPOINT = 'userEntityPermissions';
+export const PERMISSIONS_COLUMNS = [
   {
     Header: 'Permission Group',
     source: 'permission_group.name',
@@ -63,9 +17,6 @@ const BASE_FIELDS = [
       optionsEndpoint: 'permissionGroups',
     },
   },
-];
-
-const USER_FIELDS = [
   {
     Header: 'User',
     source: 'user.first_name',
@@ -78,16 +29,15 @@ const USER_FIELDS = [
   },
 ];
 
-export const getPermissionColumns = (fields, endpoint) => [
-  ...BASE_FIELDS,
-  ...fields,
+const FIELDS = [
+  ...PERMISSIONS_COLUMNS,
   {
     Header: 'Edit',
     source: 'id',
     type: 'edit',
     actionConfig: {
-      editEndpoint: endpoint,
-      fields: [...USER_FIELDS, ...fields, ...BASE_FIELDS],
+      editEndpoint: PERMISSIONS_ENDPOINT,
+      fields: PERMISSIONS_COLUMNS,
     },
   },
   {
@@ -95,24 +45,19 @@ export const getPermissionColumns = (fields, endpoint) => [
     source: 'id',
     type: 'delete',
     actionConfig: {
-      endpoint,
+      endpoint: PERMISSIONS_ENDPOINT,
     },
   },
 ];
-
-const getPermissionColumnsForTab = tabName => {
-  const { fields, endpoint } = TABS[tabName];
-  return getPermissionColumns(fields, endpoint);
-};
 
 const EDIT_CONFIG = {
   title: "Edit User's Permission",
 };
 
-const getCreateConfig = (fields, editEndpoint) => ({
+const CREATE_CONFIG = {
   title: 'Give User Permission',
   actionConfig: {
-    editEndpoint,
+    editEndpoint: PERMISSIONS_ENDPOINT,
     fields: [
       {
         Header: 'User Email',
@@ -122,37 +67,17 @@ const getCreateConfig = (fields, editEndpoint) => ({
           optionLabelKey: 'email',
         },
       },
-      ...BASE_FIELDS,
-      ...fields,
+      ...PERMISSIONS_COLUMNS,
     ],
   },
-});
-
-const getTabPage = tabName => {
-  const { title, endpoint, fields } = TABS[tabName];
-  if (!TABS[tabName]) {
-    console.warn(`No tab with name '${tabName}' found.`);
-  }
-
-  return {
-    title,
-    component: (
-      <ResourcePage
-        title={title}
-        endpoint={endpoint}
-        columns={[...USER_FIELDS, ...getPermissionColumns(fields, endpoint)]}
-        editConfig={EDIT_CONFIG}
-        createConfig={getCreateConfig(fields, endpoint)}
-      />
-    ),
-  };
 };
 
 export const PermissionsPage = () => (
-  <TabsPage tabs={[getTabPage(COUNTRY), getTabPage(GEOGRAPHICAL_AREA), getTabPage(FACILITY)]} />
+  <ResourcePage
+    title="Permissions"
+    endpoint={PERMISSIONS_ENDPOINT}
+    columns={FIELDS}
+    editConfig={EDIT_CONFIG}
+    createConfig={CREATE_CONFIG}
+  />
 );
-
-// For UsersPage expansion.
-export const COUNTRY_PERMISSION_COLUMNS = getPermissionColumnsForTab(COUNTRY);
-export const GEOGRAPHICAL_AREA_PERMISSION_COLUMNS = getPermissionColumnsForTab(GEOGRAPHICAL_AREA);
-export const FACILITY_PERMISSION_COLUMNS = getPermissionColumnsForTab(FACILITY);
