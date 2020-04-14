0acf60bf4fd1d75fc79e4d320e8fd291f274a52b
monty-wanty <lockealastair@gmail.com>
2020-04-14 17:30:38 +1000

    184, 185 Add strive overlays (migration)

--- packages/web-frontend/src/utils/measures.js
+++ packages/web-frontend/src/utils/measures.js
@@ -13,6 +13,7 @@ import {
   resolveSpectrumColour,
 } from '../components/Marker/markerColors';
 import { SPECTRUM_ICON, DEFAULT_ICON, UNKNOWN_ICON } from '../components/Marker/markerIcons';
+import { VALUE_TYPES } from '../components/View/constants';
 import { MAP_COLORS } from '../styles';
 import { formatDataValue } from './formatters';
 import { SCALE_TYPES } from '../constants';
@@ -123,27 +124,27 @@ function getFormattedValue(value, type, valueInfo, scaleType, valueType) {
 }
 
 const getSpectrumScaleValues = (measureData, measureOption) => {
-  const { key, scaleType, scaleMin, scaleMax, startDate, endDate } = measureOption;
+  const { key, scaleType, valueType, scaleMin, scaleMax, startDate, endDate } = measureOption;
 
-  switch (scaleType) {
-    case SCALE_TYPES.TIME:
-      return { min: startDate, max: endDate };
-    case SCALE_TYPES.PERFORMANCE:
-      return { min: 0, max: 1 };
-    default: {
-      const flattenedMeasureData = flattenNumericalMeasureData(measureData, key);
-      const hasScaleMin = scaleMin !== undefined;
-      const hasScaleMax = scaleMax !== undefined;
-      return {
-        min: hasScaleMin
-          ? Math.min(scaleMin, ...flattenedMeasureData)
-          : Math.min(...flattenedMeasureData),
-        max: hasScaleMax
-          ? Math.max(scaleMax, ...flattenedMeasureData)
-          : Math.max(...flattenedMeasureData),
-      };
-    }
+  if (scaleType === SCALE_TYPES.TIME) {
+    return { min: startDate, max: endDate };
   }
+
+  const flattenedMeasureData = flattenNumericalMeasureData(measureData, key);
+  const hasScaleMin = scaleMin !== undefined;
+  const hasScaleMax = scaleMax !== undefined;
+  if (valueType === VALUE_TYPES.PERCENTAGE) {
+    return { min: 0, max: scaleMax === 'auto' ? Math.max(...flattenedMeasureData) : 1 };
+  }
+
+  return {
+    min: hasScaleMin
+      ? Math.min(scaleMin, ...flattenedMeasureData)
+      : Math.min(...flattenedMeasureData),
+    max: hasScaleMax
+      ? Math.max(scaleMax, ...flattenedMeasureData)
+      : Math.max(...flattenedMeasureData),
+  };
 };
 
 export function processMeasureInfo(response) {
@@ -291,7 +292,7 @@ export function getMeasureDisplayInfo(measureData, measureOptions, hiddenMeasure
             max,
             noDataColour,
           );
-          displayInfo.icon = SPECTRUM_ICON;
+          displayInfo.icon = valueInfo.icon || displayInfo.icon || SPECTRUM_ICON;
           break;
         case MEASURE_TYPE_SHADING:
           displayInfo.color = MAP_COLORS[valueInfo.color] || MAP_COLORS.NO_DATA;
@@ -334,5 +335,5 @@ export const calculateRadiusScaleFactor = measureData => {
 // and filters NaN values (e.g. undefined).
 export function flattenNumericalMeasureData(measureData, key) {
   // eslint-disable-next-line no-restricted-globals
-  return measureData.map(v => parseInt(v[key], 10)).filter(x => !isNaN(x));
+  return measureData.map(v => parseFloat(v[key])).filter(x => !isNaN(x));
 }
