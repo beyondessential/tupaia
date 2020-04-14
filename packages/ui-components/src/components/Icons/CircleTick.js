9482a0d725bf59156155f9449c624f97828ac384
Tom Caiger <caigertom@gmail.com>
2020-04-14 14:56:42 +1200

    clean up icons

--- packages/ui-components/src/components/Icons/CircleTick.js
+++ packages/ui-components/src/components/Icons/CircleTick.js
@@ -8,17 +8,9 @@
  */
 import React from 'react';
 import styled from 'styled-components';
+import BaseIcon from './BaseIcon';
 
-const StyledSVG = styled.svg`
-  fill: currentColor;
-  width: 1em;
-  height: 1em;
-  display: inline-block;
-  font-size: 1.5rem;
-  transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  flex-shrink: 0;
-  user-select: none;
-
+const StyledSVG = styled(BaseIcon)`
   path {
     fill: none;
     stroke: currentColor;
