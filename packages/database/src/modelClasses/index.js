3ee36f07d8cb0c2a714d899928b30725fe345b1c
Edwin <edmofro@gmail.com>
2020-04-21 16:36:35 +1200

    Move MeditrakDevice into database package

--- packages/database/src/modelClasses/index.js
+++ packages/database/src/modelClasses/index.js
@@ -8,6 +8,7 @@ import { DataElementDataGroupModel } from './DataElementDataGroup';
 import { DataSourceModel } from './DataSource';
 import { GeographicalAreaModel } from './GeographicalArea';
 import { RefreshTokenModel } from './RefreshToken';
+import { MeditrakDeviceModel } from './MeditrakDevice';
 
 // export all models to be used in constructing a ModelRegistry
 export const modelClasses = {
@@ -15,10 +16,12 @@ export const modelClasses = {
   DataElementDataGroup: DataElementDataGroupModel,
   DataSource: DataSourceModel,
   GeographicalArea: GeographicalAreaModel,
+  MeditrakDevice: MeditrakDeviceModel,
   RefreshToken: RefreshTokenModel,
 };
 
 // export any models and types that are extended in other packages
 export { CountryModel } from './Country';
 export { GeographicalAreaModel } from './GeographicalArea';
+export { MeditrakDeviceModel } from './MeditrakDevice';
 export { RefreshTokenModel, RefreshTokenType } from './RefreshToken';
