8fe35773d1b7b609f32a101b00dd921ddcc3e057
Andrés Proaño <andres@oventi.org>
2020-04-22 15:24:10 +1200

    use CommonDataSourceModel

--- packages/meditrak-server/src/database/models/DataSource.js
+++ packages/meditrak-server/src/database/models/DataSource.js
@@ -3,18 +3,10 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { DatabaseModel, DatabaseType, TYPES } from '@tupaia/database';
+import { DataSourceModel as CommonDataSourceModel } from '@tupaia/database';
 
 export const DATA_SOURCE_SERVICE_TYPES = ['dhis'];
 
-class DataSourceType extends DatabaseType {
-  static databaseType = TYPES.DATA_SOURCE;
-}
-
-export class DataSourceModel extends DatabaseModel {
-  get DatabaseTypeClass() {
-    return DataSourceType;
-  }
-
+export class DataSourceModel extends CommonDataSourceModel {
   isDeletableViaApi = true;
 }
