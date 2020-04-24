b6d964d4f63d9eea7bdad0d5c15750a83c9c0331
monty-wanty <lockealastair@gmail.com>
2020-04-24 11:15:29 +1000

    322 allow end period on sum previous aggregation type

--- packages/aggregator/src/analytics/getPeriodForDataBroker.js
+++ packages/aggregator/src/analytics/getPeriodForDataBroker.js
@@ -7,16 +7,16 @@ const EARLIEST_DATA_DATE = utcMoment('2017-01-01'); // Tupaia started in 2017
 export const getPeriodForDataBroker = (aggregationType, initialPeriod) => {
   // If aggregation type is SUM_PREVIOUS_EACH_DAY we need to fetch data for all time.
   return aggregationType === AGGREGATION_TYPES.SUM_PREVIOUS_EACH_DAY
-    ? getAllTimePeriod()
+    ? getAllTimeUntilPeriod(initialPeriod.endDate)
     : initialPeriod;
 };
 
-const getAllTimePeriod = () => {
+const getAllTimeUntilPeriod = endPeriod => {
   const startDate = EARLIEST_DATA_DATE.date(1);
-  const endDate = utcMoment().endOf('month');
+  const endDate = utcMoment(endPeriod) || utcMoment().endOf('month');
   return {
-    startDate: momentToPeriod(startDate, PERIOD_TYPES.MONTH),
-    endDate: momentToPeriod(endDate, PERIOD_TYPES.MONTH),
+    startDate: momentToPeriod(startDate, PERIOD_TYPES.DAY),
+    endDate: momentToPeriod(endDate, PERIOD_TYPES.DAY),
     period: convertDateRangeToPeriodString(startDate, endDate),
   };
 };
