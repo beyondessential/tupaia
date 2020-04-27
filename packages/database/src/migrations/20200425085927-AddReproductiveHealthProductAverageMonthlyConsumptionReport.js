bed0517c860907e07b0a0fc28275ada92686cdf2
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-04-28 00:38:34 +1000

    1248: some more refactor

--- packages/database/src/migrations/20200425085927-AddReproductiveHealthProductAverageMonthlyConsumptionReport.js
+++ packages/database/src/migrations/20200425085927-AddReproductiveHealthProductAverageMonthlyConsumptionReport.js
@@ -15,104 +15,104 @@ exports.setup = function(options, seedLink) {
 };
 
 const UNFPA_RH_AMC_DATA_CLASSES = {
-  "UNFPA_RH_Male_condoms": {
+  "UNFPA_RH_AMC_Male_condoms": {
     "codes": ["AMC_3b3444bf"]
   },
-  "UNFPA_RH_Male_condoms_varied": {
+  "UNFPA_RH_AMC_Male_condoms_varied": {
     "codes": ["AMC_a162942e"]
   },
-  "UNFPA_RH_Female_condoms": {
+  "UNFPA_RH_AMC_Female_condoms": {
     "codes": ["AMC_bf4be518"]
   },
-  "UNFPA_RH_COCs": {
+  "UNFPA_RH_AMC_COCs": {
     "codes": ["AMC_402924bf"]
   },
-  "UNFPA_RH_POP": {
+  "UNFPA_RH_AMC_POP": {
     "codes": ["AMC_47d584bf"]
   },
-  "UNFPA_RH_Implant_Contraceptives": {
+  "UNFPA_RH_AMC_Implant_Contraceptives": {
     "codes": ["AMC_3ff944bf"]
   },
-  "UNFPA_RH_Jadelle": {
+  "UNFPA_RH_AMC_Jadelle": {
     "codes": ["AMC_d2d28620"]
   },
-  "UNFPA_RH_EC_2_dose": {
+  "UNFPA_RH_AMC_EC_2_dose": {
     "codes": ["AMC_47fb04bf"]
   },
-  "UNFPA_RH_EC_single_dose": {
+  "UNFPA_RH_AMC_EC_single_dose": {
     "codes": ["ACM_47fe44bf"]
   },
-  "UNFPA_RH_Depot": {
+  "UNFPA_RH_AMC_Depot": {
     "codes": ["AMC_53d014bf"]
   },
-  "UNFPA_RH_SAYANA_Press": {
+  "UNFPA_RH_AMC_SAYANA_Press": {
     "codes": ["AMC_4752843e"]
   },
-  "UNFPA_RH_Norethisterone": {
+  "UNFPA_RH_AMC_Norethisterone": {
     "codes": ["AMC_542a34bf"]
   },
-  "UNFPA_RH_IUD": {
+  "UNFPA_RH_AMC_IUD": {
     "codes": ["AMC_4718f43e"]
   },
-  "UNFPA_RH_Copper_IUD": {
+  "UNFPA_RH_AMC_Copper_IUD": {
     "codes": ["AMC_3b3994bf"]
   }
 };
 
 const UNFPA_RH_AMC_CHART_CONFIG = {
-  "UNFPA_RH_Male_condoms": {
+  "UNFPA_RH_AMC_Male_condoms": {
     "label": "Male condoms",
     "legendOrder" : 0
   },
-  "UNFPA_RH_Male_condoms_varied": {
+  "UNFPA_RH_AMC_Male_condoms_varied": {
     "label": "Male condoms, varied",
     "legendOrder" : 1
   },
-  "UNFPA_RH_Female_condoms": {
+  "UNFPA_RH_AMC_Female_condoms": {
     "label": "Female condoms",
     "legendOrder" : 2
   },
-  "UNFPA_RH_COCs": {
+  "UNFPA_RH_AMC_COCs": {
     "label": "COCs",
     "legendOrder" : 3
   },
-  "UNFPA_RH_POP": {
+  "UNFPA_RH_AMC_POP": {
     "label": "POP",
     "legendOrder" : 4
   },
-  "UNFPA_RH_Implant_Contraceptives": {
+  "UNFPA_RH_AMC_Implant_Contraceptives": {
     "label": "Implant Contraceptives",
     "legendOrder" : 5
   },
-  "UNFPA_RH_Jadelle": {
+  "UNFPA_RH_AMC_Jadelle": {
     "label": "Jadelle",
     "legendOrder" : 6
   },
-  "UNFPA_RH_EC_2_dose": {
+  "UNFPA_RH_AMC_EC_2_dose": {
     "label": "EC (2 dose)",
     "legendOrder" : 7
   },
-  "UNFPA_RH_EC_single_dose": {
+  "UNFPA_RH_AMC_EC_single_dose": {
     "label": "EC (single dose)",
     "legendOrder" : 8
   },
-  "UNFPA_RH_Depot": {
+  "UNFPA_RH_AMC_Depot": {
     "label": "Depot",
     "legendOrder" : 9
   },
-  "UNFPA_RH_SAYANA_Press": {
+  "UNFPA_RH_AMC_SAYANA_Press": {
     "label": "SAYANA Press",
     "legendOrder" : 10
   },
-  "UNFPA_RH_Norethisterone": {
+  "UNFPA_RH_AMC_Norethisterone": {
     "label": "Norethisterone",
     "legendOrder" : 11
   },
-  "UNFPA_RH_IUD": {
+  "UNFPA_RH_AMC_IUD": {
     "label": "IUD",
     "legendOrder" : 11
   },
-  "UNFPA_RH_Copper_IUD": {
+  "UNFPA_RH_AMC_Copper_IUD": {
     "label": "Copper IUD",
     "legendOrder" : 12
   }
