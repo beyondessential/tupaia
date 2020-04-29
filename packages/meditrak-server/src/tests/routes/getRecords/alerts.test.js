903c73e03a3e3efc26bda89bb58d936912b2cce7
Andrés Proaño <andres@oventi.org>
2020-04-29 17:37:24 +1200

    finalised CRUD tests for alerts

--- packages/meditrak-server/src/tests/routes/getRecords/alerts.test.js
+++ packages/meditrak-server/src/tests/routes/getRecords/alerts.test.js
@@ -3,61 +3,132 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
+import { expect } from 'chai';
 import { TestableApp } from '../../TestableApp';
 import { createAlert } from '../../testUtilities';
 
 describe('Alerts CRUD', () => {
   const app = new TestableApp();
   const models = app.models;
-  let testEntity = null;
-  let testDataElement = null;
-  let testAlert = null;
-  const testStartTime = '2012-11-10T09:08:07.654Z';
+  const testAlertData = [];
 
   before(async () => {
     await app.authenticate();
   });
 
-  describe('GET /alerts', () => {
-    before(async () => {
-      const testData = await createAlert();
-      testEntity = testData.testEntity;
-      testDataElement = testData.testDataElement;
-      testAlert = testData.testAlert;
+  describe('Create: POST /alerts', () => {
+    it('creates an alert', async () => {
+      const troz = await createAlert('TROZ');
+      const { alert, entity, dataElement } = troz;
 
-      console.log('testEntity', testEntity.id);
-      console.log('testDataElement', testDataElement.id);
-      console.log('testAlert', testAlert.id);
-
-      /*
-      testEntity = await models.entity.create({
-        code: 'ABC',
-        name: 'ABC',
+      const { statusCode, body } = await app.post('alert', {
+        body: {
+          entity_id: entity.id,
+          data_element_id: dataElement.id,
+          start_time: new Date().toISOString(),
+        },
       });
 
-      testDataElement = await models.dataSource.create({
-        code: 'DEF',
-        type: 'dataElement',
-        service_type: 'dhis',
-        config: {},
-      });
+      expect(statusCode).to.equal(200);
+      expect(body).to.deep.equal({ message: 'Successfully added alert' });
 
-      testAlert = await models.alert.create({
-        entity_id: testEntity.id,
-        data_element_id: testDataElement.id,
-        start_time: testStartTime,
-      });
-      */
+      await models.alert.delete({ id: alert.id });
+      await models.alert.delete({ entity_id: entity.id, data_element_id: dataElement.id });
+    });
+  });
+
+  describe('Read: GET /alerts', () => {
+    it('reads a list of alerts', async () => {
+      testAlertData.push(await createAlert('NARF'));
+      testAlertData.push(await createAlert('ZORT'));
+
+      const { body: alerts } = await app.get('alerts');
+      expect(alerts.length).to.equal(2);
+
+      for (const [index, alert] of alerts.entries()) {
+        expect(alert).to.have.property('id');
+        expect(alert).to.have.property('entity_id');
+        expect(alert).to.have.property('data_element_id');
+        expect(alert).to.have.property('start_time');
+        expect(alert).to.have.property('end_time');
+        expect(alert).to.have.property('event_confirmed_time');
+        expect(alert).to.have.property('archived');
+
+        expect(alert.id).to.equal(testAlertData[index].alert.id);
+        expect(alert.entity_id).to.equal(testAlertData[index].entity.id);
+        expect(alert.data_element_id).to.equal(testAlertData[index].dataElement.id);
+        expect(alert.end_time).to.be.null;
+        expect(alert.event_confirmed_time).to.be.null;
+      }
     });
 
-    it('test', async () => {
-      console.log('IT TEST');
+    it('reads a single alert', async () => {
+      testAlertData.push(await createAlert('EGAD'));
+
+      const { body: alert } = await app.get(`alerts/${testAlertData[2].alert.id}`);
+
+      expect(alert).to.be.an('object');
+
+      expect(alert).to.have.property('id');
+      expect(alert).to.have.property('entity_id');
+      expect(alert).to.have.property('data_element_id');
+      expect(alert).to.have.property('start_time');
+      expect(alert).to.have.property('end_time');
+      expect(alert).to.have.property('event_confirmed_time');
+      expect(alert).to.have.property('archived');
+
+      expect(alert.id).to.equal(testAlertData[2].alert.id);
+      expect(alert.entity_id).to.equal(testAlertData[2].entity.id);
+      expect(alert.data_element_id).to.equal(testAlertData[2].dataElement.id);
+      expect(alert.end_time).to.be.null;
+      expect(alert.event_confirmed_time).to.be.null;
+    });
+  });
+
+  describe('Update: PUT /alerts', () => {
+    it('updates a single alert', async () => {
+      const testAlertData1 = await createAlert('FIORD');
+      const { alert } = testAlertData1;
+      const newEndTime = new Date().toISOString();
+      testAlertData.push(testAlertData1);
+
+      const { statusCode, body } = await app.put(`alerts/${alert.id}`, {
+        body: { end_time: newEndTime, archived: true },
+      });
+
+      expect(statusCode).to.equal(200);
+      expect(body).to.deep.equal({ message: 'Successfully updated alerts' });
+
+      const createdAlert = (await models.alert.find({ id: alert.id })).pop();
+      expect(createdAlert.end_time).is.not.null;
+      expect(createdAlert.archived).to.be.true;
+    });
+  });
+
+  describe('Delete: DELETE /alerts', () => {
+    it('deletes an alert', async () => {
+      const testAlertData1 = await createAlert('POIT');
+      const { alert } = testAlertData1;
+      testAlertData.push(testAlertData1);
+
+      const { statusCode, body } = await app.delete(`alerts/${alert.id}`);
+
+      expect(statusCode).to.equal(200);
+      expect(body).to.deep.equal({ message: 'Successfully deleted alert' });
+
+      const deletedAlert = await models.alert.find({ id: alert.id });
+      expect(deletedAlert.length).to.equal(0);
     });
   });
 
   after(async () => {
-    await models.alert.delete({ id: testAlert.id });
-    await models.entity.delete({ id: testEntity.id });
-    await models.dataSource.delete({ id: testDataElement.id });
+    for (const item of testAlertData) {
+      await models.alert.delete({ id: item.alert.id });
+    }
+
+    for (const code of ['TROZ', 'NARF', 'ZORT', 'EGAD', 'FIORD', 'POIT']) {
+      await models.entity.delete({ code });
+      await models.dataSource.delete({ code });
+    }
   });
 });
--- packages/meditrak-server/src/tests/testUtilities/database/createAlert.js
+++ packages/meditrak-server/src/tests/testUtilities/database/createAlert.js
@@ -2,24 +2,21 @@ import { getModels } from '../../getModels';
 
 const models = getModels();
 
-export const createAlert = async () => {
-  const testEntity = await models.entity.create({
-    code: 'ABC',
-    name: 'ABC',
-  });
+export const createAlert = async (code = 'ABC', name = 'XYZ') => {
+  const entity = await models.entity.create({ code, name });
 
-  const testDataElement = await models.dataSource.create({
-    code: 'DEF',
+  const dataElement = await models.dataSource.create({
+    code,
     type: 'dataElement',
     service_type: 'dhis',
     config: {},
   });
 
-  const testAlert = await models.alert.create({
-    entity_id: testEntity.id,
-    data_element_id: testDataElement.id,
+  const alert = await models.alert.create({
+    entity_id: entity.id,
+    data_element_id: dataElement.id,
     start_time: new Date().toISOString(),
   });
 
-  return { testEntity, testDataElement, testAlert };
+  return { entity, dataElement, alert };
 };
