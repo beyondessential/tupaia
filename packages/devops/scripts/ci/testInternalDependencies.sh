e45fd2b901550a7c0f17be63f5751ad3cf18664d
Edwin <edmofro@gmail.com>
2020-04-30 15:14:26 +1200

    Add access policy to ci/cd build

--- packages/devops/scripts/ci/testInternalDependencies.sh
+++ packages/devops/scripts/ci/testInternalDependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 DIR=`dirname "$0"`
 for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
+  # skip database package - it gets tested separately
+  if [[ "$PACKAGE" == "database" ]]; then
+    continue
+  fi
   echo Testing ${PACKAGE}
   if ! yarn workspace @tupaia/${PACKAGE} test; then
     exit 1 # the tests for this internal depencency failed
