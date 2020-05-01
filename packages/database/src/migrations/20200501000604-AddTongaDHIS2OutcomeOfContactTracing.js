62f3c06bbc46e88a236a568a0db76210afff7918
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-05-01 16:40:50 +1000

    303: Added migration to create Tonga DHIS2 Outcome of Contact Tracing      report

---
+++ packages/database/src/migrations/20200501000604-AddTongaDHIS2OutcomeOfContactTracing.js
@@ -0,0 +1,152 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+const DATA_BUILDER_CONFIG = {
+  "dataClasses": {
+    "Treatment in Progress": {
+      "numerator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": 'Treatment in Progress',
+            "operator": "="
+          }
+        }
+      },
+      "denominator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": ['Treatment in Progress', 'Treated', 'Untreated', 'No Treatment Required', 'Unknown'],
+            "operator": "in"
+          }
+        }
+      }
+    },
+    "Treated": {
+      "numerator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": 'Treated',
+            "operator": "="
+          }
+        }
+      },
+      "denominator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": ['Treatment in Progress', 'Treated', 'Untreated', 'No Treatment Required', 'Unknown'],
+            "operator": "in"
+          }
+        }
+      }
+    },
+    "Untreated": {
+      "numerator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": 'Untreated',
+            "operator": "="
+          }
+        }
+      },
+      "denominator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": ['Treatment in Progress', 'Treated', 'Untreated', 'No Treatment Required', 'Unknown'],
+            "operator": "in"
+          }
+        }
+      }
+    },
+    "No Treatment Required": {
+      "numerator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": 'No Treatment Required',
+            "operator": "="
+          }
+        }
+      },
+      "denominator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": ['Treatment in Progress', 'Treated', 'Untreated', 'No Treatment Required', 'Unknown'],
+            "operator": "in"
+          }
+        }
+      }
+    },
+    "Unknown": {
+      "numerator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": 'Unknown',
+            "operator": "="
+          }
+        }
+      },
+      "denominator": {
+        "dataValues": {
+          "CD3b_015": {
+            "value": ['Treatment in Progress', 'Treated', 'Untreated', 'No Treatment Required', 'Unknown'],
+            "operator": "in"
+          }
+        }
+      }
+    }
+  },
+  "programCode": "CD3b"
+};
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+const VIEW_JSON_CONFIG = {
+  "name": " Outcome of contact tracing",
+  "type": "chart",
+  "chartType": "pie",
+  "periodGranularity": "one_month_at_a_time",
+  "valueType": "percentage"
+};
+
+exports.up = async function (db) {
+  await db.runSql(`
+    INSERT INTO "dashboardReport" ("id", "dataBuilder", "dataBuilderConfig", "viewJson", "dataServices")
+    VALUES (
+      'TO_CD_Outcome_Of_Contact_Tracing',
+      'percentagesOfEventCounts',
+      '${JSON.stringify(DATA_BUILDER_CONFIG)}',
+      '${JSON.stringify(VIEW_JSON_CONFIG)}',
+      '[{"isDataRegional": false}]'
+    );
+
+    UPDATE "dashboardGroup"
+    SET "dashboardReports" = "dashboardReports" || '{TO_CD_Outcome_Of_Contact_Tracing}'
+    WHERE code = 'Tonga_Communicable_Diseases_National'
+    AND "organisationLevel" = 'Country';
+  `);
+};
+
+exports.down = async function (db) {
+  await db.runSql(`
+    DELETE FROM "dashboardReport" WHERE id = 'TO_CD_Outcome_Of_Contact_Tracing';
+
+    UPDATE "dashboardGroup"
+    SET "dashboardReports" = array_remove("dashboardReports", 'TO_CD_Outcome_Of_Contact_Tracing')
+    WHERE code = 'Tonga_Communicable_Diseases_National'
+    AND "organisationLevel" = 'Country';
+  `);
+};
+
+exports._meta = {
+  "version": 1
+};
