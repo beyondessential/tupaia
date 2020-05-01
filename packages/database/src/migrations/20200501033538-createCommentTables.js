5b8c1bec804c2128df9e2382212bcbfb374ddeb1
Andrés Proaño <andres@oventi.org>
2020-05-01 15:43:09 +1200

    added migration

---
+++ packages/database/src/migrations/20200501033538-createCommentTables.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.runSql(`
+    CREATE TABLE comment (
+      id TEXT PRIMARY KEY,
+      user_account_id text REFERENCES user_account(id),
+      created_time TIMESTAMPTZ NOT NULL DEFAULT now(),
+      last_modified_time TIMESTAMPTZ NOT NULL DEFAULT now(),
+      text text NOT NULL
+    );
+
+    CREATE TABLE alert_comment (
+      id TEXT PRIMARY KEY,
+      alert_id text REFERENCES alert(id),
+      comment_id text REFERENCES comment(id)
+    );
+  `);
+};
+
+exports.down = function(db) {
+  return db.runSql(`
+    DROP TABLE alert_comment CASCADE;
+    DROP TABLE comment CASCADE;
+  `);
+};
+
+exports._meta = {
+  "version": 1
+};
