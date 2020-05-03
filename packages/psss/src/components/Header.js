4e276291b5e29ca8b7f2ea619c4fdb995d1aae5f
Tom Caiger <caigertom@gmail.com>
2020-05-04 10:08:51 +1200

    Add header component

--- packages/psss/src/components/Header.js
+++ packages/psss/src/components/Header.js
@@ -7,28 +7,76 @@ import MuiContainer from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import { Link as RouterLink } from 'react-router-dom';
+import MuiLink from '@material-ui/core/Link';
+import { SaveAlt, LightOutlinedButton } from '@tupaia/ui-components';
+import MuiAvatar from '@material-ui/core/Avatar';
 import * as COLORS from '../theme/colors';
 
 const HeaderMain = styled.header`
-  background-color: ${props => props.theme.palette.primary.main};
+  background-color: ${COLORS.BLUE};
   color: ${COLORS.WHITE};
 `;
 
-const HeaderInner = styled.nav`
+const HeaderInner = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 3.5rem 0 4rem;
+  height: 190px;
+  padding-bottom: 1.25rem;
 `;
 
-export const Header = ({ title }) => {
+const HeaderBack = styled(MuiLink)`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+  color: ${COLORS.WHITE};
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1rem;
+
+  svg {
+    margin-right: 0.5rem;
+  }
+`;
+
+const HeaderTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Avatar = styled(MuiAvatar)`
+  height: 5rem;
+  width: 5rem;
+  margin-right: 0.9rem;
+`;
+
+const H1 = props => <Typography {...props} variant="h1" component="h1" />;
+
+const StyledH1 = styled(H1)`
+  font-size: 2.8rem;
+  line-height: 3.3rem;
+`;
+
+export const Header = ({ title, avatarUrl, back }) => {
   return (
     <HeaderMain>
       <MuiContainer maxWidth="lg">
         <HeaderInner>
-          <Typography variant="h1" component="h1">
-            {title}
-          </Typography>
+          <div>
+            {back && (
+              <HeaderBack to={back.url} component={RouterLink}>
+                <ArrowBack />
+                Back to {back.title}
+              </HeaderBack>
+            )}
+            <HeaderTitle>
+              {avatarUrl && <Avatar src={avatarUrl} />}
+              {avatarUrl || back ? <StyledH1>{title}</StyledH1> : <H1>{title}</H1>}
+            </HeaderTitle>
+          </div>
+          <LightOutlinedButton startIcon={<SaveAlt />}>Export Data</LightOutlinedButton>
         </HeaderInner>
       </MuiContainer>
     </HeaderMain>
@@ -37,4 +85,11 @@ export const Header = ({ title }) => {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  avatarUrl: PropTypes.string,
+  back: PropTypes.object,
+};
+
+Header.defaultProps = {
+  avatarUrl: undefined,
+  back: undefined,
 };
