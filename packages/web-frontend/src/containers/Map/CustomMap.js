8c463bf3007833514e79fc195b55f69de1c0a358
Bastien Goepfert <bas.goepfert@gmail.com>
2020-05-06 15:58:39 +1000

    use existing changes branch

--- packages/web-frontend/src/containers/Map/CustomMap.js
+++ packages/web-frontend/src/containers/Map/CustomMap.js
@@ -43,12 +43,12 @@ export class CustomMap extends Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    const { measureInfo, currentOrganisationUnit, tileSet, position, innerAreas } = this.props;
+    const { measureInfo, currentOrganisationUnit, tileSet, position, currentChildren } = this.props;
     // Only updates/re-renders when the measure has changed or the orgUnit has changed.
     // These are the only cases where polygons or area tooltips should rerender.
     if (nextProps.measureInfo.measureId !== measureInfo.measureId) return true;
 
-    if (nextProps.innerAreas !== innerAreas) return true;
+    if (nextProps.currentChildren !== currentChildren) return true;
 
     if (
       (nextProps.currentOrganisationUnit || {}).organisationUnitCode !==
@@ -91,9 +91,9 @@ export class CustomMap extends Component {
   }
 
   renderPolygons() {
-    const { innerAreas, currentOrganisationUnitSiblings } = this.props;
+    const { currentChildren, currentOrganisationUnitSiblings } = this.props;
 
-    const areaPolygons = (innerAreas || []).map(area => (
+    const areaPolygons = (currentChildren || []).map(area => (
       <ConnectedPolygon area={area} key={area.organisationUnitCode} isChildArea />
     ));
 
@@ -169,7 +169,7 @@ CustomMap.propTypes = {
   currentOrganisationUnit: PropTypes.object.isRequired,
   measureInfo: PropTypes.object.isRequired,
   /* eslint-enable react/forbid-prop-types */
-  innerAreas: PropTypes.arrayOf(PropTypes.object),
+  currentChildren: PropTypes.arrayOf(PropTypes.object),
 
   shouldSnapToPosition: PropTypes.bool.isRequired,
   position: PropTypes.shape({
@@ -181,5 +181,5 @@ CustomMap.propTypes = {
 };
 
 CustomMap.defaultProps = {
-  innerAreas: [],
+  currentChildren: [],
 };
