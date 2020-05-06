930816dc032fbb7ee57e4e6c06ffe9fe2a816b88
Rohan Port <rohan@beyondessential.com.au>
2020-05-06 17:14:33 +1000

    264: Switched to using `Project.entity_hierarchy_id` in /orgUnit endpoint

--- packages/web-config-server/src/apiV1/organisationUnit.js
+++ packages/web-config-server/src/apiV1/organisationUnit.js
@@ -4,7 +4,7 @@
  */
 
 import { reduceToDictionary } from '@tupaia/utils';
-import { Project, Entity, EntityHierarchy, EntityRelation } from '/models';
+import { Project, Entity, EntityRelation } from '/models';
 import { RouteHandler } from './RouteHandler';
 import { PermissionsChecker } from './permissions';
 
@@ -19,29 +19,27 @@ export default class extends RouteHandler {
   async buildResponse() {
     const { includeCountryData, projectCode = 'explore' } = this.query;
     return includeCountryData === 'true'
-      ? this.getEntityAndCountryHierarchyByCode(projectCode)
+      ? this.getEntityAndCountryDataByCode(projectCode)
       : this.getEntityAndChildrenByCode(projectCode);
   }
 
-  async getEntityAndCountryHierarchyByCode(projectCode) {
+  async getEntityAndCountryDataByCode(projectCode) {
     const project = await Project.findOne({ code: projectCode });
     const projectEntity = await Entity.findOne({ id: project.entity_id });
-    const hierarchy = await EntityHierarchy.findOne({ name: projectCode });
     const country = await this.entity.countryEntity();
-    const countryDescendants = await country.getDescendants(hierarchy.id);
+    const countryDescendants = await country.getDescendants(project.entity_hierarchy_id);
     const orgUnitHierarchy = await this.filterForAccess([
       projectEntity,
       country,
       ...countryDescendants,
     ]);
 
-    const childIdToParentId = hierarchy
-      ? reduceToDictionary(
-          await EntityRelation.find({ entity_hierarchy_id: hierarchy.id }),
-          'child_id',
-          'parent_id',
-        )
-      : {};
+    const childIdToParentId = reduceToDictionary(
+      await EntityRelation.find({ entity_hierarchy_id: project.entity_hierarchy_id }),
+      'child_id',
+      'parent_id',
+    );
+
     // Fill in childIdToParentId with missing org units
     orgUnitHierarchy.forEach(({ id, parent_id: parentId }) => {
       if (!childIdToParentId[id]) {
@@ -59,11 +57,11 @@ export default class extends RouteHandler {
   }
 
   async getEntityAndChildrenByCode(projectCode) {
-    const hierarchy = await EntityHierarchy.findOne({ name: projectCode });
+    const project = await Project.findOne({ code: projectCode });
 
     // Don't check parent permission (as we already know we have permission for at least one of its children)
     const parent = await this.entity.parent();
-    const allChildren = await this.entity.getChildren(hierarchy.id);
+    const allChildren = await this.entity.getChildren(project.entity_hierarchy_id);
     const children = await this.filterForAccess(allChildren);
 
     return {
