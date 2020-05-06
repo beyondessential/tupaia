7c68cc8f9cba1f431df9c53d8cc400487b84ca84
Edwin <edmofro@gmail.com>
2020-05-06 14:39:54 +1200

    Remove example test

--- packages/auth/src/tests/example.test.js
+++
@@ -1,5 +0,0 @@
-// TODO, move access policy building tests in here once
-// https://github.com/beyondessential/tupaia-backlog/issues/436 is done
-describe('example', () => {
-  it('should do something', () => {});
-});
