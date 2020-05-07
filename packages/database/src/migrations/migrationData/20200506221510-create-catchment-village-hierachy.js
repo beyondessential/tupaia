277e15931270731e7f66e079786a3211e10f0088
nick <nickw@ourcomunity.com.au>
2020-05-07 12:39:20 +1000

    Adding catchments WIP

---
+++ packages/database/src/migrations/migrationData/20200506221510-create-catchment-village-hierachy.js
@@ -0,0 +1,90 @@
+export const FIJI_ENTITIES = [
+  /**
+   * Catchments
+   */
+  // Dawasamu Catchment
+  {
+    code: 'FJ_catch_Dawasamu',
+    parent_code: 'FJ',
+    name: 'Dawasamu Catchment',
+    type: 'catchment',
+    region: {
+      type: 'MultiPolygon',
+      coordinates: [
+        [
+          [
+            //[107.520737, 14.656896],
+          ],
+        ],
+      ],
+    },
+  },
+  //Waibula Catchment
+  {
+    code: 'FJ_catch_Waibula',
+    parent_code: 'FJ',
+    name: 'Waibula Catchment',
+    type: 'catchment',
+    region: {
+      type: 'MultiPolygon',
+      coordinates: [
+        [
+          [
+            //[107.520737, 14.656896],
+          ],
+        ],
+      ],
+    },
+  },
+  // Namosi Catchment
+  {
+    code: 'FJ_catch_Namosi',
+    parent_code: 'FJ',
+    name: 'Namosi Catchment',
+    type: 'catchment',
+    region: {
+      type: 'MultiPolygon',
+      coordinates: [
+        [
+          [
+            //[107.520737, 14.656896],
+          ],
+        ],
+      ],
+    },
+  },
+  // Dama Catchment
+  {
+    code: 'FJ_catch_Dama',
+    parent_code: 'FJ',
+    name: 'Dama Catchment',
+    type: 'catchment',
+    region: {
+      type: 'MultiPolygon',
+      coordinates: [
+        [
+          [
+            //[107.520737, 14.656896],
+          ],
+        ],
+      ],
+    },
+  },
+  // Bureta (Ovalau Island) Catchment
+  {
+    code: 'FJ_catch_Bureta',
+    parent_code: 'FJ',
+    name: 'Bureta (Ovalau Island) Catchment',
+    type: 'catchment',
+    region: {
+      type: 'MultiPolygon',
+      coordinates: [
+        [
+          [
+            //[107.520737, 14.656896],
+          ],
+        ],
+      ],
+    },
+  },
+];
