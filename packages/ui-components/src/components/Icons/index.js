d0a64f8b3d3653d26ca7011cccd3dbdb245b0a96
Tom Caiger <caigertom@gmail.com>
2020-05-08 11:38:57 +1200

    hot fix add missing icons to ui components

--- packages/ui-components/src/components/Icons/index.js
+++ packages/ui-components/src/components/Icons/index.js
@@ -3,9 +3,11 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
+export * from './CalendarToday';
 export * from './Clipboard';
 export * from './CircleTick';
 export * from './Dashboard';
 export * from './Home';
-export * from './Virus';
+export * from './SaveAlt';
 export * from './WarningCloud';
+export * from './Virus';
