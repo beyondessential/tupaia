d73e8af5762f9eb7ab85c0d4ac291fc3f8a3924c
Andrés Proaño <andres@oventi.org>
2020-05-07 17:47:15 +1200

    started api layer

--- packages/psss/src/components/index.js
+++ packages/psss/src/components/index.js
@@ -3,6 +3,13 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
+import { getApi } from '../api';
+
+(async () => {
+  const api = getApi();
+  console.log('a!');
+})();
+
 export * from './NavBar';
 export * from './Footer';
 export * from './Layout';
