b918115cb9a81738130103de4de5b85371245cd1
nick <nickw@ourcomunity.com.au>
2020-05-07 16:09:25 +1000

    [WIP] import

---
+++ packages/database/src/migrations/migrationData/temp.js
@@ -0,0 +1,12 @@
+export const FIJI_ENTITIES = [
+  /**
+   * Catchments
+   */
+  // Dawasamu Catchment (FD 0 from 3VTL (temp needs checking))
+  {
+    code: 'FJ_catch_Dawasamu',
+    parent_code: 'FJ',
+    name: 'Dawasamu Catchment',
+    type: 'catchment',
+  },
+];
