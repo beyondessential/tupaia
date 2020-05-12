8c787c984adbd6e1c65db6dad7deed3246a51088
Edwin <edmofro@gmail.com>
2020-05-13 10:51:34 +1200

    Make way of naming models consistent

--- packages/database/src/modelClasses/index.js
+++ packages/database/src/modelClasses/index.js
@@ -3,13 +3,13 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { DataElementDataGroupModel as DataElementDataGroup } from './DataElementDataGroup';
-import { DataSourceModel as DataSource } from './DataSource';
+import { DataElementDataGroupModel } from './DataElementDataGroup';
+import { DataSourceModel } from './DataSource';
 import { EntityModel } from './Entity';
 
 // export all models to be used in constructing a ModelRegistry
 export const modelClasses = {
-  DataElementDataGroup,
-  DataSource,
+  DataElementDataGroup: DataElementDataGroupModel,
+  DataSource: DataSourceModel,
   Entity: EntityModel,
 };
