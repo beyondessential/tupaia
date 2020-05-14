20420edf0852d4ba5d25fc7f163b781827d5c913
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-05-14 10:42:47 +1000

    Add a few simple models in @tupaia/database

---
+++ packages/database/src/modelClasses/User.js
@@ -0,0 +1,18 @@
+/**
+ * Tupaia MediTrak
+ * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
+ **/
+
+import { DatabaseModel } from '../DatabaseModel';
+import { DatabaseType } from '../DatabaseType';
+import { TYPES } from '../types';
+
+class UserType extends DatabaseType {
+  static databaseType = TYPES.USER_ACCOUNT;
+}
+
+export class UserModel extends DatabaseModel {
+  get DatabaseTypeClass() {
+    return UserType;
+  }
+}
