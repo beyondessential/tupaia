10a8647e6ac43b1b6ffa65e000da5d676978ccea
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-05-18 21:21:04 +1000

    464: Added more available data elements

--- packages/database/src/migrations/20200513063247-AddLaosSchoolBinaryMeasureMapOverlays.js
+++ packages/database/src/migrations/20200513063247-AddLaosSchoolBinaryMeasureMapOverlays.js
@@ -16,31 +16,17 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-const GROUP_NAME = 'Laos Schools';
-
-const USER_GROUP = 'Laos Schools User';
-
-const DISPLAY_TYPE = 'color';
-
-const MEASURE_BUILDER = 'valueForOrgGroup';
-
-const MEASURE_BUILDER_CONFIG =  {
-  aggregationEntityType: 'school'
-};
-
-const COUNTRY_CODES = '{"LA"}';
-
-const LAOS_SCHOOL_MAP_OVERLAYS = [
-  // {
-  //   id: 'Laos_Schools_Electricity_Available',
-  //   name: 'Schools Used as Quarantine Centres',
-  //   dataElementCode: 'SchQuar001',
-  // },
-  // {
-  //   id: 'Laos_Schools_Internet_Connection_Available_In_School',
-  //   name: 'Internet connection available in school',
-  //   dataElementCode: 'SchQuar001',
-  // },
+const LAOS_SCHOOL_BINARY_MEASURE_MAP_OVERLAYS = [
+  {
+    id: 'Laos_Schools_Electricity_Available',
+    name: 'Electricity available in school',
+    dataElementCode: 'SchFF001',
+  },
+  {
+    id: 'Laos_Schools_Internet_Connection_Available_In_School',
+    name: 'Internet connection available in school',
+    dataElementCode: 'SchFF002',
+  },
   {
     id: 'Laos_Schools_Functioning_Water_Supply',
     name: 'Functioning water supply',
@@ -51,79 +37,98 @@ const LAOS_SCHOOL_MAP_OVERLAYS = [
     name: 'Functioning toilet (vs. unusable)',
     dataElementCode: 'BCD32_event',
   },
-  // {
-  //   id: 'Laos_Schools_Hand_Washing_Facility_Available',
-  //   name: 'Hand washing facility available',
-  //   dataElementCode: 'SchQuar001',
-  // },
-  // {
-  //   id: 'Laos_Schools_Schools_Received_Learning_Materials',
-  //   name: 'Schools that received (hard copies of) learning materials for (remote) communities with limited internet and TV access',
-  //   dataElementCode: 'SchQuar001',
-  // },
+  {
+    id: 'Laos_Schools_Hand_Washing_Facility_Available',
+    name: 'Hand washing facility available',
+    dataElementCode: 'SchFF004',
+  },
+  {
+    id: 'Laos_Schools_Schools_Received_Learning_Materials',
+    name: 'Schools that received (hard copies of) learning materials for (remote) communities with limited internet and TV access',
+    dataElementCode: 'SchFF008',
+  },
   {
     id: 'Laos_Schools_Schools_Used_As_Quarantine',
     name: 'Schools used as quarantine centres',
     dataElementCode: 'SchQuar001',
   },
-  // {
-  //   id: 'Laos_Schools_Schools_Provided_With_Cleaning_Materials',
-  //   name: 'Schools provided with cleaning/disinfecting materials and guidance provided on their use',
-  //   dataElementCode: 'SchQuar001',
-  // },
-  // {
-  //   id: 'Laos_Schools_Schools_Provided_With_Hygiene_Kids',
-  //   name: 'Schools provided with hygiene kids',
-  //   dataElementCode: 'SchQuar001',
-  // },
-  // {
-  //   id: 'Laos_Schools_Schools_Received_Training_On_Safe_Protocols',
-  //   name: 'Schools received training on safe school protocols (COVID-19 prevention and control)',
-  //   dataElementCode: 'SchQuar001',
-  // },
-  // {
-  //   id: 'Laos_Schools_Schools_Implementing_Remedial_Education_Programmes',
-  //   name: 'Schools implementing remedial education programmes',
-  //   dataElementCode: 'SchQuar001',
-  // },
-  // {
-  //   id: 'Laos_Schools_Schools_Provided_With_Psychosocial_Support',
-  //   name: 'Schools provided with psychosocial support',
-  //   dataElementCode: 'SchQuar001',
-  // }
+  {
+    id: 'Laos_Schools_Schools_Provided_With_Cleaning_Materials',
+    name: 'Schools provided with cleaning/disinfecting materials and guidance provided on their use',
+    dataElementCode: 'SchFF009',
+  },
+  {
+    id: 'Laos_Schools_Schools_Provided_With_Hygiene_Kids',
+    name: 'Schools provided with hygiene kids',
+    dataElementCode: 'SchFF009a',
+  },
+  {
+    id: 'Laos_Schools_Schools_Received_Training_On_Safe_Protocols',
+    name: 'Schools received training on safe school protocols (COVID-19 prevention and control)',
+    dataElementCode: 'SchFF010',
+  },
+  {
+    id: 'Laos_Schools_Schools_Implementing_Remedial_Education_Programmes',
+    name: 'Schools implementing remedial education programmes',
+    dataElementCode: 'SchFF011',
+  },
+  {
+    id: 'Laos_Schools_Schools_Provided_With_Psychosocial_Support',
+    name: 'Schools provided with psychosocial support',
+    dataElementCode: 'SchFF016',
+  }
 ]
+
+const GROUP_NAME = 'Laos Schools';
+
+const USER_GROUP = 'Laos Schools User';
+
+const DISPLAY_TYPE = 'color';
+
+const MEASURE_BUILDER = 'valueForOrgGroup';
+
+const MEASURE_BUILDER_CONFIG =  {
+  dataSourceEntityType: 'school',
+  aggregationEntityType: 'school'
+};
+
+const COUNTRY_CODES = '{"LA"}';
+
+const VALUES = [
+  {
+      "name": "Yes",
+      "color": "green",
+      "value": 'Yes'
+  },
+  {
+      "name": "No",
+      "color": "red",
+      "value": ['No', "null"]
+  }
+];
+
+const PRESENTATION_OPTIONS =  {
+  hideByDefault: {
+    "No,null": true
+  },
+  displayOnLevel: 'District',
+};
 
 const BASIC_LAOS_SCHOOL_MAP_OVERLAY_OBJECT = {
   groupName: GROUP_NAME,
   userGroup: USER_GROUP,
   displayType: DISPLAY_TYPE,
   isDataRegional: true,
-  values: [
-    {
-        "name": "Yes",
-        "color": "green",
-        "value": 1
-    },
-    {
-        "name": "No",
-        "color": "red",
-        "value": [0, "null"]
-    }
-  ],
+  values: VALUES,
   measureBuilder: MEASURE_BUILDER,
   measureBuilderConfig: MEASURE_BUILDER_CONFIG,
-  presentationOptions: {
-    hideByDefault: {
-      "0,null": true
-    },
-    displayOnLevel: 'District',
-  },
+  presentationOptions: PRESENTATION_OPTIONS,
   countryCodes: COUNTRY_CODES,
 };
 
 exports.up = async function(db) {
   await Promise.all(
-    LAOS_SCHOOL_MAP_OVERLAYS.map((overlay, index) => {
+    LAOS_SCHOOL_BINARY_MEASURE_MAP_OVERLAYS.map((overlay, index) => {
       const { name, id, dataElementCode } = overlay;
       return insertObject(db, 'mapOverlay', {
         ...BASIC_LAOS_SCHOOL_MAP_OVERLAY_OBJECT,
@@ -139,7 +144,7 @@ exports.up = async function(db) {
 exports.down = function(db) {
   return db.runSql(`	
     DELETE FROM "mapOverlay" 
-    WHERE "id" in (${arrayToDbString(LAOS_SCHOOL_MAP_OVERLAYS.map(o => o.id))});	
+    WHERE "id" in (${arrayToDbString(LAOS_SCHOOL_BINARY_MEASURE_MAP_OVERLAYS.map(o => o.id))});	
   `);
 };
 
