af4b622d875d01e96ada152d4b2c336bf004b68b
Andrés Proaño <andres@oventi.org>
2020-05-19 01:01:51 +1200

    fixed createComment

--- packages/meditrak-server/src/tests/testUtilities/database/createComment.js
+++ packages/meditrak-server/src/tests/testUtilities/database/createComment.js
@@ -7,10 +7,10 @@ const models = getModels();
 export const createComment = async ({ alert_id, user_account_id, text }) => {
   const comment = await upsertRecord(models.comment, { user_account_id, text });
 
-  await upsertRecord(models.alertComment, { alert_id, comment_id: comment.id });
+  await upsertRecord(models.alertComment, {
     id: generateTestId(),
     alert_id,
-    comment_id: comment.id,
+    comment_id: comment.id
   });
 
   return comment;
