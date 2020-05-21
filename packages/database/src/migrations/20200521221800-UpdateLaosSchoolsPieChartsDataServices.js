07b1a49ab752fb5e322bf3eeaeff3d6f98e7c971
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-05-22 09:17:28 +1000

    fixed name of pie chart

--- packages/database/src/migrations/20200521221800-UpdateLaosSchoolsPieChartsDataServices.js
+++ packages/database/src/migrations/20200521221800-UpdateLaosSchoolsPieChartsDataServices.js
@@ -18,6 +18,10 @@ exports.setup = function(options, seedLink) {
 
 const OLD_DATA_SERVICES = [{ "isDataRegional": false }];
 const NEW_DATA_SERVICES = [{ "isDataRegional": true }];
+
+const NEW_GENDER_REPORT_NAME = 'Number of Students by Gender';
+const OLD_GENDER_REPORT_NAME = 'Male/Female';
+
 const REPORT_IDS = ['Laos_Schools_Male_Female', 'Laos_Schools_Language_Of_Students'];
 
 exports.up = async function(db) {
@@ -25,6 +29,10 @@ exports.up = async function(db) {
     update "dashboardReport"
     set "dataServices" = '${JSON.stringify(NEW_DATA_SERVICES)}'
     where id in (${arrayToDbString(REPORT_IDS)});
+
+    update "dashboardReport"
+    set "viewJson" = jsonb_set("viewJson", '{name}', '"${NEW_GENDER_REPORT_NAME}"')
+    where id = 'Laos_Schools_Male_Female';
   `);
 };
 
@@ -33,6 +41,10 @@ exports.down = async function(db) {
     update "dashboardReport"
     set "dataServices" = '${JSON.stringify(OLD_DATA_SERVICES)}'
     where id in (${arrayToDbString(REPORT_IDS)});
+
+    update "dashboardReport"
+    set "viewJson" = jsonb_set("viewJson", '{name}', '"${OLD_GENDER_REPORT_NAME}"')
+    where id = 'Laos_Schools_Male_Female';
   `);
 };
 
