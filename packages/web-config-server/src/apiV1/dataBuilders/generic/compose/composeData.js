cb2e61d2a6e1ee76c19d3ca8dd3d6b50b46f7935
monty-wanty <lockealastair@gmail.com>
2020-05-22 10:36:43 +1000

    475 Add example

--- packages/web-config-server/src/apiV1/dataBuilders/generic/compose/composeData.js
+++ packages/web-config-server/src/apiV1/dataBuilders/generic/compose/composeData.js
@@ -6,6 +6,24 @@ import { NO_DATA_AVAILABLE } from '../../constants'
  * @property {string} sortOrder
  * @property {Object<string, { dataBuilder, dataBuilderConfig }>} dataBuilders
  *
+ * Example:
+ * ```
+ * {
+ *   "dataBuilders": {
+ *        builder1: {
+ *            sortOrder: 1,
+ *            dataBuilder: <some databuilder>,
+ *            dataBuilderConfig: <some config>,
+ *        },
+ *        builder2: {
+ *            sortOrder: 2,
+ *            dataBuilder: <some databuilder>,
+ *            dataBuilderConfig: <some config>,
+ *        }
+ *    }
+ * }
+ * ```
+ * Will return [{ name: builder1, ... }, { name: builder2, ... }]
  */
 
 export const composeData = async (config, aggregator, dhisApi) => {
