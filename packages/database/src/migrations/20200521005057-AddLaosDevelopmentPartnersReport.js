1076aba51c5aaf087711bcbfd55b9d104c82b5e0
Bastien Goepfert <bas.goepfert@gmail.com>
2020-05-27 15:04:09 +1000

    fix migration errors

--- packages/database/src/migrations/20200521005057-AddLaosDevelopmentPartnersReport.js
+++ packages/database/src/migrations/20200521005057-AddLaosDevelopmentPartnersReport.js
@@ -207,8 +207,8 @@ exports.up = function(db) {
     );
 
     update "dashboardGroup"
-    set "dashboardReports" = "dashboardReports" || {'SchDP_Partner_Assistance_Types'}
-    where code in (LA_Laos_Schools_Province_Laos_Schools_User, LA_Laos_Schools_District_Laos_Schools_User, LA_Laos_Schools_Country_Laos_Schools_User);
+    set "dashboardReports" = "dashboardReports" || '{"SchDP_Partner_Assistance_Types"}'
+    where code in ('LA_Laos_Schools_Province_Laos_Schools_User', 'LA_Laos_Schools_District_Laos_Schools_User', 'LA_Laos_Schools_Country_Laos_Schools_User');
   `);
 };
 
@@ -216,7 +216,7 @@ exports.down = function(db) {
   return db.runSql(`
     update "dashboardGroup"
     set "dashboardReports" = array_remove("dashboardReports", 'SchDP_Partner_Assistance_Types')
-        where code in (LA_Laos_Schools_Province_Laos_Schools_User, LA_Laos_Schools_District_Laos_Schools_User, LA_Laos_Schools_Country_Laos_Schools_User);
+    where code in ('LA_Laos_Schools_Province_Laos_Schools_User', 'LA_Laos_Schools_District_Laos_Schools_User', 'LA_Laos_Schools_Country_Laos_Schools_User');
       
     delete from "dashboardReport" where id = 'SchDP_Partner_Assistance_Types';
   `);
