ecd35258761a5d6095798dc87bd1c967d06c1e93
Tom Caiger <caigertom@gmail.com>
2020-05-27 14:00:02 +1200

    psss drawer component (#674)
    * started api layer
    
    * Revert "started api layer"
    
    This reverts commit d73e8af5762f9eb7ab85c0d4ac291fc3f8a3924c.
    
    * basic preliminary redux implementation
    
    * add login view
    
    * add private route
    
    * add auth to redux store and update login form to use store state
    
    * continue api integration
    
    * minimal api login and request within a component
    
    * refactored based on PR
    
    * Add logging around test database migration
    
    * update login form styles and integrate login form with real api
    
    * remove admin panel auth handlers
    
    * update psss auth
    
    * remove example code
    
    * update test data
    
    * remove temp code
    
    * update test data
    
    * style tweaks
    
    * remove un-used code
    
    * remove redundant code
    
    * remove unused code
    
    * update form styles
    
    * update alert
    
    * update px to rems
    
    * update profile button
    
    * update form styles
    
    * add profile button to psss
    
    * remove old auth folder
    
    * login form tweaks
    
    * tweak auth gateway
    
    * add drawer component
    
    * update site address component
    
    * fix formatting
    
    * add drawer header content component
    
    * add weekly report pane
    
    * update placeholder text
    
    Co-authored-by: Andrés Proaño <andres@oventi.org>
    Co-authored-by: Edwin <edmofro@gmail.com>

---
+++ packages/psss/src/components/WeeklyReportPane.js
@@ -0,0 +1,98 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Button, ErrorAlert, ButtonSelect } from '@tupaia/ui-components';
+import * as COLORS from '../theme/colors';
+import { Drawer, DrawerHeaderContent, DrawerFooter, DrawerHeader } from './Drawer';
+import { SiteAddress } from './SiteAddress';
+
+// dummy data
+const options = [
+  { name: 'Afghanistan', id: 'AF' },
+  { name: 'Albania', id: 'AL' },
+  { name: 'Algeria', id: 'DZ' },
+  { name: 'Angola', id: 'AO' },
+  { name: 'Anguilla', id: 'AI' },
+  { name: 'Antarctica', id: 'AQ' },
+  { name: 'Argentina', id: 'AR' },
+  { name: 'Armenia', id: 'AM' },
+  { name: 'Aruba', id: 'AW' },
+  { name: 'Australia', id: 'AU' },
+  { name: 'Austria', id: 'AT' },
+];
+
+const address = {
+  name: 'Tafuna Health Clinic',
+  district: 'Tafuna Western District 96799,',
+  country: 'American Samoa',
+};
+
+const contact = {
+  name: 'Shakila Naidu',
+  department: 'Ministry of Health',
+  email: 'Shakila@gmail.com',
+};
+
+const GreySection = styled.section`
+  background: ${COLORS.LIGHTGREY};
+  box-shadow: 0 1px 0 ${COLORS.GREY_DE};
+  padding: 25px 20px;
+`;
+
+const MainSection = styled.section`
+  padding: 30px 20px;
+`;
+
+const WeeklyReportsPaneSubmitButton = () => {
+  const handleClick = () => {
+    console.log('submit...');
+  };
+  return (
+    <Button fullWidth onClick={handleClick}>
+      Submit now
+    </Button>
+  );
+};
+
+export const WeeklyReportPane = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (event, isOpen) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(isOpen);
+  };
+
+  const handleOpen = event => toggleDrawer(event, true);
+
+  const handleClose = event => toggleDrawer(event, false);
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpen}>Review and Confirm Now</Button>
+      <Drawer open={open} onClose={handleClose}>
+        <DrawerHeader heading="Upcoming report" onClose={handleClose}>
+          <DrawerHeaderContent heading="American Samoa" date="Week 9 Feb 25 - Mar 1, 2020" />
+        </DrawerHeader>
+        <ErrorAlert>ILI Above Threshold. Please review and verify data.</ErrorAlert>
+        <GreySection>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur laudantium non odit
+          perspiciatis quidem, repellat voluptates! Architecto dolorum ducimus ea eum ipsam, iusto
+          laborum nulla quam! Hic laudantium reiciendis sapiente.
+        </GreySection>
+        <MainSection>
+          <ButtonSelect id="button-select" options={options} />
+          <SiteAddress address={address} contact={contact} />
+        </MainSection>
+        <DrawerFooter
+          Action={WeeklyReportsPaneSubmitButton}
+          helperText="Verify data to submit Weekly Report to Regional"
+        />
+      </Drawer>
+    </React.Fragment>
+  );
+};
