0badfc5da02c91b8e6ea657e56d79b3601bebc45
monty-wanty <lockealastair@gmail.com>
2020-06-05 14:47:29 +1000

    581 Remove deleted functionality from index files

--- packages/web-config-server/src/apiV1/measureBuilders/index.js
+++ packages/web-config-server/src/apiV1/measureBuilders/index.js
@@ -6,6 +6,5 @@ export { groupEventsPerOrgUnit } from './groupEventsPerOrgUnit';
 export { mostRecentValueFromChildren } from './mostRecentValueFromChildren';
 export { sumLatestPerOrgUnit, sumAllPerOrgUnit } from './sumPerOrgUnit';
 export { valueForOrgGroup } from './valueForOrgGroup';
-export { groupSumDataPerOrgUnit } from './groupDataPerOrgUnit';
 export { groupData } from './groupData'
 export { getStringsFromBinaryData } from './getStringsFromBinaryData';
--- packages/web-config-server/src/apiV1/utils/index.js
+++ packages/web-config-server/src/apiV1/utils/index.js
@@ -33,7 +33,6 @@ export { timestampToPeriod } from './timestampToPeriod';
 export { timestampToPeriodName } from './timestampToPeriodName';
 export { limitRange } from './limitRange';
 export { regexLabel } from './regexLabel';
-export { findLatestPeriod } from './findLatestPeriod';
 export { filterEntities } from './filterEntities';
 export { getDateRange } from './getDateRange';
 export { mapDataSourcesToElementCodes } from './mapDataSourcesToElementCodes';
