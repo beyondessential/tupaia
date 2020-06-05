4b52a4252eb0e0bd85ae399d76593cb52487b754
Edwin <edmofro@gmail.com>
2020-06-05 15:12:36 +1200

    Merge model classes

--- packages/database/src/modelClasses/index.js
+++ packages/database/src/modelClasses/index.js
@@ -3,21 +3,39 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
+import { AnswerModel } from './Answer';
 import { CountryModel } from './Country';
 import { DataElementDataGroupModel } from './DataElementDataGroup';
 import { DataSourceModel } from './DataSource';
+import { EntityModel } from './Entity';
 import { GeographicalAreaModel } from './GeographicalArea';
-import { RefreshTokenModel } from './RefreshToken';
 import { MeditrakDeviceModel } from './MeditrakDevice';
+import { PermissionGroupModel } from './PermissionGroup';
+import { QuestionModel } from './Question';
+import { RefreshTokenModel } from './RefreshToken';
+import { SurveyModel } from './Survey';
+import { SurveyResponseModel } from './SurveyResponse';
+import { SurveyScreenComponentModel } from './SurveyScreenComponent';
+import { SurveyScreenModel } from './SurveyScreen';
+import { UserModel } from './User';
 
 // export all models to be used in constructing a ModelRegistry
 export const modelClasses = {
+  Answer: AnswerModel,
   Country: CountryModel,
   DataElementDataGroup: DataElementDataGroupModel,
   DataSource: DataSourceModel,
+  Entity: EntityModel,
   GeographicalArea: GeographicalAreaModel,
   MeditrakDevice: MeditrakDeviceModel,
+  PermissionGroup: PermissionGroupModel,
+  Question: QuestionModel,
   RefreshToken: RefreshTokenModel,
+  Survey: SurveyModel,
+  SurveyResponse: SurveyResponseModel,
+  SurveyScreen: SurveyScreenModel,
+  SurveyScreenComponent: SurveyScreenComponentModel,
+  User: UserModel,
 };
 
 // export any models and types that are extended in other packages
