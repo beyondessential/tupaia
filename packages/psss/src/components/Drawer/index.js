498209afe641264b158b19355a68962fcc818e5b
Tom Caiger <caigertom@gmail.com>
2020-06-22 17:15:40 +1200

    fix drawer and webpack

--- packages/psss/src/components/Drawer/index.js
+++ packages/psss/src/components/Drawer/index.js
@@ -3,7 +3,6 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-export * from './AlertsDrawerHeader';
 export * from './Drawer';
 export * from './DrawerTray';
 export * from './DrawerHeader';
