ea7e4ff8f207fde205731c2ef4e682693ff23524
Andrés Proaño <andres@oventi.org>
2020-06-26 12:04:29 +1200

    removed commented code

--- packages/meditrak-server/src/dataAccessors/editAccessRequest.js
+++ packages/meditrak-server/src/dataAccessors/editAccessRequest.js
@@ -11,18 +11,6 @@ export const editAccessRequest = async (models, id, updatedFields, { userId }) =
     throw new ValidationError(`AccessRequest has already been processed`);
   }
 
-/*
-OLD
-approving_user_id text DEFAULT NULL,
-approval_note text DEFAULT NULL,
-approval_date TIMESTAMPTZ DEFAULT NULL,
-
-NEW
-processed_by text DEFAULT NULL,
-note text DEFAULT NULL,
-processed_date TIMESTAMPTZ DEFAULT NULL,
-*/
-
   return models.accessRequest.updateById(id, {
     ...updatedFields,
     processed_by: userId,
