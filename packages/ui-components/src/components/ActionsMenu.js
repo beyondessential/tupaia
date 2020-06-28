51a8509aba141f20097f12dfcf0c070cb86ec236
Tom Caiger <caigertom@gmail.com>
2020-06-29 09:42:00 +1200

    add UserMessage components

--- packages/ui-components/src/components/ActionsMenu.js
+++ packages/ui-components/src/components/ActionsMenu.js
@@ -1,51 +1,101 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import MuiMenu from '@material-ui/core/Menu';
+import MuiMenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import styled from 'styled-components';
+
+const StyledMenu = styled(MuiMenu)`
+  .MuiPaper-root {
+    position: relative;
+    width: 8.5rem;
+    border: 1px solid ${props => props.theme.palette.grey['400']};
+    border-radius: 3px;
+    overflow: visible;
+
+    .MuiList-root {
+      padding-top: 0.25rem;
+      padding-bottom: 0.25rem;
+    }
+
+    &:before,
+    &:after {
+      position: absolute;
+      content: '';
+      right: 15px;
+      border-right: 10px solid transparent;
+      border-left: 10px solid transparent;
+    }
+
+    &:before {
+      top: -9px;
+      z-index: 1;
+      border-bottom: 10px solid white;
+    }
+
+    &:after {
+      top: -10px;
+      z-index: -1;
+      border-bottom: 10px solid ${props => props.theme.palette.grey['400']};
+    }
+  }
+`;
+
+const StyledMenuItem = styled(MuiMenuItem)`
+  padding-top: 0.625rem;
+  padding-bottom: 0.625rem;
+
+  :not(:last-child) {
+    border-bottom: 1px solid ${props => props.theme.palette.grey['400']};
+  }
+`;
+
+const StyledMenuIcon = styled(MoreVertIcon)`
+  color: ${props => props.theme.palette.text.tertiary};
+
+  &:hover {
+    color: ${props => props.theme.palette.text.primary};
+  }
+`;
 
 export const ActionsMenu = ({ options }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const handlers = {
-    click: event => setAnchorEl(event.currentTarget),
-    close: () => setAnchorEl(null),
-  };
-
   return (
-    <div>
-      <IconButton
-        aria-label="more"
-        aria-controls="long-menu"
-        aria-haspopup="true"
-        onClick={handlers.click}
-      >
-        <MoreVertIcon />
+    <>
+      <IconButton onClick={event => setAnchorEl(event.currentTarget)}>
+        <StyledMenuIcon />
       </IconButton>
-      <Menu
+      <StyledMenu
         keepMounted
         disablePortal
         getContentAnchorEl={null}
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
-        onClose={handlers.close}
+        onClose={() => setAnchorEl(null)}
         anchorOrigin={{
           vertical: 'bottom',
-          horizontal: -60,
+          horizontal: -85,
         }}
-        PaperProps={{ style: { width: '135px' } }}
       >
         {options.map(({ label, action }) => (
-          <MenuItem
+          <StyledMenuItem
             key={label}
             onClick={() => {
               action();
-              return handlers.close();
+              setAnchorEl(null);
             }}
           >
             {label}
-          </MenuItem>
+          </StyledMenuItem>
         ))}
-      </Menu>
-    </div>
+      </StyledMenu>
+    </>
   );
 };
 
---
+++ packages/ui-components/src/components/UserMessage/UserMessageFooter.js
@@ -0,0 +1,57 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import { format } from 'date-fns';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FlexSpaceBetween } from '../Layout';
+
+const FooterContainer = styled.div`
+  padding: 0 1.25rem;
+`;
+
+const FooterInner = styled(FlexSpaceBetween)`
+  padding: 0.5rem 0 0.7rem;
+  border-top: 1px solid ${props => props.theme.palette.grey['400']};
+`;
+
+const FooterText = styled(Typography)`
+  font-size: 0.6875rem;
+  line-height: 0.8rem;
+  color: #888888;
+  font-style: italic;
+`;
+
+const FooterUser = styled(FooterText)`
+  margin-left: 0.5rem;
+  color: ${props => props.theme.palette.text.primary};
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const FooterAvatar = styled(Avatar)`
+  width: 1.25rem;
+  height: 1.25rem;
+`;
+
+export const UserMessageFooter = ({ dateTime, user }) => (
+  <FooterContainer>
+    <FooterInner>
+      <FooterText>Last updated on: {format(dateTime, 'dd/MM/yyyy - hh:mm a')}</FooterText>
+      <FlexSpaceBetween>
+        <FooterAvatar src={user.avatar} />
+        <FooterUser>{user.name}</FooterUser>
+      </FlexSpaceBetween>
+    </FooterInner>
+  </FooterContainer>
+);
+
+UserMessageFooter.propTypes = {
+  user: PropTypes.object.isRequired,
+  dateTime: PropTypes.instanceOf(Date).isRequired,
+};
