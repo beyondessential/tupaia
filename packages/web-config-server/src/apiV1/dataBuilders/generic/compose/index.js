b01b6ccf606aeadf87337e500cecfd66a7085f6f
nick <nickw@ourcomunity.com.au>
2020-06-30 09:07:13 +1000

    added index reference

--- packages/web-config-server/src/apiV1/dataBuilders/generic/compose/index.js
+++ packages/web-config-server/src/apiV1/dataBuilders/generic/compose/index.js
@@ -2,3 +2,4 @@ export { composeDataPerPeriod } from './composeDataPerPeriod';
 export { composePercentagesPerPeriod } from './composePercentagesPerPeriod';
 export { composeDataPerOrgUnit } from './composeDataPerOrgUnit';
 export { composeData } from './composeData';
+export { composePercentageInGroupByDataClass } from './composePercentageInGroupByDataClass';
