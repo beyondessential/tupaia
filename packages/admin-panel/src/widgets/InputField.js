b75b78daf1173da145f3028f89771aa615489ea4
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-06-30 17:24:27 +1000

    Support enum input fields

--- packages/admin-panel/src/widgets/InputField.js
+++ packages/admin-panel/src/widgets/InputField.js
@@ -13,11 +13,23 @@ import { Autocomplete } from '../autocomplete';
 import { JsonInputField } from './JsonInputField';
 import { DropDownInputField } from './DropDownInputField';
 
+const getInputType = ({ options, optionsEndpoint, type }) => {
+  if (options) {
+    return 'enum';
+  }
+  if (optionsEndpoint) {
+    return 'autocomplete';
+  }
+  return type;
+};
+
 export const InputField = ({
   allowMultipleValues,
   label,
   value,
+  recordData,
   inputKey,
+  options,
   optionsEndpoint,
   optionLabelKey,
   optionValueKey,
@@ -27,7 +39,7 @@ export const InputField = ({
   disabled,
   getJsonFieldSchema,
 }) => {
-  const inputType = optionsEndpoint ? 'autocomplete' : type;
+  const inputType = getInputType({ options, optionsEndpoint, type });
   let inputComponent = null;
 
   switch (inputType) {
@@ -56,9 +68,20 @@ export const InputField = ({
       inputComponent = (
         <JsonInputField
           value={value}
+          recordData={recordData}
           onChange={inputValue => onChange(inputKey, inputValue)}
           disabled={disabled}
           getJsonFieldSchema={getJsonFieldSchema}
+        />
+      );
+      break;
+    case 'enum':
+      inputComponent = (
+        <DropDownInputField
+          value={value}
+          options={options.map(option => ({ label: option, value: option }))}
+          onChange={selectedOption => onChange(inputKey, selectedOption)}
+          disabled={disabled}
         />
       );
       break;
@@ -124,7 +147,9 @@ InputField.propTypes = {
   allowMultipleValues: PropTypes.bool,
   label: PropTypes.string.isRequired,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.bool]),
+  recordData: PropTypes.object,
   inputKey: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string),
   optionsEndpoint: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   optionLabelKey: PropTypes.string,
@@ -138,6 +163,8 @@ InputField.propTypes = {
 InputField.defaultProps = {
   allowMultipleValues: false,
   value: null,
+  recordData: {},
+  options: null,
   optionsEndpoint: null,
   optionLabelKey: 'name',
   optionValueKey: 'id',
