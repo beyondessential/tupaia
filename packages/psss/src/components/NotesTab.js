ab2dc3c13bd27d03dfbab819f08d0492a9468334
Tom Caiger <caigertom@gmail.com>
2020-07-01 12:13:42 +1200

    add interactions to notes tab

--- packages/psss/src/components/NotesTab.js
+++ packages/psss/src/components/NotesTab.js
@@ -2,7 +2,7 @@
  * Tupaia
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import {
@@ -11,19 +11,36 @@ import {
   UserMessage,
   UserMessageHeader,
   CardTabPanel,
+  ErrorAlert,
 } from '@tupaia/ui-components';
-import { fetchStateShape } from '../hooks';
 import { FetchLoader } from './FetchLoader';
+import { connectApi } from '../api';
 import * as COLORS from '../constants/colors';
 
-const GREY_SECTION_HEIGHT = '14rem';
+const GREY_SECTION_HEIGHT = '225px';
 
 const Container = styled.div`
-  padding-bottom: ${GREY_SECTION_HEIGHT};
+  padding-bottom: calc(${GREY_SECTION_HEIGHT} + 60px);
 `;
 
 const StyledUserMessage = styled(UserMessage)`
   margin-bottom: 1.5rem;
+`;
+
+const StyledErrorAlert = styled(ErrorAlert)`
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  width: 100%;
+`;
+
+const StyledSuccessAlert = styled(ErrorAlert)`
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  width: 100%;
+  color: #155724;
+  background-color: #d4edda;
 `;
 
 const GreySection = styled.div`
@@ -35,15 +52,24 @@ const GreySection = styled.div`
   padding: 1.5rem 1.25rem;
 `;
 
-export const NotesTab = ({ state }) => {
+const STATUS = {
+  IDLE: 'idle',
+  LOADING: 'loading',
+  ERROR: 'error',
+  SUCCESS: 'success',
+};
+
+const NotesTabComponent = ({ state, handleUpdate, handleDelete, createNote }) => {
   const { data: messages } = state;
+  const [status, setStatus] = useState('');
+  const [noteValue, setNoteValue] = useState('');
 
-  const handleUpdate = () => {
-    console.log('update....');
-  };
-
-  const handleDelete = () => {
-    console.log('delete...');
+  const handleSubmitNote = async event => {
+    event.preventDefault();
+    setStatus(STATUS.LOADING);
+    await createNote(noteValue);
+    setNoteValue('');
+    setStatus(STATUS.SUCCESS);
   };
 
   return (
@@ -68,13 +94,44 @@ export const NotesTab = ({ state }) => {
         </FetchLoader>
       </CardTabPanel>
       <GreySection>
-        <TextField name="textArea" placeholder="Type in your notes..." multiline rows="4" />
-        <Button fullWidth>Add Note</Button>
+        {status === STATUS.ERROR && (
+          <StyledErrorAlert>There was a server error. Please try again.</StyledErrorAlert>
+        )}
+        {status === STATUS.SUCCESS && (
+          <StyledSuccessAlert>Your note was created.</StyledSuccessAlert>
+        )}
+        <form onSubmit={handleSubmitNote}>
+          <TextField
+            value={noteValue}
+            onChange={event => setNoteValue(event.target.value)}
+            name="textArea"
+            placeholder="Type in your notes..."
+            multiline
+            rows="4"
+            InputProps={{
+              readOnly: status === STATUS.LOADING,
+            }}
+          />
+          <Button type="submit" fullWidth isLoading={status === STATUS.LOADING}>
+            Add Note
+          </Button>
+        </form>
       </GreySection>
     </Container>
   );
 };
 
-NotesTab.propTypes = {
-  state: PropTypes.shape(fetchStateShape).isRequired,
+NotesTabComponent.propTypes = {
+  state: PropTypes.object.isRequired,
+  createNote: PropTypes.func.isRequired,
+  handleUpdate: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
+
+const mapApiToProps = api => ({
+  createNote: () => api.post(),
+  handleUpdate: () => api.post(),
+  handleDelete: () => api.post(),
+});
+
+export const NotesTab = connectApi(mapApiToProps)(NotesTabComponent);
