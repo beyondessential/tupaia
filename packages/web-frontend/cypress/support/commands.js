35ff186fd131c32da6cad74807f9ebbd66d44018
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-07-03 18:02:39 +1000

    Add e2e test for dashboard reports

--- packages/web-frontend/cypress/support/commands.js
+++ packages/web-frontend/cypress/support/commands.js
@@ -4,3 +4,27 @@
  */
 
 import '@testing-library/cypress/add-commands';
+
+import snapshot from '@cypress/snapshot';
+import { escapeRegex } from './utilities';
+
+snapshot.register();
+
+Cypress.Commands.add('closestByTestId', { prevSubject: 'element' }, (subject, testId) =>
+  cy.wrap(subject).closest(`[data-testid="${testId}"]`),
+);
+
+Cypress.Commands.add('containsI', { prevSubject: ['element', 'optional'] }, (subject, searchText) =>
+  cy.wrap(subject).contains(new RegExp(escapeRegex(searchText), 'i')),
+);
+
+Cypress.Commands.add('findByInputName', { prevSubject: 'element' }, (subject, inputName) =>
+  cy.wrap(subject).find(`input[name="${inputName}"]`),
+);
+
+Cypress.Commands.add(
+  'findByTextI',
+  { prevSubject: ['element', 'optional'] },
+  (subject, searchText, options) =>
+    cy.wrap(subject).findByText(new RegExp(escapeRegex(searchText), 'i'), options),
+);
