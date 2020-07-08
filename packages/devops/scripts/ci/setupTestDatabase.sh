d970e8ed556638b027de6ecd1a3fa5fa2b074355
Edwin <edmofro@gmail.com>
2020-07-08 12:22:43 +1200

    Split schema and data migrations simply

--- packages/devops/scripts/ci/setupTestDatabase.sh
+++ packages/devops/scripts/ci/setupTestDatabase.sh
@@ -4,4 +4,5 @@ ${DIR}/waitForPostgres.sh
 psql -h $CI_TEST_DB_URL -p 5432 -U postgres -d postgres -c "CREATE ROLE $CI_TEST_DB_USER LOGIN SUPERUSER PASSWORD '$CI_TEST_DB_PASSWORD'"
 psql -h $CI_TEST_DB_URL -p 5432 -U postgres -d postgres -c "CREATE DATABASE $CI_TEST_DB_NAME WITH OWNER $CI_TEST_DB_USER"
 PGPASSWORD=$CI_TEST_DB_PASSWORD psql -h $CI_TEST_DB_URL -p 5432 -U $CI_TEST_DB_USER -d $CI_TEST_DB_NAME -f ./packages/database/src/tests/testData/testDataDump.sql
+rm packages/database/src/migrations/*modifies-data.js
 yarn migrate
