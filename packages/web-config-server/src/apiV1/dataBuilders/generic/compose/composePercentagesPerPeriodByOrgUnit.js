6e90210cd5490f96e57e21208989c5f4b3e08d23
Rohan Port <rohan@beyondessential.com.au>
2020-07-10 16:16:29 +1000

    1166: Performance improvement on orgUnit name lookup

--- packages/web-config-server/src/apiV1/dataBuilders/generic/compose/composePercentagesPerPeriodByOrgUnit.js
+++ packages/web-config-server/src/apiV1/dataBuilders/generic/compose/composePercentagesPerPeriodByOrgUnit.js
@@ -7,6 +7,7 @@ export const composePercentagesPerPeriodByOrgUnit = async (config, aggregator, d
   const responses = await fetchComposedData(config, aggregator, dhisApi);
   const { value: percentages } = config.dataBuilderConfig.percentages;
   const data = [];
+  const orgUnitCodeToName = {};
   for (const { name, timestamp, ...orgUnits } of responses[percentages.denominator].data) {
     const numeratorReponse = responses[percentages.numerator].data.find(
       numerator => numerator.name === name && numerator.timestamp === timestamp,
@@ -14,7 +15,10 @@ export const composePercentagesPerPeriodByOrgUnit = async (config, aggregator, d
     const dataValue = { name, timestamp };
 
     for (const [orgUnitCode, denominatorValue] of Object.entries(orgUnits)) {
-      const orgUnitName = (await Entity.findOne({ code: orgUnitCode })).name;
+      if (!orgUnitCodeToName[orgUnitCode]) {
+        orgUnitCodeToName[orgUnitCode] = (await Entity.findOne({ code: orgUnitCode })).name;
+      }
+      const orgUnitName = orgUnitCodeToName[orgUnitCode];
       const numeratorValue = numeratorReponse ? numeratorReponse[orgUnitCode] || 0 : 0;
       dataValue[orgUnitName] = divideValues(numeratorValue, denominatorValue);
       dataValue[`${orgUnitName}_metadata`] = {
