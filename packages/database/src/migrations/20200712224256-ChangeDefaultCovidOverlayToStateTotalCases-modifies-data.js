bde834e2227a530a75a89340e6b51e5702af5fa8
nick <nickw@ourcomunity.com.au>
2020-07-13 09:36:38 +1000

    remove sortOrder change

--- packages/database/src/migrations/20200712224256-ChangeDefaultCovidOverlayToStateTotalCases-modifies-data.js
+++ packages/database/src/migrations/20200712224256-ChangeDefaultCovidOverlayToStateTotalCases-modifies-data.js
@@ -14,19 +14,16 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-const mapOverlayId = 'COVID_AU_HOSPITALS_AND_TESTING_SITES';
 const defaultMapOverlayId = 'COVID_AU_State_Total_Number_Confirmed_Cases';
 
 exports.up = function(db) {
   return db.runSql(`
-    UPDATE "mapOverlay" SET "sortOrder" = 1 WHERE id = '${mapOverlayId}';
     UPDATE "project" SET "default_measure" = '${defaultMapOverlayId}' WHERE "code" = 'covidau'; 
   `);
 };
 
 exports.down = function(db) {
   return db.runSql(`
-    UPDATE "mapOverlay" SET "sortOrder" = 0 WHERE id = '${mapOverlayId}'; 
     UPDATE "project" SET "default_measure" = '126,171' WHERE "code" = 'covidau';
   `);
 };
