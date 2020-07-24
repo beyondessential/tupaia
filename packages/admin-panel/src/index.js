7b91497623c6411cd38ba7c10c7180dcbc2f5135
Tom Caiger <caigertom@gmail.com>
2020-07-24 15:25:39 +1200

    add material ui base styles

--- packages/admin-panel/src/index.js
+++ packages/admin-panel/src/index.js
@@ -5,25 +5,33 @@
 
 import React from 'react';
 import { render as renderReactApp } from 'react-dom';
+import { MuiThemeProvider, StylesProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from 'styled-components';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { Provider } from 'react-redux';
 import { withRouter } from 'react-router';
 import { ConnectedRouter } from 'react-router-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-table/react-table.css';
-
+import { theme } from './theme';
 import { history, store, persistor } from './store';
 import { AppContainer } from './App';
-import './index.css';
 
 const RoutingApp = withRouter(AppContainer);
 
 renderReactApp(
   <Provider store={store}>
     <PersistGate persistor={persistor} loading={null}>
-      <ConnectedRouter history={history}>
-        <RoutingApp />
-      </ConnectedRouter>
+      <StylesProvider injectFirst>
+        <MuiThemeProvider theme={theme}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ConnectedRouter history={history}>
+              <RoutingApp />
+            </ConnectedRouter>
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </StylesProvider>
     </PersistGate>
   </Provider>,
   document.getElementById('root'),
