41ae2f47b8e21668ee4463a833ead2efb4f36728
Tom Caiger <caigertom@gmail.com>
2020-07-30 09:16:25 +1200

    add documentation

--- packages/admin-panel/src/utilities/usePortalWithCallback.js
+++ packages/admin-panel/src/utilities/usePortalWithCallback.js
@@ -6,6 +6,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * A util which renders a component to the ref that the callback returns
+ *
+ * @param Component - a react component
+ * @param callback - a function that returns a ref to a dom node
+ *
+ */
 export const usePortalWithCallback = (Component, callback) => {
   const [el, setEl] = React.useState(null);
 
