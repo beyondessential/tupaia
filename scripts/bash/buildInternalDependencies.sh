983c362c5e16e93dbc8d2084eccc0326791d1557
Kostas Karvounis <kael89@users.noreply.github.com>
2020-08-04 12:13:51 +1000

    Add build types flag (#1078)

--- scripts/bash/buildInternalDependencies.sh
+++ scripts/bash/buildInternalDependencies.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
+
 DIR=`dirname "$0"`
-concurrent_build_commands=""
-for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
-  concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $1\"" # $1 may pass in --watch
+
+USAGE="Usage: buildInternalDependencies.sh [--watch] [--withTypes]"
+
+watch=false
+with_types=false
+while [ "$1" != "" ]; do
+  case $1 in
+      --watch)
+        shift
+        watch=true
+        ;;
+      --withTypes)
+        shift
+        with_types=true
+        shift
+        ;;
+      -h | --help )
+        echo -e "$USAGE\n";
+        exit
+        ;;
+      * )
+        echo -e "$USAGE\n"
+        exit 1
+  esac
 done
+
+[[ $watch = "true" ]] && build_args="--watch" || build_args=""
+[[ $watch = "true" ]] && build_ts_args="--watch" || build_ts_args=""
+
+concurrent_build_commands=""
+
+# Build dependencies
+for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
+  concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $build_args\""
+done
+
+# Build types
+if [ $with_types == "true" ]; then
+  for PACKAGE in $(${DIR}/getTypedInternalDependencies.sh); do
+    concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build:ts $build_ts_args\""
+  done
+fi
+
 echo yarn concurrently ${concurrent_build_commands}
 eval "yarn concurrently ${concurrent_build_commands}"
