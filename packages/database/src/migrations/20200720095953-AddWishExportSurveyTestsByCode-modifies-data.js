2b8f9d197aa798e2834ab4f5ee1fc273e9b5b2ac
nick <nickw@ourcomunity.com.au>
2020-08-04 14:36:28 +1000

    change skipHeader from string to boolean

--- packages/database/src/migrations/20200720095953-AddWishExportSurveyTestsByCode-modifies-data.js
+++ packages/database/src/migrations/20200720095953-AddWishExportSurveyTestsByCode-modifies-data.js
@@ -55,7 +55,7 @@ const DATA_BUILDER_CONFIG = {
         },
       },
       transformations: ['mergeSurveys', 'transposeMatrix'],
-      skipHeader: 'false',
+      skipHeader: false,
     },
   },
 };
