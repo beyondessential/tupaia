c009df1d30f422c082f2924d49351deb66266306
Edwin <edmofro@gmail.com>
2020-08-05 11:18:19 +1200

    Batch the building of internal dependencies

--- scripts/bash/buildInternalDependencies.sh
+++ scripts/bash/buildInternalDependencies.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 DIR=`dirname "$0"`
-concurrent_build_commands=""
+
+CONCURRENT_BUILD_BATCH_SIZE=5
+
+concurrent_build_commands=()
 for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
-  concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $1\"" # $1 may pass in --watch
+  concurrent_build_commands+=("${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $1\"") # $1 may pass in --watch
 done
-echo yarn concurrently ${concurrent_build_commands}
-eval "yarn concurrently ${concurrent_build_commands}"
+
+echo "Concurrently building internal dependencies in batches of ${CONCURRENT_BUILD_BATCH_SIZE}"
+total_commands=${#concurrent_build_commands[@]}
+for ((start_index=0; start_index<${total_commands}; start_index+=${CONCURRENT_BUILD_BATCH_SIZE})); do
+  echo "yarn concurrently ${concurrent_build_commands[@]:${start_index}:${CONCURRENT_BUILD_BATCH_SIZE}}"
+  eval "yarn concurrently ${concurrent_build_commands[@]:${start_index}:${CONCURRENT_BUILD_BATCH_SIZE}}"
+done
+
