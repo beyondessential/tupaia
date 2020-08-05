107155e7ecbce2c01f835beec7ebfb2d2e8c6aca
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-08-05 16:34:09 +1000

    838: rebased migration to remove map_overlay_group and its relations

--- packages/database/src/migrations/20200803061954-RemoveLaosSchoolsStudentResourcesMapOverlays-modifies-data.js
+++ packages/database/src/migrations/20200803061954-RemoveLaosSchoolsStudentResourcesMapOverlays-modifies-data.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { arrayToDbString } from '../utilities';
+
 var dbm;
 var type;
 var seed;
@@ -14,14 +16,40 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return db.runSql(`
-    DELETE FROM "mapOverlay" WHERE "groupName" = 'Student Resources';
+const OVERLAY_IDS = [
+  'Laos_Schools_Students_Have_Own_Textbooks',
+  'Laos_Schools_Students_Have_Textbooks_Grade_1',
+  'Laos_Schools_Students_Have_Textbooks_Grade_2',
+  'Laos_Schools_Students_Have_Textbooks_Grade_3',
+  'Laos_Schools_Students_Have_Textbooks_Grade_4',
+  'Laos_Schools_Students_Have_Textbooks_Grade_5',
+  'Laos_Schools_Students_Have_Textbooks_Grade_6',
+  'Laos_Schools_Students_Have_Textbooks_Grade_7',
+  'Laos_Schools_Students_Have_Textbooks_Grade_8',
+  'Laos_Schools_Students_Have_Textbooks_Grade_9',
+];
+
+const selectStudentResourcesGroup = async db =>
+  db.runSql(`SELECT * FROM map_overlay_group where code = 'Student_Resources'`);
+
+exports.up = async function(db) {
+  const group = (await selectStudentResourcesGroup(db)).rows[0];
+
+  await db.runSql(`
+    DELETE FROM map_overlay_group_relation WHERE map_overlay_group_id = '${group.id}';
+  `);
+
+  await db.runSql(`
+    DELETE FROM "map_overlay_group" WHERE id = '${group.id}';
+  `);
+
+  await db.runSql(`
+    DELETE FROM "mapOverlay" WHERE id IN (${arrayToDbString(OVERLAY_IDS)})
   `);
 };
 
 exports.down = function(db) {
-  return null;
+  return null; //No down migrations
 };
 
 exports._meta = {
