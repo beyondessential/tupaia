f9bcf41b9abcf912ae032c3e56fb477c9b110a27
nick <nickw@ourcomunity.com.au>
2020-08-09 13:59:48 +1000

    Use aoa for excel sheet imput Add formatMatrixDataForExcelAoA

--- packages/web-config-server/src/export/excelFormatters/formatMatrixDataForExcel.js
+++ packages/web-config-server/src/export/excelFormatters/formatMatrixDataForExcel.js
@@ -59,4 +59,83 @@ export const formatMatrixDataForExcel = (
   return formattedData;
 };
 
+// Takes in the data for a matrix type view and converts it into an array of arrays representing
+// an Excel spreadsheet in the format required by the xlsx library, i.e. an array of arrays,
+// with each child array in the array representing a row in the spreadsheet,
+// with a array[0] representing the column headers, e.g.
+// [
+//  [ 'Column Title A', 'Column Title B', ... ],
+//  [ 'Cell A1', 'Cell B1', ... },
+//  [ 'Cell A2', 'Cell B2', ... },
+//  ...,
+// ]
+// See https://docs.sheetjs.com/#array-of-arrays-input for more
+
+export const formatMatrixDataForExcelAoA = (
+  { columns, categories: rowCategories, rows },
+  configIn,
+) => {
+  // Create the empty array of objects to build the data into
+  const formattedData = [];
+  const columnCategories = columns[0].columns && columns; // If columns are grouped into categories, store as a well named const
+  const config = { ...DEFAULT_CONFIG, ...configIn };
+
+  // Returns populated array instead of object
+  const formatRowData = row => {
+    const rowData = columnCategories
+      ? // This table has categories, iterate through the column categories
+        columnCategories.map(({ columns: columnsInCategory }) => {
+          // Add an empty column to start each category, with just the header filled as the title of the category
+          // Iterate through each of the columns in the category, and add the relevant row data
+          return ['', ...columnsInCategory.map(col => addValueOrEmpty(row[col.key]))];
+        })
+      : // This table has no column categories, just one set of columns
+        columns.map(col => addValueOrEmpty(row[col.key]));
+
+    //prepend dataElementHeader
+    rowData.unshift(row.dataElement);
+
+    return rowData;
+  };
+
+  const buildTitleRow = () => {
+    const titleRowData = columnCategories
+      ? // This table has categories, iterate through the column categories add category title
+        // and iterate through each of the columns in the category, and add the relevant title
+        columnCategories.map(({ category: columnCategoryTitle, columns: columnsInCategory }) => {
+          return [columnCategoryTitle, ...columnsInCategory.map(col => col.title)];
+        })
+      : columns.map(col => col.title);
+
+    // prepend dataElementHeader
+    titleRowData.unshift(config.dataElementHeader);
+
+    return titleRowData;
+  };
+
+  // add to title row 'top' of sheet
+  const titleRow = buildTitleRow();
+  formattedData.push(titleRow);
+
+  // Iterate through all rows and add them
+  if (rowCategories) {
+    // Add rows a category at a time, so all rows from the same category are grouped
+    rowCategories.forEach(({ key: rowCategoryKey, title: rowCategoryTitle }) => {
+      // Insert a separating empty row between categories
+      formattedData.push([rowCategoryTitle].fill('', 1, titleRow.length - 1));
+      // Add the data from all rows in this category
+      const formattedRowData = rows
+        .filter(({ categoryId }) => rowCategoryKey === categoryId)
+        .map(formatRowData);
+      formattedData.push(formattedRowData);
+    });
+  } else {
+    // This table has no row categories, just one set of rows
+    formattedData.push(rows.map(formatRowData));
+  }
+  return formattedData;
+};
+
 const isEmpty = data => data === undefined || data === null;
+
+const addValueOrEmpty = value => (isEmpty(value) ? '' : value);
