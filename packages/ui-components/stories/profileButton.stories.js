cefc559753a2bc8a6889ffcf67d0892f089ce1ba
Tom Caiger <caigertom@gmail.com>
2020-08-12 10:51:55 +1200

    update ui components profile button

--- packages/ui-components/stories/profileButton.stories.js
+++ packages/ui-components/stories/profileButton.stories.js
@@ -4,18 +4,10 @@
  */
 
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import styled from 'styled-components';
 import MuiBox from '@material-ui/core/Box';
-import { Link as RouterLink } from 'react-router-dom';
-import MuiListItem from '@material-ui/core/ListItem';
-import MuiListItemIcon from '@material-ui/core/ListItemIcon';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import MuiListItemText from '@material-ui/core/ListItemText';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import * as COLORS from './story-utils/theme/colors';
 import { RouterProvider } from '../helpers/RouterProvider';
-import { ProfileButton, LightProfileButton } from '../src';
+import { ProfileButton, ProfileButtonItem } from '../src';
 
 export default {
   title: 'ProfileButton',
@@ -23,45 +15,27 @@ export default {
 };
 
 const Container = styled(MuiBox)`
+  display: flex;
+  justify-content: flex-end;
   max-width: 1200px;
   padding: 1rem;
 `;
 
-const StyledListItem = styled(MuiListItem)`
-  padding-right: 3rem;
-`;
-
-const ListItemLink = props => <StyledListItem button component={RouterLink} {...props} />;
+const exampleUser = {
+  name: 'Catherine Bell',
+  firstName: 'Catherine',
+  email: 'catherine@beyondessential.com.au',
+};
 
 const ProfileLinks = () => (
-  <React.Fragment>
-    <ListItemLink to="/profile">
-      <MuiListItemIcon>
-        <AccountCircleIcon />
-      </MuiListItemIcon>
-      <MuiListItemText primary="Profile" />
-    </ListItemLink>
-    <ListItemLink to="/logout">
-      <MuiListItemIcon>
-        <ExitToAppIcon />
-      </MuiListItemIcon>
-      <MuiListItemText primary="Logout" />
-    </ListItemLink>
-  </React.Fragment>
+  <>
+    <ProfileButtonItem to="/profile">Edit Profile</ProfileButtonItem>
+    <ProfileButtonItem to="/logout">Logout</ProfileButtonItem>
+  </>
 );
 
-export const profileButton = () => (
+export const Simple = () => (
   <Container>
-    <ProfileButton startIcon={<Avatar>K</Avatar>} listItems={<ProfileLinks />}>
-      Kupe
-    </ProfileButton>
-  </Container>
-);
-
-export const lightProfileButton = () => (
-  <Container bgcolor={COLORS.BLUE}>
-    <LightProfileButton startIcon={<Avatar>K</Avatar>} listItems={<ProfileLinks />}>
-      Kupe
-    </LightProfileButton>
+    <ProfileButton user={exampleUser} MenuOptions={ProfileLinks} />
   </Container>
 );
