846ccc50bca3342838f2fa79930d577f833604db
Edwin <edmofro@gmail.com>
2020-08-17 17:49:33 +1200

    Revert unrelated changes

--- packages/meditrak-server/src/permissions/checkers.js
+++ packages/meditrak-server/src/permissions/checkers.js
@@ -5,8 +5,6 @@
 
 import { BES_ADMIN_PERMISSION_GROUP, TUPAIA_ADMIN_PANEL_PERMISSION_GROUP } from './constants';
 
-const DEFAULT_ERROR_MESSAGE = 'Your permissions do not allow access to the requested resource';
-
 ////
 // utilities for running multiple permissions checks
 ////
@@ -23,10 +21,7 @@ export const checkNoPermissions = () => {
  * @param {function[]} permissionsCheckers  Each permission checking function should return true or throw an error
  * @param {string} errorMessage
  */
-export const checkAllPermissions = (
-  permissionsCheckers,
-  errorMessage = DEFAULT_ERROR_MESSAGE,
-) => async accessPolicy => {
+export const checkAllPermissions = (permissionsCheckers, errorMessage) => async accessPolicy => {
   try {
     for (let i = 0; i < permissionsCheckers.length; i++) {
       const permissionsChecker = permissionsCheckers[i];
@@ -42,18 +37,15 @@ export const checkAllPermissions = (
  * @param {function[]} permissionsCheckers  Each permission checking function should return true or throw an error
  * @param {string} errorMessage
  */
-export const checkAnyPermissions = (
-  permissionsCheckers,
-  errorMessage = DEFAULT_ERROR_MESSAGE,
-) => async accessPolicy => {
-  for (let i = 0; i < permissionsCheckers.length; i++) {
-    try {
+export const checkAnyPermissions = (permissionsCheckers, errorMessage) => async accessPolicy => {
+  try {
+    for (let i = 0; i < permissionsCheckers.length; i++) {
       const permissionsChecker = permissionsCheckers[i];
       await permissionsChecker(accessPolicy);
       return true;
-    } catch (e) {
-      // swallow specific errors, in case any checker returns true
     }
+  } catch (e) {
+    // swallow specific errors, in case any checker returns true
   }
   throw new Error(errorMessage);
 };
@@ -61,16 +53,7 @@ export const checkAnyPermissions = (
 ////
 // specific permissions checkers
 ////
-const checkOrThrow = checker => accessPolicy => {
-  const hasPermission = checker(accessPolicy);
-  if (!hasPermission) {
-    throw new Error(DEFAULT_ERROR_MESSAGE);
-  }
-  return true;
-};
-export const hasBESAdminAccess = checkOrThrow(accessPolicy =>
-  accessPolicy.allowsSome(null, BES_ADMIN_PERMISSION_GROUP),
-);
-export const hasTupaiaAdminPanelAccess = checkOrThrow(accessPolicy =>
-  accessPolicy.allowsSome(null, TUPAIA_ADMIN_PANEL_PERMISSION_GROUP),
-);
+export const hasBESAdminAccess = accessPolicy =>
+  accessPolicy.allowsSome([], BES_ADMIN_PERMISSION_GROUP);
+export const hasTupaiaAdminPanelUserAccess = accessPolicy =>
+  accessPolicy.allowsSome([], TUPAIA_ADMIN_PANEL_PERMISSION_GROUP);
--- packages/meditrak-server/src/permissions/constants.js
+++ packages/meditrak-server/src/permissions/constants.js
@@ -4,4 +4,4 @@
  */
 
 export const BES_ADMIN_PERMISSION_GROUP = 'BES Admin';
-export const TUPAIA_ADMIN_PANEL_PERMISSION_GROUP = 'Tupaia Admin Panel';
+export const TUPAIA_ADMIN_PANEL_PERMISSION_GROUP = 'Tupaia Admin Panel User';
