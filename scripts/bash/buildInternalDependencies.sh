b0b2bc44e7510b17cae5cae7ac1721df3c672d1e
Rohan Port <rohan@beyondessential.com.au>
2020-08-20 09:51:35 +1000

    Reverted back to including build dependencies and changed concurrency to 2

--- scripts/bash/buildInternalDependencies.sh
+++ scripts/bash/buildInternalDependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=`dirname "$0"`
 
-CONCURRENT_BUILD_BATCH_SIZE=3
+CONCURRENT_BUILD_BATCH_SIZE=2
 
 concurrent_build_commands=()
 for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
