41e4f55c6fe585e1e51bd07ba5b17f598eb5c462
Edwin <edmofro@gmail.com>
2020-08-19 16:17:46 +1200

    Don't double up first package in concurrent build commands

--- scripts/bash/buildInternalDependencies.sh
+++ scripts/bash/buildInternalDependencies.sh
@@ -5,7 +5,7 @@ CONCURRENT_BUILD_BATCH_SIZE=5
 
 concurrent_build_commands=()
 for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
-  concurrent_build_commands+=("${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $1\"") # $1 may pass in --watch
+  concurrent_build_commands+=("\"yarn workspace @tupaia/${PACKAGE} build $1\"") # $1 may pass in --watch
 done
 
 if [[ $1 == "--watch" ]]; then
