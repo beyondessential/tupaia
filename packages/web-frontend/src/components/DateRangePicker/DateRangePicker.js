527680df088971eb9273c179f7c45c5fb9cab4da
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-08-19 19:26:39 +1000

    1164: moved const to inside the component

--- packages/web-frontend/src/components/DateRangePicker/DateRangePicker.js
+++ packages/web-frontend/src/components/DateRangePicker/DateRangePicker.js
@@ -99,9 +99,6 @@ const getDatesAsString = (isSingleDate, granularity, startDate, endDate) => {
   return isSingleDate ? formattedEndDate : `${formattedStartDate} - ${formattedEndDate}`;
 };
 
-const minMomentDate = moment(MIN_DATE_PICKER_DATE);
-const maxMomentDate = moment();
-
 export const DateRangePicker = ({
   startDate,
   endDate,
@@ -116,6 +113,9 @@ export const DateRangePicker = ({
 
   const isSingleDate = GRANULARITIES_WITH_ONE_DATE.includes(granularity);
   const { momentShorthand } = GRANULARITY_CONFIG[granularity];
+
+  const minMomentDate = moment(MIN_DATE_PICKER_DATE);
+  const maxMomentDate = moment();
 
   const defaultStartDate = isSingleDate ? moment() : minMomentDate;
   const defaultEndDate = isSingleDate ? defaultStartDate : maxMomentDate;
