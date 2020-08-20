6eda757699c5a70c1997f22e3da1ebf05e15f32c
Edwin <edmofro@gmail.com>
2020-08-20 15:13:14 +1200

    Move helpers to separate file

--- packages/meditrak-server/src/routes/GETHandler/GETHandler.js
+++ packages/meditrak-server/src/routes/GETHandler/GETHandler.js
@@ -3,16 +3,19 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import formatLinkHeader from 'format-link-header';
-import { JOIN_TYPES } from '@tupaia/database';
-import { respond, ValidationError } from '@tupaia/utils';
-import { RouteHandler } from './RouteHandler';
-import { getApiUrl, resourceToRecordType } from '../utilities';
+import { respond } from '@tupaia/utils';
+import { RouteHandler } from '../RouteHandler';
+import { resourceToRecordType } from '../../utilities';
+import {
+  extractResourceFromEndpoint,
+  getQueryOptionsForColumns,
+  processColumns,
+  processColumnSelector,
+  processColumnSelectorKeys,
+  generateLinkHeader,
+} from './helpers';
 
 const MAX_RECORDS_PER_PAGE = 100;
-
-// if the endpoint is /survey/5a5d1c66ae07fb3fb025c3a3/answer, the resource is 'survey'
-const extractResourceFromEndpoint = endpoint => endpoint.split('/')[1];
 
 /**
  * Responds to GET requests for a resource.
@@ -137,96 +140,3 @@ export class GETHandler extends RouteHandler {
     return record;
   }
 }
-
-function generateLinkHeader(resource, pageString, lastPage, originalQueryParameters) {
-  const currentPage = parseInt(pageString, 10);
-
-  const getUrlForPage = page => getApiUrl(resource, { ...originalQueryParameters, page });
-
-  // We can always send through first and last, so start with that in the link header
-  const linkHeader = {
-    first: {
-      url: getUrlForPage(0),
-      rel: 'first',
-    },
-    last: {
-      url: getUrlForPage(lastPage),
-      rel: 'last',
-    },
-  };
-
-  // If not the first page, generate a 'prev' link to the page before
-  if (currentPage > 0) {
-    linkHeader.prev = {
-      url: getUrlForPage(currentPage - 1),
-      rel: 'prev',
-    };
-  }
-
-  // If not the last page, generate a 'next' link to the next page
-  if (currentPage < lastPage) {
-    linkHeader.next = {
-      url: getUrlForPage(currentPage + 1),
-      rel: 'next',
-    };
-  }
-
-  return formatLinkHeader(linkHeader);
-}
-
-function processColumns(unprocessedColumns, recordType) {
-  return unprocessedColumns.map(column => ({
-    [column]: processColumnSelector(column, recordType),
-  }));
-}
-
-function getQueryOptionsForColumns(columns, baseRecordType) {
-  const sort = [`${baseRecordType}.id`];
-  if (!columns) {
-    return { sort };
-  }
-  if (columns.some(c => c.startsWith('_'))) {
-    throw new ValidationError(
-      'No columns start with "_", and conjunction operators are reserved for internal use only',
-    );
-  }
-  const columnsNeedingJoin = columns.filter(column => column.includes('.'));
-  const multiJoin = [];
-  const recordTypesJoined = [];
-  for (let i = 0; i < columnsNeedingJoin.length; i++) {
-    // Split strings into the record type to join with and the column to select, e.g. if the column
-    // is 'survey.name', split into 'survey' and 'name'
-    const resourceName = columnsNeedingJoin[i].split('.')[0];
-    const recordType = resourceToRecordType(resourceName);
-
-    if (recordType !== baseRecordType && !recordTypesJoined.includes(recordType)) {
-      multiJoin.push({
-        joinType: JOIN_TYPES.LEFT_OUTER,
-        joinWith: recordType,
-        joinCondition: [`${recordType}.id`, `${resourceName}_id`],
-      });
-      recordTypesJoined.push(recordType);
-    }
-  }
-  // Ensure every join table is added to the sort, so that queries are predictable during pagination
-  sort.push(...recordTypesJoined.map(recordType => `${recordType}.id`));
-  return { multiJoin, sort };
-}
-
-// Make sure all column keys have the table specified to avoid ambiguous column errors,
-// and also transform any resource names into database record types
-const processColumnSelectorKeys = (object, recordType) => {
-  const processedObject = {};
-  Object.entries(object).forEach(([columnSelector, value]) => {
-    processedObject[processColumnSelector(columnSelector, recordType)] = value;
-  });
-  return processedObject;
-};
-
-const processColumnSelector = (unprocessedColumnSelector, baseRecordType) => {
-  if (unprocessedColumnSelector.includes('.')) {
-    const [recordType, column] = unprocessedColumnSelector.split('.');
-    return `${resourceToRecordType(recordType)}.${column}`;
-  }
-  return `${baseRecordType}.${unprocessedColumnSelector}`;
-};
