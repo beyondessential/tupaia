428d583ba5f77ae5db2984544d2773794604c0e3
Edwin <edmofro@gmail.com>
2020-08-20 14:56:49 +1200

    Move permissions gate requirement to RouteHandler

--- packages/meditrak-server/src/routes/GETHandler.js
+++ packages/meditrak-server/src/routes/GETHandler.js
@@ -42,16 +42,6 @@ export class GETHandler extends RouteHandler {
     this.recordType = resourceToRecordType(this.resource);
   }
 
-  /**
-   * All GET handlers should provide a concrete permissions gate implementation.
-   * This is the "gate" because there may be additional permissions filtering required in the body
-   * of the handler, but the gate will check whether the user should have any access to the endpoint
-   * whatsoever, and throw an error if they shouldn't get any further.
-   */
-  checkPermissionsGate() {
-    throw new Error(`'checkPermissionsGate' must be implemented by every GETHandler`);
-  }
-
   async handleRequest() {
     await this.checkPermissionsGate(); // run base permissions check for this endpoint
     const { headers = {}, body } = await this.buildResponse();
--- packages/meditrak-server/src/routes/RouteHandler.js
+++ packages/meditrak-server/src/routes/RouteHandler.js
@@ -13,6 +13,16 @@ export class RouteHandler {
     return this.req.checkPermissions(permissionsChecker);
   }
 
+  /**
+   * All GET handlers should provide a concrete permissions gate implementation.
+   * This is the "gate" because there may be additional permissions filtering required in the body
+   * of the handler, but the gate will check whether the user should have any access to the endpoint
+   * whatsoever, and throw an error if they shouldn't get any further.
+   */
+  checkPermissionsGate() {
+    throw new Error(`'checkPermissionsGate' must be implemented by every GETHandler`);
+  }
+
   async handleRequest() {
     throw new Error(`'handleRequest' must be implemented by every RouteHandler`);
   }
