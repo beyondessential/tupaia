49cff6d448dfa77dac9f99ce5eb06ac38c249002
Edwin <edmofro@gmail.com>
2020-08-26 14:20:17 +1200

    Fetch from ancestor descendant relation

--- packages/database/src/migrations/20200824014037-CreateEntityHierarchyCache-modifies-schema.js
+++ packages/database/src/migrations/20200824014037-CreateEntityHierarchyCache-modifies-schema.js
@@ -25,45 +25,50 @@ exports.up = async function(db) {
       hierarchy_id: {
         type: 'text',
         notNull: true,
-        // foreignKey: createForeignKeyConfig(TABLE_NAME, 'hierarchy_id', 'entity_hierarchy'),
+        foreignKey: createForeignKeyConfig(TABLE_NAME, 'hierarchy_id', 'entity_hierarchy'),
       },
       ancestor_id: {
         type: 'text',
         notNull: true,
-        // foreignKey: createForeignKeyConfig(TABLE_NAME, 'ancestor_id', 'entity'),
+        foreignKey: createForeignKeyConfig(TABLE_NAME, 'ancestor_id', 'entity'),
       },
       ancestor_code: {
         type: 'text',
         notNull: true,
-        // foreignKey: createForeignKeyConfig(TABLE_NAME, 'ancestor_code', 'entity', 'code'),
+        foreignKey: createForeignKeyConfig(TABLE_NAME, 'ancestor_code', 'entity', 'code'),
       },
       ancestor_type: {
+        type: 'text',
+        notNull: true,
+      },
+      ancestor_name: {
         type: 'text',
         notNull: true,
       },
       descendant_id: {
         type: 'text',
         notNull: true,
-        // foreignKey: createForeignKeyConfig(TABLE_NAME, 'descendant_id', 'entity'),
+        foreignKey: createForeignKeyConfig(TABLE_NAME, 'descendant_id', 'entity'),
       },
       descendant_code: {
         type: 'text',
         notNull: true,
-        // foreignKey: createForeignKeyConfig(TABLE_NAME, 'descendant_code', 'entity', 'code'),
+        foreignKey: createForeignKeyConfig(TABLE_NAME, 'descendant_code', 'entity', 'code'),
       },
       descendant_type: { type: 'text', notNull: true },
+      generational_distance: { type: 'int', notNull: true },
     },
   });
-  // await db.addIndex(TABLE_NAME, `${TABLE_NAME}_hierarchy_id_ancestor_id_descendant_type_idx`, [
-  //   'hierarchy_id',
-  //   'ancestor_id',
-  //   'descendant_type',
-  // ]);
-  // await db.addIndex(TABLE_NAME, `${TABLE_NAME}_hierarchy_id_descendant_id_ancestor_type_idx`, [
-  //   'hierarchy_id',
-  //   'descendant_id',
-  //   'ancestor_type',
-  // ]);
+  await db.addIndex(TABLE_NAME, `${TABLE_NAME}_hierarchy_id_ancestor_id_descendant_type_idx`, [
+    'hierarchy_id',
+    'ancestor_id',
+    'descendant_type',
+  ]);
+  await db.addIndex(TABLE_NAME, `${TABLE_NAME}_hierarchy_id_descendant_id_ancestor_type_idx`, [
+    'hierarchy_id',
+    'descendant_id',
+    'ancestor_type',
+  ]);
 };
 
 exports.down = function(db) {
