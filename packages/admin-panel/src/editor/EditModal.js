58b5ffc00fe35baec057f9a455725105a2a83d3d
Edwin <edmofro@gmail.com>
2020-08-27 13:36:07 +1200

    Add fix from dev
    See https://github.com/beyondessential/tupaia/pull/1164/files

--- packages/admin-panel/src/editor/EditModal.js
+++ packages/admin-panel/src/editor/EditModal.js
@@ -16,7 +16,14 @@ import { ModalContentProvider } from '../widgets';
 const getFieldSourceToEdit = field => {
   const { source, editConfig = {} } = field;
   if (editConfig.optionsEndpoint) {
-    return editConfig.sourceKey || `${source.split('.')[0]}_id`;
+    if (editConfig.sourceKey) {
+      return editConfig.sourceKey;
+    }
+    const sourceComponents = source.split('.');
+    if (sourceComponents.length > 1) {
+      const [resource] = sourceComponents;
+      return `${resource}_id`;
+    }
   }
   return source;
 };
