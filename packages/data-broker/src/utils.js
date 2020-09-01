1db2dcc899fe6621dd3b7fd29f9b92efdf67ccd2
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-01 13:22:53 +1000

    Add comment

--- packages/data-broker/src/utils.js
+++ packages/data-broker/src/utils.js
@@ -3,6 +3,9 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
+/**
+ * Groups { dataElement: value } pairs per organisationUnit/period combination
+ */
 export const analyticsToAnalyticClusters = analytics => {
   const clusterMap = {};
   analytics.forEach(analytic => {
