d7eabcd98a3d6b3a23b4d5cf4f7718f5ec04ab4e
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-01 13:03:11 +1000

    Use unknown instead of any

--- packages/indicators/globals.d.ts
+++ packages/indicators/globals.d.ts
@@ -3,7 +3,7 @@ import 'jest-extended';
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toHaveBeenCalledOnceWith<E extends any[]>(...params: E): R;
+      toHaveBeenCalledOnceWith<E extends unknown[]>(...params: E): R;
       toBeRejectedWith(error?: string | Constructable | RegExp | Error): Promise<R>;
     }
   }
