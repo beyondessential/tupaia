6b50a1d6fcd6cf9fdcde9a2e204b3ec8927d4751
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-08 09:18:52 +1000

    Do not use boolean parameter

--- packages/utils/src/period/periodExtremes.js
+++ packages/utils/src/period/periodExtremes.js
@@ -4,13 +4,18 @@
  */
 import { periodToTimestamp, isValidPeriod, isCoarserPeriod } from './period';
 
+const SORT_DIRECTION = {
+  ASC: 'ascending',
+  DESC: 'descending',
+};
+
 /**
  * @param {string[]} periods An array of periods
  * @returns {string | null} Most recent period from the input array.
  *
  * Assumes all periods are start periods (e.g. 202002 is earlier than 20200202)
  */
-export const getMostRecentPeriod = periods => getPeriodExtreme(periods, true);
+export const getMostRecentPeriod = periods => sortPeriods(periods, SORT_DIRECTION.DESC)[0] || null;
 
 /**
  * @param {string[]} periods An array of periods
@@ -18,17 +23,17 @@ export const getMostRecentPeriod = periods => getPeriodExtreme(periods, true);
  *
  * Assumes all periods are start periods (e.g. 202002 is earlier than 20200202)
  */
-export const getMostAncientPeriod = periods => getPeriodExtreme(periods, false);
+export const getMostAncientPeriod = periods => sortPeriods(periods, SORT_DIRECTION.ASC)[0] || null;
 
 /**
  * @param {string[]} periods An array of periods
- * @param {boolean} descending Whether to use descending order or not
+ * @param {boolean} direction Whether to use descending order or not
  * @returns {string | null} Most extreme period from the input array based on gettingMostRecent.
  *
  * Assumes all periods are start periods (e.g. 202002 is earlier than 20200202)
  */
-const getPeriodExtreme = (periods = [], descending) => {
-  const sortedPeriods = [...periods]
+const sortPeriods = (periods = [], direction) =>
+  periods
     .filter(period => isValidPeriod(period))
     .sort((p1, p2) => {
       const timestamp1 = periodToTimestamp(p1);
@@ -37,8 +42,5 @@ const getPeriodExtreme = (periods = [], descending) => {
       if (timestamp1 === timestamp2) {
         return isCoarserPeriod(p1, p2) ? 1 : -1;
       }
-      return (timestamp1 - timestamp2) * (descending ? -1 : 1);
+      return (timestamp1 - timestamp2) * (direction === SORT_DIRECTION.DESC ? -1 : 1);
     });
-
-  return sortedPeriods[0] || null;
-};
