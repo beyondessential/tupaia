74aa7246d80462f44a8a164d217c3c190a522887
Igor Nadj <igor@beyondessential.com.au>
2020-09-07 10:06:59 +1000

    Add weather-api package

---
+++ packages/weather-api/src/index.js
@@ -0,0 +1,1 @@
+export { WeatherApi } from './WeatherApi';
--- scripts/bash/getInternalDependencies.sh
+++ scripts/bash/getInternalDependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "access-policy" "aggregator" "auth" "database" "data-api" "data-broker" "dhis-api" "utils" "ui-components"
+echo "access-policy" "aggregator" "auth" "database" "data-api" "data-broker" "dhis-api" "utils" "ui-components" "weather-api"
