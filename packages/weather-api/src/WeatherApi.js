88016191ae0a662df7431c19b542e78aa7c56a1d
Igor Nadj <igor@beyondessential.com.au>
2020-09-07 10:06:59 +1000

    Add weather-api package

---
+++ packages/weather-api/src/WeatherApi.js
@@ -0,0 +1,69 @@
+import { fetchWithTimeout, stringifyQuery } from '@tupaia/utils';
+
+const MAX_FETCH_WAIT_TIME = 15 * 1000; // 15 seconds
+
+export class WeatherApi {
+  constructor() {
+    this.apiKey = process.env.WEATHERBIT_API_KEY;
+  }
+
+  /**
+   * Get current weather data
+   * @param string lat
+   * @param string lon
+   * @returns {{}}
+   */
+  async current(lat, lon) {
+    return await this._fetch('/v2.0/current', { lat, lon });
+  }
+
+  /**
+   * Get historic weather data
+   * @param string lat
+   * @param string lon
+   * @param string startDate
+   * @param string endDate
+   */
+  async historicDaily(lat, lon, startDate, endDate) {
+    return await this._fetch('/v2.0/history/daily', {
+      lat,
+      lon,
+      start_date: startDate,
+      end_date: endDate,
+    });
+  }
+
+  /**
+   * Get forecast weather data for the next 16 days
+   * @param string lat
+   * @param string lon
+   */
+  async forecastDaily(lat, lon) {
+    return await this._fetch('/v2.0/forecast/daily', {
+      lat,
+      lon,
+    });
+  }
+
+  /**
+   * @param string endpoint
+   * @param {} params
+   * @returns {Promise<{}>}
+   * @private
+   */
+  async _fetch(endpoint, params) {
+    params.key = this.apiKey;
+    const url = stringifyQuery('https://api.weatherbit.io', endpoint, params);
+
+    const result = await fetchWithTimeout(url, {}, MAX_FETCH_WAIT_TIME);
+
+    if (result.status !== 200) {
+      const bodyText = await result.text();
+      throw new Error(
+        `Error response from Weatherbit API. Status: ${result.status}, body: ${bodyText}`,
+      );
+    }
+
+    return await result.json();
+  }
+}
