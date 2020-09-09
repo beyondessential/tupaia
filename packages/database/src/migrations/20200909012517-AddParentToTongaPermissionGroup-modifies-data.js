42aab85db9546f5a9bb65bd2a6121ab0b38450d1
Alastair-L <lockealastair@gmail.com>
2020-09-09 12:05:53 +1000

    Migration to update parent

---
+++ packages/database/src/migrations/20200909012517-AddParentToTongaPermissionGroup-modifies-data.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+const PERMISSION_GROUP_NAME = 'Tonga Screening Forms CH11/HP02';
+const PARENT_NAME = 'Tonga Public Health Heads';
+
+exports.up = async function(db) {
+  const parentId = (
+    await db.runSql(`select "id" from "permission_group" where "name"='${PARENT_NAME}';`)
+  ).rows[0].id;
+  return db.runSql(`
+    update "permission_group"
+    set "parent_id" = '${parentId}'
+    where "name" = '${PERMISSION_GROUP_NAME}';
+  `);
+};
+
+exports.down = function(db) {
+  return db.runSql(`
+    update "permission_group"
+    set "parent_id" = NULL
+    where "name" = '${PERMISSION_GROUP_NAME}';
+  `);
+};
+
+exports._meta = {
+  version: 1,
+};
