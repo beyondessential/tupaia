23e80a0126d14a66df3bc01995d591dad621db75
Alastair-L <lockealastair@gmail.com>
2020-09-11 15:56:13 +1000

    Add test

---
+++ packages/web-frontend/src/tests/historyNavigation/UrlPeriodStringToDateRangeConversions.test.js
@@ -0,0 +1,56 @@
+import moment from 'moment';
+import {
+  convertUrlPeriodStringToDateRange,
+  convertDateRangeToUrlPeriodString,
+} from '../../historyNavigation/utils';
+
+const SINGLE_PERIOD_STRING = moment('2020-01-02').format('Do_MMM_YYYY');
+const SINGLE_DATE_RANGE = {
+  startDate: moment('2020-01-02'),
+  endDate: moment('2020-01-02'),
+};
+
+const RANGE_PERIOD_STRING = `${moment('2020-01-02').format('Do_MMM_YYYY')}-${moment(
+  '2020-01-10',
+).format('Do_MMM_YYYY')}`;
+
+const RANGE_DATE_RANGE = {
+  startDate: moment('2020-01-02'),
+  endDate: moment('2020-01-10'),
+};
+
+describe('convertUrlPeriodStringToDateRange', () => {
+  it('should decode strings with only one date', () => {
+    const { startDate, endDate } = convertUrlPeriodStringToDateRange(
+      SINGLE_PERIOD_STRING,
+      'one_day_at_a_time',
+    );
+    const { startDate: expectedStartDate, endDate: expectedEndDate } = SINGLE_DATE_RANGE;
+    expect(startDate.isSame(expectedStartDate)).toBe(true);
+    expect(endDate.isSame(expectedEndDate)).toBe(true);
+  });
+
+  it('should decode strings with multiple dates', () => {
+    const { startDate, endDate } = convertUrlPeriodStringToDateRange(RANGE_PERIOD_STRING, 'day');
+    const { startDate: expectedStartDate, endDate: expectedEndDate } = RANGE_DATE_RANGE;
+    expect(startDate.isSame(expectedStartDate)).toBe(true);
+    expect(endDate.isSame(expectedEndDate)).toBe(true);
+  });
+});
+
+// Date range => period string
+describe('convertDateRangeToUrlPeriodString', () => {
+  it('should return null if neither startDate nor endDate is defined', () => {
+    expect(convertDateRangeToUrlPeriodString({}, 'day')).toBeNull();
+  });
+
+  it('should encode strings with only one date', () => {
+    expect(convertDateRangeToUrlPeriodString(SINGLE_DATE_RANGE, 'one_day_at_a_time')).toEqual(
+      SINGLE_PERIOD_STRING,
+    );
+  });
+
+  it('should encode strings with multiple dates', () => {
+    expect(convertDateRangeToUrlPeriodString(RANGE_DATE_RANGE, 'day')).toEqual(RANGE_PERIOD_STRING);
+  });
+});
