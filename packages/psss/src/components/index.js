95bde841fac979fd85a3c154a7941a868b46a5dd
Tom Caiger <caigertom@gmail.com>
2020-09-15 08:54:13 +1200

    fix navbar import

--- packages/psss/src/components/index.js
+++ packages/psss/src/components/index.js
@@ -3,7 +3,6 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-export * from '../containers/NavBar';
 export * from './Footer';
 export * from './Layout';
 export * from './Header';
