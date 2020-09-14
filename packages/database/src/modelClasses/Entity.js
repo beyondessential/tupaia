35f3037a431a78dcc116ae80e46bae5a2a92ae4b
Igor Nadj <igor@beyondessential.com.au>
2020-09-14 13:18:59 +1000

    Add entity type city to model

--- packages/database/src/modelClasses/Entity.js
+++ packages/database/src/modelClasses/Entity.js
@@ -31,6 +31,7 @@ const CATCHMENT = 'catchment';
 const SUB_CATCHMENT = 'sub_catchment';
 const VILLAGE = 'village';
 const WORLD = 'world';
+const CITY = 'city';
 
 const ENTITY_TYPES = {
   CASE,
@@ -45,6 +46,7 @@ const ENTITY_TYPES = {
   SUB_CATCHMENT,
   VILLAGE,
   WORLD,
+  CITY,
 };
 
 const ORG_UNIT_ENTITY_TYPES = {
