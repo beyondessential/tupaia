e3ff9b5a4a7211de9a54910928109417291a10a7
Igor Nadj <igor@beyondessential.com.au>
2020-09-16 10:29:25 +1000

    Move default value to function arg default

--- packages/data-broker/src/tests/services/weather/testutil.js
+++ packages/data-broker/src/tests/services/weather/testutil.js
@@ -1,10 +1,8 @@
 import sinon from 'sinon';
 
-export const mockNow = whenIsNow => {
-  if (!whenIsNow) {
-    whenIsNow = 1549360800000; // 2019-02-05T10:00:00.000Z
-  }
+const DEFAULT_NOW_TIMESTAMP = 1549360800000; // 2019-02-05T10:00:00.000Z
 
+export const mockNow = (whenIsNow = DEFAULT_NOW_TIMESTAMP) => {
   sinon.useFakeTimers(whenIsNow);
 };
 
