ac0e5296f45d866affc3001e63751e07fadff3c7
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-21 17:45:20 +1000

    Add test coverage script

--- packages/utils/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: { '^.+\\.jsx?$': './scripts/JestTransformer.js' },
-  // testMatch: ['<rootDir>/src/tests/*.test.js?(x)'],
-  testMatch: ['<rootDir>/src/__tests__/**/**.test.js'],
-  transformIgnorePatterns: ['/node_modules/'],
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['../../jestScaffolding.js'],
-};
