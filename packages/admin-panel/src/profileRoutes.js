89ccd1d98bcfd7b09841acb19124dac1be2bb03c
Tom Caiger <caigertom@gmail.com>
2020-09-21 16:08:53 +1200

    update profile button and new button

--- packages/admin-panel/src/profileRoutes.js
+++ packages/admin-panel/src/profileRoutes.js
@@ -3,8 +3,11 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { ChangePasswordPage } from './pages/ChangePasswordPage';
+import React from 'react';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LockIcon from '@material-ui/icons/Lock';
 import { ProfilePage } from './pages/ProfilePage';
+import { ChangePasswordPage } from './pages/ChangePasswordPage';
 
 export const PROFILE_ROUTES = [
   {
@@ -13,13 +16,16 @@ export const PROFILE_ROUTES = [
       {
         label: 'Profile',
         to: '',
+        icon: <AccountCircleIcon />,
         component: ProfilePage,
       },
-      {
-        label: 'Change Password',
-        to: '/change-password',
-        component: ChangePasswordPage,
-      },
+      // Todo: @see: https://github.com/beyondessential/tupaia-backlog/issues/1218
+      // {
+      //   label: 'Change Password',
+      //   to: '/change-password',
+      //   icon: <LockIcon />,
+      //   component: ChangePasswordPage,
+      // },
     ],
   },
 ];
