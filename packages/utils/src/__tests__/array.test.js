81425c42619c9203ccafa2d080269ccf56c65237
billli0 <lbit123@outlook.com>
2020-09-21 15:51:51 +1000

    Convert to parameterize tests in array.test.js

--- packages/utils/src/__tests__/array.test.js
+++ packages/utils/src/__tests__/array.test.js
@@ -6,64 +6,67 @@
 import { countDistinct, min, max } from '../array';
 
 describe('array', () => {
+  const testData = [
+    ['countDistinct', [], 0],
+    ['single item ', [0], 1],
+    ['multiple items, same value', [0], 1],
+    ['multiple items, different values', [0, 1], 2],
+    ['multiple items, mixed same & different values', [0, 1, 0, 1, 1, 2], 3],
+  ];
   describe('countDistinct', () => {
-    it('empty array ', () => {
-      expect(countDistinct([])).toBe(0);
-    });
-
-    it('single item ', () => {
-      expect(countDistinct([0])).toBe(1);
-    });
-
-    it('multiple items, same value', () => {
-      expect(countDistinct([0, 0])).toBe(1);
-    });
-
-    it('multiple items, different values', () => {
-      expect(countDistinct([0, 1])).toBe(2);
-    });
-
-    it('multiple items, mixed same & different values', () => {
-      expect(countDistinct([0, 1, 0, 1, 1, 2])).toBe(3);
+    it.each(testData)('%s', (string, input, expected) => {
+      expect(countDistinct(input)).toBe(expected);
     });
 
     describe('custom mappers', () => {
-      it('function', () => {
-        expect(countDistinct([1.1, 2, 1.3], Math.floor)).toBe(2);
-      });
+      const customMappersTestData = [
+        ['function', [1.1, 2, 1.3], Math.floor, 2],
+        [
+          'string',
+          [
+            { age: 18, postcode: '3121', gender: 'female' },
+            { age: 18, postcode: '3122', gender: 'male' },
+            { age: 18, postcode: '3123', gender: 'female' },
+            { age: 18, postcode: '3123', gender: 'female' },
+          ],
+          'gender',
+          2,
+        ],
+      ];
 
-      it('string', () => {
-        expect(
-          countDistinct(
-            [
-              { age: 18, postcode: '3121', gender: 'female' },
-              { age: 18, postcode: '3122', gender: 'male' },
-              { age: 18, postcode: '3123', gender: 'female' },
-              { age: 18, postcode: '3123', gender: 'female' },
-            ],
-            'gender',
-          ),
-        ).toBe(2);
+      it.each(customMappersTestData)('%s', (string, array, mapperInput, expected)=>{
+        expect(countDistinct(array, mapperInput)).toBe(expected);
       });
     });
   });
 
   describe('min', () => {
-    it('should return the minimum against the provided values', () => {
-      expect(min([2, 3, 10])).toBe(2);
-      expect(min([-2, 1, 3, 10])).toBe(-2);
-    });
-
+    const testData = [
+      [[2, 3, 10], 2],
+      [[-2, 1, 3, 10], -2],
+    ];
+    it.each(testData)(
+      'should return the minimum against the provided values',
+      (input, expected) => {
+        expect(min(input)).toBe(expected);
+      },
+    );
     it('should return `undefined` for a non compatible input', () => {
       [undefined, null, []].forEach(input => expect(min(input)).toBeUndefined());
     });
   });
 
   describe('max', () => {
-    it('should return the maximum against the provided values', () => {
-      expect(max([2, 3, 10])).toBe(10);
-      expect(max([-20, 1, 3, 10])).toBe(10);
-    });
+    const testData = [
+      [[2, 3, 10], 10],
+      [[-20, 1, 3, 10], 10],
+    ];
+    it.each(testData)(
+      'should return the maximum against the provided values',
+      (input, expected) => {
+        expect(max(input)).toBe(expected);
+      },
+    );
 
     it('should return `undefined` for a non compatible input', () => {
       [undefined, null, []].forEach(input => expect(max(input)).toBeUndefined());
