bfe6ff4755c7bff0cc9ba7905e5ddcf0d6a859c3
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-22 10:31:20 +1000

    Use jest sub

--- packages/utils/src/__tests__/AsyncTaskQueue.test.js
+++ packages/utils/src/__tests__/AsyncTaskQueue.test.js
@@ -1,5 +1,3 @@
-import sinon from 'sinon';
-
 import { AsyncTaskQueue } from '../AsyncTaskQueue';
 
 const sleep = milliseconds => {
@@ -12,29 +10,18 @@ const BATCH_SIZE = 4; // must be even, as we halve it in some tests
 const DEBOUNCE_TIME = 10;
 const TASK_TIME = 100;
 const createAsyncTask = result =>
-  sinon.spy(() => new Promise(resolve => setTimeout(() => resolve(result), TASK_TIME)));
+  jest.fn(() => new Promise(resolve => setTimeout(() => resolve(result), TASK_TIME)));
 const createXTasks = numberOfTasks =>
   Array(numberOfTasks)
     .fill(1)
     .map((_, i) => createAsyncTask(i));
 
-// TODO: Convert mocha to jest
-/**
- * Skip for now.
- *
- * Reason: If using jest only, the @param TASK_TIME couldn't pass in (line 16),
- *
- * since jest.spyOn is writing in this way:
- *
- * const spy = jest.spyOn(video, 'play');
- *
- */
-describe.skip('AsyncTaskQueue', () => {
+describe('AsyncTaskQueue', () => {
   it('should process one task and return the result', async () => {
     const queue = new AsyncTaskQueue(BATCH_SIZE, DEBOUNCE_TIME);
     const task = createAsyncTask('Success!');
     const result = await queue.add(task);
-    expect(task).to.have.been.calledOnceWithExactly();
+    expect(task).toHaveBeenCalledOnceWith();
     expect(result).toBe('Success!');
   });
 
@@ -48,16 +35,16 @@ describe.skip('AsyncTaskQueue', () => {
     // wait for a little bit, so the first batch of tasks should have been started
     await sleep(DEBOUNCE_TIME * 2);
     tasks.slice(0, BATCH_SIZE).forEach(t => {
-      expect(t).to.have.been.calledOnceWithExactly();
+      expect(t).toHaveBeenCalledOnceWith();
     });
 
     // the second and third batches of tasks shouldn't have started yet, as it should still be
     // processing the first batch
-    tasks.slice(BATCH_SIZE).forEach(t => expect(t).not.to.have.been.called);
+    tasks.slice(BATCH_SIZE).forEach(t => expect(t).not.toHaveBeenCalled());
 
     // wait for all of the tasks to be processed, and check they returned the correct results
     const results = await Promise.all(promises);
-    tasks.forEach(t => expect(t).to.have.been.calledOnceWithExactly());
+    tasks.forEach(t => expect(t).toHaveBeenCalledOnceWith());
     results.forEach((p, i) => expect(p).toBe(i));
   });
 
@@ -78,9 +65,9 @@ describe.skip('AsyncTaskQueue', () => {
 
     // the first half batch should still be processing, with the rest waiting until that's done
     halfBatch.forEach(t => {
-      expect(t).to.have.been.calledOnceWithExactly();
+      expect(t).toHaveBeenCalledOnceWith();
     });
-    batchAndAHalf.forEach(t => expect(t).not.to.have.been.called);
+    batchAndAHalf.forEach(t => expect(t).not.toHaveBeenCalled());
 
     // wait for the first half batch of the tasks to be processed, plus a little bit
     await Promise.all(halfBatchPromises);
@@ -88,9 +75,9 @@ describe.skip('AsyncTaskQueue', () => {
 
     // the first batch of the batch and a half should now have been kicked off
     batchAndAHalf.slice(0, BATCH_SIZE).forEach(t => {
-      expect(t).to.have.been.calledOnceWithExactly();
+      expect(t).toHaveBeenCalledOnceWith();
     });
-    batchAndAHalf.slice(BATCH_SIZE).forEach(t => expect(t).not.to.have.been.called);
+    batchAndAHalf.slice(BATCH_SIZE).forEach(t => expect(t).not.toHaveBeenCalled());
 
     // check all results were correct
     const halfBatchResults = await Promise.all(halfBatchPromises);
@@ -117,9 +104,9 @@ describe.skip('AsyncTaskQueue', () => {
 
     // the queue should have been idle, so the first batch should have been kicked off
     batchAndAHalf.slice(0, BATCH_SIZE).forEach(t => {
-      expect(t).to.have.been.calledOnceWithExactly();
+      expect(t).toHaveBeenCalledOnceWith();
     });
-    batchAndAHalf.slice(BATCH_SIZE).forEach(t => expect(t).not.to.have.been.called);
+    batchAndAHalf.slice(BATCH_SIZE).forEach(t => expect(t).not.toHaveBeenCalled());
 
     // check all results were correct
     const halfBatchResults = await Promise.all(halfBatchPromises);
