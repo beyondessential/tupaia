b246474a011edacc16591045a2acb88bcb7123cd
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-23 09:37:14 +1000

    Restructure parameterized tests in replaceValues.test.js

--- packages/utils/src/__tests__/replaceValues.test.js
+++ packages/utils/src/__tests__/replaceValues.test.js
@@ -6,37 +6,25 @@
 import { replaceValues } from '../replaceValues';
 
 describe('replaceValues()', () => {
-  it.each([
-    ['{target}', null, '{target}'],
-    ['{target}', undefined, '{target}'],
-    ['{target}', {}, '{target}'],
-  ])('empty replacement values', (target, replacements, expected) => {
-    expect(replaceValues(target, replacements)).toBe(expected);
-  });
-
-  it('empty replacement values', () => {
-    expect(replaceValues('{target}')).toBe('{target}');
-  });
-
-  it.each([
-    ['no matching replacement value', '{target}', { other: 'replaced' }, '{target}'],
-    ['string input', '{target}', { target: 'replaced' }, 'replaced'],
-  ])('%s', (_, target, replacements, expected) => {
-    expect(replaceValues(target, replacements)).toBe(expected);
-  });
-
-  describe('object input', () => {
-    it.each([
-      ['key replacement', { '{target}': 'value' }, { target: 'replaced' }, { replaced: 'value' }],
-      ['value replacement', { key: '{target}' }, { target: 'replaced' }, { key: 'replaced' }],
+  const testData = [
+    ['undefined replacement values', ['{target}', undefined], '{target}'],
+    ['null replacement values', ['{target}', null], '{target}'],
+    ['empty replacement values', ['{target}', {}], '{target}'],
+    ['no matching replacement value', ['{target}', { other: 'replaced' }], '{target}'],
+    ['string input', ['{target}', { target: 'replaced' }], 'replaced'],
+    ['key replacement', [{ '{target}': 'value' }, { target: 'replaced' }], { replaced: 'value' }],
+    ['value replacement', [{ key: '{target}' }, { target: 'replaced' }], { key: 'replaced' }],
+    [
+      'key and value replacement',
       [
-        'key and value replacement',
         { '{targetKey}': '{targetValue}' },
         { targetKey: 'replacedKey', targetValue: 'replacedValue' },
-        { replacedKey: 'replacedValue' },
       ],
-    ])('%s', (_, target, replacements, expected) => {
-      expect(replaceValues(target, replacements)).toStrictEqual(expected);
-    });
+      { replacedKey: 'replacedValue' },
+    ],
+  ];
+
+  it.each(testData)('%s', (_, [target, replacements], expected) => {
+    expect(replaceValues(target, replacements)).toStrictEqual(expected);
   });
 });
