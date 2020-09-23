97cade3273b467586eb65b2db3bacb4d3b9d76f7
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-23 10:14:55 +1000

    Do not use parameterized tests for mix of matchers

--- packages/utils/src/__tests__/validation/validatorFunctions.test.js
+++ packages/utils/src/__tests__/validation/validatorFunctions.test.js
@@ -5,20 +5,24 @@
 
 import { allValuesAreNumbers } from '../../validation';
 
-describe('validationFunctions', () => {
+describe('validatorFunctions', () => {
   describe('allValuesAreNumbers', () => {
-    it.each([
-      ['fails if a not given an object', null, ''],
-      ['fails if a value is not a number', { hi: 'hello' }, "Value 'hi' is not a number: 'hello'"],
-    ])('%s', (_, object, expectedError) => {
-      expect(() => allValuesAreNumbers(object)).toThrowError(expectedError);
+    it('fails if a not given an object', () => {
+      expect(() => allValuesAreNumbers(null)).toThrowError();
     });
 
-    it.each([
-      ['passes if all values are numbers', { hi: 1, hello: 2 }],
-      ['passes if given an empty object', {}],
-    ])('%s', (_, object) => {
-      expect(() => allValuesAreNumbers(object)).not.toThrowError();
+    it('fails if a value is not a number', () => {
+      expect(() => allValuesAreNumbers({ hi: 'hello' })).toThrowError(
+        "Value 'hi' is not a number: 'hello'",
+      );
+    });
+
+    it('passes if all values are numbers', () => {
+      expect(() => allValuesAreNumbers({ hi: 1, hello: 2 })).not.toThrowError();
+    });
+
+    it('passes if given an empty object', () => {
+      expect(() => allValuesAreNumbers({})).not.toThrowError();
     });
   });
 });
