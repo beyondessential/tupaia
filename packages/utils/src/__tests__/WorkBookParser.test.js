bfaf8265415fc679e891f80cf3f4a7074af2054b
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-23 10:09:35 +1000

    Use promise matchers

--- packages/utils/src/__tests__/WorkBookParser.test.js
+++ packages/utils/src/__tests__/WorkBookParser.test.js
@@ -42,9 +42,8 @@ describe('WorkBookParser', () => {
         },
       };
       const parser = new WorkBookParser(mappers);
-      expect.assertions(1);
-      const result = await parser.parse(WORK_BOOK);
-      expect(result).toEqual({
+
+      return expect(parser.parse(WORK_BOOK)).resolves.toStrictEqual({
         Sheet1: [
           { Value1: 'HeaderA', Value2: 'HeaderB' },
           { Value3: 'HeaderA', Value4: 'HeaderB' },
@@ -58,7 +57,7 @@ describe('WorkBookParser', () => {
 
     it('should map the input to itself if no mapper is provided', async () => {
       const parser = new WorkBookParser();
-      await expect(parser.parse(WORK_BOOK)).resolves.toStrictEqual(SHEETS);
+      return expect(parser.parse(WORK_BOOK)).resolves.toStrictEqual(SHEETS);
     });
 
     describe('should use the provided validators to validate a spreadsheet', () => {
@@ -75,15 +74,16 @@ describe('WorkBookParser', () => {
         const triggeringValue = 'Value7';
         const validators = constructValidators(triggeringValue);
         const parser = new WorkBookParser({}, validators);
-        await expect(parser.parse(WORK_BOOK)).rejects.toThrow(errorMessage);
+
+        return expect(parser.parse(WORK_BOOK)).rejects.toThrow(errorMessage);
       });
 
       it("validator is not triggered when it shouldn't", async () => {
         const nonTriggeringValue = 'Value6';
         const validators = constructValidators(nonTriggeringValue);
         const parser = new WorkBookParser({}, validators);
-        const result = await parser.parse(WORK_BOOK);
-        return expect(result).toEqual(SHEETS);
+
+        return expect(parser.parse(WORK_BOOK)).resolves.toStrictEqual(SHEETS);
       });
     });
   });
