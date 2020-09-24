64fc67f337a3b74832a25d5f1a896508ef6fb403
billli0 <lbit123@outlook.com>
2020-09-24 10:34:15 +1000

    Restructure parameterized tests

--- packages/access-policy/src/__tests__/AccessPolicy.test.js
+++ packages/access-policy/src/__tests__/AccessPolicy.test.js
@@ -15,107 +15,92 @@ describe('AccessPolicy', () => {
   const accessPolicy = new AccessPolicy(policy);
 
   describe('allows', () => {
-    it.each([
+    const testData = [
       [
         'should allow access to an entity that has at least one permission group, when no permission group is specified',
-        'DL',
+        ['DL', ''],
         true,
       ],
-      ['should not allow access to an entity that has no permission groups', 'VU', false],
-    ])('%s', (name, entity, expected) => {
-      expect(accessPolicy.allows(entity)).toBe(expected);
-    });
-
-    it.each([
+      ['should not allow access to an entity that has no permission groups', ['VU', ''], false],
       [
         'should allow access to a permission group when a matching entity is specified',
-        'DL',
-        'Public',
+        ['DL', 'Public'],
         true,
       ],
       [
         'should not allow access to a permission group that exists for a different entity than the one specified',
-        'DL',
-        'Admin',
+        ['DL', 'Admin'],
         false,
       ],
-      [
-        'should allow access to a permission group that includes spaces',
-        'SB',
-        'Royal Australasian College of Surgeons',
-        true,
-      ],
-    ])('%s', (name, entity, permissionGroup, expected) => {
-      expect(accessPolicy.allows(entity, permissionGroup)).toBe(expected);
-    });
+      ['should not allow access with no entity', ['', ''], false],
+      ['should not allow access with null', [null, ''], false],
+    ];
 
-    it('should not allow access if no entity is specified', () => {
-      expect(accessPolicy.allows()).toBe(false);
-      expect(accessPolicy.allows(null)).toBe(false);
+    it.each(testData)('%s', (_, [entity, permissionGroup], expected) => {
+      expect(accessPolicy.allows(entity, permissionGroup)).toBe(expected);
     });
   });
 
   describe('allowsSome', () => {
-    it.each([
-      [
-        'should allow access to an entity that has at least one permission group, when no permission group is specified',
-        ['DL'],
-        true,
-      ],
-      ['should not allow access to an entity that has no permission groups', ['VU'], false],
-      ['should not allow access if no entities or permission group are specified', [], false],
-      ['should not allow access if no entities or permission group are specified', null, false],
-    ])('%s', (name, entities, expected) => {
-      expect(accessPolicy.allowsSome(entities)).toBe(expected);
+    describe('no permission group', () => {
+      const testData = [
+        [
+          'should allow access to an entity that has at least one permission group, when no permission group is specified',
+          ['DL'],
+          true,
+        ],
+        ['should not allow access to an entity that has no permission groups', ['VU'], false],
+        ['should not allow access with no entity', [], false],
+        ['should not allow access with null', null, false],
+        ["should not allow access with ''", '', false],
+      ];
+
+      it.each(testData)('%s', (_, entities, expected) => {
+        expect(accessPolicy.allowsSome(entities)).toBe(expected);
+      });
     });
 
-    it.each([
-      [
-        'should allow access to a permission group when a matching entity is specified',
-        ['DL'],
-        'Public',
-        true,
-      ],
-      [
-        'should allow access to a permission group when multiple entities are specified',
-        ['DL', 'DL_North_West', 'VU'],
-        'Public',
-        true,
-      ],
-      [
-        'should not allow access to a permission group that exists for a different entity than those specified',
-        ['DL', 'DL_North_West', 'VU'],
-        'Admin',
-        false,
-      ],
-      [
-        'should allow access to a permission group that includes spaces',
-        ['SB'],
-        'Royal Australasian College of Surgeons',
-        true,
-      ],
-      [
-        'should allow access if no entities are specified, along with a permission group',
-        undefined,
-        'Royal Australasian College of Surgeons',
-        true,
-      ],
-      [
-        'should allow access if no entities are specified, along with a permission group',
-        null,
-        'Royal Australasian College of Surgeons',
-        true,
-      ],
-    ])('%s', (name, entities, permissionGroup, expected) => {
-      expect(accessPolicy.allowsSome(entities, permissionGroup)).toBe(expected);
-    });
+    describe('with permission group', () => {
+      const testData = [
+        [
+          'should allow access to a permission group when a matching entity is specified',
+          [['DL'], 'Public'],
+          true,
+        ],
+        [
+          'should allow access to a permission group when multiple entities are specified',
+          [['DL', 'DL_North_West', 'VU'], 'Public'],
+          true,
+        ],
+        [
+          'should not allow access to a permission group that exists for a different entity than those specified',
+          [['DL', 'DL_North_West', 'VU'], 'Admin'],
+          false,
+        ],
+        [
+          'should allow access to a permission group that includes spaces',
+          [['SB'], 'Royal Australasian College of Surgeons'],
+          true,
+        ],
+        [
+          'should allow access if no entities are specified, along with a permission group',
+          [undefined, 'Royal Australasian College of Surgeons'],
+          true,
+        ],
+        [
+          'should allow access if no entities are specified, along with a permission group',
+          [null, 'Royal Australasian College of Surgeons'],
+          true,
+        ],
+      ];
 
-    it('should not allow access if no entities or permission group are specified', () => {
-      expect(accessPolicy.allowsSome()).toBe(false);
+      it.each(testData)('%s', (_, [entities, permissionGroup], expected) => {
+        expect(accessPolicy.allowsSome(entities, permissionGroup)).toBe(expected);
+      });
     });
   });
 
-  describe('AccessPolicy.getPermissionGroups', () => {
+  describe('getPermissionGroups', () => {
     it('should return all permission groups when no entities are specified', () => {
       expect(accessPolicy.getPermissionGroups()).toStrictEqual(
         expect.arrayContaining(['Public', 'Admin', 'Royal Australasian College of Surgeons']),
@@ -123,24 +108,21 @@ describe('AccessPolicy', () => {
     });
 
     describe('should return just the permission groups that relate to the provided entities', () => {
-      it.each([
-        [['KI'], ['Public', 'Admin']],
+      const testData = [
+        ['entity (i)', ['KI'], ['Public', 'Admin']],
+        ['entity (ii)', ['DL', 'KI'], ['Public', 'Admin']],
         [
-          ['DL', 'KI'],
-          ['Public', 'Admin'],
-        ],
-        [
+          'entity (iii)',
           ['SB', 'KI'],
           ['Public', 'Admin', 'Royal Australasian College of Surgeons'],
         ],
-      ])(
-        'should return just the permission groups that relate to the provided entities',
-        (arr, expected) => {
-          expect(accessPolicy.getPermissionGroups(arr)).toStrictEqual(
-            expect.arrayContaining(expected),
-          );
-        },
-      );
+      ];
+
+      it.each(testData)('%s', (_, arr, expected) => {
+        expect(accessPolicy.getPermissionGroups(arr)).toStrictEqual(
+          expect.arrayContaining(expected),
+        );
+      });
     });
   });
 });
