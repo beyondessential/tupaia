d63136606abc9b91c746124743d2835e0e8722ce
billli0 <lbit123@outlook.com>
2020-09-28 20:48:49 +1000

    Convert mocha to jest in filterAnalytics.jest.js

--- packages/aggregator/src/tests/analytics/filterAnalytics.test.js
+++ packages/aggregator/src/tests/analytics/filterAnalytics.test.js
@@ -3,83 +3,110 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { expect } from 'chai';
 import { filterAnalytics } from '../../analytics/filterAnalytics';
 
 const analytics = [{ value: 1 }, { value: 2 }, { value: 3 }];
 
 describe('filterAnalytics()', () => {
-  it('empty filter', () => {
-    expect(() => filterAnalytics(analytics)).to.not.throw();
-    expect(() => filterAnalytics(analytics, undefined)).to.not.throw();
-    expect(() => filterAnalytics(analytics, {})).to.not.throw();
+  describe('empty filter', () => {
+    const testData = [
+      ['i', []],
+      ['ii', [undefined]],
+      ['iii', [{}]],
+    ];
+    it.each(testData)('%s', (_, [filter]) => {
+      expect(() => filterAnalytics(analytics, filter)).not.toThrow();
+    });
   });
 
   describe('filter type', () => {
-    it('=', () => {
-      expect(filterAnalytics(analytics, { value: 2 })).to.deep.equal([{ value: 2 }]);
-      expect(filterAnalytics(analytics, { value: 4 })).to.deep.equal([]);
-      expect(filterAnalytics(analytics, { value: { '=': 2 } })).to.deep.equal([{ value: 2 }]);
-      expect(filterAnalytics(analytics, { value: { '=': 2 } })).to.deep.equal(
-        filterAnalytics(analytics, { value: 2 }),
-      );
-    });
-
-    it('>', () => {
-      expect(filterAnalytics(analytics, { value: { '>': 2 } })).to.deep.equal([{ value: 3 }]);
-      expect(filterAnalytics(analytics, { value: { '>': 4 } })).to.deep.equal([]);
-    });
-
-    it('>=', () => {
-      expect(filterAnalytics(analytics, { value: { '>=': 2 } })).to.deep.equal([
-        { value: 2 },
-        { value: 3 },
-      ]);
-      expect(filterAnalytics(analytics, { value: { '>=': 4 } })).to.deep.equal([]);
-    });
-
-    it('<', () => {
-      expect(filterAnalytics(analytics, { value: { '<': 2 } })).to.deep.equal([{ value: 1 }]);
-      expect(filterAnalytics(analytics, { value: { '<': 0 } })).to.deep.equal([]);
-    });
-
-    it('<=', () => {
-      expect(filterAnalytics(analytics, { value: { '<=': 2 } })).to.deep.equal([
-        { value: 1 },
-        { value: 2 },
-      ]);
-      expect(filterAnalytics(analytics, { value: { '<=': 0 } })).to.deep.equal([]);
-    });
-
-    describe('combines multiple types using AND logic', () => {
-      it('one type superset of the other', () => {
-        expect(filterAnalytics(analytics, { value: { '<=': 2, '=': 2 } })).to.deep.equal([
-          { value: 2 },
-        ]);
-        expect(filterAnalytics(analytics, { value: { '=': 2, '<=': 2 } })).to.deep.equal([
-          { value: 2 },
-        ]);
+    const testData = [
+      [
+        'single type',
+        [
+          [
+            '=',
+            [
+              [{ value: 2 }, [{ value: 2 }]],
+              [{ value: 4 }, []],
+              [{ value: { '=': 2 } }, [{ value: 2 }]],
+              [{ value: { '=': 2 } }, filterAnalytics(analytics, { value: 2 })],
+            ],
+          ],
+          [
+            '>',
+            [
+              [{ value: { '>': 2 } }, [{ value: 3 }]],
+              [{ value: { '>': 4 } }, []],
+            ],
+          ],
+          [
+            '>=',
+            [
+              [{ value: { '>=': 2 } }, [{ value: 2 }, { value: 3 }]],
+              [{ value: { '>=': 4 } }, []],
+            ],
+          ],
+          [
+            '<',
+            [
+              [{ value: { '>=': 2 } }, [{ value: 2 }, { value: 3 }]],
+              [{ value: { '>=': 4 } }, []],
+            ],
+          ],
+          [
+            '<=',
+            [
+              [{ value: { '<=': 2 } }, [{ value: 1 }, { value: 2 }]],
+              [{ value: { '<=': 0 } }, []],
+            ],
+          ],
+        ],
+      ],
+      [
+        'combines multiple types using AND logic',
+        [
+          [
+            'one type superset of the other',
+            [
+              [{ value: { '<=': 2, '=': 2 } }, [{ value: 2 }]],
+              [{ value: { '=': 2, '<=': 2 } }, [{ value: 2 }]],
+            ],
+          ],
+          [
+            'mutually exclusive types',
+            [
+              [{ value: { '=': 2, '<': 2 } }, []],
+              [{ value: { '<': 2, '=': 2 } }, []],
+            ],
+          ],
+          [
+            'known and unknown types',
+            [
+              [{ value: { '=': 2, unknownFilter: 3 } }, [{ value: 2 }]],
+              [{ value: { '=': 2, unknownFilter: 3 } }, [{ value: 2 }]],
+            ],
+          ],
+          [
+            'unknown types',
+            [
+              [
+                { value: { operator: 'unknownFilter', value: 0 } },
+                [{ value: 1 }, { value: 2 }, { value: 3 }],
+              ],
+            ],
+          ],
+        ],
+      ],
+    ];
+    testData.forEach(([name, testSuites]) => {
+      describe(name, () => {
+        it.each(testSuites)('%s', (_, testCaseData) => {
+          testCaseData.forEach(([filter, expected]) => {
+            expect(filterAnalytics(analytics, filter)).toStrictEqual(expected);
+          });
+        });
       });
-
-      it('mutually exclusive types', () => {
-        expect(filterAnalytics(analytics, { value: { '=': 2, '<': 2 } })).to.deep.equal([]);
-        expect(filterAnalytics(analytics, { value: { '<': 2, '=': 2 } })).to.deep.equal([]);
-      });
-
-      it('known and unknown types', () => {
-        expect(filterAnalytics(analytics, { value: { '=': 2, unknownFilter: 3 } })).to.deep.equal([
-          { value: 2 },
-        ]);
-        expect(filterAnalytics(analytics, { value: { '=': 2, unknownFilter: 3 } })).to.deep.equal([
-          { value: 2 },
-        ]);
-      });
-    });
-
-    it('unknown type', () => {
-      expect(
-        filterAnalytics(analytics, { value: { operator: 'unknownFilter', value: 0 } }),
-      ).to.deep.equal([{ value: 1 }, { value: 2 }, { value: 3 }]);
     });
   });
 });
