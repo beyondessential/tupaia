7d538bf44318770f4569716e21eafab6c7d02c19
billli0 <lbit123@outlook.com>
2020-09-28 10:03:28 +1000

    Add copyright

--- scripts/node/JestTransformer.js
+++ scripts/node/JestTransformer.js
@@ -1,4 +1,9 @@
 /**
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+/**
  * https://babeljs.io/docs/en/config-files#jest
  *
  * Setup jest in Monorepo. This is to create a custom jest transformer file that wraps babel-jest.
