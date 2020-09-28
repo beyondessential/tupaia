095ea4b11f1937da1fbbe0fe3866aa22c9b83ce7
billli0 <lbit123@outlook.com>
2020-09-28 20:48:22 +1000

    Convert mocha to jest in createAggregator.jest.js

--- packages/aggregator/src/tests/createAggregator.test.js
+++ packages/aggregator/src/tests/createAggregator.test.js
@@ -1,5 +1,3 @@
-import { expect } from 'chai';
-
 import { DataBroker } from '@tupaia/data-broker';
 import { Aggregator } from '../Aggregator';
 import { createAggregator } from '../createAggregator';
@@ -13,16 +11,16 @@ const CustomAggregatorClass = class CustomAggregator {
 describe('createAggregator()', () => {
   it('uses the package Aggregator class by default', () => {
     const aggregator = createAggregator();
-    expect(aggregator).to.be.instanceOf(Aggregator);
+    expect(aggregator).toBeInstanceOf(Aggregator);
   });
 
   it('allows injecting a custom Aggregator class', () => {
     const aggregator = createAggregator(CustomAggregatorClass);
-    expect(aggregator).to.be.instanceOf(CustomAggregatorClass);
+    expect(aggregator).toBeInstanceOf(CustomAggregatorClass);
   });
 
   it('call the Aggregator constructor with the DataBroker class', () => {
     const aggregator = createAggregator();
-    expect(aggregator.dataBroker).to.be.instanceOf(DataBroker);
+    expect(aggregator.dataBroker).toBeInstanceOf(DataBroker);
   });
 });
