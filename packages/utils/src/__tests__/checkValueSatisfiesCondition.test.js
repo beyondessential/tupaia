ad2e17c99526fe29ace2dbdec9ef96915e2d45c0
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-09-30 14:40:12 +1000

    Destructure test cases

--- packages/utils/src/__tests__/checkValueSatisfiesCondition.test.js
+++ packages/utils/src/__tests__/checkValueSatisfiesCondition.test.js
@@ -8,117 +8,144 @@ describe('checkAgainstConditions', () => {
       );
     });
 
-    const testData = [
-      [
-        'should correctly check if a value satisfies a plain condition',
-        [
-          [['hello', 'hello'], true],
-          [[0, 0], true],
-          [[true, true], true],
-          [[false, false], true],
-          [[null, null], true],
-          [['hello', 'hi'], false],
-          [[0, ''], false],
-          [[true, 1], false],
-          // Undefined value never satisfies any condition
-          [[undefined, undefined], false],
-        ],
-      ],
-      [
-        'should return true if the condition is "*" unless the value is undefined or ""',
-        [
-          [['hello', '*'], true],
-          [[0, '*'], true],
-          [[false, '*'], true],
-          [[null, '*'], true],
-          [['', '*'], false],
-          [[undefined, '*'], false],
-        ],
-      ],
-      [
-        'should work with basic mathematical conditions',
-        [
-          [[1, { operator: '=', value: 1 }], true],
-          [[2, { operator: '=', value: 1 }], false],
-          [[1, { operator: '<', value: 2 }], true],
-          [[2, { operator: '<', value: 2 }], false],
-          [[2, { operator: '>', value: 1 }], true],
-          [[1, { operator: '>', value: 1 }], false],
-          [[1, { operator: '>=', value: 1 }], true],
-          [[0, { operator: '>=', value: 1 }], false],
-          [[1, { operator: '<=', value: 1 }], true],
-          [[2, { operator: '<=', value: 1 }], false],
-          [[1, { operator: '<=', value: 1 }], true],
-          [[2, { operator: '<=', value: 1 }], false],
-        ],
-      ],
-      [
-        'should work with range mathematical conditions',
-        [
-          [[-1, { operator: 'range', value: [1, 2] }], false],
-          [[1, { operator: 'range', value: [1, 2] }], true],
-          [[1.5, { operator: 'range', value: [1, 2] }], true],
-          [[2, { operator: 'range', value: [1, 2] }], true],
-          [[3, { operator: 'range', value: [1, 2] }], false],
-          [[-1, { operator: 'rangeExclusive', value: [1, 2] }], false],
-          [[1, { operator: 'rangeExclusive', value: [1, 2] }], false],
-          [[1.5, { operator: 'rangeExclusive', value: [1, 2] }], true],
-          [[2, { operator: 'rangeExclusive', value: [1, 2] }], false],
-          [[3, { operator: 'rangeExclusive', value: [1, 2] }], false],
-        ],
-      ],
-      [
-        'should return false with mathematical conditions if the value is ""',
-        [
-          [['', { operator: '=', value: 1 }], false],
-          [['', { operator: '<', value: 2 }], false],
-          [['', { operator: '>', value: 1 }], false],
-          [['', { operator: '>=', value: 1 }], false],
-          [['', { operator: '<=', value: 1 }], false],
-          [['', { operator: '<=', value: 1 }], false],
-          [['', { operator: 'range', value: [1, 2] }], false],
-          [['', { operator: 'rangeExclusive', value: [1, 2] }], false],
-        ],
-      ],
-      [
-        'should check "in" correctly',
-        [
-          [['', { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], true],
-          [[null, { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], true],
-          [['hi', { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], true],
-          [[1, { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], true],
-          [[3, { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], false],
-          [[undefined, { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], false],
-          [['hello', { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] }], false],
-        ],
-      ],
-      [
-        'operator "in" works with strings correctly',
-        [
-          [['', { operator: 'in', value: 'This is my string' }], true],
-          [['string', { operator: 'in', value: 'This is my string' }], true],
-          [[0, { operator: 'in', value: 'This is my string' }], false],
-          [[undefined, { operator: 'in', value: 'This is my string' }], false],
-          [['hello', { operator: 'in', value: 'This is my string' }], false],
-        ],
-      ],
-      [
-        'operator "regex" works correctly',
-        [
-          [['hi five!', { operator: 'regex', value: 'hi' }], true],
-          [['hi five!', { operator: 'regex', value: /hi/ }], true],
-          [['hi five!', { operator: 'regex', value: '[hgf]i' }], true],
-          [['hi five!', { operator: 'regex', value: /[hgf]i/ }], true],
-          [['', { operator: 'regex', value: '' }], true],
-          [['hi five!', { operator: 'regex', value: 'Hello' }], false],
-          [['hi five!', { operator: 'regex', value: 'Hi' }], false],
-          [[undefined, { operator: 'regex', value: '' }], false],
-        ],
-      ],
-    ];
+    it('should correctly check if a value satisfies a plain condition', () => {
+      const testData = [
+        ['hello', 'hello', true],
+        [0, 0, true],
+        [true, true, true],
+        [false, false, true],
+        [null, null, true],
+        ['hello', 'hi', false],
+        [0, '', false],
+        [true, 1, false],
+        // Undefined value never satisfies any condition
+        [undefined, undefined, false],
+      ];
 
-    it.each(testData)('%s', (_, testCaseData) => {
-      testCaseData.forEach(([[value, condition], expected]) => {
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('should return true if the condition is "*" unless the value is undefined or ""', () => {
+      const testData = [
+        ['hello', '*', true],
+        [0, '*', true],
+        [false, '*', true],
+        [null, '*', true],
+        ['', '*', false],
+        [undefined, '*', false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('should work with basic mathematical conditions', () => {
+      const testData = [
+        [1, { operator: '=', value: 1 }, true],
+        [2, { operator: '=', value: 1 }, false],
+        [1, { operator: '<', value: 2 }, true],
+        [2, { operator: '<', value: 2 }, false],
+        [2, { operator: '>', value: 1 }, true],
+        [1, { operator: '>', value: 1 }, false],
+        [1, { operator: '>=', value: 1 }, true],
+        [0, { operator: '>=', value: 1 }, false],
+        [1, { operator: '<=', value: 1 }, true],
+        [2, { operator: '<=', value: 1 }, false],
+        [1, { operator: '<=', value: 1 }, true],
+        [2, { operator: '<=', value: 1 }, false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('should work with range mathematical conditions', () => {
+      const testData = [
+        [-1, { operator: 'range', value: [1, 2] }, false],
+        [1, { operator: 'range', value: [1, 2] }, true],
+        [1.5, { operator: 'range', value: [1, 2] }, true],
+        [2, { operator: 'range', value: [1, 2] }, true],
+        [3, { operator: 'range', value: [1, 2] }, false],
+        [-1, { operator: 'rangeExclusive', value: [1, 2] }, false],
+        [1, { operator: 'rangeExclusive', value: [1, 2] }, false],
+        [1.5, { operator: 'rangeExclusive', value: [1, 2] }, true],
+        [2, { operator: 'rangeExclusive', value: [1, 2] }, false],
+        [3, { operator: 'rangeExclusive', value: [1, 2] }, false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('should return false with mathematical conditions if the value is ""', () => {
+      const testData = [
+        ['', { operator: '=', value: 1 }, false],
+        ['', { operator: '<', value: 2 }, false],
+        ['', { operator: '>', value: 1 }, false],
+        ['', { operator: '>=', value: 1 }, false],
+        ['', { operator: '<=', value: 1 }, false],
+        ['', { operator: '<=', value: 1 }, false],
+        ['', { operator: 'range', value: [1, 2] }, false],
+        ['', { operator: 'rangeExclusive', value: [1, 2] }, false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('should check "in" correctly', () => {
+      const commonConfig = { operator: 'in', value: ['', undefined, null, 'hi', 1, 2] };
+
+      const testData = [
+        ['', commonConfig, true],
+        [null, commonConfig, true],
+        ['hi', commonConfig, true],
+        [1, commonConfig, true],
+        [3, commonConfig, false],
+        [undefined, commonConfig, false],
+        ['hello', commonConfig, false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('operator "in" works with strings correctly', () => {
+      const commonConfig = { operator: 'in', value: 'This is my string' };
+
+      const testData = [
+        ['', commonConfig, true],
+        ['string', commonConfig, true],
+        [0, commonConfig, false],
+        [undefined, commonConfig, false],
+        ['hello', commonConfig, false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
+        expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
+      });
+    });
+
+    it('operator "regex" works correctly', () => {
+      const testData = [
+        ['hi five!', { operator: 'regex', value: 'hi' }, true],
+        ['hi five!', { operator: 'regex', value: /hi/ }, true],
+        ['hi five!', { operator: 'regex', value: '[hgf]i' }, true],
+        ['hi five!', { operator: 'regex', value: /[hgf]i/ }, true],
+        ['', { operator: 'regex', value: '' }, true],
+        ['hi five!', { operator: 'regex', value: 'Hello' }, false],
+        ['hi five!', { operator: 'regex', value: 'Hi' }, false],
+        [undefined, { operator: 'regex', value: '' }, false],
+      ];
+
+      testData.forEach(([value, condition, expected]) => {
         expect(checkValueSatisfiesCondition(value, condition)).toBe(expected);
       });
     });
