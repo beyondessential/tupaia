884f7d393c24b7bb8d7162beec3265ba1a0a1ef5
Tom Caiger <caigertom@gmail.com>
2020-10-01 10:21:43 +1300

    fix file header

--- packages/admin-panel/src/pages/resources/ProjectsPage.js
+++ packages/admin-panel/src/pages/resources/ProjectsPage.js
@@ -1,12 +1,11 @@
-/**
- * Tupaia MediTrak
- * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ResourcePage } from './ResourcePage';
-import { prettyArray } from '../../utilities';
 
 const FIELDS = [
   {
