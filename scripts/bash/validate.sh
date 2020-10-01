2ce88402968156582bd7ce2961ff41fdeeeef084
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-02 08:38:04 +1000

    Reverse check

--- scripts/bash/validate.sh
+++ scripts/bash/validate.sh
@@ -6,9 +6,7 @@ DIR=`dirname "$0"`
 . ${DIR}/../../packages/devops/scripts/ci/utils.sh
 
 yarn -s workspace @tupaia/devops -s validate-branch-name
-
-if yarn -s workspace @tupaia/devops -s validate-tests ; then
-    :
-else
+yarn -s workspace @tupaia/devops -s validate-tests
+if [[ $? > 0 ]] ; then
     log_warn "Exclusive (.only) tests found in your code. The CI/CD build will fail if you push those"
 fi
