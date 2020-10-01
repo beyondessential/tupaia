34f1de3ee854b187fde882798e0959d1463dd7e5
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-01 21:33:50 +1000

    Add branch name length validation

--- packages/devops/scripts/ci/validateBranchName.sh
+++ packages/devops/scripts/ci/validateBranchName.sh
@@ -3,14 +3,23 @@
 DIR=`dirname "$0"`
 . ${DIR}/utils.sh
 
+MAX_LENGTH=56
 RESERVED_ENDINGS=(api config export mobile admin www)
 
 branch_name="$CI_BRANCH"
-if [[ $branch_name == "" ]];then
+if [[ $branch_name == "" ]]; then
     # Get currently checked out branch
     branch_name=`git rev-parse --abbrev-ref HEAD`
 fi
 
+# Validate branch name length
+name_length=${#branch_name}
+if [[ $name_length -gt MAX_LENGTH ]]; then
+    log_error "❌ Branch name is too long, must be $MAX_LENGTH characters max"
+    exit 1;
+fi
+
+# Validate branch name ending
 for reserved_ending in ${RESERVED_ENDINGS[@]}
 do
     if [[ "$branch_name" == *$reserved_ending ]]; then
