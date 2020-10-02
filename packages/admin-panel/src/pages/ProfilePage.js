e08b9fb7ec7e41cece72e06c37c4010febc772de
Tom Caiger <caigertom@gmail.com>
2020-10-02 15:19:38 +1300

    extract profile image field to ui components

--- packages/admin-panel/src/pages/ProfilePage.js
+++ packages/admin-panel/src/pages/ProfilePage.js
@@ -8,14 +8,11 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import MuiAvatar from '@material-ui/core/Avatar';
 import MuiDivider from '@material-ui/core/Divider';
-import Box from '@material-ui/core/Box';
-import Fab from '@material-ui/core/Fab';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { Button, SmallAlert, TextField, FileUploadField } from '@tupaia/ui-components';
+import { Button, SmallAlert, TextField, ProfileImageField } from '@tupaia/ui-components';
 import { usePortalWithCallback } from '../utilities';
 import { Header } from '../widgets';
+import { createBase64Image } from '../utilities/createBase64Image';
 import { updateProfile, getUser } from '../authentication';
 
 const Container = styled.section`
@@ -27,42 +24,13 @@ const Container = styled.section`
 `;
 
 const Divider = styled(MuiDivider)`
-  margin: 0.5rem 0 1.8rem;
+  margin: 1.8rem 0;
 `;
 
 const StyledButton = styled(Button)`
   margin-top: 1rem;
   padding-top: 0.8rem;
   padding-bottom: 0.8rem;
-`;
-
-const Avatar = styled(MuiAvatar)`
-  position: relative;
-  color: white;
-  background: ${props => props.theme.palette.success.main};
-  font-weight: 600;
-  width: 85px;
-  height: 85px;
-  font-size: 45px;
-  margin-right: 1rem;
-`;
-
-const DeleteButton = styled(Fab)`
-  position: absolute;
-  right: 5px;
-  bottom: 0;
-  width: 30px;
-  min-width: 30px;
-  height: 30px;
-  box-shadow: none;
-  min-height: 30px;
-  background: white;
-  border: 1px solid ${props => props.theme.palette.grey['400']};
-  color: ${props => props.theme.palette.text.secondary};
-
-  .MuiSvgIcon-root {
-    font-size: 18px;
-  }
 `;
 
 const ErrorMessage = styled.p`
@@ -73,21 +41,6 @@ const SuccessMessage = styled(SmallAlert)`
   margin-top: -1rem;
   margin-bottom: 1.5rem;
 `;
-
-// add to utils
-function createBase64Image(fileObject) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      resolve(reader.result);
-    };
-
-    reader.onerror = reject;
-
-    reader.readAsDataURL(fileObject);
-  });
-}
 
 const STATUS = {
   IDLE: 'idle',
@@ -105,6 +58,10 @@ const initialState = {
     fileId: null,
     data: null,
   },
+};
+
+const initReducer = user => {
+  return { ...initialState, profileImage: { data: user.profileImage, fileId: null } };
 };
 
 function reducer(state, action) {
@@ -134,10 +91,6 @@ function reducer(state, action) {
       throw new Error('type does not exist');
   }
 }
-
-const initReducer = user => {
-  return { ...initialState, profileImage: { data: user.profileImage, fileId: null } };
-};
 
 const ProfilePageComponent = React.memo(({ user, onUpdateProfile, getHeaderEl }) => {
   const { handleSubmit, register, errors } = useForm();
@@ -192,20 +145,13 @@ const ProfilePageComponent = React.memo(({ user, onUpdateProfile, getHeaderEl })
         <form onSubmit={onSubmit} noValidate>
           {status === STATUS.ERROR && <ErrorMessage>{errorMessage}</ErrorMessage>}
           {status === STATUS.SUCCESS && <SuccessMessage>{successMessage}</SuccessMessage>}
-          <Box display="flex" mb={2} alignItems="center">
-            <Box position="relative">
-              <Avatar src={profileImage && profileImage.data && profileImage.data}>
-                {userInitial}
-              </Avatar>
-              <DeleteButton onClick={handleDelete}>
-                <DeleteIcon />
-              </DeleteButton>
-            </Box>
-            {/*<div>{fileUpload}</div>*/}
-            <input type="file" name="profileImage" ref={register} onChange={handleFileChange} />
-            {/*<div>{updatedProfileImage.fileName}</div>*/}
-            {/*<FileUploadField name="avatarUpload" label="Your avatar" />*/}
-          </Box>
+          <ProfileImageField
+            name="profileImage"
+            profileImage={profileImage && profileImage.data}
+            userInitial={userInitial}
+            onChange={handleFileChange}
+            onDelete={handleDelete}
+          />
           <Divider />
           <TextField
             label="First Name"
