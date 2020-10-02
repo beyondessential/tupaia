b504aea423c05f79148a7d5a483b882c639dce4c
Tom Caiger <caigertom@gmail.com>
2020-10-02 16:22:29 +1300

    fix tests

--- packages/meditrak-server/src/s3/uploadImage.js
+++ packages/meditrak-server/src/s3/uploadImage.js
@@ -8,19 +8,20 @@ import { BUCKET_NAME, getImageFilePath } from './constants';
 
 export const uploadImage = (base64EncodedImage, fileId) => {
   const buffer = Buffer.from(base64EncodedImage.replace(/^data:image\/\w+;base64,/, ''), 'base64');
-  const filePath = getImageFilePath();
-  const fileName = fileId
-    ? `${filePath}${fileId}`
-    : `${filePath}${getCurrentTime()}_${getRandomInteger()}.png`;
 
-  const type = fileId ? base64EncodedImage.split(';')[0].split('/')[1] : 'png';
+  const filePath = getImageFilePath();
+  const type = base64EncodedImage.split(';')[0].split('/')[1] || 'png';
+
+  const fileName = fileId
+    ? `${filePath}${fileId}.${type}`
+    : `${filePath}${getCurrentTime()}_${getRandomInteger()}.png`;
 
   return new Promise((resolve, reject) => {
     const s3Client = getS3Client();
     s3Client.upload(
       {
         Bucket: BUCKET_NAME,
-        Key: `${fileName}.${type}`,
+        Key: fileName,
         Body: buffer,
         ACL: 'public-read',
         ContentType: `image/${type}`,
