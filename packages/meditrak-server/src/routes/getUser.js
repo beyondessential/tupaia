540528e32ad7fddf72c2b9d92e5b2a8e248ded2d
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-10-04 21:55:56 +1100

    resolved conflicts caused by merging dev

--- packages/meditrak-server/src/routes/getUser.js
+++ packages/meditrak-server/src/routes/getUser.js
@@ -4,7 +4,7 @@
  */
 
 import { DatabaseError } from '@tupaia/utils';
-import { getRecords } from './getRecords';
+import { GETUserAccounts } from './userAccounts';
 
 export const getUser = async (req, res, next) => {
   const { models, userId } = req;
@@ -19,7 +19,8 @@ export const getUser = async (req, res, next) => {
       recordId: userId,
       resource: 'user',
     };
-    await getRecords(req, res);
+    const getUserAccountHandlerClass = new GETUserAccounts(req, res);
+    await getUserAccountHandlerClass.handle();
   } catch (error) {
     next(error);
   }
