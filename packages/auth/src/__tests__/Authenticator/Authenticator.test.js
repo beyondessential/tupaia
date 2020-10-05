7e3a461e6cd32e6b36386471d3e7267196032758
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-05 11:21:50 +1100

    Restore folder with correct name case

---
+++ packages/auth/src/__tests__/Authenticator/Authenticator.test.js
@@ -0,0 +1,18 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+import randomToken from 'rand-token';
+import { refreshToken } from './Authenticator.fixtures';
+import { testAuthenticatePassword } from './testAuthenticatePassword';
+import { testAuthenticateOneTimeLogin } from './testAuthenticateOneTimeLogin';
+import { testAuthenticateRefreshToken } from './testAuthenticateRefreshToken';
+
+jest.mock('rand-token');
+randomToken.generate.mockReturnValue(refreshToken);
+
+describe('Authenticator', () => {
+  describe('authenticatePassword', testAuthenticatePassword);
+  describe('authenticateRefreshToken', testAuthenticateRefreshToken);
+  describe('authenticateOneTimeLogin', testAuthenticateOneTimeLogin);
+});
