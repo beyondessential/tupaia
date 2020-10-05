0eccae9a444e580bd5c2fa6f3945169494401963
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-05 15:11:38 +1100

    Extract functionality in functions

--- packages/devops/scripts/ci/validateBranchName.sh
+++ packages/devops/scripts/ci/validateBranchName.sh
@@ -7,37 +7,42 @@ MAX_LENGTH=56
 RESERVED_CHARS=('/' '\' '.' '&' '?')
 RESERVED_ENDINGS=(admin aggregation api config export mobile tonga-aggregation www)
 
-branch_name="$CI_BRANCH"
-if [[ $branch_name == "" ]]; then
-    # Get currently checked out branch
-    branch_name=`git rev-parse --abbrev-ref HEAD`
-fi
+function get_branch_name() {
+    local branch_name="$CI_BRANCH"
+    if [[ $branch_name == "" ]]; then
+        # Get currently checked out branch
+        branch_name=`git rev-parse --abbrev-ref HEAD`
+    fi
 
-# Validate name length
-name_length=${#branch_name}
-if [[ $name_length -gt MAX_LENGTH ]]; then
-    log_error "❌ Branch name is too long, must be $MAX_LENGTH characters max"
-    exit 1;
-fi
+    echo $branch_name
+}
 
-# Validate characters in name
-for character in ${RESERVED_CHARS[@]}
-do
-    if [[ "$branch_name" == *"$character"* ]]; then
-        log_error "❌ Invalid character in branch name: '$character'"
+function validate_name_length() {
+    local branch_name=$1
+    local name_length=${#branch_name}
+
+    if [[ $name_length -gt MAX_LENGTH ]]; then
+        log_error "❌ Branch name is too long, must be $MAX_LENGTH characters max"
         exit 1;
     fi
-done
+}
+
+function validate_name_ending() {
+    local branch_name=$1
+
+    for reserved_ending in ${RESERVED_ENDINGS[@]}
+    do
+        if [[ "$branch_name" == *$reserved_ending ]]; then
+            log_error "❌ Invalid branch name ending: '$reserved_ending'"
+            exit 1;
+        fi
+    done
+}
 
 
-# Validate name ending
-for reserved_ending in ${RESERVED_ENDINGS[@]}
-do
-    if [[ "$branch_name" == *$reserved_ending ]]; then
-        log_error "❌ Invalid branch name ending: '$reserved_ending'"
-        exit 1;
-    fi
-done
+branch_name=$(get_branch_name)
+validate_name_length $branch_name
+validate_name_ending $branch_name
 
 log_success "✔ Branch name is valid!"
 exit 0;
