2f0fd1d1231104d42dca8861b839b34cf8f4a922
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-05 11:23:06 +1100

    Lint files

--- packages/auth/src/__tests__/buildAccessPolicy/buildAccessPolicy.test.js
+++ packages/auth/src/__tests__/buildAccessPolicy/buildAccessPolicy.test.js
@@ -3,7 +3,6 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { buildAccessPolicy } from '../../buildAccessPolicy';
 import { Demo } from './setup';
 import { testAccessPolicyHandler } from './testAccessPolicyHandler';
 import { testAsPublicUser } from './testAsPublicUser';
