57e1f52a552e0231cd366c6fe636b07d42a7ae56
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2020-10-05 15:20:13 +1100

    Reinstate invalid char validation

--- packages/devops/scripts/ci/validateBranchName.sh
+++ packages/devops/scripts/ci/validateBranchName.sh
@@ -4,7 +4,7 @@ DIR=`dirname "$0"`
 . ${DIR}/utils.sh
 
 MAX_LENGTH=56
-RESERVED_CHARS=('/' '\' '.' '&' '?')
+INVALID_CHARS=('/' '\' '.' '&' '?')
 RESERVED_ENDINGS=(admin aggregation api config export mobile tonga-aggregation www)
 
 function get_branch_name() {
@@ -39,19 +39,27 @@ function validate_name_length() {
     fi
 }
 
-function validate_name_case() {
+function validate_name_chars() {
     local branch_name=$1
 
     if [[ $branch_name =~ [A-Z] ]]; then
         log_error "❌ Branch name cannot contain uppercase characters"
         exit 1;
     fi
+
+    for character in ${INVALID_CHARS[@]}	
+    do	
+        if [[ "$branch_name" == *"$character"* ]]; then	
+            log_error "❌ Invalid character in branch name: '$character'"	
+            exit 1;	
+        fi
+    done
 }
 
 branch_name=$(get_branch_name)
 validate_name_ending $branch_name
 validate_name_length $branch_name
-validate_name_case $branch_name
+validate_name_chars $branch_name
 
 log_success "✔ Branch name is valid!"
 exit 0;
