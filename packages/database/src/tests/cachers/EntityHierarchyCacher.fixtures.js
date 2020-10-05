d96f5b2232ff1e129eac0b3a5ac5f376974749a7
Edwin <edmofro@gmail.com>
2020-10-05 16:45:05 +1300

    Add tests for deleting entities and entity relations

--- packages/database/src/tests/cachers/EntityHierarchyCacher.fixtures.js
+++ packages/database/src/tests/cachers/EntityHierarchyCacher.fixtures.js
@@ -121,3 +121,67 @@ export const INITIAL_HIERARCHY_B = [
   // ancestor entity abb
   { ancestor_id: 'entity_abb_test', descendant_id: 'entity_aab_test', generational_distance: 1 },
 ];
+
+export const HIERARCHY_B_AFTER_ENTITY_AAA_DELETED = [
+  // ancestor entity a
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aa_test', generational_distance: 1 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_ab_test', generational_distance: 2 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aba_test', generational_distance: 3 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_abb_test', generational_distance: 3 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aab_test', generational_distance: 4 },
+  // ancestor entity aa
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_ab_test', generational_distance: 1 },
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aba_test', generational_distance: 2 },
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_abb_test', generational_distance: 2 },
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aab_test', generational_distance: 3 },
+  // ancestor entity ab
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_aba_test', generational_distance: 1 },
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_abb_test', generational_distance: 1 },
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_aab_test', generational_distance: 2 },
+  // ancestor entity abb
+  { ancestor_id: 'entity_abb_test', descendant_id: 'entity_aab_test', generational_distance: 1 },
+];
+
+export const HIERARCHY_B_AFTER_MULTIPLE_ENTITIES_DELETED = [
+  // ancestor entity a
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aa_test', generational_distance: 1 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aab_test', generational_distance: 2 },
+  // ancestor entity aa
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aab_test', generational_distance: 1 },
+];
+
+export const HIERARCHY_B_AFTER_RELATION_AA_AB_DELETED = [
+  // ancestor entity a
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aa_test', generational_distance: 1 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aaa_test', generational_distance: 2 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aab_test', generational_distance: 2 },
+  // ancestor entity aa
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aaa_test', generational_distance: 1 },
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aab_test', generational_distance: 1 },
+];
+
+// strangely enough, after deleting a -> aa, it means the canonical structure takes back over, but
+// with aaa and aab still _also_ attached to the end of aba and abb
+export const HIERARCHY_B_AFTER_MULTIPLE_RELATIONS_DELETED = [
+  // ancestor entity a
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aa_test', generational_distance: 1 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_ab_test', generational_distance: 1 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aaa_test', generational_distance: 2 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aab_test', generational_distance: 2 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aba_test', generational_distance: 2 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_abb_test', generational_distance: 2 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aaa_test', generational_distance: 3 },
+  { ancestor_id: 'entity_a_test', descendant_id: 'entity_aab_test', generational_distance: 3 },
+  // ancestor entity aa
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aaa_test', generational_distance: 1 },
+  { ancestor_id: 'entity_aa_test', descendant_id: 'entity_aab_test', generational_distance: 1 },
+  // ancestor entity ab
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_aba_test', generational_distance: 1 },
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_abb_test', generational_distance: 1 },
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_aaa_test', generational_distance: 2 },
+  { ancestor_id: 'entity_ab_test', descendant_id: 'entity_aab_test', generational_distance: 2 },
+  // ancestor entity aba
+  { ancestor_id: 'entity_aba_test', descendant_id: 'entity_aaa_test', generational_distance: 1 },
+  // ancestor entity abb
+  { ancestor_id: 'entity_abb_test', descendant_id: 'entity_aab_test', generational_distance: 1 },
+];
--- packages/database/src/tests/cachers/EntityHierarchyCacher.test.js
+++ packages/database/src/tests/cachers/EntityHierarchyCacher.test.js
@@ -13,6 +13,10 @@ import {
   TEST_DATA_TO_DEPOPULATE,
   INITIAL_HIERARCHY_A,
   INITIAL_HIERARCHY_B,
+  HIERARCHY_B_AFTER_ENTITY_AAA_DELETED,
+  HIERARCHY_B_AFTER_MULTIPLE_ENTITIES_DELETED,
+  HIERARCHY_B_AFTER_RELATION_AA_AB_DELETED,
+  HIERARCHY_B_AFTER_MULTIPLE_RELATIONS_DELETED,
 } from './EntityHierarchyCacher.fixtures';
 
 describe('EntityHierarchyCacher', () => {
@@ -54,5 +58,90 @@ describe('EntityHierarchyCacher', () => {
     it('handles a hierarchy that has entity relation links', async () => {
       await assertProjectRelationsCorrectlyBuilt('project_b_test', INITIAL_HIERARCHY_B);
     });
+  });
+
+  it('deletes all ancestor descendant relations if an entity is deleted', async () => {
+    const projectCode = 'project_b_test';
+    await buildAndCacheProject(projectCode);
+
+    // start listening for changes
+    hierarchyCacher.listenForChanges();
+
+    // delete an entity, and make sure its ancestor descendant relations are all deleted
+    const entityToDelete = 'entity_aaa_test';
+    await models.entityRelation.delete({ parent_id: entityToDelete });
+    await models.entityRelation.delete({ child_id: entityToDelete });
+    await models.entity.delete({ id: entityToDelete });
+    await models.database.waitForAllChangeHandlers();
+    await assertRelationsMatch(projectCode, HIERARCHY_B_AFTER_ENTITY_AAA_DELETED);
+  });
+
+  it('deletes all ancestor descendant relations if multiple entities are deleted', async () => {
+    const projectCode = 'project_b_test';
+    await buildAndCacheProject(projectCode);
+
+    // start listening for changes
+    hierarchyCacher.listenForChanges();
+
+    // delete a few entities, and make sure their relations are deleted
+    const entitiesToDelete = [
+      'entity_ab_test',
+      'entity_aaa_test',
+      'entity_aba_test',
+      'entity_abb_test',
+    ];
+    await models.entityRelation.delete({ parent_id: entitiesToDelete });
+    await models.entityRelation.delete({ child_id: entitiesToDelete });
+    await models.entity.delete({ id: entitiesToDelete });
+    await models.database.waitForAllChangeHandlers();
+    await assertRelationsMatch(projectCode, HIERARCHY_B_AFTER_MULTIPLE_ENTITIES_DELETED);
+  });
+
+  it.only('deletes all ancestor descendant relations if an entity relation record is deleted', async () => {
+    const projectCode = 'project_b_test';
+    await buildAndCacheProject(projectCode);
+
+    // start listening for changes
+    hierarchyCacher.listenForChanges();
+
+    // delete an entity relation, and make sure the relations in the cache are deleted
+    const entityRelationToDelete = {
+      parent_id: 'entity_aa_test',
+      child_id: 'entity_ab_test',
+      entity_hierarchy_id: 'hierarchy_b_test',
+    };
+    await models.entityRelation.delete(entityRelationToDelete);
+    await models.database.waitForAllChangeHandlers();
+    await assertRelationsMatch(projectCode, HIERARCHY_B_AFTER_RELATION_AA_AB_DELETED);
+  });
+
+  it('deletes all ancestor descendant relations if multiple entity relation records are deleted', async () => {
+    const projectCode = 'project_b_test';
+    await buildAndCacheProject(projectCode);
+
+    // start listening for changes
+    hierarchyCacher.listenForChanges();
+
+    // delete a few entity relations, and make sure the relations in the cache are deleted
+    // strangely enough, after deleting a -> aa, it means the canonical structure takes back over
+    const entityRelationsToDelete = [
+      {
+        parent_id: 'entity_aa_test',
+        child_id: 'entity_ab_test',
+        entity_hierarchy_id: 'hierarchy_b_test',
+      },
+      {
+        parent_id: 'entity_a_test',
+        child_id: 'entity_aa_test',
+        entity_hierarchy_id: 'hierarchy_b_test',
+      },
+    ];
+    await Promise.all(
+      entityRelationsToDelete.map(async entityRelation =>
+        models.entityRelation.delete(entityRelation),
+      ),
+    );
+    await models.database.waitForAllChangeHandlers();
+    await assertRelationsMatch(projectCode, HIERARCHY_B_AFTER_MULTIPLE_RELATIONS_DELETED);
   });
 });
