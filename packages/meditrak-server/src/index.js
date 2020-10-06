1fa2914502cb2fa0e66796d156f4e213fff00e1b
Edwin <edmofro@gmail.com>
2020-10-07 10:19:09 +1300

    Initial hierarchy build in migration

--- packages/meditrak-server/src/index.js
+++ packages/meditrak-server/src/index.js
@@ -19,65 +19,59 @@ import { createApp } from './app';
 
 import winston from './log';
 
-async function start() {
-  /**
-   * Set up database
-   */
-  const database = new TupaiaDatabase();
-  const models = new ModelRegistry(database, modelClasses);
+/**
+ * Set up database
+ */
+const database = new TupaiaDatabase();
+const models = new ModelRegistry(database, modelClasses);
 
-  /**
-   * Set up change handlers e.g. for syncing
-   */
-  createMeditrakSyncQueue(models);
+/**
+ * Set up change handlers e.g. for syncing
+ */
+createMeditrakSyncQueue(models);
 
-  // Pre-cache entity hierarchy details
-  const entityHierarchyCacher = new EntityHierarchyCacher(models);
-  winston.info('Building entity hierarchies. This can be deleted after deployment.');
-  await entityHierarchyCacher.buildAndCacheHierarchies();
-  entityHierarchyCacher.listenForChanges();
+// Pre-cache entity hierarchy details
+const entityHierarchyCacher = new EntityHierarchyCacher(models);
+entityHierarchyCacher.listenForChanges();
 
-  /**
-   * Set up actual app with routes etc.
-   */
-  const app = createApp(database, models);
+/**
+ * Set up actual app with routes etc.
+ */
+const app = createApp(database, models);
 
-  /**
-   * Start the server
-   */
-  const port = 8090;
-  http.createServer(app).listen(port);
-  winston.info(`Running on port ${port}`);
+/**
+ * Start the server
+ */
+const port = 8090;
+http.createServer(app).listen(port);
+winston.info(`Running on port ${port}`);
 
-  /**
-   * Regularly sync data to the aggregation servers
-   */
-  startSyncWithDhis(models);
+/**
+ * Regularly sync data to the aggregation servers
+ */
+startSyncWithDhis(models);
 
-  /**
-   * Regularly sync data to MS1
-   */
-  startSyncWithMs1(models);
+/**
+ * Regularly sync data to MS1
+ */
+startSyncWithMs1(models);
 
-  /**
-   * Regularly sync actions that have happened on meditrak with the social feed.
-   */
-  startFeedScraper(models);
+/**
+ * Regularly sync actions that have happened on meditrak with the social feed.
+ */
+startFeedScraper(models);
 
-  /**
-   * Notify PM2 that we are ready
-   */
-  if (process.send) {
-    (async () => {
-      try {
-        await database.waitForChangeChannel();
-        winston.info('Successfully connected to pubsub service');
-        process.send('ready');
-      } catch (error) {
-        winston.error(error.message);
-      }
-    })();
-  }
+/**
+ * Notify PM2 that we are ready
+ */
+if (process.send) {
+  (async () => {
+    try {
+      await database.waitForChangeChannel();
+      winston.info('Successfully connected to pubsub service');
+      process.send('ready');
+    } catch (error) {
+      winston.error(error.message);
+    }
+  })();
 }
-
-start();
