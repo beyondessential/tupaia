2b84028e1f1cb984a776a21ba68b53bd064f4979
Alastair <lockealastair@gmail.com>
2020-10-07 10:30:09 +1100

    Fix retry fetch location meditrak app (#1390)
    * Fix retry fetch location meditrak app
    
    * Clean up comments
    
    * PR review changes
    
    * Re-add getCurrentPosition
    
    Co-authored-by: sepidehdd <66226595+sepidehdd@users.noreply.github.com>

--- packages/meditrak-app/app/utilities/userLocation/index.js
+++ packages/meditrak-app/app/utilities/userLocation/index.js
@@ -3,6 +3,10 @@
  * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
  **/
 
-export { watchUserLocation, stopWatchingUserLocation } from './actions';
+export {
+  watchUserLocation,
+  refreshWatchingUserLocation,
+  stopWatchingUserLocation,
+} from './actions';
 export { reducer } from './reducer';
 export { getCurrentUserLocation } from './selectors';
