922fc4c9e2160b74914f9f84f3ced511f0de3b0a
billli0 <lbit123@outlook.com>
2020-10-06 16:26:18 +1100

    Separate test data into two blocks

--- packages/aggregator/src/__tests__/analytics/filterAnalytics.test.js
+++ packages/aggregator/src/__tests__/analytics/filterAnalytics.test.js
@@ -20,91 +20,91 @@ describe('filterAnalytics()', () => {
   });
 
   describe('filter type', () => {
-    const testData = [
-      [
-        'single type',
+    describe('single type', () => {
+      const testData = [
         [
+          '=',
           [
-            '=',
-            [
-              [{ value: 2 }, [{ value: 2 }]],
-              [{ value: 4 }, []],
-              [{ value: { '=': 2 } }, [{ value: 2 }]],
-              [{ value: { '=': 2 } }, filterAnalytics(analytics, { value: 2 })],
-            ],
+            [{ value: 2 }, [{ value: 2 }]],
+            [{ value: 4 }, []],
+            [{ value: { '=': 2 } }, [{ value: 2 }]],
+            [{ value: { '=': 2 } }, filterAnalytics(analytics, { value: 2 })],
           ],
+        ],
+        [
+          '>',
           [
-            '>',
-            [
-              [{ value: { '>': 2 } }, [{ value: 3 }]],
-              [{ value: { '>': 4 } }, []],
-            ],
+            [{ value: { '>': 2 } }, [{ value: 3 }]],
+            [{ value: { '>': 4 } }, []],
           ],
+        ],
+        [
+          '>=',
           [
-            '>=',
-            [
-              [{ value: { '>=': 2 } }, [{ value: 2 }, { value: 3 }]],
-              [{ value: { '>=': 4 } }, []],
-            ],
+            [{ value: { '>=': 2 } }, [{ value: 2 }, { value: 3 }]],
+            [{ value: { '>=': 4 } }, []],
           ],
+        ],
+        [
+          '<',
           [
-            '<',
-            [
-              [{ value: { '>=': 2 } }, [{ value: 2 }, { value: 3 }]],
-              [{ value: { '>=': 4 } }, []],
-            ],
+            [{ value: { '>=': 2 } }, [{ value: 2 }, { value: 3 }]],
+            [{ value: { '>=': 4 } }, []],
           ],
+        ],
+        [
+          '<=',
           [
-            '<=',
+            [{ value: { '<=': 2 } }, [{ value: 1 }, { value: 2 }]],
+            [{ value: { '<=': 0 } }, []],
+          ],
+        ],
+      ];
+
+      it.each(testData)('%s', (_, testCaseData) => {
+        testCaseData.forEach(([filter, expected]) => {
+          expect(filterAnalytics(analytics, filter)).toStrictEqual(expected);
+        });
+      });
+    });
+
+    describe('combines multiple types using AND logic', () => {
+      const testData = [
+        [
+          'one type superset of the other',
+          [
+            [{ value: { '<=': 2, '=': 2 } }, [{ value: 2 }]],
+            [{ value: { '=': 2, '<=': 2 } }, [{ value: 2 }]],
+          ],
+        ],
+        [
+          'mutually exclusive types',
+          [
+            [{ value: { '=': 2, '<': 2 } }, []],
+            [{ value: { '<': 2, '=': 2 } }, []],
+          ],
+        ],
+        [
+          'known and unknown types',
+          [
+            [{ value: { '=': 2, unknownFilter: 3 } }, [{ value: 2 }]],
+            [{ value: { '=': 2, unknownFilter: 3 } }, [{ value: 2 }]],
+          ],
+        ],
+        [
+          'unknown types',
+          [
             [
-              [{ value: { '<=': 2 } }, [{ value: 1 }, { value: 2 }]],
-              [{ value: { '<=': 0 } }, []],
+              { value: { operator: 'unknownFilter', value: 0 } },
+              [{ value: 1 }, { value: 2 }, { value: 3 }],
             ],
           ],
         ],
-      ],
-      [
-        'combines multiple types using AND logic',
-        [
-          [
-            'one type superset of the other',
-            [
-              [{ value: { '<=': 2, '=': 2 } }, [{ value: 2 }]],
-              [{ value: { '=': 2, '<=': 2 } }, [{ value: 2 }]],
-            ],
-          ],
-          [
-            'mutually exclusive types',
-            [
-              [{ value: { '=': 2, '<': 2 } }, []],
-              [{ value: { '<': 2, '=': 2 } }, []],
-            ],
-          ],
-          [
-            'known and unknown types',
-            [
-              [{ value: { '=': 2, unknownFilter: 3 } }, [{ value: 2 }]],
-              [{ value: { '=': 2, unknownFilter: 3 } }, [{ value: 2 }]],
-            ],
-          ],
-          [
-            'unknown types',
-            [
-              [
-                { value: { operator: 'unknownFilter', value: 0 } },
-                [{ value: 1 }, { value: 2 }, { value: 3 }],
-              ],
-            ],
-          ],
-        ],
-      ],
-    ];
-    testData.forEach(([name, testSuites]) => {
-      describe(name, () => {
-        it.each(testSuites)('%s', (_, testCaseData) => {
-          testCaseData.forEach(([filter, expected]) => {
-            expect(filterAnalytics(analytics, filter)).toStrictEqual(expected);
-          });
+      ];
+
+      it.each(testData)('%s', (_, testCaseData) => {
+        testCaseData.forEach(([filter, expected]) => {
+          expect(filterAnalytics(analytics, filter)).toStrictEqual(expected);
         });
       });
     });
