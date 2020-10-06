ef20efa401a20fe6545b86b30692807e8b32ea53
Tom Caiger <caigertom@gmail.com>
2020-10-06 14:50:22 +1300

    refactor migration

--- packages/database/src/migrations/20201002022840-AddProfileImageToUserAccount-modifies-schema.js
+++ packages/database/src/migrations/20201002022840-AddProfileImageToUserAccount-modifies-schema.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import { TupaiaDatabase } from '@tupaia/database';
-
 var dbm;
 var type;
 var seed;
@@ -16,11 +14,8 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = async function () {
-  const db = new TupaiaDatabase();
-  await db.executeSql(`ALTER TABLE user_account ADD COLUMN profile_image text`);
-  db.closeConnections();
-  return null;
+exports.up = async function (db) {
+  return db.runSql(`ALTER TABLE user_account ADD COLUMN profile_image text`);
 };
 
 exports.down = function () {
