103630bf8332c6209c082214d4d4a37bf0aadcb2
Igor Nadj <igor@beyondessential.com.au>
2020-10-07 17:13:20 +1100

    Make date picker stories dynamic

--- packages/web-frontend/stories/date-picker/date-picker.stories.js
+++ packages/web-frontend/stories/date-picker/date-picker.stories.js
@@ -4,86 +4,104 @@
  */
 
 import React from 'react';
+import styled from 'styled-components';
+import { State, Store } from '@sambego/storybook-state';
 import { DateRangePicker } from '../../src/components/DateRangePicker';
 import {
-  GRANULARITIES,
   GRANULARITIES_WITH_MULTIPLE_DATES,
   GRANULARITIES_WITH_ONE_DATE,
 } from '../../src/utils/periodGranularities';
 
+const Container = styled.div`
+  margin: 1rem auto;
+  width: 400px;
+  height: 200px;
+  background: rgba(255, 255, 255, 0.02);
+`;
+
 export default {
   title: 'FormControl/DateRangePicker',
   component: DateRangePicker,
+  decorators: [
+    Story => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+};
+
+const createDateRangePicker = () => {
+  // We use a store to allow the DateRangePicker to change it's own dates
+  const store = new Store({});
+  return args => (
+    <>
+      <State store={store}>
+        <DateRangePicker
+          {...args}
+          onSetDates={(startDate, endDate) => store.set({ startDate, endDate })}
+        />
+      </State>
+    </>
+  );
+};
+
+const multipleDatesDefaults = {
+  args: {
+    granularity: 'day',
+  },
+  argTypes: {
+    granularity: {
+      control: {
+        type: 'inline-radio',
+        options: GRANULARITIES_WITH_MULTIPLE_DATES,
+      },
+    },
+  },
+};
+
+const oneDateDefaults = {
+  args: {
+    granularity: 'one_day_at_a_time',
+  },
+  argTypes: {
+    granularity: {
+      control: {
+        type: 'inline-radio',
+        options: GRANULARITIES_WITH_ONE_DATE,
+      },
+    },
+  },
 };
 
 /*
  * Basic
  */
 
-export const MultipleDates = args => <DateRangePicker {...args} />;
+export const MultipleDates = createDateRangePicker();
+MultipleDates.args = multipleDatesDefaults.args;
+MultipleDates.argTypes = multipleDatesDefaults.argTypes;
 
-MultipleDates.args = {
-  granularity: 'day',
-};
-
-MultipleDates.argTypes = {
-  granularity: {
-    control: {
-      type: 'inline-radio',
-      options: GRANULARITIES_WITH_MULTIPLE_DATES,
-    },
-  },
-};
-
-export const OneDate = args => <DateRangePicker {...args} />;
-
-OneDate.args = {
-  granularity: 'one_day_at_a_time',
-};
-
-OneDate.argTypes = {
-  granularity: {
-    control: {
-      type: 'inline-radio',
-      options: GRANULARITIES_WITH_ONE_DATE,
-    },
-  },
-};
+export const OneDate = createDateRangePicker();
+OneDate.args = oneDateDefaults.args;
+OneDate.argTypes = oneDateDefaults.argTypes;
 
 /*
  * With defaults (initial dates)
  */
 
-export const MultipleDatesWithDefaults = args => <DateRangePicker {...args} />;
-
+export const MultipleDatesWithDefaults = createDateRangePicker();
 MultipleDatesWithDefaults.args = {
-  startDate: '2020-01-01',
-  endDate: '2020-01-05',
-  granularity: 'day',
+  ...multipleDatesDefaults.args,
+  startDate: '2019-01-01',
+  endDate: '2019-01-05',
 };
+MultipleDatesWithDefaults.argTypes = multipleDatesDefaults.argTypes;
 
-MultipleDatesWithDefaults.argTypes = {
-  granularity: {
-    control: {
-      type: 'inline-radio',
-      options: GRANULARITIES_WITH_MULTIPLE_DATES,
-    },
-  },
-};
-
-export const OneDateWithDefaults = args => <DateRangePicker {...args} />;
-
+export const OneDateWithDefaults = createDateRangePicker();
 OneDateWithDefaults.args = {
-  startDate: '2020-01-01',
-  endDate: '2020-01-05',
-  granularity: 'one_day_at_a_time',
+  ...oneDateDefaults.args,
+  startDate: '2019-01-01',
+  endDate: '2019-01-05',
 };
-
-OneDateWithDefaults.argTypes = {
-  granularity: {
-    control: {
-      type: 'inline-radio',
-      options: GRANULARITIES_WITH_ONE_DATE,
-    },
-  },
-};
+OneDateWithDefaults.argTypes = oneDateDefaults.argTypes;
