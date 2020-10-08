43c57cc31d06b79afed780dcdc4c4dc6b5f9caac
billli0 <lbit123@outlook.com>
2020-10-08 15:15:19 +1100

    Remove winston.config

--- packages/data-api/jest.setup.js
+++ packages/data-api/jest.setup.js
@@ -6,12 +6,6 @@
 import { clearTestData, getTestDatabase } from '@tupaia/database';
 import winston from 'winston';
 
-// TODO This is used to silence `winston` logs thrown by `@tupaia/utils.ObjectValidator`
-// Remove when https://github.com/beyondessential/tupaia-backlog/issues/1201 is addressed
-winston.configure({
-  transports: [new winston.transports.Console({ silent: true })],
-});
-
 afterAll(async () => {
   const database = getTestDatabase();
   await clearTestData(database);
