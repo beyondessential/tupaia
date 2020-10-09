7086fad9c384b22af276281f766928dca6428d9c
Ethan McQuarrie <ethan@beyondessential.com.au>
2020-10-09 17:21:03 +1300

    Update dashboard groups sql to account for project level permissions

--- packages/meditrak-server/src/routes/GETDashboardGroups/assertDashboardGroupsPermissions.js
+++ packages/meditrak-server/src/routes/GETDashboardGroups/assertDashboardGroupsPermissions.js
@@ -29,10 +29,25 @@ export const createDashboardGroupDBFilter = async (accessPolicy, models, criteri
     countryCodesByPermissionGroup[pg] = accessPolicy.getEntitiesAllowed(pg);
   });
 
-  // Look up the country codes from the json object and compare to the organisationUnitCode
+  // Look up the country codes from the json permissions object and compare to the organisationUnitCode
+  // OR
+  // For Projects: Pull the country codes from the children entities and compare to the json permissions object
   dbConditions[RAW] = {
-    sql: `array["organisationUnitCode"] <@ array(select trim('"' from json_array_elements(?::json->"userGroup")::text))`,
-    parameters: JSON.stringify(countryCodesByPermissionGroup),
+    sql: `(ARRAY["organisationUnitCode"] <@ ARRAY(SELECT TRIM('"' FROM JSON_ARRAY_ELEMENTS(?::JSON->"userGroup")::TEXT)))
+          OR
+          (ARRAY(SELECT entity.country_code
+                 FROM entity
+                 INNER JOIN entity_relation
+                       ON entity.id = entity_relation.child_id
+                 INNER JOIN project
+                       ON  entity_relation.parent_id = project.entity_id
+                       AND entity_relation.entity_hierarchy_id = project.entity_hierarchy_id
+                 WHERE project.code = "dashboardGroup"."organisationUnitCode")::TEXT[]
+        && ARRAY(SELECT TRIM('"' FROM JSON_ARRAY_ELEMENTS(?::JSON->"userGroup")::TEXT)))`,
+    parameters: [
+      JSON.stringify(countryCodesByPermissionGroup),
+      JSON.stringify(countryCodesByPermissionGroup),
+    ],
   };
   return dbConditions;
 };
