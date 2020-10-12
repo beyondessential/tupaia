ca8a7c76e45f4b6740b1010d9e778a2c776ee26b
nick <nickw@ourcomunity.com.au>
2020-10-13 03:08:39 +1100

    Add requested projects to getCountryAccessList

--- packages/meditrak-server/src/routes/getCountryAccessList.js
+++ packages/meditrak-server/src/routes/getCountryAccessList.js
@@ -2,7 +2,19 @@
  * Tupaia MediTrak
  * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
  */
-import { respond } from '@tupaia/utils';
+import { respond, reduceToDictionary } from '@tupaia/utils';
+
+const mapRequestsToEntities = (requestedEntities, projectCodeById) => {
+  const entityRequests = {};
+  requestedEntities.forEach(request => {
+    if (!(request.entity_id in entityRequests)) {
+      entityRequests[request.entity_id] = [];
+    }
+    const projectCode = request.project_id != null ? projectCodeById[request.project_id] : 'none';
+    entityRequests[request.entity_id].push(projectCode);
+  });
+  return entityRequests;
+};
 
 export async function getCountryAccessList(req, res, next) {
   const { userId, models } = req;
@@ -11,6 +23,13 @@ export async function getCountryAccessList(req, res, next) {
     const countries = await models.entity.find({ type: 'country' });
     const entityPermissions = await models.userEntityPermission.find({ user_id: userId });
     const permittedEntityIds = new Set(entityPermissions.map(p => p.entity_id));
+    const accessRequests = await models.accessRequest.find({
+      user_id: userId,
+      processed_date: null,
+    });
+    const projects = await models.project.find();
+    const projectCodeById = reduceToDictionary(projects, 'id', 'code');
+    const entityRequests = mapRequestsToEntities(accessRequests, projectCodeById);
 
     const countryAccessList = countries
       .filter(country => country.name !== 'No Country')
@@ -18,6 +37,7 @@ export async function getCountryAccessList(req, res, next) {
         id: country.id,
         name: country.name,
         hasAccess: permittedEntityIds.has(country.id),
+        accessRequests: entityRequests[country.id] || [],
       }))
       .sort((a, b) => a.name.localeCompare(b.name));
 
