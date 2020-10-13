a4fbfab27911d7cbf6a97aa0986b0e46b25e38b4
Tom Caiger <caigertom@gmail.com>
2020-10-14 10:06:31 +1300

    refactor profile image handling

--- packages/psss/src/views/Tabs/ProfileTabView.js
+++ packages/psss/src/views/Tabs/ProfileTabView.js
@@ -67,7 +67,7 @@ const ProfileTabViewComponent = React.memo(({ user, onUpdateProfile }) => {
         last_name: lastName,
         position: role,
         employer,
-        profileImage: profileImage.fileId && profileImage,
+        profile_image: profileImage.data !== user.profileImage && profileImage,
       });
       setStatus(STATUS.SUCCESS);
       setSuccessMessage('Profile successfully updated.');
@@ -92,7 +92,7 @@ const ProfileTabViewComponent = React.memo(({ user, onUpdateProfile }) => {
   const handleFileDelete = () => {
     // We need to set a file id to something so that the backend will pick up the profileImage field
     // as an updatedField and save the change.
-    setProfileImage({ fileId: '-', data: null });
+    setProfileImage({ fileId: null, data: null });
   };
 
   const { firstName, lastName, position, employer } = user;
