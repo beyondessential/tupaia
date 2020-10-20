b990f7d490fb11a5752f1f198632726e115486f9
Tom Caiger <caigertom@gmail.com>
2020-10-20 14:40:40 +1300

    add ga events

--- packages/web-frontend/src/utils/exports.js
+++ packages/web-frontend/src/utils/exports.js
@@ -5,11 +5,14 @@
 import downloadJs from 'downloadjs';
 import domtoimage from 'dom-to-image';
 import { download } from './request';
+import { gaEvent } from './ga';
 
 export const exportToPng = (node, filename) => {
   return new Promise(resolve => {
+    gaEvent('Export', 'png', 'Attempt');
     domtoimage.toPng(node).then(async dataUrl => {
       downloadJs(dataUrl, `${filename}.png`);
+      gaEvent('Export', 'png', 'Success');
       resolve();
     });
   });
@@ -18,6 +21,7 @@ export const exportToPng = (node, filename) => {
 export const exportToExcel = async ({ projectCode, viewContent, startDate, endDate, filename }) => {
   const { viewId, organisationUnitCode, dashboardGroupId } = viewContent;
   const dataElementHeader = viewContent?.exportConfig?.dataElementHeader;
+  gaEvent('Export', 'xlsx', 'Success');
 
   const queryString = new URLSearchParams({
     dashboardGroupId,
@@ -30,5 +34,6 @@ export const exportToExcel = async ({ projectCode, viewContent, startDate, endDa
   });
 
   await download(`export/chart?${queryString}`, null, null, `${filename}.xlsx`);
+  gaEvent('Export', 'xlsx', 'Success');
   return true;
 };
