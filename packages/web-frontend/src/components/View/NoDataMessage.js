07c9edc1e68bcd8135be6102ac7a46d4adff752b
Tom Caiger <caigertom@gmail.com>
2020-10-28 09:28:55 +1300

    re-style and refactor request project dialog

--- packages/web-frontend/src/components/View/NoDataMessage.js
+++ packages/web-frontend/src/components/View/NoDataMessage.js
@@ -9,6 +9,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Alert } from '../Alert';
+import { BLUE } from '../../styles';
 import { VIEW_CONTENT_SHAPE } from './propTypes';
 
 const StyledAlert = styled(Alert)`
@@ -18,7 +19,15 @@ const StyledAlert = styled(Alert)`
   }
 `;
 
-export const NoDataMessage = ({ viewContent }) => {
+export const PlainNoDataMessage = styled.p`
+  color: ${BLUE};
+  margin-top: 10px;
+  font-size: 16px;
+  font-weight: 500;
+  opacity: 0.8;
+`;
+
+export const NoDataMessage = ({ viewContent, severity, AlertComponent }) => {
   let message = 'No data';
 
   if (viewContent.noDataMessage) {
@@ -29,9 +38,16 @@ export const NoDataMessage = ({ viewContent }) => {
     message = `No data for ${viewContent.startDate} to ${viewContent.endDate}`;
   }
 
-  return <StyledAlert severity="info">{message}</StyledAlert>;
+  return <AlertComponent severity={severity}>{message}</AlertComponent>;
 };
 
 NoDataMessage.propTypes = {
   viewContent: PropTypes.shape(VIEW_CONTENT_SHAPE).isRequired,
+  severity: PropTypes.string,
+  AlertComponent: PropTypes.elementType,
+};
+
+NoDataMessage.defaultProps = {
+  AlertComponent: StyledAlert,
+  severity: 'info',
 };
