de564e74a400cc08c8b844a4cfb78eb2ec55fe8b
Tom Caiger <caigertom@gmail.com>
2020-10-28 12:30:41 +1300

    fix filenames

--- packages/web-frontend/src/utils/string.js
+++ packages/web-frontend/src/utils/string.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the AGPL-3.0 license
  * found in the LICENSE file in the root directory of this source tree.
  */
+import sanitize from 'sanitize-filename';
 
 /**
  *
@@ -15,6 +16,14 @@
 export const areStringsEqual = (a, b, caseSensitive = true) =>
   a.toString().localeCompare(b, undefined, caseSensitive ? {} : { sensitivity: 'accent' }) === 0;
 
+function truncate(str, num) {
+  if (str.length <= num) {
+    return str;
+  }
+  return str.slice(0, num);
+}
+
 export const stringToFilename = string => {
-  return string.replace(/[^a-z0-9]/gi, '-').toLowerCase();
+  const sanitized = sanitize(string).replace(/\s+/g, '-').toLowerCase().toLowerCase();
+  return truncate(sanitized, 255);
 };
