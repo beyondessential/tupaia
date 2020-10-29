48e6b34c700feac5f6dbc12dabe3b41b0dc5bfef
Rohan Port <rohan@beyondessential.com.au>
2020-10-29 13:26:22 +1100

    1278: Added 'report' table and model - Updated eslint config for reports package

---
+++ packages/database/src/migrations/20201028234713-AddReportsTable-modifies-schema.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.runSql(`
+    CREATE TABLE report (
+      id TEXT PRIMARY KEY,
+      code TEXT NOT NULL,
+      config JSONB NOT NULL,
+      permission_group_id TEXT NOT NULL
+    );
+  `);
+};
+
+exports.down = function (db) {
+  return db.runSql(`
+    DROP TABLE public.report;
+  `);
+};
+
+exports._meta = {
+  version: 1,
+};
