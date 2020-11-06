78cda32569022ebccbded51fecb4a1b9d1afa26f
Edwin <edmofro@gmail.com>
2020-11-06 15:26:34 +1300

    Migrate custom canonical types into the database

---
+++ packages/database/src/migrations/20201105014950-AddEntityTypesToHierarchy-modifies-schema.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.addColumn('entity_hierarchy', 'canonical_types', { type: 'text[]' });
+};
+
+exports.down = function (db) {
+  return db.removeColumn('entity_hierarchy', 'canonical_types');
+};
+
+exports._meta = {
+  version: 1,
+};
