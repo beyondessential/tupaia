ad39bf2f3f6b5f3c8d6d1e11fcadf5c7d4157636
Tom Caiger <caigertom@gmail.com>
2020-11-10 09:05:37 +1300

    remove hack for testing login form

--- packages/web-frontend/cypress/support/commands.js
+++ packages/web-frontend/cypress/support/commands.js
@@ -31,10 +31,6 @@ Cypress.Commands.add('login', () => {
   cy.visit('/');
   cy.wait('@getUser').then(({ response }) => {
     if (response.body.name === PUBLIC_USER) {
-      // TODO This is a temporary hack to make sure that the login form is stable
-      // Remove it when https://github.com/beyondessential/tupaia-backlog/issues/1358 is fixed
-      cy.wait('@projects');
-
       submitLoginForm();
       cy.wait('@getUser').then(() => {
         closeOverlay();
