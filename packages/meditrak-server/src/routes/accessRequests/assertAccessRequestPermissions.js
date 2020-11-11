36508a019c9287c131bbc927699a56344732f92e
EMcQ-BES <69437175+EMcQ-BES@users.noreply.github.com>
2020-11-12 09:19:48 +1300

    1132: Add unit tests for /accessRequests endpoint (#1589)
    * 1132 - Update access requests to restrict possible self elevation
    Add unit tests for access requests endpoints
    
    * PR commens
    * More descriptive test descriptions
    * Update variable names
    * Use consistent wording in test descriptions

--- packages/meditrak-server/src/routes/accessRequests/assertAccessRequestPermissions.js
+++ packages/meditrak-server/src/routes/accessRequests/assertAccessRequestPermissions.js
@@ -3,7 +3,11 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { hasBESAdminAccess, TUPAIA_ADMIN_PANEL_PERMISSION_GROUP } from '../../permissions';
+import {
+  hasBESAdminAccess,
+  BES_ADMIN_PERMISSION_GROUP,
+  TUPAIA_ADMIN_PANEL_PERMISSION_GROUP,
+} from '../../permissions';
 import { getAdminPanelAllowedEntityIds } from '../utilities';
 
 export const assertAccessRequestPermissions = async (accessPolicy, models, accessRequestId) => {
@@ -17,6 +21,50 @@ export const assertAccessRequestPermissions = async (accessPolicy, models, acces
     throw new Error('Need Admin Panel access to the country this access request is for');
   }
   return true;
+};
+
+export const assertAccessRequestEditPermissions = async (
+  accessPolicy,
+  models,
+  accessRequestId,
+  updatedFields,
+) => {
+  // Check we have basic permission to access the record we want to edit
+  await assertAccessRequestPermissions(accessPolicy, models, accessRequestId);
+  // Check we have permission for the change
+  await assertAccessRequestUpsertPermissions(accessPolicy, models, updatedFields);
+
+  // We can view access requests for BES admin access even if we don't have BES admin ourselves
+  // So this final check confirms we're not trying to approve a request for BES admin access
+  const accessRequest = await models.accessRequest.findById(accessRequestId);
+  const permissionGroup = await models.permissionGroup.findById(accessRequest.permission_group_id);
+  if (permissionGroup.name === BES_ADMIN_PERMISSION_GROUP) {
+    throw new Error('Need BES Admin access to make this change');
+  }
+
+  return true;
+};
+
+export const assertAccessRequestUpsertPermissions = async (
+  accessPolicy,
+  models,
+  { permission_group_id: permissionGroupId, entity_id: entityId },
+) => {
+  // Check we're not trying to change this access request to give someone:
+  // BES admin access
+  // Access to an entity we don't have admin panel access
+  if (permissionGroupId) {
+    const permissionGroup = await models.permissionGroup.findById(permissionGroupId);
+    if (permissionGroup.name === BES_ADMIN_PERMISSION_GROUP) {
+      throw new Error('Need BES Admin access to make this change');
+    }
+  }
+  if (entityId) {
+    const entity = await models.entity.findById(entityId);
+    if (!accessPolicy.allows(entity.country_code, TUPAIA_ADMIN_PANEL_PERMISSION_GROUP)) {
+      throw new Error('Need Admin Panel access to the updated entity');
+    }
+  }
 };
 
 export const createAccessRequestDBFilter = async (accessPolicy, models, criteria) => {
