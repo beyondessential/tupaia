4448d61e437c3c72394b4b25c90d988502341de7
chris-bes <63621318+chris-bes@users.noreply.github.com>
2020-11-12 22:15:35 +1100

    982: changed documentation

--- packages/meditrak-server/src/routes/utilities/translateExpression.js
+++ packages/meditrak-server/src/routes/utilities/translateExpression.js
@@ -4,9 +4,13 @@
  */
 
 /**
+ * Replace question code with question UUID in an expression.
+ * Also prefix UUIDs with '$' so that it can be used in mathjs later on.
+ *
+ * Eg:
  * (SchFF00Section1Total / SchFF00Section1TotalObtainable) * 100
  * will be converted to:
- * (5ed7558b61f76a6ba5003114 / 5ec5bf8961f76a18ad00d4f9) * 100
+ * ($5ed7558b61f76a6ba5003114 / $5ec5bf8961f76a18ad00d4f9) * 100
  * @param {*} models
  * @param {*} formula
  * @param {*} codes
