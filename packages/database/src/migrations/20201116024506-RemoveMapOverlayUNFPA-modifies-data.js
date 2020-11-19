084c68ae9d63daa68f2d2b639157e22bed9671ca
Pauline <pauline@beyondessential.com.au>
2020-11-19 17:29:51 +1100

    get id from object row

--- packages/database/src/migrations/20201116024506-RemoveMapOverlayUNFPA-modifies-data.js
+++ packages/database/src/migrations/20201116024506-RemoveMapOverlayUNFPA-modifies-data.js
@@ -17,30 +17,33 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = async function (db) {
-  const servicesProvidedId = await db.runSql(`
+  const { rows } = await db.runSql(`
     SELECT id FROM map_overlay_group WHERE code = 'Services_provided'
   `);
+  const [{ id: servicesProvidedId }] = rows;
 
   return db.runSql(`
     UPDATE "mapOverlay"
-    SET "projectCodes" = array_remove("projectCodes",'unfpa')
+      SET "projectCodes" = array_remove("projectCodes",'unfpa')
     FROM map_overlay_group_relation
     WHERE map_overlay_group_relation.map_overlay_group_id = '${servicesProvidedId}'
-    AND map_overlay_group_relation.child_id = "mapOverlay".id;
+      AND map_overlay_group_relation.child_id = "mapOverlay".id;
   `);
 };
 
 exports.down = async function (db) {
-  const servicesProvidedId = await db.runSql(`
+  const { rows } = await db.runSql(`
     SELECT id FROM map_overlay_group WHERE code = 'Services_provided'
   `);
 
+  const [{ id: servicesProvidedId }] = rows;
+
   return db.runSql(`
     UPDATE "mapOverlay"
-    SET "projectCodes" = array_prepend('unfpa',"projectCodes")
+      SET "projectCodes" = array_prepend('unfpa',"projectCodes")
     FROM map_overlay_group_relation
     WHERE map_overlay_group_relation.map_overlay_group_id = '${servicesProvidedId}'
-    AND map_overlay_group_relation.child_id = "mapOverlay".id;
+      AND map_overlay_group_relation.child_id = "mapOverlay".id;
   `);
 };
 
