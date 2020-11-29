cb3cd9ff0256a5b2cc0dba1568c9a1e161cfdecd
Tom Caiger <caigertom@gmail.com>
2020-11-30 09:17:26 +1300

    refactor mutations

--- packages/psss/src/api/mutations/useSaveCountryReport.js
+++
@@ -1,6 +0,0 @@
-import { FakeAPI } from '../singletons';
-
-export const useSaveCountryReport = params =>
-  useMutation(async data => FakeAPI.postdata(data), {
-    onSuccess: () => queryCache.invalidateQueries('country-weeks', params),
-  });
