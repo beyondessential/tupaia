0e6cb1b9a020db8402634a56df3a73ef2c8c8788
Edwin <edmofro@gmail.com>
2020-12-01 12:18:18 +1300

    Match mSupply MainApplication.java

--- packages/meditrak-app/android/app/src/main/java/com/tupaiameditrak/MainApplication.java
+++ packages/meditrak-app/android/app/src/main/java/com/tupaiameditrak/MainApplication.java
@@ -2,30 +2,14 @@ package com.tupaiameditrak;
 
 import android.app.Application;
 
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.geolocation.GeolocationPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.horcrux.svg.SvgPackage;
-import com.bugsnag.BugsnagReactNative;
-
-import com.microsoft.appcenter.reactnative.push.AppCenterReactNativePushPackage;
-import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
-import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
-import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.rnfs.RNFSPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import io.realm.react.RealmReactPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.microsoft.appcenter.push.Push;
-
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -38,26 +22,8 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new GeolocationPackage(),
-            new SvgPackage(),
-            BugsnagReactNative.getPackage(),
-
-
-            new AppCenterReactNativePackage(MainApplication.this),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
-            new AppCenterReactNativePushPackage(MainApplication.this),
-            new RNDeviceInfo(),
-            new RNFSPackage(),
-            new LinearGradientPackage(),
-            new RealmReactPackage(),
-            new ImagePickerPackage(),
-            new NetInfoPackage(),
-            new AsyncStoragePackage(),
-            new RNCWebViewPackage()
-      );
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      return packages;
     }
   };
 
@@ -69,6 +35,35 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, false);
+
+    // Disabling Flipper for now, can look into enabling it in
+    // initializeFlipper(this);
+  }
+
+      /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
