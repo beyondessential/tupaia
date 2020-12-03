34864ef553dab48a6ffe7243819e4f12ea7de7e4
Tom Caiger <caigertom@gmail.com>
2020-12-03 18:01:26 +1300

    handle date picker for weekly reports

--- packages/psss/src/store/weeklyReports.js
+++ packages/psss/src/store/weeklyReports.js
@@ -2,10 +2,12 @@
  * Tupaia
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
+import { getCurrentPeriod } from '@tupaia/utils';
 import { createReducer } from '../utils/createReducer';
 
 // actions
 const SET_ACTIVE_WEEK = 'SET_ACTIVE_WEEK';
+const SET_LATEST_VIEWABLE_WEEK = 'SET_LATEST_VIEWABLE_WEEK';
 const TOGGLE_PANEL = 'TOGGLE_PANEL';
 const VERIFY_SYNDROME = 'VERIFY_SYNDROME';
 const CLEAR_VERIFIED_STATUSES = 'CLEAR_VERIFIED_STATUSES';
@@ -18,14 +20,21 @@ export const openWeeklyReportsPanel = () => dispatch => {
 
 export const closeWeeklyReportsPanel = () => ({ type: TOGGLE_PANEL, panelIsOpen: false });
 
-export const setActiveWeek = id => ({ type: SET_ACTIVE_WEEK, id });
+export const setActiveWeek = activeWeek => ({ type: SET_ACTIVE_WEEK, activeWeek });
+
+export const setLatestViewableWeek = latestViewableWeek => ({
+  type: SET_LATEST_VIEWABLE_WEEK,
+  latestViewableWeek,
+});
 
 export const updateVerifiedStatus = id => ({ type: VERIFY_SYNDROME, id });
 
 // selectors
 export const checkWeeklyReportsPanelIsOpen = ({ weeklyReports }) => weeklyReports.panelIsOpen;
 
-export const getActiveWeekId = ({ weeklyReports }) => weeklyReports.id;
+export const getActiveWeek = ({ weeklyReports }) => weeklyReports.activeWeek;
+
+export const getLatestViewableWeek = ({ weeklyReports }) => weeklyReports.latestViewableWeek;
 
 export const getVerifiedStatus = ({ weeklyReports }, syndromeId) =>
   weeklyReports.verifiedStatuses.includes(syndromeId);
@@ -38,15 +47,22 @@ export const getUnVerifiedSyndromes = ({ weeklyReports }, alerts) =>
   );
 
 // reducer
+const defaultPeriod = getCurrentPeriod('WEEK');
+
 const defaultState = {
-  id: null, // Todo: update to be the latest week by default??
+  // latestViewableWeek: '2020W13',
+  latestViewableWeek: defaultPeriod,
+  activeWeek: null,
   panelIsOpen: false,
   verifiedStatuses: [],
 };
 
 const actionHandlers = {
-  [SET_ACTIVE_WEEK]: ({ id }) => ({
-    id,
+  [SET_ACTIVE_WEEK]: ({ activeWeek }) => ({
+    activeWeek,
+  }),
+  [SET_LATEST_VIEWABLE_WEEK]: ({ latestViewableWeek }) => ({
+    latestViewableWeek,
   }),
   [TOGGLE_PANEL]: ({ panelIsOpen }) => ({
     panelIsOpen,
