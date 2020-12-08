00d8f21a22bde00700c7ae887fdb239888f8f873
Edwin <edmofro@gmail.com>
2020-12-08 17:57:16 +1300

    Clear nav state on version update

--- packages/meditrak-app/app/navigation/reduxIntegration.js
+++ packages/meditrak-app/app/navigation/reduxIntegration.js
@@ -7,10 +7,22 @@ import {
   createNavigationReducer,
   createReduxContainer,
 } from 'react-navigation-redux-helpers';
+import { REHYDRATE } from 'redux-persist';
 import { connect } from 'react-redux';
 import { Navigator } from './Navigator';
+import { hasVersionUpdated } from '../version';
 
-export const navReducer = createNavigationReducer(Navigator);
+const reducer = createNavigationReducer(Navigator);
+export const navReducer = (state, action) => {
+  const { type, payload } = action;
+  if (type === REHYDRATE && payload) {
+    const versionDidUpdate = payload.version && hasVersionUpdated(payload.version.currentVersion);
+    if (versionDidUpdate) {
+      return reducer(null, action);
+    }
+  }
+  return reducer(state, action);
+};
 
 const NavigationReduxContainer = createReduxContainer(Navigator);
 const mapStateToProps = state => ({
