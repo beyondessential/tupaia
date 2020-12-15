f6319652d2d5fbda5fa90e2718e68f0ecb087c09
nick <nickw@ourcomunity.com.au>
2020-12-15 18:42:31 +1100

    Update migration to include facility and village Add Geodata import Add Facility json Add village  json

--- packages/database/src/migrations/20201208103439-AddPalauEntities-modifies-data.js
+++ packages/database/src/migrations/20201208103439-AddPalauEntities-modifies-data.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import { codeToId, insertObject } from '../utilities/migration';
-import { arrayToDbString, generateId } from '../utilities';
+import { generateId } from '../utilities';
 
 import palauGeoData from './migrationData/20201208103439-AddPalauEntities-modifies-data/Palau.json';
 import palauIslandGroupsGeoData from './migrationData/20201208103439-AddPalauEntities-modifies-data/Palau_Island_Groups.json';
+import palauFacilities from './migrationData/20201208103439-AddPalauEntities-modifies-data/Palau_facilities.json';
 import palauStatesGeoData from './migrationData/20201208103439-AddPalauEntities-modifies-data/Palau_States.json';
+import palauVillages from './migrationData/20201208103439-AddPalauEntities-modifies-data/Palau_villages.json';
 
 var dbm;
 var type;
@@ -33,8 +35,8 @@ const extractEntitiesFromGeoData = geoData =>
     geometry: feature.geometry,
   }));
 
-const insertEntity = async (db, entity) =>
-  insertObject(db, 'entity', {
+const insertEntity = async (db, entity) => {
+  await insertObject(db, 'entity', {
     id: entity.id,
     code: entity.code,
     parent_id: entity.parent_id,
@@ -43,12 +45,45 @@ const insertEntity = async (db, entity) =>
     country_code: entity.country_code,
   });
 
+  if (entity.point) await updatePoint(db, entity);
+
+  if (entity.region) await updateRegion(db, entity);
+};
+
+const updatePoint = (db, entity) => {
+  return db.runSql(`
+    update entity 
+    set point = ST_Force2D(ST_GeomFromGeoJSON('${entity.point}')),
+    bounds = ST_Expand(ST_Envelope(ST_GeomFromGeoJSON('${entity.point}')::geometry), 1)
+    where id = '${entity.id}';
+  `);
+};
+
+const updateRegion = (db, entity) => {
+  const regionFunction =
+    entity.region.type === 'MultiPolygon'
+      ? `ST_GeomFromGeoJSON('${entity.region}')`
+      : `ST_Multi(ST_GeomFromGeoJSON('${entity.region}'))`;
+
+  return db.runSql(`
+    update entity 
+    set region = ${regionFunction},
+    bounds = ST_Envelope(ST_GeomFromGeoJSON('${entity.region}')::geometry)
+    where id = '${entity.id}';
+  `);
+};
+
 exports.up = async function (db) {
-  // console.log('palauGeoData', palauGeoData);
-  // console.log('palauIslandGroupsGeoData', palauIslandGroupsGeoData);
+  // Palau already exists, update geoData
+  const countryRegion = JSON.stringify(palauGeoData.features[0].geometry);
+  await db.runSql(`
+    update entity 
+    set region = ST_GeomFromGeoJSON('${countryRegion}'),
+    bounds = ST_Envelope(ST_GeomFromGeoJSON('${countryRegion}')::geometry)
+    where code = '${countryCode}'
+  `);
 
   const palauIslandGroups = extractEntitiesFromGeoData(palauIslandGroupsGeoData);
-  // console.log('palauIslandGroups', palauIslandGroups);
   const mappedPalauIslandGroups = await Promise.all(
     palauIslandGroups.map(async entity => {
       const parentId = await codeToId(db, 'entity', entity.parent_code);
@@ -56,21 +91,32 @@ exports.up = async function (db) {
         ...entity,
         id: generateId(),
         parent_id: parentId,
+        region: entity.geometry ? JSON.stringify(entity.geometry) : null,
       };
     }),
   );
+  await Promise.all(mappedPalauIslandGroups.map(entity => insertEntity(db, entity)));
 
-  // console.log('mappedPalauIslandGroups', mappedPalauIslandGroups);
-
-  const results = await Promise.all(
-    mappedPalauIslandGroups.map(entity => insertEntity(db, entity)),
+  const mappedPalauFacilities = await Promise.all(
+    palauFacilities.map(async entity => {
+      const parentId = await codeToId(db, 'entity', entity.parent_code);
+      const point = entity.Latitude
+        ? { type: 'Point', coordinates: [entity.Longitude, entity.Latitude] }
+        : null;
+      return {
+        id: generateId(),
+        parent_id: parentId,
+        code: entity.code,
+        name: entity.name,
+        type: 'facility',
+        point: point ? JSON.stringify(point) : null,
+        country_code: countryCode,
+      };
+    }),
   );
-  console.log('results', results);
+  await Promise.all(mappedPalauFacilities.map(entity => insertEntity(db, entity)));
 
-  // console.log('palauStatesGeoData', palauStatesGeoData);
   const palauStates = extractEntitiesFromGeoData(palauStatesGeoData);
-  // console.log('palauStates', palauStates);
-
   const mappedPalauStates = await Promise.all(
     palauStates.map(async entity => {
       const parentId = await codeToId(db, 'entity', entity.parent_code);
@@ -78,23 +124,42 @@ exports.up = async function (db) {
         ...entity,
         id: generateId(),
         parent_id: parentId,
+        region: entity.geometry ? JSON.stringify(entity.geometry) : null,
       };
     }),
   );
-
-  console.log('mappedPalauStates', mappedPalauStates);
-
   await Promise.all(
     mappedPalauStates.map(async entity => {
       return insertEntity(db, entity);
     }),
   );
 
+  const mappedPalauVillages = await Promise.all(
+    palauVillages.map(async entity => {
+      const parentCode = entity[0];
+      const parentId = await codeToId(db, 'entity', parentCode);
+      return {
+        id: generateId(),
+        parent_id: parentId,
+        code: entity[1],
+        name: entity[2],
+        type: 'village',
+        country_code: countryCode,
+      };
+    }),
+  );
+  await Promise.all(mappedPalauVillages.map(entity => insertEntity(db, entity)));
+
   return null;
 };
 
 exports.down = function (db) {
-  return null;
+  return db.runSql(`
+    delete from entity where type = 'village' and country_code = 'PW';
+    delete from entity where type = 'sub_district' and country_code = 'PW';
+    delete from entity where type = 'facility' and country_code = 'PW';
+    delete from entity where type = 'district' and country_code = 'PW';
+  `);
 };
 
 exports._meta = {
