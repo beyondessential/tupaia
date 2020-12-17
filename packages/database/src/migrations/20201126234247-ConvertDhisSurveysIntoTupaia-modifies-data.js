1ecd7683622661cd6b0c3dc1b94f51cf0124eb1c
billli0 <lbit123@outlook.com>
2020-12-17 15:24:37 +1100

    Add migration to update databuilder config for broken reports

--- packages/database/src/migrations/20201126234247-ConvertDhisSurveysIntoTupaia-modifies-data.js
+++ packages/database/src/migrations/20201126234247-ConvertDhisSurveysIntoTupaia-modifies-data.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { arrayToDbString, generateId, insertObject, updateValues } from '../utilities';
+import { getDashboardReportById, updateBuilderConfigByReportId } from '../utilities/migration';
 
 var dbm;
 var type;
@@ -21,6 +22,27 @@ const surveyCodes = ['RHC', 'RHFSC', 'RHFSCTS'];
 
 const switchServiceType = async (db, code, serviceType) => {
   await updateValues(db, 'data_source', { service_type: serviceType }, { code });
+};
+
+const dashboardReportsChangeForValueOfInterestAndEntityAggregation = [
+  'UNFPA_RH_Stock_Cards',
+  'UNFPA_Monthly_3_Methods_of_Contraception',
+  'UNFPA_Monthly_3_Methods_of_Contraception_Regional',
+  'UNFPA_Monthly_5_Methods_of_Contraception',
+  'UNFPA_Monthly_5_Methods_of_Contraception_Regional',
+  'UNFPA_Facilities_Offering_Delivery',
+  'UNFPA_Facilities_Offering_Services',
+  'UNFPA_Delivery_Services_Stock',
+];
+const dashboardReportsChangeForEntityAggregationOnly = [
+  'UNFPA_Percentage_Of_Facilities_At_Least_1_Staff_Trained_SRH_Services',
+];
+const dashboardReportsChangeForValueFromOneToYes = 'UNFPA_Delivery_Services_Stock';
+
+const newElementInDataBuilderConfig = {
+  entityAggregation: {
+    dataSourceEntityType: 'facility',
+  },
 };
 
 const getDataSources = async (db, codes) => {
@@ -45,6 +67,32 @@ exports.up = async function (db) {
   for (const code of surveyCodes) {
     await switchServiceType(db, code, serviceType);
   }
+
+  // Modify config mapping dhis to tupaia 1 => Yes, 0 => No, from <"valueOfInterest": 1> to <valueOfInterest": "Yes">
+  dashboardReportsChangeForValueOfInterestAndEntityAggregation.forEach(async id => {
+    await db.runSql(`
+    update "dashboardReport" dr 
+    set "dataBuilderConfig" = regexp_replace(dr."dataBuilderConfig"::text, '\\"valueOfInterest\\"\\: 1','"valueOfInterest": "Yes"','g')::jsonb 
+    where id = '${id}'
+  `);
+  });
+
+  // Modify config mapping dhis to tupaia 1 => Yes, 0 => No, from <"value": 1> to <value": "Yes">
+  await db.runSql(`
+    update "dashboardReport" dr 
+    set "dataBuilderConfig" = regexp_replace(dr."dataBuilderConfig"::text, '\\"value\\"\\: 1','"value": "Yes"','g')::jsonb 
+    where id = '${dashboardReportsChangeForValueFromOneToYes}'
+  `);
+
+  [
+    ...dashboardReportsChangeForEntityAggregationOnly,
+    ...dashboardReportsChangeForValueOfInterestAndEntityAggregation,
+  ].forEach(async id => {
+    const dashboardReport = await getDashboardReportById(db, id);
+    const { dataBuilderConfig } = dashboardReport;
+    const newDataBuilderConfig = { ...dataBuilderConfig, ...newElementInDataBuilderConfig };
+    await updateBuilderConfigByReportId(db, newDataBuilderConfig, id);
+  });
 };
 
 exports.down = async function (db) {
@@ -58,6 +106,32 @@ exports.down = async function (db) {
   for (const code of surveyCodes) {
     await switchServiceType(db, code, serviceType);
   }
+
+  // Modify config mapping dhis to tupaia 1 => Yes, 0 => No, from <"valueOfInterest": "Yes"> to <valueOfInterest": 1>
+  dashboardReportsChangeForValueOfInterestAndEntityAggregation.forEach(async id => {
+    await db.runSql(`
+    update "dashboardReport" dr 
+    set "dataBuilderConfig" = regexp_replace(dr."dataBuilderConfig"::text, '\\"valueOfInterest\\"\\: \\"Yes\\"','"valueOfInterest": 1','g')::jsonb 
+    where id = '${id}'
+  `);
+  });
+
+  // Modify config mapping dhis to tupaia 1 => Yes, 0 => No, from <"value": 1> to <value": "Yes">
+  await db.runSql(`
+  update "dashboardReport" dr 
+  set "dataBuilderConfig" = regexp_replace(dr."dataBuilderConfig"::text, '\\"value\\"\\: \\"Yes\\"','"value": 1','g')::jsonb 
+  where id = '${dashboardReportsChangeForValueFromOneToYes}'
+`);
+
+  [
+    ...dashboardReportsChangeForEntityAggregationOnly,
+    ...dashboardReportsChangeForValueOfInterestAndEntityAggregation,
+  ].forEach(async id => {
+    const dashboardReport = await getDashboardReportById(db, id);
+    const { dataBuilderConfig } = dashboardReport;
+    Object.keys(newElementInDataBuilderConfig).forEach(key => delete dataBuilderConfig[key]);
+    await updateBuilderConfigByReportId(db, dataBuilderConfig, id);
+  });
 };
 
 exports._meta = {
