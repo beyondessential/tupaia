5abf8f77efebe60d32e5d9ee8aa2168da65c87b1
nick <nickw@ourcomunity.com.au>
2020-12-18 11:22:18 +1100

    camelCase variables

--- packages/database/src/migrations/20201201082830-AddProjectPalauOlangch-modifies-data.js
+++ packages/database/src/migrations/20201201082830-AddProjectPalauOlangch-modifies-data.js
@@ -16,13 +16,13 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const COUNTRY_CODE = 'PW';
-const PROJECT_CODE = 'olangch_palau';
-const PROJECT_NAME = 'MoH Project Olangch';
-const DASHBOARDGROUP_NAME = 'General';
-const DASHBOARDGROUP_CODE = 'OLANGCH_PALAU_Project';
-const DASHBOARDGROUP_COUNTRY_CODE = 'PW_Olangch_Country';
-const USER_GROUP = 'Donor';
+const countryCode = 'PW';
+const projectCode = 'olangch_palau';
+const projectName = 'MoH Project Olangch';
+const dashboardGroupName = 'General';
+const dashboardGroupCode = 'OLANGCH_PALAU_Project';
+const dashboardGroupCountryCode = 'PW_Olangch_Country';
+const userGroup = 'Donor';
 
 export const hierarchyNameToId = async (db, name) => {
   const record = await db.runSql(`SELECT id FROM entity_hierarchy WHERE name = '${name}'`);
@@ -32,66 +32,66 @@ export const hierarchyNameToId = async (db, name) => {
 exports.up = async function (db) {
   await insertObject(db, 'dashboardGroup', {
     organisationLevel: 'Project',
-    userGroup: USER_GROUP,
-    organisationUnitCode: PROJECT_CODE,
+    userGroup,
+    organisationUnitCode: projectCode,
     dashboardReports: '{project_details}',
-    name: DASHBOARDGROUP_NAME,
-    code: DASHBOARDGROUP_CODE,
-    projectCodes: `{${PROJECT_CODE}}`,
+    name: dashboardGroupName,
+    code: dashboardGroupCode,
+    projectCodes: `{${projectCode}}`,
   });
   await insertObject(db, 'dashboardGroup', {
     organisationLevel: 'Country',
-    userGroup: USER_GROUP,
-    organisationUnitCode: COUNTRY_CODE,
+    userGroup,
+    organisationUnitCode: countryCode,
     dashboardReports: '{project_details}',
-    name: DASHBOARDGROUP_NAME,
-    code: DASHBOARDGROUP_COUNTRY_CODE,
-    projectCodes: `{${PROJECT_CODE}}`,
+    name: dashboardGroupName,
+    code: dashboardGroupCountryCode,
+    projectCodes: `{${projectCode}}`,
   });
   await insertObject(db, 'entity', {
     id: generateId(),
-    code: PROJECT_CODE,
+    code: projectCode,
     parent_id: await codeToId(db, 'entity', 'World'),
-    name: PROJECT_NAME,
+    name: projectName,
     type: 'project',
   });
 
   await insertObject(db, 'entity_hierarchy', {
     id: generateId(),
-    name: PROJECT_CODE,
+    name: projectCode,
   });
   await insertObject(db, 'entity_relation', {
     id: generateId(),
-    parent_id: await codeToId(db, 'entity', PROJECT_CODE),
-    child_id: await codeToId(db, 'entity', COUNTRY_CODE),
-    entity_hierarchy_id: await hierarchyNameToId(db, PROJECT_CODE),
+    parent_id: await codeToId(db, 'entity', projectCode),
+    child_id: await codeToId(db, 'entity', countryCode),
+    entity_hierarchy_id: await hierarchyNameToId(db, projectCode),
   });
   await insertObject(db, 'project', {
     id: generateId(),
-    code: PROJECT_CODE,
+    code: projectCode,
     description: 'HIS for Palau’s public health data, integrating DHIS2 and mSupply.',
     sort_order: 10,
     image_url:
       'https://tupaia.s3-ap-southeast-2.amazonaws.com/uploads/olangch_palau_background_alt.jpeg',
     default_measure: '126,171',
-    dashboard_group_name: DASHBOARDGROUP_NAME,
-    user_groups: `{${USER_GROUP}}`,
+    dashboard_group_name: dashboardGroupName,
+    user_groups: `{${userGroup}}`,
     logo_url: '', // 'https://tupaia.s3-ap-southeast-2.amazonaws.com/uploads/olangch_palau_logo.png',
-    entity_id: await codeToId(db, 'entity', PROJECT_CODE),
-    entity_hierarchy_id: await hierarchyNameToId(db, PROJECT_CODE),
+    entity_id: await codeToId(db, 'entity', projectCode),
+    entity_hierarchy_id: await hierarchyNameToId(db, projectCode),
   });
 };
 
 exports.down = async function (db) {
-  const hierarchyId = await hierarchyNameToId(db, PROJECT_CODE);
+  const hierarchyId = await hierarchyNameToId(db, projectCode);
 
   await db.runSql(
-    `DELETE FROM "dashboardGroup" WHERE code IN ('${DASHBOARDGROUP_CODE}', '${DASHBOARDGROUP_COUNTRY_CODE}')`,
+    `DELETE FROM "dashboardGroup" WHERE code IN ('${dashboardGroupCode}', '${dashboardGroupCountryCode}')`,
   );
-  await db.runSql(`DELETE FROM project WHERE code = '${PROJECT_CODE}'`);
+  await db.runSql(`DELETE FROM project WHERE code = '${projectCode}'`);
   await db.runSql(`DELETE FROM entity_relation WHERE entity_hierarchy_id = '${hierarchyId}'`);
-  await db.runSql(`DELETE FROM entity_hierarchy WHERE name = '${PROJECT_CODE}'`);
-  await db.runSql(`DELETE FROM entity WHERE code = '${PROJECT_CODE}'`);
+  await db.runSql(`DELETE FROM entity_hierarchy WHERE name = '${projectCode}'`);
+  await db.runSql(`DELETE FROM entity WHERE code = '${projectCode}'`);
 };
 
 exports._meta = {
