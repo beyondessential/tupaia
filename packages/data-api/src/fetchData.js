040ea4274f13b04833fc9aec1808b9440e0ced9b
Edwin <edmofro@gmail.com>
2021-01-06 12:28:19 +1300

    Use analytics table in fetchData

--- packages/data-api/src/fetchData.js
+++ packages/data-api/src/fetchData.js
@@ -6,57 +6,36 @@ import { utcMoment } from '@tupaia/utils';
 
 import { SqlQuery } from './SqlQuery';
 
-const fetchIdsFromCodes = async (database, tableName, codes) => {
-  const records = await database.find(tableName, { code: codes }, { columns: ['id'] });
-  return records.map(r => r.id);
-};
-
-const generateBaseSqlQuery = async (
-  database,
-  { dataElementCodes, organisationUnitCodes, startDate, endDate },
-) => {
-  const questionIds = await fetchIdsFromCodes(database, 'question', dataElementCodes);
+const generateBaseSqlQuery = ({ dataElementCodes, organisationUnitCodes, startDate, endDate }) => {
   const sqlQuery = new SqlQuery(
     `
     SELECT
-      survey_response.id as "surveyResponseId",
-      survey_response.submission_time as "date",
-      entity.code as "entityCode",
-      entity.name as "entityName",
-      question.code as "dataElementCode",
-      question.type as "type",
-      answer.text as "value"
+      survey_response_id as "surveyResponseId",
+      date,
+      entity_code as "entityCode",
+      entity_name as "entityName",
+      data_element_code as "dataElementCode",
+      type,
+      value
     FROM
-      survey_response
-    JOIN
-      answer ON answer.survey_response_id = survey_response.id
-    JOIN
-      entity ON entity.id = survey_response.entity_id
-    JOIN
-      question ON question.id = answer.question_id
-    JOIN
-      survey ON survey.id = survey_response.survey_id
+      analytics
     WHERE
-      question_id IN ${SqlQuery.parameteriseArray(questionIds)}
+      data_element_code IN ${SqlQuery.parameteriseArray(dataElementCodes)}
     AND
-      entity.code IN ${SqlQuery.parameteriseArray(organisationUnitCodes)}
+      entity_code IN ${SqlQuery.parameteriseArray(organisationUnitCodes)}
   `,
-    [...questionIds, ...organisationUnitCodes],
+    [...dataElementCodes, ...organisationUnitCodes],
   );
 
   // Add start and end date, which are inclusive
   if (startDate) {
-    sqlQuery.addClause(`AND survey_response.submission_time >= ?`, [
-      utcMoment(startDate).startOf('day').toISOString(),
-    ]);
+    sqlQuery.addClause(`AND date >= ?`, [utcMoment(startDate).startOf('day').toISOString()]);
   }
   if (endDate) {
-    sqlQuery.addClause(`AND survey_response.submission_time <= ?`, [
-      utcMoment(endDate).endOf('day').toISOString(),
-    ]);
+    sqlQuery.addClause(`AND date <= ?`, [utcMoment(endDate).endOf('day').toISOString()]);
   }
 
-  sqlQuery.orderBy('survey_response.submission_time');
+  sqlQuery.orderBy('date');
 
   sqlQuery.logQuery();
 
@@ -64,7 +43,7 @@ const generateBaseSqlQuery = async (
 };
 
 export async function fetchEventData(database, options) {
-  const sqlQuery = await generateBaseSqlQuery(database, options);
+  const sqlQuery = generateBaseSqlQuery(options);
   const { surveyCode, eventId } = options;
   sqlQuery.addClause(`AND survey.code = ?`, [surveyCode]);
   if (eventId) {
@@ -74,6 +53,6 @@ export async function fetchEventData(database, options) {
 }
 
 export async function fetchAnalyticData(database, options) {
-  const sqlQuery = await generateBaseSqlQuery(database, options);
+  const sqlQuery = generateBaseSqlQuery(options);
   return sqlQuery.executeOnDatabase(database);
 }
