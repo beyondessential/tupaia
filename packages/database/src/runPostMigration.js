c59d940b727071db89d5dbd5932a4311daf50435
Edwin <edmofro@gmail.com>
2021-01-07 14:15:12 +1300

    Exclude analytics from trigger creation

--- packages/database/src/runPostMigration.js
+++ packages/database/src/runPostMigration.js
@@ -16,6 +16,7 @@ const EXCLUDED_TABLES_FROM_TRIGGER_CREATION = [
   'dashboardReport',
   'ancestor_descendant_relation',
   'psss_session',
+  'analytics',
 ];
 
 // tables that should only have records created and deleted, and will throw an error if an update is
