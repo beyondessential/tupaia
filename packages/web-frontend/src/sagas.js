8d66cc0a50977b85e12695523647cfdf76543d8a
Tom Caiger <caigertom@gmail.com>
2021-01-21 16:34:05 +1300

    redirect to home on logout (#1893)
    * redirect to home on logout
    
    * redirect to home after token login
    
    * Create .gitignore

--- packages/web-frontend/src/sagas.js
+++ packages/web-frontend/src/sagas.js
@@ -94,6 +94,7 @@ import {
   openEnlargedDialog,
   updateCurrentMeasureConfigOnceHierarchyLoads,
   LOCATION_CHANGE,
+  goHome,
 } from './actions';
 import { LOGIN_TYPES } from './constants';
 import {
@@ -485,6 +486,10 @@ function* attemptTokenLogin(action) {
 
 function* watchAttemptTokenLogin() {
   yield takeLatest(ATTEMPT_RESET_TOKEN_LOGIN, attemptTokenLogin);
+}
+
+function* watchAttemptTokenLoginSuccess() {
+  yield takeLatest(FETCH_RESET_TOKEN_LOGIN_SUCCESS, resetToHome);
 }
 
 function* openResetPasswordDialog() {
@@ -1072,12 +1077,16 @@ function* resetToProjectSplash() {
   yield put(setProject(DEFAULT_PROJECT_CODE));
 }
 
+function* resetToHome() {
+  yield put(goHome());
+}
+
 function* watchLoginSuccess() {
   yield takeLatest(FETCH_LOGIN_SUCCESS, fetchLoginData);
 }
 
 function* watchLogoutSuccess() {
-  yield takeLatest(FETCH_LOGOUT_SUCCESS, resetToProjectSplash);
+  yield takeLatest(FETCH_LOGOUT_SUCCESS, resetToHome);
 }
 
 function* fetchLoginData(action) {
@@ -1114,6 +1123,7 @@ export default [
   watchFetchInitialData,
   watchAttemptChangePasswordAndFetchIt,
   watchAttemptResetPasswordAndFetchIt,
+  watchAttemptTokenLoginSuccess,
   watchAttemptRequestCountryAccessAndFetchIt,
   watchAttemptUserLoginAndFetchIt,
   watchAttemptUserLogout,
