5064fe177b6660a2b734f97c524ac9b6e98c6857
nick <nickw@ourcomunity.com.au>
2021-01-22 14:33:40 +1100

    Remove village,facility,case entity_relations

--- packages/database/src/migrations/20210121213045-MigrateStriveCanonicalToAlternateHeirarchy-modifies-data.js
+++ packages/database/src/migrations/20210121213045-MigrateStriveCanonicalToAlternateHeirarchy-modifies-data.js
@@ -17,7 +17,6 @@ exports.setup = function (options, seedLink) {
 };
 
 const striveHierarchyId = '5e9d06e261f76a30c4000018';
-const countryCode = 'PG';
 const countryId = '5d3f8844f327a531bfd8ad77';
 const canonicalTypes = '{country,district,facility,village,case}';
 
@@ -46,46 +45,21 @@ const addDistrictEntityRelations = (db, ids) =>
     }),
   );
 
-const selectEntities = (db, entityType) =>
-  db.runSql(
-    `select id, parent_id from "entity" where country_code = '${countryCode}' and type = '${entityType}'`,
-  );
-
-const addEntityRelations = (db, entities) =>
-  entities.map(entity =>
-    insertObject(db, 'entity_relation', {
-      id: generateId(),
-      parent_id: entity.parent_id,
-      child_id: entity.id,
-      entity_hierarchy_id: striveHierarchyId,
-    }),
-  );
-
 exports.up = async function (db) {
   const districtIds = (await selectDistrictIds(db, striveDistrictCodes)).rows;
   await addDistrictEntityRelations(db, districtIds);
 
-  const facilities = (await selectEntities(db, 'facility')).rows;
-  await addEntityRelations(db, facilities);
-
-  const villages = (await selectEntities(db, 'village')).rows;
-  await addEntityRelations(db, villages);
-
-  const cases = (await selectEntities(db, 'case')).rows;
-  await addEntityRelations(db, cases);
-
-  await updateValues(db, 'entity_hierarchy', { canonical_types: {} }, { id: striveHierarchyId });
-};
-
-exports.down = async function (db) {
-  await db.runSql(
-    `delete from entity_relation where entity_hierarchy_id = '${striveHierarchyId}' and child_id != '${countryId}'`,
-  );
   await updateValues(
     db,
     'entity_hierarchy',
     { canonical_types: canonicalTypes },
     { id: striveHierarchyId },
+  );
+};
+
+exports.down = async function (db) {
+  await db.runSql(
+    `delete from entity_relation where entity_hierarchy_id = '${striveHierarchyId}' and child_id != '${countryId}'`,
   );
 };
 
