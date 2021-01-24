cca0545d24aea14cadb05925bceecfb012bfa6d5
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-01-24 23:33:21 +1300

    Remove dead code from index

--- packages/meditrak-server/src/dataAccessors/index.js
+++ packages/meditrak-server/src/dataAccessors/index.js
@@ -4,10 +4,6 @@
  */
 
 export { addSurveyImage } from './addSurveyImage';
-export { editAccessRequest } from './editAccessRequest';
-export { editUserAccount } from './editUserAccount';
-export { editOption } from './editOption';
-export { editOptionSet } from './editOptionSet';
 export { editSurvey } from './editSurvey';
 export { findEntitiesInCountry } from './findEntitiesInCountry';
 export { findSurveysInCountry } from './findSurveysInCountry';
@@ -17,7 +13,6 @@ export { deleteScreensForSurvey } from './deleteScreensForSurvey';
 export { deleteOrphanQuestions } from './deleteOrphanQuestions';
 export { createUser } from './createUser';
 export { updateOrCreateSurveyResponse } from './updateOrCreateSurveyResponse';
-export { editSurveyScreenComponent } from './editSurveyScreenComponent';
 export { findSurveyScreenComponentsInSurvey } from './findSurveyScreenComponentsInSurvey';
 export { findDataElementsInDataGroup } from './findDataElementsInDataGroup';
 export { createJoinChild } from './createJoinChild';
