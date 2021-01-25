9fe0d72211951aa28fafe639086ed053ad94cb81
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-01-25 17:06:22 +1300

    Remove deleted code from index file

--- packages/meditrak-server/src/dataAccessors/index.js
+++ packages/meditrak-server/src/dataAccessors/index.js
@@ -5,15 +5,9 @@
 
 export { addSurveyImage } from './addSurveyImage';
 export { editSurvey } from './editSurvey';
-export { findEntitiesInCountry } from './findEntitiesInCountry';
-export { findSurveysInCountry } from './findSurveysInCountry';
 export { findQuestionsInSurvey } from './findQuestionsInSurvey';
 export { findAnswersInSurveyResponse } from './findAnswersInSurveyResponse';
 export { deleteScreensForSurvey } from './deleteScreensForSurvey';
 export { deleteOrphanQuestions } from './deleteOrphanQuestions';
 export { createUser } from './createUser';
 export { updateOrCreateSurveyResponse } from './updateOrCreateSurveyResponse';
-export { findSurveyScreenComponentsInSurvey } from './findSurveyScreenComponentsInSurvey';
-export { findDataElementsInDataGroup } from './findDataElementsInDataGroup';
-export { createJoinChild } from './createJoinChild';
-export { findOrCountJoinChildren } from './findOrCountJoinChildren';
