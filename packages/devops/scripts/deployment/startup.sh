486531df3a6a8b60cba2efbbfd83e59be883d039
Edwin <edmofro@gmail.com>
2021-01-26 15:27:29 +1300

    Stop cloudwatch agent on startup

--- packages/devops/scripts/deployment/startup.sh
+++ packages/devops/scripts/deployment/startup.sh
@@ -11,6 +11,11 @@ DIR=$(dirname "$0")
 export STAGE=$(${DIR}/../utility/getEC2TagValue.sh Stage)
 echo "Starting up instance for ${STAGE}"
 
+# Turn off cloudwatch agent for all except prod and dev (can be turned on manually if needed on feature instances)
+if [[ $STAGE != "production" && $STAGE != "dev" ]]; then
+    /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
+fi
+
 # Set the branch based on STAGE
 if [[ $STAGE == "production" ]]; then
     export BRANCH="master"
