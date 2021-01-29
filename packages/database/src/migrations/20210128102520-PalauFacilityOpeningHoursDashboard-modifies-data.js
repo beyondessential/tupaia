b17401800ec057dd1e77828f46e3ef77b00d7b4c
billli0 <lbit123@outlook.com>
2021-01-29 11:33:07 +1100

    Create 20210128102520-PalauFacilityOpeningHoursDashboard-modifies-data.js

---
+++ packages/database/src/migrations/20210128102520-PalauFacilityOpeningHoursDashboard-modifies-data.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import {
+  insertObject,
+  addReportToGroups,
+  deleteReport,
+  removeReportFromGroups,
+} from '../utilities';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+const dashboardReport = {
+  id: 'Palau_Facility_Opening_Hours',
+  dataBuilder: 'FacilityOpeningHours',
+  dataBuilderConfig: {
+    dataElementCodes: {
+      Monday: ['FF16', 'FF20', 'FF21'],
+      Tuesday: ['FF17', 'FF22', 'FF23'],
+      Wednesday: ['BCD39D', 'FF24', 'FF25'],
+      Thursday: ['BCD39E', 'FF26', 'FF27'],
+      Friday: ['BCD39F', 'FF28', 'FF29'],
+      Saturday: ['BCD39G', 'FF30', 'FF31'],
+      Sunday: ['BCD39H', 'FF32', 'FF33'],
+    },
+  },
+  viewJson: {
+    name: 'Facility Opening Hours',
+    type: 'view',
+    viewType: 'multiValue',
+    valueType: 'text',
+  },
+};
+
+exports.up = async function (db) {
+  await insertObject(db, 'dashboardReport', dashboardReport);
+
+  await addReportToGroups(db, dashboardReport.id, ['PW_General_Facility_Public']);
+
+  return null;
+};
+
+exports.down = async function (db) {
+  await deleteReport(db, dashboardReport.id);
+
+  await removeReportFromGroups(db, dashboardReport.id, ['PW_General_Facility_Public']);
+
+  return null;
+};
+
+exports._meta = {
+  version: 1,
+};
