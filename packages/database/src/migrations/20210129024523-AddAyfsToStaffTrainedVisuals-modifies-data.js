97ee86f1f5c5f969854c197a4c19dfc27efbe1e1
nick <nickw@ourcomunity.com.au>
2021-02-03 06:06:10 +1100

    Add AYFS to UNFPA 'at least 1' matrixes

--- packages/database/src/migrations/20210129024523-AddAyfsToStaffTrainedVisuals-modifies-data.js
+++ packages/database/src/migrations/20210129024523-AddAyfsToStaffTrainedVisuals-modifies-data.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { arrayToDbString } from '../utilities';
+
 var dbm;
 var type;
 var seed;
@@ -14,7 +16,18 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const reportId = 'UNFPA_Region_Facilities_offering_services_At_Least_1_Matrix';
+const regionalReportId = 'UNFPA_Region_Facilities_offering_services_At_Least_1_Matrix';
+const countryReportIds = [
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_FJ',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_FM',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_KI',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_MH',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_WS',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_SB',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_TO',
+  'UNFPA_Country_Facilities_offering_services_At_Least_1_Matrix_VU',
+];
+const allReportIds = [regionalReportId, ...countryReportIds];
 const newDataElement = 'RHS2UNFPA240';
 const newRowName = 'Adolescent and Youth Services';
 const newRowKeySuffix = '_Adolescent_and_Youth_Services';
@@ -35,8 +48,8 @@ const baseCellConfig = {
       aggregationType: 'COUNT',
       aggregationConfig: {
         countCondition: {
-          value: 'Yes',
-          operator: '=',
+          value: '0',
+          operator: '>',
         },
       },
     },
@@ -47,32 +60,62 @@ const baseCellConfig = {
   ],
   operator: 'FRACTION_AND_PERCENTAGE',
 };
-const countryCellConfigs = Object.keys(countryCodeToName).map(code => ({
+
+const buildCellConfig = code => ({
   key: `${countryCodeToName[code]}${newRowKeySuffix}`,
   filter: {
     organisationUnit: code,
   },
   ...baseCellConfig,
-}));
-const rowsPath = '{rows}';
-const cellsPath = '{cells}';
+});
 
-exports.up = function (db) {
-  return db.runSql(`
-    update "dashboardReport"
-    set "dataBuilderConfig" = jsonb_set("dataBuilderConfig", '${rowsPath}', 
-    "dataBuilderConfig"->'rows' || '${JSON.stringify(newRowName)}'::jsonb)
-    where id = '${reportId}';
+const regionCellConfigs = Object.keys(countryCodeToName).map(code => buildCellConfig(code));
 
+const rowsInsertPath = '{rows, -1}';
+const cellsInsertPath = '{cells, -1}';
+
+const updateConfig = (db, path, value, ids) =>
+  db.runSql(`
     update "dashboardReport"
-    set "dataBuilderConfig" = jsonb_set("dataBuilderConfig", '${cellsPath}', 
-    "dataBuilderConfig"->'cells' || '${JSON.stringify([countryCellConfigs])}')
-    where id = '${reportId}';
+    set "dataBuilderConfig" = jsonb_insert("dataBuilderConfig", '${path}', 
+    '${JSON.stringify(value)}'::jsonb)
+    where id IN (${arrayToDbString(ids)});
   `);
+
+exports.up = async function (db) {
+  // all row names are the same
+  await updateConfig(db, rowsInsertPath, newRowName, allReportIds);
+
+  // regional config
+  await updateConfig(db, cellsInsertPath, regionCellConfigs, [regionalReportId]);
+
+  await Promise.all(
+    countryReportIds.map(countryReportId =>
+      updateConfig(
+        db,
+        cellsInsertPath,
+        [
+          buildCellConfig(
+            countryReportId.substring(countryReportId.length - 2, countryReportId.length),
+          ),
+        ],
+        [countryReportId],
+      ),
+    ),
+  );
 };
 
 exports.down = function (db) {
   return null;
+  // return db.runSql(`
+  //   update "dashboardReport"
+  //   set "dataBuilderConfig" = jsonb_set("dataBuilderConfig", '{rows}', "dataBuilderConfig"#>'{rows}' - 3)
+  //   where id IN (${arrayToDbString(allReportIds)});
+
+  //   update "dashboardReport"
+  //   set "dataBuilderConfig" = jsonb_set("dataBuilderConfig", '{cells}', "dataBuilderConfig"#>'{cells}' - 3)
+  //   where id IN (${arrayToDbString(allReportIds)});
+  // `);
 };
 
 exports._meta = {
