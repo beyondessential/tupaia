418815a121a888bb2cf56e61c8cd52c2684c702b
Edwin <edmofro@gmail.com>
2021-02-10 17:54:16 +1300

    Keep 'timestamp' values as string

--- packages/database/src/TupaiaDatabase.js
+++ packages/database/src/TupaiaDatabase.js
@@ -4,6 +4,7 @@
  */
 
 import autobind from 'react-autobind';
+import pg from 'pg';
 import knex from 'knex';
 import winston from 'winston';
 import { Multilock } from '@tupaia/utils';
@@ -50,6 +51,10 @@ const VALID_COMPARISON_TYPES = ['where', 'whereBetween', 'whereIn', 'orWhere'];
 // no math here, just hand-tuned to be as low as possible while
 // keeping all the tests passing
 const HANDLER_DEBOUNCE_DURATION = 250;
+
+// turn off parsing of timestamp (not timestamptz), so that it stays as a sort of "universal time"
+// string, independent of timezones, rather than being converted to local time
+pg.types.setTypeParser(1114, val => val);
 
 export class TupaiaDatabase {
   constructor(transactingConnection) {
