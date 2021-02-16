9c5fa9ff2566924161dc95f2bee3ee0c8de57e0c
Rohan Port <rohan@beyondessential.com.au>
2021-02-16 18:28:39 +1100

    2139: Shortened index names to avoid truncation

--- packages/data-api/scripts/createAnalyticsTableIndexes.sql
+++ packages/data-api/scripts/createAnalyticsTableIndexes.sql
@@ -1,8 +1,8 @@
 do $$ 
 declare
 begin 
-  CREATE INDEX analytics_data_element_code_entity_code_date_idx ON public.analytics(data_element_code, entity_code, date desc);
-  CREATE INDEX analytics_data_element_code_entity_code_survey_code_date_idx ON public.analytics(data_element_code, entity_code, survey_code, date desc);
-  CREATE INDEX analytics_data_element_code_entity_code_survey_code_event_id_date_idx ON public.analytics(data_element_code, entity_code, survey_code, event_id, date desc);
+  CREATE INDEX analytics_data_element_entity_date_idx ON public.analytics(data_element_code, entity_code, date desc);
+  CREATE INDEX analytics_data_element_entity_survey_date_idx ON public.analytics(data_element_code, entity_code, survey_code, date desc);
+  CREATE INDEX analytics_data_element_entity_survey_event_date_idx ON public.analytics(data_element_code, entity_code, survey_code, event_id, date desc);
   RAISE NOTICE 'Added analytics table indexes';
 end $$;
