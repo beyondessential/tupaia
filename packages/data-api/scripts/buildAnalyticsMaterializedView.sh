fbb9911ce723d57106f4b3cb3e8ab5f5a04ee915
Rohan Port <rohan@beyondessential.com.au>
2021-02-18 11:53:26 +1100

    2139: Updated build scripts for mv refresh analytics

--- packages/data-api/scripts/buildAnalyticsMaterializedView.sh
+++ packages/data-api/scripts/buildAnalyticsMaterializedView.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-source ../.env
-export PGPASSWORD=$DB_PG_PASSWORD
-psql --set=db_user="$DB_USER" --set=mv_user="$DB_MV_USER" -h $DB_URL -d $DB_NAME -U $DB_PG_USER -f grantMvRefreshPermissions.sql
+source .env
+cd scripts
 export PGPASSWORD=$DB_PASSWORD
 psql -h $DB_URL -d $DB_NAME -U $DB_USER -f createAnalyticsMaterializedView.sql
 export PGPASSWORD=$DB_MV_PASSWORD
