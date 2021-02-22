80b563b275d33a9c3bb655e17093a19fd746bbdf
Rohan Port <rohan@beyondessential.com.au>
2021-02-23 10:40:48 +1100

    2140: Re-wrote fetchData.js to fetch data from analytics table and support FINAL_EACH aggregations during data fetch

--- packages/data-api/src/SqlQuery.js
+++ packages/data-api/src/SqlQuery.js
@@ -6,13 +6,30 @@
 export class SqlQuery {
   static parameteriseArray = arr => `(${arr.map(() => '?').join(',')})`;
 
-  constructor(baseQuery, baseParameters) {
+  static parameteriseValues = arr => `VALUES (${arr.map(() => `?`).join('), (')})`;
+
+  constructor(baseQuery, baseParameters = []) {
     this.query = baseQuery;
     this.parameters = baseParameters;
     this.orderByClause = null;
+    this.hasWhereClause = false;
   }
 
-  addClause(clause, parameters) {
+  loggableQuery() {
+    const replacementIterator = this.parameters
+      .map(param => param.replace(/'/g, "''"))
+      [Symbol.iterator]();
+    return this.query.replace(/\?/g, () => `'${replacementIterator.next().value}'`);
+  }
+
+  wrapAs(tableDefinition) {
+    this.query = `
+    WITH ${tableDefinition}
+    AS (${this.query})
+    `;
+  }
+
+  addClause(clause, parameters = []) {
     this.query = `
       ${this.query}
       ${clause}
@@ -20,8 +37,20 @@ export class SqlQuery {
     this.parameters = this.parameters.concat(parameters);
   }
 
-  orderBy(orderByClause) {
-    this.orderByClause = orderByClause;
+  addWhereClause(clause, parameters) {
+    this.query = `
+      ${this.query}
+      ${this.hasWhereClause ? 'AND' : 'WHERE'} ${clause}
+    `;
+    this.parameters = this.parameters.concat(parameters);
+    this.hasWhereClause = true;
+  }
+
+  addOrderByClause(orderByClause) {
+    this.query = `
+      ${this.query}
+      ORDER BY ${orderByClause}
+    `;
   }
 
   async executeOnDatabase(database) {
--- packages/data-api/src/fetchData.js
+++ packages/data-api/src/fetchData.js
@@ -6,63 +6,159 @@ import { utcMoment } from '@tupaia/utils';
 
 import { SqlQuery } from './SqlQuery';
 
-const generateBaseSqlQuery = ({ dataElementCodes, organisationUnitCodes, startDate, endDate }) => {
+const SUPPORTED_AGGREGATIONS = [
+  'FINAL_EACH_DAY',
+  'FINAL_EACH_WEEK',
+  'FINAL_EACH_MONTH',
+  'FINAL_EACH_YEAR',
+  'MOST_RECENT',
+];
+
+const COMMON_FIELDS = 'data_element_code, entity_code, entity_name';
+const ANSWER_SPECIFIC_FIELDS = 'date, event_id, value, answer_type';
+
+const getA1WhereClause = conditions => {
+  let hasAnyCondition = false;
+  let clause = '';
+  for (const [condition, value] of Object.entries(conditions)) {
+    if (!value) {
+      continue;
+    }
+
+    clause = `${clause}
+              ${hasAnyCondition ? 'AND' : 'WHERE'}`;
+    switch (condition) {
+      case 'surveyCode':
+        clause = `${clause} survey_code = ?`;
+        break;
+      case 'eventId':
+        clause = `${clause} event_id = ?`;
+        break;
+      case 'startDate':
+        clause = `${clause} date >= ?`;
+        break;
+      case 'endDate':
+        clause = `${clause} date <= ?`;
+        break;
+      default:
+        throw new Error(`Unknown condition in fetch data where clause: ${condition}`);
+    }
+    hasAnyCondition = true;
+  }
+
+  return clause;
+};
+
+const getA1GroupByClause = firstAggregationType => {
+  switch (firstAggregationType) {
+    case 'FINAL_EACH_DAY':
+      return `GROUP BY ${COMMON_FIELDS}, day_period`;
+    case 'FINAL_EACH_WEEK':
+      return `GROUP BY ${COMMON_FIELDS}, week_period`;
+    case 'FINAL_EACH_MONTH':
+      return `GROUP BY ${COMMON_FIELDS}, month_period`;
+    case 'FINAL_EACH_YEAR':
+      return `GROUP BY ${COMMON_FIELDS}, year_period`;
+    case 'MOST_RECENT':
+      return `GROUP BY ${COMMON_FIELDS}`;
+    default:
+      return '';
+  }
+};
+
+const getA2FinalWhereClause = firstAggregationType => {
+  switch (firstAggregationType) {
+    case 'FINAL_EACH_DAY':
+      return 'AND day_period = a1.day_period';
+    case 'FINAL_EACH_WEEK':
+      return 'AND week_period = a1.week_period';
+    case 'FINAL_EACH_MONTH':
+      return 'AND month_period = a1.month_period';
+    case 'FINAL_EACH_YEAR':
+      return 'AND year_period = a1.year_period';
+    default:
+      return '';
+  }
+};
+
+const getA1Select = firstAggregationType => {
+  switch (firstAggregationType) {
+    case 'FINAL_EACH_DAY':
+      return `SELECT ${COMMON_FIELDS}, day_period`;
+    case 'FINAL_EACH_WEEK':
+      return `SELECT ${COMMON_FIELDS}, week_period`;
+    case 'FINAL_EACH_MONTH':
+      return `SELECT ${COMMON_FIELDS}, month_period`;
+    case 'FINAL_EACH_YEAR':
+      return `SELECT ${COMMON_FIELDS}, year_period`;
+    case 'MOST_RECENT':
+      return `SELECT ${COMMON_FIELDS}`;
+    default:
+      return `SELECT ${COMMON_FIELDS}, ${ANSWER_SPECIFIC_FIELDS}`;
+  }
+};
+
+const getA2Join = firstAggregationType => {
+  return SUPPORTED_AGGREGATIONS.includes(firstAggregationType)
+    ? `CROSS JOIN LATERAL (
+      SELECT ${ANSWER_SPECIFIC_FIELDS}
+      FROM analytics
+      WHERE entity_code = a1.entity_code
+      AND data_element_code = a1.data_element_code
+      ${getA2FinalWhereClause(firstAggregationType)}
+      order by date desc
+      limit 1
+    ) as a2`
+    : '';
+};
+
+const generateBaseSqlQuery = ({
+  dataElementCodes,
+  organisationUnitCodes,
+  surveyCode,
+  eventId,
+  startDate,
+  endDate,
+  aggregations,
+}) => {
+  const firstAggregationType = aggregations && aggregations[0] && aggregations[0].type;
   const sqlQuery = new SqlQuery(
     `
-    SELECT
-      survey_response.id as "surveyResponseId",
-      survey_response.submission_time as "date",
-      entity.code as "entityCode",
-      entity.name as "entityName",
-      question.code as "dataElementCode",
-      question.type as "type",
-      answer.text as "value"
-    FROM
-      survey_response
-    JOIN
-      answer ON answer.survey_response_id = survey_response.id
-    JOIN
-      entity ON entity.id = survey_response.entity_id
-    JOIN
-      question ON question.id = answer.question_id
-    JOIN
-      survey ON survey.id = survey_response.survey_id
-    WHERE
-      question.code IN ${SqlQuery.parameteriseArray(dataElementCodes)}
-    AND
-      entity.code IN ${SqlQuery.parameteriseArray(organisationUnitCodes)}
+    SELECT 
+      date AS "date",
+      entity_code AS "entityCode",
+      entity_name AS "entityName",
+      data_element_code AS "dataElementCode",
+      event_id AS "eventId",
+      value AS "value",
+      answer_type AS "type"
+    FROM (
+      ${getA1Select(firstAggregationType)}
+      FROM analytics
+      INNER JOIN (
+        ${SqlQuery.parameteriseValues(dataElementCodes)}
+      ) data_element_codes(code) ON data_element_codes.code = analytics.data_element_code
+      INNER JOIN (
+        ${SqlQuery.parameteriseValues(organisationUnitCodes)}
+      ) entity_codes(code) ON entity_codes.code = analytics.entity_code
+        ${getA1WhereClause({ surveyCode, eventId, startDate, endDate })}
+        ${getA1GroupByClause(firstAggregationType)}
+    ) as a1
+    ${getA2Join(firstAggregationType)}
   `,
-    [...dataElementCodes, ...organisationUnitCodes],
+    [...dataElementCodes, ...organisationUnitCodes]
+      .concat(surveyCode ? [surveyCode] : [])
+      .concat(eventId ? [eventId] : [])
+      .concat(startDate ? [utcMoment(startDate).startOf('day').toISOString()] : [])
+      .concat(endDate ? [utcMoment(endDate).endOf('day').toISOString()] : []),
   );
 
-  // Add start and end date, which are inclusive
-  if (startDate) {
-    sqlQuery.addClause(`AND survey_response.submission_time >= ?`, [
-      utcMoment(startDate).startOf('day').toISOString(),
-    ]);
-  }
-  if (endDate) {
-    sqlQuery.addClause(`AND survey_response.submission_time <= ?`, [
-      utcMoment(endDate).endOf('day').toISOString(),
-    ]);
-  }
-
-  sqlQuery.orderBy('survey_response.submission_time');
+  sqlQuery.addOrderByClause('date');
 
   return sqlQuery;
 };
 
-export async function fetchEventData(database, options) {
-  const sqlQuery = generateBaseSqlQuery(options);
-  const { surveyCode, eventId } = options;
-  sqlQuery.addClause(`AND survey.code = ?`, [surveyCode]);
-  if (eventId) {
-    sqlQuery.addClause(`AND survey_response.id = ?`, [eventId]);
-  }
-  return sqlQuery.executeOnDatabase(database);
-}
-
-export async function fetchAnalyticData(database, options) {
+export async function fetchData(database, options) {
   const sqlQuery = generateBaseSqlQuery(options);
   return sqlQuery.executeOnDatabase(database);
 }
