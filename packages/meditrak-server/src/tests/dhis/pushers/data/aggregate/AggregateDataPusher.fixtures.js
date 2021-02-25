f4261b4bff9bcc96c011285a209a3e05ae162b94
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-02-25 13:10:20 +1300

    Fix failing unit tests Delete 'only' in questionHooks test

--- packages/meditrak-server/src/tests/dhis/pushers/data/aggregate/AggregateDataPusher.fixtures.js
+++ packages/meditrak-server/src/tests/dhis/pushers/data/aggregate/AggregateDataPusher.fixtures.js
@@ -101,7 +101,7 @@ export const SURVEY_RESPONSE_DATA_VALUE_DIMENSIONS = {
 export const SURVEY_RESPONSE_DATA_VALUE = {
   ...ANSWER_DATA_VALUE,
   ...SURVEY_RESPONSE_DATA_VALUE_DIMENSIONS,
-  value: moment(SURVEY_RESPONSE.data_time).format(),
+  value: moment(SURVEY_RESPONSE.data_time).format('YYYY-MM-DDTHH:mm:ss.SSS'),
 };
 export const SURVEY_RESPONSE_SYNC_LOG_DATA = {
   ...ANSWER_SYNC_LOG_DATA,
--- packages/meditrak-server/src/tests/hooks/questionHooks.test.js
+++ packages/meditrak-server/src/tests/hooks/questionHooks.test.js
@@ -161,7 +161,7 @@ describe('Question hooks', () => {
         expect(spy2.called).to.be.true;
       });
 
-      it.only('Should not run a hook for a backdated value', async () => {
+      it('Should not run a hook for a backdated value', async () => {
         const spy = createHookSpy('backdateTestHook');
 
         await app.post('surveyResponse', {
