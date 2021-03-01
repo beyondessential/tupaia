c8c5345db5fae25823ab92832b87ecc5eb8879bf
Rohan Port <rohan@beyondessential.com.au>
2021-03-01 17:22:49 +1100

    2101: updateCiEnvVars now outputs a diff of the changes in the encrypted file

--- packages/devops/scripts/ci/updateCiEnvVars.sh
+++ packages/devops/scripts/ci/updateCiEnvVars.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
 rm -f .env
+rm -f old.env
+echo "Decryting old ci-env-vars"
+jet decrypt ci-env-vars.encrypted old.env
+echo "Creating new ci-env-vars"
 for PACKAGE in "meditrak-server" "admin-panel" "web-frontend" "web-config-server" "devops" "data-api" "database"; do
     cat ./packages/${PACKAGE}/.env >>.env
     echo "" >>.env
 done
+echo "Encrypting new ci-env-vars"
 jet encrypt .env ci-env-vars.encrypted
+
+echo "Diff between new .env and old .env"
+diff --color=always --suppress-common-lines <(source old.env; set) <(source .env; set) | grep -v '> _=.env' | grep -v '< _=old.env'
+rm -f old.env
+
+exit 0
