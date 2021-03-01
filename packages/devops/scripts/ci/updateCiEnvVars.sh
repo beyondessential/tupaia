9dab8fd0be25225a1aa1dacd2411d37db0c104eb
Rohan Port <rohan@beyondessential.com.au>
2021-03-01 12:04:53 +1100

    2101: Updated ci-env-vars to include data-api package - Added newline after each package .env file to avoid bugs when those files miss a newline at EOF

--- packages/devops/scripts/ci/updateCiEnvVars.sh
+++ packages/devops/scripts/ci/updateCiEnvVars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -f .env
-for PACKAGE in "meditrak-server" "admin-panel" "web-frontend" "web-config-server" "devops" "database"; do
+for PACKAGE in "meditrak-server" "admin-panel" "web-frontend" "web-config-server" "devops" "data-api" "database"; do
     cat ./packages/${PACKAGE}/.env >>.env
+    echo "" >>.env
 done
 jet encrypt .env ci-env-vars.encrypted
