60f1dd86e510efc59804bdc3da1d5968a8463f3d
chris-bes <63621318+chris-bes@users.noreply.github.com>
2021-03-02 19:20:14 +1100

    1378: deleted files

--- packages/indicators/src/Builder/EventCheckConditionBuilder/config/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Tupaia
- * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
- */
-
-export { EventCheckConditionConfig, DefaultValue } from './types';
-export { configValidators } from './validate';
