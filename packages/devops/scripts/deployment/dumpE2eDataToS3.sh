0e4a19fb60a1e6cf8e9da4455c04fac24be00f5b
Edwin <edmofro@gmail.com>
2021-03-04 14:11:18 +1300

    Call restoreE2eDataFromS3 from startup script

--- packages/devops/scripts/deployment/dumpE2eDataToS3.sh
+++ packages/devops/scripts/deployment/dumpE2eDataToS3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Run directly from a nightly cron job, so needs full setup of path etc.
+
 # Set PATH to include depencencies
 export PATH=/home/ubuntu/.local/bin:/home/ubuntu/.yarn/bin:/home/ubuntu/.config/yarn/global/node_modules/.bin:/home/ubuntu/.nvm/versions/node/v12.18.3/bin:/usr/local/bin:$PATH
 
