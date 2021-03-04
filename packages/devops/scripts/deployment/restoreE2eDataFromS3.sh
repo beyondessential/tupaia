9ecea6ccfbb0574dd91917f537c36565efb3ac2f
Edwin <edmofro@gmail.com>
2021-03-04 15:25:24 +1300

    Add table names to csv filename

--- packages/devops/scripts/deployment/restoreE2eDataFromS3.sh
+++ packages/devops/scripts/deployment/restoreE2eDataFromS3.sh
@@ -16,10 +16,10 @@ echo "Clearing survey response and answer tables"
 psql -U tupaia tupaia -c "TRUNCATE TABLE survey_response CASCADE;"
 
 echo "Restoring survey responses"
-psql -U tupaia tupaia -c "\copy survey_response from 'dumps/.csv' delimiter ',' csv header"
+psql -U tupaia tupaia -c "\copy survey_response from 'dumps/survey_response.csv' delimiter ',' csv header"
 
 echo "Restoring answers"
-psql -U tupaia tupaia -c "\copy answer from 'dumps/.csv' delimiter ',' csv header"
+psql -U tupaia tupaia -c "\copy answer from 'dumps/answer.csv' delimiter ',' csv header"
 
 echo "Re-enabling notification triggers"
 psql -U tupaia tupaia -c "ALTER TABLE survey_response DISABLE TRIGGER survey_response_trigger;"
