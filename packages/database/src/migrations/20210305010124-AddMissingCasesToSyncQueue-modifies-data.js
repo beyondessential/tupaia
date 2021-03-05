8c1cddbeb5c3338ce08a071ae7720f149020dd21
avaek <vanbeekandrew@gmail.com>
2021-03-05 16:56:46 +1300

    2257 add cases and update survey responses in sync queue

--- packages/database/src/migrations/20210305010124-AddMissingCasesToSyncQueue-modifies-data.js
+++ packages/database/src/migrations/20210305010124-AddMissingCasesToSyncQueue-modifies-data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { DatabaseChangeChannel } from '../DatabaseChangeChannel';
-import { markRecordsForResync } from '../utilities';
+import { arrayToDbString, markRecordsForResync } from '../utilities';
 
 var dbm;
 var type;
@@ -17,9 +17,9 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-const createSelectCasesQuery = columns => {
+const createSelectCasesQuery = () => {
   return `
-    SELECT e.${columns}
+    SELECT e.*
     FROM entity e
     LEFT JOIN dhis_sync_queue dsq ON e.id = record_id
     WHERE
@@ -29,10 +29,40 @@ const createSelectCasesQuery = columns => {
 };
 
 const selectCasesForResync = async db => {
-  const query = createSelectCasesQuery('*');
+  const query = createSelectCasesQuery();
   const { rows } = await db.runSql(query);
   return rows;
 };
+
+const createSelectSurveyResponsesQuery = () => {
+  return `
+    SELECT * FROM survey_response
+    WHERE entity_id in
+    (SELECT e.id
+    FROM entity e
+    LEFT JOIN dhis_sync_queue dsq ON e.id = record_id
+    WHERE
+        dsq.id IS NULL and
+        e.type = 'case'
+    );
+`;
+};
+
+const selectSurveyResponsesForResync = async db => {
+  const query = createSelectSurveyResponsesQuery();
+  const { rows } = await db.runSql(query);
+  return rows;
+};
+
+const updateSyncQueueForSurveyResponses = async (db, surveyResponseIds) =>
+  db.runSql(`
+    UPDATE dhis_sync_queue dsq
+    SET priority = 2 -- use priority 2 not to block up new data syncing through
+    WHERE
+      is_deleted = 'false' AND
+      record_type = 'survey_response' AND
+      type = 'update' AND
+      record_id IN (${arrayToDbString(surveyResponseIds)})`);
 
 exports.up = async function (db) {
   const changeChannel = new DatabaseChangeChannel();
@@ -40,7 +70,10 @@ exports.up = async function (db) {
   try {
     // n.b. this requires a meditrak-server instance to be running and listening for the changes
     const cases = await selectCasesForResync(db);
+    const surveyResponses = await selectSurveyResponsesForResync(db);
+    const surveyResponseIds = surveyResponses.map(sr => sr.id);
     await markRecordsForResync(changeChannel, 'entity', cases);
+    await updateSyncQueueForSurveyResponses(db, surveyResponseIds);
   } finally {
     changeChannel.close();
   }
