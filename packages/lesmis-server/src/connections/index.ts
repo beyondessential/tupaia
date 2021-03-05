dba6254b7931853abbe7a71857a2da9b4a27173b
Tom Caiger <caigertom@gmail.com>
2021-03-05 14:33:00 +1300

    update export syntax

--- packages/lesmis-server/src/connections/index.ts
+++ packages/lesmis-server/src/connections/index.ts
@@ -3,5 +3,5 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-export { MeditrakConnection } from './MeditrakConnection';
-export { ReportConnection } from './ReportConnection';
+export * from './MeditrakConnection';
+export * from './ReportConnection';
