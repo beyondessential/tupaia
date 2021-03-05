8a1171be32f0f8de4b541fe5c23cc4729e7b8e23
Kostas Karvounis <kael89@users.noreply.github.com>
2021-03-05 11:43:47 +1100

    1534: Persist e2e test snapshots (#2161)
    * Persist e2e test snapshots
    
    * Check for reserved branch names
    
    * Add getArgs util

---
+++ packages/web-frontend/cypress/scripts/testE2e/pushSnapshots.js
@@ -0,0 +1,26 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+import moment from 'moment-timezone';
+
+import { SNAPSHOTS } from '../../constants';
+
+export const pushSnapshots = async (snapshotRepo, branch, snapshots) => {
+  const date = Date.now();
+  const updateBranch = `tupaiabot/snapshot-update/${branch}_${date}`;
+  await snapshotRepo.createBranchIfNotExists(branch);
+  await snapshotRepo.createBranch(updateBranch, branch);
+
+  const commitFiles = [{ path: SNAPSHOTS.pathInRepo, content: snapshots.toString() }];
+  await snapshotRepo.commitFilesToBranch(updateBranch, commitFiles, 'Update snapshots');
+  const prTitle = `${branch} snapshot update (${moment(date).utc().format()})`;
+
+  const { html_url: pullRequestUrl } = await snapshotRepo.createPullRequest({
+    base: branch,
+    head: updateBranch,
+    title: prTitle,
+  });
+  return { pullRequestUrl };
+};
