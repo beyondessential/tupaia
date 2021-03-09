92e6fcb1890ab293b19fa29075065ca6d6610818
Edwin <edmofro@gmail.com>
2021-03-10 09:55:40 +1300

    Delete and recreate pm2 process on deployment

--- packages/devops/scripts/ci/deployBackend.sh
+++ packages/devops/scripts/ci/deployBackend.sh
@@ -4,7 +4,7 @@ DIR=$(dirname "$0")
 DEPLOYMENT_URL=$(${DIR}/determineDeploymentUrl.sh)
 if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
     echo "Deployment for ${CI_BRANCH} exists, updating with latest changes"
-    ssh ubuntu@$DEPLOYMENT_URL "cd tupaia && yarn workspace @tupaia/${PACKAGE} build && pm2 restart ${PACKAGE} --time;"
+    ssh ubuntu@$DEPLOYMENT_URL "cd tupaia && yarn workspace @tupaia/${PACKAGE} build && pm2 delete ${PACKAGE} && pm2 start --name $PACKAGE dist --wait-ready --listen-timeout 15000 --time;"
 else
     echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
