37f6cce89e2b697603bd6362109e0cd46421520f
Tom Caiger <caigertom@gmail.com>
2021-03-11 12:52:05 +1300

    Create Breadcrumbs.js

---
+++ packages/lesmis/src/components/Breadcrumbs.js
@@ -0,0 +1,81 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ *
+ */
+import React from 'react';
+import styled from 'styled-components';
+import { Link as RouterLink } from 'react-router-dom';
+import MuiBreadcrumbs from '@material-ui/core/Breadcrumbs';
+import MuiLink from '@material-ui/core/Link';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { useCurrentOrgUnitData } from '../api';
+
+const StyledBreadcrumbs = styled(MuiBreadcrumbs)`
+  font-size: 0.75rem;
+  line-height: 0.875rem;
+  text-transform: capitalize;
+
+  .MuiBreadcrumbs-separator {
+    margin-left: 0.2rem;
+    margin-right: 0.3rem;
+
+    svg {
+      font-size: 0.75rem;
+    }
+  }
+`;
+
+const ActiveSegment = styled.span`
+  color: ${props => props.theme.palette.primary.main};
+`;
+
+const Link = props => <MuiLink color="inherit" {...props} component={RouterLink} />;
+
+const getSegments = (orgUnits, orgUnitCode, heirarchy = []) => {
+  const orgUnit = orgUnits.find(entity => entity.organisationUnitCode === orgUnitCode);
+
+  if (!orgUnit) {
+    return [];
+  }
+
+  const newHeirarchy = [...heirarchy, orgUnit];
+
+  if (orgUnit.type === 'Country') {
+    return newHeirarchy.reverse();
+  }
+  return getSegments(orgUnits, orgUnit.parent, newHeirarchy);
+};
+
+const useBreadcrumbs = () => {
+  const query = useCurrentOrgUnitData({ includeCountryData: true });
+
+  if (!query.data) {
+    return { ...query, segments: [] };
+  }
+
+  const { countryHierarchy, organisationUnitCode } = query.data;
+  const segments = getSegments(countryHierarchy, organisationUnitCode);
+
+  return { ...query, segments };
+};
+
+export const Breadcrumbs = () => {
+  const { segments } = useBreadcrumbs();
+
+  return (
+    <StyledBreadcrumbs separator={<NavigateNextIcon />}>
+      <Link to="/">Home</Link>
+      {segments.map(({ name, organisationUnitCode }, index) => {
+        const last = index === segments.length - 1;
+        return last ? (
+          <ActiveSegment key={organisationUnitCode}>{name}</ActiveSegment>
+        ) : (
+          <Link to={`/${organisationUnitCode}`} key={organisationUnitCode}>
+            {name}
+          </Link>
+        );
+      })}
+    </StyledBreadcrumbs>
+  );
+};
