9749526dcfeaa9f724fc4656134811fab9f32c0d
billli0 <lbit123@outlook.com>
2021-03-10 13:27:59 +1100

    Migration

--- packages/database/src/migrations/20210309020032-MalariaMapOverlaysLaosEOC-modifies-data.js
+++ packages/database/src/migrations/20210309020032-MalariaMapOverlaysLaosEOC-modifies-data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { insertObject, generateId } from '../utilities';
+import { insertObject, generateId, deleteObject } from '../utilities';
 
 var dbm;
 var type;
@@ -16,30 +16,17 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const dataSourceId = generateId();
-const malariaMapOverlayGroupId = generateId();
-const mapOverlayGroupRelationId = generateId();
-
-const dataSource = {
-  id: dataSourceId,
-  code: 'Total_Positive_Malaria_Cases',
-  type: 'dataElement',
-  service_type: 'dhis',
-  config: { dhisId: 'WgzuSlOetQV', isDataRegional: false, dhisDataType: 'ProgramIndicator' },
-};
 const malariaMapOverlayGroup = {
-  id: malariaMapOverlayGroupId,
+  id: generateId(),
   name: 'Malaria',
   code: 'LAOS_EOC_Malaria',
 };
-const malariaCaseMayOverlay = {
-  id: 'LAOS_EOC_Total_Malaria_Cases_By_Sub_District',
-  name: 'Malaria Cases by Sub District',
+const malariaBaseMapOverlay = {
   userGroup: 'Laos EOC User',
-  dataElementCode: 'Total_Positive_Malaria_Cases',
   isDataRegional: false,
   measureBuilderConfig: {
     aggregationType: 'SUM',
+    programCodes: ['Malaria_Case_Reporting'],
     entityAggregation: {
       dataSourceEntityType: 'facility',
       aggregationEntityType: 'sub_district',
@@ -47,24 +34,11 @@ const malariaCaseMayOverlay = {
   },
   measureBuilder: 'valueForOrgGroup',
   presentationOptions: {
-    values: [
-      { color: 'blue', value: 'other' },
-      { color: 'grey', value: null },
-    ],
+    values: [{ color: 'grey', value: null }],
     scaleType: 'performanceDesc',
     displayType: 'shaded-spectrum',
     scaleBounds: { left: { max: 0 } },
     measureLevel: 'SubDistrict',
-    datePickerLimits: {
-      end: {
-        unit: 'day',
-        offset: 15,
-      },
-      start: {
-        unit: 'day',
-        offset: 0,
-      },
-    },
     defaultTimePeriod: {
       unit: 'day',
       offset: 0,
@@ -74,45 +48,55 @@ const malariaCaseMayOverlay = {
   countryCodes: '{"LA"}',
   projectCodes: '{"laos_eoc"}',
 };
+const malariaCaseMapOverlay = {
+  ...malariaBaseMapOverlay,
+  id: 'LAOS_EOC_Total_Malaria_Cases_By_Sub_District',
+  name: 'Malaria Cases by Sub District',
+  dataElementCode: 'Total_Positive_Malaria_Cases',
+};
+const malariaDeathMapOverlay = {
+  ...malariaBaseMapOverlay,
+  id: 'LAOS_EOC_Total_Malaria_Deaths_By_Sub_District',
+  name: '	Malaria Deaths by Sub District',
+  dataElementCode: 'Total_Malaria_Deaths',
+};
 const mapOverlayGroupRelation = {
-  id: mapOverlayGroupRelationId,
-  map_overlay_group_id: malariaMapOverlayGroupId,
-  child_id: malariaCaseMayOverlay.id,
+  map_overlay_group_id: malariaMapOverlayGroup.id,
   child_type: 'mapOverlay',
 };
 // Root
 const mapOverlayGroupToRootRelation = {
   id: generateId(),
   map_overlay_group_id: '5f88d3a361f76a2d3f000004',
-  child_id: malariaMapOverlayGroupId,
+  child_id: malariaMapOverlayGroup.id,
   child_type: 'mapOverlayGroup',
 };
 
 exports.up = async function (db) {
-  await insertObject(db, 'data_source', dataSource);
   await insertObject(db, 'map_overlay_group', malariaMapOverlayGroup);
-  await insertObject(db, 'mapOverlay', malariaCaseMayOverlay);
-  await insertObject(db, 'map_overlay_group_relation', mapOverlayGroupRelation);
+
+  for (const mapOverlay of [malariaCaseMapOverlay, malariaDeathMapOverlay]) {
+    await insertObject(db, 'mapOverlay', mapOverlay);
+    mapOverlayGroupRelation.id = generateId();
+    mapOverlayGroupRelation.child_id = mapOverlay.id;
+    await insertObject(db, 'map_overlay_group_relation', mapOverlayGroupRelation);
+  }
+
   await insertObject(db, 'map_overlay_group_relation', mapOverlayGroupToRootRelation);
 };
 
-const deleteObject = async (db, table, condition) => {
-  const [key, value] = Object.entries(condition)[0];
-  return db.runSql(`
-    DELETE FROM "${table}"
-    WHERE ${key} = '${value}'
-`);
-};
 exports.down = async function (db) {
-  await deleteObject(db, 'map_overlay_group_relation', {
-    child_id: mapOverlayGroupRelation.child_id,
-  });
   await deleteObject(db, 'map_overlay_group_relation', {
     child_id: mapOverlayGroupToRootRelation.child_id,
   });
+  for (const mapOverlay of [malariaCaseMapOverlay, malariaDeathMapOverlay]) {
+    await deleteObject(db, 'map_overlay_group_relation', {
+      child_id: mapOverlay.id,
+    });
+    await deleteObject(db, 'mapOverlay', { id: mapOverlay.id });
+  }
+
   await deleteObject(db, 'map_overlay_group', { code: malariaMapOverlayGroup.code });
-  await deleteObject(db, 'mapOverlay', { id: malariaCaseMayOverlay.id });
-  await deleteObject(db, 'data_source', { code: dataSource.code });
 };
 
 exports._meta = {
