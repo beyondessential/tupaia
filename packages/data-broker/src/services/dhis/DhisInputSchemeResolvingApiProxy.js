ddb8c44276376aad2841f1cb4e194794b37d6b60
Igor <igor@beyondessential.com.au>
2021-03-16 15:40:45 +1100

    Hotfix bind params too long [direct apply to feature branch] (#2253)
    * Hotfix bind params too long
    
    * Minor: add todo

--- packages/data-broker/src/services/dhis/DhisInputSchemeResolvingApiProxy.js
+++ packages/data-broker/src/services/dhis/DhisInputSchemeResolvingApiProxy.js
@@ -222,12 +222,20 @@ export class DhisInputSchemeResolvingApiProxy {
 
     const dhisIds = response.rows.map(row => row[orgUnitIdIndex]);
 
-    const mappings = await this.models.dataServiceEntity.find({
-      [QUERY_CONJUNCTIONS.RAW]: {
-        sql: `config->>'dhis_id' in (${dhisIds.map(() => '?')})`,
-        parameters: dhisIds,
-      },
-    });
+    let mappings = [];
+    // need to batch this query because we can get an SQL error if we do too many
+    // TODO convert to using `runDatabaseFunctionInBatches` or one of its wrappers
+    const SQL_BIND_CHUNK_SIZE = 1000;
+    for (let i = 0; i < dhisIds.length; i += SQL_BIND_CHUNK_SIZE) {
+      const chunkDhisIds = dhisIds.slice(i, i + SQL_BIND_CHUNK_SIZE);
+      const chunkMappings = await this.models.dataServiceEntity.find({
+        [QUERY_CONJUNCTIONS.RAW]: {
+          sql: `config->>'dhis_id' in (${chunkDhisIds.map(() => '?')})`,
+          parameters: chunkDhisIds,
+        },
+      });
+      mappings = [...mappings, ...chunkMappings];
+    }
 
     const mappingsByDhisId = reduceToDictionary(mappings, el => el.config.dhis_id, 'entity_code');
 
