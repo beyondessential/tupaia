e7f57f9cc44f111be493ff28580f68a4e0f55e66
Rohan Port <rohan@beyondessential.com.au>
2021-03-23 12:01:23 +1100

    2312: Updated linting package - Fixed incorrect tsconfig which was leading types for jest to be missing

--- packages/entity-server/src/models/types.ts
+++ packages/entity-server/src/models/types.ts
@@ -8,8 +8,8 @@ import { DatabaseModel, DatabaseType } from '@tupaia/database';
 export type DbConditional<T> = { [field in keyof T]?: T[field] | T[field][] };
 
 export type Joined<T, U extends string> = {
-  [field in keyof T as field extends string ? `${U}.${field}` : never]: T[field]
-}
+  [field in keyof T as field extends string ? `${U}.${field}` : never]: T[field];
+};
 
 export type Model<BaseModel extends DatabaseModel, Fields, Type extends DatabaseType> = {
   find: (filter: DbConditional<Fields>) => Promise<Type[]>;
