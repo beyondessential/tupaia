2c4d76a8fc34756a2fa966b02be8955b341f2037
Pauline <pauline@beyondessential.com.au>
2021-03-24 16:28:21 +1100

    its a PR race

--- packages/database/src/migrations/20210324043611-AddProjectSupplyChainFiji-modifies-data.js
+++ packages/database/src/migrations/20210324043611-AddProjectSupplyChainFiji-modifies-data.js
@@ -24,6 +24,11 @@ const DASHBOARDGROUP_CODE = 'FJ_COVID_19';
 const DASHBOARDGROUP_COUNTRY_CODE = 'FJ_Covid_Fiji_Country';
 const USER_GROUP = 'Donor';
 
+const IMAGE_URL =
+  'https://tupaia.s3-ap-southeast-2.amazonaws.com/uploads/supplychain_fiji_background.png';
+
+const LOGO_URL = 'https://tupaia.s3-ap-southeast-2.amazonaws.com/uploads/supplychain_fiji_logo.png';
+
 export const hierarchyNameToId = async (db, name) => {
   const record = await db.runSql(`SELECT id FROM entity_hierarchy WHERE name = '${name}'`);
   return record.rows[0] && record.rows[0].id;
@@ -70,11 +75,11 @@ exports.up = async function (db) {
     code: PROJECT_CODE,
     description: 'Strengthening health supply chains in Fiji',
     sort_order: 12,
-    // image_url: 'https://tupaia.s3-ap-southeast-2.amazonaws.com/uploads/covid_samoa_background.png',
+    image_url: IMAGE_URL,
     default_measure: '126,171',
     dashboard_group_name: DASHBOARDGROUP_NAME,
     user_groups: `{${USER_GROUP}}`,
-    // logo_url: 'https://tupaia.s3-ap-southeast-2.amazonaws.com/uploads/covid_samoa_logo.png',
+    logo_url: LOGO_URL,
     entity_id: await codeToId(db, 'entity', PROJECT_CODE),
     entity_hierarchy_id: await hierarchyNameToId(db, PROJECT_CODE),
   });
