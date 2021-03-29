d8a91e968c3e790465f0e63b9e1462f7e3367e8b
Rohan Port <rohan@beyondessential.com.au>
2021-03-29 18:01:05 +1100

    2407: Removed unneeded fields from Request object in lesmis-server

--- packages/lesmis-server/src/@types/express/index.d.ts
+++ packages/lesmis-server/src/@types/express/index.d.ts
@@ -2,14 +2,12 @@
  * Tupaia
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
-import { Context, SessionCookie } from '@tupaia/server-boilerplate';
-import { LesmisSessionModel, LesmisSessionType } from '../../models';
+import { Context } from '@tupaia/server-boilerplate';
+import { LesmisSessionType } from '../../models';
 
 declare global {
   namespace Express {
     export interface Request {
-      sessionModel: LesmisSessionModel;
-      sessionCookie: SessionCookie;
       session: LesmisSessionType;
       ctx: Context;
     }
