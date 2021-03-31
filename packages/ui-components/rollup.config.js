1e8f902e3809d0dea5ce51503750184e52181405
Tom Caiger <caigertom@gmail.com>
2021-03-31 14:34:44 +1300

    split rollup build

--- packages/ui-components/rollup.config.js
+++ packages/ui-components/rollup.config.js
@@ -9,11 +9,32 @@ import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import analyze from 'rollup-plugin-analyzer';
 import babel from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
 import svg from 'rollup-plugin-svg';
 import pkg from './package.json';
+
+const plugins = [
+  builtins(),
+  external(),
+  json({
+    compact: true,
+  }),
+  svg(),
+  babel({
+    exclude: 'node_modules/**',
+    configFile: './.babelrc.js',
+  }),
+  commonjs(),
+  analyze({ summaryOnly: true }),
+];
+
+const externals = [
+  ...Object.keys(pkg.devDependencies || {}),
+  ...Object.keys(pkg.dependencies || {}),
+  /@material-ui/,
+  /date-fns/,
+];
 
 export default [
   {
@@ -22,22 +43,25 @@ export default [
       { file: pkg.main, format: 'cjs', plugins: [terser()] },
       { file: pkg.module, format: 'esm', plugins: [terser()] },
     ],
-    plugins: [
-      builtins(),
-      nodeResolve(),
-      external(),
-      json({
-        compact: true,
-      }),
-      svg(),
-      babel({
-        exclude: 'node_modules/**',
-        configFile: './.babelrc.js',
-      }),
-      commonjs(),
-      analyze({ summaryOnly: true }),
-      del({ targets: ['dist'] }),
+    plugins: [...plugins, del({ targets: ['dist'] })],
+    external: externals,
+  },
+  {
+    input: 'src/components/Chart/index.js',
+    output: [
+      { file: 'lib/chart/index.js', format: 'cjs', plugins: [terser()] },
+      { file: 'lib/chart/index.esm.js', format: 'esm', plugins: [terser()] },
     ],
-    external: [/node_modules/, '@tupaia/utils'],
+    plugins: [...plugins, del({ targets: ['lib/chart'] })],
+    external: externals,
+  },
+  {
+    input: 'src/components/map/index.js',
+    output: [
+      { file: 'lib/map/index.js', format: 'cjs', plugins: [terser()] },
+      { file: 'lib/map/index.esm.js', format: 'esm', plugins: [terser()] },
+    ],
+    plugins: [...plugins, del({ targets: ['lib/map'] })],
+    external: externals,
   },
 ];
