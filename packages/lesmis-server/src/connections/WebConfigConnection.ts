e442da4e60997013defcb50b5143e100211389aa
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-04-09 13:42:53 +1200

    Update WebConfigConnection to use session switching functionality

--- packages/lesmis-server/src/connections/WebConfigConnection.ts
+++ packages/lesmis-server/src/connections/WebConfigConnection.ts
@@ -3,10 +3,11 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
-import { ApiConnection, QueryParameters } from '@tupaia/server-boilerplate';
+import { QueryParameters } from '@tupaia/server-boilerplate';
+import { SessionHandlingApiConnection } from './SessionHandlingApiConnection';
 const { WEB_CONFIG_API_URL = 'http://localhost:8000/api/v1' } = process.env;
 
-export class WebConfigConnection extends ApiConnection {
+export class WebConfigConnection extends SessionHandlingApiConnection {
   baseUrl = WEB_CONFIG_API_URL;
 
   async fetchDashboardReport(query: QueryParameters) {
