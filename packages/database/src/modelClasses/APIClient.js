cffbd803de22e242c75a4416b185b56b6eca0728
Edwin <edmofro@gmail.com>
2021-04-13 12:12:39 +1200

    Move getUser util to api client database model

--- packages/database/src/modelClasses/APIClient.js
+++ packages/database/src/modelClasses/APIClient.js
@@ -10,6 +10,23 @@ import { TYPES } from '../types';
 
 class APIClientType extends DatabaseType {
   static databaseType = TYPES.API_CLIENT;
+
+  async getUser() {
+    const userId = this.user_account_id;
+
+    // If the api client is not associated with a user, e.g. meditrak api client,
+    // the consuming function should auth the user separately
+    if (!userId) return null;
+
+    const user = await this.otherModels.user.findOne({ id: userId });
+    if (!user) {
+      // This isn't an authentication error - the client has provided the correct
+      // name and key, but the api client record has an invalid user ID attached!
+      throw new DatabaseError('API user does not exist');
+    }
+
+    return user;
+  }
 }
 
 export class APIClientModel extends DatabaseModel {
