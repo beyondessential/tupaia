ba33e6d02aa3cca5efa5b8284444274186f53137
Tom Caiger <caigertom@gmail.com>
2021-04-14 16:03:26 +1200

    Update MapOverlaysLoader.js

--- packages/lesmis/src/components/MapOverlaysPanel/MapOverlaysLoader.js
+++ packages/lesmis/src/components/MapOverlaysPanel/MapOverlaysLoader.js
@@ -7,13 +7,25 @@ import React from 'react';
 import Skeleton from '@material-ui/lab/Skeleton';
 import Box from '@material-ui/core/Box';
 
-const BigSkeleton = () => <Skeleton animation="wave" height={36} style={{ marginBottom: 18 }} />;
-const SmallSkeleton = () => <Skeleton animation="wave" height={32} />;
+const BigSkeleton = () => (
+  <Box display="flex" alignItems="center" mb={2}>
+    <Skeleton animation="wave" height={40} width={30} style={{ marginRight: 10 }} />
+    <Skeleton animation="wave" height={40} width={200} />
+  </Box>
+);
+
+const SmallSkeleton = () => (
+  <Box display="flex" alignItems="center" mb={1}>
+    <Skeleton animation="wave" height={30} width={30} style={{ marginRight: 10 }} />
+    <Skeleton animation="wave" height={30} width={300} />
+  </Box>
+);
+
 export const MapOverlaysLoader = () => (
-  <Box pl={3} pr={5} py={2}>
+  <Box pl={3} pr={2} py={2}>
     <BigSkeleton />
-    <Skeleton animation="wave" height={36} />
-    <Box pl={4} pr={5} pt={1} pb={3}>
+    <BigSkeleton />
+    <Box pl={3} pr={5} pb={2}>
       <SmallSkeleton />
       <SmallSkeleton />
       <SmallSkeleton />
