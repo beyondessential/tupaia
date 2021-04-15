68e7a4636989edc573697ea691ded6217f03cb04
Tom Caiger <caigertom@gmail.com>
2021-04-15 13:06:56 +1200

    add login verification to lesmis

---
+++ packages/lesmis-server/src/utils/verifyLoginAccess.ts
@@ -0,0 +1,17 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ *
+ */
+import { AccessPolicy } from '@tupaia/access-policy';
+import { PermissionsError } from '@tupaia/utils';
+import { LESMIS_PERMISSION_GROUP } from '../constants';
+
+export const verifyLoginAccess = (accessPolicy: AccessPolicy) => {
+  const hasAccess = new AccessPolicy(accessPolicy.policy).allowsAnywhere(LESMIS_PERMISSION_GROUP);
+  if (!hasAccess) {
+    throw new PermissionsError('Your permissions for Tupaia do not allow you to login to LESMIS');
+  }
+
+  return hasAccess;
+};
