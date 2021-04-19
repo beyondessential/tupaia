c4b5d8d556a092ba5dc2fcb6805a57563e09967e
Tom Caiger <caigertom@gmail.com>
2021-04-20 08:56:04 +1200

    update use entities data fetching

--- packages/lesmis/src/api/queries/useEntitiesData.js
+++ packages/lesmis/src/api/queries/useEntitiesData.js
@@ -4,11 +4,30 @@
  *
  */
 import { useQuery } from 'react-query';
+import keyBy from 'lodash.keyby';
 import { get } from '../api';
+import { PROJECT_CODE } from '../../constants';
 
-export const useEntitiesData = () =>
-  useQuery('entities', () => get('entities'), {
-    staleTime: 1000 * 60 * 60 * 1,
-    refetchOnWindowFocus: false,
-    retry: 1,
-  });
+const DEFAULT_PARAMS = { includeRootEntity: true };
+
+export const useEntitiesData = (entityCode, params = DEFAULT_PARAMS) => {
+  const query = useQuery(
+    ['entities', entityCode],
+    () => get(`entities/${entityCode}`, { params }),
+    {
+      staleTime: 1000 * 60 * 60 * 1,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  );
+
+  const entitiesByCode = keyBy(query.data, 'code');
+  return { ...query, entitiesByCode };
+};
+
+const DEFAULT_PROJECT_PARAMS = {
+  fields: 'id,child_codes,code,country_code,image_url,name,type,parent_code',
+};
+
+export const useProjectEntitiesData = (params = DEFAULT_PROJECT_PARAMS) =>
+  useEntitiesData(PROJECT_CODE, params);
