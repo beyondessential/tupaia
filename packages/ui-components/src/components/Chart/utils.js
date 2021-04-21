7b97cb9659351c1f6e22953fab84c1e8ff60b17d
Tom Caiger <caigertom@gmail.com>
2021-04-21 15:26:24 +1200

    make new styled tooltips and axes

--- packages/ui-components/src/components/Chart/utils.js
+++ packages/ui-components/src/components/Chart/utils.js
@@ -4,6 +4,7 @@
  */
 
 import moment from 'moment';
+import { useTheme } from '@material-ui/core/styles';
 import { GRANULARITY_CONFIG } from './periodGranularities';
 
 export const isMobile = () => process.env.REACT_APP_APP_TYPE === 'mobile';
@@ -16,3 +17,8 @@ export const getIsTimeSeries = data => data && data.length > 0 && data[0].timest
 
 export const isDataKey = key =>
   !(['name', 'timestamp'].includes(key) || key.substr(-9) === '_metadata');
+
+export const getContrastTextColor = () => {
+  const theme = useTheme();
+  return theme.palette.contrastText || theme.palette.text.primary;
+};
