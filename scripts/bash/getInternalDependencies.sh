ebe33ffeec9d9a74a7e23ad250f57262b2e85bd3
Edwin <edmofro@gmail.com>
2021-04-29 10:12:50 +1200

    Select package.json relative to calling directory

--- scripts/bash/getInternalDependencies.sh
+++ scripts/bash/getInternalDependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-DIR=$(dirname "$0")
 package_json_path=$1
 
 # if no package.json entrypoint is specified, just return all internal dependencies
@@ -11,7 +10,7 @@ fi
 
 # we are getting internal dependencies for a specific package.json
 # recursively build up an array of all internal dependencies this package depends on
-internal_dependencies=$(grep -o '@tupaia/[^"]*": "[0-9\.]*"' "${DIR}/${package_json_path}" | cut -d / -f 2 | cut -d \" -f 1)
+internal_dependencies=$(grep -o '@tupaia/[^"]*": "[0-9\.]*"' "${PWD}/${package_json_path}" | cut -d / -f 2 | cut -d \" -f 1)
 echo $internal_dependencies
 exit 0
 
