69fb744a507b4c8aa51cc54f18f289db7a71e92b
Edwin <edmofro@gmail.com>
2021-04-29 10:18:58 +1200

    Add clarifying comments to post clone script

--- packages/meditrak-app/appcenter-post-clone.sh
+++ packages/meditrak-app/appcenter-post-clone.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-# This file runs on appcenter automated builds
+## This file runs on appcenter automated builds
 
-# Move environment variables saved by appcenter as USER-DEFINED_VARIABLE_NAME into .env, ready for react-native-dotenv
+## Move environment variables saved by appcenter as USER-DEFINED_VARIABLE_NAME into .env, ready for react-native-dotenv
 echo "Setting up environment variables"
 env | grep "USER-DEFINED_.*" | awk -F "USER-DEFINED_" '{print $2}' > .env
+
+## Although appcenter generally automatically installs dependencies, it doesn't handle mono-repo
+## internal dependencies (see https://github.com/microsoft/appcenter/issues/278)
+## To manage that, we here manually install external deps, build internal deps, then redirect the
+## package.json entries to the internal filesystem
 
 # move to the root folder
 cd ../..
@@ -18,6 +23,6 @@ cd packages/meditrak-app
 # build internal dependencies of meditrak
 ../../scripts/bash/buildInternalDependencies.sh --packageJsonPath ./package.json
 
-# fix local dependencies
+# redirect package.json entries for internal dependencies to look locally
 node scripts/fixInternalDepsAppcenter.js
 
