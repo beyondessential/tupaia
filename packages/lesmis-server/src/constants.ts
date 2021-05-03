e0890d8115059c92559b498ab2f70b0f31b3bec2
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-05-03 13:52:21 +1200

    Update constants file to use new user group name

--- packages/lesmis-server/src/constants.ts
+++ packages/lesmis-server/src/constants.ts
@@ -4,6 +4,6 @@
  *
  */
 
-export const LESMIS_PERMISSION_GROUP = 'Laos Schools User';
+export const LESMIS_PERMISSION_GROUP = 'LESMIS Public';
 export const LESMIS_PROJECT_NAME = 'laos_schools';
 export const LESMIS_COUNTRY_ENTITY_CODE = 'LA';
