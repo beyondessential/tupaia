09137d62cad2e8c4cbc8fa1cc85f751f6bec7bf7
Tom Caiger <caigertom@gmail.com>
2021-05-04 15:54:46 +1200

    refactor query date utils

--- packages/lesmis/src/api/queries/utils.js
+++ packages/lesmis/src/api/queries/utils.js
@@ -13,8 +13,8 @@ import { MIN_DATA_YEAR, SINGLE_YEAR_GRANULARITY } from '../../constants';
  */
 export const yearToApiDates = year => {
   const currentYear = new Date().getFullYear().toString();
-  const startYear = !year ? MIN_DATA_YEAR : year;
-  const endYear = !year ? currentYear : year;
+  const startYear = year || MIN_DATA_YEAR;
+  const endYear = year || currentYear;
   const { startDate, endDate } = roundStartEndDates(SINGLE_YEAR_GRANULARITY, startYear, endYear);
   return { startDate: formatDateForApi(startDate), endDate: formatDateForApi(endDate) };
 };
