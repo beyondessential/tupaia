79c9f89be3bb805614e9d1daa19526f7be9f6608
Edwin <edmofro@gmail.com>
2021-05-06 11:25:57 +1200

    Remove funny and useless entity data initialisation

--- packages/lesmis/src/api/queries/useEntityData.js
+++ packages/lesmis/src/api/queries/useEntityData.js
@@ -3,11 +3,10 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  *
  */
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { get } from '../api';
 
 export const useEntityData = entityCode => {
-  const queryClient = useQueryClient();
   return useQuery(
     ['entity', entityCode],
     () => {
@@ -16,11 +15,6 @@ export const useEntityData = entityCode => {
     {
       staleTime: 1000 * 60 * 60 * 1,
       refetchOnWindowFocus: false,
-      // pre-populate the query data from the cached entities data if it's available
-      initialData: () =>
-        queryClient
-          .getQueryData('entities')
-          ?.find(entity => entity.code === entityCode && !!entity.bounds), // check the entity exists and isn't "lean"/geo-data free
     },
   );
 };
