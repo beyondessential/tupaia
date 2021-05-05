2669e5518b331337d3063e4a97b87c67d50c682e
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-05-05 16:16:31 +1200

    Hard code country vitals for now

--- packages/lesmis/src/views/DashboardView.js
+++ packages/lesmis/src/views/DashboardView.js
@@ -182,13 +182,13 @@ const CountryView = ({ vitals }) => {
         </TitleContainer>
         <EntityVitalsItem
           name="No. Schools"
-          value={vitals.NumberOfSchools}
+          value="13849"
           icon="School"
           isLoading={vitals.isLoading}
         />
         <EntityVitalsItem
           name="No. Students"
-          value={vitals.NumberOfStudents}
+          value="1659117"
           icon="Study"
           isLoading={vitals.isLoading}
         />
