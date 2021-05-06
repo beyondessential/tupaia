4664102da36d17a43fe6ef8525e20c3576068e9a
billli0 <lbit123@outlook.com>
2021-05-06 12:20:02 +1000

    dataBuilder totalNumOfPassengerByFlightDate

---
+++ packages/web-config-server/src/apiV1/dataBuilders/modules/covid-samoa/totalNumOfPassengerByFlightDate.js
@@ -0,0 +1,55 @@
+/**
+ * Tupaia Config Server
+ * Copyright (c) 2019 Beyond Essential Systems Pty Ltd
+ */
+import moment from 'moment';
+import { DataBuilder } from '/apiV1/dataBuilders/DataBuilder';
+import { Flight, getTotalNumPassengers, FLIGHT_DATE } from './flight';
+
+export class TotalNumOfPassengerByFlightDate extends DataBuilder {
+  async build() {
+    const dataElementCodes = [FLIGHT_DATE];
+    const { startDate, endDate } = this.query;
+    this.query.startDate = undefined;
+    this.query.endDate = undefined;
+    const events = await this.fetchEvents({ useDeprecatedApi: false, dataElementCodes });
+    const flights = Flight.fromEvents(events);
+    const flightsWithinPeriod = Flight.filterFlightByPeriod(flights, startDate, endDate);
+
+    return this.buildData(flightsWithinPeriod);
+  }
+
+  buildData = flights => {
+    const data = [];
+
+    flights.sort((a, b) => (a.date < b.date ? -1 : 1));
+
+    for (const flight of flights) {
+      const formattedDate = moment(flight.date).format('Do MMMM');
+      const value = getTotalNumPassengers(flight);
+      data.push({
+        name: formattedDate,
+        value,
+      });
+    }
+
+    return { data };
+  };
+}
+
+export const totalNumOfPassengerByFlightDate = async (
+  { models, dataBuilderConfig, query, entity },
+  aggregator,
+  dhisApi,
+) => {
+  const builder = new TotalNumOfPassengerByFlightDate(
+    models,
+    aggregator,
+    dhisApi,
+    dataBuilderConfig,
+    query,
+    entity,
+    dataBuilderConfig.aggregationType,
+  );
+  return builder.build();
+};
