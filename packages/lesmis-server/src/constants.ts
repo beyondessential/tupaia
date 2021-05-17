eefb4d8877805af5fee822ecf8ee4f4d3e8421ea
Tom Caiger <caigertom@gmail.com>
2021-05-17 12:49:33 +1200

    set project to fanfana for testing

--- packages/lesmis-server/src/constants.ts
+++ packages/lesmis-server/src/constants.ts
@@ -3,7 +3,7 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  *
  */
-
 export const LESMIS_PERMISSION_GROUP = 'LESMIS Public';
-export const LESMIS_PROJECT_NAME = 'laos_schools';
-export const LESMIS_COUNTRY_ENTITY_CODE = 'LA';
+// @Todo: Set back to lesmis
+export const LESMIS_PROJECT_NAME = 'fanafana';
+export const LESMIS_COUNTRY_ENTITY_CODE = 'TO';
