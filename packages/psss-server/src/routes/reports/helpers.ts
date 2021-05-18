b37fc4bbe3b849674e1cc05d6c7989e867e3ce54
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-05-18 11:20:27 +1000

    Apply suggestions

--- packages/psss-server/src/routes/reports/helpers.ts
+++ packages/psss-server/src/routes/reports/helpers.ts
@@ -3,10 +3,10 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
-const SYNDROMES: any[] = ['AFR', 'DIA', 'DLI', 'ILI', 'PF'];
+import { SYNDROMES } from '../../constants';
 
 export const validateSyndrome = (syndrome: unknown) => {
-  if (!SYNDROMES.includes(syndrome)) {
+  if (!(typeof syndrome === 'string') || !SYNDROMES.includes(syndrome)) {
     throw new Error(`Invalid syndrome ${syndrome} provided, must be one of ${SYNDROMES}`);
   }
 };
