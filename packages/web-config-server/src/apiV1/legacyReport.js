77a8266081dc59d7e8099fbf1627f4012664468f
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-05-19 12:12:56 +1200

    Allow legacy reports to find drill downs

--- packages/web-config-server/src/apiV1/legacyReport.js
+++ packages/web-config-server/src/apiV1/legacyReport.js
@@ -10,8 +10,21 @@ export default class extends ViewRouteHandler {
   // TODO: Actually check permissions
   static PermissionsChecker = NoPermissionRequiredChecker;
 
-  fetchReport = async ({ itemCode }) => {
-    const dashboardItem = await this.models.dashboardItem.findOne({ code: itemCode, legacy: true });
+  fetchReport = async ({ itemCode, drillDownLevel }) => {
+    const baseDashboardItem = await this.models.dashboardItem.findOne({
+      code: itemCode,
+      legacy: true,
+    });
+    let dashboardItem = baseDashboardItem;
+    if (drillDownLevel) {
+      const drillDownCode = baseDashboardItem.config.drillDown?.itemCodes[drillDownLevel];
+      if (!drillDownCode) {
+        throw new Error(`No drill down exists for level ${drillDownLevel}`);
+      }
+      dashboardItem = await this.models.dashboardItem.findOne({
+        code: drillDownCode,
+      });
+    }
     const legacyReport = await this.models.legacyReport.findOne({ id: dashboardItem.report_code });
     return {
       viewJson: dashboardItem.config,
