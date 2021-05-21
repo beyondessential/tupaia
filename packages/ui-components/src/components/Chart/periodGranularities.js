635acbc8616cba5f63177a4ac9f3686eb911f5e4
Pauline <shoemaker.pauline@gmail.com>
2021-05-21 17:25:31 +1000

    Add country level trend graphs, overlaid years (#2503)
    * clean branch
    
    * moving lost file
    
    * Delete layerYearOnYear.test.js
    
    * remove unnecessary first attempt code
    
    * remove outdated aggregation code
    
    * move test
    
    * refactor chart config function chain
    
    * suggested syntax edits
    
    * Update packages/web-config-server/src/apiV1/dataBuilders/generic/analytics/analyticsYearOnYear.js
    
    Co-authored-by: Rohan Port <59544282+rohan-bes@users.noreply.github.com>
    
    * Update packages/web-config-server/src/apiV1/dataBuilders/generic/analytics/analyticsYearOnYear.js
    
    Co-authored-by: Rohan Port <59544282+rohan-bes@users.noreply.github.com>
    
    * Update packages/web-config-server/src/apiV1/dataBuilders/generic/analytics/analyticsYearOnYear.js
    
    Co-authored-by: Rohan Port <59544282+rohan-bes@users.noreply.github.com>
    
    * Update packages/web-config-server/src/apiV1/dataBuilders/generic/analytics/analyticsYearOnYear.js
    
    Co-authored-by: Rohan Port <59544282+rohan-bes@users.noreply.github.com>
    
    * fix import
    
    * fix unit tests
    
    Co-authored-by: Rohan Port <59544282+rohan-bes@users.noreply.github.com>

--- packages/ui-components/src/components/Chart/periodGranularities.js
+++ packages/ui-components/src/components/Chart/periodGranularities.js
@@ -213,8 +213,8 @@ const getDefaultDatesForSingleDateGranularities = (periodGranularity, defaultTim
     }
 
     // Grab all the details and get a single default date used for both start/end period.
-    const { offset, unit, modifier } = singleDateConfig;
-    startDate = getOffsetDate(offset, unit, modifier);
+    const { offset, unit, modifier, modifierUnit } = singleDateConfig;
+    startDate = getOffsetDate(offset, unit, modifier, modifierUnit);
     endDate = startDate;
   }
 
@@ -240,13 +240,13 @@ const getDefaultDatesForRangeGranularities = (periodGranularity, defaultTimePeri
     let endDate = startDate;
 
     if (defaultTimePeriod.start) {
-      const { offset, unit, modifier } = defaultTimePeriod.start;
-      startDate = getOffsetDate(offset, unit, modifier);
+      const { offset, unit, modifier, modifierUnit } = defaultTimePeriod.start;
+      startDate = getOffsetDate(offset, unit, modifier, modifierUnit);
     }
 
     if (defaultTimePeriod.end) {
-      const { offset, unit, modifier } = defaultTimePeriod.end;
-      endDate = getOffsetDate(offset, unit, modifier);
+      const { offset, unit, modifier, modifierUnit } = defaultTimePeriod.end;
+      endDate = getOffsetDate(offset, unit, modifier, modifierUnit);
     }
 
     return roundStartEndDates(periodGranularity, startDate, endDate);
@@ -322,8 +322,8 @@ export function getLimits(periodGranularity, limits) {
       );
     }
 
-    const { offset, unit, modifier } = partConfig;
-    const offsetDate = getOffsetDate(offset, unit, modifier);
+    const { offset, unit, modifier, modifierUnit } = partConfig;
+    const offsetDate = getOffsetDate(offset, unit, modifier, modifierUnit);
     if (partKey === 'start') startDate = offsetDate;
     if (partKey === 'end') endDate = offsetDate;
   }
