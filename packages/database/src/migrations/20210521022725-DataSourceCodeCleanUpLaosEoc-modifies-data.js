6d725b86a13665ec458f3570030cd9a09924c88d
billli0 <lbit123@outlook.com>
2021-05-21 12:39:51 +1000

    fix migration

--- packages/database/src/migrations/20210521022725-DataSourceCodeCleanUpLaosEoc-modifies-data.js
+++ packages/database/src/migrations/20210521022725-DataSourceCodeCleanUpLaosEoc-modifies-data.js
@@ -38,6 +38,7 @@ const DATA_SOURCES = {
   ...dengueNoWarningDeath,
   ...dengueWithWarningDeath,
   ...dengueSevereDeath,
+  ...malariaCase,
   ...malariaDeath,
   ...measlesCase,
   ...measlesDeath,
