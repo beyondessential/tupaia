bac6340c68a0f2060c97c88d26388b29d16caf61
billli0 <lbit123@outlook.com>
2021-05-27 15:29:38 +1000

    migration initial

---
+++ packages/database/src/migrations/20210527034457-StriveLarvalHabitatBySpeciesBarChart-modifies-data.js
@@ -0,0 +1,115 @@
+'use strict';
+
+import { insertObject, generateId } from '../utilities';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+const DASHBOARD_GROUP_CODE = 'PG_Strive_PNG_Country';
+
+const SPECIES = [
+  'Ae. aegypti',
+  'Ae. albopictus',
+  'Ae. scutellaris',
+  'Aedes Other',
+  'Aedes spp.',
+  'An. farauti',
+  'An. koliensis',
+  'An. longirostris',
+  'An. punctulatus',
+  'Culex spp.',
+  'Cx. annulirostris',
+  'Cx. quinquefasciatus',
+  'Cx. sitiens',
+  'Mansoni spp.',
+];
+
+const COLOURS = [
+  '#e22709',
+  '#0df3e4',
+  '#fa6131',
+  '#9bbf87',
+  '#3fc399',
+  '#28d530',
+  '#4549a6',
+  '#c3d146',
+  '#67a6d1',
+  '#4e9469',
+  '#e617d4',
+  '#d84876',
+  '#4744cb',
+  '#b89fbd',
+];
+
+const BASE_DASHBOARD_REPORT = {
+  id: 'PG_Strive_Habitat_By_Species',
+  dataBuilder: 'reportServer',
+  dataServices: [
+    {
+      isDataRegional: true,
+    },
+  ],
+};
+
+const DATA_BUILDER_CONFIG = {
+  reportCode: 'STRIVE_Habitat_By_Species_LHS',
+};
+
+const getChartConfig = () => {
+  const chartConfig = {};
+  SPECIES.forEach((specie, index) => {
+    chartConfig[specie] = { color: COLOURS[index], stackId: 1, legendOrder: index + 1 };
+  });
+  return chartConfig;
+};
+
+const VIEW_JSON = {
+  name: 'Larval Habitat by Species',
+  type: 'chart',
+  chartType: 'bar',
+  chartConfig: getChartConfig(),
+};
+
+exports.up = async function (db) {
+  await insertObject(db, 'dashboardReport', {
+    ...BASE_DASHBOARD_REPORT,
+    dataBuilderConfig: DATA_BUILDER_CONFIG,
+    viewJson: VIEW_JSON,
+  });
+
+  return db.runSql(`
+    UPDATE
+      "dashboardGroup"
+    SET
+      "dashboardReports" = "dashboardReports" || '{ ${BASE_DASHBOARD_REPORT.id} }'
+    WHERE
+      "code" = '${DASHBOARD_GROUP_CODE}';
+  `);
+};
+
+exports.down = async function (db) {
+  return db.runSql(`
+  DELETE FROM "dashboardReport" WHERE id = '${BASE_DASHBOARD_REPORT.id}';
+  UPDATE
+    "dashboardGroup"
+  SET
+    "dashboardReports" = array_remove("dashboardReports", '${BASE_DASHBOARD_REPORT.id}')
+  WHERE
+    "code" = '${DASHBOARD_GROUP_CODE}';
+`);
+};
+
+exports._meta = {
+  version: 1,
+};
---
+++ packages/database/src/migrations/20210527034536-ReportConfigLarvalHabitatBySpecies-modifies-data.js
@@ -0,0 +1,72 @@
+'use strict';
+
+import { generateId, insertObject } from '../utilities';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+const permissionGroupNameToId = async (db, name) => {
+  const record = await db.runSql(`SELECT id FROM permission_group WHERE name = '${name}'`);
+  return record.rows[0] && record.rows[0].id;
+};
+
+const REPORT = {
+  id: generateId(),
+  code: 'STRIVE_Habitat_By_Species_LHS',
+  config: {
+    fetch: {
+      dataGroups: ['SI'],
+      aggregations: [
+        {
+          type: 'RAW',
+          config: {
+            dataSourceEntityType: 'facility',
+          },
+        },
+      ],
+      dataElements: ['STRVEC_AE_IR09', 'STRVEC_AE_IR02'],
+    },
+    transform: [
+      {
+        transform: 'filter',
+        where: "exists($row['STRVEC_AE_IR09'],$row['STRVEC_AE_IR02'])",
+      },
+      {
+        transform: 'select',
+        "'species'": '$row.STRVEC_AE_IR09',
+        "'name'": '$row.STRVEC_AE_IR02',
+      },
+      {
+        transform: 'aggregate',
+        species: 'countByRadio',
+        name: 'group',
+      },
+    ],
+  },
+};
+
+exports.up = async function (db) {
+  const permissionGroupId = await permissionGroupNameToId(db, 'STRIVE User');
+  await insertObject(db, 'report', { ...REPORT, permission_group_id: permissionGroupId });
+};
+
+exports.down = async function (db) {
+  return db.runSql(`
+   DELETE FROM "report" WHERE id = '${REPORT.id}';
+  `);
+};
+
+exports._meta = {
+  version: 1,
+};
