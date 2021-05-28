7704039546368b9f353b94dd10a5f3f503af23c4
billli0 <lbit123@outlook.com>
2021-05-28 13:35:22 +1000

    Revert "count by radio"
    This reverts commit 96b3cfd07db08466a0480455e6dd84b790692763.

--- packages/report-server/src/reportBuilder/transform/functions/aggregate/aggregate.ts
+++ packages/report-server/src/reportBuilder/transform/functions/aggregate/aggregate.ts
@@ -66,11 +66,9 @@ const getAggregatedRows = (groupRowFields: GroupRowFields, params: AggregatePara
       const aggregation = params.getFieldAggregation(fieldKey);
       const aggregatedValue = aggregations[aggregation](fieldValue);
       if (aggregatedValue !== undefined) {
-        const formattedValue =
-          typeof aggregatedValue === 'object' ? aggregatedValue : { [fieldKey]: aggregatedValue };
         aggregatedRowFields[groupKey] = {
           ...aggregatedRowFields[groupKey],
-          ...formattedValue,
+          [fieldKey]: aggregatedValue,
         };
       }
     });
--- packages/report-server/src/reportBuilder/transform/functions/aggregate/aggregations.ts
+++ packages/report-server/src/reportBuilder/transform/functions/aggregate/aggregations.ts
@@ -5,10 +5,6 @@
 
 import { FieldValue } from '../../../types';
 
-interface CountByRadioType {
-  [field: string]: number;
-}
-
 const isNotUndefined = <T>(value: T): value is Exclude<T, undefined> => {
   return value !== undefined;
 };
@@ -54,19 +50,6 @@ const avg = (values: FieldValue[]): number | undefined => {
 
 const count = (values: FieldValue[]): number => {
   return values.length;
-};
-
-const countByRadio = (values: FieldValue[]): CountByRadioType | undefined => {
-  if (values.length !== 0) {
-    const groupedValues: CountByRadioType = {};
-    values.forEach(value => {
-      const valueInString = String(value);
-      groupedValues[valueInString] ??= 0;
-      groupedValues[valueInString]++;
-    });
-    return groupedValues;
-  }
-  return undefined;
 };
 
 // helper of max() and min(), e.g.: customSort([1, 2, 3, null]) = [null, 1, 2, 3]
@@ -117,7 +100,6 @@ export const aggregations = {
   sum,
   avg,
   count,
-  countByRadio,
   max,
   min,
   unique,
