a3124d5b93b5a2bca46cab4ba5990519d94dc024
Edwin <edmofro@gmail.com>
2021-05-31 08:50:11 +1200

    Add utility script for running analytics full refresh

---
+++ packages/data-api/scripts/fullRefreshAnalyticsTable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source .env
+cd scripts
+export PGPASSWORD=$DB_MV_PASSWORD
+psql -h $DB_URL -d $DB_NAME -U $DB_MV_USER -f dropAnalyticsTableIndexes.sql
+export PGPASSWORD=$DB_PASSWORD
+psql -h $DB_URL -d $DB_NAME -U $DB_USER -c "SELECT mv\$refreshMaterializedView('analytics', 'public');"
+export PGPASSWORD=$DB_MV_PASSWORD
+psql -h $DB_URL -d $DB_NAME -U $DB_MV_USER -f createAnalyticsTableIndexes.sql
