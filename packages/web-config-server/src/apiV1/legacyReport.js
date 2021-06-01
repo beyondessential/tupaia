05a5a1f3f31fba92e32995b38304f19cb33897f4
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-06-01 16:37:20 +1200

    Remove legacy report drilldown using array

--- packages/web-config-server/src/apiV1/legacyReport.js
+++ packages/web-config-server/src/apiV1/legacyReport.js
@@ -9,21 +9,11 @@ import { DashboardItemPermissionChecker } from './permissions';
 export default class extends ViewRouteHandler {
   static PermissionsChecker = DashboardItemPermissionChecker;
 
-  fetchReport = async ({ itemCode, drillDownLevel }) => {
-    const baseDashboardItem = await this.models.dashboardItem.findOne({
+  fetchReport = async ({ itemCode }) => {
+    const dashboardItem = await this.models.dashboardItem.findOne({
       code: itemCode,
       legacy: true,
     });
-    let dashboardItem = baseDashboardItem;
-    if (drillDownLevel) {
-      const drillDownCode = baseDashboardItem.config.drillDown?.itemCodes[drillDownLevel];
-      if (!drillDownCode) {
-        throw new Error(`No drill down exists for level ${drillDownLevel}`);
-      }
-      dashboardItem = await this.models.dashboardItem.findOne({
-        code: drillDownCode,
-      });
-    }
     const legacyReport = await this.models.legacyReport.findOne({ id: dashboardItem.report_code });
     return {
       viewJson: dashboardItem.config,
