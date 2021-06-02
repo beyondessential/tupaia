4a9a8ee5f66fdd9d893a1a2bafc2fb55f35c85d8
Tom Caiger <caigertom@gmail.com>
2021-06-03 08:08:07 +1200

    Update 20210602041852-Rename-lesmis-permission-groups-modifies-data.js

--- packages/database/src/migrations/20210602041852-Rename-lesmis-permission-groups-modifies-data.js
+++ packages/database/src/migrations/20210602041852-Rename-lesmis-permission-groups-modifies-data.js
@@ -14,7 +14,7 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const updateLESMISReportPermissionGroup = function (db, oldName, newName) {
+const updatePermissionGroupName = function (db, oldName, newName) {
   return db.runSql(`
   UPDATE "permission_group"
   SET "name" = '${newName}'
@@ -22,13 +22,28 @@ const updateLESMISReportPermissionGroup = function (db, oldName, newName) {
   `);
 };
 
-exports.up = async function (db) {
-  await updateLESMISReportPermissionGroup(db, 'Laos Schools Admin', 'LESMIS Admin');
-  await updateLESMISReportPermissionGroup(db, 'Laos Schools Super User', 'LESMIS User');
+const updatePermissionGroupParent = function (db, name, newParentId) {
+  return db.runSql(`
+  UPDATE "permission_group"
+  SET "parent_id" = '${newParentId}'
+  WHERE "name" = '${name}'
+  `);
 };
 
-exports.down = function (db) {
-  return null;
+exports.up = async function (db) {
+  await updatePermissionGroupName(db, 'Laos Schools Admin', 'LESMIS Admin');
+  await updatePermissionGroupName(db, 'Laos Schools Super User', 'LESMIS User');
+
+  const LESMIS_ADMIN_ID = '607e36bf61f76a40c6000a23';
+  await updatePermissionGroupParent(db, 'LESMIS User', LESMIS_ADMIN_ID);
+};
+
+exports.down = async function (db) {
+  await updatePermissionGroupName(db, 'LESMIS Admin', 'Laos Schools Admin');
+  await updatePermissionGroupName(db, 'LESMIS User', 'Laos Schools Super User');
+
+  const ADMIN_ID = '59085f2dfc6a0715dae508e3';
+  await updatePermissionGroupParent(db, 'Laos Schools Super User', ADMIN_ID);
 };
 
 exports._meta = {
