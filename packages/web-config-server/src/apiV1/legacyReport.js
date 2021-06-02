e70fb6958c7efc132bcb106fbeff77cb464fb18e
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-06-02 13:09:33 +1200

    Better error messaging

--- packages/web-config-server/src/apiV1/legacyReport.js
+++ packages/web-config-server/src/apiV1/legacyReport.js
@@ -14,7 +14,13 @@ export default class extends ViewRouteHandler {
       code: itemCode,
       legacy: true,
     });
+    if (!dashboardItem) {
+      throw new Error(`No dashboard item exists with the code ${itemCode}`);
+    }
     const legacyReport = await this.models.legacyReport.findOne({ id: dashboardItem.report_code });
+    if (!legacyReport) {
+      throw new Error(`No legacy report exists with id ${dashboardItem.report_code}`);
+    }
     return {
       viewJson: dashboardItem.config,
       dataBuilder: legacyReport.dataBuilder,
