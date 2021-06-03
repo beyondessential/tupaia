33e83c2f3a910c53b3b71c6500cbdd1d9a86f764
Tom Caiger <caigertom@gmail.com>
2021-06-03 14:53:00 +1200

    Update useValidatedQuery.js

--- packages/lesmis/src/api/queries/useValidatedQuery.js
+++ packages/lesmis/src/api/queries/useValidatedQuery.js
@@ -4,18 +4,11 @@
  *
  */
 import { useHistory } from 'react-router-dom';
-import { useUser } from './useUser';
 
 export const useValidatedQuery = query => {
   const history = useHistory();
-  const { isLoggedIn } = useUser();
-
   if (query.isError && query.error.code === 403) {
-    if (isLoggedIn) {
-      history.push('/page-not-found');
-    } else {
-      history.push('/login', { referer: history.location });
-    }
+    history.push('/page-not-found');
   }
   return query;
 };
