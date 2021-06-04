5a1c3f1ee69631a654eebeda3224e9278e4c6830
Tom Caiger <caigertom@gmail.com>
2021-06-04 15:56:04 +1200

    Update UserPermissionsCell.js

--- packages/lesmis/src/components/DataGrid/UserPermissionsCell.js
+++ packages/lesmis/src/components/DataGrid/UserPermissionsCell.js
@@ -3,12 +3,12 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  *
  */
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import moment from 'moment';
+import MuiSnackbar from '@material-ui/core/Snackbar';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import Tooltip from '@material-ui/core/Tooltip';
-import { Select } from '@tupaia/ui-components';
+import { Select, SmallAlert } from '@tupaia/ui-components';
 import { useUpdateUserEntityPermission } from '../../api/mutations/useUpdateUserEntityPermission';
 import { LESMIS_PERMISSION_GROUPS } from '../../constants';
 
@@ -28,44 +28,87 @@ const StyledSelect = styled(Select)`
     font-size: 0.875rem;
     font-weight: 500;
     line-height: 1rem;
+    padding-left: 0;
   }
 `;
 
 export const UserPermissionsCell = ({ value, row }) => {
-  const [activeValue, setActiveValue] = React.useState(value);
-  const { mutate: update, isLoading } = useUpdateUserEntityPermission();
-  const { userEntityPermissionId, createdAt } = row.original;
+  const [activeValue, setActiveValue] = useState(value);
+  const [isOpen, setIsOpen] = useState(false);
+  const { mutate, isLoading, isError, isSuccess } = useUpdateUserEntityPermission();
+  const { userEntityPermissionId } = row.original;
 
-  const handleChange = event => {
-    const newValue = event.target.value;
-    setActiveValue(newValue);
-    update({
-      userEntityPermissionId,
-      permissionGroupName: newValue,
-    });
+  const handleClose = () => {
+    setIsOpen(false);
   };
 
-  const date = moment(createdAt).format('D MMMM YYYY');
+  const handleChange = async event => {
+    const newValue = event.target.value;
+    setActiveValue(newValue);
+    mutate(
+      {
+        userEntityPermissionId,
+        permissionGroupName: newValue,
+      },
+      {
+        onSuccess: () => {
+          setIsOpen(true);
+        },
+        onError: () => {
+          setIsOpen(true);
+          setActiveValue(value);
+        },
+      },
+    );
+  };
 
   return (
-    <Tooltip title={date} arrow placement="top" disableFocusListener disableTouchListener>
-      <div>
-        <StyledSelect
-          id="permissions"
-          disabled={isLoading}
-          options={[
-            { label: 'Public', value: LESMIS_PERMISSION_GROUPS.PUBLIC },
-            { label: 'Senior User', value: LESMIS_PERMISSION_GROUPS.USER },
-            { label: 'Admin', value: LESMIS_PERMISSION_GROUPS.ADMIN },
-          ]}
-          onChange={handleChange}
-          value={activeValue}
-          showPlaceholder={false}
-          SelectProps={{
-            IconComponent: iconProps => <ArrowDropDownIcon {...iconProps} />,
-          }}
-        />
-      </div>
-    </Tooltip>
+    <>
+      <StyledSelect
+        id="permissions"
+        disabled={isLoading}
+        options={[
+          { label: 'Public', value: LESMIS_PERMISSION_GROUPS.PUBLIC },
+          { label: 'Senior User', value: LESMIS_PERMISSION_GROUPS.USER },
+          { label: 'Admin', value: LESMIS_PERMISSION_GROUPS.ADMIN },
+        ]}
+        onChange={handleChange}
+        value={activeValue}
+        showPlaceholder={false}
+        SelectProps={{
+          IconComponent: iconProps => <ArrowDropDownIcon {...iconProps} />,
+        }}
+      />
+      <MuiSnackbar
+        open={isOpen}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      >
+        <>
+          {isSuccess && (
+            <SmallAlert onClose={handleClose} severity="success">
+              Success
+            </SmallAlert>
+          )}
+          {isError && (
+            <SmallAlert onClose={handleClose} severity="error">
+              Error. Please click refresh and try again.
+            </SmallAlert>
+          )}
+        </>
+      </MuiSnackbar>
+    </>
   );
+};
+
+UserPermissionsCell.propTypes = {
+  value: PropTypes.string,
+  row: PropTypes.shape({
+    original: PropTypes.object,
+  }).isRequired,
+};
+
+UserPermissionsCell.defaultProps = {
+  value: null,
 };
