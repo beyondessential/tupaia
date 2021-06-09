29ed076e0830ec6ed8929b0c810165fe9814b64b
billli0 <lbit123@outlook.com>
2021-06-09 10:55:26 +1000

    apply suggestions

--- packages/database/src/migrations/20210607053528-AddPostcodeLevelDataInAU-modifies-data.js
+++ packages/database/src/migrations/20210607053528-AddPostcodeLevelDataInAU-modifies-data.js
@@ -1,8 +1,5 @@
 'use strict';
 
-import { generateId, codeToId, insertObject } from '../utilities';
-import POSTCODE_ARRAY from './migrationData/20210607053528-AddPostcodeLevelDataInAU.json';
-
 var dbm;
 var type;
 var seed;
@@ -17,59 +14,18 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const STATES = {
-  ACT: 'AU_Australian Capital Territory',
-  NT: 'AU_Northern Territory',
-  NSW: 'AU_New South Wales',
-  VIC: 'AU_Victoria',
-  QLD: 'AU_Queensland',
-  WA: 'AU_Western Australia',
-  TAS: 'AU_Tasmania',
-  SA: 'AU_South Australia',
-};
-
 const HIERARCHY_CODE = 'covidau';
 
 exports.up = async function (db) {
-  for (const data of POSTCODE_ARRAY) {
-    // Insert into entity table
-    const { postcode, state, long, lat } = data;
-    const entityCode = `AU_${state}_${postcode}`;
-    const isExisted = await codeToId(db, 'entity', entityCode);
-    // There are same postcodes for multiple area from JSON, but they have similar latitude and longitude, we can pick one of them.
-    if (isExisted) {
-      continue;
-    }
-    const entity = {
-      id: generateId(),
-      code: entityCode,
-      parent_id: await codeToId(db, 'entity', STATES[state]),
-      name: postcode,
-      type: 'postcode',
-      country_code: 'AU',
-      metadata: {
-        dhis: {
-          isDataRegional: true,
-        },
-        openStreetMaps: {},
-      },
-    };
-    const point = { type: 'Point', coordinates: [long, lat] };
-    await insertObject(db, 'entity', entity);
-    await db.runSql(`
-        update entity 
-        SET point = ST_Force2D(ST_GeomFromGeoJSON('${JSON.stringify(point)}'))
-        where code = '${entity.code}';
-    `);
-    // Insert relationships into entity_relation table
-    await db.runSql(
-      `UPDATE entity_hierarchy SET canonical_types = '{country, district, sub_district, postcode}' WHERE name = '${HIERARCHY_CODE}';`,
-    );
-  }
+  await db.runSql(
+    `UPDATE entity_hierarchy SET canonical_types = '{country, district, sub_district, postcode}' WHERE name = '${HIERARCHY_CODE}';`,
+  );
 };
 
-exports.down = function (db) {
-  return null;
+exports.down = async function (db) {
+  await db.runSql(
+    `UPDATE entity_hierarchy SET canonical_types = '{}' WHERE name = '${HIERARCHY_CODE}';`,
+  );
 };
 
 exports._meta = {
