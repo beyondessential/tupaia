1b71ea8347c18f9f6b8d9cbf75c8a0fc91f4f4df
billli0 <lbit123@outlook.com>
2021-06-10 16:13:02 +1000

    two indicators

--- packages/database/src/migrations/20210607020237-AddIndicatorsForAuFlutracking-modifies-data.js
+++ packages/database/src/migrations/20210607020237-AddIndicatorsForAuFlutracking-modifies-data.js
@@ -16,75 +16,98 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const INDICATORS = [
-  {
-    id: generateId(),
-    code: 'FluTracker_LGA_Percent_First_Nations_ILI',
-    builder: 'analyticArithmetic',
-    config: {
-      formula: 'firstExistingValue(FWV_LGA_ILI_Percentage_prior_2018, FWV_LGA_ILI_Percentage)',
-      parameters: [
-        {
-          builder: 'analyticArithmetic',
-          code: 'FWV_LGA_ILI_Percentage_prior_2018',
-          config: {
-            formula: 'FluTracking_NationalFNILI',
-            aggregation: [
-              {
-                type: 'FINAL_EACH_DAY',
-                config: {
-                  dataSourceEntityType: 'country',
-                },
+const INDICATOR = {
+  id: generateId(),
+  code: 'FluTracker_LGA_Percent_First_Nations_ILI',
+  builder: 'analyticArithmetic',
+  config: {
+    formula:
+      'firstExistingValue(FWV_LGA_ILI_Percentage_prior_March_2021, FWV_LGA_ILI_Percentage_Till_Current)',
+    parameters: [
+      {
+        builder: 'analyticArithmetic',
+        code: 'FWV_LGA_ILI_Percentage_prior_March_2021',
+        config: {
+          formula: 'FluTracking_NationalFNILI',
+          aggregation: ['FINAL_EACH_DAY'],
+        },
+      },
+      {
+        builder: 'analyticArithmetic',
+        code: 'FWV_LGA_ILI_Percentage_Till_Current',
+        config: {
+          formula:
+            '(FWV_LGA_004a_Total_By_Country >= 0 and FWV_LGA_003a_Total_By_Country > 0) ? (FWV_LGA_004a_Total_By_Country / FWV_LGA_003a_Total_By_Country * 100) : 0',
+          parameters: [
+            // Sum up 'FWV_LGA_004a' per period in national level as 'FWV_LGA_004a_Total_By_Country'
+            {
+              builder: 'analyticArithmetic',
+              code: 'FWV_LGA_004a_Total_By_Country',
+              config: {
+                formula: 'FWV_LGA_004a',
+                aggregation: [
+                  'FINAL_EACH_DAY',
+                  {
+                    type: 'SUM_PER_PERIOD_PER_ORG_GROUP',
+                    config: {
+                      dataSourceEntityType: 'sub_district',
+                      aggregationEntityType: 'country',
+                    },
+                  },
+                ],
               },
-            ],
+            },
+            // Sum up 'FWV_LGA_003a' per period in national level as 'FWV_LGA_003a_Total_By_Country'
+            {
+              builder: 'analyticArithmetic',
+              code: 'FWV_LGA_003a_Total_By_Country',
+              config: {
+                formula: 'FWV_LGA_003a',
+                aggregation: [
+                  'FINAL_EACH_DAY',
+                  {
+                    type: 'SUM_PER_PERIOD_PER_ORG_GROUP',
+                    config: {
+                      dataSourceEntityType: 'sub_district',
+                      aggregationEntityType: 'country',
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+          aggregation: [],
+          defaultValues: {
+            FWV_LGA_004a_Total_By_Country: 'undefined',
+            FWV_LGA_003a_Total_By_Country: 'undefined',
           },
         },
-        {
-          builder: 'analyticArithmetic',
-          code: 'FWV_LGA_ILI_Percentage',
-          config: {
-            formula: 'FWV_LGA_004a / FWV_LGA_003a',
-            aggregation: [
-              {
-                type: 'FINAL_EACH_DAY',
-                config: {
-                  dataSourceEntityType: 'sub_district',
-                  aggregationEntityType: 'requested',
-                },
-              },
-            ],
-          },
-        },
-      ],
-      aggregation: {
-        FWV_LGA_ILI_Percentage_prior_2018: 'FINAL_EACH_DAY',
-        FWV_LGA_ILI_Percentage: 'FINAL_EACH_DAY',
       },
-      defaultValues: {
-        FWV_LGA_ILI_Percentage_prior_2018: 'undefined', // to differentiate between no data and 0
-        FWV_LGA_ILI_Percentage: 'undefined',
-      },
+    ],
+    aggregation: {
+      FWV_LGA_ILI_Percentage_prior_March_2021: 'FINAL_EACH_DAY',
+      FWV_LGA_ILI_Percentage_Till_Current: 'FINAL_EACH_DAY',
+    },
+    defaultValues: {
+      FWV_LGA_ILI_Percentage_prior_March_2021: 'undefined',
+      FWV_LGA_ILI_Percentage_Till_Current: 'undefined',
     },
   },
-];
+};
 
 exports.up = async function (db) {
-  for (const indicator of INDICATORS) {
-    await insertObject(db, 'indicator', indicator);
-    await insertObject(db, 'data_source', {
-      id: generateId(),
-      code: indicator.code,
-      type: 'dataElement',
-      service_type: 'indicator',
-    });
-  }
+  await insertObject(db, 'indicator', INDICATOR);
+  await insertObject(db, 'data_source', {
+    id: generateId(),
+    code: INDICATOR.code,
+    type: 'dataElement',
+    service_type: 'indicator',
+  });
 };
 
 exports.down = async function (db) {
-  for (const indicator of INDICATORS) {
-    await deleteObject(db, 'indicator', { code: indicator.code });
-    await deleteObject(db, 'data_source', { code: indicator.code });
-  }
+  await deleteObject(db, 'indicator', { code: INDICATOR.code });
+  await deleteObject(db, 'data_source', { code: INDICATOR.code });
 };
 
 exports._meta = {
