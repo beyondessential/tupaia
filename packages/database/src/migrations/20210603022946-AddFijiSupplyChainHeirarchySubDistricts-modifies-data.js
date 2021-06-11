d10839737c89439a8511acf57097c460822d1232
nick <nickw@ourcomunity.com.au>
2021-06-11 23:11:32 +1000

    update entity_hierarchy for supplychain_fiji unfpa

--- packages/database/src/migrations/20210603022946-AddFijiSupplyChainHeirarchySubDistricts-modifies-data.js
+++ packages/database/src/migrations/20210603022946-AddFijiSupplyChainHeirarchySubDistricts-modifies-data.js
@@ -5,31 +5,39 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
+exports.up = function (db) {
   return db.runSql(`
     update "entity_hierarchy" 
-    set "canonical_types" = '{country,district,sub_district,facility}' 
+    set "canonical_types" = '{country,district}' 
     where "name" = 'supplychain_fiji';
+
+    update "entity_hierarchy" 
+    set "canonical_types" = '{country,district,facility}' 
+    where "name" = 'unfpa';
   `);
 };
 
-exports.down = function(db) {
+exports.down = function (db) {
   return db.runSql(`
     update "entity_hierarchy" 
     set "canonical_types" = '{country,district,village,facility}' 
     where "name" = 'supplychain_fiji';
+
+    update "entity_hierarchy" 
+    set "canonical_types" = '{}' 
+    where "name" = 'unfpa';
   `);
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
