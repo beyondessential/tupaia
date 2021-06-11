76c37b7e60997141ab2e29feafec988ccd2a40ab
Rohan Port <rohan@beyondessential.com.au>
2021-06-11 15:11:49 +1000

    2936: Added aggregatorEntityConnectionCache to session object - Simple cache that saves us requesting multiple times from the entity-server for the same request in one request context - Now Solomon Islands loads in ~11 seconds

--- packages/aggregator/src/connections/EntityConnection.js
+++ packages/aggregator/src/connections/EntityConnection.js
@@ -13,6 +13,22 @@ export class EntityConnection extends ApiConnection {
   constructor(session) {
     const { getAuthHeader } = session;
     super({ getAuthHeader });
+
+    session.aggregatorEntityConnectionCache = session.aggregatorEntityConnectionCache || {};
+    this.cache = session.aggregatorEntityConnectionCache;
+  }
+
+  // functionArguments should receive the 'arguments' object
+  getCacheKey = (url, query) => `${url}:${JSON.stringify(query)}`;
+
+  runCachedFetch(url, query) {
+    const cacheKey = this.getCacheKey(url, query);
+    if (this.cache[cacheKey]) {
+      return this.cache[cacheKey];
+    }
+
+    this.cache[cacheKey] = this.get(url, query); // may be async, in which case we cache the promise to be awaited
+    return this.cache[cacheKey];
   }
 
   async getDataSourceEntities(
@@ -21,7 +37,7 @@ export class EntityConnection extends ApiConnection {
     dataSourceEntityType,
     dataSourceEntityFilter = {}, // TODO: Add support for dataSourceEntityFilter https://github.com/beyondessential/tupaia-backlog/issues/2660
   ) {
-    return this.get(`hierarchy/${hierarchyName}/relatives`, {
+    return this.runCachedFetch(`hierarchy/${hierarchyName}/relatives`, {
       entities: entityCodes.join(','),
       descendant_filter: `type:${dataSourceEntityType}`,
       field: 'code',
@@ -47,12 +63,12 @@ export class EntityConnection extends ApiConnection {
       query.ancestor_filter = `type:${aggregationEntityType}`;
     }
 
-    const response = await this.get(`hierarchy/${hierarchyName}/relationships`, query);
-
-    const formattedRelationships = {};
-    Object.entries(response).forEach(([descendant, ancestor]) => {
-      formattedRelationships[descendant] = { code: ancestor };
+    return this.runCachedFetch(`hierarchy/${hierarchyName}/relationships`, query).then(response => {
+      const formattedRelationships = {};
+      Object.entries(response).forEach(([descendant, ancestor]) => {
+        formattedRelationships[descendant] = { code: ancestor };
+      });
+      return [Object.keys(formattedRelationships), formattedRelationships];
     });
-    return [Object.keys(formattedRelationships), formattedRelationships];
   }
 }
