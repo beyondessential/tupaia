df5d3e3defdf30adc8468662dd5dfa5955e5efe4
Kostas Karvounis <kael89@users.noreply.github.com>
2021-06-15 08:50:14 +1000

    2864:  Use known dimensions for dashboard report tests (#2655)
    * Use known report dimensions in dashboard report tests
    
    * Fix data existence check for matrices

--- packages/web-frontend/cypress/scripts/generateConfig/generateConfig.js
+++ packages/web-frontend/cypress/scripts/generateConfig/generateConfig.js
@@ -3,14 +3,10 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import { writeFileSync } from 'fs';
-
 import { TupaiaDatabase } from '@tupaia/database';
 import { getLoggerInstance } from '@tupaia/utils';
 import { createTestUser } from './createTestUser';
 import { generateReportConfig } from './generateReportConfig';
-
-const REPORT_CONFIG_PATH = 'cypress/config/dashboardReports.json';
 
 export const generateConfig = async () => {
   const logger = getLoggerInstance();
@@ -22,7 +18,6 @@ export const generateConfig = async () => {
   logger.success(`✔ Test user`);
 
   logger.success('Generating dashboard report config...');
-  const reportConfig = await generateReportConfig(db);
-  writeFileSync(REPORT_CONFIG_PATH, JSON.stringify(reportConfig, null, 2));
+  generateReportConfig();
   logger.success(`✔ Dashboard reports`);
 };
