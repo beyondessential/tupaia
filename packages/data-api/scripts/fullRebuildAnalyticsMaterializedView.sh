8995cc5b4c183b65701575f92ee3badccff12f86
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2021-06-17 12:58:41 +1000

    2961: Allow creating/removing indexes on Analytics Table as the Tupaia database user (#2675)
    * 2961: Updated pg-mv-fast-refresh and analytics table build scripts
    - New version pg-mv-fast-refresh version has functions that allow the tupaia user to add/remove indexes from the analytics table
    - New scripts to drop analytics table and uninstall the mvrefresh module

--- packages/data-api/scripts/fullRebuildAnalyticsMaterializedView.sh
+++ packages/data-api/scripts/fullRebuildAnalyticsMaterializedView.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-source .env
-cd scripts
-export PGPASSWORD=$DB_PASSWORD
-psql -h $DB_URL -d $DB_NAME -U $DB_USER -f dropAnalyticsMaterializedView.sql
 
-cd ..
+./scripts/dropAnalyticsMaterializedView.sh
 ./scripts/buildAnalyticsMaterializedView.sh
