4343c5d0b0f03699b994166f51e1d409d1288453
Igor Nadj <igor@beyondessential.com.au>
2021-06-21 14:50:53 +1000

    temp50

--- init-services.sh
+++ init-services.sh
@@ -3,8 +3,13 @@ set -x
 
 PACKAGES=("meditrak-server" "web-config-server" "psss-server" "lesmis-server" "report-server" "entity-server" "web-frontend" "admin-panel" "psss" "lesmis")
 
+HOME_DIRECTORY=/tupaia
+
 for PACKAGE in ${PACKAGES[@]}; do
     echo "Preparing to start ${PACKAGE}"
+
+    cd ${HOME_DIRECTORY}/packages/$PACKAGE
+
     if [[ $PACKAGE == *server ]]; then
         echo "Starting ${PACKAGE}"
         pm2 start --name $PACKAGE dist --wait-ready --listen-timeout 15000 --time
