661604b80742d67c6725ef021e409eb772897667
Rohan Port <rohan@beyondessential.com.au>
2021-06-24 17:34:20 +1000

    3008: Switched default version of microservice apis to be v[0-9]

--- packages/server-boilerplate/src/microService/api/ApiBuilder.ts
+++ packages/server-boilerplate/src/microService/api/ApiBuilder.ts
@@ -30,7 +30,7 @@ export class ApiBuilder {
     this.models = new ModelRegistry(transactingConnection);
     this.app = express();
 
-    this.version = 'v1'; // Default version
+    this.version = 'v[0-9]'; // Default version
 
     /**
      * Add middleware
