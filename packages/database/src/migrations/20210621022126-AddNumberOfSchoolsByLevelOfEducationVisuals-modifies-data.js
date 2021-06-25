2337237d5e3c5925b69290a4734c322d89f75711
Edwin <edmofro@gmail.com>
2021-06-25 14:10:37 +1200

    Update migration utilities to have more expected interface

--- packages/database/src/migrations/20210621022126-AddNumberOfSchoolsByLevelOfEducationVisuals-modifies-data.js
+++ packages/database/src/migrations/20210621022126-AddNumberOfSchoolsByLevelOfEducationVisuals-modifies-data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { insertObject, generateId, findSingleRecord } from '../utilities';
+import { insertObject, generateId, findSingleRecord, findSingleRecordBySql } from '../utilities';
 
 var dbm;
 var type;
@@ -104,7 +104,7 @@ const addNewDashboardItemAndReport = async (
   // insert report
   const reportId = generateId();
   const permissionGroupId = (
-    await findSingleRecord(db, `SELECT id FROM permission_group WHERE name = '${permissionGroup}';`)
+    await findSingleRecord(db, 'permission_group', { name: permissionGroup })
   ).id;
   await insertObject(db, 'report', {
     id: reportId,
@@ -123,11 +123,9 @@ const addNewDashboardItemAndReport = async (
   });
 
   // insert relation record connecting dashboard item to dashboard
-  const dashboardId = (
-    await findSingleRecord(db, `SELECT id FROM dashboard WHERE code = '${dashboardCode}'`)
-  ).id;
+  const dashboardId = (await await findSingleRecord(db, 'dashboard', { code: dashboardCode })).id;
   const maxSortOrder = (
-    await findSingleRecord(
+    await findSingleRecordBySql(
       db,
       `SELECT max(sort_order) as max_sort_order FROM dashboard_relation WHERE dashboard_id = '${dashboardId}';`,
     )
