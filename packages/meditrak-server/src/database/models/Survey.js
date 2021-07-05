e3ddd291a94de90ac6e46bff4cdb569e7e4c257d
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-07-06 09:12:22 +1000

    Update outdated status on survey response change

--- packages/meditrak-server/src/database/models/Survey.js
+++ packages/meditrak-server/src/database/models/Survey.js
@@ -5,6 +5,22 @@
 
 import { MaterializedViewLogDatabaseModel, DatabaseType, TYPES } from '@tupaia/database';
 
+export const PERIOD_GRANULARITIES = {
+  YEARLY: 'yearly',
+  QUARTERLY: 'quarterly',
+  MONTHLY: 'monthly',
+  WEEKLY: 'weekly',
+  DAILY: 'daily',
+};
+
+export const PERIOD_GRANULARITY_TO_MOMENT_UNIT = {
+  [PERIOD_GRANULARITIES.YEARLY]: 'year',
+  [PERIOD_GRANULARITIES.QUARTERLY]: 'quarter',
+  [PERIOD_GRANULARITIES.MONTHLY]: 'month',
+  [PERIOD_GRANULARITIES.WEEKLY]: 'isoWeek',
+  [PERIOD_GRANULARITIES.DAILY]: 'day',
+};
+
 class SurveyType extends DatabaseType {
   static databaseType = TYPES.SURVEY;
 
