3d56f96a12fb1956df78382a692fcf86f7e8e0f6
Rohan Port <rohan@beyondessential.com.au>
2021-07-05 19:33:54 +1000

    1400: Updated migration to use dashboard_item table

--- packages/database/src/migrations/20210601015828-AddStriveLabConfirmedPostiveResultsStackedBarChart-modifies-data.js
+++ packages/database/src/migrations/20210601015828-AddStriveLabConfirmedPostiveResultsStackedBarChart-modifies-data.js
@@ -1,15 +1,10 @@
 'use strict';
 
-import { generateId, insertObject, arrayToDbString } from '../utilities';
+import { insertObject, generateId, findSingleRecord, findSingleRecordBySql } from '../utilities';
 
 var dbm;
 var type;
 var seed;
-
-const permissionGroupNameToId = async (db, name) => {
-  const record = await db.runSql(`SELECT id FROM permission_group WHERE name = '${name}'`);
-  return record.rows[0] && record.rows[0].id;
-};
 
 /**
  * We receive the dbmigrate dependency from dbmigrate initially.
@@ -21,106 +16,131 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-const REPORT = {
-  id: generateId(),
-  code: 'PG_Strive_PNG_Lab_Confirmed_Positive_Results',
-  config: {
-    fetch: {
-      dataElements: ['STR_QMAL05', 'STR_PF05', 'STR_PV05', 'STR_PM05', 'STR_PO05'],
-      aggregations: [
-        {
-          type: 'REPLACE_ORG_UNIT_WITH_ORG_GROUP',
-          config: { aggregationEntityType: 'requested', dataSourceEntityType: 'case' },
-        },
-      ],
-    },
-    transform: [
+const CODE = 'PG_Strive_PNG_Lab_Confirmed_Positive_Results';
+
+const REPORT_CONFIG = {
+  fetch: {
+    dataElements: ['STR_QMAL05', 'STR_PF05', 'STR_PV05', 'STR_PM05', 'STR_PO05'],
+    aggregations: [
       {
-        transform: 'filter',
-        where: '$row.value == 1', // 0 indicates negative response, we are only interested in positive responses
-      },
-      'keyValueByDataElementName',
-      'convertPeriodToWeek',
-      {
-        transform: 'aggregate',
-        organisationUnit: 'drop',
-        period: 'group',
-        '...': 'count',
-      },
-      {
-        transform: 'select',
-        "'name'": "periodToDisplayString($row.period, 'WEEK')",
-        "'timestamp'": 'periodToTimestamp($row.period)',
-        "'Pf+ve'": 'divide($row.STR_PF05,$row.STR_QMAL05)',
-        "'Pv+ve'": 'divide($row.STR_PV05,$row.STR_QMAL05)',
-        "'PM+ve'": 'divide($row.STR_PM05,$row.STR_QMAL05)',
-        "'PO+ve'": 'divide($row.STR_PO05,$row.STR_QMAL05)',
+        type: 'REPLACE_ORG_UNIT_WITH_ORG_GROUP',
+        config: { aggregationEntityType: 'requested', dataSourceEntityType: 'case' },
       },
     ],
   },
+  transform: [
+    {
+      transform: 'filter',
+      where: '$row.value == 1', // 0 indicates negative response, we are only interested in positive responses
+    },
+    'keyValueByDataElementName',
+    'convertPeriodToWeek',
+    {
+      transform: 'aggregate',
+      organisationUnit: 'drop',
+      period: 'group',
+      '...': 'count',
+    },
+    {
+      transform: 'select',
+      "'name'": "periodToDisplayString($row.period, 'WEEK')",
+      "'timestamp'": 'periodToTimestamp($row.period)',
+      "'Pf+ve'": 'divide($row.STR_PF05,$row.STR_QMAL05)',
+      "'Pv+ve'": 'divide($row.STR_PV05,$row.STR_QMAL05)',
+      "'PM+ve'": 'divide($row.STR_PM05,$row.STR_QMAL05)',
+      "'PO+ve'": 'divide($row.STR_PO05,$row.STR_QMAL05)',
+    },
+  ],
 };
 
-const getReport = permissionGroupId => ({ ...REPORT, permission_group_id: permissionGroupId });
-
-const DASHBOARD_GROUP_CODE = 'PG_Strive_PNG_Facility';
-
-const DASHBOARD_REPORT = {
-  id: 'PG_Strive_PNG_Lab_Confirmed_Positive_Results_Stacked_Bar_Chart',
-  dataBuilder: 'reportServer',
-  dataBuilderConfig: {
-    reportCode: REPORT.code,
+const FRONT_END_CONFIG = {
+  name: 'Lab Confirmed Positive Results, Bar Graph',
+  type: 'chart',
+  chartType: 'bar',
+  chartConfig: {
+    'Pf+ve': { stackId: 1 },
+    'Pv+ve': { stackId: 1 },
+    'PM+ve': { stackId: 1 },
+    'PO+ve': { stackId: 1 },
   },
-  viewJson: {
-    name: 'Lab Confirmed Positive Results, Bar Graph',
-    type: 'chart',
-    chartType: 'bar',
-    chartConfig: {
-      'Pf+ve': { stackId: 1 },
-      'Pv+ve': { stackId: 1 },
-      'PM+ve': { stackId: 1 },
-      'PO+ve': { stackId: 1 },
-    },
-    periodGranularity: 'week',
-    valueType: 'percentage',
-    presentationOptions: {
-      hideAverage: true,
-    },
+  periodGranularity: 'week',
+  valueType: 'percentage',
+  presentationOptions: {
+    hideAverage: true,
   },
+};
+
+const addNewDashboardItemAndReport = async (
+  db,
+  { code, frontEndConfig, reportConfig, permissionGroup, dashboardCode, entityTypes, projectCodes },
+) => {
+  // insert report
+  const reportId = generateId();
+  const permissionGroupId = (
+    await findSingleRecord(db, 'permission_group', { name: permissionGroup })
+  ).id;
+  await insertObject(db, 'report', {
+    id: reportId,
+    code,
+    config: reportConfig,
+    permission_group_id: permissionGroupId,
+  });
+
+  // insert dashboard item
+  const dashboardItemId = generateId();
+  await insertObject(db, 'dashboard_item', {
+    id: dashboardItemId,
+    code,
+    config: frontEndConfig,
+    report_code: code,
+  });
+
+  // insert relation record connecting dashboard item to dashboard
+  await addItemToDashboard(db, { code, dashboardCode, entityTypes, projectCodes, permissionGroup });
+};
+
+const addItemToDashboard = async (
+  db,
+  { code, dashboardCode, permissionGroup, entityTypes, projectCodes },
+) => {
+  const dashboardItemId = (await findSingleRecord(db, 'dashboard_item', { code })).id;
+  const dashboardId = (await findSingleRecord(db, 'dashboard', { code: dashboardCode })).id;
+  const maxSortOrder = (
+    await findSingleRecordBySql(
+      db,
+      `SELECT max(sort_order) as max_sort_order FROM dashboard_relation WHERE dashboard_id = '${dashboardId}';`,
+    )
+  ).max_sort_order;
+  await insertObject(db, 'dashboard_relation', {
+    id: generateId(),
+    dashboard_id: dashboardId,
+    child_id: dashboardItemId,
+    entity_types: `{${entityTypes.join(', ')}}`,
+    project_codes: `{${projectCodes.join(', ')}}`,
+    permission_groups: `{${permissionGroup}}`,
+    sort_order: maxSortOrder + 1,
+  });
+};
+
+const removeDashboardItemAndReport = async (db, code) => {
+  await db.runSql(`DELETE FROM dashboard_item WHERE code = '${code}';`); // delete cascades to dashboard_relation
+  await db.runSql(`DELETE FROM report WHERE code = '${code}';`);
 };
 
 exports.up = async function (db) {
-  const permissionGroupId = await permissionGroupNameToId(db, 'STRIVE User');
-  const report = getReport(permissionGroupId);
-  await insertObject(db, 'report', report);
-
-  await insertObject(db, 'dashboardReport', DASHBOARD_REPORT);
-
-  await db.runSql(`
-    UPDATE
-      "dashboardGroup"
-    SET
-      "dashboardReports" = "dashboardReports" || '{ ${DASHBOARD_REPORT.id} }'
-    WHERE
-      "code" = '${DASHBOARD_GROUP_CODE}';
-  `);
+  await addNewDashboardItemAndReport(db, {
+    code: CODE,
+    reportConfig: REPORT_CONFIG,
+    frontEndConfig: FRONT_END_CONFIG,
+    permissionGroup: 'STRIVE User',
+    dashboardCode: 'PG_STRIVE_PNG',
+    entityTypes: ['facility'],
+    projectCodes: ['strive'],
+  });
 };
 
-exports.down = async function (db) {
-  await db.runSql(`
-    DELETE FROM report
-    WHERE code = '${REPORT.code}';
-  `);
-
-  await db.runSql(`
-    DELETE FROM "dashboardReport" WHERE id = '${DASHBOARD_REPORT.id}';
-
-    UPDATE
-      "dashboardGroup"
-    SET
-      "dashboardReports" = array_remove("dashboardReports", '${DASHBOARD_REPORT.id}')
-    WHERE
-      "code" = '${DASHBOARD_GROUP_CODE}';
-  `);
+exports.down = function (db) {
+  return removeDashboardItemAndReport(db, CODE);
 };
 
 exports._meta = {
