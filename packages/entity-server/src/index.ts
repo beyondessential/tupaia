c779e0b230303146d6a909880e944761fa377123
Rohan Port <rohan@beyondessential.com.au>
2021-07-06 17:28:50 +1000

    3008: Added EntityApi back to server-boilerplate

--- packages/entity-server/src/index.ts
+++ packages/entity-server/src/index.ts
@@ -3,4 +3,36 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
-export { EntityApi } from './api';
+import { PartialOrArray } from '@tupaia/server-boilerplate';
+
+import { EntityFields } from './models';
+import {
+  FlattableEntityFieldName,
+  ExtendedEntityFieldName,
+  EntityResponseObject,
+} from './routes/hierarchy/types';
+
+export type EntityFilterObject = PartialOrArray<EntityFields>;
+
+export type EntityApiQueryOptions = {
+  field?: FlattableEntityFieldName;
+  fields?: ExtendedEntityFieldName[];
+  filter?: EntityFilterObject;
+};
+export type RelationshipsSubQueryOptions = Omit<EntityApiQueryOptions, 'fields'>;
+
+export type EntityApiResponse<T extends ExtendedEntityFieldName[]> = Required<
+  Pick<EntityResponseObject, T[number]>
+>;
+
+export {
+  GroupByAncestorRelationshipsResponseBody,
+  GroupByDescendantRelationshipsResponseBody,
+} from './routes/hierarchy/relationships';
+
+export {
+  FlattableEntityFieldName,
+  ExtendedEntityFieldName,
+  EntityResponseObject,
+  FlattenedEntity,
+} from './routes/hierarchy/types';
