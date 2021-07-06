1d0584a0d50ccd42daa9d0c5d17c4c568db602ad
chris@beyondessential.com.au <chris@beyondessential.com.au>
2021-07-06 16:27:11 +1000

    Update e2e test to fit new dashboard structure

--- packages/web-frontend/cypress/scripts/generateConfig/VisualisationFilter.js
+++ packages/web-frontend/cypress/scripts/generateConfig/VisualisationFilter.js
@@ -3,7 +3,8 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
-import { keyBy, upperFirst } from 'lodash';
+import { camel } from 'case';
+import { keyBy, upperFirst, mapKeys } from 'lodash';
 
 import { getUniqueEntries, hasIntersection, toArray } from '@tupaia/utils';
 
@@ -19,6 +20,13 @@ class VisualisationFilter {
   // eslint-disable-next-line no-unused-vars
   fetchRecords = async objectUrls => {
     throw new Error('Any subclass of VisualisationFilter must implement the "fetchRecords" method');
+  };
+
+  /**
+   * @abstract
+   */
+  getKey = () => {
+    throw new Error('Any subclass of VisualisationFilter must implement the "getKey" method');
   };
 
   /**
@@ -40,20 +48,25 @@ class VisualisationFilter {
       return objectUrls;
     }
 
+    const visualKey = this.getKey();
     const records = await this.fetchRecords(objectUrls);
-    const recordsById = keyBy(records, 'id');
+    // Camel case all the record keys
+    const transformedRecords = records.map(r => mapKeys(r, (_, key) => camel(key)));
+    const recordsByKey = keyBy(transformedRecords, visualKey);
 
     const filterUrl = url =>
       Object.entries(this.filter).some(([key, targetValue]) => {
-        const record = recordsById[url.id];
+        const record = recordsByKey[url[visualKey]];
         if (!record) {
           throw new Error(
-            `${upperFirst(this.getName())} with id "${url.id}" was not found in the database`,
+            `${upperFirst(this.getName())} with code "${url.code}" was not found in the database`,
           );
         }
 
         const isBuilderFilter = key === this.getBuilderKey();
-        const value = isBuilderFilter ? this.getBuildersInRecord(recordsById[url.id]) : url[key];
+        const value = isBuilderFilter
+          ? this.getBuildersInRecord(recordsByKey[url[visualKey]])
+          : url[key];
         return hasIntersection(toArray(targetValue), toArray(value));
       });
 
@@ -70,11 +83,31 @@ class VisualisationFilter {
 }
 
 export class DashboardReportFilter extends VisualisationFilter {
-  fetchRecords = async objectUrls =>
-    this.db.find('dashboardReport', {
-      id: objectUrls.map(u => u.id),
-      drillDownLevel: null, // Drill down reports are not currently supported in e2e tests
+  fetchRecords = async objectUrls => {
+    const dashboardItems = await this.db.find('dashboard_item', {
+      code: objectUrls.map(u => u.code),
     });
+    const legacyDashboardItems = dashboardItems.filter(di => di.legacy);
+    const legacyReports = await this.db.find('legacy_report', {
+      code: legacyDashboardItems.map(di => di.report_code),
+    });
+    const legacyReportById = keyBy(legacyReports, 'code');
+    return dashboardItems.map(di => {
+      const legacyReport = di.legacy ? legacyReportById[di.report_code] : {};
+
+      if (legacyReport) {
+        return {
+          ...di,
+          data_builder: legacyReport.data_builder,
+          data_builder_config: legacyReport.data_builder_config,
+        };
+      }
+
+      return di;
+    });
+  };
+
+  getKey = () => 'code';
 
   getBuilderKey = () => 'dataBuilder';
 
@@ -86,6 +119,8 @@ export class MapOverlayFilter extends VisualisationFilter {
     this.db.find('mapOverlay', {
       id: objectUrls.map(u => u.id).flat(),
     });
+
+  getKey = () => 'id';
 
   getBuilderKey = () => 'measureBuilder';
 
