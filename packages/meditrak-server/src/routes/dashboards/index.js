97ffa3db78d2ec42a718225e4c883c2a7f121712
chris-bes <63621318+chris-bes@users.noreply.github.com>
2021-07-08 08:48:33 +1000

    2939: Add new admin panel tabs for new dashboard structure (#2763)
    * 2939: Added new admin panel tabs for new dashboard structure
    
    * 2939: Added more doco
    
    * 2939: initial dashboard relation tab
    
    * 2939: More of adding dashboard_relation tab
    
    * 2939: more of dashboard_relation tab
    
    * 2939: removed console log
    
    * 2939: adjust legacy report
    
    * 2939: cleaned up
    
    * 2939: added tests
    
    * 2939: removed .only from test files
    
    * 2939: Addressed review comments
    
    * 2939: Dropping deprecated dashboardGroup and dashboardReport tables.
          Remove all their references in the code base
    
    * 2939: fixed tostring()
    
    * 2939: Fixed typo
    
    * 2939: Adjusted permission error message
    
    * 2939: fixed ui bug that 0 is shown as empty in Text Field

---
+++ packages/meditrak-server/src/routes/dashboards/index.js
@@ -0,0 +1,9 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+export { DeleteDashboard } from './DeleteDashboard';
+export { EditDashboard } from './EditDashboard';
+export { GETDashboards } from './GETDashboards';
+export { assertDashboardGetPermissions } from './assertDashboardsPermissions';
