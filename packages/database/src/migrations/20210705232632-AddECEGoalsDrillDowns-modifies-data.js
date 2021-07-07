8d2de1bcecb37d11a03398ca8004ab8a957fb843
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-07-08 10:49:08 +1200

    Add migration for ECE target drill down visuals

---
+++ packages/database/src/migrations/20210705232632-AddECEGoalsDrillDowns-modifies-data.js
@@ -0,0 +1,144 @@
+'use strict';
+
+import { insertObject, generateId, findSingleRecord, findSingleRecordBySql } from '../utilities';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+const generateReport = (dataElement, target) => ({
+  fetch: {
+    dataElements: [dataElement],
+    aggregations: [
+      {
+        type: 'MOST_RECENT',
+      },
+      {
+        type: 'SUM_PER_ORG_GROUP',
+        config: {
+          dataSourceEntityType: 'sub_district',
+          aggregationEntityType: 'requested',
+        },
+      },
+    ],
+  },
+  transform: [
+    'keyValueByDataElementName',
+    {
+      transform: 'select',
+      "'Current'": `$row.${dataElement}/100`,
+      "'Target'": `${target || undefined}`,
+      "'timestamp'": 'periodToTimestamp($row.period)',
+    },
+    {
+      transform: 'aggregate',
+      timestamp: 'group',
+      '...': 'last',
+    },
+  ],
+});
+
+const FRONT_END_CONFIG = {
+  type: 'chart',
+  chartType: 'line',
+  xName: 'Year',
+  yName: 'Rate',
+  periodGranularity: 'year',
+  valueType: 'percentage',
+  chartConfig: {
+    Current: {
+      color: '#f44336',
+    },
+    Target: {
+      color: '#4caf50',
+    },
+  },
+};
+
+const CONFIG_0_2_REPORT = {
+  code: 'LESMIS_enrolment_ece_0_2_target',
+  reportConfig: generateReport('er_district_ece_0_2_t', '0.07'),
+  frontEndConfig: { ...FRONT_END_CONFIG, name: 'Enrolment rate of 0-2 year old students in ECE' },
+};
+
+const CONFIG_3_4_REPORT = {
+  code: 'LESMIS_enrolment_ece_3_4_target',
+  reportConfig: generateReport('er_district_ece_3_4_t'),
+  frontEndConfig: {
+    ...FRONT_END_CONFIG,
+    name: 'Enrolment rate of 3-4 year old students in ECE',
+    chartConfig: { Current: { color: '#f44336' } }, // Remove target from legend
+  },
+};
+
+const CONFIG_5_REPORT = {
+  code: 'LESMIS_enrolment_ece_5_target',
+  reportConfig: generateReport('er_district_ece_5_t', '0.86'),
+  frontEndConfig: { ...FRONT_END_CONFIG, name: 'Enrolment rate of 5 year old students in ECE' },
+};
+
+const addNewDashboardItemAndReport = async (
+  db,
+  { code, frontEndConfig, reportConfig, permissionGroup },
+) => {
+  // insert report
+  const reportId = generateId();
+  const permissionGroupId = (
+    await findSingleRecord(db, 'permission_group', { name: permissionGroup })
+  ).id;
+  await insertObject(db, 'report', {
+    id: reportId,
+    code,
+    config: reportConfig,
+    permission_group_id: permissionGroupId,
+  });
+
+  // insert dashboard item
+  const dashboardItemId = generateId();
+  await insertObject(db, 'dashboard_item', {
+    id: dashboardItemId,
+    code,
+    config: frontEndConfig,
+    report_code: code,
+  });
+};
+
+const removeDashboardItemAndReport = async (db, code) => {
+  await db.runSql(`DELETE FROM dashboard_item WHERE code = '${code}';`); // delete cascades to dashboard_relation
+  await db.runSql(`DELETE FROM report WHERE code = '${code}';`);
+};
+
+exports.up = async function (db) {
+  for (const { code, reportConfig, frontEndConfig } of [
+    CONFIG_0_2_REPORT,
+    CONFIG_3_4_REPORT,
+    CONFIG_5_REPORT,
+  ]) {
+    await addNewDashboardItemAndReport(db, {
+      code,
+      reportConfig,
+      frontEndConfig,
+      permissionGroup: 'LESMIS Public',
+    });
+  }
+};
+
+exports.down = async function (db) {
+  for (const { code } of [CONFIG_0_2_REPORT, CONFIG_3_4_REPORT, CONFIG_5_REPORT]) {
+    await removeDashboardItemAndReport(db, code);
+  }
+};
+
+exports._meta = {
+  version: 1,
+};
