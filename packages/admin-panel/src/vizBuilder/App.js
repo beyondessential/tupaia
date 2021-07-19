e9a92d018c89fec62006666961e1e189029b2d09
Tom Caiger <caigertom@gmail.com>
2021-07-19 18:33:10 +1200

    add page layout

--- packages/admin-panel/src/vizBuilder/App.js
+++ packages/admin-panel/src/vizBuilder/App.js
@@ -4,23 +4,46 @@
  */
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Tabs, Toolbar, JsonEditor, FlexSpaceBetween } from './components';
 import styled from 'styled-components';
+import MuiContainer from '@material-ui/core/Container';
+import { Tabs, Toolbar, JsonEditor, FlexSpaceBetween } from './components';
 import { useUser } from './api/queries';
 import { FullPageLoader } from './components/FullPageLoader';
 
 const Main = styled.main`
+  display: flex;
+  flex-direction: column;
   background: #e5e5e5;
   height: 100vh;
 `;
 
-const Container = styled(FlexSpaceBetween)`
-  align-items: flex-start;
+const Container = styled(MuiContainer)`
+  flex: 1;
+  display: flex;
+  align-items: stretch;
+`;
+
+const LeftCol = styled.section`
+  position: relative;
+  width: 400px;
+  z-index: 1;
+  //border: 1px dotted black;
+
+  &:before {
+    position: absolute;
+    content: '';
+    background: white;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 10000px;
+    z-index: -1;
+  }
 `;
 
 const RightCol = styled.section`
-  //background: #e5e5e5;
-  //height: 100vh;
+  flex: 1;
+  border: 1px dotted black;
 `;
 
 export const App = () => {
@@ -37,7 +60,9 @@ export const App = () => {
     <Main>
       <Toolbar />
       <Container>
-        <Tabs />
+        <LeftCol>
+          <Tabs />
+        </LeftCol>
         <RightCol />
       </Container>
     </Main>
--- packages/admin-panel/src/vizBuilder/components/Toolbar.js
+++ packages/admin-panel/src/vizBuilder/components/Toolbar.js
@@ -5,17 +5,51 @@
 import React from 'react';
 import MuiContainer from '@material-ui/core/Container';
 import styled from 'styled-components';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { Button } from '@tupaia/ui-components';
+import { FlexStart, FlexEnd } from './Flexbox';
 
-const StyledToolbar = styled.div`
+const Wrapper = styled.div`
   width: 100%;
   height: 100px;
   background: white;
 `;
 
+const Container = styled(MuiContainer)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`;
+
+const StyledImg = styled.img`
+  height: 70px;
+  width: auto;
+  margin-right: 10px;
+`;
+
 export const Toolbar = () => {
   return (
-    <StyledToolbar>
-      <MuiContainer>Toolbar1</MuiContainer>
-    </StyledToolbar>
+    <Wrapper>
+      <Container>
+        <FlexStart>
+          <StyledImg src="/document-icon.svg" alt="document-icon" />
+          <div>
+            <Typography variant="h6">Project: COVID-19 Samoa • Immunization Module</Typography>
+            <Typography variant="h6">PSSS Weekly Influence-like Illness Cases</Typography>
+            <Typography variant="h6">Add summary content</Typography>
+          </div>
+        </FlexStart>
+        <FlexEnd>
+          <IconButton>
+            <GetAppIcon />
+          </IconButton>
+          <Button>Save</Button>
+        </FlexEnd>
+      </Container>
+    </Wrapper>
   );
 };
