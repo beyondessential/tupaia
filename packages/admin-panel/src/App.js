7195ce55c05eceb69174fe3125952da9c586a644
Tom Caiger <caigertom@gmail.com>
2021-07-19 17:19:30 +1200

    add viz builder route

--- packages/admin-panel/src/App.js
+++ packages/admin-panel/src/App.js
@@ -3,10 +3,9 @@
  *  Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { TabsToolbar } from '@tupaia/ui-components';
 import { Navbar, Footer } from './widgets';
-import { App as VizBuilder } from './vizBuilder';
 import { ROUTES } from './routes';
 import { PROFILE_ROUTES } from './profileRoutes';
 import { PrivateRoute } from './authentication';
@@ -20,37 +19,32 @@ export const App = () => {
   };
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/login" exact>
-          <LoginPage />
-        </Route>
-        <PrivateRoute path="/">
-          <Navbar links={ROUTES} />
-          <div ref={headerEl} />
-          <Switch>
-            {[...ROUTES, ...PROFILE_ROUTES].map(route => (
-              <Route key={route.to} path={route.to}>
-                <TabsToolbar links={route.tabs} />
-                <Switch>
-                  {route.tabs.map(tab => (
-                    <Route key={`${route.to}-${tab.to}`} path={`${route.to}${tab.to}`} exact>
-                      <tab.component getHeaderEl={getHeaderEl} />
-                    </Route>
-                  ))}
-                  <Redirect to={route.to} />
-                </Switch>
-              </Route>
-            ))}
-            <Route path="/viz-builder" exact>
-              <VizBuilder />
+    <Switch>
+      <Route path="/login" exact>
+        <LoginPage />
+      </Route>
+      <PrivateRoute path="/">
+        <Navbar links={ROUTES} />
+        <div ref={headerEl} />
+        <Switch>
+          {[...ROUTES, ...PROFILE_ROUTES].map(route => (
+            <Route key={route.to} path={route.to}>
+              <TabsToolbar links={route.tabs} />
+              <Switch>
+                {route.tabs.map(tab => (
+                  <Route key={`${route.to}-${tab.to}`} path={`${route.to}${tab.to}`} exact>
+                    <tab.component getHeaderEl={getHeaderEl} />
+                  </Route>
+                ))}
+                <Redirect to={route.to} />
+              </Switch>
             </Route>
-            <Redirect to="surveys" />
-          </Switch>
-        </PrivateRoute>
-        <Redirect to="/login" />
-      </Switch>
-      <Footer />
-    </Router>
+          ))}
+          <Redirect to="surveys" />
+        </Switch>
+        <Footer />
+      </PrivateRoute>
+      <Redirect to="/login" />
+    </Switch>
   );
 };
