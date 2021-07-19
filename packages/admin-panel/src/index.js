7e5227cc638838bbed0f3b3ba406592e1370e500
Tom Caiger <caigertom@gmail.com>
2021-07-20 10:44:39 +1200

    add code splitting

--- packages/admin-panel/src/index.js
+++ packages/admin-panel/src/index.js
@@ -2,29 +2,33 @@
  * Tupaia
  *  Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { render as renderReactApp } from 'react-dom';
 import 'react-table/react-table.css';
 import { App } from './App';
-import { App as VizBuilder } from './vizBuilder';
+// import { App as VizBuilder } from './vizBuilder';
 import { AdminPanelProviders, VizBuilderProviders } from './utilities';
+
+const VizBuilder = lazy(() => import('./vizBuilder'));
 
 renderReactApp(
   <Router>
-    <Switch>
-      <Route path="/viz-builder" exact>
-        <VizBuilderProviders>
-          <VizBuilder />
-        </VizBuilderProviders>
-      </Route>
-      <Route path="/">
-        <AdminPanelProviders>
-          <App />
-        </AdminPanelProviders>
-      </Route>
-      <Redirect to="/login" />
-    </Switch>
+    <Suspense fallback={<div>loading...</div>}>
+      <Switch>
+        <Route path="/viz-builder" exact>
+          <VizBuilderProviders>
+            <VizBuilder />
+          </VizBuilderProviders>
+        </Route>
+        <Route path="/">
+          <AdminPanelProviders>
+            <App />
+          </AdminPanelProviders>
+        </Route>
+        <Redirect to="/login" />
+      </Switch>
+    </Suspense>
   </Router>,
   document.getElementById('root'),
 );
--- packages/admin-panel/src/vizBuilder/index.js
+++ packages/admin-panel/src/vizBuilder/index.js
@@ -3,4 +3,5 @@
  *  Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
-export * from './App';
+// Must be a default export as React.lazy currently only supports default exports.
+export { App as default } from './App';
