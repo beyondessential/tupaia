871369d289216077fe2adfd146fb430172d8a082
Igor Nadj <igor@beyondessential.com.au>
2021-07-20 15:18:08 +1000

    Update CI scripts for new ssh connection config

--- packages/devops/scripts/ci/deployFrontend.sh
+++ packages/devops/scripts/ci/deployFrontend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PACKAGE=$1
 DIR=$(dirname "$0")
-DEPLOYMENT_URL=$(${DIR}/determineDeploymentUrl.sh)
-if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
+DEPLOYMENT_SSH_URL=$(${DIR}/determineDeploymentSshUrl.sh)
+if curl --output /dev/null --silent --head --fail DEPLOYMENT_SSH_URL; then
     echo "Deployment for ${CI_BRANCH} exists, updating with latest changes"
-    ssh -o ServerAliveInterval=15 ubuntu@$DEPLOYMENT_URL "cd tupaia; yarn workspace @tupaia/${PACKAGE} build"
+    ssh -o ServerAliveInterval=15 ubuntu@DEPLOYMENT_SSH_URL "cd tupaia; yarn workspace @tupaia/${PACKAGE} build"
 else
     echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
