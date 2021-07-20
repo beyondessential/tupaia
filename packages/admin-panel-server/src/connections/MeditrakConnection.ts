c47b99897d9731c139065b0cf68348301bef6a7f
Tom Caiger <caigertom@gmail.com>
2021-07-21 09:13:07 +1200

    add user route

--- packages/admin-panel-server/src/connections/MeditrakConnection.ts
+++ packages/admin-panel-server/src/connections/MeditrakConnection.ts
@@ -2,13 +2,29 @@
  * Tupaia
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
-
-import { QueryParameters, ApiConnection } from '@tupaia/server-boilerplate';
+import camelcaseKeys from 'camelcase-keys';
+import { AccessPolicy } from '@tupaia/access-policy';
+import { QueryParameters } from '@tupaia/server-boilerplate';
+import { SessionHandlingApiConnection } from './SessionHandlingApiConnection';
+import { BES_ADMIN_PERMISSION_GROUP } from '../constants';
 
 const { MEDITRAK_API_URL = 'http://localhost:8090/v2' } = process.env;
 
-export class MeditrakConnection extends ApiConnection {
+const isBESAdmin = (policy: Record<string, string[]>) => {
+  return new AccessPolicy(policy).allowsSome(null, BES_ADMIN_PERMISSION_GROUP);
+};
+
+export class MeditrakConnection extends SessionHandlingApiConnection {
   baseUrl = MEDITRAK_API_URL;
+
+  async getUser() {
+    // if user is not logged in, return null rather than fetching the api client user details
+    if (!this.hasSession) {
+      return {};
+    }
+    const user = await this.get('me');
+    return { ...camelcaseKeys(user), isBESAdmin: isBESAdmin(user.accessPolicy) };
+  }
 
   async fetchResources(endpoint: string, queryParams: QueryParameters) {
     return this.get(endpoint, queryParams);
