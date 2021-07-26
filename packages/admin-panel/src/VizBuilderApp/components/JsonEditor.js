7255246f1a7be1bbcb863f73dfe67f208d6d1906
Tom Caiger <caigertom@gmail.com>
2021-07-26 16:00:09 +1200

    3102 viz builder fe scaffolding (#2854)
    * Add raygun crash reporting (#2801)
    
    * merge epic branch
    
    * E2e tests: Allow reusing custom dashboard report configuration (#2671)
    
    * Support multiple dashboard report url configs
    
    * Add dashboard report config for covidau
    
    * Rename documentation files to README
    
    * 2865: Compare snapshots in CI/CD (#2674)
    
    * Use local snapshots for comparison
    
    * Update config generation scripts
    
    * Capture response body snapshots
    
    * Update documentation
    
    * resolve merge conflicts
    
    * 3004-3138: E2e tests visualisation filter (#2694), Update e2e tests new dashboard structure (#2781)
    
    Co-authored-by: chris@beyondessential.com.au <chris@beyondessential.com.au>
    
    * 1486 add verified colum to users table in admin panel (#2709) (#2798)
    
    * pulled package.json from remote dev branch
    
    * Added the 'Verified' column to the users table. This column indicate whether a user is verified, new (no verified) or a historical user who does not need verification
    
    * Update package.json
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    Co-authored-by: karineagredo <39760902+karineagredo@users.noreply.github.com>
    
    * 2813 spinner behind modal new (#2708) (#2797)
    
    * pulled package.json from remote dev branch
    
    * Loading spinner is now displaying behind popup window
    
    * Update package.json
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    Co-authored-by: karineagredo <39760902+karineagredo@users.noreply.github.com>
    
    * 3000: E2E lesmis config  (#2810)
    
    * Adding lesmis vis to E2E
    
    * revert lesmis Laos_Schools_Male_Female code
    
    * to remove one extra line
    
    * sorting the code alphabetically also adding Lesmis.json file
    
    * 1648: use session cookie in admin panel
    
    * 1648: Exclude creating trigger for admin_panel_session
    
    * 1648: removed session cookie set up from meditrak-server
    
    * 3100: admin-panel-server
    
    * 3100: updated yarn.lock
    
    * 3100: changed admin-panel-api to admin-api to be consistent with other
          domain names
          fixed wrong port
    
    * add viz builder
    
    * add viz builder route
    
    * add user auth check
    
    * add page layout
    
    * Update rootReducer.js
    
    * front end permission handling
    
    * add code splitting
    
    * add header and footer
    
    * Handle logout
    
    * add camelcase keys to admin panel server
    
    * remove header and footer from viz builder
    
    * rename viz builder folder
    
    * move flexbox and full page loader to ui components
    
    Co-authored-by: Igor <igor@beyondessential.com.au>
    Co-authored-by: Kostas Karvounis <kael89@users.noreply.github.com>
    Co-authored-by: chris@beyondessential.com.au <chris@beyondessential.com.au>
    Co-authored-by: karineagredo <39760902+karineagredo@users.noreply.github.com>
    Co-authored-by: sepidehdd <66226595+sepidehdd@users.noreply.github.com>
    Co-authored-by: chris-bes <63621318+chris-bes@users.noreply.github.com>

---
+++ packages/admin-panel/src/VizBuilderApp/components/JsonEditor.js
@@ -0,0 +1,16 @@
+/*
+ * Tupaia
+ *  Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTabs = styled.div`
+  width: 380px;
+  height: 100%;
+  background-color: lightblue;
+`;
+
+export const JsonEditor = () => {
+  return <StyledTabs />;
+};
--- packages/admin-panel/src/rootReducer.js
+++ packages/admin-panel/src/rootReducer.js
@@ -2,7 +2,6 @@
  * Tupaia MediTrak
  * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
  */
-
 import { combineReducers } from 'redux';
 import { reducer as authentication, LOGOUT } from './authentication';
 import { reducer as tables } from './table';
@@ -10,15 +9,15 @@ import { reducer as autocomplete } from './autocomplete';
 import { reducer as editor } from './editor';
 import { reducer as dataChangeListener } from './dataChangeListener';
 
-const appReducer = combineReducers({
-  authentication,
-  tables,
-  autocomplete,
-  editor,
-  dataChangeListener,
-});
-
 export const rootReducer = (state, action) => {
+  const appReducer = combineReducers({
+    authentication,
+    tables,
+    autocomplete,
+    editor,
+    dataChangeListener,
+  });
+
   // on logout, wipe all redux state except auth
   if (action.type === LOGOUT) {
     return appReducer({ authentication: state.authentication }, action);
