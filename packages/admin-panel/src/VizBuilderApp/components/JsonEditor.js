f2539abb73458ea134f550077d5285e9f8c200ae
Tom Caiger <caigertom@gmail.com>
2021-07-28 14:26:17 +1200

    add tabs and json edit boxes

--- packages/admin-panel/src/VizBuilderApp/components/JsonEditor.js
+++ packages/admin-panel/src/VizBuilderApp/components/JsonEditor.js
@@ -3,14 +3,27 @@
  *  Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 import React from 'react';
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
+import ace from 'brace';
+import 'brace/mode/json';
+import 'brace/theme/github';
 
-const StyledTabs = styled.div`
-  width: 380px;
-  height: 100%;
-  background-color: lightblue;
-`;
+export const JsonEditor = ({ value, onChange }) => {
+  return (
+    <Editor
+      value={value}
+      onChange={onChange}
+      ace={ace}
+      theme="ace/theme/github"
+      mode="code"
+      mainMenuBar={false}
+    />
+  );
+};
 
-export const JsonEditor = () => {
-  return <StyledTabs />;
+JsonEditor.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
