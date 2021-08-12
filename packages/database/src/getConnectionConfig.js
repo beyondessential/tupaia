afbe089e445a98ee47081d018675086c2776e68f
Alastair <lockealastair@gmail.com>
2021-08-12 12:37:54 +1000

    Allow non-5432 db ports (#2820)
    * Allow non-5432 db ports
    
    * Add DB_PORT to .env.example
    
    * Add default port to pg-mv-fast-refresh scripts
    
    * Add default port to getConnectionConfig
    
    * Add configurable port to setupTestDatabase script

--- packages/database/src/getConnectionConfig.js
+++ packages/database/src/getConnectionConfig.js
@@ -3,19 +3,20 @@
  * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
  */
 
-import { requireEnv } from '@tupaia/utils';
+import { requireEnv, getEnvVarOrDefault } from '@tupaia/utils';
 
 const getServerConfig = () => ({
   host: requireEnv('DB_URL'),
+  port: getEnvVarOrDefault('DB_PORT', 5432),
   user: requireEnv('DB_USER'),
   password: requireEnv('DB_PASSWORD'),
   database: requireEnv('DB_NAME'),
   ssl:
     process.env.DB_ENABLE_SSL === 'true'
       ? {
-          // Test server cannot turn on ssl, so sets the env to disable it
-          rejectUnauthorized: false,
-        }
+        // Test server cannot turn on ssl, so sets the env to disable it
+        rejectUnauthorized: false,
+      }
       : null,
 });
 
