77c402b22fd799de1f314e17b5f54e120140079a
billli0 <lbit123@outlook.com>
2021-08-24 11:25:40 +1000

    Refactor fetch & test report routes

--- packages/report-server/src/routes/FetchReportRoute.ts
+++ packages/report-server/src/routes/FetchReportRoute.ts
@@ -10,8 +10,8 @@ import { Route } from '@tupaia/server-boilerplate';
 
 import { Aggregator } from '../aggregator';
 import { ReportBuilder, BuiltReport } from '../reportBuilder';
-import { ReportType } from '../models';
 import { ReportRouteQuery, ReportRouteBody } from './types';
+import { getRequestedOrgUnitObjects, getAccessibleOrgUnitCodes } from './helpers';
 
 export type FetchReportRequest = Request<
   { reportCode: string },
@@ -32,77 +32,29 @@ export class FetchReportRoute extends Route<FetchReportRequest> {
     return report;
   }
 
-  async getRequestedOrgUnitObjects(
-    hierarchy = 'explore',
-    requestedOrgUnitCodes: string[],
-  ): Promise<{ code: string; country_code: string | null; type: string | null }[]> {
-    const { ctx } = this.req;
-    const entities = await ctx.microServices.entityApi.getEntities(
-      hierarchy,
-      requestedOrgUnitCodes,
-      {
-        fields: ['code', 'country_code', 'type'],
-      },
-    );
-    if (entities.length === 0) {
-      throw new Error(`No entities found with codes ${requestedOrgUnitCodes}`);
-    }
-
-    // If entity is a project
-    if (entities.length === 1 && entities[0].type === 'project') {
-      const countryEntities = await ctx.microServices.entityApi.getDescendantsOfEntities(
-        hierarchy,
-        requestedOrgUnitCodes,
-        {
-          fields: ['code', 'country_code', 'type'],
-          filter: { type: 'country' },
-        },
-        false,
-      );
-
-      return countryEntities;
-    }
-    return entities;
-  }
-
-  async getAccessibleOrgUnitCodes(
-    report: ReportType,
-    hierarchy = 'explore',
-    requestedOrgUnitCodes: string | string[],
-  ) {
-    const { accessPolicy } = this.req;
-    const permissionGroupName = await report.permissionGroupName();
-
-    const requestedOrgUnitCodesArray = Array.isArray(requestedOrgUnitCodes)
-      ? requestedOrgUnitCodes
-      : requestedOrgUnitCodes.split(',');
-    const foundOrgUnits = await this.getRequestedOrgUnitObjects(
-      hierarchy,
-      requestedOrgUnitCodesArray,
-    );
-
-    const accessibleOrgUnits = foundOrgUnits.filter(
-      ({ country_code: countryCode }) =>
-        countryCode !== null && accessPolicy.allows(countryCode, permissionGroupName),
-    );
-    if (accessibleOrgUnits.length === 0) {
-      throw new Error(`No permissions to any one of entities ${foundOrgUnits}`);
-    }
-    return accessibleOrgUnits.map(orgUnit => orgUnit.code);
-  }
-
   async buildResponse() {
     const { query, body } = this.req;
-    const { organisationUnitCodes, hierarchy, ...restOfParams } = { ...query, ...body };
+    const { organisationUnitCodes, hierarchy = 'explore', ...restOfParams } = { ...query, ...body };
     if (!organisationUnitCodes) {
       throw new Error('Must provide organisationUnitCodes URL parameter');
     }
     const report = await this.findReport();
+    const permissionGroupName = await report.permissionGroupName();
 
-    const accessibleOrgUnitCodes = await this.getAccessibleOrgUnitCodes(
-      report,
+    const requestedOrgUnitCodesArray = Array.isArray(organisationUnitCodes)
+      ? organisationUnitCodes
+      : organisationUnitCodes.split(',');
+
+    const foundOrgUnits = await getRequestedOrgUnitObjects(
       hierarchy,
-      organisationUnitCodes,
+      requestedOrgUnitCodesArray,
+      this.req.ctx.microServices.entityApi,
+    );
+
+    const accessibleOrgUnitCodes = await getAccessibleOrgUnitCodes(
+      permissionGroupName,
+      foundOrgUnits,
+      this.req.accessPolicy,
     );
 
     const aggregator = createAggregator(Aggregator, this.req.ctx);
