8406a2cb9a2cde72f439aeee8700ac052619a46c
Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
2021-08-24 18:49:29 +1200

    NZ-803: Email download link for long running exports (#2965)

---
+++ packages/meditrak-server/src/apiV2/middleware/logApiRequest.js
@@ -0,0 +1,19 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+import { extractRefreshTokenFromReq } from '@tupaia/auth';
+
+export const logApiRequest = async (req, res, next) => {
+  const refreshToken = await extractRefreshTokenFromReq(req);
+  const { id: apiRequestLogId } = await req.models.apiRequestLog.create({
+    version: 2, // only version that can get through current route handling
+    endpoint: req.path,
+    user_id: req.userId,
+    query: req.query,
+    refresh_token: refreshToken,
+  });
+  req.apiRequestLogId = apiRequestLogId;
+  req.refreshToken = refreshToken;
+  next();
+};
