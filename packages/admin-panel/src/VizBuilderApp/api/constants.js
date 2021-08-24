a16ebbe096022ffb485a61d65fe819eb697ac6a1
chris-bes <63621318+chris-bes@users.noreply.github.com>
2021-08-24 11:10:12 +1000

    Disable query cache completely

--- packages/admin-panel/src/VizBuilderApp/api/constants.js
+++ packages/admin-panel/src/VizBuilderApp/api/constants.js
@@ -8,4 +8,5 @@ export const DEFAULT_REACT_QUERY_OPTIONS = {
   retry: 0,
   // should be refetched in the background every hour
   refetchOnWindowFocus: false,
+  cacheTime: 0, // disable cache entirely because we always need to show live data
 };
