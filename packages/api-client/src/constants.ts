56abb0f4dbb6ecde513e5e486de5222c7618708e
Igor Nadj <igor@beyondessential.com.au>
2021-08-24 11:00:52 +1000

    Add Copyright header

--- packages/api-client/src/constants.ts
+++ packages/api-client/src/constants.ts
@@ -1,3 +1,8 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
 export const DATA_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 export type EndpointBaseUrlSet = {
--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
 /*
  * Main entrypoint
  */
