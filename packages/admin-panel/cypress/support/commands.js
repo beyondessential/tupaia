c61f244cff8658798abb53878fca18a9a7afba43
Bindu <tammireddymadhavi@gmail.com>
2021-08-25 16:58:12 +1000

    added a new package to support cypress upload and created a test case to submit single survey.

--- packages/admin-panel/cypress/support/commands.js
+++ packages/admin-panel/cypress/support/commands.js
@@ -4,11 +4,29 @@
  */
 
 import '@testing-library/cypress/add-commands';
+import 'cypress-file-upload';
 
 Cypress.Commands.add('login', ({ email, password }) => {
   cy.visit('/login');
-
   cy.findByPlaceholderText(/email/i).type(email);
   cy.findByPlaceholderText(/password/i).type(password, { log: false });
   cy.findByText('Login to your account').click();
+});
+
+/*
+ *   ---------- SURVEY COMMANDS ----------------------
+ */
+
+Cypress.Commands.add('selectIntoTextBox', (lableText, inputText) => {
+  cy.findByLabelText(lableText).type(inputText).type('{downarrow}').type('{enter}');
+});
+
+Cypress.Commands.add('selectDropDownValue', (lableText, currentValue, valueToBeSelected) => {
+  cy.contains(lableText).parent().contains(currentValue).click();
+  cy.contains(valueToBeSelected).click();
+});
+
+Cypress.Commands.add('uploadFile', filePath => {
+  cy.findByText('Choose file').click();
+  cy.get('input[type="file"]').attachFile(filePath);
 });
