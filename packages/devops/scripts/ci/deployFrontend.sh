4173ca27253a41443352db4425644b5f44708853
Edwin <edmofro@gmail.com>
2021-08-26 15:39:15 +1200

    NZ-917 Tamanu on the mind

--- packages/devops/scripts/ci/deployFrontend.sh
+++ packages/devops/scripts/ci/deployFrontend.sh
@@ -4,7 +4,7 @@ set -x
 PACKAGE=$1
 DIR=$(dirname "$0")
 DEPLOYMENT_SSH_URL=$(${DIR}/determineDeploymentSshUrl.sh)
-if [ -f "./tamanu_builds/deployment_exists" ]; then
+if [ -f "./tupaia_builds/deployment_exists" ]; then
     echo "Deployment for ${CI_BRANCH} exists, updating with latest build"
     scp ./tupaia_builds/${PACKAGE} ubuntu@$DEPLOYMENT_SSH_URL:/tupaia/packages/${PACKAGE}/served_build
 else
