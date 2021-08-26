a4366b0e6da7649903e6de40c6eb9f84aa4bb4f3
Rohan Port <rohan@beyondessential.com.au>
2021-08-26 19:47:05 +1000

    MEL-453: admin-panel-server spring cleaning 🎉 - Added @types/express/index.d.ts - Added explicit defintions for routes - Refactored validators

--- packages/admin-panel-server/src/viz-builder/dashboardVisualisation/validators.ts
+++ packages/admin-panel-server/src/viz-builder/dashboardVisualisation/validators.ts
@@ -5,70 +5,52 @@
 
 import { yup } from '@tupaia/utils';
 
-import { VisualisationValidator, DashboardVisualisationObject } from '../types';
+export const baseVisualisationValidator = yup.object().shape({
+  presentation: yup.object(),
+  data: yup.object(),
+});
 
-export class DraftReportValidator implements VisualisationValidator {
-  validationSchema: yup.ObjectSchema;
+export const draftReportValidator = yup.object().shape({
+  code: yup.string().required('Requires "code" for the visualisation'),
+  config: yup.object().shape({
+    fetch: yup.object().shape(
+      {
+        dataElements: yup
+          .array()
+          .of(yup.string())
+          .when('dataGroups', {
+            // dataGroups is required if there are no dataElements
+            is: (dataGroups: string[]) => !dataGroups || dataGroups.length === 0,
+            then: yup
+              .array()
+              .of(yup.string())
+              .required('Requires "dataGroups" or "dataElements"')
+              .min(1),
+            otherwise: yup.array().of(yup.string()),
+          }),
+        dataGroups: yup
+          .array()
+          .of(yup.string())
+          .when('dataElements', {
+            // dataElements is required if there are no dataGroups
+            is: (dataElements: string[]) => !dataElements || dataElements.length === 0,
+            then: yup
+              .array()
+              .of(yup.string())
+              .required('Requires "dataGroups" or "dataElements"')
+              .min(1),
+            otherwise: yup.array().of(yup.string()),
+          }),
+        aggregations: yup.array(),
+      },
+      [['dataElements', 'dataGroups']],
+    ),
+    transform: yup.array(),
+    output: yup.object(),
+  }),
+});
 
-  constructor() {
-    this.validationSchema = yup.object().shape({
-      code: yup.string().required('Requires "code" for the visualisation'),
-      data: yup.object().shape(
-        {
-          dataElements: yup
-            .array()
-            .of(yup.string())
-            .when('dataGroups', {
-              // dataGroups is required if there are no dataElements
-              is: (dataGroups: string[]) => !dataGroups || dataGroups.length === 0,
-              then: yup
-                .array()
-                .of(yup.string())
-                .required('Requires "dataGroups" or "dataElements"')
-                .min(1),
-              otherwise: yup.array().of(yup.string()),
-            }),
-          dataGroups: yup
-            .array()
-            .of(yup.string())
-            .when('dataElements', {
-              // dataElements is required if there are no dataGroups
-              is: (dataElements: string[]) => !dataElements || dataElements.length === 0,
-              then: yup
-                .array()
-                .of(yup.string())
-                .required('Requires "dataGroups" or "dataElements"')
-                .min(1),
-              otherwise: yup.array().of(yup.string()),
-            }),
-          aggregations: yup.array(),
-          transform: yup.array(),
-        },
-        ['dataElements', 'dataGroups'],
-      ),
-    });
-  }
-
-  validate(visualisation: DashboardVisualisationObject) {
-    this.validationSchema.validateSync(visualisation);
-  }
-}
-
-export class DraftDashboardItemValidator implements VisualisationValidator {
-  validationSchema: yup.ObjectSchema;
-
-  constructor() {
-    this.validationSchema = yup.object().shape({
-      code: yup.string().required('Requires "code" for the visualisation'),
-      presentation: yup.object().shape({
-        type: yup.string().required('Requires "type" in chart config'),
-        config: yup.object(),
-        output: yup.object(),
-      }),
-    });
-  }
-
-  validate(visualisation: DashboardVisualisationObject) {
-    this.validationSchema.validateSync(visualisation);
-  }
-}
+export const draftDashboardItemValidator = yup.object().shape({
+  code: yup.string().required('Requires "code" for the visualisation'),
+  config: yup.object().shape({ type: yup.string().required('Requires "type" in chart config') }),
+});
