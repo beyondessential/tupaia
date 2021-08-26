c89e65392705750cb89da70754cae251532eb3d7
Edwin <edmofro@gmail.com>
2021-08-26 18:25:07 +1200

    NZ-917 Refer to tupaia_builds from /root

--- packages/devops/scripts/ci/buildFrontend.sh
+++ packages/devops/scripts/ci/buildFrontend.sh
@@ -15,7 +15,7 @@ if [ -f "/root/tupaia_builds/deployment_exists" ]; then
 
     echo "Building"
     CI=false yarn workspace @tupaia/${PACKAGE} build # set CI to false to ignore warnings https://github.com/facebook/create-react-app/issues/3657
-    mv ./packages/${PACKAGE}/served_build/* ./tupaia_builds/${PACKAGE}
+    mv ./packages/${PACKAGE}/served_build/* /root/tupaia_builds/${PACKAGE}
 else
     echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
--- packages/devops/scripts/ci/deployFrontend.sh
+++ packages/devops/scripts/ci/deployFrontend.sh
@@ -6,7 +6,7 @@ DIR=$(dirname "$0")
 DEPLOYMENT_SSH_URL=$(${DIR}/determineDeploymentSshUrl.sh)
 if [ -f "/root/tupaia_builds/deployment_exists" ]; then
     echo "Deployment for ${CI_BRANCH} exists, updating with latest build"
-    scp ./tupaia_builds/${PACKAGE} ubuntu@$DEPLOYMENT_SSH_URL:/tupaia/packages/${PACKAGE}/served_build
+    scp /root/tupaia_builds/${PACKAGE} ubuntu@$DEPLOYMENT_SSH_URL:/tupaia/packages/${PACKAGE}/served_build
 else
     echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
