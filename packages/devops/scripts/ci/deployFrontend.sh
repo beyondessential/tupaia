76595533032c47915ef056eee6054362770ce972
Edwin <edmofro@gmail.com>
2021-08-26 20:48:50 +1200

    NZ-917 Correct remote directory

--- packages/devops/scripts/ci/deployFrontend.sh
+++ packages/devops/scripts/ci/deployFrontend.sh
@@ -6,7 +6,7 @@ DIR=$(dirname "$0")
 DEPLOYMENT_SSH_URL=$(${DIR}/determineDeploymentSshUrl.sh)
 if [ -f "/root/tupaia_builds/deployment_exists" ]; then
     echo "Deployment for ${CI_BRANCH} exists, updating with latest build"
-    scp -o StrictHostKeyChecking=no -r /root/tupaia_builds/${PACKAGE} ubuntu@$DEPLOYMENT_SSH_URL:/tupaia/packages/${PACKAGE}
+    scp -o StrictHostKeyChecking=no -r /root/tupaia_builds/${PACKAGE} ubuntu@$DEPLOYMENT_SSH_URL:/home/tupaia/packages/${PACKAGE}
 else
     echo "No deployment exists for ${CI_BRANCH}, cancelling update"
 fi
