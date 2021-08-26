aff76aae7469eb7161b80c22b479807faae1d591
Edwin <edmofro@gmail.com>
2021-08-27 07:04:48 +1200

    NZ-917 Combine build scripts

--- packages/devops/scripts/ci/buildPackage.sh
+++ packages/devops/scripts/ci/buildPackage.sh
@@ -14,10 +14,17 @@ if [ -f "/root/tupaia_builds/deployment_exists" ]; then
 
     echo "Building"
     CI=false yarn workspace @tupaia/${PACKAGE} build # set CI to false to ignore warnings https://github.com/facebook/create-react-app/issues/3657
-    mkdir -p /root/tupaia_builds/${PACKAGE}
-    mv ./packages/${PACKAGE}/.env /root/tupaia_builds/${PACKAGE}
-    mv ./packages/${PACKAGE}/dist /root/tupaia_builds/${PACKAGE}
-
+    mkdir -p /root/tupaia_builds # in case tupaia_builds doesn't yet exist
+    rm -rf /root/tupaia_builds/${PACKAGE} # in the odd case that build might be running a second time
+    if [[ $PACKAGE == *server ]]; then
+        # back end package, move dist and .env
+        mkdir /root/tupaia_builds/${PACKAGE}
+        mv ./packages/${PACKAGE}/dist /root/tupaia_builds/${PACKAGE}/dist
+        mv ./packages/${PACKAGE}/.env /root/tupaia_builds/${PACKAGE}/.env
+    else
+        # front end package, move contents of build folder
+        mv ./packages/${PACKAGE}/build /root/tupaia_builds/${PACKAGE}
+    fi
 else
     echo "No deployment exists for ${CI_BRANCH}, skipping build"
 fi
