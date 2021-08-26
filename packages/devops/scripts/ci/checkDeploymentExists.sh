41418ac9f99f22cb3f0f10d880b4733239145ee6
Edwin <edmofro@gmail.com>
2021-08-26 15:41:43 +1200

    NZ-917 Use common volume correctly

--- packages/devops/scripts/ci/checkDeploymentExists.sh
+++ packages/devops/scripts/ci/checkDeploymentExists.sh
@@ -4,5 +4,5 @@ set -x
 DIR=$(dirname "$0")
 DEPLOYMENT_SSH_URL=$(${DIR}/determineDeploymentSshUrl.sh)
 if curl --output /dev/null --silent --head --fail $DEPLOYMENT_SSH_URL; then
-  touch ./tupaia_builds/deployment_exists
+  touch /root/tupaia_builds/deployment_exists
 fi
