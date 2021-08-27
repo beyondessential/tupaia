c7bee9667665be61e650fdc728e20130e392312d
Edwin <edmofro@gmail.com>
2021-08-27 14:41:44 +1200

    NZ-917 Substitute branch-name in environment variables

--- packages/devops/scripts/ci/buildPackage.sh
+++ packages/devops/scripts/ci/buildPackage.sh
@@ -3,14 +3,27 @@ set -x
 
 PACKAGE=$1
 
-if [ -f "/root/tupaia_builds/deployment_exists" ]; then
+if [ -f "./tupaia_builds/deployment_exists" ]; then
     echo "Deployment for ${CI_BRANCH} exists, building ${PACKAGE}"
 
     echo "Pulling environment variables"
     set +x # do not output next command, as it would show credentials in plain text
     echo "${LASTPASS_PASSWORD}" | lpass login ${LASTPASS_EMAIL}
     set -x
-    lpass show --notes ${PACKAGE}.dev.env > ./packages/${PACKAGE}/.env
+
+    # checkout branch specific env vars, or dev as fallback
+    lpass show --notes ${PACKAGE}.${CI_BRANCH}.env > ./packages/${PACKAGE}/.env || lpass show --notes ${PACKAGE}.dev.env > ./packages/${PACKAGE}/.env
+
+    # Replace any instances of the placeholder [branch-name] in the .env file with the actual branch
+    # name (e.g. [branch-name]-api.tupaia.org -> specific-branch-api.tupaia.org)
+    sed -i -e "s/\[branch-name\]/${BRANCH}/g" .env
+
+    if [[ "${CI_BRANCH}" == *-e2e || "${CI_BRANCH}" == e2e ]]; then
+        # Update e2e environment variables
+        if [[ ${PACKAGE} == "meditrak-server" || ${PACKAGE} == "web-config-server" ]]; then
+            sed -i -E 's/^AGGREGATION_URL_PREFIX="?dev-"?$/AGGREGATION_URL_PREFIX=e2e-/g' .env
+        fi
+    fi
 
     echo "Building"
     CI=false yarn workspace @tupaia/${PACKAGE} build # set CI to false to ignore warnings https://github.com/facebook/create-react-app/issues/3657
