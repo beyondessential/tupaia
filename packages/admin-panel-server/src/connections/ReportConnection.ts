92eac23013cb6241eb89f2dde4f1fe5cd792a817
Igor Nadj <igor@beyondessential.com.au>
2021-08-27 14:34:41 +1000

    Revert "Delete unused connection classes"
    This reverts commit baed1c7379b65618861c7a95c0aad279700df2be.

---
+++ packages/admin-panel-server/src/connections/ReportConnection.ts
@@ -0,0 +1,16 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+import { QueryParameters, ApiConnection } from '@tupaia/api-client';
+
+const { REPORT_API_URL = 'http://localhost:8030/v2' } = process.env;
+
+export class ReportConnection extends ApiConnection {
+  baseUrl = REPORT_API_URL;
+
+  async fetchReport(reportCode: string, query: QueryParameters) {
+    return this.get(`fetchReport/${reportCode}`, query);
+  }
+}
