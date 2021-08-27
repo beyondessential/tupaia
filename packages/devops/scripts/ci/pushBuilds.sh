ab02c212582926e1d73068bff8373a8a349d97d6
Edwin <edmofro@gmail.com>
2021-08-27 19:40:11 +1200

    NZ-917 Overwrite old files when uncompressing

--- packages/devops/scripts/ci/pushBuilds.sh
+++ packages/devops/scripts/ci/pushBuilds.sh
@@ -7,8 +7,8 @@ if [ -f "/root/tupaia/deployment_exists" ]; then
     echo "Deployment for ${CI_BRANCH} exists, updating with latest builds"
     DEPLOYMENT_SSH_URL=$(${DIR}/determineDeploymentSshUrl.sh)
     cd /root && tar -zcvf tupaia.tar.gz tupaia
-    scp -o StrictHostKeyChecking=no -p /root/tupaia.tar.gz ubuntu@$DEPLOYMENT_SSH_URL:./_incoming_build
-    ssh -o ServerAliveInterval=15 ubuntu@$DEPLOYMENT_SSH_URL "cd _incoming_build && tar -zxvf tupaia.tar.gz && rm tupaia.tar.gz && cd .. && rm -rf tupaia && mv _incoming_build/tupaia . && rm -rf _incoming_build"
+    scp -o StrictHostKeyChecking=no -p /root/tupaia.tar.gz ubuntu@$DEPLOYMENT_SSH_URL:./tupaia.tar.gz
+    ssh -o ServerAliveInterval=15 ubuntu@$DEPLOYMENT_SSH_URL "tar -zxvf tupaia.tar.gz && rm tupaia.tar.gz"
 else
     echo "No deployment exists for ${CI_BRANCH}, skipping push builds"
 fi
