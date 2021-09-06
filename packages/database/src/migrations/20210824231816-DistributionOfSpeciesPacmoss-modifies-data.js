c3b159ccc400260263fc86445ffe92c6b3ab7131
billli0 <lbit123@outlook.com>
2021-09-06 14:40:25 +1000

    add async

--- packages/database/src/migrations/20210824231816-DistributionOfSpeciesPacmoss-modifies-data.js
+++ packages/database/src/migrations/20210824231816-DistributionOfSpeciesPacmoss-modifies-data.js
@@ -161,7 +161,7 @@ exports.up = async function (db) {
   );
 };
 
-exports.down = function (db) {
+exports.down = async function (db) {
   Promise.all(
     SPECIES.map(async ([_, species]) => {
       HIERARCHY_LEVELS.map(async hierarchyLevel => {
