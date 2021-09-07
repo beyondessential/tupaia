6ce8ae4538e9f42a23e9a35d4d9f4fb837ea03d6
Edwin <edmofro@gmail.com>
2021-09-07 16:24:20 +1200

    NZ-917 Revert extra packages needing env files

--- scripts/bash/getPackagesWithEnvFiles.sh
+++ scripts/bash/getPackagesWithEnvFiles.sh
@@ -2,8 +2,8 @@
 
 DIR=$(dirname "$0")
 
-# packages with .env files are (currently) all deployable, plus a few extras
+# packages with .env files are (currently) all deployable, plus a auth and data-api
 PACKAGES=$(${DIR}/getDeployablePackages.sh)
-PACKAGES+=" auth data-api entity-server report-server"
+PACKAGES+=" auth data-api"
 echo $PACKAGES
 exit 0
