89cab5eb208b8de42639d3015dc56d9cb34b123c
Rohan Port <rohan@beyondessential.com.au>
2021-09-09 09:55:03 +1000

    SOL-410: Fixed expression-parser functions missing from report-server

--- packages/report-server/src/reportBuilder/transform/parser/TransformParser.ts
+++ packages/report-server/src/reportBuilder/transform/parser/TransformParser.ts
@@ -125,7 +125,7 @@ export class TransformParser extends ExpressionParser {
   };
 
   protected getCustomFunctions() {
-    return functions;
+    return { ...super.getCustomFunctions(), ...functions };
   }
 
   public static isExpression(expression: string) {
