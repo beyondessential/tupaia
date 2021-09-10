7c063f84798ab737be2977b1131fa428dc497252
Edwin <edmofro@gmail.com>
2021-09-10 13:32:18 +1200

    NZ-917 Pull database .env during ci/cd

--- scripts/bash/getPackagesWithEnvFiles.sh
+++ scripts/bash/getPackagesWithEnvFiles.sh
@@ -2,8 +2,8 @@
 
 DIR=$(dirname "$0")
 
-# packages with .env files are (currently) all deployable, plus auth and data-api
+# packages with .env files are (currently) all deployable, plus auth, data-api, and database
 PACKAGES=$(${DIR}/getDeployablePackages.sh)
-PACKAGES+=" auth data-api"
+PACKAGES+=" auth data-api database"
 echo $PACKAGES
 exit 0
