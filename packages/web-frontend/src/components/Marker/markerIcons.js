e2f93f111add49dea235ab3737cfd3f7270cbced
Tom Caiger <caigertom@gmail.com>
2021-09-10 14:44:44 +1200

    make svgs components

--- packages/web-frontend/src/components/Marker/markerIcons.js
+++ packages/web-frontend/src/components/Marker/markerIcons.js
@@ -21,11 +21,11 @@ import Help from '@material-ui/icons/Help';
 import CheckBox from '@material-ui/icons/CheckBox';
 
 // from https://thenounproject.com/ochavisual/collection/ocha-humanitarian-icons/
-import { ReactComponent as Cyclone } from '../../images/cyclone.svg';
-import { ReactComponent as Earthquake } from '../../images/earthquake.svg';
-import { ReactComponent as Tsunami } from '../../images/tsunami.svg';
-import { ReactComponent as Volcano } from '../../images/volcano.svg';
-import { ReactComponent as Flood } from '../../images/flood.svg';
+import { Cyclone } from '../../icons/Cyclone';
+import { Earthquake } from '../../icons/Earthquake';
+import { Tsunami } from '../../icons/Tsunami';
+import { Volcano } from '../../icons/Volcano';
+import { Flood } from '../../icons/Flood';
 import { BREWER_PALETTE, WHITE } from '../../styles';
 
 // allows passing a color to a material icon & scales it down a bit
