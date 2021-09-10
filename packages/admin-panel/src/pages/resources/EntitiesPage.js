296b9c3148a57760fa49049ec7858cdb032037d7
nick <nickw@ourcomunity.com.au>
2021-09-10 16:35:43 +1000

    Add Atuo geo fetch parent_code note

--- packages/admin-panel/src/pages/resources/EntitiesPage.js
+++ packages/admin-panel/src/pages/resources/EntitiesPage.js
@@ -65,7 +65,7 @@ const IMPORT_CONFIG = {
       },
     },
     {
-      label: 'Automatically fetch GeoJSON (defaults to "Yes")',
+      label: 'Automatically fetch GeoJSON (defaults to "Yes", requires parent_code)',
       parameterKey: 'automaticallyFetchGeojson',
       type: 'boolean',
       editConfig: {
