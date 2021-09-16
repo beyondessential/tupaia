3c41baae409eb2c566972c2aaee7a5d0198eed7b
Edwin <edmofro@gmail.com>
2021-09-16 13:00:20 +1200

    Remove inaccessible code

--- scripts/bash/validate.sh
+++ scripts/bash/validate.sh
@@ -5,6 +5,3 @@ DIR=$(dirname "$0")
 
 yarn -s workspace @tupaia/devops -s validate-branch-name
 yarn -s workspace @tupaia/devops -s validate-tests
-if [[ $? > 0 ]]; then
-    log_warn "Exclusive (.only) tests found in your code. The CI/CD build will fail if you push those"
-fi
