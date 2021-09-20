01d1e55d7908286f9bfc4d356c09d2cb370acefc
Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
2021-09-20 12:41:13 +1200

    WAI-952: Add remote tracking of correct branch, if it exists (#3250)

--- packages/devops/scripts/deployment/checkoutLatest.sh
+++ packages/devops/scripts/deployment/checkoutLatest.sh
@@ -3,11 +3,19 @@
 # Get latest code and dependencies
 echo "Checking out ${BRANCH}, or dev if that doesn't exist"
 cd ${HOME_DIRECTORY}
+BRANCH_ON_REMOTE=$(git ls-remote --heads origin ${BRANCH})
+if [[ $BRANCH_ON_REMOTE == *${BRANCH} ]]; then
+  echo "${BRANCH} exists"
+  BRANCH_TO_USE=${BRANCH}
+else
+  echo "${BRANCH} does not exist, defaulting to dev"
+  BRANCH_TO_USE="dev"
+fi
+git remote set-branches --add origin ${BRANCH_TO_USE}
 git fetch --all --prune
-git checkout dev # Ensure we have dev as our default, if the specified branch doesn't exist
-git reset --hard origin/dev
-git checkout $BRANCH # Now try the requested branch
-git reset --hard origin/${BRANCH}
+git reset --hard # clear out any manual changes that have been made, which would cause checkout to fail
+git checkout ${BRANCH_TO_USE}
+git reset --hard origin/${BRANCH_TO_USE}
 yarn install
 
 echo "Checked out latest code"
