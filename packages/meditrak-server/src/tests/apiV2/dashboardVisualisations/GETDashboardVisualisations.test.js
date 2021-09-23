bacfbfc82e36a3cea034c8d587452193f19aedbb
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-09-23 12:21:40 +1000

    Allow getting legacy vizes

---
+++ packages/meditrak-server/src/tests/apiV2/dashboardVisualisations/GETDashboardVisualisations.test.js
@@ -0,0 +1,111 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+import { setupTest } from '@tupaia/database';
+import { camelKeys } from '@tupaia/utils';
+
+import { expect } from 'chai';
+import { resetTestData, TestableApp } from '../../testUtilities';
+import { findTestRecordByCode, TEST_SETUP } from './dashboardVisualisations.fixtures';
+
+describe('GET dashboard visualisations', () => {
+  const expectError = (response, error) => {
+    const { body, statusCode } = response;
+    const errorMatch = typeof error === 'string' ? new RegExp(error) : error;
+
+    expect(statusCode).to.equal(500);
+    expect(body).to.have.property('error');
+    expect(body.error).to.match(errorMatch);
+  };
+
+  const expectSuccess = (response, expectedBody) => {
+    const { body, statusCode } = response;
+
+    expect(statusCode).to.equal(200, body.error);
+    expect(body).to.deep.equal(expectedBody);
+  };
+
+  const getVizId = code => findTestRecordByCode('dashboardItem', code).id;
+
+  const app = new TestableApp();
+  const { models } = app;
+
+  const modernDashboardItem = findTestRecordByCode('dashboardItem', 'Modern_Dashboard_Item');
+  const legacyDashboardItem = findTestRecordByCode('dashboardItem', 'Legacy_Dashboard_Item');
+  const modernReport = findTestRecordByCode('report', 'Modern_Report');
+  const legacyReport = findTestRecordByCode('legacyReport', 'Legacy_Report');
+
+  const MODERN_DASHBOARD_VISUALISATION = {
+    dashboardItem: camelKeys(modernDashboardItem),
+    report: {
+      code: modernReport.code,
+      config: modernReport.config,
+      permissionGroup: 'Viz_Permissions',
+    },
+  };
+  const LEGACY_DASHBOARD_VISUALISATION = {
+    dashboardItem: camelKeys(legacyDashboardItem),
+    report: {
+      code: legacyReport.code,
+      dataBuilder: legacyReport.data_builder,
+      config: legacyReport.data_builder_config,
+      dataServices: JSON.parse(legacyReport.data_services),
+    },
+  };
+
+  before(async () => {
+    await resetTestData();
+    await app.grantFullAccess();
+    await setupTest(models, TEST_SETUP);
+  });
+
+  after(() => {
+    app.revokeAccess();
+  });
+
+  describe('GET /dashboardVisualisations/:id', () => {
+    it('Throws error if id or code is not provided', async () => {
+      const response = await app.get('dashboardVisualisations/invalid_id');
+      expectError(response, /visualisation does not exist/i);
+    });
+
+    it('Throws error if  visualisation has no report', async () => {
+      const id = getVizId('Dashboard_Item_No_Report');
+      const response = await app.get(`dashboardVisualisations/${id}`);
+      expectError(response, 'Cannot find a report for visualisation');
+    });
+
+    it('Throws error if dashboard visualisation has no report', async () => {
+      const id = getVizId('Dashboard_Item_Invalid_Report');
+      const response = await app.get(`dashboardVisualisations/${id}`);
+      expectError(response, 'Cannot find a report for visualisation');
+    });
+
+    it('Returns an existing modern dashboard visualisation', async () => {
+      const id = getVizId('Modern_Dashboard_Item');
+      const response = await app.get(`dashboardVisualisations/${id}`);
+      expectSuccess(response, MODERN_DASHBOARD_VISUALISATION);
+    });
+
+    it('Returns an existing legacy dashboard visualisation', async () => {
+      const id = getVizId('Legacy_Dashboard_Item');
+      const response = await app.get(`dashboardVisualisations/${id}`);
+      expectSuccess(response, LEGACY_DASHBOARD_VISUALISATION);
+    });
+  });
+
+  describe('GET /dashboardVisualisations', () => {
+    it('Returns existing dashboard visualisations, filtered by code', async () => {
+      const response = await app.get('dashboardVisualisations', {
+        query: {
+          filter: {
+            code: ['Modern_Dashboard_Item', 'Legacy_Dashboard_Item'],
+          },
+        },
+      });
+      expectSuccess(response, [MODERN_DASHBOARD_VISUALISATION, LEGACY_DASHBOARD_VISUALISATION]);
+    });
+  });
+});
---
+++ packages/meditrak-server/src/tests/apiV2/dashboardVisualisations/dashboardVisualisations.fixtures.js
@@ -0,0 +1,89 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+import { generateTestId } from '@tupaia/database';
+
+const permissionGroupId = generateTestId();
+
+const DASHBOARD_ITEMS = [
+  {
+    id: generateTestId(),
+    code: 'Modern_Dashboard_Item',
+    config: { name: 'Modern Dashboard Item', type: 'view', viewType: 'singleValue' },
+    report_code: 'Modern_Report',
+    legacy: false,
+  },
+  {
+    id: generateTestId(),
+    code: 'Legacy_Dashboard_Item',
+    config: { name: 'Legacy Dashboard Item', type: 'chart', chartType: 'bar' },
+    report_code: 'Legacy_Report',
+    legacy: true,
+  },
+  {
+    id: generateTestId(),
+    code: 'Dashboard_Item_No_Report',
+    config: {},
+  },
+  {
+    id: generateTestId(),
+    code: 'Dashboard_Item_Invalid_Report',
+    config: {},
+    report_code: 'Invalid_Report',
+  },
+];
+
+const PERMISSION_GROUPS = [
+  {
+    id: permissionGroupId,
+    name: 'Viz_Permissions',
+  },
+];
+
+const REPORTS = [
+  {
+    code: 'Modern_Report',
+    config: {
+      fetch: {
+        dataElements: ['BCD1'],
+        aggregations: ['FINAL_EACH_YEAR'],
+      },
+      transform: [{ by: '=$dataElement', transform: 'sortRows' }],
+    },
+    permission_group_id: permissionGroupId,
+  },
+];
+
+const LEGACY_REPORTS = [
+  {
+    code: 'Legacy_Report',
+    data_builder: 'analytics',
+    data_builder_config: {
+      dataElementCodes: ['BCD1'],
+    },
+    data_services: JSON.stringify([{ isDataRegional: true }]),
+  },
+];
+
+export const TEST_SETUP = {
+  dbRecords: {
+    dashboardItem: DASHBOARD_ITEMS,
+    permissionGroup: PERMISSION_GROUPS,
+    report: REPORTS,
+    legacyReport: LEGACY_REPORTS,
+  },
+};
+
+export const findTestRecordByCode = (recordType, code) => {
+  const recordsForType = TEST_SETUP.dbRecords[recordType] || [];
+  const fixture = recordsForType.find(record => record.code === code);
+  if (!fixture) {
+    throw new Error(
+      `Test error: could not find fixture with type '${recordType}' and code '${code}'`,
+    );
+  }
+
+  return fixture;
+};
