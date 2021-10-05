88c9555f5d047f375163ef255ac2f049d7719387
Tom Caiger <caigertom@gmail.com>
2021-10-06 10:08:04 +1300

    add http-proxy-middleware to server boiler plate

--- packages/admin-panel/.babelrc.js
+++ packages/admin-panel/.babelrc.js
@@ -20,11 +20,6 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true,
-      },
-    ],
+    ['@babel/plugin-proposal-class-properties'],
   ],
 };
