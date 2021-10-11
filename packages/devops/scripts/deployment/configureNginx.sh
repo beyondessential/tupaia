b6928a0c5851f243d1329bee64b2e6990a5d1f42
Edwin <edmofro@gmail.com>
2021-10-12 11:40:14 +1300

    Copy nginx as well as servers configs

--- packages/devops/scripts/deployment/configureNginx.sh
+++ packages/devops/scripts/deployment/configureNginx.sh
@@ -7,6 +7,7 @@ TUPAIA_DIR=$DIR/../../../..
 service nginx stop
 
 # Copy servers.conf to proper nginx location
+cp ${TUPAIA_DIR}/packages/devops/configs/nginx.conf /etc/nginx/nginx.conf
 cp ${TUPAIA_DIR}/packages/devops/configs/servers.conf /etc/nginx/conf.d/servers.conf
 
 cp ${TUPAIA_DIR}/packages/devops/misc/error_page.html ${TUPAIA_DIR}/error_page.html
