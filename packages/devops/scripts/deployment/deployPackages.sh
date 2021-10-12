6fce47d540af77fc9bad85577740fc55599ce841
Edwin <edmofro@gmail.com>
2021-10-12 15:16:04 +1300

    Turn migrations back on

--- packages/devops/scripts/deployment/deployPackages.sh
+++ packages/devops/scripts/deployment/deployPackages.sh
@@ -41,22 +41,22 @@ for PACKAGE in ${PACKAGES[@]}; do
         REACT_APP_BRANCH=${BRANCH} yarn build
     fi
 
-    # if [[ $PACKAGE == 'meditrak-server' ]]; then
-    #     # reset cwd back to `/tupaia`
-    #     cd ${TUPAIA_DIR}
+    if [[ $PACKAGE == 'meditrak-server' ]]; then
+        # reset cwd back to `/tupaia`
+        cd ${TUPAIA_DIR}
 
-    #     # now that meditrak-server is up and listening for changes, we can run any migrations
-    #     # if run earlier when meditrak-server isn't listening, changes will be missed from the
-    #     # sync queues
-    #     echo "Migrating the database"
-    #     yarn migrate
+        # now that meditrak-server is up and listening for changes, we can run any migrations
+        # if run earlier when meditrak-server isn't listening, changes will be missed from the
+        # sync queues
+        echo "Migrating the database"
+        yarn migrate
 
-    #     # After running migrations it's good to ensure that the analytics table is fully built
-    #     yarn download-env-vars $BRANCH data-api
-    #     echo "Building analytics table"
-    #     yarn workspace @tupaia/data-api install-mv-refresh
-    #     yarn workspace @tupaia/data-api build-analytics-table
-    # fi
+        # After running migrations it's good to ensure that the analytics table is fully built
+        yarn download-env-vars $BRANCH data-api
+        echo "Building analytics table"
+        yarn workspace @tupaia/data-api install-mv-refresh
+        yarn workspace @tupaia/data-api build-analytics-table
+    fi
 done
 
 echo "Finished deploying latest"
