f357d4c635f935ea9b322b8399f3d5cb87717d34
Igor Nadj <igor@beyondessential.com.au>
2021-10-18 15:52:14 +1100

    temp

--- packages/meditrak-server/src/tests/apiV2/surveyResponse.test.js
+++ packages/meditrak-server/src/tests/apiV2/surveyResponse.test.js
@@ -39,7 +39,7 @@ const expectError = (response, expectedError) => expectErrors(response, expected
 
 let surveyId;
 
-describe('surveyResponse endpoint', () => {
+describe.only('surveyResponse endpoint', () => {
   const app = new TestableApp();
   const { models } = app;
 
