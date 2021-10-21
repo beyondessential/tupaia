db675c95e97c76e1eaa9ab74d756bc00f4f4e6e7
Edwin <edmofro@gmail.com>
2021-10-21 15:43:12 +1300

    Allow cloning running instances

--- packages/devops/scripts/lambda/actions/refresh_cloned_instances.py
+++ packages/devops/scripts/lambda/actions/refresh_cloned_instances.py
@@ -25,14 +25,13 @@
 import asyncio
 
 from helpers.clone import clone_volume_into_instance
-from helpers.utilities import find_instances, get_tag
+from helpers.utilities import find_instances, get_tag, start_instance, stop_instance
 
 loop = asyncio.get_event_loop()
 
 def refresh_cloned_instances(event):
     filters = [
-        { 'Name': 'tag-key', 'Values': ['ClonedFrom'] },
-        { 'Name': 'instance-state-name', 'Values': ['stopped'] }
+        { 'Name': 'tag-key', 'Values': ['ClonedFrom'] }
     ]
     if 'ClonedFrom' in event:
       print('Refreshing instances cloned from ' + event['ClonedFrom'])
@@ -41,15 +40,32 @@ def refresh_cloned_instances(event):
       print('Refreshing the ' + event['Branch'] + ' branch clone')
       filters.append({'Name': 'tag:Stage', 'Values': [event['Branch']]})
 
-    instances = find_instances(filters)
+
+    running_instances = find_instances(filters + [{ 'Name': 'instance-state-name', 'Values': ['running'] }])
+    stopped_instances = find_instances(filters + [{ 'Name': 'instance-state-name', 'Values': ['stopped'] }])
+
+    instances = running_instances + stopped_instances
 
     if len(instances) == 0:
       print('No clones to refresh')
       return
 
-    tasks = sum(
+    stop_tasks = sum(
+    [
+        [asyncio.ensure_future(stop_instance(instance)) for instance in instances]
+    ], [])
+    loop.run_until_complete(asyncio.wait(stop_tasks))
+
+    clone_tasks = sum(
     [
         [asyncio.ensure_future(clone_volume_into_instance(instance, get_tag(instance, 'ClonedFrom'))) for instance in instances]
     ], [])
-    loop.run_until_complete(asyncio.wait(tasks))
+    loop.run_until_complete(asyncio.wait(clone_tasks))
+
+    start_tasks = sum(
+    [
+        [asyncio.ensure_future(start_instance(instance)) for instance in instances]
+    ], [])
+    loop.run_until_complete(asyncio.wait(start_tasks))
+
     print('Finished refreshing all clones')
