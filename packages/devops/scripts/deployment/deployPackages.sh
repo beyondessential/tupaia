237d06e3121fe84369fb49409bfe5fc40dcb3809
Edwin <edmofro@gmail.com>
2021-10-22 11:10:04 +1300

    Speed up deployment of packages

--- packages/devops/scripts/deployment/deployPackages.sh
+++ packages/devops/scripts/deployment/deployPackages.sh
@@ -17,47 +17,82 @@ LASTPASS_EMAIL=$($DIR/fetchParameterStoreValue.sh LASTPASS_EMAIL)
 LASTPASS_PASSWORD=$($DIR/fetchParameterStoreValue.sh LASTPASS_PASSWORD)
 LASTPASS_EMAIL=$LASTPASS_EMAIL LASTPASS_PASSWORD=$LASTPASS_PASSWORD yarn download-env-vars $BRANCH
 
-# For each package, get the latest and deploy it
+# Build each front end package
+front_end_build_batch_size=3 # max 3 concurrent builds to avoid running out of memory
+front_end_build_pids=()
+front_end_build_index=0
 for PACKAGE in ${PACKAGES[@]}; do
-    # reset cwd back to `/tupaia`
-    cd ${TUPAIA_DIR}
+    if [[ $PACKAGE != *server ]]; then
+        # It's a front end package, build it in the background
+        echo "Building ${PACKAGE}"
+        cd ${TUPAIA_DIR}/packages/$PACKAGE
+        if [[ $PACKAGE == "web-frontend" ]]; then
+            # The package web-frontend has a desktop and a mobile version, build both in background
 
-    cd ${TUPAIA_DIR}/packages/$PACKAGE
+            # Build desktop version
+            REACT_APP_BRANCH=${BRANCH} BUILD_PATH=/build/desktop mkdir -p build/desktop && yarn build-desktop &
+            front_end_build_pids[${i}]=$!
+            front_end_build_index=front_end_build_index+1
 
-    # If it's a server, start it running on pm2, otherwise build it
-    echo "Preparing to start or build ${PACKAGE}"
+            # Build mobile version
+            REACT_APP_BRANCH=${BRANCH} BUILD_PATH=/build/mobile mkdir -p build/mobile && yarn build-mobile &
+            front_end_build_pids[${i}]=$!
+            front_end_build_index=front_end_build_index+1
+        else
+            # Build package in background
+            REACT_APP_BRANCH=${BRANCH} yarn build &
+            front_end_build_pids[${i}]=$!
+            front_end_build_index=front_end_build_index+1
+        fi
+    fi
+
+    # If we've already started a few builds, wait for those to finish before moving on, lest we start
+    # too many and run out of memory
+    if (( front_end_build_index > front_end_build_batch_size - 1 ))
+        for pid in ${front_end_build_pids[*]}; do
+            wait $pid
+        done
+        front_end_build_pids=()
+        front_end_build_index=0
+    fi
+done
+
+# Wait for all front end builds to complete
+for pid in ${front_end_build_pids[*]}; do
+    wait $pid
+done
+
+# Build and start back end server packages
+for PACKAGE in ${PACKAGES[@]}; do
     if [[ $PACKAGE == *server ]]; then
         # It's a server, start the pm2 process
-        echo "Starting ${PACKAGE}"
+        echo "Building ${PACKAGE}"
+        cd ${TUPAIA_DIR}/packages/$PACKAGE
         yarn build
 
+        echo "Starting ${PACKAGE}"
         REPLICATION_PM2_CONFIG=''
         if [[ $PACKAGE == "web-config-server" ]]; then
             # as many replicas as cpu cores - 1
             REPLICATION_PM2_CONFIG='-i -1'
         fi
-
         pm2 start --name $PACKAGE dist --wait-ready --listen-timeout 15000 --time $REPLICATION_PM2_CONFIG
-    else
-        # It's a static package, build it
-        echo "Building ${PACKAGE}"
-        REACT_APP_BRANCH=${BRANCH} yarn build
-    fi
 
-    if [[ $PACKAGE == 'meditrak-server' ]]; then
-        # reset cwd back to `/tupaia`
-        cd ${TUPAIA_DIR}
+        if [[ $PACKAGE == 'meditrak-server' ]]; then
+            # reset cwd back to `/tupaia`
+            cd ${TUPAIA_DIR}
 
-        # now that meditrak-server is up and listening for changes, we can run any migrations
-        # if run earlier when meditrak-server isn't listening, changes will be missed from the
-        # sync queues
-        echo "Migrating the database"
-        yarn migrate
+            # now that meditrak-server is up and listening for changes, we can run any migrations
+            # if run earlier when meditrak-server isn't listening, changes will be missed from the
+            # sync queues
+            echo "Migrating the database"
+            yarn migrate
 
-        # After running migrations it's good to ensure that the analytics table is fully built
-        echo "Building analytics table"
-        yarn workspace @tupaia/data-api install-mv-refresh
-        yarn workspace @tupaia/data-api build-analytics-table
+            # After running migrations it's good to ensure that the analytics table is fully built
+            echo "Building analytics table"
+            yarn workspace @tupaia/data-api install-mv-refresh
+            yarn workspace @tupaia/data-api build-analytics-table
+        fi
     fi
 done
 
