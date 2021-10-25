a254efe296965e7d5363c3532701d1568b04facf
Rohan Port <rohan@beyondessential.com.au>
2021-10-25 11:59:45 +1100

    RN-190: Added support for using '*' in matrix column names array - Can be used to achieve ordering of columns even if some of them are dynamic - Moved tests from output.test.ts -> matrix.test.ts as all tests were for the matrix output type

--- packages/report-server/src/reportBuilder/output/functions/matrix/matrix.ts
+++ packages/report-server/src/reportBuilder/output/functions/matrix/matrix.ts
@@ -62,7 +62,7 @@ const matrix = (rows: Row[], params: MatrixParams): Matrix => {
 const buildParams = (params: unknown): MatrixParams => {
   const validatedParams = paramsValidator.validateSync(params);
   const { columns, categoryField, rowField } = validatedParams;
-  const includeFields = columns || '*';
+  const includeFields = Array.isArray(columns) ? columns : ['*'];
 
   const excludeFields = categoryField ? [categoryField, rowField] : [rowField];
 
