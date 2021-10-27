348a7046e3ddb27e62a83971d3617abb601f58b4
Alastair-L <lockealastair@gmail.com>
2021-10-27 11:15:43 +1100

    Separate tests more logically

--- packages/meditrak-server/src/tests/apiV2/export/exportSurveys/configCellBuilders/fixtures/ArithmeticConfigCellBuilderFixtures.js
+++ packages/meditrak-server/src/tests/apiV2/export/exportSurveys/configCellBuilders/fixtures/ArithmeticConfigCellBuilderFixtures.js
@@ -10,6 +10,10 @@ export const ARITHMETIC_TEST_CASES = [
   },
   {
     description: 'Question code translation',
+    config: 'formula: $question_1_code + $question_2_code',
+  },
+  {
+    description: 'Duplicate question code translation',
     config: 'formula: $question_1_code + $question_2_code + ($question_1_code * 2)',
   },
 ];
