571439d9d64f4f86f9dfc7a277f9238411571ffa
Edwin <edmofro@gmail.com>
2021-10-29 15:26:00 +1300

    Enable codeship to work via triggering a redeploy

--- packages/devops/scripts/ci/testInternalDependencies.sh
+++ packages/devops/scripts/ci/testInternalDependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 DIR=$(dirname "$0")
-${DIR}/copyFromCommonVolume.sh
+${DIR}/copyInEnvironmentVariables.sh
 for PACKAGE in $(${DIR}/../../../../scripts/bash/getInternalDependencies.sh); do
     # skip the following packages - they get tested separately as they require db access
     if [[ "$PACKAGE" == "database" || "$PACKAGE" == "data-api" || "$PACKAGE" == "auth" || "$PACKAGE" == "indicators" || "$PACKAGE" == "server-boilerplate" ]]; then
---
+++ packages/devops/scripts/ci/triggerRedeploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+DIR=$(dirname "$0")
+
+if [[ $CI_BRANCH == "master" ]]; then
+    DEPLOYMENT_URL="tupaia.org"
+else
+    DEPLOYMENT_URL="${CI_BRANCH}.tupaia.org"
+fi
+
+if curl --output /dev/null --silent --head --fail $DEPLOYMENT_URL; then
+  echo "Existing deployment, triggering redeploy"
+  RESPONSE_FILE=lambda_swap_out_response.json
+  aws lambda invoke \
+    --function-name deployment \
+    --payload "{\"Action\": \"redeploy_tupaia_server\", \"Branch\": \"$CI_BRANCH\", \"ServerDeploymentCode\": \"etest3-server\" }" \
+    --cli-binary-format raw-in-base64-out $RESPONSE_FILE \
+    --no-cli-pager
+
+  if grep -q errorMessage "$RESPONSE_FILE"; then
+    echo "Error while trying to redeploy"
+    cat $RESPONSE_FILE
+    exit 1
+  fi
+
+  NEW_INSTANCE_ID=$(cat $RESPONSE_FILE | cut -d "\"" -f 2)
+
+  echo "Waiting for instance to run its startup build script. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
+  WAIT_ATTEMPTS=0
+  while true; do
+    aws ec2 wait instance-exists \
+      --instance-ids ${NEW_INSTANCE_ID} \
+      --filters Name=tag-key,Values=SubdomainsViaGateway \
+      --no-cli-pager
+    if [ $? -eq 0 ]; then
+      echo "New instance is online."
+      break
+    else
+      if [ "$WAIT_ATTEMPTS" -ge 10 ]; then
+        echo "Build failed! Waited 10 times, but new instance is still not reachable"
+        exit 1
+      else
+        echo "Still waiting for instance startup build to complete. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
+        WAIT_ATTEMPTS=$((WAIT_ATTEMPTS+1))
+      fi
+    fi
+  done
+
+else
+  echo "No deployment running, skipping redeploy"
+fi
