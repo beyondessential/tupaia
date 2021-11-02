880fb88d4af25b8677cfe8e24f9f8a40f5c58d5d
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-11-02 16:47:10 +1300

    WAI-1022 Swap out confusing reduce

--- packages/web-config-server/src/apiV1/dataBuilders/helpers/translateEventEntityIdsToNames.js
+++ packages/web-config-server/src/apiV1/dataBuilders/helpers/translateEventEntityIdsToNames.js
@@ -8,21 +8,16 @@ import { transformValue } from 'apiV1/dataBuilders/transform';
 export const translateEventEntityIdsToNames = async (models, events, dataElementCodes) => {
   return Promise.all(
     events.map(async event => {
-      const updatedDataValues = await dataElementCodes.reduce(
-        async (accPromise, dataElementCode) => {
-          // Since this is an async function after the first loop returns, the accumulator is a promise
-          // So await the promise before continuing
-          const acc = await accPromise;
+      const updatedDataValues = {};
+      await Promise.all(
+        dataElementCodes.map(async dataElementCode => {
           const dataValueEntityId = event.dataValues[dataElementCode];
           if (dataValueEntityId) {
             const entityName = await transformValue(models, 'entityIdToName', dataValueEntityId);
-            return { ...acc, [dataElementCode]: entityName };
+            updatedDataValues[dataElementCode] = entityName;
           }
-          return acc;
-        },
-        {},
+        }),
       );
-
       return { ...event, dataValues: { ...event.dataValues, ...updatedDataValues } };
     }),
   );
