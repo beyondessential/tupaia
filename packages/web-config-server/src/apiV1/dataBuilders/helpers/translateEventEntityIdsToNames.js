cfba10a7e31a55da0fd9a03cc27ebe05eef546ac
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-11-02 15:34:09 +1300

    WAI-1022 Fix async reduce to correctly allow multiple entity id to name translations

--- packages/web-config-server/src/apiV1/dataBuilders/helpers/translateEventEntityIdsToNames.js
+++ packages/web-config-server/src/apiV1/dataBuilders/helpers/translateEventEntityIdsToNames.js
@@ -8,14 +8,20 @@ import { transformValue } from 'apiV1/dataBuilders/transform';
 export const translateEventEntityIdsToNames = async (models, events, dataElementCodes) => {
   return Promise.all(
     events.map(async event => {
-      const updatedDataValues = await dataElementCodes.reduce(async (acc, dataElementCode) => {
-        const dataValueEntityId = event.dataValues[dataElementCode];
-        if (dataValueEntityId) {
-          const entityName = await transformValue(models, 'entityIdToName', dataValueEntityId);
-          return { ...acc, [dataElementCode]: entityName };
-        }
-        return acc;
-      }, {});
+      const updatedDataValues = await dataElementCodes.reduce(
+        async (accPromise, dataElementCode) => {
+          // Since this is an async function after the first loop returns, the accumulator is a promise
+          // So await the promise before continuing
+          const acc = await accPromise;
+          const dataValueEntityId = event.dataValues[dataElementCode];
+          if (dataValueEntityId) {
+            const entityName = await transformValue(models, 'entityIdToName', dataValueEntityId);
+            return { ...acc, [dataElementCode]: entityName };
+          }
+          return acc;
+        },
+        {},
+      );
 
       return { ...event, dataValues: { ...event.dataValues, ...updatedDataValues } };
     }),
