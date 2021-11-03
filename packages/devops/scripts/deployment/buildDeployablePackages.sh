75b8397eb42e414b7cc0f6daf5241de3c0eb0090
Edwin <edmofro@gmail.com>
2021-11-04 08:33:38 +1300

    Add debug logs

--- packages/devops/scripts/deployment/buildDeployablePackages.sh
+++ packages/devops/scripts/deployment/buildDeployablePackages.sh
@@ -3,6 +3,8 @@
 DIR=$(dirname "$0")
 TUPAIA_DIR=$DIR/../../../..
 DEPLOYMENT_NAME=$1
+
+echo "Building deployable packages"
 PACKAGES=$(${TUPAIA_DIR}/scripts/bash/getDeployablePackages.sh)
 
 # Initialise NVM (which sets the path for access to npm, yarn etc. as well)
