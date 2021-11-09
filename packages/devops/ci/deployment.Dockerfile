2b9cea7ec0de227c7fcdaaef525eef5ca91785cb
Edwin <edmofro@gmail.com>
2021-11-09 13:28:25 +1300

    Allow for multiple deployments per branch

--- packages/devops/ci/deployment.Dockerfile
+++ packages/devops/ci/deployment.Dockerfile
@@ -2,4 +2,8 @@ FROM amazon/aws-cli
 
 COPY ./packages/devops/scripts/ci/triggerRedeploy.sh .
 
+RUN curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > jq
+RUN chmod +x jq
+RUN mv jq /bin
+
 ENTRYPOINT [ "" ]
--- packages/devops/scripts/ci/triggerRedeploy.sh
+++ packages/devops/scripts/ci/triggerRedeploy.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-if [[ $CI_BRANCH == "master" ]]; then
-    DEPLOYMENT_URL="tupaia.org"
-else
-    DEPLOYMENT_URL="${CI_BRANCH}.tupaia.org"
-fi
 EXISTING_INSTANCES=$(aws ec2 describe-instances \
       --filters Name=tag:Branch,Values=${CI_BRANCH} Name=tag-key,Values=SubdomainsViaGateway Name=instance-state-name,Values=running,stopped \
       --no-cli-pager)
 
-if [[ $EXISTING_INSTANCES == *"Instances"* ]]; then
-  echo "Existing deployment, triggering redeploy"
-  RESPONSE_FILE=lambda_redeploy_response.json
-  aws lambda invoke \
-    --function-name deployment \
-    --payload "{\"Action\": \"redeploy_tupaia_server\", \"Branch\": \"$CI_BRANCH\" }" \
-    --cli-binary-format raw-in-base64-out $RESPONSE_FILE \
-    --no-cli-pager
+if [[ $EXISTING_INSTANCES != *"Instances"* ]]; then
+  echo "No deployment running, skipping redeploy"
+  exit 0
+fi
 
-  if grep -q errorMessage "$RESPONSE_FILE"; then
-    echo "Error while trying to redeploy"
-    cat $RESPONSE_FILE
-    exit 1
-  fi
+echo "At least one existing deployment, triggering redeploy of any tagged with Branch ${CI_BRANCH}"
+RESPONSE_FILE=lambda_redeploy_response.json
+aws lambda invoke \
+  --function-name deployment \
+  --payload "{\"Action\": \"redeploy_tupaia_server\", \"User\": \"codeship\", \"Branch\": \"$CI_BRANCH\" }" \
+  --cli-binary-format raw-in-base64-out $RESPONSE_FILE \
+  --no-cli-pager
 
-  NEW_INSTANCE_ID=$(cat $RESPONSE_FILE | cut -d "\"" -f 2)
+if grep -q errorMessage "$RESPONSE_FILE"; then
+  echo "Error while trying to redeploy"
+  cat $RESPONSE_FILE
+  exit 1
+fi
 
-  echo "Waiting for instance to run its startup build script. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
+DEPLOYMENTS=$(cat $RESPONSE_FILE | jq -r '.[] | @base64')
+for DEPLOYMENT_BASE64 in $DEPLOYMENTS; do
+  DEPLOYMENT=$(echo $DEPLOYMENT_BASE64 | base64 --decode)
+  DEPLOYMENT_NAME=$(echo $DEPLOYMENT | jq -r '.DeploymentName')
+  NEW_INSTANCE_ID=$(echo $DEPLOYMENT | jq -r '.NewInstanceId')
+
+  echo "Waiting for ${DEPLOYMENT_NAME} to run its startup build script. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
   WAIT_ATTEMPTS=0
   while true; do
     aws ec2 wait instance-exists \
@@ -34,25 +37,24 @@ if [[ $EXISTING_INSTANCES == *"Instances"* ]]; then
       --filters Name=tag:StartupBuildProgress,Values=complete \
       --no-cli-pager
     if [ $? -eq 0 ]; then
-      echo "New instance is ready, swapping over ELB"
+      echo "New instance ${NEW_INSTANCE_ID} is ready, swapping over ELB"
       aws lambda invoke \
         --function-name deployment \
-        --payload "{\"Action\": \"swap_out_tupaia_server\", \"DeploymentName\": \"$DEPLOYMENT_NAME\", \"NewInstanceId\": \"$INSTANCE_ID\" }" \
+        --payload "{\"Action\": \"swap_out_tupaia_server\", \"User\": \"codeship\", \"DeploymentName\": \"$DEPLOYMENT_NAME\", \"NewInstanceId\": \"$NEW_INSTANCE_ID\" }" \
         --cli-binary-format raw-in-base64-out $RESPONSE_FILE \
         --no-cli-pager
-      echo "Redeploy complete"
+      echo "ELB for ${DEPLOYMENT_NAME} now points to ${NEW_INSTANCE_ID}"
       break
     else
       if [ "$WAIT_ATTEMPTS" -ge 10 ]; then
         echo "Build failed! Waited 10 times, but new instance is still not reachable"
         exit 1
       else
-        echo "Still waiting for instance startup build to complete. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
+        echo "Still waiting for ${DEPLOYMENT_NAME} startup build to complete. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
         WAIT_ATTEMPTS=$((WAIT_ATTEMPTS+1))
       fi
     fi
   done
+done
 
-else
-  echo "No deployment running, skipping redeploy"
-fi
+echo "Redeploy complete"
