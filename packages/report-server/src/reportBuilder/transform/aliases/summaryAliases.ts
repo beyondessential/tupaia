b63fefbb145f7abb47cd1074ae3953a2a86ae52b
Chris Pollard <therealchrispollard@gmail.com>
2021-11-09 16:22:14 +1100

    refactor insertSummaryRowAndColumn

--- packages/report-server/src/reportBuilder/transform/aliases/summaryAliases.ts
+++ packages/report-server/src/reportBuilder/transform/aliases/summaryAliases.ts
@@ -10,65 +10,69 @@ import { Row } from '../../types';
  * { rowFieldName: 'Availability of Male Condoms', facilityNameA: 'N', facilityNameB: 'N', facilityNameC: 'Y', facilityNameD: 'N'  }
  *  => { rowFieldName: 'Availability of Male Condoms', facilityNameA: 'N', facilityNameB: 'N', facilityNameC: 'Y', facilityNameD: 'N', rowSummary: 0.75 }
  */
-export const insertSummaryRowAndColumn = () => (rows: Row[]) => {
-  const dataWithSummaryColumn = rows.map(row => {
-    const numerator = Object.entries(row).filter(([key,value]) => value === 0).length;
-    const denominator = Object.entries(row).filter(([key,value]) => value === 0 || value === 1).length;
+
+const getRowsWithSummaryColumn = (rows: Row[]) => {
+  return rows.map(row => {
+    const numerator = Object.entries(row).filter(([key,value]) => value === 'N').length;
+    const denominator = Object.entries(row).filter(([key,value]) => value === 'N' || value === 'Y').length;
     if (denominator === 0) {
       return { 'summaryColumn': null, ...row };
     }
-    const summary = numerator / denominator;
+    const summary = `${(numerator / denominator * 100).toFixed(1)}%`;
     return { 'summaryColumn': summary, ...row };
-  });
+  })
+};
 
-  const numeratorRow = rows.reduce((accum: Row, currentRow: Row) => {
+const getFractionRow = (rows: Row[],isNumerator: boolean) => {
+  return rows.reduce((accum: Row, currentRow: Row) => {
     const rowKeyValueArray = Object.entries(currentRow)
-    let futureValue = {...accum}
+    let futureValue: Row = {...accum}
     rowKeyValueArray.forEach(([key,value]) => {
-      if (value === 0) {
-        if (typeof futureValue[key] === 'number') {
-          futureValue[key] = futureValue[key] + 1;
-        } else {
-          futureValue[key] = 1;
+      let currentValue = futureValue[key] as string | undefined | number; 
+      if (isNumerator) {
+        if (value === 'N') {
+          if (typeof currentValue === 'number') {
+            currentValue = currentValue + 1;
+          } else {
+            currentValue = 1;
+          }
+        } else if (value === 'Y'){
+          if (typeof currentValue === 'number') {
+            currentValue = currentValue + 0;
+          } else {
+            currentValue = 0;
+          }
         }
-      } else if (value === 1){
-        if (typeof futureValue[key] === 'number') {
-          futureValue[key] = futureValue[key] + 0;
-        } else {
-          futureValue[key] = 0;
+      } else {
+        if (value === 'N' || value === 'Y') {
+          if (typeof currentValue === 'number') {
+            currentValue = currentValue + 1;
+          } else {
+            currentValue = 1;
+          }
         }
       }
     })
     return futureValue
-  },{});
+  },{})
+};
 
-  const denominatorRow = rows.reduce((accum: Row, currentRow: Row) => {
-    const rowKeyValueArray = Object.entries(currentRow)
-    let futureValue = {...accum}
-    
-    rowKeyValueArray.forEach(([key,value]) => {
-      if (value === 0 || value === 1) {
-        if (typeof futureValue[key] === 'number') {
-          futureValue[key] = futureValue[key] + 1;
-        } else {
-          futureValue[key] = 1;
-        }
-      }
-    })
-    console.log('returned denominator',JSON.stringify(futureValue))
-    return futureValue
-  },{});
-
-  const summaryRow = Object.fromEntries(Object.entries(denominatorRow).map(([key,value]) => {
+const getSummaryRow = (numeratorRow: Row, denominatorRow: Row) => {
+  return Object.fromEntries(Object.entries(denominatorRow).map(([key,value]) => {
+    const numeratorValue = numeratorRow[key] as number;
     if (typeof value === 'number') {
-      const percentage = numeratorRow[key] / value;
+      const percentage = `${(numeratorValue / value * 100).toFixed(1)}%`;
     return [key, percentage]
     }
     return [key, value]
-  }));
+  }))
+}
 
-
-  return [...dataWithSummaryColumn,summaryRow]
-
+export const insertSummaryRowAndColumn = () => (rows: Row[]) => {
+  const rowsWithSummaryColumn = getRowsWithSummaryColumn(rows);
+  const numeratorRow = getFractionRow(rows,true);
+  const denominatorRow = getFractionRow(rows,false)
+  const summaryRow = getSummaryRow(numeratorRow,denominatorRow)
+  return [...rowsWithSummaryColumn,summaryRow]
 };
 
