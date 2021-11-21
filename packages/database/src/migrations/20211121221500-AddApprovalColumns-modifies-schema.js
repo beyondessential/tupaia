0a8beb280e1fce08857af64e60e56f443a8c37f7
Ethan McQuarrie <ethan@beyondessential.com.au>
2021-11-22 11:45:23 +1300

    WAI-832 Add migration for approval workflow columns

---
+++ packages/database/src/migrations/20211121221500-AddApprovalColumns-modifies-schema.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function (db) {
+  await db.runSql(
+    `CREATE TYPE approval_status AS ENUM('not_required', 'pending', 'rejected', 'approved')`,
+  );
+  await db.addColumn('survey_response', 'approval_status', {
+    type: 'approval_status',
+    defaultValue: 'not_required',
+  });
+  await db.addColumn('survey', 'requires_approval', {
+    type: 'boolean',
+    defaultValue: 'false',
+  });
+};
+
+exports.down = async function (db) {
+  await db.removeColumn('survey_response', 'approval_status');
+  await db.removeColumn('survey', 'requires_approval');
+  await db.runSql('DROP TYPE approval_status');
+};
+
+exports._meta = {
+  version: 1,
+};
