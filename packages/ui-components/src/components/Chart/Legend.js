07a03092f81194357a7b89a660fe451d6cc622d0
Tom Caiger <caigertom@gmail.com>
2021-11-30 16:24:47 +1300

    Update Legend.js

--- packages/ui-components/src/components/Chart/Legend.js
+++ packages/ui-components/src/components/Chart/Legend.js
@@ -111,7 +111,7 @@ export const getPieLegend = ({
   legendPosition,
   viewContent,
 }) => ({ payload }) => (
-  <PieLegendContainer position={legendPosition}>
+  <PieLegendContainer $position={legendPosition}>
     {payload.map(({ color, value, payload: item }) => {
       const displayValue = getPieLegendDisplayValue(
         chartConfig,
