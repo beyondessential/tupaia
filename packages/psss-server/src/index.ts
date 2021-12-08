06b9878ad8093fd1997774a151cc468ff7dcdbcc
Igor Nadj <igor@beyondessential.com.au>
2021-12-08 14:32:02 +1100

    Fix types psss-server (index.ts)

--- packages/psss-server/src/index.ts
+++ packages/psss-server/src/index.ts
@@ -5,13 +5,15 @@
 
 import '@babel/polyfill';
 
-import {} from 'dotenv/config'; // Load the environment variables into process.env
+import * as dotenv from 'dotenv';
 import http from 'http';
 import { TupaiaDatabase } from '@tupaia/database';
 
 import { createApp } from './app';
 import winston from './log';
 import { PsssSessionModel } from './models';
+
+dotenv.config(); // Load the environment variables into process.env
 
 /**
  * Set up database
