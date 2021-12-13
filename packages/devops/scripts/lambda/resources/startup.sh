a880a56e3dece6715ddf8d088e5a25cb39a23252
Edwin <edmofro@gmail.com>
2021-12-14 11:21:32 +1300

    Escape things from being evaluated too early

--- packages/devops/scripts/lambda/resources/startup.sh
+++ packages/devops/scripts/lambda/resources/startup.sh
@@ -36,7 +36,7 @@ mkdir -p $LOGS_DIR
 # Add preaggregation cron job if production
 if [[ $DEPLOYMENT_NAME == "production" ]]; then
   set +e # Temporarily allow non 0 exit, as crontab probably doesn't exist yet
-  (crontab -l ; echo "10 13 * * * /home/ubuntu/tupaia/packages/web-config-server/run_preaggregation.sh |& while IFS= read -r line; do printf '\%s \%s\n' "$(date)" "$line"; done > /home/ubuntu/logs/preaggregation.txt") | crontab -
+  (crontab -l ; echo "10 13 * * * /home/ubuntu/tupaia/packages/web-config-server/run_preaggregation.sh |& while IFS= read -r line; do printf '\%s \%s\\\\n' \"\$(date)\" \"\$line\"; done > /home/ubuntu/logs/preaggregation.txt") | crontab -
   set -e
 fi
 
