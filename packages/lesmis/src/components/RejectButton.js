31aa41df3ac553ec08a98440ec9ad53be4347744
Tom Caiger <caigertom@gmail.com>
2021-12-15 12:26:33 +1300

    add Data Change Action Component

--- packages/lesmis/src/components/RejectButton.js
+++ packages/lesmis/src/components/RejectButton.js
@@ -5,22 +5,25 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ConfirmModal } from '@tupaia/ui-components';
-import { IconButton } from '@tupaia/admin-panel/lib';
-import { Delete, Clear } from '@material-ui/icons';
+import { IconButton, DataChangeAction } from '@tupaia/admin-panel/lib';
+import { Delete } from '@material-ui/icons';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { FlexCenter } from './Layout';
 import { useRejectSurveyResponseStatus } from '../api';
-import { RED } from '../constants';
 
 export const RejectButton = ({ value: id }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isRejected, setIsRejected] = useState(false);
   const { mutate, isLoading, isError, error } = useRejectSurveyResponseStatus();
 
-  const handleClickReject = () => {
+  const handleClickReject = ({ onEditBegin, onEditSuccess, onEditError }) => {
+    onEditBegin();
+
     mutate(id, {
       onSuccess: () => {
-        setIsRejected(true);
+        onEditSuccess();
+        setIsOpen(false);
+      },
+      onError: errorMessage => {
+        onEditError(errorMessage.message);
         setIsOpen(false);
       },
     });
@@ -28,26 +31,24 @@ export const RejectButton = ({ value: id }) => {
 
   return (
     <>
-      {isRejected ? (
-        <FlexCenter>
-          <Clear style={{ color: RED }} />
-        </FlexCenter>
-      ) : (
-        <IconButton onClick={() => setIsOpen(true)}>
-          {isLoading ? <CircularProgress size={16} color="inherit" /> : <Delete />}
-        </IconButton>
-      )}
-      <ConfirmModal
-        onClose={() => setIsOpen(false)}
-        isOpen={isOpen}
-        handleAction={handleClickReject}
-        isLoading={isLoading}
-        error={isError && error}
-        title="Reject Survey Response"
-        mainText="Are you sure you want to reject this Survey Response?"
-        description="Rejecting a Survey Response will remove the record from this tab and prevent the data displaying in any visualisations."
-        actionText="Yes, Reject"
-        loadingText="Saving"
+      <IconButton onClick={() => setIsOpen(true)}>
+        {isLoading ? <CircularProgress size={16} color="inherit" /> : <Delete />}
+      </IconButton>
+      <DataChangeAction
+        render={props => (
+          <ConfirmModal
+            onClose={() => setIsOpen(false)}
+            isOpen={isOpen}
+            handleAction={() => handleClickReject(props)}
+            isLoading={isLoading}
+            error={isError ? error : null}
+            title="Reject Survey Response"
+            mainText="Are you sure you want to reject this Survey Response?"
+            description="Rejecting a Survey Response will remove the record from this tab and prevent the data displaying in any visualisations."
+            actionText="Yes, Reject"
+            loadingText="Saving"
+          />
+        )}
       />
     </>
   );
