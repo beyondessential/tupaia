76b91106a01bf548e53bd32d3faabe2949ed0814
Igor Nadj <igor@beyondessential.com.au>
2022-01-13 13:35:01 +1100

    Minor: remove unused import

--- packages/database/src/migrations/20200409013211-AddAddCovidTestsPerCapitaReport.js
+++ packages/database/src/migrations/20200409013211-AddAddCovidTestsPerCapitaReport.js
@@ -4,19 +4,17 @@ var dbm;
 var type;
 var seed;
 
-import { AGGREGATION_TYPES } from '@tupaia/aggregator/src/aggregationTypes';
-
 /**
  * We receive the dbmigrate dependency from dbmigrate initially.
  * This enables us to not have to rely on NODE_PATH.
  */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
+exports.up = function (db) {
   return db.runSql(`
     insert into "dashboardReport" ("id", "dataBuilder", "dataBuilderConfig", "viewJson")
     values (
@@ -72,7 +70,7 @@ exports.up = function(db) {
   `);
 };
 
-exports.down = function(db) {
+exports.down = function (db) {
   return db.runSql(`
     delete from "dashboardReport" where id = 'COVID_Tests_Per_Capita';
 
