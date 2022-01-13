9d5d0a181f481ec712d18ffba890e71ab677ea67
Igor Nadj <igor@beyondessential.com.au>
2022-01-13 15:11:49 +1100

    Add tests to admin panel, with a single passing stub test

---
+++ packages/admin-panel/src/__tests__/stub.test.js
@@ -0,0 +1,10 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+describe('stub', () => {
+  it('passes', async () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ packages/admin-panel/src/jest.config.js
@@ -0,0 +1,8 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+module.exports = {
+  moduleDirectories: ['node_modules'],
+};
