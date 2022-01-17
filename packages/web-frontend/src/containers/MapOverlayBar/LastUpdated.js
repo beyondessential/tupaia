f3587152f26496a2018b632a4b50ba7847fded69
Biao Li <lbit123@outlook.com>
2022-01-17 11:53:17 +1100

    Display latest overlay date in a format

--- packages/web-frontend/src/containers/MapOverlayBar/LastUpdated.js
+++ packages/web-frontend/src/containers/MapOverlayBar/LastUpdated.js
@@ -8,7 +8,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import moment from 'moment';
+import { periodToDisplayString } from '@tupaia/utils';
 
 import { VIEW_STYLES } from '../../styles';
 import { selectCurrentMapOverlayCodes } from '../../selectors';
@@ -16,7 +16,7 @@ import { selectCurrentMapOverlayCodes } from '../../selectors';
 class LastUpdated extends Component {
   getFormattedDate = () => {
     const { latestAvailable } = this.props;
-    return `Latest overlay data: ${moment(latestAvailable).format('DD/MM/YYYY')}`;
+    return `Latest overlay data: ${periodToDisplayString(latestAvailable)}`;
   };
 
   shouldRender = () => !!this.props.latestAvailable;
