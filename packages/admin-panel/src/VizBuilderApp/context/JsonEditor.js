2ddfae8cd87e184df43f5ad0de154958551bcc10
Biao Li <lbit123@outlook.com>
2022-01-20 09:55:48 +1100

    2021 to 2022 in copyright

--- packages/admin-panel/src/VizBuilderApp/context/JsonEditor.js
+++ packages/admin-panel/src/VizBuilderApp/context/JsonEditor.js
@@ -1,6 +1,6 @@
 /**
  * Tupaia
- * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ * Copyright (c) 2017 - 2022 Beyond Essential Systems Pty Ltd
  */
 
 import PropTypes from 'prop-types';
