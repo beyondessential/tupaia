0e14e6e96fffc6d47f26ce105261e50c018d94e1
Edwin <edmofro@gmail.com>
2022-01-21 15:09:50 +1300

    Extend deployment timeouts to ~4 hours

--- packages/devops/scripts/ci/triggerRedeploy.sh
+++ packages/devops/scripts/ci/triggerRedeploy.sh
@@ -75,8 +75,8 @@ for DEPLOYMENT_BASE64 in $DEPLOYMENTS; do
       echo "ELB for ${DEPLOYMENT_NAME} now points to ${NEW_INSTANCE_ID}"
       break
     else
-      if [ "$WAIT_ATTEMPTS" -ge 10 ]; then
-        echo "Build failed! Waited 10 times, but new instance is still not reachable"
+      if [ "$WAIT_ATTEMPTS" -ge 75 ]; then
+        echo "Build failed! Waited 75 times, but new instance is still not reachable"
         exit 1
       else
         echo "Still waiting for ${DEPLOYMENT_NAME} startup build to complete. To watch detailed progress, connect to instance ${NEW_INSTANCE_ID} and run tail -f logs/deployment_log.txt"
