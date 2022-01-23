4bf7b1fc177da6f982e44266f24e3a57dd162954
Biao Li <31789355+billli0@users.noreply.github.com>
2022-01-24 10:47:25 +1100

    RN-186 toggle back to json (#3631)

---
+++ packages/admin-panel/src/VizBuilderApp/components/JsonToggleButton.js
@@ -0,0 +1,36 @@
+/*
+ * Tupaia
+ *  Copyright (c) 2017 - 2022 Beyond Essential Systems Pty Ltd
+ */
+import React from 'react';
+import styled from 'styled-components';
+import { FormControlLabel, Switch } from '@material-ui/core';
+import { useTabPanel } from '../context';
+
+const ControlLabel = styled(FormControlLabel)`
+  color: ${props => (props.jsonToggleEnabled ? props.theme.palette.primary.main : 'grey')};
+  margin-bottom: -10px;
+  .MuiFormControlLabel-label {
+    font-size: 10px;
+    margin-bottom: -10px;
+  }
+`;
+
+export const JsonToggleButton = () => {
+  const { jsonToggleEnabled, setJsonToggleEnabled } = useTabPanel();
+
+  const handleClick = () => {
+    setJsonToggleEnabled(!jsonToggleEnabled);
+  };
+
+  return (
+    <ControlLabel
+      value="top"
+      control={<Switch color="primary" />}
+      label="JSON"
+      onChange={handleClick}
+      labelPlacement="top"
+      jsonToggleEnabled={jsonToggleEnabled}
+    />
+  );
+};
