5b5e8615d91bd201c16d9d8a3fb955777726c1a5
Biao Li <lbit123@outlook.com>
2022-01-24 12:19:47 +1100

    Small change on style

--- packages/ui-components/src/components/DataLibrary/options.js
+++ packages/ui-components/src/components/DataLibrary/options.js
@@ -123,23 +123,29 @@ export const SelectableMultipleTimesOption = ({ option, onSelect }) => (
   </StyledSelectableMultipleTimesOption>
 );
 
-export const SelectedDataCard = ({ option, onRemove, index }) => (
-  <Draggable draggableId={option.code} index={index}>
-    {(provided, snapshot) => (
-      <StyledSelectedDataCard
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-        isDragging={snapshot.isDragging}
-      >
-        <OptionText>
-          <OptionCode>{option.code}</OptionCode>
-          <OptionName>{option.name}</OptionName>
-        </OptionText>
-        <IconWrapper onClick={event => onRemove(event, option)} className="icon-wrapper">
-          <Close />
-        </IconWrapper>
-      </StyledSelectedDataCard>
-    )}
-  </Draggable>
-);
+export const SelectedDataCard = ({ option, onRemove, index }) => {
+  const [isDragging, setIsDragging] = React.useState(false);
+
+  return (
+    <Draggable draggableId={option.code} index={index}>
+      {(provided, snapshot) => (
+        <StyledSelectedDataCard
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging || isDragging}
+          onMouseOver={() => setIsDragging(true)}
+          onMouseLeave={() => setIsDragging(false)}
+        >
+          <OptionText>
+            <OptionCode>{option.code}</OptionCode>
+            <OptionName>{option.name}</OptionName>
+          </OptionText>
+          <IconWrapper onClick={event => onRemove(event, option)} className="icon-wrapper">
+            <Close />
+          </IconWrapper>
+        </StyledSelectedDataCard>
+      )}
+    </Draggable>
+  );
+};
