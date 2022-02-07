4eda3c32674d49c9ff01b386fc6ffd02d90ff169
Rohan Port <rohan@beyondessential.com.au>
2022-02-08 10:40:07 +1100

    RN-195: Reverted unneccessary changes to fetching from lastpass logic

--- packages/devops/scripts/deployment/startBackEnds.sh
+++ packages/devops/scripts/deployment/startBackEnds.sh
@@ -2,7 +2,6 @@
 
 DIR=$(dirname "$0")
 TUPAIA_DIR=$DIR/../../../..
-DEPLOYMENT_NAME=$1
 PACKAGES=$(${TUPAIA_DIR}/scripts/bash/getDeployablePackages.sh)
 
 # Initialise NVM (which sets the path for access to npm, yarn etc. as well)
