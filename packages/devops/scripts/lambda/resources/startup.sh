43e31c04cb3ef37468b8f418414e090a9941e867
Rohan Port <rohan@beyondessential.com.au>
2022-02-08 10:40:07 +1100

    RN-195: Reverted unneccessary changes to fetching from lastpass logic

--- packages/devops/scripts/lambda/resources/startup.sh
+++ packages/devops/scripts/lambda/resources/startup.sh
@@ -70,9 +70,8 @@ sudo -Hu ubuntu git checkout ${BRANCH_TO_USE}
 sudo -Hu ubuntu git reset --hard origin/${BRANCH_TO_USE}
 
 # Deploy each package, including injecting environment variables from LastPass
-sudo -Hu ubuntu $DEPLOYMENT_SCRIPTS/fetchEnvVarsFromLastpass.sh $DEPLOYMENT_NAME |& while IFS= read -r line; do printf '\%s \%s\n' "$(date)" "$line"; done  >> $LOGS_DIR/deployment_log.txt
 sudo -Hu ubuntu $DEPLOYMENT_SCRIPTS/buildDeployablePackages.sh $DEPLOYMENT_NAME |& while IFS= read -r line; do printf '\%s \%s\n' "$(date)" "$line"; done  >> $LOGS_DIR/deployment_log.txt
-sudo -Hu ubuntu $DEPLOYMENT_SCRIPTS/startBackEnds.sh $DEPLOYMENT_NAME |& while IFS= read -r line; do printf '\%s \%s\n' "$(date)" "$line"; done  >> $LOGS_DIR/deployment_log.txt
+sudo -Hu ubuntu $DEPLOYMENT_SCRIPTS/startBackEnds.sh |& while IFS= read -r line; do printf '\%s \%s\n' "$(date)" "$line"; done  >> $LOGS_DIR/deployment_log.txt
 
 # Set nginx config and start the service running
 $DEPLOYMENT_SCRIPTS/configureNginx.sh |& while IFS= read -r line; do printf '\%s \%s\n' "$(date)" "$line"; done  >> $LOGS_DIR/deployment_log.txt
--- scripts/bash/getPackagesWithEnvFiles.sh
+++ scripts/bash/getPackagesWithEnvFiles.sh
@@ -4,6 +4,6 @@ DIR=$(dirname "$0")
 
 # packages with .env files are (currently) all deployable, plus auth, data-api, and database
 PACKAGES=$(${DIR}/getDeployablePackages.sh)
-PACKAGES+=" auth data-api database devops"
+PACKAGES+=" auth data-api database"
 echo $PACKAGES
 exit 0
