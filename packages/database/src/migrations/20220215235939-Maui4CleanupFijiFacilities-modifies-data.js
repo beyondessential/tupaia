d1efcfd37882307b90dc0b225dbebea7dd7febb9
avaek <vanbeekandrew@gmail.com>
2022-02-17 20:17:43 +1300

    update to fiji facility cleanup

--- packages/database/src/migrations/20220215235939-Maui4CleanupFijiFacilities-modifies-data.js
+++ packages/database/src/migrations/20220215235939-Maui4CleanupFijiFacilities-modifies-data.js
@@ -1,12 +1,19 @@
 'use strict';
 
-import { codeToId } from '../utilities';
+import { codeToId, generateId } from '../utilities';
+import { insertObject } from '../utilities/migration';
 
 var dbm;
 var type;
 var seed;
 
-const facilitiesToDelete = ['FJ_059', 'FJ_223', 'FJ_230'];
+const facilitiesToDelete = ['FJ_059', 'FJ_045', 'FJ_157', 'FJ-14_HC_Sawakasa', 'FJ_244'];
+
+const facilitiesToAddToEntityRelation = [
+  { code: 'FJ_243', parent_code: 'FJ_Western' },
+  { code: 'FJ_223', parent_code: 'FJ_Central' },
+  { code: 'FJ_230', parent_code: 'FJ_Eastern' },
+];
 /**
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
@@ -30,6 +37,18 @@ exports.up = async function (db) {
     await db.runSql(`
       DELETE FROM clinic WHERE id = '${clinicId}';
     `);
+  });
+
+  facilitiesToAddToEntityRelation.forEach(async facility => {
+    const entityId = await codeToId(db, 'entity', facility.code);
+    const parentId = await codeToId(db, 'entity', facility.parent_code);
+    const newEntityRelation = {
+      id: generateId(),
+      parent_id: parentId,
+      child_id: entityId,
+      entity_hierarchy_id: '606517f361f76a144800000a',
+    };
+    await insertObject(db, 'entity_relation', newEntityRelation);
   });
 };
 
