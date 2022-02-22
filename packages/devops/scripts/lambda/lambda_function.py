afed6d0047f0112e7c136afcc72f945113efddec
Rohan Port <rohan@beyondessential.com.au>
2022-02-22 14:40:20 +1100

    RN-195: Rename 'deleteOldDeployments' -> 'deleteOldServers'

--- packages/devops/scripts/lambda/lambda_function.py
+++ packages/devops/scripts/lambda/lambda_function.py
@@ -1,5 +1,5 @@
 from actions.backup_instances import backup_instances
-from actions.delete_old_deployments import delete_old_deployments
+from actions.delete_old_servers import delete_old_servers
 from actions.delete_old_snapshots import delete_old_snapshots
 from actions.refresh_cloned_servers import refresh_cloned_servers
 from actions.refresh_cloned_databases import refresh_cloned_databases
@@ -17,7 +17,7 @@ from actions.tear_down_dhis_deployment import tear_down_dhis_deployment
 
 actions = {
   'backup_instances': backup_instances,
-  'delete_old_deployments': delete_old_deployments,
+  'delete_old_servers': delete_old_servers,
   'delete_old_snapshots': delete_old_snapshots,
   'refresh_cloned_servers': refresh_cloned_servers,
   'refresh_cloned_databases': refresh_cloned_databases,
