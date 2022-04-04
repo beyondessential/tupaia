9298f33d3683f9d4d17215d24fb27636c269d4a4
Rohan Port <rohan@beyondessential.com.au>
2022-04-04 15:49:33 +1000

    Updated scripts to make it easier to setup the local test database

--- packages/devops/scripts/ci/setupTestDatabase.sh
+++ packages/devops/scripts/ci/setupTestDatabase.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 DIR=$(dirname "$0")
+DIR=$(pwd)
+echo $DIR
 ${DIR}/waitForPostgres.sh
 
 # Set default port in case it wasn't in .env
 : "${DB_PORT:=5432}"
 
-psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD '$DB_PG_PASSWORD'"
-psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "CREATE ROLE $DB_USER LOGIN SUPERUSER PASSWORD '$DB_PASSWORD'"
-psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD '$DB_PG_PASSWORD'"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "CREATE ROLE $DB_USER LOGIN SUPERUSER PASSWORD '$DB_PASSWORD'"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "DROP DATABASE IF EXISTS $DB_NAME"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U postgres -d postgres -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_USER -d $DB_NAME -f ./packages/database/src/tests/testData/testDataDump.sql
 
 echo "Installing mvrefresh"
@@ -20,3 +23,4 @@ yarn workspace @tupaia/data-api build-analytics-table
 echo "Deleting migrations that target data modifications, as there is no data to migrate on the test database"
 rm packages/database/src/migrations/*modifies-data.js
 yarn migrate
+git checkout -- 'packages/database/src/migrations/*modifies-data.js'
