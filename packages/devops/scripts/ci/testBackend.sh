b12c16d1e24c12b2b583b2d0002ec2be782f612e
Rohan Port <rohan@beyondessential.com.au>
2022-04-05 15:12:09 +1000

    Moved setupTestDatabase.sh to database package

--- packages/devops/scripts/ci/testBackend.sh
+++ packages/devops/scripts/ci/testBackend.sh
@@ -3,7 +3,7 @@ DIR=$(dirname "$0")
 ${DIR}/waitForPostgres.sh
 
 echo "Connected to postgres server: $DB_URL, starting to setup database"
-yarn setup-test-database
+yarn workspace @tupaia/database setup-test-database
 
 PACKAGE=$1
 yarn workspace @tupaia/${PACKAGE} test
