53a9b1c36a5d104c465ea66558d0230cb7edbc62
Rohan Port <rohan@beyondessential.com.au>
2022-04-11 10:56:07 +1000

    Added 'check-test-database-exists' script to ensure the test database is created before running tests

---
+++ packages/database/scripts/checkTestDatabaseExists.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# Use whatever existing .env vars have been specified
+curenv=$(declare -p -x)
+source .env
+eval "$curenv"
+
+# Set default port in case it wasn't in .env
+: "${DB_PORT:=5432}"
+
+if [ "$(PGPASSWORD=$DB_PG_PASSWORD psql -p $DB_PORT -X -A -h $DB_URL -U $DB_PG_USER -t -c "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" )" = '1' ]
+then
+    exit 0
+fi
+
+echo -e "Error: $DB_NAME database does not exist!\n\nTo create it, please run:\nyarn workspace @tupaia/database setup-test-database\n"
+exit 1
