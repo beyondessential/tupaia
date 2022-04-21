764c51037d4d1e4426c955bcb8503025eb775dfd
Biao Li <lbit123@outlook.com>
2022-04-21 13:28:42 +1000

    Add test for raw data export

---
+++ packages/report-server/src/__tests__/reportBuilder/output/rawDataExport.test.ts
@@ -0,0 +1,92 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2022 Beyond Essential Systems Pty Ltd
+ */
+
+import { Aggregator } from '@tupaia/aggregator';
+import { ReportServerAggregator } from '../../../aggregator';
+import { buildOutput } from '../../../reportBuilder/output';
+import { MULTIPLE_TRANSFORMED_DATA_FOR_RAW_DATA_EXPORT } from './output.fixtures';
+
+describe('rawDataExport', () => {
+  const dataBroker = { context: {} };
+  const aggregator = new Aggregator(dataBroker);
+  jest.spyOn(aggregator, 'fetchDataGroup').mockImplementation(() => {
+    return new Promise(resolve => {
+      process.nextTick(() =>
+        resolve({
+          dataElements: [
+            { code: 'dataElement_A', text: 'Laos' },
+            { code: 'dataElement_B', text: 'Tonga' },
+            { code: 'dataElement_C', text: 'Australia' },
+            { code: 'dataElement_D', text: 'New Zealand' },
+          ],
+        }),
+      );
+    });
+  });
+
+  it('Add the rest of data elements that has no value into columns', async () => {
+    const expectedData = {
+      columns: [
+        {
+          key: 'EntityName',
+          title: 'EntityName',
+        },
+        {
+          key: 'dataElement_A',
+          title: 'Laos',
+        },
+        {
+          key: 'dataElement_B',
+          title: 'Tonga',
+        },
+        {
+          key: 'dataElement_C',
+          title: 'Australia',
+        },
+        {
+          key: 'dataElement_D',
+          title: 'New Zealand',
+        },
+      ],
+      rows: [
+        {
+          EntityName: 'clinic',
+          dataElement: undefined,
+          dataElement_A: 3,
+          dataElement_B: 0,
+        },
+        {
+          EntityName: 'hospital',
+          dataElement: undefined,
+          dataElement_A: 4,
+          dataElement_B: 9,
+        },
+        {
+          EntityName: 'park',
+          dataElement: undefined,
+          dataElement_B: 0,
+        },
+        {
+          EntityName: 'others',
+          dataElement: undefined,
+          dataElement_B: 5,
+        },
+      ],
+    };
+    const output = buildOutput({
+      type: 'rawDataExport',
+      columns: '*',
+      rowField: '*',
+      dataGroups: ['dataGroupCode'], // required
+    });
+
+    const reportServerAggregator = new ReportServerAggregator(aggregator);
+    const results = await output(
+      MULTIPLE_TRANSFORMED_DATA_FOR_RAW_DATA_EXPORT,
+      reportServerAggregator,
+    );
+    expect(results).toEqual(expectedData);
+  });
+});
