6e782093f2fc2c9bd7a4e61ea27cd6d8aa3ca02d
Biao Li <lbit123@outlook.com>
2022-04-22 13:55:08 +1000

    Use date-fns to manipulate date in transform

--- packages/expression-parser/src/expression-parser/customFunctions.js
+++ packages/expression-parser/src/expression-parser/customFunctions.js
@@ -3,6 +3,8 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
+import * as fns from 'date-fns';
+
 // We use the `'undefined'` string to indicate a missing value in cases where the
 // actual `undefined` JS type cannot be used, e.g. in json config persisted in the DB
 const isUndefined = value => value !== undefined && value !== 'undefined';
@@ -30,9 +32,12 @@ const translate = (value, translations) => {
 
 const date = (...argumentList) => new Date(...argumentList);
 
+const dateUtils = () => fns;
+
 export const customFunctions = {
   avg: average,
   firstExistingValue,
   translate,
   date,
+  dateUtils,
 };
