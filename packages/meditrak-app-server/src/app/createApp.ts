8acdb56e0b695b4006ff8fbb899f2690dc9e2e6e
Rohan Port <rohan@beyondessential.com.au>
2022-05-05 11:34:02 +1000

    RN-493: Added api-client to meditrak-server request context

--- packages/meditrak-app-server/src/app/createApp.ts
+++ packages/meditrak-app-server/src/app/createApp.ts
@@ -4,12 +4,32 @@
  */
 import { TupaiaDatabase } from '@tupaia/database';
 import { MicroServiceApiBuilder } from '@tupaia/server-boilerplate';
+import {
+  ForwardingAuthHandler,
+  getBaseUrlsForHost,
+  LOCALHOST_BASE_URLS,
+  TupaiaApiClient,
+} from '@tupaia/api-client';
 
 /**
  * Set up express server with middleware,
  */
 export function createApp() {
-  const app = new MicroServiceApiBuilder(new TupaiaDatabase()).build();
+  const app = new MicroServiceApiBuilder(new TupaiaDatabase())
+    .middleware((req, res, next) => {
+      const baseUrls =
+        process.env.NODE_ENV === 'test' ? LOCALHOST_BASE_URLS : getBaseUrlsForHost(req.hostname);
+      const context = {
+        services: new TupaiaApiClient(
+          new ForwardingAuthHandler(req.headers.authorization),
+          baseUrls,
+        ),
+      };
+      req.ctx = context;
+      res.ctx = context;
+      next();
+    })
+    .build();
 
   return app;
 }
