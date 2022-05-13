06ad5274a92e1eb2ccad3aa4914075f94c296bc6
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2022-05-13 15:13:44 +1000

    RN-493: Add AuthRoute to meditrak-app-server (#3907)
    * RN-493: Added AuthApi to api-client

--- packages/meditrak-app-server/src/app/createApp.ts
+++ packages/meditrak-app-server/src/app/createApp.ts
@@ -3,13 +3,35 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 import { TupaiaDatabase } from '@tupaia/database';
-import { MicroServiceApiBuilder } from '@tupaia/server-boilerplate';
+import { handleWith, MicroServiceApiBuilder } from '@tupaia/server-boilerplate';
+import {
+  ForwardingAuthHandler,
+  getBaseUrlsForHost,
+  LOCALHOST_BASE_URLS,
+  TupaiaApiClient,
+} from '@tupaia/api-client';
+import { AuthRequest, AuthRoute } from '../routes';
 
 /**
  * Set up express server with middleware,
  */
 export function createApp() {
-  const app = new MicroServiceApiBuilder(new TupaiaDatabase()).build();
+  const app = new MicroServiceApiBuilder(new TupaiaDatabase())
+    .middleware((req, res, next) => {
+      const baseUrls =
+        process.env.NODE_ENV === 'test' ? LOCALHOST_BASE_URLS : getBaseUrlsForHost(req.hostname);
+      const context = {
+        services: new TupaiaApiClient(
+          new ForwardingAuthHandler(req.headers.authorization),
+          baseUrls,
+        ),
+      };
+      req.ctx = context;
+      res.ctx = context;
+      next();
+    })
+    .post<AuthRequest>('auth', handleWith(AuthRoute))
+    .build();
 
   return app;
 }
