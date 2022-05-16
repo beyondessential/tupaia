f06ea0850272aaf3d4fa4a80112f316ba28ccad9
Igor Nadj <igor@beyondessential.com.au>
2022-05-16 10:17:06 +1000

    Add GETDhisInstances route

--- packages/central-server/src/apiV2/index.js
+++ packages/central-server/src/apiV2/index.js
@@ -104,6 +104,7 @@ import {
   EditMapOverlayVisualisation,
   GETMapOverlayVisualisations,
 } from './mapOverlayVisualisations';
+import { GETDhisInstances } from './GETDhisInstances';
 
 // quick and dirty permission wrapper for open endpoints
 const allowAnyone = routeHandler => (req, res, next) => {
@@ -206,6 +207,7 @@ apiV2.get('/clinics/:recordId?', useRouteHandler(GETClinics));
 apiV2.get('/facilities/:recordId?', useRouteHandler(GETClinics));
 apiV2.get('/geographicalAreas/:recordId?', useRouteHandler(GETGeographicalAreas));
 apiV2.get('/reports/:recordId?', useRouteHandler(GETReports));
+apiV2.get('/dhisInstances/:recordId?', useRouteHandler(GETDhisInstances));
 
 /**
  * POST routes
