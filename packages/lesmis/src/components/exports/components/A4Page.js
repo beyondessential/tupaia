4aecd89a06172bdd902569b516a098b1ebe275fc
Biao Li <lbit123@outlook.com>
2022-05-27 15:39:56 +1000

    A4 page component

---
+++ packages/lesmis/src/components/exports/components/A4Page.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Header from './Header';
+
+const A4Container = styled.div`
+  width: 1300px;
+  margin-top: 20px;
+  position: absolute;
+  z-index: ${props => (props.$isSelected ? 0 : -1)};
+`;
+
+const Content = styled.div`
+  margin: 0px 150px;
+`;
+
+export const A4Page = ({ addToRefs, children, page, currentPage }) => {
+  const isSelected = page === currentPage;
+
+  return (
+    <A4Container ref={addToRefs} $isSelected={isSelected}>
+      <Header />
+      <Content> {children}</Content>
+    </A4Container>
+  );
+};
+
+A4Page.propTypes = {
+  children: PropTypes.node.isRequired,
+  addToRefs: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+};
