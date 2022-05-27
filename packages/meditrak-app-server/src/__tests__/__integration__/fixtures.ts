6e3191417ffe3f8b76f30337532a8179f85034fc
Rohan Port <rohan@beyondessential.com.au>
2022-05-27 14:55:02 +1000

    Added tests for UserRewardsRoute

--- packages/meditrak-app-server/src/__tests__/__integration__/fixtures.ts
+++ packages/meditrak-app-server/src/__tests__/__integration__/fixtures.ts
@@ -18,6 +18,13 @@ const USERS = [CAT_USER];
 
 const SESSIONS = [CAT_USER_SESSION];
 
+export const CAT_QUESTION = { code: 'CAT_QUESTION', text: 'What kind of cat do you have?' };
+export const CAT_SURVEY = {
+  code: 'CAT_SURVEY',
+  name: 'Cat Survey',
+  questions: [CAT_QUESTION],
+};
+
 export const TEST_DATA = {
   users: USERS,
   sessions: SESSIONS,
