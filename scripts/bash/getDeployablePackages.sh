0d8fc83abd775817d51c9364cb6226d3707320f2
Edwin <edmofro@gmail.com>
2022-06-16 11:13:55 +1200

    Build front ends after building the servers

--- scripts/bash/getDeployablePackages.sh
+++ scripts/bash/getDeployablePackages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-echo "admin-panel" "lesmis" "psss" "web-frontend" "admin-panel-server" "central-server" "entity-server" "lesmis-server" "meditrak-app-server" "psss-server" "report-server" "web-config-server"
+echo "admin-panel-server" "central-server" "entity-server" "lesmis-server" "meditrak-app-server" "psss-server" "report-server" "web-config-server" "admin-panel" "lesmis" "psss" "web-frontend"
 exit 0
