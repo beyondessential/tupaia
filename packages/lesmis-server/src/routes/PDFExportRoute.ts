b98127f4adbc9e070613ca1afbee1c006ec76346
Biao Li <lbit123@outlook.com>
2022-06-29 15:52:11 +1000

    Backend: Forward params to request

--- packages/lesmis-server/src/routes/PDFExportRoute.ts
+++ packages/lesmis-server/src/routes/PDFExportRoute.ts
@@ -6,13 +6,20 @@
 import { Request, Response, NextFunction } from 'express';
 import { Route } from '@tupaia/server-boilerplate';
 import puppeteer from 'puppeteer';
+import { stringifyQuery } from '@tupaia/utils';
 
 export type RegisterRequest = Request;
 type Cookies = {
-  host: string;
   sessionCookieName: string;
   sessionCookieValue: string;
 };
+
+type Body = {
+  endpoint: string;
+  hostname: string;
+  restOfParams: Record<string, string>;
+};
+
 export class PDFExportRoute extends Route<RegisterRequest> {
   public constructor(req: Request, res: Response, next: NextFunction) {
     super(req, res, next);
@@ -20,29 +27,43 @@ export class PDFExportRoute extends Route<RegisterRequest> {
   }
 
   private unloadSessionCookies = (): Cookies => {
-    const { host, cookie } = this.req.headers;
+    const { cookie } = this.req.headers;
     if (!cookie || Array.isArray(cookie)) {
       throw new Error('Array type or undefined "cookie" is not supported');
     }
-    if (!host) {
-      throw new Error(`"host" in headers should be defined`);
-    }
+
     const sessionCookieName = cookie.slice(0, cookie.indexOf('='));
     const sessionCookieValue = cookie.slice(cookie.indexOf('=') + 1);
-    return { host, sessionCookieName, sessionCookieValue };
+
+    return { sessionCookieName, sessionCookieValue };
+  };
+
+  private verifyBody = (body: any): Body => {
+    const { endpoint, hostname, ...restOfParams } = body;
+    if (!endpoint || typeof endpoint !== 'string') {
+      throw new Error(`"endpoint" should be provided in request body, got: ${endpoint}`);
+    }
+    if (!hostname || typeof endpoint !== 'string') {
+      throw new Error(`"host" should be provided in request body, got: ${endpoint}`);
+    }
+
+    return { endpoint, hostname, restOfParams };
   };
 
   private exportPDF = async (): Promise<Buffer> => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const { host, sessionCookieName, sessionCookieValue } = this.unloadSessionCookies();
+    const { sessionCookieName, sessionCookieValue } = this.unloadSessionCookies();
+    const body = this.verifyBody(this.req.body);
     await page.setCookie({
       name: sessionCookieName,
-      domain: host,
+      domain: body.hostname.split(':')[0], // localhost:8030 -> localhost
       httpOnly: true,
       value: sessionCookieValue,
     });
-    await page.goto('http://localhost:3003/en/LA/dashboard', { waitUntil: 'networkidle0' });
+    const protocol = body.hostname.includes('localhost') ? 'http' : 'https';
+    const url = stringifyQuery(`${protocol}://${body.hostname}`, body.endpoint, body.restOfParams);
+    await page.goto(url, { waitUntil: 'networkidle0' });
     const result = await page.pdf({
       format: 'a4',
       printBackground: true,
@@ -59,6 +80,6 @@ export class PDFExportRoute extends Route<RegisterRequest> {
       'Content-Length': buffer.length,
       'Content-Disposition': 'attachment',
     });
-    return { contents: buffer, filePath: 'download.pdf' };
+    return { contents: buffer };
   }
 }
