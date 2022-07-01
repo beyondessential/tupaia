a18c737abce1da5f5b94f29d496fbd94f09302d5
Igor Nadj <igor@beyondessential.com.au>
2022-07-01 16:41:10 +1000

    Add CRUD for data service data groups

--- packages/database/src/modelClasses/DataServiceSyncGroup.js
+++ packages/database/src/modelClasses/DataServiceSyncGroup.js
@@ -7,11 +7,17 @@ import { DatabaseModel } from '../DatabaseModel';
 import { DatabaseType } from '../DatabaseType';
 import { TYPES } from '../types';
 
+const SERVICE_TYPES = {
+  KOBO: 'kobo',
+};
+
 class DataServiceSyncGroupType extends DatabaseType {
   static databaseType = TYPES.DATA_SERVICE_SYNC_GROUP;
 }
 
 export class DataServiceSyncGroupModel extends DatabaseModel {
+  SERVICE_TYPES = SERVICE_TYPES;
+
   get DatabaseTypeClass() {
     return DataServiceSyncGroupType;
   }
