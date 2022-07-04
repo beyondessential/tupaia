3a5fdd5215cf67692e2d9a68ef84100bfd89fda4
Igor Nadj <igor@beyondessential.com.au>
2022-07-04 14:30:28 +1000

    Add permissions checks to sync groups CRUD

---
+++ packages/central-server/src/apiV2/syncGroups/index.js
@@ -0,0 +1,9 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+export { DeleteSyncGroups } from './DeleteSyncGroups';
+export { EditSyncGroups } from './EditSyncGroups';
+export { GETSyncGroups } from './GETSyncGroups';
+export { CreateSyncGroups } from './CreateSyncGroups';
