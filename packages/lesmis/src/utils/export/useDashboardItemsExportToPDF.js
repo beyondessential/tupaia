cbfb5818d381ca0d4ef184f45e2f36d1f5d6f762
Biao Li <lbit123@outlook.com>
2022-07-06 14:55:42 +1000

    Export modal handles network error

--- packages/lesmis/src/utils/export/useDashboardItemsExportToPDF.js
+++ packages/lesmis/src/utils/export/useDashboardItemsExportToPDF.js
@@ -11,20 +11,31 @@ import { post } from '../../api';
 export const useDashboardItemsExportToPDF = options => {
   const { locale, entityCode, ...restOfoptions } = options;
   const [isExporting, setIsExporting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const exportToPDF = async fileName => {
     setIsExporting(true);
-    const response = await post('pdf', {
-      data: {
-        endpoint: `${locale}/pdf-export/${entityCode}`,
-        ...restOfoptions,
-        hostname: window.location.host,
-      },
-      responseType: 'blob',
-    });
-    downloadJs(response, `${fileName}.pdf`);
+    try {
+      const response = await post('pdf', {
+        data: {
+          endpoint: `${locale}/pdf-export/${entityCode}`,
+          ...restOfoptions,
+          hostname: window.location.host,
+        },
+        responseType: 'blob',
+      });
+      downloadJs(response, `${fileName}.pdf`);
+    } catch (e) {
+      setErrorMessage('Network Error. Please try again');
+    }
+
     setIsExporting(false);
   };
 
-  return { isExporting, exportToPDF };
+  const onReset = () => {
+    setErrorMessage(null);
+    setIsExporting(false);
+  };
+
+  return { isExporting, exportToPDF, errorMessage, onReset };
 };
