2ff6e2ed6b1412a1cfb91a2282f9b8f8bb07e2bb
Igor Nadj <igor@beyondessential.com.au>
2022-07-26 16:53:55 +1000

    Minor: add comment

--- packages/data-broker/src/services/dhis/translators/DhisCodeToIdTranslator.js
+++ packages/data-broker/src/services/dhis/translators/DhisCodeToIdTranslator.js
@@ -10,6 +10,13 @@ import {
 import { runDatabaseFunctionInBatches } from '@tupaia/database';
 import { reduceToDictionary } from '@tupaia/utils';
 
+/**
+ * DhisCodeToIdTranslator can be used for data pulls from a DHIS instance where there are no codes set against
+ * the Data Elements, Org Units etc.
+ *
+ * It detects such instances, and swaps all the codes it is given to ids for the fetch, then does the same for
+ * the data coming back (swapping ids in the response data for codes).
+ */
 export class DhisCodeToIdTranslator {
   constructor(models, api) {
     this.models = models;
