fe0f335ef60c86be82fcc6ada5798d61b5561047
Rohan Port <rohan@beyondessential.com.au>
2022-08-09 17:03:01 +1000

    Silence noisy warning about the 'loose' setting for '@babel/plugin-proposal-private-methods' in @tupaia/ui-components

--- packages/ui-components/.babelrc.js
+++ packages/ui-components/.babelrc.js
@@ -30,7 +30,13 @@ module.exports = {
       '@babel/plugin-proposal-private-property-in-object',
       {
         loose: true,
-      }
-    ]
-  ]
+      },
+    ],
+    [
+      '@babel/plugin-proposal-private-methods',
+      {
+        loose: true,
+      },
+    ],
+  ],
 };
