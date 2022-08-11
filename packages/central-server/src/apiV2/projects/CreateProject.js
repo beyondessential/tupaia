16acd3d1110ee7fc2bb66b305984adb838995a56
Chris Pollard <therealchrispollard@gmail.com>
2022-08-11 13:51:50 +1000

    Add rollback transaction

--- packages/central-server/src/apiV2/projects/CreateProject.js
+++ packages/central-server/src/apiV2/projects/CreateProject.js
@@ -34,22 +34,24 @@ export class CreateProject extends CreateHandler {
       dashboard_group_name,
     } = this.newRecordData;
 
-    try {
-      await this.createProjectEntity(code, name);
-      await this.createEntityHierarchy(code, entityTypes);
-      await this.createProjectEntityRelations(code, countries);
-      await this.createProjectDashboard(dashboard_group_name, code);
+    await this.models.wrapInTransaction(async transactingModels => {
+      await this.createProjectEntity(transactingModels, code, name);
+      await this.createEntityHierarchy(transactingModels, code, entityTypes);
+      await this.createProjectEntityRelations(transactingModels, code, countries);
+      await this.createProjectDashboard(transactingModels, dashboard_group_name, code);
 
-      const { id: projectEntityId } = await this.models.entity.findOne({ 'entity.code': code });
-      const { id: projectEntityHierarchyId } = await this.models.entityHierarchy.findOne({
+      const { id: projectEntityId } = await transactingModels.entity.findOne({
+        'entity.code': code,
+      });
+      const { id: projectEntityHierarchyId } = await transactingModels.entityHierarchy.findOne({
         'entity_hierarchy.name': code,
       });
 
-      const { name: dashboardGroupName } = await this.models.dashboard.findOne({
+      const { name: dashboardGroupName } = await transactingModels.dashboard.findOne({
         'dashboard.root_entity_code': code,
       });
 
-      await this.models.project.create({
+      return transactingModels.project.create({
         code,
         description,
         sort_order,
@@ -61,31 +63,14 @@ export class CreateProject extends CreateHandler {
         entity_id: projectEntityId,
         entity_hierarchy_id: projectEntityHierarchyId,
       });
-    } catch (error) {
-      await this.rollbackRecords(code);
-      throw new DatabaseError('Creating project record', error);
-    }
-  }
-
-  async rollbackRecords(projectCode) {
-    await this.models.project.delete({ code: projectCode });
-    await this.models.dashboard.delete({ root_entity_code: projectCode });
-    const projectEntity = await this.models.entity.findOne({
-      code: projectCode,
-      type: 'project',
     });
-    if (projectEntity !== null) {
-      await this.models.entityRelation.delete({ parent_id: projectEntity.id });
-      await this.models.entity.delete({ id: projectEntity.id });
-    }
-    await this.models.entityHierarchy.delete({ name: projectCode });
   }
 
-  async createProjectEntity(code, name) {
+  async createProjectEntity(models, code, name) {
     const worldCode = 'World';
-    const { id: worldId } = await this.models.entity.findOne({ 'entity.code': worldCode });
+    const { id: worldId } = await models.entity.findOne({ 'entity.code': worldCode });
 
-    await this.models.entity.create({
+    await models.entity.create({
       name,
       code,
       parent_id: worldId,
@@ -93,23 +78,23 @@ export class CreateProject extends CreateHandler {
     });
   }
 
-  async createProjectEntityRelations(code, countries) {
-    const { id: projectEntityId } = await this.models.entity.findOne({
+  async createProjectEntityRelations(models, code, countries) {
+    const { id: projectEntityId } = await models.entity.findOne({
       'entity.code': code,
     });
-    const { id: entityHierarchyId } = await this.models.entityHierarchy.findOne({
+    const { id: entityHierarchyId } = await models.entityHierarchy.findOne({
       'entity_hierarchy.name': code,
     });
 
     for (const countryId of countries) {
-      const { code: countryCode } = await this.models.country.findOne({
+      const { code: countryCode } = await models.country.findOne({
         'country.id': countryId,
       });
-      const { id: entityId } = await this.models.entity.findOne({
+      const { id: entityId } = await models.entity.findOne({
         'entity.code': countryCode,
         'entity.type': 'country',
       });
-      await this.models.entityRelation.create({
+      await models.entityRelation.create({
         parent_id: projectEntityId,
         child_id: entityId,
         entity_hierarchy_id: entityHierarchyId,
@@ -117,16 +102,16 @@ export class CreateProject extends CreateHandler {
     }
   }
 
-  async createProjectDashboard(dashboard_group_name, code) {
-    await this.models.dashboard.create({
+  async createProjectDashboard(models, dashboard_group_name, code) {
+    await models.dashboard.create({
       code: `${code}_project`,
       name: dashboard_group_name,
       root_entity_code: code,
     });
   }
 
-  async createEntityHierarchy(code, entityTypes) {
-    await this.models.entityHierarchy.create({
+  async createEntityHierarchy(models, code, entityTypes) {
+    await models.entityHierarchy.create({
       name: code,
       canonical_types: entityTypes ? `{${entityTypes.join(',')}}` : '{}',
     });
