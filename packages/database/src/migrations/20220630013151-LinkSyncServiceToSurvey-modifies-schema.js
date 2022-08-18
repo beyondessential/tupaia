f125a967c3ab1d61ac0dc11dca1c5c15e7343e89
Rohan Port <rohan@beyondessential.com.au>
2022-08-18 14:48:26 +1000

    RN-423: Reworked migration to now keep the 'code' column - give it the same value as the 'data_group_code' column as a convention

--- packages/database/src/migrations/20220630013151-LinkSyncServiceToSurvey-modifies-schema.js
+++ packages/database/src/migrations/20220630013151-LinkSyncServiceToSurvey-modifies-schema.js
@@ -19,25 +19,33 @@ exports.up = async function (db) {
     `DROP TRIGGER IF EXISTS data_service_sync_group_trigger ON data_service_sync_group`,
   );
 
-  await db.runSql(`ALTER TABLE data_service_sync_group RENAME COLUMN code TO data_group_code`);
+  // Add new code column which should match current syncGroupCode
+  await db.runSql(`ALTER TABLE data_service_sync_group ADD COLUMN data_group_code text`);
+
   await db.runSql(`
     UPDATE data_service_sync_group 
     SET 
       data_group_code = trim(both '"' from config->>'internalSurveyCode'),
-      config = (config || jsonb_build_object('syncGroupCode', data_group_code)) - 'internalSurveyCode'
+      code = trim(both '"' from config->>'internalSurveyCode'),
+      config = (config || jsonb_build_object('syncGroupCode', code)) - 'internalSurveyCode'
   `);
+  // Making code and data_group_code the same as a new convention (saves needing to have a made up code)
+  // not mandatory however, and can be changed to something else if wished
   // Note: syncGroupCode doesn't mean anything, because a Sync Group is and extension of a Data Group,
   // but we keep the old value around anyway just in case we will need it in the future
+
+  // Make data_group_code NOT NULL
+  await db.runSql(`ALTER TABLE data_service_sync_group ALTER COLUMN data_group_code SET NOT NULL`);
 };
 
 exports.down = async function (db) {
   await db.runSql(`
     UPDATE data_service_sync_group 
     SET 
-      data_group_code = trim(both '"' from config->>'syncGroupCode'),
+      code = trim(both '"' from config->>'syncGroupCode'),
       config = (config || jsonb_build_object('internalSurveyCode', data_group_code)) - 'syncGroupCode'
   `);
-  await db.runSql(`ALTER TABLE data_service_sync_group RENAME COLUMN data_group_code TO code`);
+  await db.runSql(`ALTER TABLE data_service_sync_group DROP COLUMN data_group_code`);
 };
 
 exports._meta = {
