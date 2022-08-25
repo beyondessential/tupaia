c83b02461422f982bdf740a9a44476e99ca738e0
Rohan Port <rohan@beyondessential.com.au>
2022-08-25 17:45:42 +1000

    RN-619: Made sync_group_sync_status an enum type

--- packages/database/src/migrations/20220822035354-AddSyncStatusToSyncGroup-modifies-schema.js
+++ packages/database/src/migrations/20220822035354-AddSyncStatusToSyncGroup-modifies-schema.js
@@ -15,13 +15,17 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = async function (db) {
-  return db.runSql(
-    `ALTER TABLE data_service_sync_group ADD COLUMN sync_status text DEFAULT 'IDLE'`,
-  );
+  return db.runSql(`
+    CREATE TYPE sync_group_sync_status AS ENUM ('IDLE', 'SYNCING', 'ERROR');
+    ALTER TABLE data_service_sync_group ADD COLUMN sync_status sync_group_sync_status DEFAULT 'IDLE';  
+  `);
 };
 
 exports.down = function (db) {
-  return db.runSql(`ALTER TABLE data_service_sync_group DROP COLUMN sync_status`);
+  return db.runSql(`
+    ALTER TABLE data_service_sync_group DROP COLUMN sync_status;
+    DROP TYPE IF EXISTS sync_group_sync_status;
+  `);
 };
 
 exports._meta = {
