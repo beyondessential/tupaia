43eb740126a65c55ef180983786a81b0bb75192d
Biao Li <lbit123@outlook.com>
2022-08-29 11:53:49 +1000

    Add assertUserId to permission check

--- packages/central-server/src/apiV2/userFavouriteDashboardItem/GETUserFavouriteDashboardItems.js
+++ packages/central-server/src/apiV2/userFavouriteDashboardItem/GETUserFavouriteDashboardItems.js
@@ -3,10 +3,19 @@
  * Copyright (c) 2017 - 2022 Beyond Essential Systems Pty Ltd
  */
 
+import { assertAnyPermissions } from '../../permissions';
 import { GETHandler } from '../GETHandler';
 
 export class GETUserFavouriteDashboardItems extends GETHandler {
   async assertUserHasAccess() {
-    this.req.flagPermissionsChecked(); // bypass permissions assertion
+    const assertUserId = () => {
+      const { userId } = JSON.parse(this.req.query.filter);
+      if (!userId || !this.user_id || userId !== this.user_id) {
+        return false;
+      }
+      return true;
+    };
+
+    await this.assertPermissions(assertAnyPermissions([assertUserId]));
   }
 }
