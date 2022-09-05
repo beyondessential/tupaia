0e07bc8358a0eb9d5bc31a13343bc335f8dc07d3
Igor Nadj <igor@beyondessential.com.au>
2022-09-05 10:00:33 +1000

    Fix constraint

--- packages/database/src/migrations/20220829032330-AddConstraintDataServiceConfig-modifies-schema.js
+++ packages/database/src/migrations/20220829032330-AddConstraintDataServiceConfig-modifies-schema.js
@@ -21,13 +21,13 @@ exports.up = async function (db) {
     // If any data elements or groups have not been updated to use a specific dhis instance, we set them to NOT_SET so we can add the constraint
     await db.runSql(`
       UPDATE ${table} SET config = (config || '{"dhisInstanceCode": "NOT_SET"}')
-      WHERE service_type = 'dhis' AND config->'dhisInstanceCode'::text = 'null';
+      WHERE service_type = 'dhis' AND config->>'dhisInstanceCode'::text = 'null';
     `);
 
     // Add constraint
     await db.runSql(`
       ALTER TABLE ${table}
-      ADD CONSTRAINT valid_data_service_config CHECK (service_type <> 'dhis' OR config->'dhisInstanceCode'::text <> 'null');
+      ADD CONSTRAINT valid_data_service_config CHECK (service_type <> 'dhis' OR config->>'dhisInstanceCode'::text is not null);
     `);
   }
 };
