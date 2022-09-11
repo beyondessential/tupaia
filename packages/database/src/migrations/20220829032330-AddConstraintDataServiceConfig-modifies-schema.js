09a19cd5910fffb12fbbee0054eef8a881a4bcd3
Igor <igor@beyondessential.com.au>
2022-09-12 09:48:34 +1000

    RN-625 disable 1:many mapping dhis (#4126)
    * Remove entity based dhis instance resolution
    
    * Add constraint
    
    * Remove obsolete method getApiForValue
    
    * Update test
    
    * Make schema migration safe
    
    * Fix constraint
    
    * Always use dhis instance code on data element / group
    
    * Remove obsolete fn
    
    * Stop passing deleted detectDataServices flag
    
    * Fix test
    
    Co-authored-by: Sima-BES <87400368+Sima-BES@users.noreply.github.com>

---
+++ packages/database/src/migrations/20220829032330-AddConstraintDataServiceConfig-modifies-schema.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+const TABLES = ['data_element', 'data_group'];
+
+exports.up = async function (db) {
+  for (const table of TABLES) {
+    // If any data elements or groups have not been updated to use a specific dhis instance, we set them to NOT_SET so we can add the constraint
+    await db.runSql(`
+      UPDATE ${table} SET config = (config || '{"dhisInstanceCode": "NOT_SET"}')
+      WHERE service_type = 'dhis' AND config->>'dhisInstanceCode'::text = 'null';
+    `);
+
+    // Add constraint
+    await db.runSql(`
+      ALTER TABLE ${table}
+      ADD CONSTRAINT valid_data_service_config CHECK (service_type <> 'dhis' OR config->>'dhisInstanceCode'::text is not null);
+    `);
+  }
+};
+
+exports.down = async function (db) {
+  for (const table of TABLES) {
+    // Remove constraint
+    await db.runSql(`
+      ALTER TABLE ${table}
+      DROP CONSTRAINT valid_data_service_config
+    `);
+
+    // Revert data thing
+    await db.runSql(`
+      UPDATE ${table} SET config = (config || '{"dhisInstanceCode": null}')
+      WHERE service_type = 'dhis' AND config->>'dhisInstanceCode' = 'NOT_SET';
+    `);
+  }
+};
+
+exports._meta = {
+  version: 1,
+};
