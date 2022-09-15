363e65a280137d7fe7ac1922a00968f87ff0ca55
Chris Pollard <therealchrispollard@gmail.com>
2022-09-15 16:53:34 +0200

    Create tongaRawData custom report

--- packages/report-server/src/reportBuilder/customReports/index.ts
+++ packages/report-server/src/reportBuilder/customReports/index.ts
@@ -7,10 +7,16 @@ import { Resolved } from '@tupaia/tsutils';
 import { FetchReportQuery } from '../../types';
 import { ReqContext } from '../context';
 import { testCustomReport } from './testCustomReport';
+import { tongaCovidRawData } from './tongaCovidRawData';
+import { tongaCovidRawDataFaster } from './tongaCovidRawDataFaster';
 
 type CustomReportBuilder = (reqContext: ReqContext, query: FetchReportQuery) => Promise<unknown>;
 
-export const customReports: Record<string, CustomReportBuilder> = { testCustomReport };
+export const customReports: Record<string, CustomReportBuilder> = {
+  testCustomReport,
+  tongaCovidRawData,
+  tongaCovidRawDataFaster,
+};
 
 export type CustomReportOutputType = Resolved<
   ReturnType<typeof customReports[keyof typeof customReports]>
