2db6116b1a6a64c9d04bd8690989fc88a248f256
Rohan Port <rohan@beyondessential.com.au>
2022-09-19 09:55:00 +1000

    RN-175: Switched to using TransformTable in output step

--- packages/report-server/src/reportBuilder/output/functions/default.ts
+++ packages/report-server/src/reportBuilder/output/functions/default.ts
@@ -3,8 +3,8 @@
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
 
-import { Row } from '../../types';
+import { TransformTable } from '../../transform';
 
 export const buildDefault = () => {
-  return (rows: Row[]) => rows;
+  return (table: TransformTable) => table.getRows();
 };
