cf1cab688e2a83e833f3ccf865c2990d17f5ff41
Igor Nadj <igor@beyondessential.com.au>
2022-09-23 17:38:30 +1000

    Updates to superset-api for proxy

--- packages/superset-api/src/SupersetApi.ts
+++ packages/superset-api/src/SupersetApi.ts
@@ -2,24 +2,27 @@
  * Tupaia
  * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
  */
-import { fetchWithTimeout } from '@tupaia/utils';
 import {
   ChartDataResponseSchema,
   SecurityLoginRequestBodySchema,
   SecurityLoginResponseBodySchema,
 } from './types';
-import { Agent as HttpsAgent } from 'https';
 import winston from 'winston';
 import { HttpsProxyAgent } from 'https-proxy-agent';
+import needle from 'needle';
+import type {
+  NeedleHttpVerbs,
+  BodyData as NeedleBodyData,
+  NeedleOptions,
+  NeedleResponse,
+} from 'needle';
 
 const MAX_RETRIES = 1;
-const MAX_FETCH_WAIT_TIME = 45 * 1000; // 45 seconds
 
 export class SupersetApi {
   protected serverName: string;
   protected baseUrl: string;
   protected insecure: boolean;
-  protected insecureAgent: HttpsAgent;
   protected accessToken: string | null = null;
   protected proxyAgent?: HttpsProxyAgent;
 
@@ -29,10 +32,10 @@ export class SupersetApi {
     this.serverName = serverName;
     this.baseUrl = baseUrl;
     this.insecure = insecure;
-    this.insecureAgent = new HttpsAgent({ rejectUnauthorized: false });
-    if (process.env.PROXY_URL) {
-      winston.info(`Superset using proxy ${process.env.PROXY_URL}`);
-      this.proxyAgent = new HttpsProxyAgent(process.env.PROXY_URL);
+    const proxyUrl = this.getServerVariable('SUPERSET_API_PROXY_URL');
+    if (proxyUrl) {
+      winston.info(`Superset using proxy`);
+      this.proxyAgent = new HttpsProxyAgent(proxyUrl);
     }
   }
 
@@ -51,43 +54,39 @@ export class SupersetApi {
     }
 
     const fetchConfig: any = {
-      method: 'GET',
       headers: {
         Authorization: `Bearer ${this.accessToken}`,
         'Content-Type': 'application/json',
       },
     };
-    if (this.insecure) fetchConfig.agent = this.insecureAgent;
-    if (this.proxyAgent) fetchConfig.agent = this.proxyAgent;
-    winston.info(`Superset fetch ${this.insecure ? '(insecure) ' : ' '}${url}`);
+    const result = await this.apiRequest('get', url, undefined, fetchConfig);
 
-    const result = await fetchWithTimeout(url, fetchConfig, MAX_FETCH_WAIT_TIME);
-
-    if (result.status !== 200) {
-      const bodyText = await result.text();
-
-      if (result.status === 422 || result.status === 401) {
-        winston.info(`Superset Auth error, response: ${bodyText}`);
+    if (result.statusCode !== 200) {
+      if (result.statusCode === 422 || result.statusCode === 401) {
+        winston.info(`Superset Auth error, response: ${result.body}`);
         await this.refreshAccessToken();
         return this.fetch(url, numRetries + 1);
       }
 
       throw new Error(
-        `Error response from Superset API. Status: ${result.status}, body: ${bodyText}`,
+        `Error response from Superset API. Status: ${result.statusCode}, body: ${result.body}`,
       );
     }
 
-    return await result.json();
+    return result.body as T;
+  }
+
+  protected getServerVariable(variableName: string) {
+    return (
+      process.env[`${this.serverName.toUpperCase()}_${variableName}`] ||
+      process.env[variableName] ||
+      ''
+    );
   }
 
   protected async refreshAccessToken() {
-    const getServerVariable = (variableName: string) =>
-      process.env[`${this.serverName.toUpperCase()}_${variableName}`] ||
-      process.env[variableName] ||
-      '';
-
-    const username = getServerVariable('SUPERSET_API_USERNAME');
-    const password = getServerVariable('SUPERSET_API_PASSWORD');
+    const username = this.getServerVariable('SUPERSET_API_USERNAME');
+    const password = this.getServerVariable('SUPERSET_API_PASSWORD');
 
     const body: SecurityLoginRequestBodySchema = {
       username,
@@ -98,25 +97,42 @@ export class SupersetApi {
 
     const url = `${this.baseUrl}/api/v1/security/login`;
     const fetchConfig: any = {
-      method: 'POST',
-      body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
     };
-    if (this.insecure) fetchConfig.agent = this.insecureAgent;
-    if (this.proxyAgent) fetchConfig.agent = this.proxyAgent;
+    const result = await this.apiRequest('post', url, body, fetchConfig);
 
-    winston.info(`Superset refresh access token ${this.insecure ? '(insecure) ' : ' '}${url}`);
-    const result = await fetchWithTimeout(url, fetchConfig, MAX_FETCH_WAIT_TIME);
-
-    if (result.status !== 200) {
-      const bodyText = await result.text();
+    if (result.statusCode !== 200) {
       throw new Error(
-        `Superset failed to refresh access token. Status: ${result.status}, body: ${bodyText}`,
+        `Superset failed to refresh access token. Status: ${result.statusCode}, body: ${result.body}`,
       );
     }
 
-    const resultBody: SecurityLoginResponseBodySchema = await result.json();
+    const { access_token } = result.body as SecurityLoginResponseBodySchema;
+    this.accessToken = access_token;
+  }
 
-    this.accessToken = resultBody.access_token;
+  protected async apiRequest(
+    method: NeedleHttpVerbs,
+    url: string,
+    reqBody: NeedleBodyData = {},
+    options: NeedleOptions = {},
+  ): Promise<NeedleResponse> {
+    // We use the `needle` package instead of the built-in `node-fetch` package
+    // because the fetch package does not let us set rejectUnauthorized=false
+    // to avoid SSL issues. It only lets us set agent, but we need to be able
+    // to set a proxy as the agent instead. So we have to use something that lets
+    // us do this, and needle is one such package.
+    const opts: any = {
+      ...options,
+    };
+    if (this.insecure) opts.rejectUnauthorized = false;
+    if (this.proxyAgent) opts.agent = this.proxyAgent;
+    winston.info(`Superset request ${method} ${url}`);
+
+    // TODO: opts.rejectUnauthorized not working, bad workaround for now
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    const x = await needle(method, url, reqBody, opts);
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = undefined;
+    return x;
   }
 }
