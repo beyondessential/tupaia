882641d39a8fa1202ddd6be2928925ae442a6185
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2022-10-03 17:32:38 +1100

    [no-issue]: Remove unused code

--- packages/web-config-server/jest.setup.js
+++ packages/web-config-server/jest.setup.js
@@ -5,19 +5,6 @@
 
 import { clearTestData, getTestDatabase } from '@tupaia/database';
 
-import { getIsProductionEnvironment } from '/utils';
-
-// These setup tasks need to be performed before any test, so we
-// do them in this file outside of any describe block.
-
-beforeAll(() => {
-  const isProductionEnvironment = getIsProductionEnvironment();
-  if (isProductionEnvironment) {
-    // Don't test on production
-    throw new Error('Never run the test suite on the production server, it messes with data!');
-  }
-});
-
 afterAll(async () => {
   const database = getTestDatabase();
   await clearTestData(database);
