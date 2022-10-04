137781f390c2f721b05303bc93b963fb42760f1a
Igor Nadj <igor@beyondessential.com.au>
2022-10-04 17:22:41 +1100

    Drop and reinstall materialized views on test data dump

--- packages/database/scripts/updateTestData.sh
+++ packages/database/scripts/updateTestData.sh
@@ -3,15 +3,26 @@
 SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$SCRIPT_DIR"
 
+if [ "$1" != "OK" ]; then
+  echo "Warning, rebuild of analytics table will take a while, run with OK to proceed"
+  exit 1;
+fi
+
 source ../.env
 
 # Set default port in case it wasn't in .env
 : "${DB_PORT:=5432}"
 
-# Exclude tables user tupaia doesn't have access to
-EXCLUDETABLE=$(PGPASSWORD=$DB_PASSWORD psql -t -h $DB_URL -p $DB_PORT -U $DB_USER -d $DB_NAME -c "select '--exclude-table=' || string_agg(tablename,' --exclude-table=') FROM pg_catalog.pg_tables WHERE schemaname = 'public' AND tableowner NOT LIKE '$DB_USER';" )
+# Drop materialized views
+yarn workspace @tupaia/data-api drop-analytics-table
+yarn workspace @tupaia/data-api uninstall-mv-refresh
 
+# Dump
 PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_URL -p $DB_PORT -U $DB_USER -O --schema public $EXCLUDETABLE -s $DB_NAME > "../src/__tests__/testData/testDataDump.sql"
 PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_URL -p $DB_PORT -U $DB_USER -O --schema public $EXCLUDETABLE -c -t migrations $DB_NAME >> "../src/__tests__/testData/testDataDump.sql"
+
+# Reinstall materialized views
+yarn workspace @tupaia/data-api install-mv-refresh
+yarn workspace @tupaia/data-api build-analytics-table
 
 echo "Done"
