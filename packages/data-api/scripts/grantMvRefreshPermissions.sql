b5ef3aeb086c23fbef8ab59d460c2ef62b3c91b9
Igor Nadj <igor@beyondessential.com.au>
2022-10-06 13:54:13 +1100

    Allow tupaia db to not be named tupaia

--- packages/data-api/scripts/grantMvRefreshPermissions.sql
+++ packages/data-api/scripts/grantMvRefreshPermissions.sql
@@ -9,7 +9,7 @@ ALTER ROLE :mv_user SET search_path TO public,:mv_user;
 ALTER ROLE :db_user SET search_path TO public,:mv_user;
 
 GRANT USAGE ON SCHEMA public TO :mv_user;
-GRANT ALL PRIVILEGES ON DATABASE :db_user TO :mv_user;
+GRANT ALL PRIVILEGES ON DATABASE :db_name TO :mv_user;
 GRANT ALL ON SCHEMA public  TO :mv_user;
 GRANT USAGE ON SCHEMA public  TO :mv_user;
 GRANT USAGE ON SCHEMA :mv_user TO :db_user;
