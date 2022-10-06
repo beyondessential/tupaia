2e37cf317e90f79082378f75e8d648880d66d8c7
Igor Nadj <igor@beyondessential.com.au>
2022-10-06 13:56:18 +1100

    Add setup-new-database script

---
+++ packages/database/scripts/setupNewDatabase.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$SCRIPT_DIR"
+
+if [ "$1" != "OK" ]; then
+  echo "Note: this script reads .env vars"
+  echo "   [1] Update DB_NAME etc... in database .env"
+  echo "   [2] Update DB_NAME etc... in data-api .env"
+  echo ""
+  echo "re-run with OK to continue"
+  exit 1;
+fi
+
+source ../.env
+
+# Set default port in case it wasn't in .env
+: "${DB_PORT:=5432}"
+
+set -x
+
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_PG_USER -c "CREATE ROLE $DB_USER LOGIN SUPERUSER PASSWORD '$DB_PASSWORD'" || echo "Role $DB_USER already exists?"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_PG_USER -c "CREATE ROLE tupaia_read PASSWORD '$DB_PASSWORD'" || echo "Role tupaia_read already exists?"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_PG_USER -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_PG_USER $DB_NAME -c "CREATE EXTENSION postgis"
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_PG_USER -c "ALTER USER $DB_USER WITH SUPERUSER"
+PGPASSWORD=$DB_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_USER -d $DB_NAME -f ../src/__tests__/testData/testDataDump.sql
+PGPASSWORD=$DB_PG_PASSWORD psql -h $DB_URL -p $DB_PORT -U $DB_PG_USER -c "ALTER USER $DB_USER WITH NOSUPERUSER"
+
+set +x
+
+echo "Installing mvrefresh"
+yarn workspace @tupaia/data-api install-mv-refresh
+echo "Patching mvrefresh"
+yarn workspace @tupaia/data-api patch-mv-refresh up
+echo "Installing Analytics table"
+yarn workspace @tupaia/data-api build-analytics-table
+
+echo "Deleting migrations that target data modifications, as there is no data to migrate in the new database"
+rm -rf ./src/migrations-backup
+mkdir  ./src/migrations-backup
+cp -r ./src/migrations/* ./src/migrations-backup/
+rm ./src/migrations/*modifies-data.js
+yarn migrate
+cp -r ./src/migrations-backup/* ./src/migrations/
+rm -rf ./src/migrations-backup
+
+echo "Done"
