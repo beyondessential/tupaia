66ffe09414531deeee10ca50ff994c1f11a76e7a
Biao Li <lbit123@outlook.com>
2022-10-26 13:10:41 +1100

    Setup QR code scanning for android

--- packages/meditrak-app/app/assessment/specificQuestions/EntityQuestion.js
+++ packages/meditrak-app/app/assessment/specificQuestions/EntityQuestion.js
@@ -2,28 +2,53 @@
  * Tupaia MediTrak
  * Copyright (c) 2017 Beyond Essential Systems Pty Ltd
  */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { EntityList } from '../../entityMenu';
 import { takeScrollControl, releaseScrollControl } from '../actions';
 import { CodeGeneratorQuestion } from './CodeGeneratorQuestion';
+import { QrCodeScannerScreen } from '../QrCodeScannerScreen';
 import {
+  fetchEntities,
   getEntityAttributeChecker,
   getEntityBaseFilters,
   getRecentEntities,
 } from '../../entityMenu/helpers';
+import { View } from 'react-native';
+import { Text } from '../../widgets';
 
 const DumbEntityQuestion = props => {
-  const { config, isOnlyQuestionOnScreen } = props;
-  const shouldGenerateCode = config && config.entity && config.entity.createNew;
+  const [qrCodeValue, setQrCodeValue] = useState(null);
 
-  return shouldGenerateCode ? (
-    <CodeGeneratorQuestion {...props} />
-  ) : (
-    <EntityList startOpen={isOnlyQuestionOnScreen} {...props} />
-  );
+  const onSuccess = e => {
+    props.onRowPress({ id: e.data });
+    setQrCodeValue(e.data);
+  };
+
+  const { config, isOnlyQuestionOnScreen, getEntityNameFromQRCode } = props;
+  const shouldGenerateCode = config && config.entity && config.entity.createNew;
+  const shouldShowQRCodeScanner = config?.entity?.qrCodeScanner;
+
+  if (qrCodeValue) {
+    const entityName = getEntityNameFromQRCode(qrCodeValue);
+    return (
+      <View style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+        <Text style={{ fontSize: 20 }}>ID: {entityName}</Text>
+      </View>
+    );
+  }
+
+  if (shouldGenerateCode) {
+    return <CodeGeneratorQuestion {...props} />;
+  }
+
+  if (shouldShowQRCodeScanner) {
+    return <QrCodeScannerScreen onSuccess={onSuccess} />;
+  }
+
+  return <EntityList startOpen={isOnlyQuestionOnScreen} {...props} />;
 };
 
 DumbEntityQuestion.propTypes = {
@@ -41,6 +66,10 @@ const mapStateToProps = (
 ) => {
   const baseEntityFilters = getEntityBaseFilters(state, database, questionId);
   const recentEntities = getRecentEntities(database, state, baseEntityFilters);
+  const getEntityNameFromQRCode = id => {
+    const entities = fetchEntities(database, { id });
+    return entities[0]?.code;
+  };
   const checkEntityAttributes = getEntityAttributeChecker(state, questionId);
 
   return {
@@ -48,6 +77,7 @@ const mapStateToProps = (
     checkEntityAttributes,
     recentEntities,
     selectedEntityId,
+    getEntityNameFromQRCode,
   };
 };
 
