52bbe791af72af3b1e08943f37d0ac6777531df4
Biao Li <31789355+billli0@users.noreply.github.com>
2022-12-01 13:13:24 +1100

    MAUI-1335 Add AIM demo description (#4273)

--- packages/web-frontend/src/containers/OverlayDiv/constants.js
+++ packages/web-frontend/src/containers/OverlayDiv/constants.js
@@ -8,5 +8,6 @@ export const REQUEST_PROJECT_ACCESS = 'requestProjectAccess';
 export const OVERLAY_PADDING = `35px ${isMobile() ? '35px' : '64px'}`;
 
 export const PROJECTS_WITH_LANDING_PAGES = {
+  aim_demo: true,
   covidau: true,
 };
