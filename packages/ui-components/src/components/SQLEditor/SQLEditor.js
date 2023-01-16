73a2ef9b8f057d9dd3c2ef300939dc0b728735ef
Biao Li <lbit123@outlook.com>
2023-01-16 14:51:38 +1100

    Add sql editor with custom keywords/parameters

--- packages/ui-components/src/components/SQLEditor/SQLEditor.js
+++ packages/ui-components/src/components/SQLEditor/SQLEditor.js
@@ -1,8 +1,10 @@
-import React from 'react';
 /*
  * Tupaia
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
+
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import AceEditor from 'react-ace';
 
@@ -10,18 +12,40 @@ import 'ace-builds/src-noconflict/mode-pgsql';
 import 'ace-builds/src-noconflict/theme-xcode';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
-function onChange(newValue) {
-  console.log(newValue);
-}
+export const SQLEditor = props => {
+  const { customKeywords, mode, onChange, value } = props;
+  const editorName = 'sqlEditor';
 
-export const SQLEditor = () => {
   return (
     <AceEditor
-      mode="pgsql"
+      name={editorName}
+      placeholder="Example: SELECT * FROM tablename"
+      mode={mode}
       theme="xcode"
+      value={value}
       onChange={onChange}
-      name="UNIQUE_ID_OF_DIV"
-      editorProps={{ $blockScrolling: true }}
+      editorProps={{
+        $blockScrolling: true,
+      }}
+      setOptions={{ enableLiveAutocompletion: true }}
+      onFocus={editor => {
+        const sqlEditor = editor.view.ace.edit(editorName);
+        sqlEditor.session.$mode.$highlightRules.$keywordList.push(...customKeywords);
+        sqlEditor.session.$mode.$keywordList?.push(...customKeywords);
+      }}
     />
   );
+};
+
+SQLEditor.propTypes = {
+  customKeywords: PropTypes.string,
+  mode: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+};
+
+SQLEditor.defaultProps = {
+  customKeywords: [],
+  mode: 'pgsql',
+  value: '',
 };
