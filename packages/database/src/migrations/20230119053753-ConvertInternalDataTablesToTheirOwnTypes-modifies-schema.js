a544826be92331572069d219ef6721c3b3e9c93e
Rohan Port <rohan@beyondessential.com.au>
2023-01-20 10:43:48 +1100

    RN-648: Fixed up migration

--- packages/database/src/migrations/20230119053753-ConvertInternalDataTablesToTheirOwnTypes-modifies-schema.js
+++ packages/database/src/migrations/20230119053753-ConvertInternalDataTablesToTheirOwnTypes-modifies-schema.js
@@ -18,7 +18,7 @@ exports.up = async function (db) {
   // Copy sync_cursor over to data_service_sync_group
   const internalDataTables = (await db.runSql("SELECT * FROM data_table WHERE type = 'internal'"))
     .rows;
-  for (let i = 0; i < internalDataTables; i++) {
+  for (let i = 0; i < internalDataTables.length; i++) {
     const dataTable = internalDataTables[i];
     await db.runSql(
       `
