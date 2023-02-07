7a9429fea298781e4d0fa0b78ed710c9f803cd5d
Rohan Port <rohan@beyondessential.com.au>
2023-02-07 15:57:37 +1100

    MDEV-105: Removed fetchDataElements and fetchDataGroup from TupaiaDataApi

--- packages/data-api/src/utils.ts
+++ packages/data-api/src/utils.ts
@@ -3,20 +3,6 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-export const sanitizeMetadataValue = (value: string, type: string) => {
-  switch (type) {
-    case 'Number': {
-      const sanitizedValue = parseFloat(value);
-      return Number.isNaN(sanitizedValue) ? '' : sanitizedValue;
-    }
-    case 'Binary':
-    case 'Checkbox':
-      return value === 'Yes' ? 1 : 0;
-    default:
-      return value;
-  }
-};
-
 /**
  * Analytics table values have already been partially sanitized
  * see: `build_analytics_table()` database function
