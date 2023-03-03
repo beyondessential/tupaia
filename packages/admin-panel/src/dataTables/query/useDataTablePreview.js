a1555416b9ed3c33ab2dad3a22382bcab10bfb47
Biao Li <lbit123@outlook.com>
2023-03-03 16:00:11 +1100

    Assign defaultValue to runtimeParameters if undefined in requests

--- packages/admin-panel/src/dataTables/query/useDataTablePreview.js
+++ packages/admin-panel/src/dataTables/query/useDataTablePreview.js
@@ -6,13 +6,37 @@ import { useQuery } from 'react-query';
 import { post } from '../../VizBuilderApp/api/api';
 import { DEFAULT_REACT_QUERY_OPTIONS } from '../../VizBuilderApp/api/constants';
 
+const assignDefaultValueToRuntimeParameter = (previewConfig, runtimeParameters) => {
+  const newRuntimeParameters = { ...runtimeParameters };
+  const { additionalParameters } = previewConfig?.config;
+  additionalParameters.forEach(p => {
+    const runtimeParameterValue = runtimeParameters[p.name];
+    if (
+      p?.config?.hasDefaultValue &&
+      (runtimeParameterValue === undefined ||
+        runtimeParameterValue === null ||
+        runtimeParameterValue === '')
+    ) {
+      newRuntimeParameters[p.name] = p?.config?.defaultValue;
+    }
+  });
+
+  return newRuntimeParameters;
+};
+
 export const useDataTablePreview = ({ previewConfig, runtimeParameters, onSettled }) =>
   useQuery(
     ['fetchDataTablePreviewData', previewConfig],
     async () => {
       const response = await post('fetchDataTablePreviewData', {
         data: {
-          previewConfig: { ...previewConfig, runtimeParameters },
+          previewConfig: {
+            ...previewConfig,
+            runtimeParameters: assignDefaultValueToRuntimeParameter(
+              previewConfig,
+              runtimeParameters,
+            ),
+          },
         },
       });
 
