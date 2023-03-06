5c6762b877805ed4bf5e4de30bd097b7d237581a
Chris Pollard <therealchrispollard@gmail.com>
2023-03-06 14:28:23 +1100

    add moment-timezone

--- packages/web-frontend/src/tests/utils/periodGranularities.test.js
+++ packages/web-frontend/src/tests/utils/periodGranularities.test.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import moment from 'moment';
+import moment from 'moment-timezone';
 import { mockNow, resetMocks } from '../testutil';
 import { roundStartEndDates, getDefaultDates, getLimits } from '../../utils/periodGranularities';
 
