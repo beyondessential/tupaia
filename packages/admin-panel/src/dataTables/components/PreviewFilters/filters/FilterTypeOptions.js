fc410480ca13d4f22fa3e3a6a8e884d00dfc5144
Biao Li <lbit123@outlook.com>
2023-03-17 10:06:48 +1100

    Add HierarchyField

--- packages/admin-panel/src/dataTables/components/PreviewFilters/filters/FilterTypeOptions.js
+++ packages/admin-panel/src/dataTables/components/PreviewFilters/filters/FilterTypeOptions.js
@@ -7,10 +7,12 @@ import { DatePicker } from './DatePicker';
 import { TextField } from './TextField';
 import { NumberField } from './NumberField';
 import { BooleanField } from './BooleanField';
+import { HierarchyField } from './HierarchyField';
 
 export const FilterTypeOptions = [
-  { label: 'Text', value: 'text', FilterComponent: TextField },
+  { label: 'Text', value: 'string', FilterComponent: TextField },
   { label: 'Date', value: 'date', FilterComponent: DatePicker },
   { label: 'Boolean', value: 'boolean', FilterComponent: BooleanField },
   { label: 'Number', value: 'number', FilterComponent: NumberField },
+  { label: 'Hierarchy', value: 'hierarchy', FilterComponent: HierarchyField },
 ];
