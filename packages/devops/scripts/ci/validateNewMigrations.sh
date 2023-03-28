f47b26fae4130be5a3cd2b529c966b02d4911a8a
Biao Li <lbit123@outlook.com>
2023-03-28 11:13:13 +1100

    git fetch quite

--- packages/devops/scripts/ci/validateNewMigrations.sh
+++ packages/devops/scripts/ci/validateNewMigrations.sh
@@ -54,9 +54,7 @@ function validate_migrations(){
 current_branch_name=$(get_branch_name)
 origin_branch_name="master"
 
-git fetch
-git checkout $origin_branch_name
-git checkout $current_branch_name
+git fetch origin $origin_branch_name --quiet
 
 validate_migrations $current_branch_name $origin_branch_name
 
