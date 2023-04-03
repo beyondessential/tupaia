1d7fda7c9cb9c7d453c5a59d54038a3e61844ad0
Igor Nadj <igor@beyondessential.com.au>
2023-04-03 11:54:34 +1000

    temp99

--- packages/devops/scripts/ci/validateTypesAndSchemaInSync.sh
+++ packages/devops/scripts/ci/validateTypesAndSchemaInSync.sh
@@ -4,16 +4,21 @@ SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$SCRIPT_DIR"
 cd ../../../..
 
-if git diff-index --quiet HEAD packages/types; then
+# Re-generate
+yarn workspace @tupaia/types generate:models
+
+# Check for changes
+typesdiff=$(git diff --color packages/types)
+
+if [ -z "$typesdiff" ]; then
   echo "Schema and @tupaia/types are in sync"
   exit 0
 else
   echo "❌ There are changes in the schema which are not reflected in @tupaia/types."
   echo ""
-  echo "Run yarn workspace @tupaia/types generate to fix"
+  echo "Run 'yarn workspace @tupaia/types generate' to fix"
   echo ""
   echo "Diff:"
-  git --no-pager diff
-  echo "Exiting build"
+  echo "$typesdiff"
   exit 1
 fi
