29397b7430bc9e4268b55846b6701243763cb78e
Anthony Menasse <amenasse@gmail.com>
2023-04-04 11:57:23 +1000

    Run postgres with docker compose
    Use the postgis/postgis image. The `refreshDatabase` script can be
    configured to not install the postgres extension by passing the
    `--no-gis` flag.

--- packages/database/scripts/refreshDatabase.js
+++ packages/database/scripts/refreshDatabase.js
@@ -10,7 +10,7 @@ require('dotenv/config');
 const fs = require('fs');
 const path = require('path');
 
-const { requireEnv, Script } = require('@tupaia/utils');
+const { requireEnv, Script, getEnvVarOrDefault } = require('@tupaia/utils');
 
 class RefreshDatabaseScript extends Script {
   config = {
@@ -25,6 +25,12 @@ class RefreshDatabaseScript extends Script {
         alias: 'r',
         type: 'string',
         description: 'Root for provided paths (relative to packages/database)',
+      },
+      gis: {
+        alias: 'g',
+        default: true,
+        type: 'boolean',
+        description: 'Create postgis extension',
       },
     },
     version: '1.0.0',
@@ -41,6 +47,9 @@ class RefreshDatabaseScript extends Script {
     const dbName = requireEnv('DB_NAME');
     const dbUser = requireEnv('DB_USER');
     const dbPgUser = requireEnv('DB_PG_USER');
+    const dbPort = getEnvVarOrDefault('DB_PORT', 5432);
+    const dbHost = getEnvVarOrDefault('DB_HOST', 'localhost');
+
 
     this.logInfo(`Refreshing the ${dbName} database (owner: ${dbUser})...`);
 
@@ -50,18 +59,23 @@ class RefreshDatabaseScript extends Script {
     }
 
     this.verifyPsql();
-    this.execDbCommand(`DROP DATABASE IF EXISTS ${dbName}`, { user: dbPgUser });
-    this.execDbCommand(`CREATE DATABASE ${dbName} WITH OWNER ${dbUser}`, { user: dbPgUser });
-    this.execDbCommand('CREATE EXTENSION postgis', { db: dbName, user: dbPgUser });
-    this.execDbCommand(`ALTER USER ${dbUser} WITH SUPERUSER`, { user: dbPgUser });
-    this.exec(`psql -U ${dbUser} -d ${dbName} -f "${dumpPath}"`);
-    this.execDbCommand(`ALTER USER ${dbUser} WITH NOSUPERUSER`, { user: dbPgUser });
+    const dbArgs = { user: dbPgUser, port: dbPort, host: dbHost };
+    this.execDbCommand(`DROP DATABASE IF EXISTS ${dbName}`, dbArgs);
+    this.execDbCommand(`CREATE DATABASE ${dbName} WITH OWNER ${dbUser}`, dbArgs);
+    // Loading GIS extension is optional - it's already loaded when using
+    // postgis/postgis Docker container
+    if (this.args.gis) {
+      this.execDbCommand('CREATE EXTENSION postgis', dbArgs);
+    }
+    this.execDbCommand(`ALTER USER ${dbUser} WITH SUPERUSER`, dbArgs);
+    this.exec(`psql -U ${dbUser} -d ${dbName} -h "${dbHost}" -p ${dbPort} -f "${dumpPath}"`);
+    this.execDbCommand(`ALTER USER ${dbUser} WITH NOSUPERUSER`, dbArgs);
   }
 
   resolvePath = relativePath =>
     path.resolve([this.args.root, relativePath].filter(x => !!x).join('/'));
 
-  execDbCommand = (dbCommand, { user, db } = {}) => {
+  execDbCommand = (dbCommand, { user, db, host, port } = {}) => {
     const parts = ['psql'];
     if (user) {
       parts.push('-U', user);
@@ -71,6 +85,13 @@ class RefreshDatabaseScript extends Script {
     }
     if (db) {
       parts.push('-d', db);
+    }
+
+    if (host) {
+      parts.push('-h', host);
+    }
+    if (port) {
+      parts.push('-p', port);
     }
     parts.push('-c', `"${dbCommand}"`);
 
