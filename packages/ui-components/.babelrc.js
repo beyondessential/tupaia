3f844bd1213566163ae8bdcbe6c94d04b97b8545
Alex Dunham <alex@beyondessential.com.au>
2023-04-04 12:07:14 +1200

    WAITP-1145 create commands for reloading on dev

--- packages/ui-components/.babelrc.js
+++ packages/ui-components/.babelrc.js
@@ -20,6 +20,7 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
     [
       '@babel/plugin-proposal-class-properties',
       {
