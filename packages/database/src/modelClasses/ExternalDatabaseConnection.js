191a54e7962bc21e663a90c7bd044067e0883d82
Biao Li <lbit123@outlook.com>
2023-04-11 11:21:31 +1000

    Knex to handle numeric type

--- packages/database/src/modelClasses/ExternalDatabaseConnection.js
+++ packages/database/src/modelClasses/ExternalDatabaseConnection.js
@@ -5,12 +5,17 @@
 
 import { getEnvVarOrDefault, requireEnv } from '@tupaia/utils';
 import knex from 'knex';
+import { types as pgTypes } from 'pg';
 
 import { DatabaseModel } from '../DatabaseModel';
 import { DatabaseType } from '../DatabaseType';
 import { TYPES } from '../types';
 
 const EXT_DB_CONNECTION_ENV_VAR_PREFIX = 'EXT_DB';
+
+pgTypes.setTypeParser(pgTypes.builtins.NUMERIC, value => {
+  return parseFloat(value);
+});
 
 export class ExternalDatabaseConnectionType extends DatabaseType {
   static databaseType = TYPES.EXTERNAL_DATABASE_CONNECTION;
