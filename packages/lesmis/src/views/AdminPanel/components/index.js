ffe49d0182d8c9b1a74e15894d8c1df200eafe76
Rohan Port <rohan@beyondessential.com.au>
2023-04-25 11:34:22 +1000

    RN-736: Fixed approval button not working

---
+++ packages/lesmis/src/views/AdminPanel/components/index.js
@@ -0,0 +1,8 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+export * from './getSurveyResponsesExportModal';
+export * from './ApproveButton';
+export * from './RejectButton';
