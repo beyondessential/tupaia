c3ae0294405efad665013b83e12ba6ab9e7b19eb
Tom Caiger <caigertom@gmail.com>
2023-04-27 09:39:05 +1200

    Update GETProjects.js

--- packages/central-server/src/apiV2/GETProjects.js
+++ packages/central-server/src/apiV2/GETProjects.js
@@ -39,6 +39,8 @@ export class GETProjects extends GETHandler {
     entity_hierarchy: ['entity_hierarchy.id', 'project.entity_hierarchy_id'],
   };
 
+  countriesFieldKey = 'countries';
+
   async findSingleRecord(projectId, options) {
     const projectPermissionChecker = accessPolicy =>
       assertProjectPermissions(accessPolicy, this.models, projectId);
@@ -46,31 +48,36 @@ export class GETProjects extends GETHandler {
       assertAnyPermissions([assertBESAdminAccess, projectPermissionChecker]),
     );
 
-    const countryNameKey = options.columns.find(x => Object.keys(x)[0] === 'countries');
+    const countryNameKey = options.columns.find(x => Object.keys(x)[0] === this.countriesFieldKey);
     if (!countryNameKey) {
       return super.findSingleRecord(projectId, options);
     }
 
-    const columns = options.columns.filter(x => Object.keys(x)[0] !== 'countries');
+    const columns = options.columns.filter(x => Object.keys(x)[0] !== this.countriesFieldKey);
     const dbOptions = { ...options, columns: [...columns, { id: 'project.id' }] };
-    const countries = await this.getProjectCountries();
-    const countriesById = keyBy(countries, 'projectId');
+    const countriesById = await this.getProjectCountriesByProjectId();
     const project = await super.findSingleRecord(projectId, dbOptions);
     return { ...project, countries: countriesById[project.id].countryNames };
   }
 
   async findRecords(criteria, options) {
-    const countryNameKey = options.columns.find(x => Object.keys(x)[0] === 'countries');
+    const { recordId } = this;
+    const projectPermissionChecker = accessPolicy =>
+      assertProjectPermissions(accessPolicy, this.models, recordId);
+    await this.assertPermissions(
+      assertAnyPermissions([assertBESAdminAccess, projectPermissionChecker]),
+    );
+
+    const countryNameKey = options.columns.find(x => Object.keys(x)[0] === this.countriesFieldKey);
     if (!countryNameKey) {
       return super.findRecords(criteria, options);
     }
 
-    const columns = options.columns.filter(x => Object.keys(x)[0] !== 'countries');
+    const columns = options.columns.filter(x => Object.keys(x)[0] !== this.countriesFieldKey);
     const dbOptions = { ...options, columns };
     const projects = await this.database.find(this.recordType, criteria, dbOptions);
 
-    const countries = await this.getProjectCountries();
-    const countriesById = keyBy(countries, 'projectId');
+    const countriesById = await this.getProjectCountriesByProjectId();
     return projects.map(project => {
       return {
         ...project,
@@ -79,14 +86,15 @@ export class GETProjects extends GETHandler {
     });
   }
 
-  async getProjectCountries() {
-    return this.database.executeSql(
+  async getProjectCountriesByProjectId() {
+    const countries = await this.database.executeSql(
       `SELECT p.id as "projectId", ARRAY_AGG(child_entity.name) as "countryNames" FROM entity e
         JOIN project p ON p.entity_id = e.id
         JOIN entity_relation er ON er.entity_hierarchy_id = p.entity_hierarchy_id AND er.parent_id = e.id
         JOIN entity child_entity ON child_entity.id = er.child_id
        WHERE child_entity.type = 'country' GROUP BY p.id;`,
     );
+    return keyBy(countries, 'projectId');
   }
 
   async getPermissionsFilter(criteria, options) {
