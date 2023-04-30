38f12298f502e359544c1cac83cf4da8f9283914
Tom Caiger <caigertom@gmail.com>
2023-05-01 11:52:04 +1200

    MDEV-78: Add permission group ancestors column to Admin Panel (#4499)
    * add parent column
    
    * add ancestors
    
    * Update GETPermissionGroups.js
    
    * Update GETPermissionGroups.js

--- packages/admin-panel/src/pages/resources/PermissionGroupsPage.js
+++ packages/admin-panel/src/pages/resources/PermissionGroupsPage.js
@@ -6,11 +6,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ResourcePage } from './ResourcePage';
+import { prettyArray } from '../../utilities';
 
 const COLUMNS = [
   {
     Header: 'Name',
     source: 'name',
+  },
+  {
+    Header: 'Ancestors',
+    source: 'ancestors',
+    filterable: false,
+    // eslint-disable-next-line react/prop-types
+    Cell: ({ value: ancestors }) => (ancestors.length > 0 ? prettyArray(ancestors) : <ul> - </ul>),
+    accessor: ({ ancestors }) => ancestors.map(a => a.name).reverse(),
   },
 ];
 
@@ -20,7 +29,10 @@ const CREATE_CONFIG = {
     title: 'Edit Permission Group',
     editEndpoint: 'permissionGroups',
     fields: [
-      ...COLUMNS,
+      {
+        Header: 'Name',
+        source: 'name',
+      },
       {
         Header: 'Parent',
         source: 'parent_id',
