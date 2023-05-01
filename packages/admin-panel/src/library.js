b8adcc117334fa60c1a34bb2f2cc99ca560a3b6a
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2023-05-01 15:50:24 +1000

    RN-736: Fixed LESMIS Admin Panel permissions (#4301)

--- packages/admin-panel/src/library.js
+++ packages/admin-panel/src/library.js
@@ -11,5 +11,6 @@ export { ReduxAutocomplete } from './autocomplete';
 export { ExportModal } from './importExport';
 export { IconButton } from './widgets';
 export { AdminPanelDataProviders } from './utilities/AdminPanelProviders';
+export { useApi } from './utilities/ApiProvider';
 export { DataChangeAction } from './editor';
 export { App as VizBuilderApp } from './VizBuilderApp/App';
