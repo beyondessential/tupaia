93bd94aabd1c118627dc79165e0dec880a538dab
acdunham <alex@beyondessential.com.au>
2023-05-02 14:52:25 +1200

    WAITP-1191 Create page for custom landing pages

---
+++ packages/admin-panel/src/pages/resources/CustomLandingPagesPage.js
@@ -0,0 +1,290 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SECTION_FIELD_TYPE } from '../../editor/constants';
+import { ResourcePage } from './ResourcePage';
+import { ArrayFilter } from '../../table/columnTypes/columnFilters';
+import { prettyArray } from '../../utilities';
+
+const LANDING_PAGES_ENDPOINT = 'landingPages';
+
+// the URL prefix to display in the url_segment field
+const URL_PREFIX = `${window.location.origin
+  .replace('admin', 'www')
+  .replace(new RegExp('(https://)|(http://)'), '')}/`;
+
+// All the fields for create/edit of a custom landing page
+const FIELDS = [
+  {
+    type: SECTION_FIELD_TYPE,
+    fields: [
+      {
+        Header: 'Name',
+        source: 'name',
+        // this is to denote this field as a column in the table
+        isColumn: true,
+      },
+      {
+        Header: 'Add name to header title (optional)',
+        source: 'include_name_in_header',
+        editConfig: {
+          type: 'checkbox',
+          name: 'include_name_in_header',
+          // the value that will be sent to the server if the checkbox is checked
+          optionValue: true,
+          labelTooltip: 'Check this if your logo does not include a name',
+        },
+      },
+      {
+        Header: 'Custom link',
+        source: 'url_segment',
+        isColumn: true,
+        // this is to mark this field as being non-editable (disabled in edit mode)
+        updateDisabled: true,
+        // format the value to include the URL prefix in the table
+        Cell: ({ value }) => `${URL_PREFIX}${value}`,
+        editConfig: {
+          // the prefix to display in the field
+          startAdornment: URL_PREFIX,
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    title: 'Add main image',
+    description: '(2000px x 2000px)',
+    fields: [
+      {
+        Header: 'Main image',
+        source: 'image_url',
+        editConfig: {
+          type: 'image',
+          avatarVariant: 'square',
+          deleteModal: {
+            title: 'Remove landing page image',
+            message: 'Are you sure you want to delete your image?',
+          },
+          maxHeight: 2000,
+          maxWidth: 2000,
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    title: 'Add logo image',
+    description: '(800px x 800px)',
+    fields: [
+      {
+        Header: 'Logo image',
+        source: 'logo_url',
+        editConfig: {
+          type: 'image',
+          avatarVariant: 'square',
+          deleteModal: {
+            title: 'Remove logo image',
+            message: 'Are you sure you want to delete your image?',
+          },
+          maxHeight: 800,
+          maxWidth: 800,
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    title: 'Primary colour',
+    description:
+      'This colour will be used for the header bar and other details. If no colour is specified a default colour will be applied.',
+    fields: [
+      {
+        Header: 'Primary colour hex code',
+        source: 'primary_hexcode',
+        editConfig: {
+          type: 'hexcode',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Secondary colour',
+    description:
+      'This colour will be used for text and other accents. Please select the option that provides the most contrast to your primary colour. If no colour is specified a default colour will be applied.',
+    type: SECTION_FIELD_TYPE,
+    fields: [
+      {
+        Header: 'Secondary colour',
+        source: 'secondary_hexcode',
+        editConfig: {
+          type: 'radio',
+          name: 'secondary_hexcode',
+          options: [
+            {
+              label: 'White',
+              value: '#FFFFFF',
+            },
+            {
+              label: 'Black',
+              value: '#000000',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    fields: [
+      {
+        Header: 'Extended title (max 60 characters)',
+        source: 'extended_title',
+        editConfig: {
+          maxLength: 60,
+          type: 'text',
+          placeholder: 'A short sentence about your project',
+        },
+      },
+      {
+        Header: 'Long bio (max 250 characters)',
+        source: 'long_bio',
+        editConfig: {
+          maxLength: 250,
+          type: 'textarea',
+          placeholder: 'A longer bio about your project',
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    title: 'Contact information',
+    description: 'Add contact information you would like to show on the landing page.',
+    fields: [
+      {
+        Header: 'Phone',
+        source: 'phone_number',
+      },
+      {
+        Header: 'Website',
+        source: 'website_url',
+        editConfig: {
+          placeholder: 'www.example.com',
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    title: 'External link',
+    description: 'Add an external link where users can learn more about your project',
+    fields: [
+      {
+        Header: 'URL',
+        source: 'external_link',
+        editConfig: {
+          placeholder: 'www.example.com',
+        },
+      },
+    ],
+  },
+  {
+    type: SECTION_FIELD_TYPE,
+    title: 'Project',
+    description: 'Enter project code below. You may enter multiple codes separated by a comma.',
+    fields: [
+      {
+        Header: 'Project code/s',
+        source: 'project_codes',
+        isColumn: true,
+        Filter: ArrayFilter,
+        Cell: ({ value }) => prettyArray(value),
+        editConfig: {
+          optionsEndpoint: 'projects',
+          optionLabelKey: 'code',
+          optionValueKey: 'code',
+          allowMultipleValues: true,
+        },
+      },
+    ],
+  },
+];
+
+const EDIT_FIELDS = FIELDS.reduce((result, item) => {
+  if (item.type === SECTION_FIELD_TYPE) {
+    return [
+      ...result,
+      {
+        ...item,
+        fields: item.fields.map(field => ({
+          ...field,
+          editConfig: {
+            ...(field.editConfig || {}),
+            editable: field.updateDisabled !== true,
+          },
+        })),
+      },
+    ];
+  }
+  return [
+    ...result,
+    {
+      ...item,
+      editConfig: {
+        ...(item.editConfig || {}),
+        editable: item.updateDisabled !== true,
+      },
+    },
+  ];
+}, []);
+
+// Only show fields that are marked as columns, plus the edit and delete buttons
+const COLUMNS = [
+  ...FIELDS.reduce((result, item) => {
+    if (item.type === SECTION_FIELD_TYPE) {
+      return [...result, ...item.fields.filter(field => field.isColumn)];
+    }
+    return item.isColumn ? [...result, item] : result;
+  }, []),
+  {
+    Header: 'Edit',
+    type: 'edit',
+    source: 'id',
+    actionConfig: {
+      title: 'Edit Landing page',
+      editEndpoint: LANDING_PAGES_ENDPOINT,
+      fields: EDIT_FIELDS,
+    },
+  },
+  {
+    Header: 'Delete',
+    source: 'id',
+    type: 'delete',
+    actionConfig: {
+      endpoint: LANDING_PAGES_ENDPOINT,
+    },
+  },
+];
+
+const CREATE_CONFIG = {
+  actionConfig: {
+    title: 'New landing page',
+    editEndpoint: LANDING_PAGES_ENDPOINT,
+    fields: FIELDS,
+  },
+};
+
+export const CustomLandingPagesPage = ({ getHeaderEl }) => {
+  return (
+    <ResourcePage
+      title="Landing Pages"
+      endpoint={LANDING_PAGES_ENDPOINT}
+      columns={COLUMNS}
+      getHeaderEl={getHeaderEl}
+      createConfig={CREATE_CONFIG}
+    />
+  );
+};
+
+CustomLandingPagesPage.propTypes = {
+  getHeaderEl: PropTypes.func.isRequired,
+};
