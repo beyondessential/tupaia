5410431689ffb7365793b8e67d675a8cde3f6898
acdunham <alex@beyondessential.com.au>
2023-05-12 14:33:16 +1200

    WAITP-1257 configure ts in ui-components

--- packages/ui-components/src/components/index.js
+++ packages/ui-components/src/components/index.js
@@ -44,4 +44,4 @@ export * from './Toolbar';
 export * from './Tooltip';
 export * from './PDFExportComponent';
 export * from './CheckboxList';
-export * from './FavouriteButton';
+export * from './FavouriteButton'; 
