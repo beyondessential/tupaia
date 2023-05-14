28347c6e60b65e96098bf90a9455586631f15f9f
acdunham <129009580+acdunham@users.noreply.github.com>
2023-05-15 11:18:50 +1200

    Waitp 1257 UI components ts setup (#4563)
    * WAITP-1257 configure ts in ui-components
    
    * WAITP-1257 update scripts that watch ui-components
    
    * WAITP-1257 revert change with build-dev script
    
    ---------
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>

--- packages/ui-components/.storybook/main.js
+++ packages/ui-components/.storybook/main.js
@@ -1,5 +1,5 @@
 // import the build chain webpack config and merge it with storybook config
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.js', '../stories/**/*.stories.ts'],
   addons: ['@storybook/addon-essentials'],
 };
