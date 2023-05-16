3253f09d070e51ef32e33a9866240b9b3663456e
acdunham <alex@beyondessential.com.au>
2023-05-17 11:08:25 +1200

    WAITP-1260 DataTable to ts

---
+++ packages/ui-components/src/components/DataTable/DataTableCell.tsx
@@ -0,0 +1,34 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+import React, { FC, ReactElement } from 'react';
+import { TableCell, TableCellProps } from '@material-ui/core';
+
+const getDataType = (value: any): string => (value === null ? 'null' : typeof value);
+
+const formatValue = (value: any): string => {
+  const type = getDataType(value);
+  switch (type) {
+    case 'null':
+      return 'NULL';
+    case 'object':
+      return JSON.stringify(value);
+    case 'function':
+      return '[Function]';
+    default:
+      return `${value}`;
+  }
+};
+
+export const DataTableCell: FC<
+  TableCellProps & {
+    value: number | string | boolean | Object | Function;
+  }
+> = ({ value, ...props }): ReactElement => {
+  return (
+    <TableCell {...props} className={`data-type-${getDataType(value)}`}>
+      {formatValue(value)}
+    </TableCell>
+  );
+};
