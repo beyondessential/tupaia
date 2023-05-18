a91549dbaf5fd0d29d43c6d79e4eea2ac106c601
acdunham <alex@beyondessential.com.au>
2023-05-18 12:41:54 +1200

    WAITP-1260 Datatable part 2

--- packages/ui-components/src/types/react-table-config.d.ts
+++ packages/ui-components/src/types/react-table-config.d.ts
@@ -6,8 +6,6 @@ import {
 } from 'react-table';
 
 declare module 'react-table' {
-  // take this file as-is, or comment out the sections that don't apply to your plugin configuration
-
   export interface TableOptions<D extends object>
     extends UseExpandedOptions<D>,
       UseSortByOptions<D>,
