8ded7f569863f85f00057fd3dca652b5146914a5
acdunham <alex@beyondessential.com.au>
2023-05-18 12:45:33 +1200

    WAITP-1260 Update copyright dates

--- packages/ui-components/src/components/DataTable/StyledTable.js
+++ packages/ui-components/src/components/DataTable/StyledTable.js
@@ -1,6 +1,6 @@
 /*
  * Tupaia
- *  Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 import styled from 'styled-components';
 import MuiTable from '@material-ui/core/Table';
