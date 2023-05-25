6f0c1909c746f8ca82c8b01f41a7e8b5b7e032e8
acdunham <alex@beyondessential.com.au>
2023-05-25 13:19:23 +1200

    WAITP-1259 PopupDataItemList to ts

---
+++ packages/ui-map-components/src/components/PopupDataItemList.tsx
@@ -0,0 +1,44 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ *
+ */
+
+import React from 'react';
+import { getFormattedInfo } from '../utils';
+import { Series } from '../types';
+
+interface PopupDataItemProps {
+  measureName: Series['name'];
+  value: Series['value'];
+}
+
+const PopupDataItem = ({ measureName, value }: PopupDataItemProps) => (
+  <div key={measureName}>
+    <span>{`${measureName}: `}</span>
+    <strong>{value}</strong>
+  </div>
+);
+
+interface PopupDataItemListProps {
+  serieses: Series[];
+  data?: object;
+}
+
+export const PopupDataItemList = ({ serieses, data = {} }: PopupDataItemListProps) => {
+  return (
+    <>
+      {serieses
+        .filter(series => !series.hideFromPopup)
+        .sort((measure1, measure2) => measure1.sortOrder - measure2.sortOrder)
+        .map(series => {
+          const { name } = series;
+          const { formattedValue, valueInfo } = getFormattedInfo(data, series);
+          return valueInfo.hideFromPopup ? null : (
+            <PopupDataItem key={name} measureName={name} value={formattedValue} />
+          );
+        })
+        .filter(popupItem => popupItem !== null)}
+    </>
+  );
+};
