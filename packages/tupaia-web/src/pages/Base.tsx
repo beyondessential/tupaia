c681fe94d4d38014901ef05df14e5a7b1a4620d7
acdunham <alex@beyondessential.com.au>
2023-06-01 14:34:41 +1200

    WAITP-1202 tidy up code

--- packages/tupaia-web/src/pages/Base.tsx
+++ packages/tupaia-web/src/pages/Base.tsx
@@ -7,11 +7,14 @@ import { useLocation } from 'react-router-dom';
 import { LandingPage, Project } from '.';
 
 export const Base = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
-  const finalURLChar = location.pathname[location.pathname.length - 1];
-  const isLandingPage =
-    location.pathname.split('/').length - 1 - (finalURLChar === '/' ? 1 : 0) === 1;
+  // This is to handle trailing '/' in a pathname
+  const finalURLChar = pathname[pathname.length - 1];
+  const finalCharSubtractor = finalURLChar === '/' ? 1 : 0;
+
+  // If a pathname has only 1 segment, treat as a landing page, otherwise treat as a project
+  const isLandingPage = pathname.split('/').length - 1 - finalCharSubtractor === 1;
 
   return isLandingPage ? <LandingPage /> : <Project />;
 };
