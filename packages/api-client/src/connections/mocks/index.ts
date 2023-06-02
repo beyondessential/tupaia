0fe798bd608efe36b55e88c18738b8215d28f5f0
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-06-02 13:46:57 +1200

    WAITP-1255 Add mock export

--- packages/api-client/src/connections/mocks/index.ts
+++ packages/api-client/src/connections/mocks/index.ts
@@ -8,3 +8,4 @@ export { MockCentralApi } from './MockCentralApi';
 export { MockDataTableApi } from './MockDataTableApi';
 export { MockEntityApi } from './MockEntityApi';
 export { MockReportApi } from './MockReportApi';
+export { MockWebConfigApi } from './MockWebConfigApi';
