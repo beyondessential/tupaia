d9a50aa2600c5b3648568054f0aa99056dd8a81b
Tom Caiger <caigertom@gmail.com>
2023-06-06 10:00:26 +1200

    fix tupaia-web

--- packages/tupaia-web/src/App.tsx
+++ packages/tupaia-web/src/App.tsx
@@ -2,7 +2,7 @@
  * Tupaia
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
-
+import React from 'react';
 import { Alert } from '@tupaia/ui-components';
 import { AppProviders } from './AppProviders';
 
--- packages/tupaia-web/src/AppProviders.tsx
+++ packages/tupaia-web/src/AppProviders.tsx
@@ -2,7 +2,7 @@
  * Tupaia
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import {
   createMuiTheme,
   ThemeProvider as MuiThemeProvider,
