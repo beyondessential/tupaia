2b078832de8bf80d3de87eac189a5b2fe0873980
acdunham <alex@beyondessential.com.au>
2023-06-06 13:22:42 +1200

    WAITP-1200 Update styled-components version and config, + theme

--- packages/tupaia-web/src/AppProviders.tsx
+++ packages/tupaia-web/src/AppProviders.tsx
@@ -2,41 +2,32 @@
  * Tupaia
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import {
   StylesProvider,
-  createTheme,
+  createMuiTheme,
   ThemeProvider as MuiThemeProvider,
 } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from 'styled-components';
 
-const muiTheme = createTheme({
+// Base theme overrides. Any extra overrides should be added here, as needed.
+const theme = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: {
+      main: '#0296c5', // Main blue (as seen on primary buttons)
+    },
+    secondary: {
+      main: '##ee6230', // Tupaia Orange
+    },
+    background: {
+      default: '#262834', // Dark blue background
+    },
   },
 });
 
-const theme = {
-  ...muiTheme,
-  boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.3)',
-  colors: {
-    black: {
-      high: 'rgba(43, 45, 56, 0.97)',
-      mid: 'rgba(43, 45, 56, 0.94)',
-      low: 'rgba(43, 45, 56, 0.8)',
-    },
-    blue: {
-      darkest: '#262834',
-      dark: '#2e3040',
-      mid: '#22c7fc',
-      primary: '#2196f3',
-      light: '#cde9ff',
-    },
-  },
-};
-
-export const AppStyleProviders = ({ children }: { children: ReactNode }) => (
+export const AppProviders = ({ children }: { children: ReactNode }) => (
   <StylesProvider injectFirst>
     <ThemeProvider theme={theme}>
       <MuiThemeProvider theme={theme}>
