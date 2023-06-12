07f0b889a93efbc7cefebc2a60fd524224cfe9ba
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-06-12 14:35:18 +1200

    WAITP-1256 Add ancestors routes to entity-server

---
+++ packages/entity-server/src/routes/hierarchy/MultiEntityAncestorsRoute.ts
@@ -0,0 +1,36 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+import { Route } from '@tupaia/server-boilerplate';
+import { formatEntitiesForResponse } from './format';
+import {
+  MultiEntityRequest,
+  MultiEntityRequestParams,
+  MultiEntityRequestBody,
+  EntityRequestQuery,
+  EntityResponse,
+} from './types';
+
+export type MultiEntityAncestorsRequest = MultiEntityRequest<
+  MultiEntityRequestParams,
+  EntityResponse[],
+  MultiEntityRequestBody,
+  EntityRequestQuery & { includeRootEntity?: string }
+>;
+export class MultiEntityAncestorsRoute extends Route<MultiEntityAncestorsRequest> {
+  public async buildResponse() {
+    const { hierarchyId, entities, fields, field, filter } = this.req.ctx;
+    const { includeRootEntity: includeRootEntityString = 'false' } = this.req.query;
+    const includeRootEntity = includeRootEntityString?.toLowerCase() === 'true';
+    const ancestors = await this.req.models.entity.getAncestorsOfEntities(
+      hierarchyId,
+      entities.map(entity => entity.id),
+      { ...filter },
+    );
+    const entitiesToUse = includeRootEntity ? entities.concat(ancestors) : ancestors;
+
+    return formatEntitiesForResponse(this.req.models, this.req.ctx, entitiesToUse, field || fields);
+  }
+}
