53c9b4319d2568bfc4c5b6f9600d063752f94325
Chris Pollard <therealchrispollard@gmail.com>
2023-06-16 09:33:51 +1000

    RN-928: Tupaia Api to S3 File Upload (#4617)
    * lint update
    
    * add file to s3
    
    * RN-928: update based on new sdk
    
    * rn-928: update function name

--- packages/central-server/src/apiV2/meditrakApp/utilities/addSurveyFile.js
+++ packages/central-server/src/apiV2/meditrakApp/utilities/addSurveyFile.js
@@ -4,11 +4,24 @@
  */
 
 import winston from 'winston';
+import { S3Client, S3 } from '@tupaia/utils';
 
-export const addSurveyFile = async (models, { id, filename, data }) => {
+/**
+ *  action object e.g.
+ * {
+ *    "action": "AddSurveyFile",
+ *    "payload": {
+ *         "id": "5da02ed278d10e8695530688",
+ *         "filename": "report.pdf",
+ *         "data": "ASDFJASD..." // etc very long base64 data
+ *    }
+ *  }
+ * */
+
+export const addSurveyFile = async (models, { filename, data }) => {
   try {
-    // TODO: stub
-    console.log('addSurveyFile', id, filename);
+    const s3Client = new S3Client(new S3());
+    await s3Client.uploadFile(filename, data);
   } catch (error) {
     winston.error(error.message);
   }
