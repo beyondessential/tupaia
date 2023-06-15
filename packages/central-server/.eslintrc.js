2beb48658205e36c9ed7d2b49c5d4dd7e72731bd
Igor <igor@beyondessential.com.au>
2023-06-15 15:54:19 +1000

    RN-926 Eslint setup for monorepo (#4643)
    * RN-926 Eslint setup for monorepo
    
    * Simplify common lint script
    
    * Minor: add missing devDependency sinon
    
    * Fix lesmis build breaking after linting change
    
    * Add build/ to eslint ignore
    
    * Fix psss build: move faker to dependencies
    
    This was marked as a devDependency but used in production.
    
    * Fix psss build breaking after linting change

---
+++ packages/central-server/.eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = {
+  // Don't extend standard eslint-js because we use mocha not jest
+  extends: '@beyondessential/js',
+  parser: 'babel-eslint',
+  rules: {
+    'import/no-extraneous-dependencies': 'error',
+  },
+  overrides: [
+    {
+      files: ['src/tests/**/*.js'],
+      env: {
+        mocha: true
+      },
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true // allow importing from devDependencies for tests
+          }
+        ],
+        'no-unused-expressions': 'off' // not chai friendly
+      }
+    }
+  ],
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    '.eslintrc.js'
+  ]
+};
