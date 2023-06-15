e3848fe55698c3175ccbeb71bd6e57e898f8e758
Tom Caiger <caigertom@gmail.com>
2023-06-15 17:22:42 +1200

    Update Sidebar.tsx

--- packages/tupaia-web/src/layout/Sidebar/Sidebar.tsx
+++ packages/tupaia-web/src/layout/Sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
 import { TRANSPARENT_BLACK } from '../../constants';
 import { ExpandButton } from './ExpandButton';
 
@@ -14,20 +15,59 @@ const MAX_SIDEBAR_COLLAPSED_WIDTH = 350;
 const Panel = styled.div<{
   $isExpanded: boolean;
 }>`
-  display: flex;
-  align-items: stretch;
-  flex-direction: column;
-  align-content: stretch;
   position: relative;
-  overflow: visible;
   background-color: ${TRANSPARENT_BLACK};
-  height: 100%;
-  cursor: auto;
   transition: width 0.5s ease, max-width 0.5s ease;
   width: ${({ $isExpanded }) => ($isExpanded ? 45 : 30)}%;
   min-width: 335px;
   max-width: ${({ $isExpanded }) =>
     $isExpanded ? MAX_SIDEBAR_EXPANDED_WIDTH : MAX_SIDEBAR_COLLAPSED_WIDTH}px;
+`;
+
+const Breadcrumbs = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  right: 10px;
+  background: #efefef;
+  height: 30px;
+`;
+
+const Image = styled.div`
+  background: #62698d;
+  width: 100%;
+  height: 200px;
+`;
+
+const Button = styled.div`
+  background: #efefef;
+  width: 100%;
+  height: 300px;
+`;
+
+const Title = styled(Typography)`
+  color: white;
+  margin: 1rem;
+`;
+
+const Dropdown = styled.div`
+  background: #4a4b55;
+  width: 100%;
+  padding: 1rem;
+  font-size: 1rem;
+`;
+
+const ChartsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #4a4b55;
+`;
+
+const Chart = styled.div`
+  background: #272832;
+  width: 100%;
+  height: 220px;
+  margin-bottom: 0.5rem;
 `;
 
 export const Sidebar = () => {
@@ -40,6 +80,17 @@ export const Sidebar = () => {
   return (
     <Panel $isExpanded={isExpanded}>
       <ExpandButton setIsExpanded={toggleExpanded} isExpanded={isExpanded} />
+      <Breadcrumbs />
+      <Image />
+      <Title variant="h5">Northern</Title>
+      <Dropdown>General</Dropdown>
+      <ChartsContainer>
+        <Chart />
+        <Chart />
+        <Chart />
+        <Chart />
+        <Chart />
+      </ChartsContainer>
     </Panel>
   );
 };
