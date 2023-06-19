d22518ebbaabd2cfc22867f0a18497829f5554e3
Igor Nadj <igor@beyondessential.com.au>
2023-06-19 16:15:24 +1000

    Refactor: move assertCanAddDataElementInGroup
    It is specific to survey importing, doesn't belong in utilities.

--- packages/central-server/src/database/utilities/index.js
+++ packages/central-server/src/database/utilities/index.js
@@ -3,6 +3,5 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-export { assertCanAddDataElementInGroup } from './assertCanAddDataElementInGroup';
 export { getEntityIdFromClinicId } from './getEntityIdFromClinicId';
 export { getUniversalTypes } from './getUniversalTypes';
