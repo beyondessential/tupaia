28e565441f2c88105038746197d64678391f2bf6
acdunham <alex@beyondessential.com.au>
2023-06-20 11:43:59 +1200

    WAITP-1207 naming

--- packages/tupaia-web/src/utils/removeUrlSearchParams.ts
+++ packages/tupaia-web/src/utils/removeUrlSearchParams.ts
@@ -3,10 +3,10 @@
  *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 
-export const removeUrlSearchParams = (paramsToRemove?: string[]) => {
+export const removeUrlSearchParams = (searchParamsToRemove?: string[]) => {
   const urlSearchParams = new URLSearchParams(window.location.search); // need to access the params directly from the location object, because useSearchParams from react-router-dom doesn't work when called outside of a component
-  if (paramsToRemove) {
-    paramsToRemove.forEach(param => {
+  if (searchParamsToRemove) {
+    searchParamsToRemove.forEach(param => {
       urlSearchParams.delete(param);
     });
   }
