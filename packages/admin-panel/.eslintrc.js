392a327b17dbef76242251f8b22b82012cc850dc
Igor Nadj <igor@beyondessential.com.au>
2023-06-20 17:05:06 +1000

    Delete cypress from admin-panel, psss
    Not maintained, causing issues with dependencies. If we want to restore these test specs we should put them in the e2e package in the future.

--- packages/admin-panel/.eslintrc.js
+++ packages/admin-panel/.eslintrc.js
@@ -1,14 +1,4 @@
 /* eslint-env node */
 module.exports = {
   extends: '../../.eslintrc-js-frontend.json',
-  // ignore cypress, not used
-  ignorePatterns: [
-    'build/',
-    'cypress/',
-    'dist/',
-    'doc/',
-    'node_modules/',
-    'public/',
-    '.eslintrc.js'
-  ]
 };
