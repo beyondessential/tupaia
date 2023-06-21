ac72478fcf527a49cc2ecb7eb0bc3dae2e7622fa
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-06-22 11:50:06 +1200

    WAITP-1256 Update dashboard route to assemble dashboard item list

--- packages/tupaia-web-server/src/routes/DashboardsRoute.ts
+++ packages/tupaia-web-server/src/routes/DashboardsRoute.ts
@@ -20,23 +20,66 @@ export class DashboardsRoute extends Route<DashboardsRequest> {
         columns: JSON.stringify(['entity.code', 'entity_hierarchy.name']),
       })
     )[0];
-    const baseEntity = await ctx.services.entity.getEntity(
+
+    // We're including the root entity in this request, so we don't need to double up fetching it
+    const entities = await ctx.services.entity.getAncestorsOfEntity(
       project['entity_hierarchy.name'],
       entityCode,
+      {},
+      true
     );
+    const rootEntity = entities.find((e: any) => e.code === entityCode);
 
     const dashboards = await ctx.services.central.fetchResources('dashboards', {
-      filter: { root_entity_code: baseEntity.code },
+      filter: { root_entity_code: entities.map((e: any) => e.code) },
     });
 
-    const response = await Promise.all(
-      dashboards.map(async (dash: any) => ({
-        ...dash,
-        items: await ctx.services.central.fetchResources(
-          `dashboards/${dash.id}/dashboardRelations`,
-        ),
-      })),
+    // Fetch all dashboard relations
+    const dashboardRelations = await ctx.services.central.fetchResources(
+      'dashboardRelations',
+      {
+        filter: {
+          // Attached to the given dashboards
+          dashboard_id: {
+            comparator: 'IN',
+            comparisonValue: dashboards.map((d: any) => d.id),
+          },
+          // For the root entity type
+          entity_types: {
+            comparator: '@>',
+            comparisonValue: [rootEntity.type],
+          },
+          // Within the selected project
+          project_codes: {
+            comparator: '@>',
+            comparisonValue: [projectCode],
+          }
+        }
+      }
     );
+
+    const dashboardItems = await ctx.services.central.fetchResources(
+      'dashboardItems',
+      {
+        filter: {
+          id: {
+            comparator: 'IN',
+            comparisonValue: dashboardRelations.map((dr: any) => dr.child_id),
+          }
+        }
+      }
+    );
+
+    const dashboardsWithItems = dashboards.map((dashboard: any) => {
+      const childRelations = dashboardRelations.filter((relation: any) => relation.dashboard_id === dashboard.id);
+      const childItemIds = childRelations.map((relation: any) => relation.child_id);
+      return {
+        ...dashboard,
+        items: dashboardItems.filter((item: any) => childItemIds.includes(item.id)),
+      };
+    });
+
+    const response = dashboardsWithItems.filter((dashboard: any) => dashboard.items.length > 0);
 
     return camelcaseKeys(response, { deep: true });
   }
