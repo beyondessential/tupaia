ae802302fbcfc4fd1adf292ef4b16128fad2a303
acdunham <129009580+acdunham@users.noreply.github.com>
2023-06-26 13:49:58 +1200

    Waitp 1244 pie chart (#4674)
    * WIP
    
    * WAITP-1244 arrange files
    
    * WAITP-1244 mobile button
    
    * Implement hot reloading
    
    * Fix storybook and convert Tabs component
    
    * Remove unnecessary scripts
    
    * WAITP-1244 Get pie chart working in mobile
    
    * WAITP-1244 add comments
    
    * Update types
    
    * WAITP-1244 Update styling
    
    * Update chart folder
    
    * Update useReport.ts
    
    * Update Chart.tsx
    
    * WAITP-1244 tweaks

---
+++ packages/tupaia-web/src/features/DashboardItem/DashboardItem.tsx
@@ -0,0 +1,90 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router';
+import { DashboardCode, DashboardItemType } from '../../types';
+import { useReport } from '../../api/queries';
+import { EnlargedDashboardItem } from './EnlargedDashboardItem';
+import { DashboardItemContent } from './DashboardItemContent';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: stretch;
+  place-content: stretch center;
+  margin-bottom: 0.5rem;
+  width: 100%;
+  max-width: 100%;
+  position: relative;
+  padding: 1rem 1rem;
+  background-color: ${({ theme }) => theme.palette.background.default};
+`;
+
+const Container = styled.div`
+  flex-flow: column nowrap;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: stretch;
+`;
+
+interface DashboardItemProps {
+  dashboardItem: DashboardItemType;
+  dashboardCode: DashboardCode;
+}
+
+/**
+ * This is the dashboard item, and renders the item in the dashboard itself, as well as a modal if the item is expandable
+ */
+export const DashboardItem = ({ dashboardItem, dashboardCode }: DashboardItemProps) => {
+  const { projectCode, entityCode } = useParams();
+  const { legacy, code, reportCode, viewType, type } = dashboardItem;
+  // query for the report data
+  const { data: reportData, isLoading, error, isError, refetch } = useReport(
+    projectCode,
+    entityCode,
+    dashboardCode,
+    reportCode,
+    code,
+    legacy,
+  );
+
+  const viewContent = {
+    ...dashboardItem,
+    ...reportData,
+  };
+
+  const { periodGranularity } = viewContent;
+
+  const isExpandable =
+    periodGranularity || type === 'chart' || type === 'matrix' || viewType === 'dataDownload';
+
+  const Content = ({ isEnlarged = false }: { isEnlarged?: boolean }) => (
+    <DashboardItemContent
+      viewContent={viewContent}
+      isEnlarged={isEnlarged}
+      isLoading={isLoading}
+      error={isError ? error : null}
+      onRetryFetch={refetch}
+      isExpandable={isExpandable && !isEnlarged}
+    />
+  );
+
+  return (
+    <Wrapper>
+      {/** render the item in the dashboard */}
+      <Container>
+        <Content />
+      </Container>
+      {/** render modal for enlarged item, if is expandable */}
+      {isExpandable && (
+        <EnlargedDashboardItem reportCode={reportCode}>
+          <Content isEnlarged />
+        </EnlargedDashboardItem>
+      )}
+    </Wrapper>
+  );
+};
---
+++ packages/tupaia-web/src/features/DashboardItem/EnlargedDashboardItem.tsx
@@ -0,0 +1,50 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import { useSearchParams } from 'react-router-dom';
+import { Modal } from '../../components';
+import { DashboardItemType } from '../../types';
+
+const Wrapper = styled.div`
+  width: 48rem;
+  max-width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  .recharts-responsive-container {
+    min-height: 22.5rem;
+  }
+`;
+
+interface EnlargedDashboardItemProps {
+  children: ReactNode;
+  reportCode: DashboardItemType['reportCode'];
+}
+/**
+ * EnlargedDashboardItem is the dashboard item modal. It is visible when the report code in the url is equal to the report code of the item.
+ */
+export const EnlargedDashboardItem = ({ children, reportCode }: EnlargedDashboardItemProps) => {
+  const [urlSearchParams, setUrlSearchParams] = useSearchParams();
+  // On close, remove the report search param from the url
+  const handleCloseModal = () => {
+    urlSearchParams.delete('report');
+    setUrlSearchParams(urlSearchParams.toString());
+  };
+  const isOpen = urlSearchParams.get('report') === reportCode;
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleCloseModal}>
+      <Wrapper>
+        <Container>{children}</Container>
+      </Wrapper>
+    </Modal>
+  );
+};
