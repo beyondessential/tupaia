67371710ce4ea4348bd396f107f7a00db72a6104
Tom Caiger <caigertom@gmail.com>
2023-06-27 19:21:28 +1200

    merging

---
+++ packages/tupaia-web/src/layout/MainLayout.tsx
@@ -0,0 +1,42 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import { Outlet } from 'react-router';
+import { EnvBanner } from '@tupaia/ui-components';
+import { TopBar } from './TopBar';
+import { MOBILE_BREAKPOINT } from '../constants';
+
+/**
+ * This is the layout for the entire app, which contains the top bar and the main content. This is used to wrap the entire app content
+ */
+const Container = styled.div`
+  position: fixed;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  width: 100%;
+  display: flex;
+  align-items: stretch;
+  align-content: stretch;
+  overflow-y: auto; // allows scroll at mobile size
+  height: 100%;
+  svg.recharts-surface {
+    overflow: visible;
+  }
+  @media screen and (min-width: ${MOBILE_BREAKPOINT}) {
+    overflow-y: hidden;
+  }
+`;
+
+export const MainLayout = () => {
+  return (
+    <Container>
+      <EnvBanner />
+      <TopBar />
+      <Outlet />
+    </Container>
+  );
+};
