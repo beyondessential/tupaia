891d15e90dd77d6dd13a5b5061a773eb8daf97f7
Tom Caiger <caigertom@gmail.com>
2023-06-29 16:29:23 +1200

    Update useEntityAncestors.ts

--- packages/tupaia-web/src/api/queries/useEntityAncestors.ts
+++ packages/tupaia-web/src/api/queries/useEntityAncestors.ts
@@ -7,23 +7,6 @@ import { useQuery } from 'react-query';
 import { EntityCode, ProjectCode } from '../../types';
 import { get } from '../api';
 
-const response = [
-  {
-    code: 'explore',
-    name: 'Explore',
-    parentEntityCode: null,
-  },
-  {
-    code: 'TO',
-    name: 'Tonga',
-    parentEntityCode: 'explore',
-  },
-  {
-    code: 'TO_Tongatapu',
-    name: 'Tongatapu',
-    parentEntityCode: 'TO',
-  },
-];
 export const useEntityAncestors = (projectCode?: ProjectCode, entityCode?: EntityCode) => {
   return useQuery(
     ['entityAncestors', projectCode, entityCode],
