b439c7097ec45ca5cdca98a43380e53293d80914
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-07-03 14:41:37 +1200

    Fixup moment.tz import

--- packages/utils/src/__tests__/period/periodGranularities.test.js
+++ packages/utils/src/__tests__/period/periodGranularities.test.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-import moment from 'moment';
+import moment from 'moment-timezone';
 import sinon from 'sinon';
 import { roundStartEndDates, getDefaultDates, getLimits } from '../../period/periodGranularities';
 
