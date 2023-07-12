45d6f102ac240635bb6e7310ba5abf4310041f8a
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-07-12 14:37:38 +1200

    WAITP-1278 Add tupaia-web-server report request to types

---
+++ packages/types/src/types/requests/tupaia-web-server/defaults.ts
@@ -0,0 +1,7 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+export type EmptyRequestBody = Record<string, never>;
+export type ProjectRequestParam = { projectCode: string };
