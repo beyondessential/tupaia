23da9fdc30fe4f867d2530db7e46a099031f8463
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-07-13 11:55:40 +1200

    WAITP-1278 Update project request type

--- packages/types/src/types/requests/tupaia-web-server/ProjectRequest.ts
+++ packages/types/src/types/requests/tupaia-web-server/ProjectRequest.ts
@@ -3,10 +3,13 @@
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 
+import { Project } from '../../models';
+import { KeysToCamelCase } from '../../../utils';
+
 export interface Params {
   projectCode: string;
 }
 
-export type ResBody = Record<string, never>;
+export type ResBody = KeysToCamelCase<Project>;
 export type ReqBody = Record<string, never>;
 export type ReqQuery = Record<string, never>;
