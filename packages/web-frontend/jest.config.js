74f45c2a1b4ed8b38cdad91ad53884af214db241
Tom Caiger <caigertom@gmail.com>
2023-07-12 16:04:10 +1200

    use jest config to set test timezone (#4726)

--- packages/web-frontend/jest.config.js
+++ packages/web-frontend/jest.config.js
@@ -3,6 +3,7 @@ const baseConfig = require('../../jest.config-js.json');
 module.exports = {
   ...baseConfig,
   rootDir: '.',
+  globalSetup: '<rootDir>/config/jest/jest-setup.js',
   setupFiles: ['<rootDir>/config/polyfills.js'],
   testMatch: ['<rootDir>/src/**/__tests__/**/*.js?(x)', '<rootDir>/src/**/?(*.)(spec|test).js?(x)'],
   testURL: 'http://localhost',
