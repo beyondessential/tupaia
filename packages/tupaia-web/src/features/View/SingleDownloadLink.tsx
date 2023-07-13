291bd9659484779fd2354e9e41928d2f1719aee7
acdunham <129009580+acdunham@users.noreply.github.com>
2023-07-13 16:27:44 +1200

    Update packages/tupaia-web/src/features/View/SingleDownloadLink.tsx
    Co-authored-by: Tom Caiger <caigertom@gmail.com>

--- packages/tupaia-web/src/features/View/SingleDownloadLink.tsx
+++ packages/tupaia-web/src/features/View/SingleDownloadLink.tsx
@@ -26,7 +26,7 @@ interface SingleDownloadLinkProps {
 }
 
 const transformDownloadLink = (resourceUrl: string) => {
-  const baseUrl = process.env.REACT_APP_CONFIG_SERVER_BASE_URL || 'http://localhost:8080/api/v1/';
+  const baseUrl = import.meta.env.REACT_APP_TUPAIA_WEB_API_URL || 'http://localhost:8080/api/v1/';
   if (resourceUrl.includes('http')) return resourceUrl;
   return `${baseUrl}${resourceUrl}`;
 };
