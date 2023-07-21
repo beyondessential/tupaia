73fde7e851ec9ea4450ad067916fd25ae4097b9c
Igor <igor@beyondessential.com.au>
2023-07-21 10:19:14 +1000

    RN-903 QR code Admin Panel (#4723)
    * Replace react-qr-code with qrcode
    
    qrcode gives us a way of rending into a canvas, which we need for image download
    
    * RN-903 Add QR Code modal
    
    * RN-903 Replace old qr modal with new
    
    * Add downloadjs to admin-panel
    
    * QR code use downloadjs instead of custom implementation

---
+++ packages/admin-panel/src/qrCode/reducer.js
@@ -0,0 +1,26 @@
+/**
+ * Tupaia MediTrak
+ * Copyright (c) 2018 Beyond Essential Systems Pty Ltd
+ */
+
+import { createReducer } from '../utilities';
+import { QR_CODE_MODAL_DISMISS, QR_CODE_MODAL_OPEN } from './constants';
+
+const defaultState = {
+  isOpen: false,
+  recordId: null,
+  recordData: null,
+};
+
+const stateChanges = {
+  [QR_CODE_MODAL_OPEN]: payload => ({
+    isOpen: true,
+    ...payload,
+  }),
+  [QR_CODE_MODAL_DISMISS]: payload => ({
+    isOpen: false,
+    ...payload,
+  }),
+};
+
+export const reducer = createReducer(defaultState, stateChanges);
