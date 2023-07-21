aad03b697a18e22c7562a2b582472f2eacb85be7
Chao Xin Nhin <chaon@missionreadyhq.com>
2023-07-21 13:04:30 +1200

    added target to baseLink route

--- packages/tupaia-web/src/layout/ProjectCardList/ProjectCard.tsx
+++ packages/tupaia-web/src/layout/ProjectCardList/ProjectCard.tsx
@@ -128,7 +128,7 @@ export const ProjectPendingLink = () => (
   </OutlineLink>
 );
 export const ProjectAllowedLink = ({ url }: LinkProps) => (
-  <BaseLink to={url}>View project</BaseLink>
+  <BaseLink to={url} target= '_newTab'>View project</BaseLink>
 );
 
 interface ProjectCardProps extends Partial<SingleProject> {
