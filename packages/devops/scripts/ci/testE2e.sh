b8766cb2c5a7fa9990c82f0004837ba9daf6c2c4
Igor Nadj <igor@beyondessential.com.au>
2023-08-06 15:17:23 +0200

    Delete old files

--- packages/devops/scripts/ci/testE2e.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Echo date so we can see timezone, see RN-803
-date
-
-yarn workspace "@tupaia/e2e" test-e2e --ciBuildId $CI_BUILD_ID
--- packages/devops/scripts/ci/testFrontend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-DIR=$(dirname "$0")
-PACKAGE=$1
-yarn workspace @tupaia/${PACKAGE} test
