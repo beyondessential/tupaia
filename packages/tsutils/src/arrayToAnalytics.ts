5f943c50cfc86aa65fabc05a92054fbca4241b9c
Igor Nadj <igor@beyondessential.com.au>
2023-08-08 19:21:03 +0200

    Refactor: move arrayToAnalytics() to tsutls
    This is the last dependency of aggregator on data-broker

---
+++ packages/tsutils/src/arrayToAnalytics.ts
@@ -0,0 +1,21 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ */
+
+interface Analytic {
+  dataElement: string;
+  organisationUnit: string;
+  period: string; // should be in format YYYYMMDD e.g. "20210103"
+  value: string | number;
+}
+
+type ArrayAnalytic = [string, string, string, string | number];
+
+export const arrayToAnalytics = (arrayAnalytics: ArrayAnalytic[]): Analytic[] =>
+  arrayAnalytics.map(([dataElement, organisationUnit, period, value]) => ({
+    dataElement,
+    organisationUnit,
+    period,
+    value,
+  }));
