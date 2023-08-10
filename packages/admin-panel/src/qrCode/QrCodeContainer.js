5226323b32e78e1088fb4479e6f3324b1faa0266
Chris Pollard <therealchrispollard@gmail.com>
2023-08-10 14:44:04 +1000

    Update qr code in admin panel

---
+++ packages/admin-panel/src/qrCode/QrCodeContainer.js
@@ -0,0 +1,45 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2021 Beyond Essential Systems Pty Ltd
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import * as download from 'downloadjs';
+import { QrCodeImage, Button, getCanvasUrlForDownload } from '@tupaia/ui-components';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 30px;
+`;
+
+export const QrCodeContainer = ({ qrCodeContents, humanReadableId }) => {
+  const handleDownload = async () => {
+    const dataUri = await getCanvasUrlForDownload(humanReadableId, qrCodeContents);
+
+    if (!dataUri) {
+      return;
+    }
+    download(dataUri, `${humanReadableId}.jpeg`, 'image/jpeg');
+  };
+
+  return (
+    <Container>
+      <QrCodeImage qrCodeContents={qrCodeContents} humanReadableId={humanReadableId} />
+      <ButtonContainer>
+        <Button type="button" onClick={handleDownload}>
+          Download
+        </Button>
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+QrCodeContainer.propTypes = {
+  qrCodeContents: PropTypes.string.isRequired,
+  humanReadableId: PropTypes.string.isRequired,
+};
