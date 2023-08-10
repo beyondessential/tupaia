36f41bec0c8b5145c7f31a28566a148f7272d964
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-08-11 09:09:13 +1200

    WAITP-1364 WIP First pass map overlay config schema

---
+++ packages/types/src/types/models-extra/mapOverlay.ts
@@ -0,0 +1,73 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import { DateOffsetSpec, VizPeriodGranularity } from './dashboard-item/common';
+
+export type MapOverlayConfig = {
+  customColors?: string; // Comma separated string
+  customLabel?: string;
+  datePickerLimits?: {
+    start?: DateOffsetSpec;
+    end?: DateOffsetSpec;
+  };
+  defaultTimePeriod?: DateOffsetSpec;
+  disableRenameLegend?: boolean;
+  displayLevel?: string; // "District"/"SubDistrict"
+  displayOnLevel?: string; // "District"/"SubDistrict"
+  displayType?: string; // ui-map-components/constants MEASURE_TYPES
+  displayedValueKey?: string; // "originalValue"/"schoolTypeName"/"name"/"facilityTypeName"
+  hideByDefault?: Record<string, boolean>;
+  hideFromLegend?: boolean;
+  hideFromMenu?: boolean;
+  hideFromPopup?: boolean;
+  icon?: string; // "circle"
+  info?: {
+    reference: {
+      link?: string;
+      name?: string;
+      text?: string;
+    };
+  };
+  isTimePeriodEditable?: boolean;
+  // linkedMeasures?: null; // There's one record in the db that has this field and it's null
+  measureConfig?: Record<
+    string,
+    {
+      type: string; // ui-map-components/constants MEASURE_TYPES
+      measureLevel?: string; // "District"/"SubDistrict"
+      values?: InlineValue[];
+      sortOrder?: number;
+      hideFromLegend?: boolean;
+    }
+  >;
+  measureLevel?: string | string[]; // "City"/"District"/"SubDistrict" ... more
+  name?: string;
+  noDataColour?: string; // Hex code string e.g. #99237f
+  periodGranularity?: VizPeriodGranularity;
+  popupHeaderFormat?: string; // {code}:{name}
+  scaleBounds?: {
+    left?: {
+      min: number | 'auto';
+      max: number | 'auto';
+    };
+    right?: {
+      min: number | 'auto';
+      max: number | 'auto';
+    };
+  };
+  scaleColorScheme?: string; // ui-map-components/constants SCHEME "default"/"default-reverse"
+  scaleType?: string; // ui-map-components/constants SCALE_TYPES
+  valueType?: string; // formatDataValueByType VALUE_TYPES
+  values?: InlineValue[];
+};
+
+type InlineValue = {
+  color?: string;
+  hideFromLegend?: boolean;
+  hideFromPopup?: boolean;
+  icon: string;
+  name: string;
+  value: string | number;
+};
