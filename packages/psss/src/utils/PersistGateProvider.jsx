2fdfc7d180b6ceea2ae4522ceb101b7dcb09f5fd
acdunham <alex@beyondessential.com.au>
2023-08-10 12:06:44 +1200

    Fix types and arrange scripts

--- packages/psss/src/utils/PersistGateProvider.jsx
+++ packages/psss/src/utils/PersistGateProvider.jsx
@@ -29,7 +29,7 @@ function initPersistor(store) {
 
 export const PersistGateProvider = ({ children, store }) => {
   // Do not persist state for tests
-  if (import.meta.env.mode === 'test') {
+  if (process.env.NODE_ENV === 'test') {
     return children;
   }
   return <PersistGate persistor={initPersistor(store)}>{children}</PersistGate>;
