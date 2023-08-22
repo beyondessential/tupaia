c6258efa18bc868d7966395f5854b767162005b1
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-08-22 16:32:15 +1200

    WAITP-1367 Add datatrak-server package

---
+++ packages/datatrak-server/README.md
@@ -0,0 +1,3 @@
+# @tupaia/datatrak-server
+
+Back end server for the DataTrak web app.
---
+++ packages/datatrak-server/src/routes/UserRoute.ts
@@ -0,0 +1,35 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import { Request } from 'express';
+import { Route } from '@tupaia/server-boilerplate';
+import { DataTrakUserRequest } from '@tupaia/types';
+
+export type UserRequest = Request<
+  DataTrakUserRequest.Params,
+  DataTrakUserRequest.ResBody,
+  DataTrakUserRequest.ReqBody,
+  DataTrakUserRequest.ReqQuery
+>;
+
+export class UserRoute extends Route<UserRequest> {
+  public async buildResponse() {
+    const { ctx, session } = this.req;
+
+    // Avoid sending a 'me' request as the api user
+    if (!session) {
+      // Triggers frontend login
+      return {};
+    }
+
+    const {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+    } = await ctx.services.central.getUser();
+
+    return { userName: `${firstName} ${lastName}`, email };
+  }
+}
