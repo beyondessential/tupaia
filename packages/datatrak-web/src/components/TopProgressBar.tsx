537cdf9aab4c735332d5170ae9a34c0067e74375
Chao Xin Nhin <chaon@missionreadyhq.com>
2023-08-29 17:33:36 +1200

    ready for retest

--- packages/datatrak-web/src/components/TopProgressBar.tsx
+++ packages/datatrak-web/src/components/TopProgressBar.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Button } from '@material-ui/core';
-import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
 
 const Wrapper = styled.div`
   background: rgba(0, 65, 103, 0.3);
@@ -19,37 +17,6 @@ const ProgressBar = styled.div<{
   height: 0.75rem;
   width: ${({ $progress }) => `${$progress}`}%;
 `;
-
-/*
-Styling of buttons as the project progresses.
-
-const ProgressButton = styled(Button).attrs({
-  color: 'primary',
-})`
-  background: ${({ theme }) => theme.palette.primary.main};
-  color: white;
-  border-radius: 3px;
-  text-transform: none;
-  font-size: 0.9rem;
-  height: 2.8rem;
-  padding: 0.8rem 1.6rem;
-`;
-
-const CancelButton = styled(Button).attrs({
-  variant: 'outlined',
-  color: 'primary',
-})`
-  border-radius: 3px;
-  text-transform: none;
-  font-size: 0.9rem;
-  height: 2.8rem;
-  padding: 0.8rem 1.6rem;
-`;
-
-const BackButton = styled(Button)`
-  text-align: center;
-`;
-*/
 
 interface ProgressPercentage {
   currentSurveyQuestion: number;
