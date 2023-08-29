b3212cb4131759f3dda2bddadda5a500f4599b13
Tom Caiger <caigertom@gmail.com>
2023-08-29 12:35:28 +1200

    no-issue-fix-bad-merge

--- packages/tupaia-web/src/features/DashboardItem/index.ts
+++ packages/tupaia-web/src/features/DashboardItem/index.ts
@@ -4,4 +4,5 @@
  */
 export { DashboardItem } from './DashboardItem';
 export { DashboardItemContent } from './DashboardItemContent';
+export { DashboardItemContext } from './DashboardItemContext';
 export { DashboardInfoHover } from './DashboardInfoHover';
