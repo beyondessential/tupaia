cbccddb0e1d1c7be1c70a89d69c57ca50cbeaeda
acdunham <alex@beyondessential.com.au>
2023-08-29 16:25:25 +1200

    Add types

--- packages/datatrak-web/src/features/Questions/InstructionQuestion.tsx
+++ packages/datatrak-web/src/features/Questions/InstructionQuestion.tsx
@@ -5,6 +5,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
+import { SurveyQuestionInputProps } from '../../types';
 
 const InstructionHeading = styled(Typography).attrs({
   variant: 'h3',
@@ -12,7 +13,7 @@ const InstructionHeading = styled(Typography).attrs({
   font-size: 1.125rem;
   font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
 `;
-export const InstructionQuestion = ({ text, detailLabel }) => {
+export const InstructionQuestion = ({ text, detailLabel }: SurveyQuestionInputProps) => {
   if (!text && !detailLabel) {
     return null;
   }
