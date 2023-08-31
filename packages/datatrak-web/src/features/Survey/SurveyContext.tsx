1f27ec53e113d0b4623bb306b1f394535f30573e
acdunham <alex@beyondessential.com.au>
2023-08-31 14:59:27 +1200

    Handle more than 26 questions on a screen

--- packages/datatrak-web/src/features/Survey/SurveyContext.tsx
+++ packages/datatrak-web/src/features/Survey/SurveyContext.tsx
@@ -10,7 +10,14 @@ import { SurveyParams, SurveyScreenComponent } from '../../types';
 import { useSurveyScreenComponents } from '../../api/queries';
 
 const convertNumberToLetter = (number: number) => {
-  return String.fromCharCode(65 + number);
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  if (number > 25) {
+    // occasionally there are more than 26 questions on a screen, so we then start at aa, ab....
+    const firstLetter = alphabet[Math.floor(number / 26) - 1];
+    const secondLetter = alphabet[number % 26];
+    return `${firstLetter}${secondLetter}`;
+  }
+  return alphabet[number];
 };
 
 type SurveyFormContextType = {
@@ -48,6 +55,7 @@ export const SurveyContext = ({ children }) => {
     const questionNumber = nonInstructionQuestions.findIndex(
       nonInstructionQuestion => question.questionId === nonInstructionQuestion.questionId,
     );
+    if (questionNumber === -1) return question;
     return {
       ...question,
       questionNumber: `${screenNumber}${convertNumberToLetter(questionNumber)}.`,
