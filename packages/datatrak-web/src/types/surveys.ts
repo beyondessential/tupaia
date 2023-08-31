6c890ce9b4032ec52541dc4bed0727d361e0e30b
acdunham <alex@beyondessential.com.au>
2023-08-31 13:10:08 +1200

    Reorder imports

--- packages/datatrak-web/src/types/surveys.ts
+++ packages/datatrak-web/src/types/surveys.ts
@@ -3,8 +3,9 @@
  *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 import { Ref } from 'react';
-import { DatatrakWebSurveysRequest, DatatrakWebSurveyScreenComponentsRequest } from '@tupaia/types';
 import { ControllerRenderProps } from 'react-hook-form';
+import { DatatrakWebSurveysRequest, DatatrakWebSurveyScreenComponentsRequest } from '@tupaia/types';
+
 export type Survey = DatatrakWebSurveysRequest.ResBody[number];
 
 export type SurveyScreenComponent = DatatrakWebSurveyScreenComponentsRequest.ResBody[number] & {
