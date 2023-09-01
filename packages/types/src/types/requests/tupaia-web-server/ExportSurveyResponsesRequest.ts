bb38c0d748a3ac92ea42216e6ba362000d1f75ee
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-09-01 15:11:16 +1200

    Update types for export request

--- packages/types/src/types/requests/tupaia-web-server/ExportSurveyResponsesRequest.ts
+++ packages/types/src/types/requests/tupaia-web-server/ExportSurveyResponsesRequest.ts
@@ -4,6 +4,25 @@
  */
 
 export type Params = Record<string, never>;
-export type ResBody = any;
-export type ReqBody = any;
-export type ReqQuery = any;
+export type ResBody = {
+  contents: Buffer;
+  filePath: string;
+  type: string;
+};
+export type ReqBody = Record<string, never>;
+export type ReqQuery = {
+  organisationUnitCode?: string;
+  surveyCodes?: string[];
+  latest?: boolean;
+  /**
+   * @format iso-date-time
+   */
+  startDate?: string;
+  /**
+   * @format iso-date-time
+   */
+  endDate?: string;
+  timeZone?: string;
+  itemCode?: string;
+  easyReadingMode?: boolean;
+};
