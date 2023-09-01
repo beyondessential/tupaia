3af0f57ac9df15ceba6b9c416629ad4b46b06ad3
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2023-09-01 12:06:55 +1000

    MDEV-174: Add fillRows transform to the viz-builder (#4795)

---
+++ packages/report-server/src/reportBuilder/transform/functions/utils/buildRowKey.ts
@@ -0,0 +1,15 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import { Row } from '../../../types';
+
+const divider = '___';
+
+export const buildRowKey = (row: Row, columnsOfInterest?: string[]) => {
+  if (columnsOfInterest) {
+    return columnsOfInterest.map(columnName => row[columnName]).join(divider);
+  }
+  return Object.values(row).sort().join(divider);
+};
