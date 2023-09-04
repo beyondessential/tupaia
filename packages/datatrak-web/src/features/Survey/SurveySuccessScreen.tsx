ede2565948c6f1b4c8b2540a20439be9b429d1bf
Chao Xin Nhin <chaon@missionreadyhq.com>
2023-09-05 11:56:43 +1200

    almost done with routes

--- packages/datatrak-web/src/features/Survey/SurveySuccessScreen.tsx
+++ packages/datatrak-web/src/features/Survey/SurveySuccessScreen.tsx
@@ -5,31 +5,84 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { Paper as MuiPaper } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import { ButtonLink } from '../../components';
+import { generatePath, useNavigate, useParams } from 'react-router-dom';
+import { useSurveyForm } from './SurveyContext.tsx';
+import { ROUTES } from '../../constants';
 
-const Container = styled.div`
-  flex: 1;
+const Wrapper = styled.div`
   display: flex;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 7.3rem;
 `;
 
-const Paper = styled(MuiPaper).attrs({
-  variant: 'outlined',
-  elevation: 0,
+const StyledImg = styled.img`
+  width: 21.4rem;
+  height: 21.4rem;
+  margin-bottom: 2.75rem;
+`;
+
+const SurveySubmit = styled(Typography).attrs({
+  color: 'textPrimary',
 })`
-  flex: 1;
-  overflow: auto;
-  padding: 3rem;
-  max-width: 70rem;
-  margin: 0 auto;
+  font-size: 1.9rem;
+  font-weight: 600;
+  margin-bottom: 1.19rem;
+`;
+
+const SubmissionText = styled(Typography).attrs({
+  color: 'textPrimary',
+})`
+  width: 34.6rem;
+  text-align: center;
+  margin-bottom: 1.875rem;
+`;
+
+const RepeatSurvey = styled(Button).attrs({
+  variant: 'outlined',
+  color: 'primary',
+})`
+  padding: 0.875rem 11rem;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 1.25rem;
+`;
+
+const CloseBtn = styled(ButtonLink).attrs({
+  variant: 'contained',
+  color: 'primary',
+})`
+  padding: 0.875rem 13rem;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 `;
 export const SurveySuccessScreen = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const { numberOfScreens } = useSurveyForm();
+
+  const onStepPrevious = () => {
+    const path = generatePath(ROUTES.SURVEY_SCREEN, {
+      ...params,
+      screenNumber: String(numberOfScreens),
+    });
+    navigate(path);
+  };
   return (
-    <Container>
-      <Paper>
-        <h2>Survey submitted</h2>
-      </Paper>
-    </Container>
+    <Wrapper>
+      <StyledImg src="/submit-success.svg" alt="submit-success" />
+      <SurveySubmit variant="h1">Survey submitted!</SurveySubmit>
+      <SubmissionText>
+        To repeat the same survey again click the button below otherwise ‘Close’ to return back to
+        your dashboard
+      </SubmissionText>
+      <RepeatSurvey onClick={onStepPrevious}>Repeat Survey</RepeatSurvey>
+      <CloseBtn to="/">Close</CloseBtn>
+    </Wrapper>
   );
 };
