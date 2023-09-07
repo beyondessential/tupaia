2cd911a05ec92cbf184817affc305f8ed32d48d4
Chao Xin Nhin <chaon@missionreadyhq.com>
2023-09-07 12:56:22 +1200

    survey success commit

--- packages/datatrak-web/src/features/Survey/SurveySuccessScreen.tsx
+++ packages/datatrak-web/src/features/Survey/SurveySuccessScreen.tsx
@@ -3,22 +3,51 @@
  *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Typography, Button } from '@material-ui/core';
+import { Description } from '@material-ui/icons';
 import { useForm } from 'react-hook-form';
 import { ButtonLink } from '../../components';
 import { generatePath, useNavigate, useParams } from 'react-router-dom';
 import { useSurveyForm } from './SurveyContext.tsx';
 import { ROUTES } from '../../constants';
-import { FormatShapesTwoTone } from '@material-ui/icons';
+import { useSurvey } from '../../api/queries';
+
+const Toolbar = styled.div`
+  height: 4.7rem;
+  background: ${({ theme }) => theme.palette.background.paper};
+  border-top: 1px solid ${({ theme }) => theme.palette.divider};
+  margin-left: -0.9375rem;
+  margin-right: -0.9375rem;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+const SurveyTitleWrapper = styled.div`
+  padding: 1.3rem;
+`;
+
+const SurveyIcon = styled(Description).attrs({
+  color: 'primary',
+})`
+  margin-right: 0.5rem;
+`;
+
+const SurveyTitle = styled(Typography).attrs({
+  variant: 'h1',
+})`
+  display: flex;
+  align-items: center;
+`;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 7.3rem;
+  height: 100%;
 `;
 
 const StyledImg = styled.img`
@@ -64,37 +93,44 @@ const CloseBtn = styled(ButtonLink).attrs({
   gap: 10px;
 `;
 export const SurveySuccessScreen = () => {
+  const { surveyCode } = useParams();
   const params = useParams();
   const navigate = useNavigate();
   const { activeScreen, formData, setFormData } = useSurveyForm();
-  const { reset} = useForm({defaultValues: formData})
-  
-  const repeatSurvey = (data) => {
+  const { reset } = useForm({ defaultValues: formData });
+  const { data: survey } = useSurvey(surveyCode);
+
+  const repeatSurvey = data => {
     setFormData(reset({ ...formData, ...data }));
     const path = generatePath(ROUTES.SURVEY_SCREEN, {
       ...params,
-      screenNumber: String(activeScreen), 
+      screenNumber: String(activeScreen),
     });
-    navigate(path)
-};
-      // useEffect(() => {
-      //   if(formState.isSubmitSuccessful) {
-      //     reset({formData})
-      //   }
-      // }, [formState, reset]) 
+    navigate(path);
+  };
 
   return (
     <>
-    <Wrapper>
-      <StyledImg src="/submit-success.svg" alt="submit-success" />
-      <SurveySubmit variant="h1">Survey submitted!</SurveySubmit>
-      <SubmissionText>
-        To repeat the same survey again click the button below otherwise ‘Close’ to return back to
-        your dashboard
-      </SubmissionText>
-      <RepeatSurvey onClick={repeatSurvey}>Repeat Survey</RepeatSurvey>
-      <CloseBtn to="/">Close</CloseBtn>
-    </Wrapper>
+      <Toolbar>
+        <SurveyTitleWrapper>
+          {survey?.name && (
+            <SurveyTitle>
+              <SurveyIcon />
+              {survey?.name}
+            </SurveyTitle>
+          )}
+        </SurveyTitleWrapper>
+      </Toolbar>
+      <Wrapper>
+        <StyledImg src="/submit-success.svg" alt="submit-success" />
+        <SurveySubmit variant="h1">Survey submitted!</SurveySubmit>
+        <SubmissionText>
+          To repeat the same survey again click the button below otherwise ‘Close’ to return back to
+          your dashboard
+        </SubmissionText>
+        <RepeatSurvey onClick={repeatSurvey}>Repeat Survey</RepeatSurvey>
+        <CloseBtn to="/">Close</CloseBtn>
+      </Wrapper>
     </>
   );
 };
