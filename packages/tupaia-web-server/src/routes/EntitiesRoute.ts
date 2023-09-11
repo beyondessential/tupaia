82607d9316e97003ab0cfd95a526a5cbb6e3e903
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-09-11 16:51:23 +1200

    WAITP-1211 Filter child_codes first layer by second layer

--- packages/tupaia-web-server/src/routes/EntitiesRoute.ts
+++ packages/tupaia-web-server/src/routes/EntitiesRoute.ts
@@ -54,28 +54,19 @@ export class EntitiesRoute extends Route<EntitiesRequest> {
       query.includeRootEntity || false,
     );
 
-    // The child_codes aren't filtered for frontendExcludedTypes
-    // So we fetch a layer of filtered descendents and 
-    const childEntities = await ctx.services.entity.getDescendantsOfEntities(
-      projectCode,
-      flatEntities.map((e: any) => e.code),
-      {
-        filter: {
-          generational_distance: {
-            comparator: '<=',
-            comparisonValue: 1,
-          },
-          ...generateFrontendExcludedFilter(config, typesExcludedFromWebFrontend),
-        },
-        fields: ['code'],
-      },
-    );
-    const formattedEntities = flatEntities.map((e: any) => {
-      const filteredChildren = e.child_codes?.filter((code: string) =>
-        childEntities.some((ce: any) => ce.code === code),
+    // The child_codes list won't have been filtered for frontendExcludedTypes
+    // Since we fetch two layers at a time, we can clean up child_codes in the
+    // first layer, by checking the child exists in the second
+    const formattedEntities = flatEntities.map((entity: any) => {
+      // Only the first layer
+      if (entity.parent_code !== rootEntityCode) {
+        return entity;
+      }
+      const filteredChildren = entity.child_codes?.filter((childCode: string) =>
+        flatEntities.some(({ code }: { code: string }) => code === childCode),
       );
       return {
-        ...e,
+        ...entity,
         child_codes: filteredChildren?.length > 0 ? filteredChildren : undefined,
       };
     });
