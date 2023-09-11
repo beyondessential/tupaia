dac1415b0e0d918f236c5d597af866657ea73778
Chris Pollard <therealchrispollard@gmail.com>
2023-09-11 10:09:25 +1000

    rn-751: uncomment tiny

--- packages/meditrak-app/app/database/types/utilities/updateListOfStrings.js
+++ packages/meditrak-app/app/database/types/utilities/updateListOfStrings.js
@@ -5,18 +5,11 @@
 import { complement } from 'set-manipulator';
 
 export const updateListOfStrings = (database, currentRealmStrings, newStrings, addString) => {
-  // const newRealmStrings = newStrings.map(newString => ({
-  //   // Any strings that look like JSON will have been parsed into an object on interpreting the
-  //   // HTTP result, so we need to return them to a string
-  //   string: typeof newString === 'object' ? JSON.stringify(newString) : newString,
-  // }));
-  const newRealmStrings = newStrings.map(newString => {
-    return {
-      // Any strings that look like JSON will have been parsed into an object on interpreting the
-      // HTTP result, so we need to return them to a string
-      string: typeof newString === 'object' ? JSON.stringify(newString) : newString,
-    };
-  });
+  const newRealmStrings = newStrings.map(newString => ({
+    // Any strings that look like JSON will have been parsed into an object on interpreting the
+    // HTTP result, so we need to return them to a string
+    string: typeof newString === 'object' ? JSON.stringify(newString) : newString,
+  }));
 
   const getRealmStringIdentifier = realmString => realmString.string;
   const toDelete = complement(currentRealmStrings, newRealmStrings, getRealmStringIdentifier);
