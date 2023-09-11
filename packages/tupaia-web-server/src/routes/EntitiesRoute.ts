86fea29b252c52989d58bd074120991e91f7659d
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-09-11 16:46:34 +1200

    WAITP-1211 First pass of filtering child_codes

--- packages/tupaia-web-server/src/routes/EntitiesRoute.ts
+++ packages/tupaia-web-server/src/routes/EntitiesRoute.ts
@@ -6,7 +6,7 @@
 import { Request } from 'express';
 import { Route } from '@tupaia/server-boilerplate';
 import camelcaseKeys from 'camelcase-keys';
-import { TupaiaWebEntitiesRequest } from '@tupaia/types';
+import { TupaiaWebEntitiesRequest, Entity } from '@tupaia/types';
 import { generateFrontendExcludedFilter } from '../utils';
 
 export type EntitiesRequest = Request<
@@ -23,7 +23,7 @@ const DEFAULT_FILTER = {
   },
 };
 
-const DEFAULT_FIELDS = ['parent_code', 'code', 'name', 'type'];
+const DEFAULT_FIELDS = ['parent_code', 'code', 'name', 'type', 'child_codes'];
 
 export class EntitiesRoute extends Route<EntitiesRequest> {
   public async buildResponse() {
@@ -54,6 +54,32 @@ export class EntitiesRoute extends Route<EntitiesRequest> {
       query.includeRootEntity || false,
     );
 
-    return camelcaseKeys(flatEntities, { deep: true });
+    // The child_codes aren't filtered for frontendExcludedTypes
+    // So we fetch a layer of filtered descendents and 
+    const childEntities = await ctx.services.entity.getDescendantsOfEntities(
+      projectCode,
+      flatEntities.map((e: any) => e.code),
+      {
+        filter: {
+          generational_distance: {
+            comparator: '<=',
+            comparisonValue: 1,
+          },
+          ...generateFrontendExcludedFilter(config, typesExcludedFromWebFrontend),
+        },
+        fields: ['code'],
+      },
+    );
+    const formattedEntities = flatEntities.map((e: any) => {
+      const filteredChildren = e.child_codes?.filter((code: string) =>
+        childEntities.some((ce: any) => ce.code === code),
+      );
+      return {
+        ...e,
+        child_codes: filteredChildren?.length > 0 ? filteredChildren : undefined,
+      };
+    });
+
+    return camelcaseKeys(formattedEntities, { deep: true });
   }
 }
