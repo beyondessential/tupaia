34ee5498650c5dceb8feaeeffb8a71c9b1ac73d7
Rohan Port <rohan@bes.au>
2023-09-11 12:08:51 +1000

    RN-751: Fixed attributes exporting for Entity Questions

--- packages/central-server/src/apiV2/export/exportSurveys/cellBuilders/questionConfigCellBuilders/EntityConfigCellBuilder.js
+++ packages/central-server/src/apiV2/export/exportSurveys/cellBuilders/questionConfigCellBuilders/EntityConfigCellBuilder.js
@@ -17,6 +17,10 @@ const KEY_TRANSLATION = {
 };
 
 export class EntityConfigCellBuilder extends KeyValueCellBuilder {
+  individualFieldProcessors = {
+    attributes: EntityConfigCellBuilder.processAttributesField,
+  };
+
   processKey(key) {
     return KEY_TRANSLATION[key] || key;
   }
@@ -25,6 +29,34 @@ export class EntityConfigCellBuilder extends KeyValueCellBuilder {
     return SPECIAL_VALUE_PROCESSORS[key]
       ? SPECIAL_VALUE_PROCESSORS[key](value)
       : this.fetchQuestionCode(value);
+  }
+
+  /**
+   * Currently we only actually support parentId as an attribute
+   * But this processor function processes all keys for future proofing
+   * Since it's not a one to one relationship we can't run it through the generic handlers
+   *
+   * Input:
+   * attibutes: {
+   *   parentId: {
+   *    questionId: <guid>
+   *   }
+   * }
+   * Converted to Excel config:
+   * attributes.parent: <questionCode>
+   */
+  static async processAttributesField(models, attributesObject = {}) {
+    const processedAttributes = await Promise.all(
+      Object.entries(attributesObject).map(async ([key, value]) => {
+        const translatedKey = KEY_TRANSLATION[key] || key;
+        const question = await models.question.findById(value?.questionId);
+        if (!question) {
+          throw new Error(`Could not find a question with id matching ${value?.questionId}`);
+        }
+        return `attributes.${translatedKey}: ${question.code}`;
+      }),
+    );
+    return processedAttributes.join('\r\n');
   }
 
   extractRelevantObject({ entity }) {
