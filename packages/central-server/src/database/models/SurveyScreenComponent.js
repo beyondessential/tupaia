a6d456324ec73efda9088f5ddea7a4d0d9128ddd
Chris Pollard <therealchrispollard@gmail.com>
2023-09-18 09:54:00 +1000

    rn-751:  fix import, fix reference

--- packages/central-server/src/database/models/SurveyScreenComponent.js
+++ packages/central-server/src/database/models/SurveyScreenComponent.js
@@ -5,14 +5,20 @@
 
 import { SurveyScreenComponentModel as CommonSurveyScreenComponentModel } from '@tupaia/database';
 import { compare } from 'compare-versions';
-import { CONFIG_BREAK_MEDITRAK_APP_VERSION } from '../constants';
+import { SURVEY_SCREEN_COMPONENT_ENTITY_CONFIG_SCHEMA_CHANGE_APP_VERSION } from '../constants';
 import { translateEntityConfig } from '../utilities';
 
 export class SurveyScreenComponentModel extends CommonSurveyScreenComponentModel {
   meditrakConfig = {
     minAppVersion: '0.0.1',
     translateRecordForSync: (record, userAppVersion) => {
-      if (compare(userAppVersion, CONFIG_BREAK_MEDITRAK_APP_VERSION, '<')) {
+      if (
+        compare(
+          userAppVersion,
+          SURVEY_SCREEN_COMPONENT_ENTITY_CONFIG_SCHEMA_CHANGE_APP_VERSION,
+          '<',
+        )
+      ) {
         return translateEntityConfig(record);
       }
       return record;
