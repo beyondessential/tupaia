2d98033338d233147dfc6ad5c9330ee513c23961
acdunham <alex@beyondessential.com.au>
2023-09-18 15:02:21 +1200

    Fix on select issue

--- packages/datatrak-web/src/features/ProjectSelectForm.tsx
+++ packages/datatrak-web/src/features/ProjectSelectForm.tsx
@@ -107,6 +107,7 @@ export const ProjectSelectForm = ({
         tooltip,
         button: !hasPendingAccess,
         appearsDisabled: hasAccess === false,
+        hasAccess
       };
     });
   };
