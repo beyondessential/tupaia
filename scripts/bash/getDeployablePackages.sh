31e80015f20f6022006809dde0d04e48ece81cd9
Rohan Port <rohan@bes.au>
2023-09-19 11:14:09 +1000

    RN-502: Switched to building the meditrak-sync-view using the meditrak-app-server

--- scripts/bash/getDeployablePackages.sh
+++ scripts/bash/getDeployablePackages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
-echo "admin-panel" "lesmis" "psss" "datatrak-web" "tupaia-web" "web-frontend" "central-server" "data-table-server" "datatrak-web-server" "entity-server" "lesmis-server" "meditrak-app-server" "psss-server" "report-server" "tupaia-web-server" "web-config-server" "admin-panel-server" # admin-panel-server last as it depends on report-server
+# meditrak-app-server before central-server so that the meditrak-sync-queue is listening before we run the migrations
+# admin-panel-server last as it depends on report-server
+echo "admin-panel" "lesmis" "psss" "datatrak-web" "tupaia-web" "web-frontend" "meditrak-app-server" "central-server" "data-table-server" "datatrak-web-server" "entity-server" "lesmis-server" "psss-server" "report-server" "tupaia-web-server" "web-config-server" "admin-panel-server"
 exit 0
