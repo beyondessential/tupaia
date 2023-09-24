71bc26edfd04279bb17eec0f7ebe1aa484da198d
Tom Caiger <caigertom@gmail.com>
2023-09-25 11:13:45 +1300

    Update useUser.ts

--- packages/datatrak-web/src/api/queries/useUser.ts
+++ packages/datatrak-web/src/api/queries/useUser.ts
@@ -6,21 +6,18 @@
 import { useQuery } from 'react-query';
 import { DatatrakWebUserRequest } from '@tupaia/types';
 import { get } from '../api';
-import { useProjects } from './useProjects.ts';
 
 export const useUser = () => {
   const userResponse = useQuery(
     'getUser',
     (): Promise<DatatrakWebUserRequest.ResBody> => get('getUser'),
   );
-  const { data: projects } = useProjects();
 
   const { data: user } = userResponse;
-  const userProject = projects?.find(({ id }) => id === user?.projectId);
 
   return {
     ...userResponse,
-    data: { ...user, name: user?.userName, project: userProject },
+    data: { ...user, name: user?.userName },
     isLoggedIn: !!user?.email,
   };
 };
