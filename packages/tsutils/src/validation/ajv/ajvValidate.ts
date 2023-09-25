7ef95f6b2e2a7207c4b8d1d3d8d83c27865eefe3
Rohan Port <rohan@bes.au>
2023-09-25 17:28:20 +1000

    RN-502: Removed async validation schema logic - Reworked AJV to standardise validation

---
+++ packages/tsutils/src/validation/ajv/ajvValidate.ts
@@ -0,0 +1,19 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2022 Beyond Essential Systems Pty Ltd
+ */
+import { Schema } from 'ajv';
+import betterAjvErrors from 'better-ajv-errors';
+import { getAjv } from './getAjv';
+
+export const ajvValidate = <T>(schema: Schema, data: unknown) => {
+  const ajv = getAjv();
+  const validate = ajv.compile<T>(schema);
+
+  if (validate(data)) {
+    return data;
+  }
+
+  const betterErrors = betterAjvErrors(schema, data, validate.errors || [], { format: 'js' });
+  throw new Error(`Validation Error: ${betterErrors.map(err => err.error)}`);
+};
