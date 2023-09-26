6b882e61ed826ffe7ad874e1c1fce8a0fecd95ef
acdunham <alex@beyondessential.com.au>
2023-09-26 13:13:40 +1300

    Generate types

--- packages/types/src/types/requests/datatrak-web-server/UserRequest.ts
+++ packages/types/src/types/requests/datatrak-web-server/UserRequest.ts
@@ -5,17 +5,17 @@
 
 import { ProjectResponse } from '../web-server';
 import { Entity } from '../../models';
-import { KeysToCamelCase } from '../../../utils';
+
+type Country = Pick<Entity, 'id' | 'name' | 'code'>;
 
 export type Params = Record<string, never>;
 export interface ResBody {
   id?: string;
   userName?: string;
   email?: string;
-  preferences?: any;
   project?: ProjectResponse | null;
   projectId?: string;
-  country?: KeysToCamelCase<Entity> | null;
+  country?: Country | null;
 }
 export type ReqBody = Record<string, never>;
 export type ReqQuery = Record<string, never>;
