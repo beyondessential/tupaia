7c6a4cd75df45361abba8ee91c35a2150a3d6c13
Rohan Port <rohan@bes.au>
2023-10-02 15:08:59 +1100

    RN-502: Added flow to AuthRoute for updating meditrak_device

--- packages/meditrak-app-server/src/routes/AuthRoute.ts
+++ packages/meditrak-app-server/src/routes/AuthRoute.ts
@@ -9,13 +9,15 @@ import { Route } from '@tupaia/server-boilerplate';
 import { Resolved } from '@tupaia/types';
 import { yup } from '@tupaia/utils';
 import { TupaiaApiClient } from '@tupaia/api-client';
+import { MeditrakDeviceType } from '../models/MeditrakDevice';
 
 export type AuthRequest = Request<
   Record<string, never>,
   | Resolved<ReturnType<TupaiaApiClient['auth']['login']>>
   | Resolved<ReturnType<TupaiaApiClient['auth']['refreshAccessToken']>>,
-  Record<string, never>,
+  Record<string, unknown>,
   {
+    appVersion: string;
     grantType?: 'refresh_token';
   }
 >;
@@ -33,14 +35,74 @@ const loginValidator = yup.object().shape({
 });
 
 export class AuthRoute extends Route<AuthRequest> {
-  public async buildResponse() {
-    const { query, body } = this.req;
-    if (query.grantType === 'refresh_token') {
-      const { refreshToken } = reAuthValidator.validateSync(body);
-      return this.req.ctx.services.auth.refreshAccessToken(refreshToken);
+  private async updateMeditrakDevice(meditrakDevice: MeditrakDeviceType, refreshToken: string) {
+    const { appVersion } = this.req.query;
+
+    // eslint-disable-next-line no-param-reassign
+    meditrakDevice.app_version = appVersion;
+    // eslint-disable-next-line no-param-reassign
+    meditrakDevice.refresh_token = refreshToken;
+    // eslint-disable-next-line no-param-reassign
+    meditrakDevice.last_login_time = new Date();
+
+    await meditrakDevice.save();
+  }
+
+  private async refreshTokenLogin(refreshToken: string) {
+    const meditrakDevice = await this.req.models.meditrakDevice.findOne({
+      refresh_token: refreshToken,
+    });
+
+    if (!meditrakDevice) {
+      throw new Error(`Could not find meditrak device for refresh token. Please login again.`);
     }
 
-    const userFields = loginValidator.validateSync(body);
-    return this.req.ctx.services.auth.login(userFields);
+    const loginResponse = await this.req.ctx.services.auth.refreshAccessToken(refreshToken);
+    await this.updateMeditrakDevice(meditrakDevice, loginResponse.refreshToken);
+    return loginResponse;
+  }
+
+  private async userPasswordLogin(
+    emailAddress: string,
+    password: string,
+    deviceName: string,
+    installId: string,
+    platform: string,
+  ) {
+    const loginResponse = await this.req.ctx.services.auth.login({
+      emailAddress,
+      password,
+      deviceName,
+    });
+
+    const meditrakDevice = await this.req.models.meditrakDevice.findOrCreate(
+      {
+        user_id: loginResponse.user.id,
+        install_id: installId,
+      },
+      { platform },
+    );
+
+    await this.updateMeditrakDevice(meditrakDevice, loginResponse.refreshToken);
+    return loginResponse;
+  }
+
+  public async buildResponse() {
+    const { query, body } = this.req;
+
+    if (query.grantType === 'refresh_token') {
+      const { refreshToken } = reAuthValidator.validateSync(body);
+      return this.refreshTokenLogin(refreshToken);
+    }
+
+    const {
+      emailAddress,
+      password,
+      deviceName,
+      installId,
+      devicePlatform,
+    } = loginValidator.validateSync(body);
+
+    return this.userPasswordLogin(emailAddress, password, deviceName, installId, devicePlatform);
   }
 }
