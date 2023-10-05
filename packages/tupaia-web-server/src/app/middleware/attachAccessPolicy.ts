dd761026ab601b4889a8f0b42753681ab6e2e4dd
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-10-05 13:25:30 +1300

    WAITP-1366 Merge access policies

--- packages/tupaia-web-server/src/app/middleware/attachAccessPolicy.ts
+++ packages/tupaia-web-server/src/app/middleware/attachAccessPolicy.ts
@@ -4,22 +4,24 @@
  */
 
 import { RequestHandler } from 'express';
-import { AccessPolicyBuilder } from '@tupaia/auth';
+import { AccessPolicyBuilder, mergeAccessPolicies } from '@tupaia/auth';
 import { AccessPolicy } from '@tupaia/access-policy';
 
 const { API_CLIENT_NAME } = process.env;
 
 export const attachAccessPolicy: RequestHandler = async (req, res, next) => {
-  if (req.session) {
-    req.accessPolicy = req.session.accessPolicy;
-  } else {
-    const accessPolicyBuilder = new AccessPolicyBuilder(req.models);
-    const apiUser = await req.models.user.findOne({ email: API_CLIENT_NAME });
-    if (!apiUser) {
-      throw new Error('API Client not found');
-    }
-    req.accessPolicy = new AccessPolicy(await accessPolicyBuilder.getPolicyForUser(apiUser.id));
+  const accessPolicyBuilder = new AccessPolicyBuilder(req.models);
+  const apiUser = await req.models.user.findOne({ email: API_CLIENT_NAME });
+  if (!apiUser) {
+    throw new Error('API Client not found');
   }
+  const apiAccessPolicy = new AccessPolicy(await accessPolicyBuilder.getPolicyForUser(apiUser.id));
+
+  // The session accessPolicy is the raw user access policy
+  // We need to merge with the api access policy for full access
+  req.accessPolicy = req.session
+    ? mergeAccessPolicies(req.accessPolicy, apiAccessPolicy)
+    : apiAccessPolicy;
 
   next();
 };
