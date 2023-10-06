d3c1c3f712bb2913e81092d842ea1c5f075985a5
acdunham <alex@beyondessential.com.au>
2023-10-06 16:34:17 +1300

    Handle location errors

--- packages/datatrak-web/src/features/Questions/GeolocateQuestion/MapModal/Map.tsx
+++ packages/datatrak-web/src/features/Questions/GeolocateQuestion/MapModal/Map.tsx
@@ -52,10 +52,21 @@ const UserLocationMap = ({
   useEffect(() => {
     // If the user has not yet dropped a pin, use their current location
     if (coordinatesInvalid) {
-      map.locate().on('locationfound', e => {
-        map.flyTo(e.latlng, DEFAULT_ZOOM_LEVEL);
-        setCoordinates(e.latlng);
-      });
+      map
+        .locate()
+        .on('locationfound', e => {
+          map.flyTo(e.latlng, DEFAULT_ZOOM_LEVEL);
+          setCoordinates(e.latlng);
+        })
+        .on('locationerror', () => {
+          // if there is a problem getting the user's location, use the center of the default bounds. This is in cases where, for example, the user denies location access
+          const latLong = {
+            lat: DEFAULT_BOUNDS[0][0] + (DEFAULT_BOUNDS[1][0] - DEFAULT_BOUNDS[0][0]) / 2,
+            lng: DEFAULT_BOUNDS[0][1] + (DEFAULT_BOUNDS[1][1] - DEFAULT_BOUNDS[0][1]) / 2,
+          };
+          map.flyTo(latLong, UNSET_LOCATION_ZOOM_LEVEL);
+          setCoordinates(latLong);
+        });
     } else {
       // Otherwise use the location they have already dropped a pin on/entered manually
       map.setView([lat!, lng!], DEFAULT_ZOOM_LEVEL);
