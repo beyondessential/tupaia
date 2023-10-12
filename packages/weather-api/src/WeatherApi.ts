e82871b01619831fe376204a45a2b7906a31b4c5
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2023-10-12 16:58:14 +1100

    MDEV-191: Fix weather-api when pulling via data-table-server (#5036)

---
+++ packages/weather-api/src/WeatherApi.ts
@@ -0,0 +1,60 @@
+import { fetchWithTimeout, requireEnv, stringifyQuery } from '@tupaia/utils';
+
+type WeatherResult = {
+  min_temp: number;
+  max_temp: number;
+  precip: number;
+  datetime: string;
+};
+
+const MAX_FETCH_WAIT_TIME = 15 * 1000; // 15 seconds
+
+export class WeatherApi {
+  public async current(lat: string, lon: string) {
+    return this.fetch('/v2.0/current', { lat, lon });
+  }
+
+  public async historicDaily(lat: string, lon: string, startDate: string, endDate: string) {
+    return this.fetch('/v2.0/history/daily', {
+      lat,
+      lon,
+      start_date: startDate,
+      end_date: endDate,
+    });
+  }
+
+  public async forecastDaily(lat: string, lon: string, days = 16) {
+    return this.fetch('/v2.0/forecast/daily', {
+      lat,
+      lon,
+      days,
+    });
+  }
+
+  private async fetch(
+    endpoint: string,
+    params: {
+      lat: string;
+      lon: string;
+      days?: number;
+      start_date?: string;
+      end_date?: string;
+    },
+  ): Promise<{ data: WeatherResult[] }> {
+    const apiKey = requireEnv('WEATHERBIT_API_KEY');
+    const queryParams = { ...params, key: apiKey };
+
+    const url = stringifyQuery('https://api.weatherbit.io', endpoint, queryParams);
+
+    const result = await fetchWithTimeout(url, {}, MAX_FETCH_WAIT_TIME);
+
+    if (result.status !== 200) {
+      const bodyText = await result.text();
+      throw new Error(
+        `Error response from Weatherbit API. Status: ${result.status}, body: ${bodyText}`,
+      );
+    }
+
+    return result.json();
+  }
+}
