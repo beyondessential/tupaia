bfcede92796d7f9c0f731070b97ae55c7f9a54e3
Chris Pollard <therealchrispollard@gmail.com>
2023-10-17 14:58:35 +1100

    rn-1059: create actions menu with icons

--- packages/ui-components/src/components/ActionsMenu.tsx
+++ packages/ui-components/src/components/ActionsMenu.tsx
@@ -4,46 +4,52 @@
  */
 
 import React, { CSSProperties } from 'react';
-import MuiMenu from '@material-ui/core/Menu';
-import MuiMenuItem from '@material-ui/core/MenuItem';
-import IconButton from '@material-ui/core/IconButton';
+import {
+  ListItemIcon,
+  MenuItem as MuiMenuItem,
+  Menu as MuiMenu,
+  IconButton,
+  Typography,
+  Tooltip,
+} from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import styled from 'styled-components';
 
 const StyledMenu = styled(MuiMenu)`
-  .MuiPaper-root {
-    position: relative;
-    width: 8.5rem;
-    border: 1px solid ${props => props.theme.palette.grey['400']};
-    border-radius: 3px;
-    overflow: visible;
+  // .MuiPaper-root {
+  //   position: absolute;
+  //   display: inline-block;
+  //   min-width: 8.5rem;
+  //   border: 1px solid ${props => props.theme.palette.grey['400']};
+  //   border-radius: 3px;
+  //   overflow: visible;
 
-    .MuiList-root {
-      padding-top: 0.25rem;
-      padding-bottom: 0.25rem;
-    }
+  //   // .MuiList-root {
+  //   //   padding-top: 0.25rem;
+  //   //   padding-bottom: 0.25rem;
+  //   // }
 
-    &:before,
-    &:after {
-      position: absolute;
-      content: '';
-      right: 15px;
-      border-right: 10px solid transparent;
-      border-left: 10px solid transparent;
-    }
+  //   // &:before,
+  //   // &:after {
+  //   //   position: absolute;
+  //   //   content: '';
 
-    &:before {
-      top: -9px;
-      z-index: 1;
-      border-bottom: 10px solid white;
-    }
+  //   //   // border-right: 10px solid transparent;
+  //   //   // border-left: 10px solid transparent;
+  //   // }
 
-    &:after {
-      top: -10px;
-      z-index: -1;
-      border-bottom: 10px solid ${props => props.theme.palette.grey['400']};
-    }
-  }
+  //   &:before {
+  //     top: -9px;
+  //     z-index: 1;
+  //     border-bottom: 10px solid white;
+  //   }
+
+  //   &:after {
+  //     top: -10px;
+  //     z-index: -1;
+  //     border-bottom: 10px solid ${props => props.theme.palette.grey['400']};
+  //   }
+  // }
 `;
 
 const StyledMenuItem = styled(MuiMenuItem)`
@@ -67,9 +73,12 @@ interface Option {
   label: string;
   action: () => void;
   style?: CSSProperties;
+  ActionIcon?: React.ElementType;
+  toolTipTitle?: string;
+  color?: string;
 }
 
-export const ActionsMenu = ({ options }: { options: Option[] }) => {
+export const ActionsMenu = ({ options }: { options: Option[] }, includesIcons = false) => {
   const [anchorEl, setAnchorEl] = React.useState<(EventTarget & HTMLButtonElement) | null>(null);
   return (
     <>
@@ -85,10 +94,11 @@ export const ActionsMenu = ({ options }: { options: Option[] }) => {
         onClose={() => setAnchorEl(null)}
         anchorOrigin={{
           vertical: 'bottom',
-          horizontal: -85,
+          horizontal: 'left',
         }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
       >
-        {options.map(({ label, action, style }: Option) => (
+        {options.map(({ label, action, style, ActionIcon, toolTipTitle, color }: Option) => (
           <StyledMenuItem
             role="button"
             key={label}
@@ -98,7 +108,20 @@ export const ActionsMenu = ({ options }: { options: Option[] }) => {
               setAnchorEl(null);
             }}
           >
-            {label}
+            {includesIcons && ActionIcon ? (
+              <>
+                <ListItemIcon>
+                  <ActionIcon fontSize="small" color={color} />
+                </ListItemIcon>
+                <Tooltip title={toolTipTitle || ''} arrow>
+                  <Typography display="inline" variant="inherit">
+                    {label}
+                  </Typography>
+                </Tooltip>
+              </>
+            ) : (
+              label
+            )}
           </StyledMenuItem>
         ))}
       </StyledMenu>
