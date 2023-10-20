8fada900b436ea8ad7c62546f7ff6a1739c11434
Igor Nadj <igor@beyondessential.com.au>
2023-10-20 16:04:25 +1100

    Reimplement QR code scan
    Using new non-deprecated package react-native-vision-camera

--- packages/meditrak-app-new/app/assessment/QrCodeScanner.jsx
+++ packages/meditrak-app-new/app/assessment/QrCodeScanner.jsx
@@ -2,49 +2,73 @@ import React, {useState} from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-// import Scanner from 'react-native-qrcode-scanner';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { RNCamera } from 'react-native-camera';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useCodeScanner,
+} from 'react-native-vision-camera';
 import {Button, STATUS_MESSAGE_ERROR, StatusMessage, Text} from '../widgets';
 import {THEME_COLOR_ONE, THEME_COLOR_TWO} from '../globalStyles';
 
-// eslint-disable-next-line no-unused-vars
 export const QrCodeScanner = ({onRead, onStartScan, onFinishScan}) => {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const device = useCameraDevice('back');
+
   const [isScanningQrCode, setIsScanningQrCode] = useState(false);
   const [error, setError] = useState(null);
 
-  // const CancelScanButton = (
-  //   <Button
-  //     style={localStyles.cancelQrCodeScannerButton}
-  //     onPress={() => {
-  //       onFinishScan();
-  //       setIsScanningQrCode(false);
-  //     }}
-  //     title={null}
-  //     Icon={<Icon name="close" color={THEME_COLOR_ONE} size={24} />}
-  //   />
-  // );
+  const getPermissionAndStartScan = async () => {
+    const granted = await requestPermission();
+    if (!granted) {
+      setError('Camera permission was not granted');
+      setIsScanningQrCode(false);
+      return;
+    }
+    onStartScan();
+    setError(null);
+    setIsScanningQrCode(true);
+  };
 
-  if (isScanningQrCode) {
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr'],
+    onCodeScanned: codes => {
+      const [code] = codes;
+      try {
+        onRead(code.value);
+      } catch (e) {
+        setError(e.message);
+      }
+      onFinishScan();
+      setIsScanningQrCode(false);
+    },
+  });
+
+  if (hasPermission && isScanningQrCode) {
+    if (device == null) return <Text>No Camera detected on this device</Text>;
     return (
-      <Text>TODO</Text>
-      // <Scanner
-      //   style={localStyles.qrCodeScanner}
-      //   onRead={({ data }) => {
-      //     try {
-      //       onRead(data);
-      //     } catch (e) {
-      //       setError(e.message);
-      //     }
-      //
-      //     onFinishScan();
-      //     setIsScanningQrCode(false);
-      //   }}
-      //   flashMode={RNCamera.Constants.FlashMode.auto}
-      //   topContent={CancelScanButton}
-      // />
+      <View style={localStyles.scannerView}>
+        <Button
+          style={localStyles.cancelButton}
+          onPress={() => {
+            onFinishScan();
+            setIsScanningQrCode(false);
+          }}
+          title=" "
+          Icon={<Icon name="close" color={THEME_COLOR_ONE} size={24} />}
+        />
+        <View style={localStyles.cameraView}>
+          <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive
+            codeScanner={codeScanner}
+          />
+        </View>
+      </View>
     );
   }
 
@@ -68,11 +92,7 @@ export const QrCodeScanner = ({onRead, onStartScan, onFinishScan}) => {
       ) : null}
       <Button
         style={localStyles.qrCodeButton}
-        onPress={() => {
-          onStartScan();
-          setError(null);
-          setIsScanningQrCode(true);
-        }}
+        onPress={getPermissionAndStartScan}
         Icon={QrCodeIcon}
         title="Scan QR Code"
         textStyle={localStyles.qrCodeButtonLabelText}
@@ -111,10 +131,7 @@ const localStyles = StyleSheet.create({
   qrCodeButtonLabelText: {
     fontWeight: 'bold',
   },
-  qrCodeScanner: {
-    position: 'relative',
-  },
-  cancelQrCodeScannerButton: {
+  cancelButton: {
     width: 30,
     height: 30,
     backgroundColor: 'transparent',
@@ -122,10 +139,26 @@ const localStyles = StyleSheet.create({
     paddingHorizontal: 0,
     zIndex: 1,
     position: 'absolute',
-    right: 35,
+    right: 10,
     top: 5,
   },
-  cancelQrCodeScannerButtonLabelText: {
-    color: THEME_COLOR_ONE,
+  requestPermissionButton: {
+    marginTop: 15,
+    marginBottom: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    width: '100%',
+  },
+  scannerView: {
+    width: '100%',
+    height: 400,
+    position: 'relative',
+  },
+  cameraView: {
+    width: '100%',
+    height: 400,
+    position: 'relative', // camera will absolute fill this space
+    borderColor: 'transparent', // hack to get camera layout to behave
+    borderWidth: 1, // hack to get camera layout to behave
   },
 });
