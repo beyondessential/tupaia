2b15e13d8e65ffb2091ff4d09ee575ff44f95423
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2023-10-23 16:19:37 +1100

    no-issue: Added <Model>Create and <Model>Update types (#5106)

--- packages/types/generate-models.ts
+++ packages/types/generate-models.ts
@@ -5,7 +5,7 @@
 
 // See https://rmp135.github.io/sql-ts/#/?id=totypescript
 
-import sqlts from '@rmp135/sql-ts';
+import sqlts, { Table } from '@rmp135/sql-ts';
 
 import Knex from 'knex';
 // @ts-ignore
@@ -26,17 +26,63 @@ const db = Knex({
   },
 });
 
+const renameTables = (tables: Table[], suffix: string) =>
+  tables.map(table => ({
+    ...table,
+    interfaceName: `${table.interfaceName}${suffix}`,
+  }));
+
+const combineTables = (baseTables: Table[], createTables: Table[], updateTables: Table[]) => {
+  const combinedTables: Table[] = [];
+  for (let i = 0; i < baseTables.length; i++) {
+    combinedTables.push(baseTables[i], createTables[i], updateTables[i]);
+  }
+  return combinedTables;
+};
+
+const removeIdColumn = (tables: Table[]) =>
+  tables.map(({ columns, ...restOfTable }) => ({
+    ...restOfTable,
+    columns: columns.filter(({ name }) => name !== 'id'),
+  }));
+
+const makeColumnsOptional = (tables: Table[]): Table[] =>
+  tables.map(({ columns, ...restOfTable }) => ({
+    ...restOfTable,
+    columns: columns.map(col => ({ ...col, optional: true })),
+  }));
+
+const removeUnwantedColumns = (tables: Table[]) =>
+  tables.map(({ columns, ...restOfTable }) => ({
+    ...restOfTable,
+    columns: columns.filter(({ name }) => !name.includes('m_row$')), // Remove mvrefresh columns
+  }));
+
 const run = async () => {
   const failOnChanges = process.argv[2] === '--failOnChanges';
 
   // @ts-ignore
-  const tsString = await sqlts.toTypeScript(config, db);
+  const definitions = await sqlts.toObject(config, db);
+
+  // ModelCreate tables don't require an id column as one will be generated at create time
+  const createTables = removeIdColumn(renameTables(definitions.tables, 'Create'));
+
+  // ModelUpdate tables have all fields as optional since we are just updating an existing record
+  const updateTables = makeColumnsOptional(renameTables(definitions.tables, 'Update'));
+
+  const allTables = removeUnwantedColumns(
+    combineTables(definitions.tables, createTables, updateTables),
+  );
+
+  const tsString = sqlts.fromObject({ ...definitions, tables: allTables }, config);
 
   if (failOnChanges) {
     const currentTsString = fs.readFileSync(config.filename, { encoding: 'utf8' });
     if (currentTsString !== tsString) {
-      console.log("❌ There are changes in the db schema which are not reflected in @tupaia/types.")
-      console.log("Run 'yarn workspace @tupaia/types generate' to fix")
+      console.log(
+        '❌ There are changes in the db schema which are not reflected in @tupaia/types.',
+      );
+      console.log("Run 'yarn workspace @tupaia/types generate' to fix");
       process.exit(1);
     }
   }
