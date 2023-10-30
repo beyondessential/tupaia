ee6fe0a5a70883a0f152bf5f3a60acdcb6d26262
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2023-10-31 08:15:48 +1100

    no-issue: Move SurveyScreenComponent config types (#5115)

--- packages/datatrak-web/src/features/Questions/EntityQuestion/utils.ts
+++ packages/datatrak-web/src/features/Questions/EntityQuestion/utils.ts
@@ -2,13 +2,11 @@
  * Tupaia
  *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
-import { DatatrakWebSurveyRequest } from '@tupaia/types';
+import { SurveyScreenComponentConfig } from '@tupaia/types';
 import { useUser } from '../../../api/queries';
 import { useSurveyForm } from '../../Survey';
 
-export const useEntityBaseFilters = (
-  config: DatatrakWebSurveyRequest.SurveyScreenComponentConfig,
-) => {
+export const useEntityBaseFilters = (config: SurveyScreenComponentConfig) => {
   const { getAnswerByQuestionId } = useSurveyForm();
   const { data: userData } = useUser();
   const countryCode = userData?.country?.code;
@@ -38,9 +36,7 @@ export const useEntityBaseFilters = (
 /*
  * Returns a function that filters entities based on configured attribute values and questions
  */
-export const useAttributeFilter = (
-  questionConfig: DatatrakWebSurveyRequest.SurveyScreenComponentConfig,
-) => {
+export const useAttributeFilter = (questionConfig: SurveyScreenComponentConfig) => {
   const { getAnswerByQuestionId } = useSurveyForm();
   const attributes = questionConfig.entity?.filter?.attributes;
   if (!attributes) {
