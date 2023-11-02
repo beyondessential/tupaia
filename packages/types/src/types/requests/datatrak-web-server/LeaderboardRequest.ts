9a5deb738a42ca42272a7955c24262333336c951
acdunham <alex@beyondessential.com.au>
2023-11-03 08:57:44 +1300

    Fix types

--- packages/types/src/types/requests/datatrak-web-server/LeaderboardRequest.ts
+++ packages/types/src/types/requests/datatrak-web-server/LeaderboardRequest.ts
@@ -3,10 +3,11 @@
  *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 
+import { KeysToCamelCase } from '../../../utils/casing';
 import { LeaderboardItem } from '../../models-extra';
 
 export type Params = Record<string, never>;
 
-export type ResBody = LeaderboardItem[];
+export type ResBody = KeysToCamelCase<LeaderboardItem>[];
 export type ReqBody = Record<string, never>;
 export type ReqQuery = Record<string, never>;
