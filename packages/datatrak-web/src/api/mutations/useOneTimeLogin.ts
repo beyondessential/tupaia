1b2c5b4ced1ff43298ce73c484401223105495cf
Tom Caiger <caigertom@gmail.com>
2023-11-03 17:16:30 +1300

    one time login flow (#5138)

---
+++ packages/datatrak-web/src/api/mutations/useOneTimeLogin.ts
@@ -0,0 +1,30 @@
+/*
+ * Tupaia
+ *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import { useMutation, useQueryClient } from 'react-query';
+import { post } from '../api';
+
+type OneTimeLoginToken = string;
+
+export const useOneTimeLogin = () => {
+  const queryClient = useQueryClient();
+  return useMutation<any, Error, OneTimeLoginToken, unknown>(
+    (token: OneTimeLoginToken) => {
+      return post('login/oneTimeLogin', {
+        data: {
+          token,
+        },
+      });
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries();
+      },
+      meta: {
+        applyCustomErrorHandling: true,
+      },
+    },
+  );
+};
