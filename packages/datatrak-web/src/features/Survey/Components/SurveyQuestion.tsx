f7f3d9ea8ba0a8add84bd4a7b5fc0aa7761c7bb4
Tom Caiger <caigertom@gmail.com>
2023-11-06 14:32:28 +1300

    update primary entity question name

--- packages/datatrak-web/src/features/Survey/Components/SurveyQuestion.tsx
+++ packages/datatrak-web/src/features/Survey/Components/SurveyQuestion.tsx
@@ -33,15 +33,6 @@ const QuestionPlaceholder = styled.div`
   max-width: 30rem;
 `;
 
-const Placeholder = ({ name, type, id }) => {
-  return (
-    <QuestionPlaceholder id={id}>
-      <p>Question name: {name}</p>
-      <p>Question type: {type}</p>
-    </QuestionPlaceholder>
-  );
-};
-
 const QuestionWrapper = styled.div`
   width: 100%;
   .MuiFormHelperText-root.Mui-error {
@@ -71,10 +62,6 @@ export enum QUESTION_TYPES {
   File = FileQuestion,
 }
 
-const getNameForController = (name, type) => {
-  return type === 'PrimaryEntity' ? 'entityId' : name;
-};
-
 /**
  * This is the component that renders a single question in a survey.
  */
@@ -97,7 +84,6 @@ export const SurveyQuestion = ({
   if (READ_ONLY_QUESTION_TYPES.includes(type)) {
     return <FieldComponent {...props} name={name} type={type} />;
   }
-  const controllerName = getNameForController(name, type);
 
   // If the question dictates the visibility of any other questions, we need to update the formData when the value changes, so the visibility of other questions can be updated in real time. This doesn't happen that often, so it shouldn't have too much of a performance impact, and we are only updating the formData for the question that is changing, not the entire formData object.
   const handleOnChange = e => {
@@ -113,7 +99,7 @@ export const SurveyQuestion = ({
   const getDefaultValue = () => {
     // This is so that the default value gets carried through to the component, and dates that have a visible value of 'today' have that value recognised when validating
     if (type?.includes('Date')) return new Date();
-    return formData[controllerName];
+    return formData[name];
   };
 
   const defaultValue = getDefaultValue();
@@ -124,7 +110,7 @@ export const SurveyQuestion = ({
   return (
     <QuestionWrapper>
       <Controller
-        name={controllerName}
+        name={name}
         control={control}
         defaultValue={defaultValue}
         render={({ onChange, ref, ...renderProps }, { invalid }) => (
@@ -142,7 +128,7 @@ export const SurveyQuestion = ({
             required={required}
             min={min}
             max={max}
-            name={controllerName}
+            name={name}
             type={type}
           />
         )}
