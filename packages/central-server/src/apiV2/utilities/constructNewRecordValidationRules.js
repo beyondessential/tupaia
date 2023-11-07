a6b40052549a2a915eea201eaea24303243f4931
acdunham <alex@beyondessential.com.au>
2023-11-08 09:05:45 +1300

    Remove permission group and country from admin panel feed items

--- packages/central-server/src/apiV2/utilities/constructNewRecordValidationRules.js
+++ packages/central-server/src/apiV2/utilities/constructNewRecordValidationRules.js
@@ -83,7 +83,7 @@ export const constructForSingle = (models, recordType) => {
         user_id: [constructIsEmptyOr(constructRecordExistsWithId(models.user))],
         permission_group_id: [
           // Always require a permission group ID, by default an item should at least be public.
-          constructRecordExistsWithId(models.permissionGroup),
+          constructIsEmptyOr(constructRecordExistsWithId(models.permissionGroup)),
         ],
         type: [isAString],
         template_variables: [constructIsEmptyOr(isPlainObject)],
