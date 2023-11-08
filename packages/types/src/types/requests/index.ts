86bbac707c311338d4eb42a1f4eeced5ce742248
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-11-09 12:28:59 +1300

    WAITP-1478 Fixup export

--- packages/types/src/types/requests/index.ts
+++ packages/types/src/types/requests/index.ts
@@ -14,6 +14,7 @@ export {
   DatatrakWebSubmitSurveyRequest,
   DatatrakWebSurveyRequest,
   DatatrakWebSurveyResponsesRequest,
+  DatatrakWebRecentSurveyResponseRequest,
 } from './datatrak-web-server';
 export {
   TupaiaWebChangePasswordRequest,
