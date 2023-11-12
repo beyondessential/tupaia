f8bad375d21dcf0eec8b067c41d80a0401a227e8
acdunham <alex@beyondessential.com.au>
2023-11-13 10:27:53 +1300

    Create migration

---
+++ packages/database/src/migrations/20231112212117-AddProjectIdToSurveys-modifies-schema.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.runSql(`ALTER TABLE survey ADD project_id TEXT NULL`);
+};
+
+exports.down = function (db) {
+  return db.runSql(`
+  ALTER TABLE survey DROP COLUMN project_id;
+`);
+};
+
+exports._meta = {
+  version: 1,
+};
