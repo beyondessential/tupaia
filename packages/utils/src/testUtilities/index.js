7c10a52d549325992cb73d2650a3d63b61c263b6
Igor Nadj <igor@beyondessential.com.au>
2023-11-14 13:13:14 +1300

    Build each package individually

--- packages/utils/src/testUtilities/index.js
+++ packages/utils/src/testUtilities/index.js
@@ -3,6 +3,5 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 
-export { addCustomJestMatchers } from './addCustomJestMatchers';
 export { createJestMockInstance } from './createJestMockInstance';
 export { randomEmail, randomIntBetween, randomString } from './randomData';
