f5eb5805adf02315b829278f6aafa254cff30be2
Tom Caiger <caigertom@gmail.com>
2023-11-16 09:05:53 +1300

    remove tsx extentions

--- packages/datatrak-web/src/api/queries/useSurveyResponses.ts
+++ packages/datatrak-web/src/api/queries/useSurveyResponses.ts
@@ -4,7 +4,7 @@
  */
 import { useQuery } from 'react-query';
 import { DatatrakWebSurveyResponsesRequest, UserAccount, Project } from '@tupaia/types';
-import { useCurrentUser } from '../CurrentUserContext.tsx';
+import { useCurrentUser } from '../CurrentUserContext';
 import { get } from '../api';
 
 export const useSurveyResponses = (userId?: UserAccount['id'], projectId?: Project['id']) => {
