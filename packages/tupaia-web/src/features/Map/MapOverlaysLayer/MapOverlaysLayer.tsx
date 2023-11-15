ca35da850b8fbb167ea64c83489ab945288f6b57
acdunham <alex@beyondessential.com.au>
2023-11-16 10:18:51 +1300

    PR fixes

--- packages/tupaia-web/src/features/Map/MapOverlaysLayer/MapOverlaysLayer.tsx
+++ packages/tupaia-web/src/features/Map/MapOverlaysLayer/MapOverlaysLayer.tsx
@@ -12,12 +12,14 @@ import { PolygonLayer } from './PolygonLayer';
 import { MarkerLayer } from './MarkerLayer';
 import { useEntity } from '../../../api/queries';
 
+const POINT_ZOOM_LEVEL = 15;
 // When entity is selected, zoom to entity
 const useZoomToEntity = () => {
   const { projectCode, entityCode } = useParams();
   const { data: entity } = useEntity(projectCode, entityCode);
   const map = useMap();
 
+  // This is a replacement for the map positioning being handled in the ui-map-components LeafletMap file. We are doing this because we need access to the user's current zoom level, and are also slowly moving away from class based components to use hooks instead.
   useEffect(() => {
     if (!entity || !map || (!entity.point && !entity.bounds)) return;
     if (entity.bounds) {
@@ -26,13 +28,13 @@ const useZoomToEntity = () => {
       });
     } else {
       const currentZoom = map.getZoom();
-      // if already zoomed in beyond 15, don't zoom out
-      const zoomLevel = currentZoom > 15 ? currentZoom : 15;
+      // if already zoomed in beyond the POINT_ZOOM_LEVEL, don't zoom out
+      const zoomLevel = currentZoom > POINT_ZOOM_LEVEL ? currentZoom : POINT_ZOOM_LEVEL;
       map.flyTo(entity.point, zoomLevel, {
         animate: false, // don't animate, as it can slow things down a bit
       });
     }
-  }, [entity]);
+  }, [JSON.stringify(entity?.point), JSON.stringify(entity?.bounds)]);
 };
 
 export const MapOverlaysLayer = ({
