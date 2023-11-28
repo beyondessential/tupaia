649ee66eac5f6f54735ca77c3e13f25e333e5f60
Chris Pollard <therealchrispollard@gmail.com>
2023-11-28 11:34:13 +1100

    rn-1059: rm edit permission for create route

--- packages/central-server/src/apiV2/dashboardMailingListEntries/assertDashboardMailingListEntryPermissions.js
+++ packages/central-server/src/apiV2/dashboardMailingListEntries/assertDashboardMailingListEntryPermissions.js
@@ -3,15 +3,11 @@
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 
-const isTupaiaServerUser = async models => {
-  return models.user.findOne({ first_name: 'TUPAIA', last_name: 'Server' });
-};
-
 export const assertIsOwnEmail = async (userId, models, dashboardMailingListEmail) => {
   const user = await models.user.findById(userId);
 
   // A user who is not logged in will have user_account record of Tupaia Server.
-  if (!isTupaiaServerUser(models) && user.email !== dashboardMailingListEmail) {
+  if (user.email !== dashboardMailingListEmail) {
     throw new Error(`Dashboard mailing list entry must belong to user`);
   }
 
