94b43f0301071261a6a89ef42f0d93e8df9d96ef
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-12-05 14:43:49 +1300

    fix form width in medium size class

--- packages/datatrak-web/src/views/AccountSettingsPage/ChangePasswordSection/ChangePasswordForm.tsx
+++ packages/datatrak-web/src/views/AccountSettingsPage/ChangePasswordSection/ChangePasswordForm.tsx
@@ -13,8 +13,11 @@ import { ResetPasswordParams } from '../../../types';
 import { useResetPassword } from '../../../api';
 
 const StyledForm = styled(Form)`
-  max-width: 44.25rem;
   width: 100%;
+
+  ${({ theme }) => theme.breakpoints.up('lg')} {
+    max-width: 44.25rem;
+  }
 `;
 
 const StyledFieldset = styled.fieldset`
