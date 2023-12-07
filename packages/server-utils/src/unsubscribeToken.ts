af35cd2e743a7fe8a274c25f8efa24a92c254996
Rohan Port <rohan@bes.au>
2023-12-07 18:37:26 +1100

    RN-1075: Added unsubscribeToken to server-utils

---
+++ packages/server-utils/src/unsubscribeToken.ts
@@ -0,0 +1,20 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ */
+
+import { requireEnv } from '@tupaia/utils';
+import sha256 from 'sha256';
+
+export const generateUnsubscribeToken = (email: string) => {
+  const secretKey = requireEnv('UNSUBSCRIBE_TOKEN_SECRET');
+  return sha256(`${email}${secretKey}`);
+};
+
+export const verifyUnsubscribeToken = (token: string, email: string) => {
+  if (generateUnsubscribeToken(email) === token) {
+    return true;
+  }
+
+  throw new Error(`Invalid unsubscribe token for email: ${email}`);
+};
