d17b33351e0681394c986eb68c037640d3d19a50
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-12-11 15:51:20 +1300

    fix “Approval in progress” tooltip always appearing

--- packages/datatrak-web/src/views/AccountSettingsPage/RequestCountryAccessSection/RequestCountryAccessForm.tsx
+++ packages/datatrak-web/src/views/AccountSettingsPage/RequestCountryAccessSection/RequestCountryAccessForm.tsx
@@ -127,7 +127,7 @@ export const RequestCountryAccessForm = () => {
                 key={country.id}
                 name={country.name}
                 label={country.name}
-                tooltip={country.accessRequests ? 'Approval in progress' : null}
+                tooltip={country.accessRequests.length ? 'Approval in progress' : null}
               />
             ))}
           </CountryList>
