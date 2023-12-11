c0099bb5a36fa2d8b0f018091d5157c9d4f21b45
Rohan Port <rohan@bes.au>
2023-12-11 17:52:25 +1100

    RN-1073: Updated ensure to an assertion

--- packages/tsutils/src/typeGuards.ts
+++ packages/tsutils/src/typeGuards.ts
@@ -5,15 +5,17 @@
 
 export const isDefined = <T>(value: T): value is Exclude<T, undefined> => value !== undefined;
 
-export const isNotNullish = <T>(val: T): val is Exclude<T, undefined | null> =>
-  val !== undefined && val !== null;
+export const isNotNullish = <T>(val: T): val is NonNullable<T> => val !== undefined && val !== null;
+
+export function assertIsNotNullish<T>(val: T): asserts val is NonNullable<T> {
+  if (!isNotNullish(val)) {
+    throw new Error('Unexpected null or undefined value');
+  }
+}
 
 export const ensure = <T>(val: T) => {
-  if (isNotNullish(val)) {
-    return val;
-  }
-
-  throw new Error(`Unexpected null or undefined value`);
+  assertIsNotNullish(val);
+  return val;
 };
 
 export const isObject = (val: unknown): val is Record<string, unknown> =>
