66fc09c956da9004bb60375f4c16c9ebb31e559a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-12-11 21:33:05 +1300

    show “Account settings” only when project selected

--- packages/datatrak-web/src/layout/UserMenu/MenuList.tsx
+++ packages/datatrak-web/src/layout/UserMenu/MenuList.tsx
@@ -11,6 +11,15 @@ import { useCurrentUser } from '../../api';
 import { useLogout } from '../../api/mutations';
 import { ROUTES } from '../../constants';
 
+interface MenuItem {
+  label: string;
+  to?: string;
+  href?: string;
+  isExternal?: boolean;
+  onClick?: () => void;
+  component?: ComponentType<any> | string;
+}
+
 const Menu = styled.ul`
   list-style: none;
   padding-inline-start: 0;
@@ -37,7 +46,8 @@ export const MenuButton = styled(Button).attrs({
 `;
 
 /**
- * This is the menu list that appears in both the drawer and popover menus. It shows different options depending on whether the user is logged in or not.
+ * The menu list that appears in both the drawer and popover menus. It shows different options depending on whether the
+ * user is logged in or not.
  */
 export const MenuList = ({
   children,
@@ -46,69 +56,63 @@ export const MenuList = ({
   children?: ReactNode;
   onCloseMenu: () => void;
 }) => {
-  const { isLoggedIn } = useCurrentUser();
+  const { isLoggedIn, projectId } = useCurrentUser();
   const { mutate: logout } = useLogout();
 
-  // The help centre link is the same for both logged in and logged out users
-  const helpCentre = {
+  const accountSettingsItem = {
+    label: 'Account settings',
+    to: ROUTES.ACCOUNT_SETTINGS,
+  };
+
+  // The help centre link is the same for both logged-in and logged-out users
+  const helpCentreItem = {
     label: 'Help centre',
     href: 'https://beyond-essential.slab.com/posts/tupaia-instruction-manuals-05nke1dm',
     isExternal: true,
     component: Link,
   };
-  const onClickLogout = () => {
-    logout();
-    onCloseMenu();
+
+  const logOutItem = {
+    label: 'Log out',
+    onClick: () => {
+      logout();
+      onCloseMenu();
+    },
+    component: 'button',
   };
 
-  const menuItems = isLoggedIn
-    ? [
-        {
-          label: 'Account settings',
-          to: ROUTES.ACCOUNT_SETTINGS,
-        },
-        helpCentre,
-        {
-          label: 'Log out',
-          onClick: onClickLogout,
-          component: 'button',
-        },
-      ]
-    : [helpCentre];
+  const menuItems = () => {
+    const hasProjectSelected = !!projectId;
+    const items: MenuItem[] = [];
+
+    if (isLoggedIn && hasProjectSelected) {
+      items.push(accountSettingsItem);
+    }
+    items.push(helpCentreItem);
+    if (isLoggedIn) {
+      items.push(logOutItem);
+    }
+
+    return items;
+  };
 
   return (
     <Menu>
       {children}
-      {menuItems.map(
-        ({
-          label,
-          to,
-          href,
-          isExternal,
-          onClick,
-          component,
-        }: {
-          label: string;
-          to?: string;
-          href?: string;
-          isExternal?: boolean;
-          onClick?: () => void;
-          component?: ComponentType<any> | string;
-        }) => (
-          <MenuListItem key={label} button>
-            <MenuButton
-              component={component || RouterLink}
-              underline="none"
-              target={isExternal ? '_blank' : null}
-              onClick={onClick || onCloseMenu}
-              to={to}
-              href={href}
-            >
-              {label}
-            </MenuButton>
-          </MenuListItem>
-        ),
-      )}
+      {menuItems().map(({ label, to, href, isExternal, onClick, component }) => (
+        <MenuListItem key={label} button>
+          <MenuButton
+            component={component || RouterLink}
+            underline="none"
+            target={isExternal ? '_blank' : null}
+            onClick={onClick || onCloseMenu}
+            to={to}
+            href={href}
+          >
+            {label}
+          </MenuButton>
+        </MenuListItem>
+      ))}
     </Menu>
   );
 };
