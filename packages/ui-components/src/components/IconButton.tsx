c2f592890fba1726defc325fa2930dc5fc16e228
acdunham <alex@beyondessential.com.au>
2023-12-19 14:43:53 +1300

    Fix icon button type issue

--- packages/ui-components/src/components/IconButton.tsx
+++ packages/ui-components/src/components/IconButton.tsx
@@ -3,13 +3,16 @@
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
 import styled from 'styled-components';
-import MuiIconButton from '@material-ui/core/IconButton';
+import MuiIconButton, { IconButtonProps } from '@material-ui/core/IconButton';
+import { OverrideableComponentProps } from '../types';
 
 // export as a styled component so that we can forward refs without any other configuration
-export const IconButton = styled(MuiIconButton).attrs(props => ({
-  color: 'primary',
-  ...props,
-}))``;
+export const IconButton = styled(MuiIconButton).attrs(
+  (props: OverrideableComponentProps<IconButtonProps>) => ({
+    color: 'primary',
+    ...props,
+  }),
+)``;
 
 export const LightIconButton = styled(IconButton)`
   color: white;
