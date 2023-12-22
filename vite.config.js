1f47e43db17281c425ff0333b22b7228a0e98645
Tom Caiger <caigertom@gmail.com>
2023-12-22 13:23:24 +1300

    WAITP-1527: GZIP JS files and json responses on tupaia and datatrak (#5309)
    * Update servers.conf
    
    * gzip_static on
    
    * Update servers.conf
    
    * Update servers.conf
    
    * Update servers.conf

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@
 
 import { defineConfig, loadEnv } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import dns from 'dns';
@@ -39,6 +40,7 @@ export default defineConfig(({ command, mode }) => {
     // ViteEjsPlugin is used to allow the use of EJS templates in the index.html file, for analytics scripts etc
     plugins: [
       ViteEjsPlugin(),
+      viteCompression(),
       react({
         jsxRuntime: 'classic',
       }),
