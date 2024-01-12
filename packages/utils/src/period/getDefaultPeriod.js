d6f80a1afdc1330de93b29fd749c96e22c9205d0
Rohan Port <rohan@bes.au>
2024-01-12 11:41:52 +1100

    no-issue: Extend default startDate back to 2017-01-01

--- packages/utils/src/period/getDefaultPeriod.js
+++ packages/utils/src/period/getDefaultPeriod.js
@@ -8,14 +8,10 @@ import { utcMoment } from '../datetime';
 
 export const EARLIEST_DATA_DATE_STRING = '2017-01-01'; // Tupaia started in 2017
 export const EARLIEST_DATA_DATE = utcMoment(EARLIEST_DATA_DATE_STRING);
-const MAXIMUM_MONTHS_TO_LOOK_BACK = 60; // Last 5 years
 
 // Assemble a default date range using monthly granularity.
 export const getDefaultPeriod = () => {
-  const minimumStartDate = utcMoment().subtract(MAXIMUM_MONTHS_TO_LOOK_BACK, 'month');
-  const startDateToUse =
-    EARLIEST_DATA_DATE > minimumStartDate ? EARLIEST_DATA_DATE : minimumStartDate;
-  const startDate = startDateToUse.date(1);
+  const startDate = EARLIEST_DATA_DATE.date(1);
   const endDate = utcMoment();
 
   return convertDateRangeToPeriods(startDate, endDate).join(';');
