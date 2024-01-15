01ee9bd165f975eed4db8ddfaa82cdbb44795312
acdunham <129009580+acdunham@users.noreply.github.com>
2024-01-15 14:17:03 +1300

    Add edit checks

--- packages/central-server/src/apiV2/dashboardItems/EditDashboardItem.js
+++ packages/central-server/src/apiV2/dashboardItems/EditDashboardItem.js
@@ -6,6 +6,7 @@
 import { EditHandler } from '../EditHandler';
 import { assertAnyPermissions, assertBESAdminAccess } from '../../permissions';
 import { assertDashboardItemEditPermissions } from './assertDashboardItemsPermissions';
+import { ObjectValidator, constructIsEmptyOr, constructRecordExistsWithCode } from '@tupaia/utils';
 
 export class EditDashboardItem extends EditHandler {
   async assertUserHasAccess() {
@@ -14,6 +15,28 @@ export class EditDashboardItem extends EditHandler {
     await this.assertPermissions(
       assertAnyPermissions([assertBESAdminAccess, dashboardItemChecker]),
     );
+  }
+
+  async validateReportExists(reportCode) {
+    const existingRecord = await this.models.dashboardItem.findOne({
+      id: this.recordId,
+    });
+    // if the report is not a legacy report, check if the report exists
+    if (!existingRecord.legacy) {
+      return constructRecordExistsWithCode(this.models.report)(reportCode);
+    }
+    return true;
+  }
+
+  async validate() {
+    const validationCriteria = {
+      report_code: [constructIsEmptyOr(this.validateReportExists)],
+    };
+
+    const validator = new ObjectValidator(validationCriteria);
+    return validator.validate({
+      report_code: this.updatedFields.report_code,
+    }); // Will throw an error if not valid
   }
 
   async editRecord() {
