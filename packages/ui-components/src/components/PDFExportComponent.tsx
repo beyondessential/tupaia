636ba4b8367da64150645476c0ca6671c5c4de2b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-01-15 13:38:40 +1100

    replace deprecated CSS

--- packages/ui-components/src/components/PDFExportComponent.tsx
+++ packages/ui-components/src/components/PDFExportComponent.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const A4_PAGE_WIDTH_PX = 1192;
 
 export const A4Page = styled.div`
+  break-after: page;
   padding: 0px 70px;
-  page-break-after: always;
   width: ${A4_PAGE_WIDTH_PX}px;
 `;
