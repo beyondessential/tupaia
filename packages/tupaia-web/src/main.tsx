45e5a060b80c18fc06eb7885f15b27f71725f1ae
Rohan Port <rohan@bes.au>
2024-01-22 14:12:04 +1100

    no-issue: Rename .env var

--- packages/tupaia-web/src/main.tsx
+++ packages/tupaia-web/src/main.tsx
@@ -8,8 +8,8 @@ import App from './App';
 
 if (
   process.env.NODE_ENV === 'development' &&
-  process.env.REACT_APP_MOCKING_ENABLED &&
-  process.env.REACT_APP_MOCKING_ENABLED.toLowerCase() === 'true'
+  process.env.REACT_APP_ENABLE_MOCKING &&
+  process.env.REACT_APP_ENABLE_MOCKING.toLowerCase() === 'true'
 ) {
   const { worker } = await import('./__tests__/mocks/browser');
   worker.start({ onUnhandledRequest: 'bypass' });
