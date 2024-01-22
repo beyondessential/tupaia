fbe91eddec6d5e664085688a788c9f37d780ece8
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2024-01-22 15:25:42 +1100

    no-issue: Disable MSW mocking by default in tupaia-web (#5359)
    * no-issue: Added REACT_APP_MOCKING_ENABLED .env var to tupaia-web
    - Disables MSW mocking by default
    
    * no-issue: Rename .env var
    
    * newline before EOF
    
    ---------
    
    Co-authored-by: Rohan Port <rohan@bes.au>
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>

--- packages/tupaia-web/src/main.tsx
+++ packages/tupaia-web/src/main.tsx
@@ -6,7 +6,11 @@ import React from 'react';
 import { render as renderReactApp } from 'react-dom';
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
+if (
+  process.env.NODE_ENV === 'development' &&
+  process.env.REACT_APP_ENABLE_MOCKING &&
+  process.env.REACT_APP_ENABLE_MOCKING.toLowerCase() === 'true'
+) {
   const { worker } = await import('./__tests__/mocks/browser');
   worker.start({ onUnhandledRequest: 'bypass' });
 }
