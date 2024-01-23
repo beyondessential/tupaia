0e64b50650a1cc69b2e5e640ee0198a21358dd0d
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-01-24 09:49:24 +1300

    Fix tupaia-web-server tests

--- packages/tupaia-web-server/jest.setup.ts
+++ packages/tupaia-web-server/jest.setup.ts
@@ -3,10 +3,9 @@
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
 
-import { getTestDatabase, clearTestData } from '@tupaia/database';
+import { getTestDatabase } from '@tupaia/database';
 
 afterAll(async () => {
   const database = getTestDatabase();
-  await clearTestData(database);
   await database.closeConnections();
 });
