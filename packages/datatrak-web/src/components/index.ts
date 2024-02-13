dfdff97a0bc4768575909d5544fc5a8bedf6155b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-02-13 14:11:51 +1300

    export `Icons` before `ChangeProjectButton`

--- packages/datatrak-web/src/components/index.ts
+++ packages/datatrak-web/src/components/index.ts
@@ -3,17 +3,17 @@
  * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
  */
 
-export { PageContainer } from './PageContainer';
-export { ButtonLink } from './ButtonLink';
-export { Modal } from './Modal';
-export { TopProgressBar } from './TopProgressBar';
-export { Button } from './Button';
-export { ChangeProjectButton } from './ChangeProjectButton';
-export * from './SelectList';
-export { TextInput } from './TextInput';
-export { Toast } from './Toast';
-export { Tile, LoadingTile } from './Tile';
 export * from './Icons';
-export { SmallModal } from './SmallModal';
+export * from './SelectList';
+export { Button } from './Button';
+export { ButtonLink } from './ButtonLink';
 export { CancelConfirmModal } from './CancelConfirmModal';
+export { ChangeProjectButton } from './ChangeProjectButton';
 export { ErrorDisplay } from './ErrorDisplay';
+export { Modal } from './Modal';
+export { PageContainer } from './PageContainer';
+export { SmallModal } from './SmallModal';
+export { TextInput } from './TextInput';
+export { Tile, LoadingTile } from './Tile';
+export { Toast } from './Toast';
+export { TopProgressBar } from './TopProgressBar';
