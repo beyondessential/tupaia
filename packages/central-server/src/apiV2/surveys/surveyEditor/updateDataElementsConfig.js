87598c98d9158aa6a1ce3cb8b4dd102e905e5340
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-02-22 09:35:18 +1300

    Fix early return

--- packages/central-server/src/apiV2/surveys/surveyEditor/updateDataElementsConfig.js
+++ packages/central-server/src/apiV2/surveys/surveyEditor/updateDataElementsConfig.js
@@ -26,7 +26,8 @@ export const updateDataElementsConfig = async (models, dataGroup) => {
     });
 
     // Only update the data element service type if we have to, i.e. if it is not a Tupaia data element. This is because if we update all the data elements to have the same service type as the data group when they don't need to, if the data element service type has been changed manually, then we can unintentionally break visualisations when we reimport survey questions.
-    if (!dataElementMustMatchDataGroupServiceType(dataElement.service_type)) return;
+
+    if (!dataElementMustMatchDataGroupServiceType(dataElement.service_type)) continue;
 
     dataElement.service_type = serviceType;
     dataElement.config = config;
