2995fded9803aff23f8827926a34ac15279fb030
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-03-07 09:17:52 +1300

    fix string slicing bug

--- packages/tsutils/src/datetime.ts
+++ packages/tsutils/src/datetime.ts
@@ -12,7 +12,7 @@ export const ISO_DATE_PATTERN = /^\d{4}-\d{2}-\d{2}$/;
  * @returns ISO date string in the format "yyyy-mm-dd".
  * @remarks Assumes the input date object is valid.
  */
-export const getIsoDateString = (date: Date) => date.toISOString().slice(1, 10);
+export const getIsoDateString = (date: Date) => date.toISOString().slice(0, 10);
 
 /**
  * @returns utcOffset in format: "+05:00"
