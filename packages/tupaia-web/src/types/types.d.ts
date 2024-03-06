62dc3e44b9654084abf14c565e73f795220c3519
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-03-07 12:12:08 +1300

    update copyright date

--- packages/tupaia-web/src/types/types.d.ts
+++ packages/tupaia-web/src/types/types.d.ts
@@ -1,7 +1,8 @@
 /*
  * Tupaia
- *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
  */
+
 import {
   LandingPage,
   Project,
