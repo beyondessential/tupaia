a8fa6dccec7cff80efb381a7068980f94febfd9c
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-03-08 11:38:16 +1300

    Fix datatabletype

--- packages/data-table-server/src/__tests__/dataTableService/services/SurveyResponseDataTableService.test.ts
+++ packages/data-table-server/src/__tests__/dataTableService/services/SurveyResponseDataTableService.test.ts
@@ -4,12 +4,12 @@
  */
 
 import { MockTupaiaApiClient, MockCentralApi } from '@tupaia/api-client';
-import { DataTableRecord } from '@tupaia/types';
+import { DataTableType } from '@tupaia/types';
 import { DataTableServiceBuilder } from '../../../dataTableService';
 import { CENTRAL_API_RESPONSES } from './fixtures';
 
 const surveyResponseDataTableService = new DataTableServiceBuilder()
-  .setServiceType(DataTableRecord.survey_responses)
+  .setServiceType(DataTableType.survey_responses)
   .setContext({
     apiClient: new MockTupaiaApiClient({ central: new MockCentralApi(CENTRAL_API_RESPONSES) }),
   })
