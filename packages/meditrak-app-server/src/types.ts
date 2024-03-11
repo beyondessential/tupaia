f809f09118b0cb3e1218ff74ad25a963fb91401d
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-03-11 14:44:20 +1300

    meditrak-app-server feed item model

--- packages/meditrak-app-server/src/types.ts
+++ packages/meditrak-app-server/src/types.ts
@@ -5,7 +5,6 @@
 
 import { TupaiaApiClient } from '@tupaia/api-client';
 import {
-  ModelRegistry,
   EntityModel,
   OptionSetModel,
   QuestionModel,
@@ -19,17 +18,17 @@ import {
   SurveyScreenModel,
   SurveyScreenComponentModel,
 } from '@tupaia/database';
-import { FeedItemModel, MeditrakSyncQueueModel, SurveyModel, SurveyResponseModel } from './models';
+import { ServerBoilerplateModelRegistry } from '@tupaia/server-boilerplate';
+import { MeditrakSyncQueueModel, SurveyModel, SurveyResponseModel } from './models';
 
 export type RequestContext = {
   services: TupaiaApiClient;
 };
 
-export interface MeditrakAppServerModelRegistry extends ModelRegistry {
+export interface MeditrakAppServerModelRegistry extends ServerBoilerplateModelRegistry {
   readonly entity: EntityModel;
   readonly country: CountryModel;
   readonly facility: FacilityModel;
-  readonly feedItem: FeedItemModel;
   readonly geographicalArea: GeographicalAreaModel;
   readonly meditrakSyncQueue: MeditrakSyncQueueModel;
   readonly option: OptionModel;
