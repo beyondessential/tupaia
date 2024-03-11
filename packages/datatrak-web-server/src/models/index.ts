5f70e9c7a0424ccb2e20d20b39810f9f36bbd179
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-03-11 13:35:57 +1300

    Datatrak web user type

--- packages/datatrak-web-server/src/models/index.ts
+++ packages/datatrak-web-server/src/models/index.ts
@@ -7,5 +7,4 @@ export { DataTrakSessionModel, DataTrakSessionRecord } from './DataTrakSession';
 export { EntityModel } from './Entity';
 export { SurveyResponseModel } from './SurveyResponse';
 export { FeedItemModel } from './FeedItem';
-export { UserModel } from './User';
 export { SurveyModel } from './Survey';
--- packages/datatrak-web-server/src/types.ts
+++ packages/datatrak-web-server/src/types.ts
@@ -4,26 +4,24 @@
  */
 
 import {
-  ModelRegistry,
   EntityModel,
   EntityRecord as BaseEntityRecord,
   SurveyModel,
   OneTimeLoginModel,
   OneTimeLoginRecord as BaseOneTimeLoginRecord,
 } from '@tupaia/database';
-import { Model } from '@tupaia/server-boilerplate';
+import { Model, ServerBoilerplateModelRegistry } from '@tupaia/server-boilerplate';
 import { Entity, OneTimeLogin } from '@tupaia/types';
-import { FeedItemModel, SurveyResponseModel, UserModel } from './models';
+import { FeedItemModel, SurveyResponseModel } from './models';
 import { OptionModel } from '@tupaia/database';
 
 export type EntityRecord = BaseEntityRecord & Entity;
 export type OneTimeLoginRecord = BaseOneTimeLoginRecord & OneTimeLogin;
 
-export interface DatatrakWebServerModelRegistry extends ModelRegistry {
+export interface DatatrakWebServerModelRegistry extends ServerBoilerplateModelRegistry {
   readonly entity: Model<EntityModel, Entity, EntityRecord>;
   readonly surveyResponse: SurveyResponseModel;
   readonly feedItem: FeedItemModel;
-  readonly user: UserModel;
   readonly survey: SurveyModel;
   readonly oneTimeLogin: Model<OneTimeLoginModel, OneTimeLogin, OneTimeLoginRecord>;
   readonly option: OptionModel;
