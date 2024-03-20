8ebd7a762cfda34d8a2f1b79c51cb1277638aca2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-03-21 11:15:36 +1300

    remove `/export/surveyDataDownload` endpoint

--- packages/web-config-server/src/apiV1/index.js
+++ packages/web-config-server/src/apiV1/index.js
@@ -15,12 +15,7 @@ import {
   appVerifyEmail,
 } from '/appServer';
 import { login, logout, oneTimeLogin } from '/authSession';
-import {
-  exportChartHandler,
-  ExportSurveyDataHandler,
-  ExportSurveyResponsesHandler,
-  PDFExportHandler,
-} from '/export';
+import { exportChartHandler, ExportSurveyResponsesHandler, PDFExportHandler } from '/export';
 import { getUser } from './getUser';
 import MeasuresHandler from './measures';
 import MeasuresDataHandler from './measureData';
@@ -49,7 +44,6 @@ export const getRoutesForApiV1 = () => {
   api.post('/resendEmail', catchAsyncErrors(appResendEmail())); // KEEP
   api.get('/export/chart', catchAsyncErrors(exportChartHandler)); // KEEP
   api.get('/export/surveyResponses', handleWith(ExportSurveyResponsesHandler));
-  api.get('/export/surveyDataDownload', handleWith(ExportSurveyDataHandler)); // CULL
   api.get('/measures', handleWith(MeasuresHandler)); // KEEP
   api.get('/measureData', handleWith(MeasuresDataHandler)); // KEEP
   api.get('/projects', catchAsyncErrors(getProjects)); // KEEP
