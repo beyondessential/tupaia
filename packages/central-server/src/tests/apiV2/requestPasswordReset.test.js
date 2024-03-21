75909f5da4e42de8f33e32e2233768b97803eb93
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-03-22 08:06:41 +1300

    Remove only test

--- packages/central-server/src/tests/apiV2/requestPasswordReset.test.js
+++ packages/central-server/src/tests/apiV2/requestPasswordReset.test.js
@@ -10,7 +10,7 @@ import { createBearerHeader, randomEmail, randomString } from '@tupaia/utils';
 
 import { getAuthorizationHeader, TestableApp } from '../testUtilities';
 
-describe.only('Reset Password', () => {
+describe('Reset Password', () => {
   const app = new TestableApp();
   const { models } = app;
   const { VERIFIED } = models.user.emailVerifiedStatuses;
