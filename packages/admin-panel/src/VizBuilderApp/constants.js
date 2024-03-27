0dd952e4c23fc26f593a034f1dca55d8d33187fd
Rohan Port <rohan@bes.au>
2024-03-28 10:27:01 +1100

    no-issue: Fixed initial config matches for matrix and map overlays

--- packages/admin-panel/src/VizBuilderApp/constants.js
+++ packages/admin-panel/src/VizBuilderApp/constants.js
@@ -168,8 +168,13 @@ export const DASHBOARD_ITEM_VIZ_TYPES = {
   MATRIX: {
     name: 'Matrix',
     schema: MatrixVizBuilderConfigSchema,
+    vizMatchesType: viz => viz.type === 'matrix',
     initialConfig: {
       type: 'matrix',
+      output: {
+        type: 'matrix',
+        rowField: '',
+      },
     },
   },
 
@@ -185,6 +190,7 @@ export const MAP_OVERLAY_VIZ_TYPES = {
   ICON: {
     name: 'Icon',
     schema: IconMapOverlayConfigSchema,
+    vizMatchesType: viz => viz.displayType === 'icon',
     initialConfig: {
       displayType: 'icon',
       icon: 'pin',
@@ -214,6 +220,7 @@ export const MAP_OVERLAY_VIZ_TYPES = {
   SPECTRUM: {
     name: 'Spectrum',
     schema: SpectrumMapOverlayConfigSchema,
+    vizMatchesType: viz => viz.displayType === 'shaded-spectrum',
     initialConfig: {
       displayType: 'shaded-spectrum',
       scaleColorScheme: 'default',
