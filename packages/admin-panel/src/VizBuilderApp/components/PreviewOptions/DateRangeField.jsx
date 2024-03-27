204d928b36fa5a15f69fcef9cac9c5ef605d50c8
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-03-28 09:27:28 +1300

    comment date correcting helper function

--- packages/admin-panel/src/VizBuilderApp/components/PreviewOptions/DateRangeField.jsx
+++ packages/admin-panel/src/VizBuilderApp/components/PreviewOptions/DateRangeField.jsx
@@ -40,18 +40,37 @@ export const DateRangeField = () => {
     }
   }, [defaultStartDate, startDate, defaultEndDate, endDate]);
 
-  const convertDateToIsoString = date =>
-    !date || isNaN(new Date(date).getTime()) ? null : date.toISOString().slice(0, 10);
-  const shiftEpoch = date => date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  /**
+   * When you select a date in the date picker (either with the interactive calendar or by inserting
+   * a valid date string), the picker actually selects a specific moment in time on that date with
+   * the normal, millisecond granularity of a Date object. The time of day it selects is the current
+   * local time on the system where VizBuilder is running.
+   *
+   * When inputting the date, the date picker interprets it in the user’s time zone. Under the hood,
+   * this is converted to UTC. But since we only need day-level granularity, this timezone
+   * conversion can make the date picker select a date different from what the user input (±1 day)
+   * when {@link converDateToIsoString} discards the timestamp.
+   *
+   * This helper function accounts for that discrepancy.
+   *
+   * @param date A valid date object
+   * @returns {Date}
+   */
+  const shiftEpoch = date =>
+    new Date(date.setMinutes(date.getMinutes() - date.getTimezoneOffset()));
+
+  const convertDateToIsoString = date => {
+    if (!date || isNaN(new Date(date).getTime())) return null;
+    const correctedDate = shiftEpoch(date);
+    return correctedDate.toISOString().slice(0, 10);
+  };
 
   const handleChangeStartDate = date => {
-    shiftEpoch(date);
     const newDate = convertDateToIsoString(date);
     setStartDate(newDate);
   };
 
   const handleChangeEndDate = date => {
-    shiftEpoch(date);
     const newDate = convertDateToIsoString(date);
     setEndDate(newDate);
   };
