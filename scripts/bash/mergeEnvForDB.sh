b5705a40220efbd91ef2b6e190a15e2119bb12b0
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-04-02 10:15:43 +1300

    Get script paths

--- scripts/bash/mergeEnvForDB.sh
+++ scripts/bash/mergeEnvForDB.sh
@@ -1,17 +1,39 @@
 #!/bin/bash -e 
- 
 
-DIR=$(pwd "$0")
+# Function to get the directory of the package that's calling this script
+get_caller_package_directory() {
+    local dir
+    dir=$(dirname "$(readlink -f "$0")")
+    while [[ "$dir" != "/" ]]; do
+        if [[ -f "$dir/package.json" ]]; then
+            echo "$dir"
+            return
+        fi
+        dir=$(dirname "$dir")
+    done
+}
+
+# Get the directory of the package that's calling this script
+CALLING_SCRIPT_DIR=$(get_caller_package_directory)
+ 
+# Get the directory of this script
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Fixed paths to the .env files for the test db
-file1="../../env/db.env"
-file2="../../env/pg.env"
-file3="../../env/data-lake.env"
-file4="$DIR/.env"
- 
+file1="$CURRENT_DIR/../../env/db.env"
+file2="$CURRENT_DIR/../../env/pg.env"
+file3="$CURRENT_DIR/../../env/data-lake.env"
+file4="$CALLING_SCRIPT_DIR/.env"
+
+common_files="$file1 $file2 $file3"
+
+# Check if the current package has a .env file
+if [ -f "$file4" ]; then
+    common_files+=" $file4"
+fi
 
 # Load environment variables from .env files
-merged_content="$(cat "$file1" "$file2" "$file3" "$file4")"
+merged_content="$(cat $common_files)" 
 
 # Process command line arguments, overwriting values if present
 for var in $(env); do
