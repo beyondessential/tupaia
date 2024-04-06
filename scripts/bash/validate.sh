b2523f28e09f189d18ff30d2833ec0c6be38c9d0
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-06 16:07:05 +1300

    double-quote path

--- scripts/bash/validate.sh
+++ scripts/bash/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 DIR=$(dirname "$0")
-. ${DIR}/../../packages/devops/scripts/ci/utils.sh
+. "$DIR/../../packages/devops/scripts/ci/utils.sh"
 
 yarn workspace @tupaia/devops validate-branch-name
 yarn workspace @tupaia/devops validate-tests
