1160b28d04e21cb99a0c3f507e16c121c288b92f
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-07 23:58:01 +1200

    prefer array over string in `buildNonInternalDeps`

--- scripts/bash/buildNonInternalDependencies.sh
+++ scripts/bash/buildNonInternalDependencies.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 set -ex
 
-PACKAGES="report-server admin-panel-server central-server data-table-server datatrak-web datatrak-web-server entity-server lesmis lesmis-server meditrak-app-server psss psss-server web-config-server tupaia-web tupaia-web-server"
+PACKAGES=(
+    'report-server'
+    'admin-panel-server'
+    'central-server'
+    'data-table-server'
+    'datatrak-web'
+    'datatrak-web-server'
+    'entity-server'
+    'lesmis'
+    'lesmis-server'
+    'meditrak-app-server'
+    'psss'
+    'psss-server'
+    'web-config-server'
+    'tupaia-web'
+    'tupaia-web-server'
+)
 
 CONCURRENT_BUILD_BATCH_SIZE=1
 
@@ -9,9 +25,9 @@ build_commands=()
 build_prefixes=()
 
 # Build dependencies
-for PACKAGE in $PACKAGES; do
-    build_commands+=("\"yarn workspace @tupaia/${PACKAGE} build\"")
-    build_prefixes+=("${PACKAGE},")
+for PACKAGE in "${PACKAGES[@]}"; do
+    build_commands+=("\"yarn workspace @tupaia/$PACKAGE build\"")
+    build_prefixes+=("$PACKAGE,")
 done
 
 eval "yarn concurrently -m $CONCURRENT_BUILD_BATCH_SIZE --names \"${build_prefixes[*]}\" -k ${build_commands[*]}"
