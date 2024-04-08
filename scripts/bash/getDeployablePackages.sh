0ba73f16e11b16a5429315e7ddc6da44e7604e61
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-08 21:17:38 +1200

    prefer array for `getDeployablePackages.sh`

--- scripts/bash/getDeployablePackages.sh
+++ scripts/bash/getDeployablePackages.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-echo 'admin-panel' \
-    'lesmis' \
-    'psss' \
-    'datatrak-web' \
-    'tupaia-web' \
-    'central-server' \
-    'data-table-server' \
-    'datatrak-web-server' \
-    'entity-server' \
-    'lesmis-server' \
-    'meditrak-app-server' \
-    'psss-server' \
-    'report-server' \
-    'tupaia-web-server' \
-    'web-config-server' \
+DEPLOYABLE_PACKAGES=(
+    'admin-panel'
+    'lesmis'
+    'psss'
+    'datatrak-web'
+    'tupaia-web'
+    'central-server'
+    'data-table-server'
+    'datatrak-web-server'
+    'entity-server'
+    'lesmis-server'
+    'meditrak-app-server'
+    'psss-server'
+    'report-server'
+    'tupaia-web-server' 
+    'web-config-server' 
     'admin-panel-server' # admin-panel-server last as it depends on report-server
+)
+echo "${DEPLOYABLE_PACKAGES[@]}"
 
 exit 0
--- scripts/bash/getPackagesWithEnvFiles.sh
+++ scripts/bash/getPackagesWithEnvFiles.sh
@@ -4,7 +4,8 @@ set -e
 DIR=$(dirname "$0")
 
 # packages with .env files are (currently) all deployable, plus auth, data-api, and database
-PACKAGES=$(${DIR}/getDeployablePackages.sh)
-PACKAGES+=" data-api"
-echo $PACKAGES
+PACKAGES=$("$DIR/getDeployablePackages.sh")
+PACKAGES+=('data-api')
+echo "${PACKAGES[@]}"
+
 exit 0
