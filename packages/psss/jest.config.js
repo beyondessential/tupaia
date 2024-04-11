9cc763054dfe22d2ce69e5dfb4d4f496ecfde4ed
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-11 14:15:02 +1200

    Revert "deps: RN-1112: bump `axios` to ≥0.28 (#5493)"
    This reverts commit 7e454c00560cf1dc765439fce1488cff9b7c40ca.

--- packages/psss/jest.config.js
+++ packages/psss/jest.config.js
@@ -8,10 +8,4 @@ const baseConfig = require('../../jest.config-js.json');
 module.exports = async () => ({
   ...baseConfig,
   rootDir: '.',
-  /**
-   * Since 1.0.0, Axios switched from emitting a CommonJS module to ECMAScript, which causes errors
-   * with Jest. This is a workaround.
-   * @see https://github.com/axios/axios/issues/5026
-   */
-  transformIgnorePatterns: ['node_modules/(?!axios)'],
 });
