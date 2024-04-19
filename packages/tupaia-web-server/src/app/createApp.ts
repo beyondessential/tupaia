7cb2f97750fb0c04de61ed636006f6c059531c86
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-19 15:43:27 +1200

    fix `dashboards` and `projects` endpoints

--- packages/tupaia-web-server/src/app/createApp.ts
+++ packages/tupaia-web-server/src/app/createApp.ts
@@ -95,8 +95,10 @@ export async function createApp(db: TupaiaDatabase = new TupaiaDatabase()) {
     )
     .use('downloadFiles', forwardRequest(CENTRAL_API_URL, { authHandlerProvider }))
     // Forward everything else to webConfigApi
+    .use('dashboards', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }))
     .use('landingPage/:landingPageUrl', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }))
     .use('login/oneTimeLogin', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }))
+    .use('projects', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }))
     .use('resendEmail', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }))
     .use('resetPassword', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }))
     .use('signup', forwardRequest(WEB_CONFIG_API_URL, { authHandlerProvider }));
