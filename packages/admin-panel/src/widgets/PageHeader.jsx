069e6c153e09afbdd95dde3cca7d6ce6b02f7e5c
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-29 13:52:13 +1200

    organise imports

--- packages/admin-panel/src/widgets/PageHeader.jsx
+++ packages/admin-panel/src/widgets/PageHeader.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { ImportModal } from '../importExport';
-import { CreateButton as SingleCreateButton, BulkCreateButton } from '../editor';
+import { BulkCreateButton, CreateButton as SingleCreateButton } from '../editor';
 
 const Container = styled.div`
   display: flex;
