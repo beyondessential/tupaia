944b4dd29a373c7dd9d1f26a5a434273dbdf1771
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-05-07 09:26:49 +1200

    fix(database): Revert change to database count method

--- packages/central-server/src/apiV2/mapOverlayGroups/GETMapOverlayGroups.js
+++ packages/central-server/src/apiV2/mapOverlayGroups/GETMapOverlayGroups.js
@@ -32,31 +32,14 @@ export class GETMapOverlayGroups extends GETHandler {
     return mapOverlayGroup;
   }
 
-  async getPermissionsFilter(criteria, options = {}) {
+  async getPermissionsFilter(criteria, dbOptions = {}) {
     const dbConditions = await createMapOverlayGroupDBFilter(
       this.accessPolicy,
       this.models,
       criteria,
     );
 
-    const dbOptions = { ...options };
-
     // Join with map_overlay_group_relation so that we can also fetch map overlay groups with no children so they can be displayed and edited
-    dbOptions.multiJoin = mergeMultiJoin(
-      [
-        {
-          joinWith: RECORDS.MAP_OVERLAY_GROUP_RELATION,
-          joinType: JOIN_TYPES.LEFT,
-          joinCondition: [
-            `${RECORDS.MAP_OVERLAY_GROUP}.id`,
-            `${RECORDS.MAP_OVERLAY_GROUP_RELATION}.map_overlay_group_id`,
-          ],
-        },
-      ],
-      dbOptions?.multiJoin,
-    );
-    // should be distinct to avoid duplicates that are caused by the multiJoin with map_overlay_group_relation
-    dbOptions.distinct = true;
     return { dbConditions, dbOptions };
   }
 }
