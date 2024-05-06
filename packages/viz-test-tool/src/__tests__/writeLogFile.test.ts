c662c8bb43a0aa5d6a4501e45994d1981bfc9aae
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2024-05-06 11:51:29 +1000

    feat(vizTestTool): RN-1269: Add viz-test-tool e2e testing of vizes (#5556)

---
+++ packages/viz-test-tool/src/__tests__/writeLogFile.test.ts
@@ -0,0 +1,45 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
+ */
+
+import { LOGS_DIR, writeLogFile } from '../writeLogFile';
+
+const mockFileSystem: Record<string, string> = {};
+
+jest.mock('fs', () => ({
+  writeFileSync: (filePath: string, fileContents: string) =>
+    (mockFileSystem[filePath] = fileContents),
+  existsSync: () => true,
+}));
+
+const date = new Date('2020-01-01');
+jest.useFakeTimers().setSystemTime(date);
+
+describe('writeLogFile', () => {
+  it('writes the log file', () => {
+    const results = {
+      successes: 7,
+      errors: ["bad_report - Oh no! Where's my tooth brush!?"],
+      skipped: ['skipped_report'],
+      total: 9,
+    };
+
+    writeLogFile(results);
+
+    const logFileName = `${LOGS_DIR}/results_${date.getTime()}.log`;
+    const logFile = mockFileSystem[logFileName];
+
+    expect(logFile).toEqual(
+      `successes: 7
+
+errors:
+  bad_report - Oh no! Where's my tooth brush!?
+
+skipped:
+  skipped_report
+
+`,
+    );
+  });
+});
