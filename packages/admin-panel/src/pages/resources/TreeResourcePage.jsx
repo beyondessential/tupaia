47f3f137b5e2837a8214904adfac7f5d1cf9a8d6
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-09 15:31:35 +1200

    resource name in `TreeResourcePage`

--- packages/admin-panel/src/pages/resources/TreeResourcePage.jsx
+++ packages/admin-panel/src/pages/resources/TreeResourcePage.jsx
@@ -7,9 +7,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { HorizontalTree } from '@tupaia/ui-components';
-import { PageHeader, PageBody } from '../../widgets';
+import { PageBody, PageHeader } from '../../widgets';
 import { LogsModal } from '../../logsTable';
 import * as COLORS from '../../theme/colors';
+import { generateTitle } from './resourceName';
 
 const Container = styled(PageBody)`
   overflow: auto;
@@ -27,11 +28,20 @@ const StyledHorizontalTree = styled(HorizontalTree)`
   }
 `;
 
-export const TreeResourcePage = ({ title, fetchRoot, fetchBranch, ExportModalComponent }) => {
+export const TreeResourcePage = ({
+  resourceName,
+  title,
+  fetchRoot,
+  fetchBranch,
+  ExportModalComponent,
+}) => {
   return (
     <>
       <Container>
-        <PageHeader title={title} ExportModalComponent={ExportModalComponent} />
+        <PageHeader
+          title={title ?? generateTitle(resourceName)}
+          ExportModalComponent={ExportModalComponent}
+        />
         <StyledHorizontalTree fetchRoot={fetchRoot} fetchBranch={fetchBranch} />
       </Container>
       <LogsModal />
@@ -40,10 +50,15 @@ export const TreeResourcePage = ({ title, fetchRoot, fetchBranch, ExportModalCom
 };
 
 TreeResourcePage.defaultProps = {
+  resourceName: {},
   ExportModalComponent: null,
 };
 
 TreeResourcePage.propTypes = {
+  resourceName: PropTypes.shape({
+    singular: PropTypes.string.isRequired,
+    plural: PropTypes.string,
+  }),
   title: PropTypes.string.isRequired,
   fetchRoot: PropTypes.func.isRequired,
   fetchBranch: PropTypes.func.isRequired,
