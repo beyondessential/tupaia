378896222d8a32de931ad2ca43e45b3b75f75eff
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-10 15:45:11 +1200

    support multiple attributes

--- packages/central-server/src/apiV2/export/exportOptionSet/exportOptionSet.js
+++ packages/central-server/src/apiV2/export/exportOptionSet/exportOptionSet.js
@@ -7,18 +7,28 @@ import xlsx from 'xlsx';
 import { respondWithDownload, toFilename } from '@tupaia/utils';
 import { getExportPathForUser } from '../getExportPathForUser';
 
+const objectToHumanReadableKeyValuePairs = obj => {
+  const entries = [];
+  for (const [key, value] of Object.entries(obj)) {
+    entries.push(`${key}: ${value}`);
+  }
+  return entries.join('\n');
+};
+
 export async function exportOptionSet(req, res) {
   const { models, userId } = req;
   const { optionSetId } = req.params;
 
   const optionSetRecord = await models.optionSet.findOne({ id: optionSetId });
   const optionRecords = await optionSetRecord.options();
-  const options = optionRecords.map(option => ({
-    value: option.value,
-    label: option.label,
-    sort_order: option.sort_order,
-    attributes: option.attributes,
-  }));
+  const options = optionRecords.map(option => {
+    return {
+      value: option.value,
+      label: option.label,
+      sort_order: option.sort_order,
+      attributes: objectToHumanReadableKeyValuePairs(option.attributes),
+    };
+  });
 
   const worksheet = xlsx.utils.json_to_sheet(options);
   const sheetName = optionSetRecord.name.substring(0, 31); // Sheet name max. length is hard-coded in Excel
