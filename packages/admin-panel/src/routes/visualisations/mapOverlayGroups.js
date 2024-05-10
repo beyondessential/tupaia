1d92495ac836204fdde78e34f184591b11d51bcf
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-10 16:43:31 +1200

    update copyright messages

--- packages/admin-panel/src/routes/visualisations/mapOverlayGroups.js
+++ packages/admin-panel/src/routes/visualisations/mapOverlayGroups.js
@@ -1,6 +1,6 @@
 /*
  * Tupaia
- * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
+ * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
  */
 
 const RESOURCE_NAME = { singular: 'map overlay group' };
