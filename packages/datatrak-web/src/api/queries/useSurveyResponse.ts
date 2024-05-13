6a8058f8dcbcf2094df3d94f272e681d0cb7d8a2
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-05-14 11:29:31 +1200

    Rearrange routes

--- packages/datatrak-web/src/api/queries/useSurveyResponse.ts
+++ packages/datatrak-web/src/api/queries/useSurveyResponse.ts
@@ -1,6 +1,6 @@
 /*
  * Tupaia
- *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
+ *  Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
  */
 import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router';
@@ -8,9 +8,12 @@ import { DatatrakWebSingleSurveyResponseRequest } from '@tupaia/types';
 import { get } from '../api';
 import { ROUTES } from '../../constants';
 import { errorToast } from '../../utils';
+import { useSurveyForm } from '../../features';
 
 export const useSurveyResponse = (surveyResponseId?: string) => {
+  const { setFormData } = useSurveyForm();
   const navigate = useNavigate();
+
   return useQuery(
     ['surveyResponse', surveyResponseId],
     (): Promise<DatatrakWebSingleSurveyResponseRequest.ResBody> =>
@@ -24,6 +27,15 @@ export const useSurveyResponse = (surveyResponseId?: string) => {
         if (error.code === 403)
           return navigate(ROUTES.NOT_AUTHORISED, { state: { errorMessage: error.message } });
         errorToast(error.message);
+      },
+      onSuccess: data => {
+        // handle updating answers here - if this is done in the component, the answers get reset on every re-render
+        const formattedAnswers = Object.entries(data.answers).reduce((acc, [key, value]) => {
+          // If the value is a stringified object, parse it
+          const isStringifiedObject = typeof value === 'string' && value.startsWith('{');
+          return { ...acc, [key]: isStringifiedObject ? JSON.parse(value) : value };
+        }, {});
+        setFormData(formattedAnswers);
       },
     },
   );
