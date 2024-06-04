3933f2c44f65ba64958fcb778c2a23481541143c
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-06-05 10:48:45 +1200

    Add default end date to params for querying data

--- packages/tupaia-web/src/api/queries/useReport.ts
+++ packages/tupaia-web/src/api/queries/useReport.ts
@@ -3,7 +3,7 @@
  * Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  *
  */
-import { Moment } from 'moment';
+import moment, { Moment } from 'moment';
 import { useQuery } from 'react-query';
 import { formatDateForApi, getBrowserTimeZone } from '@tupaia/utils';
 import { TupaiaWebReportRequest } from '@tupaia/types';
@@ -23,9 +23,11 @@ type QueryParams = Record<string, unknown> & {
 export const useReport = (reportCode: DashboardItem['reportCode'], params: QueryParams) => {
   const { dashboardCode, projectCode, entityCode, itemCode, startDate, endDate, legacy, ...rest } =
     params;
+  const today = moment();
+  const endDateToUse = endDate ?? today; // default to today if no end date is provided, so that the default end date is always in the user's timezone, not UTC
   const timeZone = getBrowserTimeZone();
   const formattedStartDate = formatDateForApi(startDate, null);
-  const formattedEndDate = formatDateForApi(endDate, null);
+  const formattedEndDate = formatDateForApi(endDateToUse, null);
   const endPoint = legacy ? 'legacyDashboardReport' : 'report';
   return useQuery(
     [
