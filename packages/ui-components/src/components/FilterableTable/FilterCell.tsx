4da82238c176670e43a0d71ed390460cfba880f1
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-06-20 13:56:28 +1200

    Move useDebounce to ui-components for reuse

--- packages/ui-components/src/components/FilterableTable/FilterCell.tsx
+++ packages/ui-components/src/components/FilterableTable/FilterCell.tsx
@@ -9,6 +9,7 @@ import { StandardTextFieldProps } from '@material-ui/core';
 import { ColumnInstance } from 'react-table';
 import { TextField } from '../Inputs';
 import { HeaderDisplayCell, HeaderDisplayCellProps } from './Cells';
+import { useDebounce } from '../../hooks';
 
 const FilterWrapper = styled.div`
   .MuiFormControl-root {
@@ -68,32 +69,6 @@ const DefaultFilterInput = styled(TextField)`
     color: ${({ theme }) => theme.palette.divider};
   }
 `;
-
-// see https://github.com/tannerlinsley/react-query/issues/293
-// see https://usehooks.com/useDebounce/
-export function useDebounce(value: string, delay: number) {
-  // State and setters for debounced value
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(
-    () => {
-      // Update debounced value after delay
-      const handler = setTimeout(() => {
-        setDebouncedValue(value);
-      }, delay);
-
-      // Cancel the timeout if value changes (also on delay change or unmount)
-      // This is how we prevent debounced value from updating if value is changed ...
-      // .. within the delay period. Timeout gets cleared and restarted.
-      return () => {
-        clearTimeout(handler);
-      };
-    },
-    [value, delay], // Only re-call effect if value or delay changes
-  );
-
-  return debouncedValue;
-}
 
 interface DefaultFilterProps extends Omit<StandardTextFieldProps, 'onChange' | 'value'> {
   value?: string | null;
