34ae8d3f13079f28364a05594c40877b8ebb6b62
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-06-25 09:05:18 +1200

    refactor(adminPanel): RN-1335: Move filterable table into ui-components (#5733)
    * Add migration for table
    
    * Remove status type if exists
    
    * Create task db model
    
    * Add to db exports
    
    * Generate types
    
    * Fix migrate-down
    
    * Get tasks endpoint
    
    * Create task endpoint
    
    * WIP edit tasks
    
    * Edit task endpoint
    
    * Heading and route
    
    * Add alignment for children
    
    * Allow assignee to be empty
    
    * Don't cascade user accounts
    
    * Generate types
    
    * Tasks route
    
    * Request tasks
    
    * WIP table
    
    * Working table
    
    * Working table
    
    * Fix error type
    
    * Remove unused actions
    
    * Fix sorting
    
    * Undo unneeded changes
    
    * revert
    
    * Revert "Tasks route"
    
    This reverts commit 1511c374923817b7af7808d57b70888cbb0a88f0.
    
    * Revert "Add alignment for children"
    
    This reverts commit ff86345bdc9770b1c9dd552b8271906d05e77ac1.
    
    * Revert "Heading and route"
    
    This reverts commit bbbc6aeec3a7db440bab74e9a111ca2ca8be00d6.
    
    * Remove extra file
    
    * Only show filter row when applicable
    
    * Add assignee name field for easy filtering and indexes
    
    * Generate types
    
    * Change schema for repeating
    
    * Fix tests
    
    * Fix tests
    
    * Make due date nullable
    
    * Rename repeat_schedule field
    
    * Fix tests
    
    * Fix validation rules
    
    * Link status and assignee name to records at the model level
    
    * Generate types

--- packages/admin-panel/src/table/DataFetchingTable/Cells.jsx
+++ packages/admin-panel/src/table/DataFetchingTable/Cells.jsx
@@ -5,45 +5,8 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
-import { TableCell as MuiTableCell } from '@material-ui/core';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-const Cell = styled(MuiTableCell)`
-  font-size: 0.75rem;
-  padding: 0;
-  border: none;
-  position: relative;
-  &:first-child {
-    padding-inline-start: 1.5rem;
-  }
-  &:last-child {
-    padding-inline-end: 1rem;
-  }
-`;
-
-const CellContentWrapper = styled.div`
-  padding: 0.7rem;
-  ${({ $isButtonColumn }) =>
-    $isButtonColumn &&
-    css`
-      padding-inline: 0;
-      padding-block: 0;
-      text-align: center;
-    `}
-  height: 100%;
-  display: flex;
-  align-items: center;
-
-  tr:not(:last-child) & {
-    border-bottom: 1px solid ${({ theme }) => theme.palette.grey[400]};
-  }
-  td:first-child & {
-    padding-inline-start: 0.2rem;
-  }
-
-  line-height: 1.5;
-`;
 
 // Flex does not support ellipsis so we need to have another container to handle the ellipsis
 const CellContentContainer = styled.div`
@@ -52,84 +15,6 @@ const CellContentContainer = styled.div`
   text-overflow: ellipsis;
   width: 100%;
 `;
-
-const HeaderCell = styled(Cell)`
-  color: ${({ theme }) => theme.palette.text.secondary};
-  font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-  background-color: ${({ theme }) => theme.palette.background.paper};
-  border-bottom: 1px solid ${({ theme }) => theme.palette.grey[400]};
-  padding-block: 0.7rem;
-  padding-inline: 0.7rem 0;
-  display: flex;
-  position: initial; // override this because we have 2 sticky header rows so we will apply sticky to the thead element
-  background-color: ${({ theme }) => theme.palette.background.paper};
-  .MuiTableSortLabel-icon {
-    opacity: 0.5;
-  }
-  .MuiTableSortLabel-active .MuiTableSortLabel-icon {
-    opacity: 1;
-  }
-`;
-
-const ColResize = styled.div.attrs({
-  onClick: e => {
-    // suppress other events when resizing
-    e.preventDefault();
-    e.stopPropagation();
-  },
-})`
-  width: 2rem;
-  height: 100%;
-  cursor: col-resize;
-`;
-
-export const HeaderDisplayCell = ({ children, canResize, getResizerProps, ...props }) => {
-  return (
-    <HeaderCell {...props} $canResize={canResize}>
-      <CellContentContainer> {children}</CellContentContainer>
-      {canResize && <ColResize {...getResizerProps()} />}
-    </HeaderCell>
-  );
-};
-
-HeaderDisplayCell.propTypes = {
-  children: PropTypes.node,
-  width: PropTypes.string,
-  canResize: PropTypes.bool,
-  getResizerProps: PropTypes.func,
-};
-
-HeaderDisplayCell.defaultProps = {
-  width: null,
-  children: null,
-  canResize: false,
-  getResizerProps: () => {},
-};
-
-export const TableCell = ({ children, width, isButtonColumn, url, ...props }) => {
-  return (
-    <Cell $isButtonColumn={isButtonColumn} {...props}>
-      <CellContentWrapper $width={width} $isButtonColumn={isButtonColumn}>
-        <CellContentContainer to={url} as={url ? CellLink : 'div'} $isButtonColumn={isButtonColumn}>
-          {children}
-        </CellContentContainer>
-      </CellContentWrapper>
-    </Cell>
-  );
-};
-
-TableCell.propTypes = {
-  children: PropTypes.node.isRequired,
-  width: PropTypes.number,
-  isButtonColumn: PropTypes.bool,
-  url: PropTypes.string,
-};
-
-TableCell.defaultProps = {
-  width: null,
-  isButtonColumn: false,
-  url: null,
-};
 
 const CellLink = styled(Link)`
   color: inherit;
@@ -162,7 +47,6 @@ export const DisplayCell = ({
   getNestedViewLink,
   basePath,
   isButtonColumn,
-  ...props
 }) => {
   const generateLink = () => {
     if (isButtonColumn || (!detailUrl && !getNestedViewLink)) return null;
@@ -176,9 +60,9 @@ export const DisplayCell = ({
   };
   const url = generateLink();
   return (
-    <TableCell {...props} isButtonColumn={isButtonColumn} url={url}>
+    <CellContentContainer to={url} as={url ? CellLink : 'div'} $isButtonColumn={isButtonColumn}>
       {children}
-    </TableCell>
+    </CellContentContainer>
   );
 };
 
