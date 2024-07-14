be079b27742fdc8ae8bf2094a2ca3ea2d823ed66
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-07-15 11:04:18 +1200

    Update schemas.ts

--- packages/types/src/schemas/schemas.ts
+++ packages/types/src/schemas/schemas.ts
@@ -87653,6 +87653,15 @@ export const TaskResponseSchema = {
 	"additionalProperties": false,
 	"type": "object",
 	"properties": {
+		"id": {
+			"type": "string"
+		},
+		"entityId": {
+			"type": "string"
+		},
+		"surveyId": {
+			"type": "string"
+		},
 		"assigneeId": {
 			"type": "string"
 		},
@@ -87884,253 +87893,12 @@ export const TaskResponseSchema = {
 				"valueOf"
 			]
 		},
-		"dueDate": {
-			"type": "object",
-			"properties": {
-				"toString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toDateString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toTimeString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toLocaleString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toLocaleDateString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toLocaleTimeString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"valueOf": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getTime": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getFullYear": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCFullYear": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getMonth": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCMonth": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getDate": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCDate": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getDay": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCDay": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getHours": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCHours": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getMinutes": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCMinutes": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getSeconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCSeconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getMilliseconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getUTCMilliseconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getTimezoneOffset": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setTime": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setMilliseconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCMilliseconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setSeconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCSeconds": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setMinutes": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCMinutes": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setHours": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCHours": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setDate": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCDate": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setMonth": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCMonth": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setFullYear": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"setUTCFullYear": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toUTCString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toISOString": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"toJSON": {
-					"type": "object",
-					"additionalProperties": false
-				},
-				"getVarDate": {
-					"type": "object",
-					"additionalProperties": false
-				}
-			},
-			"additionalProperties": false,
-			"required": [
-				"getDate",
-				"getDay",
-				"getFullYear",
-				"getHours",
-				"getMilliseconds",
-				"getMinutes",
-				"getMonth",
-				"getSeconds",
-				"getTime",
-				"getTimezoneOffset",
-				"getUTCDate",
-				"getUTCDay",
-				"getUTCFullYear",
-				"getUTCHours",
-				"getUTCMilliseconds",
-				"getUTCMinutes",
-				"getUTCMonth",
-				"getUTCSeconds",
-				"getVarDate",
-				"setDate",
-				"setFullYear",
-				"setHours",
-				"setMilliseconds",
-				"setMinutes",
-				"setMonth",
-				"setSeconds",
-				"setTime",
-				"setUTCDate",
-				"setUTCFullYear",
-				"setUTCHours",
-				"setUTCMilliseconds",
-				"setUTCMinutes",
-				"setUTCMonth",
-				"setUTCSeconds",
-				"toDateString",
-				"toISOString",
-				"toJSON",
-				"toLocaleDateString",
-				"toLocaleString",
-				"toLocaleTimeString",
-				"toString",
-				"toTimeString",
-				"toUTCString",
-				"valueOf"
-			]
-		},
-		"entityId": {
-			"type": "string"
-		},
-		"id": {
-			"type": "string"
-		},
-		"repeatSchedule": {
-			"type": "object",
-			"additionalProperties": false
-		},
 		"status": {
 			"enum": [
 				"cancelled",
 				"completed",
 				"to_do"
 			],
-			"type": "string"
-		},
-		"surveyId": {
 			"type": "string"
 		},
 		"assigneeName": {
@@ -88185,6 +87953,19 @@ export const TaskResponseSchema = {
 				"id",
 				"name"
 			]
+		},
+		"dueDate": {
+			"type": "string",
+			"format": "date-time"
+		},
+		"repeatSchedule": {
+			"type": "object",
+			"properties": {
+				"frequency": {
+					"type": "string"
+				}
+			},
+			"additionalProperties": false
 		}
 	},
 	"required": [
