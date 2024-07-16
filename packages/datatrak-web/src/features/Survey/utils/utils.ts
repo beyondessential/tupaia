79fc7bb9fbe4cfb2009c55b426e94581a2a6fc0d
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-07-16 15:40:00 +1200

    Fix imports

--- packages/datatrak-web/src/features/Survey/utils/utils.ts
+++ packages/datatrak-web/src/features/Survey/utils/utils.ts
@@ -2,8 +2,8 @@
  * Tupaia
  *  Copyright (c) 2017 - 2023 Beyond Essential Systems Pty Ltd
  */
-import { QuestionType } from "@tupaia/types";
-import { SurveyScreen } from "../../types";
+import { QuestionType } from '@tupaia/types';
+import { SurveyScreen } from '../../../types';
 
 export const READ_ONLY_QUESTION_TYPES = [
   QuestionType.Condition,
@@ -15,42 +15,33 @@ export const getSurveyScreenNumber = (screens, screen) => {
   if (!screen) return null;
   const { surveyScreenComponents, id } = screen;
   const nonInstructionScreens =
-    screens?.filter((screen) =>
-      screen.surveyScreenComponents.some(
-        (component) => component.type !== QuestionType.Instruction
-      )
+    screens?.filter(screenItem =>
+      screenItem.surveyScreenComponents.some(
+        component => component.type !== QuestionType.Instruction,
+      ),
     ) ?? [];
 
   const screenNumber = surveyScreenComponents.some(
-    (component) => component.type !== QuestionType.Instruction
+    component => component.type !== QuestionType.Instruction,
   )
-    ? nonInstructionScreens.findIndex(
-        (nonInstructionScreen) => nonInstructionScreen.id === id
-      ) + 1
+    ? nonInstructionScreens.findIndex(nonInstructionScreen => nonInstructionScreen.id === id) + 1
     : null;
 
   return screenNumber;
 };
 
 export const getAllSurveyComponents = (surveyScreens?: SurveyScreen[]) => {
-  return (
-    surveyScreens
-      ?.map(({ surveyScreenComponents }) => surveyScreenComponents)
-      ?.flat() ?? []
-  );
+  return surveyScreens?.map(({ surveyScreenComponents }) => surveyScreenComponents)?.flat() ?? [];
 };
 
 export const getErrorsByScreen = (
   errors: Record<string, Record<string, string>>,
-  visibleScreens?: SurveyScreen[]
+  visibleScreens?: SurveyScreen[],
 ) => {
   return (
     Object.entries(errors).reduce((acc, [questionName, error]) => {
-      const screenIndex = visibleScreens?.findIndex(
-        ({ surveyScreenComponents }) =>
-          surveyScreenComponents.find(
-            (question) => question.questionId === questionName
-          )
+      const screenIndex = visibleScreens?.findIndex(({ surveyScreenComponents }) =>
+        surveyScreenComponents.find(question => question.questionId === questionName),
       );
 
       if (screenIndex === undefined || screenIndex === -1) return acc;
