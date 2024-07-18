212dfcc73a4d881141ed03f316008893e8e6907c
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-07-19 09:33:18 +1200

    feat(datatrakWeb): RN-1339: View completed survey response from task (#5789)
    * Create survey_response_id column
    
    * Update change handler
    
    * View completed survey
    
    * Error handling
    
    * Fix comment
    
    * fix tasks button
    
    * removeTaskFilterSetting on logout
    
    * PR fixes
    
    ---------
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>

--- packages/datatrak-web/src/views/Tasks/TaskDetailsPage.tsx
+++ packages/datatrak-web/src/views/Tasks/TaskDetailsPage.tsx
@@ -3,16 +3,18 @@
  * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { generatePath, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
 import { TaskStatus } from '@tupaia/types';
-import { SpinningLoader } from '@tupaia/ui-components';
+import { Modal, ModalCenteredContent, SpinningLoader } from '@tupaia/ui-components';
 import { Button } from '../../components';
 import { TaskDetails, TaskPageHeader, TaskActionsMenu } from '../../features';
 import { useTask } from '../../api';
 import { ROUTES } from '../../constants';
 import { useFromLocation } from '../../utils';
+import { Task } from '../../types';
 
 const ButtonWrapper = styled.div`
   display: flex;
@@ -20,12 +22,34 @@ const ButtonWrapper = styled.div`
   flex: 1;
 `;
 
-export const TaskDetailsPage = () => {
-  const { taskId } = useParams();
-  const { data: task, isLoading } = useTask(taskId);
+const ErrorModal = ({ isOpen, onClose }) => {
+  return (
+    <Modal
+      title="Error loading survey response"
+      isOpen={isOpen}
+      onClose={onClose}
+      buttons={[
+        {
+          text: 'Close',
+          onClick: onClose,
+          id: 'close',
+        },
+      ]}
+    >
+      <ModalCenteredContent>
+        <Typography>
+          This response has since been deleted in the admin panel. Please contact your system
+          administrator for further questions.
+        </Typography>
+      </ModalCenteredContent>
+    </Modal>
+  );
+};
 
-  const showCompleteButton =
-    task && task.taskStatus !== TaskStatus.completed && task.taskStatus !== TaskStatus.cancelled;
+const ButtonComponent = ({ task, openErrorModal }: { task?: Task; openErrorModal: () => void }) => {
+  const from = useFromLocation();
+
+  if (!task) return null;
 
   const surveyUrl = task
     ? generatePath(ROUTES.SURVEY_SCREEN, {
@@ -35,21 +59,43 @@ export const TaskDetailsPage = () => {
       })
     : '';
 
-  const from = useFromLocation();
+  if (task.taskStatus === TaskStatus.cancelled) return null;
+  if (task.taskStatus === TaskStatus.completed) {
+    if (!task.surveyResponseId)
+      return (
+        <Button onClick={openErrorModal} variant="outlined">
+          View completed survey
+        </Button>
+      );
+    return (
+      <Button to={`?responseId=${task.surveyResponseId}`} variant="outlined">
+        View completed survey
+      </Button>
+    );
+  }
+  return (
+    <Button to={surveyUrl} state={{ from }}>
+      Complete task
+    </Button>
+  );
+};
+
+export const TaskDetailsPage = () => {
+  const [errorModalOpen, setErrorModalOpen] = useState(false);
+  const { taskId } = useParams();
+  const { data: task, isLoading } = useTask(taskId);
+
   return (
     <>
       <TaskPageHeader title="Task details">
-        {showCompleteButton && (
-          <ButtonWrapper>
-            <Button to={surveyUrl} state={{ from }}>
-              Complete
-            </Button>
-            <TaskActionsMenu {...task} />
-          </ButtonWrapper>
-        )}
+        <ButtonWrapper>
+          <ButtonComponent task={task} openErrorModal={() => setErrorModalOpen(true)} />
+          {task && <TaskActionsMenu {...task} />}
+        </ButtonWrapper>
       </TaskPageHeader>
       {isLoading && <SpinningLoader />}
       {task && <TaskDetails task={task} />}
+      <ErrorModal isOpen={errorModalOpen} onClose={() => setErrorModalOpen(false)} />
     </>
   );
 };
