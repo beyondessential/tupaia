1b1fb78ee6b517953d6b905437fc6563e0a93912
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-08-14 12:54:50 +1200

    WIP rrule

---
+++ packages/utils/src/rrule.js
@@ -0,0 +1,65 @@
+/**
+ * Tupaia
+ * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
+ */
+import { RRule } from 'rrule';
+import { isLastDayOfMonth } from 'date-fns';
+
+export const generateDailyRRule = startDate => {
+  return new RRule({
+    freq: RRule.DAILY,
+    dtstart: startDate,
+    interval: 1,
+  });
+};
+
+export const generateWeeklyRRule = (startDate, daysOfWeek) => {
+  return new RRule({
+    freq: RRule.WEEKLY,
+    dtstart: startDate,
+    byweekday: daysOfWeek,
+  });
+};
+
+export const generateMonthlyRRule = startDate => {
+  const dayOfMonth = startDate.getDate();
+
+  if (dayOfMonth <= 28) {
+    return new RRule({
+      freq: RRule.MONTHLY,
+      dtstart: startDate,
+      interval: 1,
+      bymonthday: [dayOfMonth],
+    });
+  }
+
+  // If the day of the month is the last day of the month, return a rule that will be applied to the last day of the month every month
+  if (isLastDayOfMonth(startDate)) {
+    return new RRule({
+      freq: RRule.MONTHLY,
+      dtstart: startDate,
+      interval: 1,
+      bymonthday: [-1],
+    });
+  }
+
+  // Get the days of the month to check - if the given date is the 30th, this will include 28, 29, 30. If the given date is the 29th, this will include 28, 29
+  const bymonthday = dayOfMonth === 30 ? [28, 29, 30] : [28, 29];
+
+  return new RRule({
+    freq: RRule.MONTHLY,
+    dtstart: startDate,
+    interval: 1,
+    bymonthday,
+    // This will get the last available date from the bymonthday array. For example, if the dayOfMonth is 30, the bymonthday array will be [28, 29, 30] and the bysetpos will be -1, which will get the 30th day of the month if it exists, otherwise the 28th or 29th (for February)
+    bysetpos: -1,
+  });
+};
+
+export const generateYearlyRRule = startDate => {
+  return new RRule({
+    freq: RRule.YEARLY,
+    dtstart: startDate,
+    interval: 1,
+  });
+};
