406ed91a7aa40d7c09eedaebb3016ffae6aef0ef
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-08-16 11:12:00 +1200

    Build fixes

--- packages/datatrak-web/src/features/Tasks/TaskSummary.tsx
+++ packages/datatrak-web/src/features/Tasks/TaskSummary.tsx
@@ -64,7 +64,7 @@ const Row = styled.div`
 
 const getDisplayRepeatSchedule = (task: SingleTaskResponse) => {
   // TODO: When repeating tasks are implemented, make sure the repeat schedule is displayed correctly once a due date is returned with the task
-  const repeatScheduleOptions = getRepeatScheduleOptions(task.dueDate);
+  const repeatScheduleOptions = getRepeatScheduleOptions(task.taskDueDate);
   const { label } = repeatScheduleOptions[0];
   if (!task.repeatSchedule?.frequency) {
     return label;
