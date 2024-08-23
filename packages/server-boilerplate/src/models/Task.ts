732df942bd5067525f3a91f3d88c89cfb71a7b4b
Salman <114740396+hrazasalman@users.noreply.github.com>
2024-08-23 11:08:37 +1000

    types update

--- packages/server-boilerplate/src/models/Task.ts
+++ packages/server-boilerplate/src/models/Task.ts
@@ -6,6 +6,10 @@ import { TaskRecord as BaseTaskRecord, TaskModel as BaseTaskModel } from '@tupai
 import { Task } from '@tupaia/types';
 import { Model } from './types';
 
-export interface TaskRecord extends Task, BaseTaskRecord {}
+export interface TaskRecord extends Task, BaseTaskRecord {
+  project_id?: string | null;
+  data_time?: Date | null;
+  timezone?: string | null;
+}
 
 export interface TaskModel extends Model<BaseTaskModel, Task, TaskRecord> {}
