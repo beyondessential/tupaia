948ff0d33a6ec1af4e7395000ad542fa601b819b
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-08-27 13:50:53 +1200

    Display 'user deleted' message for deleted message

--- packages/datatrak-web/src/features/Tasks/TaskDetails/TaskComments.tsx
+++ packages/datatrak-web/src/features/Tasks/TaskDetails/TaskComments.tsx
@@ -170,12 +170,12 @@ const UserComment = ({ message }: { message: Comments[0]['message'] }) => {
 };
 
 const SingleComment = ({ comment }: { comment: Comments[0] }) => {
-  const { createdAt, type, userName, message, templateVariables } = comment;
+  const { createdAt, type, userName, message, templateVariables, userId } = comment;
 
   return (
     <CommentContainer>
       <CommentDetails>
-        {displayDateTime(createdAt)} - {userName}
+        {displayDateTime(createdAt)} - {userName} {!userId ? '(user deleted)' : ''}
       </CommentDetails>
 
       {type === TaskCommentType.system ? (
