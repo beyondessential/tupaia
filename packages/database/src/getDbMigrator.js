1feef5330b2b712d2b71f2f4a13a383edd19efe4
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-09-02 15:54:18 +1200

    remove throw uncatched

--- packages/database/src/getDbMigrator.js
+++ packages/database/src/getDbMigrator.js
@@ -49,7 +49,6 @@ export const getDbMigrator = (forCli = false) =>
           ...getConnectionConfig(),
         },
       },
-      throwUncatched: true,
     },
     forCli ? cliCallback : appCallback,
   );
