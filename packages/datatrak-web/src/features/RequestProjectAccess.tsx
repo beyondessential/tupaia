6d93cebce66cf53202cd32184b59f5f73b7d14ce
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-09-13 15:23:10 +1200

    Move datatrak web to use new ui component

---
+++ packages/datatrak-web/src/features/RequestProjectAccess.tsx
@@ -0,0 +1,33 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
+ */
+
+import React from 'react';
+import { RequestProjectAccess as UIRequestProjectAccess } from '@tupaia/ui-components';
+import { useCountryAccessList, useProject, useRequestProjectAccess } from '../api';
+
+interface RequestProjectAccessProps {
+  variant?: 'page' | 'modal';
+  projectCode?: string;
+  onClose?: () => void;
+}
+
+export const RequestProjectAccess = ({ projectCode, onClose }: RequestProjectAccessProps) => {
+  const { data: project, isLoading: isLoadingProject, isFetched } = useProject(projectCode);
+  const { mutate: requestProjectAccess, isLoading, isSuccess } = useRequestProjectAccess();
+  const { data: countries } = useCountryAccessList(projectCode);
+
+  return (
+    <UIRequestProjectAccess
+      onClose={onClose}
+      project={project}
+      onSubmit={requestProjectAccess}
+      isLoading={isLoadingProject}
+      isSubmitting={isLoading}
+      isSuccess={isSuccess}
+      isFetched={isFetched}
+      countries={countries}
+    />
+  );
+};
