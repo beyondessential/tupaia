c51808a839abb15f1b8bf1a537038571d88dc32d
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-09-25 09:55:33 +1200

    Remove GA logs

--- packages/tupaia-web/src/utils/ga.ts
+++ packages/tupaia-web/src/utils/ga.ts
@@ -7,12 +7,7 @@ type WindowWithGa = Window & {
   ga: (...args: any[]) => void;
 };
 
-const ga = ((window as unknown) as WindowWithGa).ga || (() => {});
-
-if (!((window as unknown) as WindowWithGa).ga) {
-  // eslint-disable-next-line no-console
-  console.warn('Google Analytics library not found');
-}
+const ga = (window as unknown as WindowWithGa).ga || (() => {});
 
 export const gaEvent = (category: string, action: string, label?: string) =>
   ga('send', 'event', category, action, label);
