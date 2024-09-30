46c85b4338c75f3348fa53b5c961252e82528e07
alexd-bes <129009580+alexd-bes@users.noreply.github.com>
2024-09-30 14:06:46 +1300

    media queries

--- packages/datatrak-web/src/constants/constants.ts
+++ packages/datatrak-web/src/constants/constants.ts
@@ -8,3 +8,13 @@ export const TITLE_BAR_HEIGHT = '3.875rem';
 export const DESKTOP_MEDIA_QUERY = `@media screen and (min-width: 1440px) and (min-height: 900px)`;
 
 export const REDIRECT_ERROR_PARAM = 'redirectError';
+
+export const TABLET_PORTRAIT_MEDIA_QUERY = `@media screen and (min-width: 600px) and (max-width: 960px) and (orientation: portrait)`;
+
+export const TABLET_LANDSCAPE_MEDIA_QUERY = `@media screen and (min-width: 600px) and (max-width: 960px) and (orientation: landscape)`;
+
+export const MOBILE_PORTRAIT_MEDIA_QUERY = `@media screen and (max-width: 600px) and (orientation: portrait)`;
+
+export const MOBILE_LANDSCAPE_MEDIA_QUERY = `@media screen and (max-width: 600px) and (orientation: landscape)`;
+
+export const MOBILE_MEDIA_QUERY = `@media screen and (max-width: ${MOBILE_BREAKPOINT})`;
