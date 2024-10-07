e9db8e5d140a38585be0e831b684d60f160e2503
Tom Caiger <caigertom@gmail.com>
2024-10-07 15:30:57 +1300

    update asset pre caching

--- packages/datatrak-web/service-worker.ts
+++ packages/datatrak-web/service-worker.ts
@@ -4,7 +4,14 @@
  */
 
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/v1/'),
+  new CacheFirst({ cacheName: 'apiResponses' }),
+);
--- packages/datatrak-web/workbox-config.js
+++ packages/datatrak-web/workbox-config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  globDirectory: 'public/',
-  globPatterns: ['**/*.{svg,png,ico,jpg,json,js,txt}'],
-  swDest: 'public/sw.js',
-  swSrc: 'public/sw.js',
+  globDirectory: 'dist/',
+  globPatterns: ['**/*.{svg,png,ico,jpg,json,js,txt,html}'],
+  swSrc: 'dist/sw.js',
+  swDest: 'dist/sw.js',
+  maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
 };
