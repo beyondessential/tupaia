2e7e7572f64e2f3d6f3f605e31e38aa3559ae63e
Tom Caiger <caigertom@gmail.com>
2024-10-25 11:21:00 +1300

    fix merge

--- packages/datatrak-web/src/layout/UserMenu/DrawerMenu.tsx
+++ packages/datatrak-web/src/layout/UserMenu/DrawerMenu.tsx
@@ -7,13 +7,13 @@ import styled from 'styled-components';
 import { Drawer as MuiDrawer, Paper as MuiPaper, Typography } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { IconButton, RouterLink } from '@tupaia/ui-components';
-import { MOBILE_BREAKPOINT, ROUTES } from '../../constants';
+import { DESKTOP_BREAKPOINT, ROUTES } from '../../constants';
 import { useCurrentUserContext } from '../../api';
 import { Button } from '../../components';
 import { MenuButton, MenuList } from './MenuList';
 
 const Drawer = styled(MuiDrawer)`
-  @media screen and (min-width: ${MOBILE_BREAKPOINT}) {
+  @media screen and (min-width: ${DESKTOP_BREAKPOINT}) {
     display: none;
   }
 `;
