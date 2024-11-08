f4dfcaa916ec30fe806ab9c638cfdc0d14ed70c9
Tom Caiger <caigertom@gmail.com>
2024-11-08 16:52:13 +1300

    VizBuilderPrivateRoute

---
+++ packages/admin-panel/src/authentication/VizBuilderPrivateRoute.jsx
@@ -0,0 +1,24 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
+ */
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useUser } from '../api/queries';
+import { AUTH_ROUTES } from '../routes';
+
+export const VizBuilderPrivateRoute = () => {
+  const location = useLocation();
+  const { isLoggedIn, isLoading, hasVizBuilderAccess } = useUser();
+  if (isLoading) return null;
+
+  if (!isLoggedIn) {
+    return <Navigate to={AUTH_ROUTES.LOGIN} state={{ from: location.pathname }} />;
+  }
+
+  if (!hasVizBuilderAccess) {
+    return <Navigate to="/" state={{ from: location.pathname }} />;
+  }
+
+  return <Outlet />;
+};
