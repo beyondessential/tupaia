216a6dac55447ff66a107e810e221975ec5e1326
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-01-06 16:11:28 +1300

    add `accent-color` to css baseline

--- packages/datatrak-web/src/theme/theme.ts
+++ packages/datatrak-web/src/theme/theme.ts
@@ -7,140 +7,150 @@ import { createMuiTheme } from '@material-ui/core';
 import { DESKTOP_BREAKPOINT } from '../constants';
 
 const overMobileBreakpoint = `@media (min-width: ${DESKTOP_BREAKPOINT})`;
-export const theme = createMuiTheme({
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#328DE5', // Main blue (as seen on primary buttons)
-      dark: '#004167',
-    },
-    secondary: {
-      main: '#EC642D', // Tupaia orange
-    },
-    background: {
-      default: '#F9F9F9', // Off white background
-      paper: '#ffffff', // White background
-    },
-    text: {
-      primary: '#2E2F33', // dark text color
-      secondary: '#898989', // light grey text color
-    },
-    success: {
-      main: '#25D366',
-      light: '#25D36622',
-    },
-    error: {
-      main: '#F76853',
-      light: '#F7685333',
-      dark: '#FB5531',
-    },
-    divider: '#DFDFDF',
-    grey: {
-      400: '#B8B8B8',
-    },
-    action: {
-      hover: '#2a78c3',
-    },
-    primaryHover: '#ebf5ff',
+
+const palette = {
+  type: 'light',
+  primary: {
+    main: '#328DE5', // Main blue (as seen on primary buttons)
+    dark: '#004167',
   },
-  typography: {
-    h1: {
-      fontSize: '1rem', // page titles
-      fontWeight: 500,
-      [overMobileBreakpoint]: {
-        fontSize: '1.125rem',
-      },
+  secondary: {
+    main: '#EC642D', // Tupaia orange
+  },
+  background: {
+    default: '#F9F9F9', // Off white background
+    paper: '#ffffff', // White background
+  },
+  text: {
+    primary: '#2E2F33', // dark text color
+    secondary: '#898989', // light grey text color
+  },
+  success: {
+    main: '#25D366',
+    light: '#25D36622',
+  },
+  error: {
+    main: '#F76853',
+    light: '#F7685333',
+    dark: '#FB5531',
+  },
+  divider: '#DFDFDF',
+  grey: {
+    400: '#B8B8B8',
+  },
+  action: {
+    hover: '#2a78c3',
+  },
+  primaryHover: '#ebf5ff',
+} as const;
+
+const typography = {
+  h1: {
+    fontSize: '1rem', // page titles
+    fontWeight: 500,
+    [overMobileBreakpoint]: {
+      fontSize: '1.125rem',
     },
-    h2: {
+  },
+  h2: {
+    fontSize: '1rem',
+    [overMobileBreakpoint]: {
+      fontSize: '1.125rem',
+    },
+    fontWeight: 600,
+  },
+  h3: {
+    fontSize: '1rem',
+    fontWeight: 500,
+    [overMobileBreakpoint]: {
+      fontSize: '1.125rem',
+    },
+  },
+  body1: {
+    fontSize: '0.875rem',
+    lineHeight: 1.2,
+    [overMobileBreakpoint]: {
       fontSize: '1rem',
-      [overMobileBreakpoint]: {
-        fontSize: '1.125rem',
-      },
-      fontWeight: 600,
     },
-    h3: {
-      fontSize: '1rem',
-      fontWeight: 500,
-      [overMobileBreakpoint]: {
-        fontSize: '1.125rem',
+  },
+} as const;
+
+const overrides = {
+  MuiCssBaseline: {
+    '@global': {
+      fieldset: {
+        border: 0,
+        margin: 0,
+        minWidth: 0,
+        padding: 0,
+      },
+      ':root': {
+        accentColor: palette.primary.main,
       },
     },
-    body1: {
+  },
+  MuiDialogActions: {
+    root: {
+      padding: '1.5rem 0 0 0',
+    },
+  },
+  MuiListItem: {
+    button: {
+      '&:hover': {
+        backgroundColor: '#328DE515',
+      },
+    },
+  },
+  MuiButton: {
+    root: {
+      textTransform: 'none',
+      verticalAlign: 'baseline',
+    },
+    label: {
+      fontSize: '0.875rem',
+    },
+    containedPrimary: {
+      '&:hover': {
+        backgroundColor: '#2A78C3',
+      },
+    },
+  },
+  MuiMenuItem: {
+    root: {
+      fontSize: '0.875rem',
+    },
+  },
+  MuiFormControlLabel: {
+    label: {
       fontSize: '0.875rem',
       lineHeight: 1.2,
       [overMobileBreakpoint]: {
         fontSize: '1rem',
+        fontWeight: 400,
       },
     },
   },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        fieldset: {
-          border: 0,
-          margin: 0,
-          minWidth: 0,
-          padding: 0,
-        },
-      },
-    },
-    MuiDialogActions: {
-      root: {
-        padding: '1.5rem 0 0 0',
-      },
-    },
-    MuiListItem: {
-      button: {
-        '&:hover': {
-          backgroundColor: '#328DE515',
-        },
-      },
-    },
-    MuiButton: {
-      root: {
-        textTransform: 'none',
-        verticalAlign: 'baseline',
-      },
-      label: {
-        fontSize: '0.875rem',
-      },
-      containedPrimary: {
-        ['&:hover']: {
-          backgroundColor: '#2A78C3',
-        },
-      },
-    },
-    MuiMenuItem: {
-      root: {
-        fontSize: '0.875rem',
-      },
-    },
-    MuiFormControlLabel: {
-      label: {
-        fontSize: '0.875rem',
-        lineHeight: 1.2,
-        [overMobileBreakpoint]: {
-          fontSize: '1rem',
-          fontWeight: 400,
-        },
-      },
-    },
-    MuiFormLabel: {
-      root: {
-        fontSize: '0.875rem',
-        fontWeight: 500,
-        lineHeight: 1.2,
-        color: '#2E2F33',
-        [overMobileBreakpoint]: {
-          fontSize: '1rem',
-          fontWeight: 400,
-        },
-      },
-    },
-    MuiPopover: {
-      paper: {
-        boxShadow: '4px 4px 25px 4px rgba(0, 0, 0, 0.10)',
+  MuiFormLabel: {
+    root: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      color: '#2E2F33',
+      [overMobileBreakpoint]: {
+        fontSize: '1rem',
+        fontWeight: 400,
       },
     },
   },
+  MuiPopover: {
+    paper: {
+      boxShadow: '0.25rem 0.25rem 1.5rem .25rem oklch(0 0 0 / 10%)',
+    },
+  },
+} as const;
+
+export const theme = createMuiTheme({
+  palette,
+  typography,
+  overrides,
 });
