7540e511152ad657d81cd9bae86f52eff6558c2a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-01-14 10:07:03 +1300

    `<DateTimeDisplay/>`

---
+++ packages/datatrak-web/src/components/DateTimeDisplay.tsx
@@ -0,0 +1,29 @@
+import { isValid } from 'date-fns';
+import React, { HTMLAttributes } from 'react';
+
+type DateTimeDisplayVariant = 'date' | 'dateTime' | 'time';
+
+interface DateTimeDisplayProps extends HTMLAttributes<HTMLTimeElement> {
+  date?: Date | null;
+  variant: DateTimeDisplayVariant;
+}
+
+const localeStringMethods: Record<
+  DateTimeDisplayVariant,
+  keyof Pick<Date, 'toLocaleDateString' | 'toLocaleString' | 'toLocaleTimeString'>
+> = {
+  date: 'toLocaleDateString',
+  dateTime: 'toLocaleString',
+  time: 'toLocaleTimeString',
+};
+
+export const DateTimeDisplay = ({ date, variant = 'dateTime', ...props }: DateTimeDisplayProps) => {
+  if (!date || !isValid(date)) return null;
+
+  const stringifier = localeStringMethods[variant];
+  return (
+    <time dateTime={date.toISOString()} {...props}>
+      {date[stringifier]()}
+    </time>
+  );
+};
