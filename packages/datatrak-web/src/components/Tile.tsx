f4621e8e6df6cfdddab476b3950b8fc5a0f54dec
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-01-20 16:01:14 +1300

    fix alignment

--- packages/datatrak-web/src/components/Tile.tsx
+++ packages/datatrak-web/src/components/Tile.tsx
@@ -63,6 +63,7 @@ const Header = styled.header`
     return css`
       ${down('lg')} {
         margin-block-end: 0.5rem;
+        block-size: 1.5rem;
       }
       ${up('lg')} {
         flex-direction: column;
