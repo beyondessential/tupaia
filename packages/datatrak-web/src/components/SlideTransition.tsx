10b4e729d28a142e7e550b644bb355bdee16b816
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-01-21 11:43:13 +1300

    repo: remove copyright headers from epic

--- packages/datatrak-web/src/components/SlideTransition.tsx
+++ packages/datatrak-web/src/components/SlideTransition.tsx
@@ -1,7 +1,3 @@
-/*
- * Tupaia
- *  Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
- */
 import React from 'react';
 import { TransitionProps } from '@material-ui/core/transitions';
 import Slide from '@material-ui/core/Slide';
