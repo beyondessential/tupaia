4fb6cbd1eca38aec92d308ba1544c72ff8c30a19
Tom Caiger <caigertom@gmail.com>
2025-01-29 16:23:42 +1300

    Update useResetPassword.ts

--- packages/datatrak-web/src/api/mutations/useResetPassword.ts
+++ packages/datatrak-web/src/api/mutations/useResetPassword.ts
@@ -42,7 +42,7 @@ export const useResetPassword = (options?: {
         navigate({
           ...location,
           search: urlSearchParams.toString(),
-        });
+        }, { replace: true });
 
         if (options?.onSuccess) options.onSuccess(response);
       },
