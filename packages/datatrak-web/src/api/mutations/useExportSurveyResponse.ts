ddddbf702e8308b89814ba10e1ef8e290b482fb5
Tom Caiger <caigertom@gmail.com>
2025-02-04 16:24:16 +1300

    use survey response timezone

--- packages/datatrak-web/src/api/mutations/useExportSurveyResponse.ts
+++ packages/datatrak-web/src/api/mutations/useExportSurveyResponse.ts
@@ -9,16 +9,13 @@ import { API_URL, post } from '../api';
 import { successToast } from '../../utils';
 
 // Requests a survey response PDF export from the server, and returns the response
-export const useExportSurveyResponse = (surveyResponseId: string) => {
+export const useExportSurveyResponse = (surveyResponseId: string, timezone: string) => {
   return useMutation<any, Error, unknown, unknown>(
     () => {
       const baseUrl = `${window.location.protocol}//${window.location.host}`;
 
       // Auth cookies are saved against this domain. Pass this to server, so that when it pretends to be us, it can do the same.
       const cookieDomain = new URL(API_URL).hostname;
-
-      const timezone = getBrowserTimeZone();
-
 
       return post(`export/${surveyResponseId}`, {
         responseType: 'blob',
