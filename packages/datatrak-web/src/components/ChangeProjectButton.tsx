b45923e0ca450f75c642d1a27fefaf0c88b75612
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-17 12:36:58 +1300

    fix `<ChangeProjectButton>` font weight

--- packages/datatrak-web/src/components/ChangeProjectButton.tsx
+++ packages/datatrak-web/src/components/ChangeProjectButton.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { HTMLAttributes, useState } from 'react';
 import styled from 'styled-components';
-import { ProjectSelectModal } from '../layout/UserMenu/ProjectSelectModal';
+
 import { useCurrentUserContext } from '../api';
+import { ProjectSelectModal } from '../layout/UserMenu/ProjectSelectModal';
 import { UserDetails as NavbarUserDetails } from '../layout/UserMenu/UserInfo';
 import { Button, TooltipButtonWrapper } from './Button';
 
@@ -17,7 +18,7 @@ const Container = styled.div`
     padding-inline-start: 0.5rem;
   }
 
-  .MuiTypography-root & {
+  :is(p, h1, h2, h3, h4, h5, h6) & {
     &,
     > ${TooltipButtonWrapper} // Prevent span wrapper on button from growing to fill parent
     {
@@ -51,19 +52,15 @@ const ProjectButton = styled(Button).attrs({
   .MuiButton-root,
   .MuiButton-label {
     font-size: inherit;
-    font-weight: 500;
+    font-weight: inherit;
     line-height: inherit;
     inline-size: fit-content;
     margin: 0;
     padding: 0;
   }
-
-  .MuiTypography-root & .MuiButton-label {
-    font-weight: 400;
-  }
 `;
 
-export const ChangeProjectButton = ({ className }: { className?: string }) => {
+export const ChangeProjectButton = (props: HTMLAttributes<HTMLDivElement>) => {
   const { project } = useCurrentUserContext();
   const projectName = project?.name ?? null;
 
@@ -72,7 +69,7 @@ export const ChangeProjectButton = ({ className }: { className?: string }) => {
   const closeProjectModal = () => setProjectModalIsOpen(false);
 
   return (
-    <Container className={className}>
+    <Container {...props}>
       <ProjectButton onClick={openProjectModal} tooltip="Change project">
         {projectName ?? 'Select project'}
       </ProjectButton>
