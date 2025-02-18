dcadd0a0a60826f93223e392753ee0e18487e2b0
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-18 16:31:30 +1300

    fix(datatrak): RN-1453: custom toast collision with Android 12+ toast (#6083)
    * use logical prop
    
    * more informative toast
    
    * type `useQuery`
    
    * rename to `CopyUrlButton`, simplify interface
    
    * allow x-axis scroll on `<MobileSurveyMenu>`
    
    * temporarily show ✅
    
    * fix elliptical hover state
    
    * smarter padding
    
    * less informative toast
    
    * organise imports

--- packages/datatrak-web/src/features/Survey/Components/MobileSurveyMenu.tsx
+++ packages/datatrak-web/src/features/Survey/Components/MobileSurveyMenu.tsx
@@ -1,33 +1,38 @@
+import { IconButton } from '@material-ui/core';
 import React, { HTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-import { IconButton as MuiIconButton } from '@material-ui/core';
 import { FormatListBulleted, KeyboardArrowRight } from '@material-ui/icons';
 
-import { CopyIcon, ShareIcon, Button as UIButton } from '../../../components';
+import { ShareIcon, Button as UIButton } from '../../../components';
 import { useSurveyForm } from '../SurveyContext';
 import { useShare } from '../utils/useShare';
-import { useCopySurveyUrl } from './CopySurveyUrlButton';
+import { CopyUrlButton } from './CopyUrlButton';
 
 const Container = styled.div`
   align-items: stretch;
   background: white;
   block-size: 3.5rem;
   border-block-start: max(0.0625rem, 1px) solid ${props => props.theme.palette.divider};
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(3.5rem, 1fr)) minmax(min-content, 1fr);
   inline-size: 100%;
   inset-block-end: 0;
   justify-content: space-between;
   margin-bottom: env(safe-area-inset-bottom, 0);
   padding-block: 0;
-  padding-inline: 0.5rem;
+  padding-left: env(safe-area-inset-left, 0);
+  padding-right: env(safe-area-inset-right, 0);
   position: fixed;
-  touch-action: pinch-zoom;
+  touch-action: pan-x pinch-zoom;
+
+  & > button {
+    border-radius: 0;
+  }
 `;
 
-const IconButton = styled(MuiIconButton)`
+const StyledCopyUrlButton = styled(CopyUrlButton).attrs({ noTooltip: true })`
   border-radius: 0;
-  flex: 1;
   color: ${({ theme }) => theme.palette.text.primary};
 `;
 
@@ -38,21 +43,11 @@ const Button = styled(UIButton).attrs({
 })`
   border-inline-start: max(0.0625rem, 1px) solid ${props => props.theme.palette.divider};
   border-radius: 0;
-  flex: 1;
-  min-inline-size: 8rem;
   padding-block: 1.2rem;
 `;
 
 export const MobileSurveyMenu = (props: HTMLAttributes<HTMLDivElement>) => {
   const { toggleSideMenu, isLast, isReviewScreen, isResubmitReviewScreen } = useSurveyForm();
-  const copyPageUrl = useCopySurveyUrl({
-    toastOptions: {
-      anchorOrigin: {
-        horizontal: 'center',
-        vertical: 'bottom',
-      },
-    },
-  });
   const share = useShare();
 
   const getNextButtonText = () => {
@@ -70,9 +65,7 @@ export const MobileSurveyMenu = (props: HTMLAttributes<HTMLDivElement>) => {
       <IconButton onClick={share}>
         <ShareIcon />
       </IconButton>
-      <IconButton onClick={copyPageUrl}>
-        <CopyIcon />
-      </IconButton>
+      <StyledCopyUrlButton />
       <Button type="submit">{getNextButtonText()}</Button>
     </Container>
   );
