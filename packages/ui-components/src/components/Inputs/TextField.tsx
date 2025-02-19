e1731bddf0d802bcf5bb856cbbdbeed7fdeb30cc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-20 12:53:27 +1300

    closer match

--- packages/ui-components/src/components/Inputs/TextField.tsx
+++ packages/ui-components/src/components/Inputs/TextField.tsx
@@ -88,7 +88,7 @@ const StyledTextField = styled(MuiTextField)<{ $focusColor?: Property.Color }>`
   /* Override MaterialUI which hides the placeholder due to conflict with its floating labels */
   &&&& .MuiInputBase-input::placeholder {
     color: inherit;
-    opacity: 55% !important;
+    opacity: 45% !important;
   }
 
   // disable MaterialUI underline
