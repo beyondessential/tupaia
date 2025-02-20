cf59f6fb3181539d7512fca31c04e45ae4c594b9
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-20 13:24:15 +1300

    fix survey success layout

--- packages/datatrak-web/src/features/Survey/Components/SurveySuccess.tsx
+++ packages/datatrak-web/src/features/Survey/Components/SurveySuccess.tsx
@@ -1,13 +1,19 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
-import { Typography } from '@material-ui/core';
-import { SurveyQRCode } from '../SurveyQRCode';
+
 import { useCurrentUserContext } from '../../../api';
+import { SurveyQRCode } from '../SurveyQRCode';
 import { useQRCodeLocationData } from '../SurveyQRCode/useQRCodeLocationData';
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  align-items: center;
+  display: grid;
+  grid-template-areas: '. --sidebar' '--main --sidebar' '--supplementary --sidebar';
+  grid-template-columns: 1fr minmax(0, auto);
+  grid-template-rows: 1fr auto var(--wrapper-row-3-size, 3fr);
+  inline-size: 100%;
+  justify-items: stretch;
   min-block-size: 100%;
   padding-bottom: max(env(safe-area-inset-bottom, 0), 1.5rem);
   padding-left: max(env(safe-area-inset-left, 0), 1.5rem);
@@ -21,6 +27,7 @@ const Wrapper = styled.div`
 `;
 
 const Container = styled.div<{ $showQrCode?: boolean }>`
+  grid-area: --main;
   align-items: center;
   display: flex;
   flex-direction: column;
@@ -67,6 +74,16 @@ const Text = styled(Typography)`
   margin-block-end: 1.875rem;
 `;
 
+const StyledSurveyQrCode = styled(SurveyQRCode)`
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    justify-self: center;
+    grid-area: --supplementary;
+    ${Wrapper}:has(&) {
+      --wrapper-row-3-size: max(min-content, auto);
+    }
+  }
+`;
+
 interface SurveySuccessProps {
   text: string;
   title: string;
@@ -89,7 +106,9 @@ export const SurveySuccess = ({ text, title, children }: SurveySuccessProps) => 
           </>
         )}
       </Container>
-      {qrCodeEntitiesCreated && <SurveyQRCode qrCodeEntitiesCreated={qrCodeEntitiesCreated} />}
+      {qrCodeEntitiesCreated && (
+        <StyledSurveyQrCode qrCodeEntitiesCreated={qrCodeEntitiesCreated} />
+      )}
     </Wrapper>
   );
 };
