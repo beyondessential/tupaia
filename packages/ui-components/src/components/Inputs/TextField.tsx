1dcdc19911c9b03ab48c551e542ae833a36a0394
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-21 11:18:36 +1300

    remove redundant override

--- packages/ui-components/src/components/Inputs/TextField.tsx
+++ packages/ui-components/src/components/Inputs/TextField.tsx
@@ -85,7 +85,6 @@ const StyledTextField = styled(MuiTextField)<{ $focusColor?: Property.Color }>`
 
   /* Override MaterialUI which hides the placeholder due to conflict with its floating labels */
   &&&& .MuiInputBase-input::placeholder {
-    color: ${props => props.theme.palette.text.hint};
     opacity: initial !important;
   }
 
