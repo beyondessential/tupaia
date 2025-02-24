578bf8e2dd0ad7cb9c068b0c0ae3ba1aad7f88fc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-25 11:38:32 +1300

    JSDoc

--- packages/datatrak-web/src/api/queries/useCountryAccessList.ts
+++ packages/datatrak-web/src/api/queries/useCountryAccessList.ts
@@ -3,6 +3,10 @@ import { Project, ProjectCountryAccessListRequest } from '@tupaia/types';
 import { get } from '../api';
 import { useCurrentUserContext } from '..';
 
+/**
+ * Returns the `UseQueryResult` for the countries the currently logged-in user has access to for the
+ * given project. If no project code is provided, it defaults to the user’s current project.
+ */
 export const useCountryAccessList = (projectCode?: Project['code']) => {
   const user = useCurrentUserContext();
   const code = projectCode ?? user?.project?.code;
