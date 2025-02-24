7368429e8052600d20cb5a070c8a227630402b20
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-02-24 13:25:24 +1300

    reorder lines for readability

--- packages/datatrak-web/src/features/Questions/ReadOnlyQuestion.tsx
+++ packages/datatrak-web/src/features/Questions/ReadOnlyQuestion.tsx
@@ -6,13 +6,13 @@ import { SurveyQuestionInputProps } from '../../types';
 import { InputHelperText, TextInput } from '../../components';
 
 const Wrapper = styled.div`
-  inline-size: 100%;
   border-block-start: max(0.0625rem, 1px) solid ${({ theme }) => theme.palette.divider};
   flex-direction: column;
   inline-size: 100%;
+  inline-size: 100%;
   justify-content: center;
-  padding-block-start: 2rem;
 
+  padding-block-start: 2rem;
   ${({ theme }) => theme.breakpoints.down('sm')} {
     padding-block-start: 1rem;
   }
