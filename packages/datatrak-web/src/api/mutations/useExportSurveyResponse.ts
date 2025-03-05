7953b43baabe81e941ff0ff83c6613df1e5827da
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-06 11:58:28 +1300

    remove copyright headers

--- packages/datatrak-web/src/api/mutations/useExportSurveyResponse.ts
+++ packages/datatrak-web/src/api/mutations/useExportSurveyResponse.ts
@@ -1,7 +1,3 @@
-/*
- * Tupaia
- *  Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
- */
 import { useMutation } from '@tanstack/react-query';
 import download from 'downloadjs';
 import { API_URL, post } from '../api';
