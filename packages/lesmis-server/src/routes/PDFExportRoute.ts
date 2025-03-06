3eea37f301be4583dd748e5985302104a9846939
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-06 16:26:16 +1300

    fix types

--- packages/lesmis-server/src/routes/PDFExportRoute.ts
+++ packages/lesmis-server/src/routes/PDFExportRoute.ts
@@ -2,16 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import { Route } from '@tupaia/server-boilerplate';
 import { downloadPageAsPDF } from '@tupaia/server-utils';
 
-type Body = {
-  pdfPageUrl: string;
-};
-
-export type PDFExportRequest = Request<
-  Record<string, never>,
-  { contents: Buffer },
-  Body,
-  Record<string, any>
->;
+export interface PDFExportRequest
+  extends Request<
+    Record<string, never>,
+    { contents: Uint8Array },
+    { pdfPageUrl: string },
+    Record<string, unknown>
+  > {}
 
 export class PDFExportRoute extends Route<PDFExportRequest> {
   public constructor(req: PDFExportRequest, res: Response, next: NextFunction) {
