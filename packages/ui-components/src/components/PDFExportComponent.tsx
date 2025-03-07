f46d8654c02460fb51fe2ab7bf3c2730af4fd3bc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-07 13:32:11 +1300

    retire deprecated CSS

--- packages/ui-components/src/components/PDFExportComponent.tsx
+++ packages/ui-components/src/components/PDFExportComponent.tsx
@@ -11,9 +11,11 @@ export const A4_PAGE_HEIGHT_PX = 1683; // at 144ppi
 export const A4Page = styled.div<{
   separatePage?: boolean;
 }>`
-  width: ${A4_PAGE_WIDTH_PX}px;
-  page-break-after: ${({ separatePage }) => (separatePage ? 'always' : 'auto')};
+  width: 210mm;
+  height: 297mm;
+  break-after: ${({ separatePage }) => (separatePage ? 'always' : 'auto')};
   break-inside: avoid;
   flex-direction: column;
-  padding: 1.5cm 4.5cm 2cm; // Bottom slightly taller than top for *optical* alignment
+  padding-block: 1.5cm 2cm; // Bottom slightly taller than top for *optical* alignment
+  padding-inline: 4.5cm;
 `;
