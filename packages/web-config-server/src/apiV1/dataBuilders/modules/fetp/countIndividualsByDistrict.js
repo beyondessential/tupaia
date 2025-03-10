56da0b97ceaa99fe5a859aaf10f9d38a1414921e
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-11 12:13:32 +1300

    remove straggling headers

--- packages/web-config-server/src/apiV1/dataBuilders/modules/fetp/countIndividualsByDistrict.js
+++ packages/web-config-server/src/apiV1/dataBuilders/modules/fetp/countIndividualsByDistrict.js
@@ -1,8 +1,5 @@
 /**
- * Tupaia
- * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
- *
- * count entities of type 'individual' by district and sub_district grouping by district
+ * Count entities of type 'individual' by district and sub_district grouping by district
  */
 export const countIndividualsByDistrict = async ({
   models,
