20f3f6500b820a9b3e40bafbf2de7b79e5c0529d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-11 10:47:18 +1300

    Revert "dedupe"
    This reverts commit 862b7c7bbadc0d9ae2b4264c6d854194fec95970.

--- packages/datatrak-web/src/utils/useBeforeUnload.ts
+++ packages/datatrak-web/src/utils/useBeforeUnload.ts
@@ -5,17 +5,14 @@ function handleBeforeUnload(event: BeforeUnloadEvent) {
   event.returnValue = ''; // For legacy browsers
 }
 
-function addListener() {
-  window.addEventListener('beforeunload', handleBeforeUnload);
-}
-
-function removeListener() {
-  window.removeEventListener('beforeunload', handleBeforeUnload);
-}
-
 export function useBeforeUnload(enabled = true) {
   useEffect(() => {
-    (enabled ? addListener : removeListener)();
-    return removeListener;
+    if (enabled) {
+      window.addEventListener('beforeunload', handleBeforeUnload);
+    } else {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    }
+
+    return () => window.removeEventListener('beforeunload', handleBeforeUnload);
   }, [enabled]);
 }
