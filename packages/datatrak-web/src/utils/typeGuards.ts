33c2803ce07238c349d9c41b3c5d0db09dad8a03
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-11 14:02:30 +1300

    commend

--- packages/datatrak-web/src/utils/typeGuards.ts
+++ packages/datatrak-web/src/utils/typeGuards.ts
@@ -1,3 +1,8 @@
+/*
+ * Duplicated from @tupaia/tsutils, which cannot (currently!) be used as a dependency of our front-end
+ * packages due some incompatibilities with its transitive dependencies (as of May 2024).
+ */
+
 export function isEmptyArray(val: unknown): val is [] {
   return Array.isArray(val) && val.length === 0;
 }
