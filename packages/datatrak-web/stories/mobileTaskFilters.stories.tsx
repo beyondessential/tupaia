0c65f8e8503a1750c0dd6febf5ec3513303e8cbc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-18 10:58:07 +1300

    remove copyright headers

--- packages/datatrak-web/stories/mobileTaskFilters.stories.tsx
+++ packages/datatrak-web/stories/mobileTaskFilters.stories.tsx
@@ -1,7 +1,3 @@
-/*
- * Tupaia
- *  Copyright (c) 2017 - 2024 Beyond Essential Systems Pty Ltd
- */
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import styled from 'styled-components';
