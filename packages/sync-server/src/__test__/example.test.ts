90bbbfdccbe06fba4c5b1c7e120406d20f479e38
chris-bes <chris@beyondessential.com.au>
2025-03-21 15:05:12 +1100

    more scaffolding

---
+++ packages/sync-server/src/__test__/example.test.ts
@@ -0,0 +1,11 @@
+/*
+ * Tupaia
+ * Copyright (c) 2017 - 2025 Beyond Essential Systems Pty Ltd
+ *
+ */
+
+describe('Example test', () => {
+  it('passes a stub test', () => {
+    expect(1).toBe(1);
+  });
+});
