b195a0374a2374790afeeef3deeee7773f0f95bf
chris-bes <chris@beyondessential.com.au>
2025-03-21 15:26:22 +1100

    move migrate.js to core

--- packages/database/scripts/migrateCreate.sh
+++ packages/database/scripts/migrateCreate.sh
@@ -3,4 +3,4 @@
 read -p "Enter migration name: " MIGRATION_NAME
 read -p "Enter the scope of this migration ('schema' or 'data'): " MIGRATION_SCOPE
 
-babel-node ./src/migrate.js create ${MIGRATION_NAME}-modifies-${MIGRATION_SCOPE} --migrations-dir ./src/migrations -v --config-file "../../babel.config.json"
+babel-node ./src/core/migrate.js create ${MIGRATION_NAME}-modifies-${MIGRATION_SCOPE} --migrations-dir ./src/migrations -v --config-file "../../babel.config.json"
