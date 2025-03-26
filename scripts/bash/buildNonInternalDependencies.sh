f671a091f27710a734aa7d319f082c72a2a148f8
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-27 09:11:01 +1300

    `--topological`

--- scripts/bash/buildNonInternalDependencies.sh
+++ scripts/bash/buildNonInternalDependencies.sh
@@ -4,6 +4,7 @@ set -ex
 
 yarn workspaces foreach \
     --parallel \
+    --topological \
     --verbose \
     --jobs unlimited \
     --include '@tupaia/{admin-panel-server,central-server,data-table-server,datatrak-web,datatrak-web-server,entity-server,lesmis,lesmis-server,meditrak-app-server,psss,psss-server,report-server,tupaia-web,tupaia-web-server,web-config-server}' \
