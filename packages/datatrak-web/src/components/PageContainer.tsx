4f0e5171caa38cfb3776ced44df130f7ba7269b5
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-04-01 11:24:37 +1300

    dedupe with custom property

--- packages/datatrak-web/src/components/PageContainer.tsx
+++ packages/datatrak-web/src/components/PageContainer.tsx
@@ -25,21 +25,23 @@ export const SafeArea = styled.div<{
   right?: boolean;
 }>(props => {
   return css`
+    --safe-area-min-padding-y: 1rem;
+    --safe-area-min-padding-x: 1.25rem;
     ${props.top &&
     css`
-      padding-top: max(env(safe-area-inset-top, 0), 1rem);
+      padding-top: max(env(safe-area-inset-top, 0), var(--safe-area-min-padding-y));
     `}
     ${props.bottom &&
     css`
-      padding-bottom: max(env(safe-area-inset-bottom, 0), 1rem);
+      padding-bottom: max(env(safe-area-inset-bottom, 0), var(--safe-area-min-padding-y));
     `}
     ${props.left &&
     css`
-      padding-left: max(env(safe-area-inset-left, 0), 1.25rem);
+      padding-left: max(env(safe-area-inset-left, 0), var(--safe-area-min-padding-x));
     `}
     ${props.right &&
     css`
-      padding-right: max(env(safe-area-inset-right, 0), 1.25rem);
+      padding-right: max(env(safe-area-inset-right, 0), var(--safe-area-min-padding-x));
     `}
   `;
 });
