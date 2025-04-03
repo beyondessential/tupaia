4babad9aaa7452a9b6eaba7069ff409d2d35ec6d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-04-03 15:01:55 +1300

    tweak: reusable `<SafeArea>` component (#6175)
    * introduce `<SafeArea>`
    
    * dedupe with custom property
    
    * use in one place (for now)
    
    * move to ui-components
    
    * update JSDoc
    
    * reminder for semantics
    
    * explain absolutes
    
    * JSDoc formatting

---
+++ packages/ui-components/src/components/SafeArea.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/CSS/env
+ * @privateRemarks Using absolute properties because `safe-area-inset-*`
+ * [has no logical equivalents](https://github.com/w3c/csswg-drafts/issues/6379).
+ */
+export const SafeArea = styled.div<{
+  top?: boolean;
+  bottom?: boolean;
+  left?: boolean;
+  right?: boolean;
+}>(props => {
+  return css`
+    --safe-area-min-padding-y: 1rem;
+    --safe-area-min-padding-x: 1.25rem;
+    ${props.top &&
+    css`
+      padding-top: max(env(safe-area-inset-top, 0), var(--safe-area-min-padding-y));
+    `}
+    ${props.bottom &&
+    css`
+      padding-bottom: max(env(safe-area-inset-bottom, 0), var(--safe-area-min-padding-y));
+    `}
+    ${props.left &&
+    css`
+      padding-left: max(env(safe-area-inset-left, 0), var(--safe-area-min-padding-x));
+    `}
+    ${props.right &&
+    css`
+      padding-right: max(env(safe-area-inset-right, 0), var(--safe-area-min-padding-x));
+    `}
+  `;
+});
+
+export const SafeAreaColumn = (props: React.HTMLAttributes<HTMLDivElement>) => (
+  <SafeArea {...props} left right />
+);
+
+/** @privateRemarks Consider setting `as="header"` */
+export const SafeAreaHeader = (props: React.HTMLAttributes<HTMLDivElement>) => (
+  <SafeArea {...props} top />
+);
+
+/** @privateRemarks Consider setting `as="footer"` */
+export const SafeAreaFooter = (props: React.HTMLAttributes<HTMLDivElement>) => (
+  <SafeArea {...props} bottom />
+);
