de191ae0ce99795570b37254cd404fd23ad15320
chris-bes <chris@beyondessential.com.au>
2025-04-11 08:14:04 +1000

    fix export scripts

--- packages/database/src/server/index.js
+++ packages/database/src/server/index.js
@@ -1,5 +1,8 @@
+export * from '../core';
+export * from './testUtilities';
 export { TupaiaDatabase } from './TupaiaDatabase';
 export { getDbMigrator } from './getDbMigrator';
 export { getConnectionConfig } from './getConnectionConfig';
 export { DatabaseChangeChannel } from './DatabaseChangeChannel';
 export { runPostMigration } from './runPostMigration';
+export * from './changeHandlers';
