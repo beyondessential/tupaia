f81ff25f10196612a9e306161496a62f77670fc2
chris-bes <chris@beyondessential.com.au>
2025-04-28 12:17:26 +1000

    use time when creating ObjectID

--- packages/database/src/core/utilities/generateId.js
+++ packages/database/src/core/utilities/generateId.js
@@ -1,9 +1,12 @@
 import ObjectID from 'bson-objectid';
 
 export const generateId = () => {
-  // This module allows you to create and parse ObjectIDs 
+  // This module allows you to create and parse ObjectIDs
   // without a reference to the mongodb or bson modules.
-  return ObjectID().toString();
+  const seconds = Math.floor(new Date() / 1000);
+
+  // Constructs the ID based on the current time so that IDs are ordered by the time they're created
+  return ObjectID(seconds).toString();
 };
 
 export const getHighestPossibleIdForGivenTime = timestamp =>
