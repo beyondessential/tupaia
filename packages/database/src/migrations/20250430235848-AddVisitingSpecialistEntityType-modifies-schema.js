14410b1ed46d5b1198ddc2f357497f2fafc7a9f6
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-01 12:40:14 +1200

    lint

--- packages/database/src/migrations/20250430235848-AddVisitingSpecialistEntityType-modifies-schema.js
+++ packages/database/src/migrations/20250430235848-AddVisitingSpecialistEntityType-modifies-schema.js
@@ -5,23 +5,23 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return db.runSql(`ALTER TYPE public.entity_type ADD VALUE IF NOT EXISTS 'visiting_specialist';`);
+exports.up = function (db) {
+  return db.runSql("ALTER TYPE public.entity_type ADD VALUE IF NOT EXISTS 'visiting_specialist';");
 };
 
-exports.down = function(db) {
+exports.down = function (db) {
   return null;
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
