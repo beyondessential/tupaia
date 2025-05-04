8bafeb52466bbf7dbf6461b3f2aa451e732fa573
chris-bes <chris@beyondessential.com.au>
2025-05-04 16:28:00 +1000

    fixed

--- packages/database/src/server/testUtilities/upsertDummyRecord.js
+++ packages/database/src/server/testUtilities/upsertDummyRecord.js
@@ -1,7 +1,6 @@
 import pluralize from 'pluralize';
 import { generateValueOfType } from './generateValueOfType';
-import { generateId } from '../utilities';
-import { RECORDS } from '../records';
+import { generateId, RECORDS } from '../../core';
 
 const { ENTITY, SURVEY_RESPONSE } = RECORDS;
 
