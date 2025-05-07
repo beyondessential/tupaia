aaaa26623ad03b70a8b65a06ca85ec9a9e388d76
chris-bes <chris@beyondessential.com.au>
2025-05-07 17:27:15 +1000

    fix pglite error

--- packages/database/src/browser/getConnectionConfig.js
+++
@@ -1,5 +0,0 @@
-import { getEnvVarOrDefault } from '@tupaia/utils';
-
-export const getConnectionConfig = () => ({
-  connectionString: getEnvVarOrDefault('PG_LITE_CONNECTION_STRING', 'idb://datatrak-db'),
-});
