eb6c94b54213913d046a10d5e6d09de593b65336
chris-bes <chris@beyondessential.com.au>
2025-05-08 11:01:43 +1000

    test

--- packages/datatrak-web/src/database/createDatabase.ts
+++ packages/datatrak-web/src/database/createDatabase.ts
@@ -1,15 +1,14 @@
-import { ModelRegistry } from '@tupaia/database';
+import { ModelRegistry, MigrationManager } from '@tupaia/database';
 
 import { DatatrakDatabase } from './DatatrakDatabase';
-// import { MigrationManager } from './MigrationManager';
 
 export const createDatabase = async () => {
   const database = new DatatrakDatabase();
   const models = new ModelRegistry(database);
 
   // TODO: Move this to when app is started when service worker is set up
-  //  const migrationManager = new MigrationManager(database);
-  //  await migrationManager.initialize();
-  //  await migrationManager.migrate();
+   const migrationManager = new MigrationManager(database);
+   await migrationManager.initialize();
+   await migrationManager.migrate();
   return { models };
 };
