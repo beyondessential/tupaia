4f8e6342aab9e13ffcba2cb10d003a25142247e5
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-19 10:53:51 +1200

    give up desktop filter state restoration 😭

--- packages/datatrak-web/src/features/Tasks/TasksTable/useDisableDesktopTaskFiltersWhileMounted.tsx
+++ packages/datatrak-web/src/features/Tasks/TasksTable/useDisableDesktopTaskFiltersWhileMounted.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useTasksTable } from './useTasksTable';
 
 /**
@@ -7,19 +7,7 @@ import { useTasksTable } from './useTasksTable';
  * whilst the toggles aren’t mounted (e.g. due to a change to the viewport’s size class).
  */
 export function useDisableDesktopTaskFiltersWhileMounted() {
-  const {
-    showAllAssignees,
-    setShowAllAssignees,
-    showCancelled,
-    setShowCancelled,
-    showCompleted,
-    setShowCompleted,
-  } = useTasksTable();
-
-  // Get current settings, to be restored upon unmount
-  const [allAssignees] = useState(showAllAssignees);
-  const [cancelled] = useState(showCancelled);
-  const [completed] = useState(showCompleted);
+  const { setShowAllAssignees, setShowCancelled, setShowCompleted } = useTasksTable();
 
   // When calling component mounts, set sensible defaults
   useEffect(
@@ -30,19 +18,5 @@ export function useDisableDesktopTaskFiltersWhileMounted() {
     },
     // Effect is idempotent, so exhaustively declaring dependencies is actually optional
     [setShowAllAssignees, setShowCancelled, setShowCompleted],
-  );
-
-  useEffect(
-    () => {
-      return () => {
-        // Restore previous settings
-        setShowAllAssignees(allAssignees);
-        setShowCancelled(cancelled);
-        setShowCompleted(completed);
-      };
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // Expressly want cleanup function to run only when caller unmounts
-    [],
   );
 }
