af8c6a9465adb6526502e640b8272ecaf1e11aa9
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-19 13:41:41 +1200

    Revert "give up desktop filter state restoration 😭"
    This reverts commit 4f8e6342aab9e13ffcba2cb10d003a25142247e5.
    
    # Conflicts:
    #	packages/datatrak-web/src/features/Tasks/TasksTable/useDisableDesktopTaskFiltersWhileMounted.tsx

--- packages/datatrak-web/src/features/Tasks/TasksTable/useDisableDesktopTaskFiltersWhileMounted.tsx
+++ packages/datatrak-web/src/features/Tasks/TasksTable/useDisableDesktopTaskFiltersWhileMounted.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTasksTable } from './useTasksTable';
 
 /**
@@ -7,12 +7,42 @@ import { useTasksTable } from './useTasksTable';
  * whilst the toggles aren’t mounted (e.g. due to a change to the viewport’s size class).
  */
 export function useDisableDesktopTaskFiltersWhileMounted() {
-  const { setShowAllAssignees, setShowCancelled, setShowCompleted } = useTasksTable();
+  const {
+    showAllAssignees,
+    setShowAllAssignees,
+    showCancelled,
+    setShowCancelled,
+    showCompleted,
+    setShowCompleted,
+  } = useTasksTable();
+
+  // Get current settings, to be restored upon unmount
+  const [allAssignees] = useState(showAllAssignees);
+  const [cancelled] = useState(showCancelled);
+  const [completed] = useState(showCompleted);
 
   // When calling component mounts, set sensible defaults
-  useEffect(() => {
-    setShowAllAssignees(false);
-    setShowCancelled(true);
-    setShowCompleted(true);
-  }, [setShowAllAssignees, setShowCancelled, setShowCompleted]);
+  useEffect(
+    () => {
+      setShowAllAssignees(false);
+      setShowCancelled(true);
+      setShowCompleted(true);
+    },
+    // Effect is idempotent, so exhaustively declaring dependencies is actually optional
+    [setShowAllAssignees, setShowCancelled, setShowCompleted],
+  );
+
+  useEffect(
+    () => {
+      return () => {
+        // Restore previous settings
+        setShowAllAssignees(allAssignees);
+        setShowCancelled(cancelled);
+        setShowCompleted(completed);
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // Expressly want cleanup function to run only when caller unmounts
+    [],
+  );
 }
