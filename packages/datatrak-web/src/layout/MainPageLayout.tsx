e8cc637b1c7b616263ccfc9b8dbeac56456a22b6
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-19 15:59:35 +1200

    hide bottom navigation on some pages

--- packages/datatrak-web/src/layout/MainPageLayout.tsx
+++ packages/datatrak-web/src/layout/MainPageLayout.tsx
@@ -59,13 +59,31 @@ const useHeaderVisibility = () => {
   return !desktopHeaderlessRoutes.some(pathPattern => matchPath(pathPattern, pathname));
 };
 
+const useBottomNavigationBarVisibility = () => {
+  const { pathname } = useLocation();
+  const blockList = [
+    ROUTES.EXPORT_SURVEY_RESPONSE,
+    ROUTES.FORGOT_PASSWORD,
+    ROUTES.LOGIN,
+    ROUTES.REGISTER,
+    ROUTES.REPORTS,
+    ROUTES.RESET_PASSWORD,
+    ROUTES.SYNC,
+    ROUTES.VERIFY_EMAIL_RESEND,
+    ROUTES.VERIFY_EMAIL,
+    ROUTES.WELCOME,
+  ];
+  return !blockList.some(pathPattern => matchPath(pathPattern, pathname));
+};
+
 export const MainPageLayout = () => {
   const showHeader = useHeaderVisibility();
+  const showBottomNavigation = useBottomNavigationBarVisibility();
   return (
     <PageWrapper>
       {showHeader && <Header />}
       <Outlet />
-      <Nav />
+      {showBottomNavigation && <Nav />}
 
       <SurveyResponseModal />
     </PageWrapper>
