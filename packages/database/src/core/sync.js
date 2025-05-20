47545489dd2d75bbf04b36c81fd003bcb6dc6570
chris-bes <chris@beyondessential.com.au>
2025-05-20 14:08:08 +1000

    removed redundant file

--- packages/database/src/core/sync.js
+++
@@ -1,6 +0,0 @@
-export const SYNC_DIRECTIONS = {
-  BIDIRECTIONAL: 'bidirectional',
-  PUSH_TO_CENTRAL: 'push_to_central',
-  PULL_FROM_CENTRAL: 'pull_from_central',
-  DO_NOT_SYNC: 'do_not_sync',
-};
