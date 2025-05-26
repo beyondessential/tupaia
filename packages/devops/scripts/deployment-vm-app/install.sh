5c99c98764a20e998adc8c5531de456e7aa73e5b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-26 13:41:11 +1200

    suppress output of secrets

--- packages/devops/scripts/deployment-vm-app/install.sh
+++ packages/devops/scripts/deployment-vm-app/install.sh
@@ -10,8 +10,6 @@ cd "$SCRIPT_DIR"
 
 ./checkRequiredEnvVars.sh
 
-set -x
-
 ../deployment-common/configureNginx.sh
 
 # clone our repo
@@ -36,16 +34,17 @@ corepack enable yarn
 yarn install --immutable
 
 # Fetch env vars
+set +x # Suppress output of Bitwarden secrets
 BW_CLIENTID="$BW_CLIENTID" \
     BW_CLIENTSECRET="$BW_CLIENTSECRET" \
     BW_PASSWORD="$BW_PASSWORD" \
     yarn run download-env-vars "$DEPLOYMENT_NAME"
+set -x
 
 # Build packages and their dependencies
 package_names_glob=$("$TUPAIA_DIR/scripts/bash/getDeployablePackages.sh" --as-glob)
-set -x
+
 REACT_APP_DEPLOYMENT_NAME="$DEPLOYMENT_NAME" \
     yarn run build:from "$package_names_glob"
-set +x
 
 echo "Tupaia installed successfully"
