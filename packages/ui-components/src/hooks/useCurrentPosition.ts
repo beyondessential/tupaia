600f0c731f9101119a796470c14011ae1d78b593
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-26 12:49:19 +1200

    error if no `!navigator.geolocation`

--- packages/ui-components/src/hooks/useCurrentPosition.ts
+++ packages/ui-components/src/hooks/useCurrentPosition.ts
@@ -2,32 +2,42 @@
 
 import { useEffect, useState } from 'react';
 
+const noSupportError = {
+  code: GeolocationPositionError.POSITION_UNAVAILABLE,
+  message: 'Your device or browser doesn’t support location services',
+} as const;
+
 export function useCurrentPosition({
   enableHighAccuracy = true,
   timeout,
   maximumAge,
 }: PositionOptions) {
   const [position, setPosition] = useState<GeolocationPosition | null>(null);
-  const [error, setError] = useState<GeolocationPositionError | null>(null);
+  const [error, setError] = useState<GeolocationPositionError | typeof noSupportError | null>(null);
 
   useEffect(() => {
     let aborted = false;
 
-    const successCallback: PositionCallback = pos => {
+    if (!('geolocation' in navigator)) {
       if (aborted) return;
-      setPosition(pos);
-      setError(null);
-    };
-    const errorCallback: PositionErrorCallback = err => {
-      if (aborted) return;
-      setError(err);
-    };
+      setError(noSupportError);
+    } else {
+      const successCallback: PositionCallback = pos => {
+        if (aborted) return;
+        setPosition(pos);
+        setError(null);
+      };
+      const errorCallback: PositionErrorCallback = err => {
+        if (aborted) return;
+        setError(err);
+      };
 
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
-      enableHighAccuracy,
-      timeout,
-      maximumAge,
-    });
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
+        enableHighAccuracy,
+        timeout,
+        maximumAge,
+      });
+    }
 
     return () => {
       aborted = true;
