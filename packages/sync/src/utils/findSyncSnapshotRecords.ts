8b56b256b3c01671c76b8b36c0b297352c942ef1
chris-bes <chris@beyondessential.com.au>
2025-05-28 15:35:43 +1000

    removed console log

--- packages/sync/src/utils/findSyncSnapshotRecords.ts
+++ packages/sync/src/utils/findSyncSnapshotRecords.ts
@@ -33,7 +33,6 @@ export const findSyncSnapshotRecords = async (
     },
   )) as SyncSnapshotAttributes[];
 
-  console.log('recordssss', records);
   return records.map(r =>
     Object.fromEntries(Object.entries(r).map(([key, value]) => [camel(key), value])),
   ) as SyncSnapshotAttributes[];
