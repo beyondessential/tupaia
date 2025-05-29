61c2bc1906128fd43bde9a4618f8bfe7ae945755
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-29 15:50:37 +1200

    narrow return type

--- packages/datatrak-web/src/components/BottomNavigation/useBottomNavigationActiveTab.ts
+++ packages/datatrak-web/src/components/BottomNavigation/useBottomNavigationActiveTab.ts
@@ -3,7 +3,7 @@ import { matchPath, useLocation } from 'react-router';
 import type { TabValue } from './BottomNavigation';
 import { ROUTES } from '../../constants';
 
-export function useBottomNavigationActiveTab(): TabValue | null {
+export function useBottomNavigationActiveTab(): TabValue  {
   const { pathname } = useLocation();
   if (matchPath(`${ROUTES.SURVEY_SELECT}/*`, pathname)) return 'surveys';
   if (matchPath(`${ROUTES.TASKS}/*`, pathname)) return 'tasks';
