e7930f34a1ea761a5f3761616b964b67ae0ec9fc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-30 15:39:42 +1200

    make CG accessible via `getCountryCode`

--- packages/utils/src/getCountryCode.js
+++ packages/utils/src/getCountryCode.js
@@ -1,9 +1,12 @@
+import { getCode as getCountryIsoCode } from 'countrynames';
 import { ImportValidationError } from './errors';
 
 // We sometimes use non-official names for countries. The most obvious example is Demo Land, but
 // also Laos instead of Laos People's Democratic Republic, etc.
 const UNOFFICIAL_NAME_TO_CODE = {
   'demo land': 'DL',
+
+  'congo, the': 'CG',
   laos: 'LA',
   "côte d'ivoire": 'CI',
   'federated states of micronesia': 'FM',
@@ -11,9 +14,8 @@ const UNOFFICIAL_NAME_TO_CODE = {
 };
 
 export const getCountryCode = countryName => {
-  const { getCode: getCountryIsoCode } = require('countrynames');
   const countryCode =
-    UNOFFICIAL_NAME_TO_CODE[countryName.toLowerCase()] || getCountryIsoCode(countryName);
+    UNOFFICIAL_NAME_TO_CODE[countryName.toLowerCase()] ?? getCountryIsoCode(countryName);
   if (!countryCode) throw new ImportValidationError(`${countryName} is not a recognised country`);
   return countryCode;
 };
