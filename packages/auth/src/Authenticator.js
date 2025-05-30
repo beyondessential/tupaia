fc49d53d6c0304e9c4bc1f342cfab5620bc9fa2c
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-05-30 13:04:29 +1200

    parallelise awaits

--- packages/auth/src/Authenticator.js
+++ packages/auth/src/Authenticator.js
@@ -67,8 +67,10 @@ export class Authenticator {
     const user = await this.getAuthenticatedUser({ emailAddress, password, deviceName });
     const meditrakDevice =
       meditrakDeviceDetails && (await this.saveMeditrakDevice(user, meditrakDeviceDetails));
-    const refreshToken = await this.upsertRefreshToken(user.id, deviceName, meditrakDevice);
-    const accessPolicy = await this.getAccessPolicyForUser(user.id, meditrakDevice);
+    const [refreshToken, accessPolicy] = await Promise.all([
+      this.upsertRefreshToken(user.id, deviceName, meditrakDevice),
+      this.getAccessPolicyForUser(user.id, meditrakDevice),
+    ]);
 
     return { refreshToken: refreshToken.token, user, accessPolicy };
   }
@@ -93,8 +95,10 @@ export class Authenticator {
 
     // There was a valid refresh token, find the user and respond
     const userId = refreshToken.user_id;
-    const user = await this.models.user.findById(userId);
-    const meditrakDevice = await refreshToken.meditrakDevice();
+    const [user, meditrakDevice] = await Promise.all([
+      this.models.user.findById(userId),
+      refreshToken.meditrakDevice(),
+    ]);
     const accessPolicy = await this.getAccessPolicyForUser(user.id, meditrakDevice);
 
     return { refreshToken: refreshToken.token, user, accessPolicy };
@@ -110,8 +114,10 @@ export class Authenticator {
     foundToken.save();
 
     const user = await this.models.user.findById(foundToken.user_id);
-    const refreshToken = await this.upsertRefreshToken(user.id, deviceName);
-    const accessPolicy = await this.getAccessPolicyForUser(user.id);
+    const [refreshToken, accessPolicy] = await Promise.all([
+      this.upsertRefreshToken(user.id, deviceName),
+      this.getAccessPolicyForUser(user.id),
+    ]);
 
     return { refreshToken: refreshToken.token, user, accessPolicy };
   }
