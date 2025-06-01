2859fc68a03c13594213de24147c8a4628dcbd57
chris-bes <chris@beyondessential.com.au>
2025-06-02 00:18:53 +1000

    fix

--- packages/datatrak-web/src/database/getConnectionConfig.ts
+++ packages/datatrak-web/src/database/getConnectionConfig.ts
@@ -6,16 +6,8 @@ export const getConnectionConfig = async () => {
   const connectionString = getEnvVarOrDefault('PG_LITE_CONNECTION_STRING', 'idb://datatrak-db');
 
   if (process.env.NODE_ENV === 'production') {
-    const response = await fetch('pglite.data');
-    const fsBundle = await response.blob();
-    const text = await fsBundle.text();
-
-    console.log('fsBundle', fsBundle);
-    console.log('texttt', text);
     return {
-      pglite: new PGlite(connectionString, {
-        fsBundle
-      }),
+      pglite: new PGlite(connectionString),
     };
   }
 
