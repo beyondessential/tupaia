613e395b029afa006d31e237f0c45a485cd7ff56
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-06-06 12:39:22 +1200

    temp: default to debug level

--- packages/server-boilerplate/src/utils/configureWinston.ts
+++ packages/server-boilerplate/src/utils/configureWinston.ts
@@ -52,7 +52,7 @@ export const configureWinston = () => {
           winston.format.colorize(),
           winston.format.simple(),
         ),
-        level: getEnvVarOrDefault('LOG_LEVEL', 'info'),
+        level: getEnvVarOrDefault('LOG_LEVEL', 'debug'),
       }),
     ],
   });
