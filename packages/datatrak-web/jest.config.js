35d94638a5a104179657a28884503d969d876a45
chris-bes <chris@beyondessential.com.au>
2025-06-09 13:28:43 +1000

    test fix

--- packages/datatrak-web/jest.config.js
+++ packages/datatrak-web/jest.config.js
@@ -12,4 +12,20 @@ module.exports = async () => ({
   },
   transformIgnorePatterns: ['/node_modules/(?!(msw)/).*/'],
   setupFiles: ['./jest.setup.js'],
+  '^.+\\.tsx?$': [
+    'ts-jest',
+    {
+      diagnostics: {
+        ignoreCodes: [1343],
+      },
+      astTransformers: {
+        before: [
+          {
+            path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+            options: { metaObjectReplacement: { url: 'https://www.url.com' } },
+          },
+        ],
+      },
+    },
+  ],
 });
