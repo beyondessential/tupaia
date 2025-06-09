7f55c0a85320ded0b867fe20ec171f1967c85479
chris-bes <chris@beyondessential.com.au>
2025-06-09 13:36:27 +1000

    test

--- packages/datatrak-web/jest.config.js
+++ packages/datatrak-web/jest.config.js
@@ -11,21 +11,5 @@ module.exports = async () => ({
     '^axios$': require.resolve('axios'),
   },
   transformIgnorePatterns: ['/node_modules/(?!(msw)/).*/'],
-  setupFiles: ['./jest.setup.js'],
-  '^.+\\.tsx?$': [
-    'ts-jest',
-    {
-      diagnostics: {
-        ignoreCodes: [1343],
-      },
-      astTransformers: {
-        before: [
-          {
-            path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
-            options: { metaObjectReplacement: { url: 'https://www.url.com' } },
-          },
-        ],
-      },
-    },
-  ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
 });
