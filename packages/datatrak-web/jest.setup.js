ef65861ef7cb33f60186fbfb7a23a52cf00ffc95
chris-bes <chris@beyondessential.com.au>
2025-06-09 14:59:23 +1000

    fix

--- packages/datatrak-web/jest.setup.js
+++ packages/datatrak-web/jest.setup.js
@@ -1,9 +1,4 @@
 // TODO: Set up database for testing later
-const { createDatabase } = require('./src/database/createDatabase');
-
-global.importMeta = {
-  glob: jest.fn().mockResolvedValue({}),
-};
 
 jest.mock('./src/database/createDatabase', () => ({
   createDatabase: jest.fn().mockResolvedValue({
--- packages/datatrak-web/src/api/DatabaseContext.tsx
+++ packages/datatrak-web/src/api/DatabaseContext.tsx
@@ -5,17 +5,22 @@ import { DatatrakWebUserRequest } from '@tupaia/types';
 import { createDatabase } from '../database/createDatabase';
 import { DatatrakWebModelRegistry } from '../types/model';
 
-export type DatabaseContextType = DatatrakWebUserRequest.ResBody & { models: DatatrakWebModelRegistry | null };
+export type DatabaseContextType = DatatrakWebUserRequest.ResBody & {
+  models: DatatrakWebModelRegistry | null;
+};
 
 const DatabaseContext = createContext<DatabaseContextType | null>(null);
 
 export const DatabaseProvider = ({ children }: { children: React.ReactNode }) => {
   const [models, setModels] = useState<DatatrakWebModelRegistry | null>(null);
-  
+
   useEffect(() => {
     const init = async () => {
-      const { models } = await createDatabase();
-      setModels(models);
+      // // TODO: Remove this once we have a proper way to test the database for front end in RN-1680
+      if (!process.env.JEST_WORKER_ID) {
+        const { models } = await createDatabase();
+        setModels(models);
+      }
     };
 
     init();
@@ -30,6 +35,6 @@ export const useDatabase = (): DatabaseContextType => {
   if (!context) {
     throw new Error('useDatabase must be used within a DatabaseProvider');
   }
-  
+
   return context;
 };
