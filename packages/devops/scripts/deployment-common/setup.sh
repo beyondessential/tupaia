220116d08c5c07cab1f6b7e9a3e2842325c2f138
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-06-17 09:09:08 +1200

    clean up diff

--- packages/devops/scripts/deployment-common/setup.sh
+++ packages/devops/scripts/deployment-common/setup.sh
@@ -108,7 +108,8 @@ echo "PM2 $(pm2 --version) is installed"
 
 # install bitwarden
 if ! command -v bw &>/dev/null; then
-  echo 'Bitwarden CLI not installed. Installing...'  # Avoid 2025.5.0, which has a known issue. See https://github.com/bitwarden/clients/issues/14995
+  echo 'Bitwarden CLI not installed. Installing...'
+  # Avoid 2025.5.0, which has a known issue. See https://github.com/bitwarden/clients/issues/14995
   npm install --global '@bitwarden/cli@<2025.5.0 || >2025.5.0'
 elif ! bw --version &>/dev/null; then
   echo "Bad Bitwarden CLI version installed (probably 2025.5.0). Replacing with '<2025.5.0 || >2025.5.0'..."
