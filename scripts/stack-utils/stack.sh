dcfa74ee5c11233a7a00457ab9cf4dc2d873e561
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-06-23 09:11:00 +1200

    help

--- scripts/stack-utils/stack.sh
+++ scripts/stack-utils/stack.sh
@@ -16,28 +16,56 @@ fi
 root_dir=$(realpath -- "$script_dir"/../..)
 . "$root_dir"/scripts/bash/ansiControlSequences.sh
 
-print_help_message() {
-	command_table="
-  ${BOLD}${MAGENTA}list${RESET}/List the packages in the named server stacks (or ${BOLD}${MAGENTA}ls${RESET})
-  ${BOLD}${MAGENTA}build${RESET}/Build the packages in the named server stacks and their dependencies
-  ${BOLD}${MAGENTA}start${RESET}/Start all the servers in the named stack using PM2
-"
-
+print_synopsis() {
 	echo -e "${BOLD}stack${RESET} is a small set of convenience commands for working with server stacks in the Tupaia monorepo."
-	echo
+}
+
+print_usage() {
+	echo -e "\n${BOLD}USAGE${RESET}"
+	echo -e "  yarn run stack [-h | --help]"
+	echo -e "  yarn run stack ${UNDERLINE}subcommand${RESET} [-h | --help] ${UNDERLINE}stack...${RESET}"
+}
+
+print_subcommands() {
+	local command_table="
+  ${BOLD}${MAGENTA}list${RESET} ${DIM}${UNDERLINE}stack...${RESET}/List the packages in the named server ${UNDERLINE}stack${RESET}s (or ${BOLD}${MAGENTA}ls${RESET})
+  ${BOLD}${MAGENTA}build${RESET} ${DIM}${UNDERLINE}stack...${RESET}/Build the packages in the named server ${UNDERLINE}stack${RESET}s and their dependencies
+  ${BOLD}${MAGENTA}start${RESET} ${DIM}${UNDERLINE}stack${RESET}/Start all the servers in the named ${UNDERLINE}stack${RESET} using PM2"
+	echo -e "\n${BOLD}SUBCOMMANDS${RESET}"
 	echo -e "$command_table" | column -t -s /
-	echo
-	echo -e "All these commands may be called with ${BOLD}--help${RESET}"
+}
+
+print_examples() {
+	echo -e "\n${BOLD}EXAMPLES${RESET}"
+	echo -e "  ${DIM}>${RESET} yarn run ${GREEN}stack ${MAGENTA}build ${BLUE}admin-panel datatrak${RESET}"
+	echo -e "  ${DIM}>${RESET} yarn run ${GREEN}stack ${MAGENTA}list ${BLUE}admin-panel datatrak${RESET}"
+	echo -e "  ${DIM}>${RESET} yarn run ${GREEN}stack ${MAGENTA}start ${BLUE}admin-panel datatrak${RESET}"
+}
+
+print_help() {
+	print_synopsis "$@"
+	print_usage "$@"
+
+	if [[ $1 = --concise ]]; then
+		print_examples "$@"
+		echo -e "\nFor more verbose help, use ${BOLD}yarn run stack --help${RESET}.  Each subcommand may also be called with ${BOLD}--help${RESET}."
+		return
+	fi
+
+	print_subcommands "$@"
+	print_examples "$@"
+	echo -e "\n${BOLD}SEE ALSO${RESET}"
+	echo -e "  • ${DIM}$script_dir/${RESET}$(basename -- "${BASH_SOURCE[0]}")"
 }
 
 case "$1" in
 
 # Flag
 -h | --help)
-	print_help_message
+	print_help
 	;;
 
-# Commands
+# Subcommands
 list | ls)
 	shift
 	node "$script_dir"/stack-list "$@" "${valid_stacks[@]}"
@@ -56,7 +84,7 @@ start)
 
 # Missing command
 *)
-	print_help_message
+	print_help --concise
 	exit 1
 	;;
 
