729c849d36d502cbe1412a02a9c9d360dad2530a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-06-30 17:36:38 +1200

    lint
    lint

--- scripts/bash/backendStartDev.sh
+++ scripts/bash/backendStartDev.sh
@@ -6,17 +6,16 @@
 
 set -e
 
-DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-. "$DIR/ansiControlSequences.sh"
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+. "$script_dir/ansiControlSequences.sh"
 
-USAGE="Usage: ${BOLD}backendStartDev babel_port_inspector${RESET} [${BOLD}-i${RESET}|${BOLD}--include-internal${RESET}] [${BOLD}-ts${RESET}|${BOLD}--typescript${RESET}]"
 watch_flags=''
 include_internal=false
 type_script=false
-inspect_port=${1}
+inspect_port=$1
 
 # Start server command for JS
-start_server="nodemon -w src --exec \"babel-node src --inspect=${inspect_port} --config-file '../../babel.config.json'\""
+start_server=nodemon -w src --exec "babel-node src --inspect=${inspect_port@Q} --config-file '../../babel.config.json'"
 
 while [[ $2 != '' ]]; do
     case $2 in
@@ -28,12 +27,8 @@ while [[ $2 != '' ]]; do
         include_internal=true
         shift
         ;;
-    -s | --skip-internal)
-        echo -e "Skipping internal dependencies is now done by default. Remove the ${BOLD}--skip-internal${RESET} (${BOLD}-s${RESET}) flag, and if you want to include internal dependencies, add a ${BOLD}-i${RESET}. (Do try it - it’s a lot faster than it used to be, because it only builds those relevant to the current package$EXCLAMATION_MARK)"
-        exit 1
-        ;;
     *)
-        echo -e "$USAGE"
+        echo -e "Usage: ${BOLD}backendStartDev babel_port_inspector${RESET} [${BOLD}-i${RESET}|${BOLD}--include-internal${RESET}] [${BOLD}-ts${RESET}|${BOLD}--typescript${RESET}]"
         exit 1
         ;;
     esac
@@ -41,7 +36,7 @@ done
 
 # Start server command for TS
 if [[ $type_script = true ]]; then
-    start_server="nodemon --watch src -e ts,json,js --exec node --inspect=${inspect_port} -r ts-node/register src/index.ts"
+    start_server="nodemon --watch src -e ts,json,js --exec node --inspect=${inspect_port@Q} -r ts-node/register src/index.ts"
 fi
 
 echo -e "${BOLD}Starting server...${RESET}"
@@ -49,14 +44,15 @@ echo -e "${BOLD}Starting server...${RESET}"
 # If internal dependencies are included, add them to the watch list. This will watch for changes to the dist folder of each internal dependency. If the internal dependency then gets rebuilt, the server will restart.
 if [[ $include_internal = true ]]; then
     echo 'Internal dependencies are under watch for hot reload'
-    for PACKAGE in $("$DIR/getInternalDependencies.sh"); do
-        watch_flags+=" --watch ../$PACKAGE/dist"
+    for package in $("$DIR"/getInternalDependencies.sh); do
+        dist_path=../$package/dist
+        watch_flags+=" --watch ${dist_path@Q}"
     done
     # add the watch flags to the server start process, as well as a 1 second delay to debounce the
     # many restarts that otherwise happen during the initial build of internal dependencies
     start_server+=" --delay 1 $watch_flags"
 else
-    echo -e "Starting server without internal dependency build and watch. To include internal dependencies, add the ${BOLD}-i${RESET} flag. (It’s much faster than it used to be!)"
+    echo -e "Starting server without internal dependency build and watch. To include internal dependencies, add the ${BOLD}-i${RESET} flag."
 fi
 
 eval "$start_server"
