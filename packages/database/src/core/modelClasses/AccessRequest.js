15bcefe741c0c6b25910e3f2baf3884b0649bdf7
chris-bes <chris@beyondessential.com.au>
2025-07-09 22:12:07 +1000

    fix

--- packages/database/src/core/modelClasses/AccessRequest.js
+++ packages/database/src/core/modelClasses/AccessRequest.js
@@ -1,4 +1,5 @@
 import { SyncDirections } from '@tupaia/constants';
+
 import { DatabaseModel } from '../DatabaseModel';
 import { DatabaseRecord } from '../DatabaseRecord';
 import { RECORDS } from '../records';
