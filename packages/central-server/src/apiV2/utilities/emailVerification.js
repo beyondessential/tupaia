cb5886de491a367d54e51b0c3d974a5ff7ddc0d6
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-07-10 15:24:44 +1200

    handle old tokens
    extract

--- packages/central-server/src/apiV2/utilities/emailVerification.js
+++ packages/central-server/src/apiV2/utilities/emailVerification.js
@@ -1,4 +1,4 @@
-import { encryptPassword, verifyPassword } from '@tupaia/auth';
+import { encryptPassword, sha256EncryptPassword as sha256, verifyPassword } from '@tupaia/auth';
 import { sendEmail } from '@tupaia/server-utils';
 import { requireEnv } from '@tupaia/utils';
 
@@ -26,6 +26,12 @@ const ORIGINS = {
 
 const getEmailVerificationToken = user => `${user.email}${user.password_hash}`;
 
+/**
+ * Verification tokens prior to our adoption of Argon2 were generated with
+ * {@link sha256}.
+ */
+const isLegacyToken = token => /^[0-9a-f]{64}$/.test(token);
+
 export const sendEmailVerification = async user => {
   const token = await encryptPassword(getEmailVerificationToken(user));
   const platform = user.primary_platform ? user.primary_platform : 'tupaia';
@@ -49,17 +55,27 @@ export const sendEmailVerification = async user => {
   });
 };
 
-export const verifyEmailHelper = async (models, searchCondition, token) => {
-  const users = await models.user.find({
-    verified_email: searchCondition,
+const legacyVerifyEmailHelper = async (models, searchCondition, token) => {
+  const users = await models.user.find({ verified_email: searchCondition });
+  return users.find(user => {
+    if (!user.password_hash_old || !user.password_salt) return false;
+    const legacyToken = sha256(`${user.email}${user.password_hash_old}`, user.password_salt);
+    return legacyToken === token;
   });
+};
+
+export const verifyEmailHelper = async (models, searchCondition, token) => {
+  // Token may have been generated prior to adoption of Argon2. Fall back to legacy method.
+  if (isLegacyToken(token)) {
+    return (await legacyVerifyEmailHelper(models, searchCondition, token)) ?? null;
+  }
+
+  const users = await models.user.find({ verified_email: searchCondition });
 
   for (const user of users) {
     const verified = await verifyPassword(getEmailVerificationToken(user), token);
-
-    if (verified) {
-      return user;
-    }
+    if (verified) return user;
   }
+
   return null;
 };
