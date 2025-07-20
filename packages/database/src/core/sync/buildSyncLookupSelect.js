76b305acb86ec9d965c5dd42c357ceb258c92cb2
chris-bes <chris@beyondessential.com.au>
2025-07-21 09:11:52 +1000

    fix

--- packages/database/src/core/sync/buildSyncLookupSelect.js
+++ packages/database/src/core/sync/buildSyncLookupSelect.js
@@ -16,7 +16,6 @@ export async function buildSyncLookupSelect(model, columns = {}) {
           .filter(a => !COLUMNS_EXCLUDED_FROM_SYNC.includes(a))
           .map(a => `'${a}', ${table}.${a}`)}
       ),
-      ${projectIds || 'NULL'},
-      tombstone.record_id IS NOT NULL
+      ${projectIds || 'NULL'}
   `;
 }
