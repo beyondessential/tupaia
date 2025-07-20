e9e7a0d4b541549cf5ae088584168c476bd5f611
chris-bes <chris@beyondessential.com.au>
2025-07-20 14:45:49 +1000

    fix

--- packages/datatrak-web/src/database/getConnectionConfig.ts
+++ packages/datatrak-web/src/database/getConnectionConfig.ts
@@ -2,16 +2,13 @@ import { getEnvVarOrDefault } from '@tupaia/utils';
 import { getPGliteInstance } from './getPGliteInstance';
 
 export const getConnectionConfig = () => {
-  return {
-    pglite: getPGliteInstance(),
-  };
-  // if (process.env.NODE_ENV === 'production') {
-  //   return {
-  //     pglite: getPGliteInstance(),
-  //   };
-  // }
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      pglite: getPGliteInstance(),
+    };
+  }
 
-  // return {
-  //   connectionString: getEnvVarOrDefault('PG_LITE_CONNECTION_STRING', 'idb://datatrak-db'),
-  // };
+  return {
+    connectionString: getEnvVarOrDefault('PG_LITE_CONNECTION_STRING', 'idb://datatrak-db'),
+  };
 };
