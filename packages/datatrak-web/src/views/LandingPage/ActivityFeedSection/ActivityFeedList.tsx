c705faaf0fb1e8140a25169567a6e19d2e84b2fc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-07-21 10:16:58 +1200

    more readable

--- packages/datatrak-web/src/views/LandingPage/ActivityFeedSection/ActivityFeedList.tsx
+++ packages/datatrak-web/src/views/LandingPage/ActivityFeedSection/ActivityFeedList.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
 import { Link } from '@material-ui/core';
 import { DatatrakWebActivityFeedRequest, FeedItemTypes } from '@tupaia/types';
+import React from 'react';
+import styled from 'styled-components';
 import { MarkdownFeedItem, SurveyResponseFeedItem } from '../../../types';
-import { ActivityFeedSurveyItem } from './ActivityFeedSurveyItem';
-import { ActivityFeedMarkdownItem } from './ActivityFeedMarkdownItem';
-import { PinnedFeedItem } from './PinnedFeedItem';
+import { isNotNullish } from '../../../utils';
 import { ActivityFeedItem } from './ActivityFeedItem';
+import { ActivityFeedMarkdownItem } from './ActivityFeedMarkdownItem';
+import { ActivityFeedSurveyItem } from './ActivityFeedSurveyItem';
+import { PinnedFeedItem } from './PinnedFeedItem';
 
 const List = styled.ul`
   border-radius: 10px;
@@ -23,27 +24,29 @@ interface ActivityFeedListProps {
 }
 
 export const ActivityFeedList = ({ items }: ActivityFeedListProps) => {
+  // HACK: Sometimes get `undefined` items in test environment (culprit: .../__integration__/login.test.tsx)
+  // TODO: Investigate why this is happening. Likely due to mocking of feed items API response.
+  const ensuredItems = items?.filter(isNotNullish);
+
   return (
     <List>
       <PinnedFeedItem />
       {/** Creating a new flattened array out of these pages caused a re-render on all updates which led to some bugs, so doing a nested map is better here */}
-      {items
-        ?.filter(Boolean) // HACK: sometimes get `undefined` items in test environment
-        .map(feedItem => (
-          <ActivityFeedItem
-            key={feedItem.id}
-            button={!!feedItem?.templateVariables?.link}
-            component={feedItem?.templateVariables?.link ? Link : undefined}
-            href={feedItem?.templateVariables?.link}
-            target="_blank"
-          >
-            {feedItem.type === FeedItemTypes.SurveyResponse ? (
-              <ActivityFeedSurveyItem feedItem={feedItem as SurveyResponseFeedItem} />
-            ) : (
-              <ActivityFeedMarkdownItem feedItem={feedItem as MarkdownFeedItem} />
-            )}
-          </ActivityFeedItem>
-        ))}
+      {ensuredItems?.map(feedItem => (
+        <ActivityFeedItem
+          key={feedItem.id}
+          button={!!feedItem?.templateVariables?.link}
+          component={feedItem?.templateVariables?.link ? Link : undefined}
+          href={feedItem?.templateVariables?.link}
+          target="_blank"
+        >
+          {feedItem.type === FeedItemTypes.SurveyResponse ? (
+            <ActivityFeedSurveyItem feedItem={feedItem as SurveyResponseFeedItem} />
+          ) : (
+            <ActivityFeedMarkdownItem feedItem={feedItem as MarkdownFeedItem} />
+          )}
+        </ActivityFeedItem>
+      ))}
     </List>
   );
 };
