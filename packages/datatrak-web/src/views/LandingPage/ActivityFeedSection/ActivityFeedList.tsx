86d53ec6fb63c76dcf528b79adf4888bdfd72b95
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-07-21 17:19:42 +1200

    fix(datatrakWeb): flaky test (#6316)
    * fix????
    
    * more readable
    
    * organise imports

--- packages/datatrak-web/src/views/LandingPage/ActivityFeedSection/ActivityFeedList.tsx
+++ packages/datatrak-web/src/views/LandingPage/ActivityFeedSection/ActivityFeedList.tsx
@@ -1,12 +1,15 @@
+import { Link } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from '@material-ui/core';
+
 import { DatatrakWebActivityFeedRequest, FeedItemTypes } from '@tupaia/types';
+
 import { MarkdownFeedItem, SurveyResponseFeedItem } from '../../../types';
-import { ActivityFeedSurveyItem } from './ActivityFeedSurveyItem';
-import { ActivityFeedMarkdownItem } from './ActivityFeedMarkdownItem';
-import { PinnedFeedItem } from './PinnedFeedItem';
+import { isNotNullish } from '../../../utils';
 import { ActivityFeedItem } from './ActivityFeedItem';
+import { ActivityFeedMarkdownItem } from './ActivityFeedMarkdownItem';
+import { ActivityFeedSurveyItem } from './ActivityFeedSurveyItem';
+import { PinnedFeedItem } from './PinnedFeedItem';
 
 const List = styled.ul`
   border-radius: 10px;
@@ -22,12 +25,16 @@ interface ActivityFeedListProps {
   items?: DatatrakWebActivityFeedRequest.ResBody['items'];
 }
 
-export const ActivityFeedList = ({ items = [] }: ActivityFeedListProps) => {
+export const ActivityFeedList = ({ items }: ActivityFeedListProps) => {
+  // HACK: Sometimes get `undefined` items in test environment (culprit: .../__integration__/login.test.tsx)
+  // TODO: Investigate why this is happening. Likely due to mocking of feed items API response.
+  const ensuredItems = items?.filter(isNotNullish);
+
   return (
     <List>
       <PinnedFeedItem />
       {/** Creating a new flattened array out of these pages caused a re-render on all updates which led to some bugs, so doing a nested map is better here */}
-      {items?.map(feedItem => (
+      {ensuredItems?.map(feedItem => (
         <ActivityFeedItem
           key={feedItem.id}
           button={!!feedItem?.templateVariables?.link}
