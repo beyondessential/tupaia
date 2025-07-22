eeb3eb2c28a29ef4aa4bdacc73c357369a2c2043
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-07-22 12:12:35 +1200

    fix

--- packages/tupaia-web/src/utils/useEntityLink.ts
+++ packages/tupaia-web/src/utils/useEntityLink.ts
@@ -1,18 +1,27 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { Path, useLocation, useParams } from 'react-router-dom';
 import { useProject } from '../api/queries';
+import { useMemo } from 'react';
 
-export const useEntityLink = (entityCode?: string) => {
-  const location = useLocation();
+/**
+ * @returns `undefined` if and only if the eventual return value is pending.
+ */
+export const useEntityLink = (entityCode?: string): Partial<Path> | undefined => {
+  const { hash, search } = useLocation();
   const { projectCode, entityCode: entityCodeParam } = useParams();
   const { data: project } = useProject(projectCode);
 
   // If entityCode is not provided, use the one from the URL
   const newEntityCode = entityCode || entityCodeParam;
+  const dashboardGroupName = project?.dashboardGroupName
+    ? encodeURIComponent(project.dashboardGroupName)
+    : '';
 
-  return {
-    ...location,
-    pathname: `/${projectCode}/${newEntityCode}/${
-      project?.dashboardGroupName ? encodeURIComponent(project.dashboardGroupName) : ''
-    }`,
-  };
+  return useMemo(() => {
+    if (!projectCode || !newEntityCode || !dashboardGroupName) return undefined;
+    return {
+      hash,
+      pathname: `/${projectCode}/${newEntityCode}/${dashboardGroupName}`,
+      search,
+    };
+  }, [dashboardGroupName, hash, newEntityCode, projectCode, search]);
 };
