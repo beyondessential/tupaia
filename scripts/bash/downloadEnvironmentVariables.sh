ed5465cdd423cb01b1d56b5b32c72abb0cdbf457
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-07-24 11:51:49 +1200

    more robust script dir

--- scripts/bash/downloadEnvironmentVariables.sh
+++ scripts/bash/downloadEnvironmentVariables.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e +x # Do not output commands in this script, as some would show credentials in plain text
 
-DIR=$(dirname "$0")
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 if ! "$DIR"/requireCommands.sh yarn jq; then
     exit 1
