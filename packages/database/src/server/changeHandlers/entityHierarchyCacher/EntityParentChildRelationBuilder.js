f795475c150dadbf59d355fd8e6bdc1ff9b14c1b
chris-bes <chris@beyondessential.com.au>
2025-07-26 20:57:30 +1000

    revert

--- packages/database/src/server/changeHandlers/entityHierarchyCacher/EntityParentChildRelationBuilder.js
+++ packages/database/src/server/changeHandlers/entityHierarchyCacher/EntityParentChildRelationBuilder.js
@@ -191,6 +191,12 @@ export class EntityParentChildRelationBuilder {
           )
         `);
 
+        await transactingDatabase.executeSql(`
+          CREATE TEMPORARY TABLE ${tempParentIdsTableName} (
+            parent_id TEXT
+          )
+        `);
+
         await transactingDatabase.executeSql(
           `INSERT INTO ${tempValidPairsTableName} (parent_id, child_id) 
             VALUES ${validParentChildIdPairs.map(() => '(?, ?)').join(', ')}`,
