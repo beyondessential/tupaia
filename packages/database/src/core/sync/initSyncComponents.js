63d7974489f34da96c54093d3175f66ab84f000f
chris-bes <chris@beyondessential.com.au>
2025-08-03 18:11:02 +1000

    ok let's test

--- packages/database/src/core/sync/initSyncComponents.js
+++ packages/database/src/core/sync/initSyncComponents.js
@@ -1,8 +1,6 @@
 import { SyncTickFlags } from '@tupaia/constants';
 
 export const SYNCING_TABLES = [
-  'user_account',
-  'user_entity_permission',
   'answer',
   'entity',
   'entity_hierarchy',
@@ -19,6 +17,8 @@ export const SYNCING_TABLES = [
   'survey_screen_component',
   'task',
   'task_comment',
+  'user_account',
+  'user_entity_permission',
 ];
 
 const TABLES_WITHOUT_COLUMN_QUERY = `
--- packages/database/src/core/sync/toggleTombstoneTriggers.js
+++ packages/database/src/core/sync/toggleTombstoneTriggers.js
@@ -5,6 +5,30 @@ export const toggleTombstoneTriggers = async (database, enabled) => {
 
   const action = enabled ? 'ENABLE' : 'DISABLE';
   for (const { table } of tablesWithTrigger) {
+    if (table === 'user_account') {
+      const locks = await database.executeSql(`
+        SELECT 
+            l.locktype,
+            l.database,
+            l.relation::regclass,
+            l.page,
+            l.tuple,
+            l.virtualxid,
+            l.transactionid,
+            l.mode,
+            l.granted,
+            a.usename,
+            a.query,
+            a.query_start,
+            a.state,
+            a.pid
+        FROM pg_locks l
+        LEFT JOIN pg_stat_activity a ON l.pid = a.pid
+        WHERE l.relation = 'user_account'::regclass
+        ORDER BY l.granted, a.query_start;
+      `);
+      console.log('lockssss', locks);
+    }
     await database.executeSql(`
         ALTER TABLE "${table}" ${action} TRIGGER add_${table}_tombstone_on_delete;
       `);
