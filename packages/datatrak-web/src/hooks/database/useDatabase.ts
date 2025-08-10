e21e13640dcba2aa7b0499cfb64d951224b6d0b0
chris-bes <chris@beyondessential.com.au>
2025-08-10 19:01:07 +1000

    format

--- packages/datatrak-web/src/hooks/database/useDatabase.ts
+++ packages/datatrak-web/src/hooks/database/useDatabase.ts
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import { DatabaseContext, DatabaseContextType } from '../../api';
 
 export const useDatabase = (): DatabaseContextType => {
-    const context = useContext(DatabaseContext);
+  const context = useContext(DatabaseContext);
 
-    if (!context) {
-      throw new Error('useDatabase must be used within a DatabaseProvider');
-    }
-  
-    return context;
+  if (!context) {
+    throw new Error('useDatabase must be used within a DatabaseProvider');
+  }
+
+  return context;
 };
