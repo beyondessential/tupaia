f5969fc52a4f9f37576ef1c09ee3ab3a4cb8744f
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-08-11 18:43:10 +1200

    prefer `??`

--- packages/admin-panel/src/dataTables/DataTableEditFields.jsx
+++ packages/admin-panel/src/dataTables/DataTableEditFields.jsx
@@ -159,7 +159,7 @@ export const DataTableEditFields = React.memo(
                     externalDatabaseConnections.find(
                       connection =>
                         connection.code === recordData?.config?.externalDatabaseConnectionCode,
-                    ) || {}
+                    ) ?? {}
                   }
                 />
               )}
