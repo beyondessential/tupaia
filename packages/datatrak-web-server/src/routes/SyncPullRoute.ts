bcaf902e2a0e9e84ed09c0b9c34ade3f9bee7112
chris-bes <chris@beyondessential.com.au>
2025-08-14 18:54:36 +1000

    remove unused

--- packages/datatrak-web-server/src/routes/SyncPullRoute.ts
+++ packages/datatrak-web-server/src/routes/SyncPullRoute.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request } from 'express';
 
 import { Route } from '@tupaia/server-boilerplate';
 import { DatatrakWebSyncPullRequest } from '@tupaia/types';
