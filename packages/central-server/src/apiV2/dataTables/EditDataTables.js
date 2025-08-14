9563443f9368fbfd0e069c7c021e1b9231c78a84
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-08-14 16:14:10 +1200

    separate READ and EDIT perms for data tables

--- packages/central-server/src/apiV2/dataTables/EditDataTables.js
+++ packages/central-server/src/apiV2/dataTables/EditDataTables.js
@@ -1,3 +1,5 @@
+import { ensure } from '@tupaia/tsutils';
+import { DataTableType } from '@tupaia/types';
 import { PermissionsError } from '@tupaia/utils';
 import {
   assertAllPermissions,
@@ -6,10 +8,27 @@ import {
   assertVizBuilderAccess,
 } from '../../permissions';
 import { EditHandler } from '../EditHandler';
-import { hasDataTablePermissions } from './assertDataTablePermissions';
+import { hasExternalDatabaseConnectionPermissions } from '../externalDatabaseConnections/GETExternalDatabaseConnections';
+
+async function hasDataTableWritePermissions(accessPolicy, models, dataTableId) {
+  const dataTable = ensure(
+    await models.dataTable.findById(dataTableId),
+    `No data table exists with ID ${dataTableId}`,
+  );
+
+  /**
+   * Editing non-SQL data tables require BES Admin
+   * @see EditDataTables#assertUserHasAccess
+   */
+  if (dataTable.type !== DataTableType.sql) return false;
+
+  // Editing SQL data tables require access to its external database connection
+  const dbConnection = await dataTable.getExternalDatabaseConnection();
+  return await hasExternalDatabaseConnectionPermissions(accessPolicy, models, dbConnection.id);
+}
 
 const assertDataTableEditPermissions = async (accessPolicy, models, dataTableId) => {
-  const authorized = await hasDataTablePermissions(accessPolicy, models, dataTableId);
+  const authorized = await hasDataTableWritePermissions(accessPolicy, models, dataTableId);
   if (!authorized) {
     throw new PermissionsError('You don’t have permission to edit this data table');
   }
