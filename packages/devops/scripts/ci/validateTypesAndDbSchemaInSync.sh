bcf088e7ecb1080ede42394d3b8dc54ec1a8eed5
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-08-15 14:14:56 +1200

    moar

--- packages/devops/scripts/ci/validateTypesAndDbSchemaInSync.sh
+++ packages/devops/scripts/ci/validateTypesAndDbSchemaInSync.sh
@@ -4,16 +4,22 @@ set -ex
 script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 root_dir="$script_dir"/../../../..
 
-# Load environment variables from .env files
+if [[ $CI = true ]]; then
+    echo '::group::Load environment variables from .env files'
+fi
 source "$root_dir"/scripts/bash/mergeEnvForDB.sh
+if [[ $CI = true ]]; then
+    echo '::endgroup::'
+fi
+
 cd "$root_dir"
 
-echo "Connected to postgres server: $DB_URL, starting to setup database"
 
 if [[ $CI = true ]]; then
     echo '::group::Set up test database'
 fi
 
+echo "Connected to PostgreSQL server: $DB_URL, starting to setup database"
 yarn workspace @tupaia/database setup-test-database
 
 if [[ $CI = true ]]; then
