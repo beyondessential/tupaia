efa938cf5762cefa50170285521a33f3f71e0619
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-08-18 18:07:28 +1200

    lint

--- packages/database/src/migrations/20250817055244-AddKiuarFacility-modifies-schema.js
+++ packages/database/src/migrations/20250817055244-AddKiuarFacility-modifies-schema.js
@@ -5,25 +5,23 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return db.runSql(`
-    ALTER TYPE public.entity_type ADD VALUE IF NOT EXISTS 'kiuar_facility';
-  `);
+exports.up = function (db) {
+  return db.runSql("ALTER TYPE public.entity_type ADD VALUE IF NOT EXISTS 'kiuar_facility';");
 };
 
-exports.down = function(db) {
+exports.down = function (db) {
   return null;
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
