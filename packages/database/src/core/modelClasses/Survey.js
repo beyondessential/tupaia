2005a956a7d426536649f4b7f3630a9769b0b536
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-11 11:06:50 +1200

    fix meditrak-app-server build

--- packages/database/src/core/modelClasses/Survey.js
+++ packages/database/src/core/modelClasses/Survey.js
@@ -280,7 +280,7 @@ export class SurveyModel extends MaterializedViewLogDatabaseModel {
       `,
       surveyIds,
     );
-    return this.#getAggregatedQuestions(rows);
+    return this.getAggregatedQuestions(rows);
   }
 
   async buildSyncLookupQueryDetails() {
@@ -294,7 +294,7 @@ export class SurveyModel extends MaterializedViewLogDatabaseModel {
   /**
    * @param {ReturnType<typeof this.getQuestionsValues>} rawResults
    */
-  #getAggregatedQuestions(rawResults) {
+  getAggregatedQuestions(rawResults) {
     const initialValue = {};
     const surveyQuestions = rawResults.reduce((questionsObject, currentResult) => {
       const { survey_id: id } = currentResult;
