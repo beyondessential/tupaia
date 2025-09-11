e092e0a502747bb3cd34686c148da2f9518f343c
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-11 13:06:58 +1200

    import `matchMedia` mock

--- packages/datatrak-web/src/__tests__/__integration__/survey.test.tsx
+++ packages/datatrak-web/src/__tests__/__integration__/survey.test.tsx
@@ -1,8 +1,9 @@
-import { screen, fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { renderSurveyPage } from '../helpers/render';
 import { handlers } from '../mocks/handlers';
+import '../mocks/matchMedia.mock';
 
 const server = setupServer(
   ...handlers,
