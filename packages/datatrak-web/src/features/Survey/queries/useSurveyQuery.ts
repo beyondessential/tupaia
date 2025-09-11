ba18783c92376ca8d8f3179494febb9e328bfbd1
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-11 17:40:20 +1200

    attach project to survey

--- packages/datatrak-web/src/features/Survey/queries/useSurveyQuery.ts
+++ packages/datatrak-web/src/features/Survey/queries/useSurveyQuery.ts
@@ -1,7 +1,9 @@
 import { UseQueryOptions } from '@tanstack/react-query';
 
-import { DatatrakWebSurveyRequest, Survey } from '@tupaia/types';
+import { ProjectRecord } from '@tupaia/database';
+import { SurveyRecord } from '@tupaia/tsmodels';
 import { camelcaseKeys, ensure } from '@tupaia/tsutils';
+import { Country, DatatrakWebSurveyRequest, Survey } from '@tupaia/types';
 import { get, useDatabaseQuery } from '../../../api';
 import { useIsOfflineFirst } from '../../../api/offlineFirst';
 import { DatatrakWebModelRegistry } from '../../../types';
@@ -19,20 +21,28 @@ const surveyQueryFunctions = {
     surveyCode,
   }: UseSurveyQueryFunctionContext & { models: DatatrakWebModelRegistry }) =>
     await models.wrapInReadOnlyTransaction(async trxModels => {
-      const survey = await trxModels.survey.findOne({ code: surveyCode });
-      const [surveyQuestionsValues, countryNames, countryCodes] = await Promise.all([
+      const survey: SurveyRecord & {
+        surveyQuestions?: unknown[];
+        countryNames?: Country['name'][];
+        countryCodes?: Country['code'][];
+        project?: ProjectRecord;
+      } = await trxModels.survey.findOne({ code: ensure(surveyCode) });
+
+      const [surveyQuestionsValues, countryNames, countryCodes, project] = await Promise.all([
         getSurveyQuestionsValues(trxModels, [survey.id]),
         getSurveyCountryNames(trxModels, [survey.id]),
         getSurveyCountryCodes(trxModels, [survey.id]),
+        survey.getProject(),
       ]);
       survey.surveyQuestions = surveyQuestionsValues[survey.id];
       survey.countryNames = countryNames[survey.id];
       survey.countryCodes = countryCodes[survey.id];
+      survey.project = project;
 
       survey.model = undefined;
-      const rv = camelcaseKeys(survey, { deep: true });
-      console.log('local', rv);
-      return rv;
+      survey.project.model = undefined;
+
+      return camelcaseKeys(survey, { deep: true });
     }),
 };
 
