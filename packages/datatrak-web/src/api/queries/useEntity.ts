67fe705c326233dfbb8b951e90e2ebd4c0d3d211
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-11 18:47:45 +1200

    offline `useEntity`

--- packages/datatrak-web/src/api/queries/useEntity.ts
+++ packages/datatrak-web/src/api/queries/useEntity.ts
@@ -1,38 +1,90 @@
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { UseQueryOptions } from '@tanstack/react-query';
+
+import { assertIsNotNullish, camelcaseKeys, ensure } from '@tupaia/tsutils';
 import { DatatrakWebEntitiesRequest, Entity } from '@tupaia/types';
+import { DatatrakWebModelRegistry } from '../../types';
 import { get } from '../api';
+import { useIsOfflineFirst } from '../offlineFirst';
+import { useDatabaseQuery } from './useDatabaseQuery';
+
+interface UseEntityByCodeQueryFunctionContext {
+  entityCode?: Entity['code'];
+}
+
+const entityByCodeQueryFunctions = {
+  remote: async ({ entityCode }: UseEntityByCodeQueryFunctionContext) => {
+    return await get(`entity/${encodeURIComponent(ensure(entityCode))}`);
+  },
+  local: async ({
+    entityCode,
+    models,
+  }: UseEntityByCodeQueryFunctionContext & { models: DatatrakWebModelRegistry }) => {
+    const [entity] = await models.entity.find({ code: ensure(entityCode) });
+    entity.model = undefined;
+    return camelcaseKeys(entity, { deep: true });
+  },
+};
+
+interface UseEntityByIdQueryFunctionContext {
+  entityId?: Entity['id'];
+}
 
 export const useEntityByCode = (
   entityCode?: Entity['code'],
   useQueryOptions?: UseQueryOptions<DatatrakWebEntitiesRequest.EntitiesResponseItem>,
 ) => {
-  return useQuery<DatatrakWebEntitiesRequest.EntitiesResponseItem>(
-    ['entity', entityCode],
-    async () => await get(`entity/${entityCode}`),
+  const isOfflineFirst = useIsOfflineFirst();
+
+  const options = Object.assign(
     {
-      ...useQueryOptions,
-      enabled: !!entityCode && (useQueryOptions?.enabled ?? true),
+      enabled: Boolean(entityCode),
+      localContext: { entityCode },
     },
+    useQueryOptions,
   );
+
+  return useDatabaseQuery(
+    ['entity', entityCode],
+    isOfflineFirst ? entityByCodeQueryFunctions.local : entityByCodeQueryFunctions.remote,
+    options,
+  );
+};
+
+const entityByIdQueryFunctions = {
+  remote: async ({ entityId }: UseEntityByIdQueryFunctionContext) => {
+    return await get('entities', {
+      params: {
+        filter: { id: ensure(entityId) },
+      },
+    });
+  },
+  local: async ({
+    entityId,
+    models,
+  }: UseEntityByIdQueryFunctionContext & { models: DatatrakWebModelRegistry }) => {
+    const [entity] = await models.entity.find({ id: ensure(entityId) });
+    entity.model = undefined;
+    return camelcaseKeys(entity, { deep: true });
+  },
 };
 
 export const useEntityById = (
   entityId?: Entity['id'],
   useQueryOptions?: UseQueryOptions<DatatrakWebEntitiesRequest.EntitiesResponseItem>,
 ) => {
-  return useQuery<DatatrakWebEntitiesRequest.EntitiesResponseItem>(
-    ['entities', entityId],
-    async () => {
-      const [entity] = await get('entities', {
-        params: {
-          filter: { id: entityId },
-        },
-      });
-      return entity;
-    },
+  const isOfflineFirst = useIsOfflineFirst();
+
+  const options = Object.assign(
     {
-      ...useQueryOptions,
-      enabled: !!entityId && (useQueryOptions?.enabled ?? true),
+      enabled: Boolean(entityId),
+      localContext: { entityId },
     },
+    useQueryOptions,
+  );
+
+  return useDatabaseQuery(
+    ['entities', entityId],
+    isOfflineFirst ? entityByIdQueryFunctions.local : entityByIdQueryFunctions.remote,
+    options,
   );
 };
