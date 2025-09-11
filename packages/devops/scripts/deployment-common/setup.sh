677bbefa6263e5fe93e05a8e218c7524b5bff8c4
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-12 10:26:48 +1200

    install with APT

--- packages/devops/scripts/deployment-common/setup.sh
+++ packages/devops/scripts/deployment-common/setup.sh
@@ -100,13 +100,28 @@ sudo apt-get -yqq install \
   wget \
   xdg-utils
 
-# Install Tailscale
-if ! command -v tailscale &>/dev/null; then
-  echo 'Tailscale not installed. Installing...'
-  curl -fsSL https://tailscale.com/install.sh | sh
-fi
-echo 'Tailscale version:'
-tailscale version
+install_tailscale() {
+  if ! command -v tailscale &>/dev/null; then
+    echo 'Tailscale not installed. Installing...'
+
+    # See https://docs.aws.amazon.com/linux/al2023/ug/ident-os-release.html
+    local os_id=$(source /etc/os-release && echo "$ID")
+    local os_codename=$(source /etc/os-release && echo "$VERSION_CODENAME")
+
+    sudo cp "$tupaia_dir"/packages/devops/keyrings/tailscale.gpg /usr/share/keyrings/tailscale.gpg
+    echo "deb [signed-by=/usr/share/keyrings/tailscale.gpg] https://pkgs.tailscale.com/stable/$os_id $os_codename main" |
+      sudo tee /etc/apt/sources.list.d/tailscale.list
+    sudo apt-get update
+    sudo apt-get -qq install tailscale
+  else
+    echo 'Updating Tailscale...'
+    sudo tailscale update
+  fi
+
+  echo 'Tailscale version:'
+  tailscale version
+}
+install_tailscale
 
 # install node and yarn
 if ! command -v node &>/dev/null; then
