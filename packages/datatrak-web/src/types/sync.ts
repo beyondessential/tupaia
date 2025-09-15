0696c8f4ba4913e0652c99dd56ddd79361aa6480
chris-bes <chris@beyondessential.com.au>
2025-09-15 13:42:59 +1000

    wrong const

--- packages/datatrak-web/src/types/sync.ts
+++ packages/datatrak-web/src/types/sync.ts
@@ -12,6 +12,5 @@ export const SYNC_EVENT_ACTIONS = {
   SYNC_STARTED: 'syncStarted',
   SYNC_STATE_CHANGED: 'syncStateChanged',
   SYNC_ENDED: 'syncEnded',
-  SYNC_ERROR: 'syncRecordError',
-  SYNC_RECORD_ERROR: 'syncRecordError',
+  SYNC_ERROR: 'syncError',
 } as const;
