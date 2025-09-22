7a3a7ea9a27d2d39276302746deed6abdd28e2cc
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-23 11:59:42 +1200

    refactor `getEC2TagValue.sh`

--- packages/devops/scripts/utility/getEC2TagValue.sh
+++ packages/devops/scripts/utility/getEC2TagValue.sh
@@ -1,8 +1,26 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+# Given a tag name, prints the value of that tag for the calling EC2 instance. If the tag is
+# unavailable, prints ‘None’.
+set -e
 
-# Get the stage tag of this ec2 instance from AWS
-TAG_NAME=$1
-INSTANCE_ID="$(wget -qO- http://instance-data/latest/meta-data/instance-id)"
-REGION="`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
-TAG_VALUE="$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$TAG_NAME" --region $REGION --output=text | cut -f5)"
-echo $TAG_VALUE
+if (($# != 1)); then
+  script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+  source "$script_dir"/../../../../scripts/bash/ansiControlSequences.sh
+  this_script=$(basename "${BASH_SOURCE[0]}")
+  {
+    echo -en "${BOLD}${RED}Usage error.${RESET} "
+    echo -e "${BOLD}$this_script${RESET} expects 1 argument, but got $#. Example usage:"
+    echo
+    echo -e "  ${GREEN}path/to/$this_script ${BLUE}DeploymentName${RESET}"
+  } >&2
+  exit 2
+fi
+
+tag_name=$1
+instance_id=$(curl --silent http://instance-data/latest/meta-data/instance-id)
+
+aws ec2 describe-tags \
+  --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=$tag_name" \
+  --no-cli-pager \
+  --output text \
+  --query Tags[0].Value
