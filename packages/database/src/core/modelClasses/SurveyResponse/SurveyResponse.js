b1cad40a9959f09c56c888f2d85da308ee2f287c
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-23 16:33:49 +1200

    fix type hint

--- packages/database/src/core/modelClasses/SurveyResponse/SurveyResponse.js
+++ packages/database/src/core/modelClasses/SurveyResponse/SurveyResponse.js
@@ -35,7 +35,7 @@ export class SurveyResponseModel extends MaterializedViewLogDatabaseModel {
   }
 
   /**
-   * @param {Record<import('@tupaia/types').QuestionType, AnswerBodyParser> | undefined} answerBodyParsers
+   * @param {Record<import('@tupaia/types').QuestionType, AnswerBodyParser> | undefined} [answerBodyParsers]
    */
   static async saveResponsesToDatabase(models, userId, surveyResponses, answerBodyParsers) {
     return await saveResponsesToDatabase(models, userId, surveyResponses, answerBodyParsers);
