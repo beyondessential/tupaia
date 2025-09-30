304af945d5efbdd06fb71ce98b6d4b4378f9d03e
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-30 15:31:28 +1300

    move

--- packages/constants/src/permission.ts
+++ packages/constants/src/permission.ts
@@ -18,3 +18,5 @@ export const API_CLIENT_PERMISSIONS = [
 ] as const;
 
 export const BES_ADMIN_PERMISSION_GROUP = 'BES Admin';
+export const TUPAIA_ADMIN_PANEL_PERMISSION_GROUP = 'Tupaia Admin Panel';
+export const VIZ_BUILDER_PERMISSION_GROUP = 'Viz Builder User';
