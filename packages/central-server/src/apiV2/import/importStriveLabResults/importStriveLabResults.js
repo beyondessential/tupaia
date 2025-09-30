b77346f2217fe38d92c79c936e699be6f20362f1
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-09-30 14:34:10 +1300

    fix

--- packages/central-server/src/apiV2/import/importStriveLabResults/importStriveLabResults.js
+++ packages/central-server/src/apiV2/import/importStriveLabResults/importStriveLabResults.js
@@ -87,7 +87,7 @@ export const importStriveLabResults = async (req, res) => {
   const entitiesGroupedBySurveyCode = await getEntitiesGroupedBySurveyCode(models, inputsPerSurvey);
 
   const importSurveyResponsePermissionsChecker = async accessPolicy => {
-    await this.models.surveyResponse.assertCanImport(accessPolicy, entitiesGroupedBySurveyCode);
+    await models.surveyResponse.assertCanImport(accessPolicy, entitiesGroupedBySurveyCode);
   };
 
   await req.assertPermissions(
