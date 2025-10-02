2fa2bc4a8d7d7692b71e15a13173822e302062cf
chris-bes <chris@beyondessential.com.au>
2025-10-02 23:18:56 +1000

    fix

--- packages/tupaia-web/src/api/mutations/useEditUser.ts
+++ packages/tupaia-web/src/api/mutations/useEditUser.ts
@@ -1,55 +1,25 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-
-import { useDatabaseContext } from '../../hooks/database';
-import { UserAccountDetails } from '../../types';
 import { put } from '../api';
 
-/**
- * Converts a string from camel case to snake case.
- *
- * @remarks
- * Ignores whitespace characters, including wordspaces and newlines. Does not handle fully-
- * uppercase acronyms/initialisms. e.g. 'HTTPRequest' -> 'h_t_t_p_request'.
- */
-function camelToSnakeCase(camelCaseString: string): string {
-  return camelCaseString
-    ?.split(/\.?(?=[A-Z])/)
-    .join('_')
-    .toLowerCase();
-}
+type UserAccountDetails = Record<string, any>;
 
-export const useEditUser = (onSuccess?: () => void) => {
+export const useEditUser = (onSuccess?: (data: UserAccountDetails) => void) => {
   const queryClient = useQueryClient();
-  const { models } = useDatabaseContext();
 
   return useMutation<any, Error, UserAccountDetails, unknown>(
-    async (userDetails: UserAccountDetails) => {
-      if (!userDetails) return;
-
-      // `mobile_number` field in database is nullable; don't just store an empty string
-      if (!userDetails?.mobileNumber) {
-        userDetails.mobileNumber = null;
-      }
-
-      const updates = Object.fromEntries(
-        Object.entries(userDetails).map(([key, value]) => [camelToSnakeCase(key), value]),
-      );
-
-      await put('me', { data: updates });
+    async ({ projectId }: Record<string, any>) => {
+      const data = {
+        project_id: projectId,
+      };
+      await put('me', { data });
+      return {
+        projectId,
+      };
     },
     {
-      onSuccess: async (_, variables) => {
-        await queryClient.invalidateQueries(['getUser']);
-        // If the user changes their project, we need to invalidate the entity descendants query so that recent entities are updated if they change back to the previous project without refreshing the page
-        if (variables.projectId) {
-          await queryClient.invalidateQueries(['entityDescendants']);
-          await queryClient.invalidateQueries(['tasks']);
-
-          (async () => {
-            await models.localSystemFact.addProjectForSync(variables.projectId);
-          })();
-        }
-        onSuccess?.();
+      onSuccess: data => {
+        queryClient.invalidateQueries(['getUser']);
+        if (onSuccess) onSuccess(data);
       },
     },
   );
