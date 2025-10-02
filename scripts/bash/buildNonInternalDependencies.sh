ddb6fd487adb0bf9aa60bd95def85be54ff37750
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-10-03 09:27:53 +1300

    add missing `-W` flags, restore maximum verbosity

--- scripts/bash/buildNonInternalDependencies.sh
+++ scripts/bash/buildNonInternalDependencies.sh
@@ -5,7 +5,7 @@ set -ex
 yarn workspaces foreach \
     --parallel \
     --topological \
-    --verbose \
+    --verbose --verbose \
     --jobs unlimited \
     --include '@tupaia/{admin-panel-server,central-server,data-table-server,datatrak-web,datatrak-web-server,entity-server,lesmis,lesmis-server,meditrak-app-server,psss,psss-server,report-server,tupaia-web,tupaia-web-server,web-config-server}' \
     run build
