a08a89411f14eca13d877889e98813fd182dce58
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-10-06 15:09:23 +1300

    diff

--- packages/types/generate-models.ts
+++ packages/types/generate-models.ts
@@ -1,9 +1,9 @@
 // See https://rmp135.github.io/sql-ts/#/?id=totypescript
 
 import sqlts, { Table } from '@rmp135/sql-ts';
-import path from 'path';
-
+import { createPatch } from 'diff';
 import Knex from 'knex';
+import path from 'path';
 // @ts-ignore
 import config from './config/models/config.json';
 
@@ -123,18 +123,25 @@ const run = async () => {
   if (failOnChanges) {
     const currentTsString = fs.readFileSync(config.filename, { encoding: 'utf8' });
     if (currentTsString !== tsString) {
+      const patch = createPatch(config.filename, currentTsString, tsString);
       console.log(
-        '❌ There are changes in the db schema which are not reflected in @tupaia/types.',
+        `${process.env.CI ? '::error::' : '❌ '}There are changes in the database schema which are not reflected in @tupaia/types. Run \`yarn workspace @tupaia/types run generate\` to fix.`,
       );
-      console.log("Run 'yarn workspace @tupaia/types generate' to fix");
+      console.log(patch);
       process.exit(1);
     }
   }
 
   fs.writeFile(config.filename, tsString, () => {
-    console.log(`File written: ${config.filename}`);
+    console.log(`💾 Wrote ${config.filename}`);
     process.exit(0);
   });
 };
 
+console.log('🎰 Generating models...');
+const tic = performance.now();
+
 run();
+
+const duration = performance.now() - tic;
+console.log(`✅ Done in ${Math.round(duration)} ms`);
