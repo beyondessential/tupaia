fa7aa748ce335904184a060badcc79f0baf24c0c
chris-bes <chris@beyondessential.com.au>
2025-10-11 23:54:40 +1100

    fix record update task

--- packages/datatrak-web/src/database/task/editTask.ts
+++ packages/datatrak-web/src/database/task/editTask.ts
@@ -2,20 +2,23 @@ import { Task } from '@tupaia/types';
 import { ensure } from '@tupaia/tsutils';
 
 import { DatatrakWebModelRegistry } from '../../types';
+import { CurrentUser } from '../../api/CurrentUserContext';
 
 type PartialTask = Partial<Task>;
 
 export const editTask = async ({
   models,
   taskId,
+  user,
   data,
 }: {
   models: DatatrakWebModelRegistry;
   taskId?: Task['id'];
+  user: CurrentUser;
   data: PartialTask;
 }) => {
   const ensuredTaskId = ensure(taskId);
   const originalTask = ensure(await models.task.findById(ensuredTaskId));
   const formattedData = models.task.formatTaskChanges(data, originalTask);
-  return await models.task.updateById(ensuredTaskId, formattedData);
+  return await models.task.updateById(ensuredTaskId, formattedData, user.id);
 };
