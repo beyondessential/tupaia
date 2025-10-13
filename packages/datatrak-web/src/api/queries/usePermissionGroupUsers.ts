63e1ffa2d8faa6b8404f031ff71701b8ddd35d23
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-10-13 15:23:08 +1300

    offline `usePermissionGroupUsers`

--- packages/datatrak-web/src/api/queries/usePermissionGroupUsers.ts
+++ packages/datatrak-web/src/api/queries/usePermissionGroupUsers.ts
@@ -1,23 +1,65 @@
-import { useQuery } from '@tanstack/react-query';
-import { Country, DatatrakWebUsersRequest, PermissionGroup } from '@tupaia/types';
+import { ensure } from '@tupaia/tsutils';
+import { Country, DatatrakWebUsersRequest, Entity, PermissionGroup } from '@tupaia/types';
 import { get } from '../api';
+import { useIsOfflineFirst } from '../offlineFirst';
+import { ContextualQueryFunctionContext, useDatabaseQuery } from './useDatabaseQuery';
+
+interface UsePermissionGroupUsersQueryParams {
+  countryCode?: Entity['code'];
+  permissionGroupId?: PermissionGroup['id'];
+  searchTerm?: string;
+}
+
+interface PermissionGroupUsersQueryFunctionContext
+  extends UsePermissionGroupUsersQueryParams,
+    ContextualQueryFunctionContext {}
+
+const getRemote = async ({
+  countryCode,
+  permissionGroupId,
+  searchTerm,
+}: PermissionGroupUsersQueryFunctionContext) => {
+  if (!permissionGroupId || !countryCode) return [];
+  return await get(`users/${encodeURIComponent(countryCode)}`, {
+    params: { searchTerm, permissionGroupId },
+  });
+};
+
+const getLocal = async ({
+  models,
+  countryCode,
+  permissionGroupId,
+  searchTerm,
+}: PermissionGroupUsersQueryFunctionContext) => {
+  if (!permissionGroupId || !countryCode) return [];
+  const permissionGroup = ensure(
+    await models.permissionGroup.findById(permissionGroupId),
+    `No permission group exists with ID ${permissionGroupId}`,
+  );
+  return await models.user.getFilteredUsersForPermissionGroup(
+    countryCode,
+    permissionGroup,
+    searchTerm,
+  );
+};
 
 export const usePermissionGroupUsers = (
   countryCode?: Country['code'],
   permissionGroupId?: PermissionGroup['id'],
   searchTerm?: string,
 ) => {
-  return useQuery<DatatrakWebUsersRequest.ResBody>(
+  const isOfflineFirst = useIsOfflineFirst();
+
+  return useDatabaseQuery<DatatrakWebUsersRequest.ResBody>(
     ['users', permissionGroupId, countryCode, searchTerm],
-    () =>
-      get(`users/${countryCode}`, {
-        params: {
-          searchTerm,
-          permissionGroupId,
-        },
-      }),
+    isOfflineFirst ? getLocal : getRemote,
     {
       enabled: !!permissionGroupId && !!countryCode,
+      localContext: {
+        countryCode,
+        permissionGroupId,
+        searchTerm,
+      },
     },
   );
 };
