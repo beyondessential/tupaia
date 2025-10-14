72f6f6bc7e5fd6feb3db1c6eda46dd98166822a5
chris-bes <chris@beyondessential.com.au>
2025-10-15 10:57:14 +1100

    Show syncing projects

--- packages/datatrak-web/src/components/ChangeProjectButton.tsx
+++ packages/datatrak-web/src/components/ChangeProjectButton.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import { useCurrentUserContext } from '../api';
 import { ProjectSelectModal } from '../layout/UserMenu/ProjectSelectModal';
 import { Button, TooltipButtonWrapper } from './Button';
+import { useDatabaseContext } from '../hooks/database';
+import { ensure } from '@tupaia/tsutils';
+import { useIsOfflineFirst } from '../api/offlineFirst';
 
 /**
  * Semantically useless wrapper, but prevents {@link TooltipButtonWrapper} from wreaking havoc on
@@ -60,16 +63,38 @@ interface ChangeProjectButtonProps extends React.ComponentPropsWithoutRef<typeof
 
 export const ChangeProjectButton = ({ leadingBorder, ...props }: ChangeProjectButtonProps) => {
   const { project } = useCurrentUserContext();
-  const projectName = project?.name ?? null;
+  const { models } = useDatabaseContext() || {};
+  const isOfflineFirst = useIsOfflineFirst();
 
   const [projectModalIsOpen, setProjectModalIsOpen] = useState(false);
+  const [hasSynced, setHasSynced] = useState(false);
   const openProjectModal = () => setProjectModalIsOpen(true);
   const closeProjectModal = () => setProjectModalIsOpen(false);
+
+  useEffect(() => {
+    const checkSynced = async () => {
+      if (isOfflineFirst) {
+        const projectCount = await ensure(models).project.count({});
+        if (projectCount) {
+          setHasSynced(true);
+        }
+      }
+    };
+    checkSynced();
+  }, [isOfflineFirst, models]);
+
+  const getProjectName = useCallback(() => {
+    if (project?.name) {
+      return project?.name;
+    }
+    const noSyncedProject = isOfflineFirst && !hasSynced;
+    return noSyncedProject ? 'Syncing projects...' : 'Select project';
+  }, [project?.name, isOfflineFirst, hasSynced]);
 
   return (
     <Container $leadingBorder={leadingBorder} {...props}>
       <ProjectButton onClick={openProjectModal} tooltip="Change project">
-        {projectName ?? 'Select project'}
+        {getProjectName()}
       </ProjectButton>
       {projectModalIsOpen && <ProjectSelectModal onBack={closeProjectModal} />}
     </Container>
