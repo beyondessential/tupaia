e1b7438a34cdb3aa759394f473fcaa750118730b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-10-14 13:29:47 +1300

    wip offline query function
    fix bad query syntax
    
    await promise

--- packages/datatrak-web/src/api/queries/useSurveyResponse.ts
+++ packages/datatrak-web/src/api/queries/useSurveyResponse.ts
@@ -1,17 +1,87 @@
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
-import { DatatrakWebSingleSurveyResponseRequest } from '@tupaia/types';
+import { UseQueryOptions } from '@tanstack/react-query';
+
+import { camelcaseKeys, ensure } from '@tupaia/tsutils';
+import { DatatrakWebSingleSurveyResponseRequest, Question, QuestionType } from '@tupaia/types';
 import { get } from '../api';
+import { useCurrentUserContext } from '../CurrentUserContext';
+import { useIsOfflineFirst } from '../offlineFirst';
+import { ContextualQueryFunctionContext, useDatabaseQuery } from './useDatabaseQuery';
+
+interface SurveyResponseQueryFunctionContext extends ContextualQueryFunctionContext {
+  requesterId?: string | undefined;
+  surveyResponseId?: string | null;
+}
+
+const getRemote = async ({ surveyResponseId }: SurveyResponseQueryFunctionContext) => {
+  if (!surveyResponseId) return null;
+  return await get(`surveyResponse/${encodeURIComponent(surveyResponseId)}`);
+};
+
+const getLocal = async ({
+  models,
+  requesterId,
+  surveyResponseId,
+}: SurveyResponseQueryFunctionContext) => {
+  if (!requesterId || !surveyResponseId) return null;
+
+  return await models.wrapInReadOnlyTransaction(async transactingModels => {
+    const surveyResponse = ensure(
+      await transactingModels.surveyResponse.findById(surveyResponseId),
+      `No survey response exists with ID ${surveyResponseId}`,
+    );
+    const projectId = await surveyResponse.getProjectId();
+
+    const [entityParentName, answerList] = await Promise.all([
+      transactingModels.entity.getEntityParentName(projectId, surveyResponse.entity_id),
+      transactingModels.answer.find(
+        { survey_response_id: surveyResponseId },
+        { columns: ['text', 'question_id', 'type'] },
+      ),
+    ]);
+
+    // TODO: Dedupe this type
+    const answers: Record<
+      Question['id'],
+      string | number | boolean | null | undefined | { name: string; id: string }
+    > = {};
+    for (const { question_id: questionId, type, text } of answerList) {
+      if (!text) continue;
+      if (type === QuestionType.User) {
+        const user = await models.user.findById(text);
+        if (!user) {
+          // Log the error but continue to the next answer. This is in case the user was deleted
+          console.error(`User with id ${text} not found`);
+          continue;
+        }
+        answers[questionId] = { id: user.id, name: user.full_name };
+        continue;
+      }
+      answers[questionId] = text;
+    }
+
+    return camelcaseKeys({
+      ...surveyResponse,
+      // countryCode,
+      entityParentName,
+      answers,
+    });
+  });
+};
 
 export const useSurveyResponse = (
   surveyResponseId?: string | null,
   useQueryOptions?: UseQueryOptions<DatatrakWebSingleSurveyResponseRequest.ResBody>,
 ) => {
-  return useQuery<DatatrakWebSingleSurveyResponseRequest.ResBody>(
+  const isOfflineFirst = useIsOfflineFirst();
+  const requesterId = useCurrentUserContext().id;
+
+  return useDatabaseQuery<DatatrakWebSingleSurveyResponseRequest.ResBody>(
     ['surveyResponse', surveyResponseId],
-    () => get(`surveyResponse/${surveyResponseId}`),
+    isOfflineFirst ? getLocal : getRemote,
     {
       ...useQueryOptions,
-      enabled: !!surveyResponseId && (useQueryOptions?.enabled ?? true),
+      enabled: Boolean(surveyResponseId) && (useQueryOptions?.enabled ?? true),
+      localContext: { requesterId, surveyResponseId },
     },
   );
 };
