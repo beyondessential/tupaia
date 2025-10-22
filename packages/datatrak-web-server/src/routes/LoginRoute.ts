0476bc0f040365c79381f68548758df618dc6994
chris-bes <chris@beyondessential.com.au>
2025-10-22 14:55:43 +1100

    fixed accessPolicy

--- packages/datatrak-web-server/src/routes/LoginRoute.ts
+++ packages/datatrak-web-server/src/routes/LoginRoute.ts
@@ -1,12 +1,16 @@
+import { mergeAccessPolicies } from '@tupaia/auth';
 import { LoginRoute as BaseLoginRoute } from '@tupaia/server-boilerplate';
 
 export class LoginRoute extends BaseLoginRoute {
   // @ts-ignore LoginRoute types cannot be extended at this time
   public async buildResponse() {
-    const { accessPolicy } = this.req;
     const authResponse = await super.buildResponse();
+    const { accessPolicy } = this.req;
+
     // Reassign the access policy to the user with a more sufficient one
-    authResponse.user.accessPolicy = accessPolicy.policy;
+    authResponse.user.accessPolicy = this.req.session
+      ? mergeAccessPolicies(this.req.session.accessPolicy.policy, accessPolicy.policy)
+      : accessPolicy.policy;
 
     return authResponse;
   }
