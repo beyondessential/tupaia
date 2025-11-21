c6229b2f9818e34ee2ed035b3407f76aa1a1353d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-11-21 16:30:15 +1300

    more unit tests

--- packages/server-utils/src/__tests__/s3/S3Client.test.ts
+++ packages/server-utils/src/__tests__/s3/S3Client.test.ts
@@ -1,3 +1,4 @@
+import { ConflictError, UnsupportedMediaTypeError } from '@tupaia/utils';
 import { S3, S3Client } from '../../s3';
 import * as getUniqueFileNameModule from '../../s3/getUniqueFileName';
 
@@ -27,9 +28,28 @@ describe('S3Client', () => {
     const base64Gif =
       'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 
-    it('should throw if file is not an image', () => {});
+    it('should throw if file is not an image', async () => {
+      /** ‘Hello World’, encoded */
+      const base64TextFile = 'data:text/plain;base64,SGVsbG8gV29ybGQ=';
+      const fileId = crypto.randomUUID();
 
-    it('should throw if image type is not supported', () => {});
+      await expect(s3Client.uploadImage(base64TextFile, fileId)).rejects.toThrow(
+        new UnsupportedMediaTypeError('Expected image file but got text/plain'),
+      );
+    });
+
+    it('should throw if image type is not supported', async () => {
+      /** 1×1 transparent HEIF */
+      const base64Heif =
+        'data:image/heic;base64,AAAAGGZ0eXBoZWljAAAAAG1pZjFoZWljAAACWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAHBpY3QAAAAAAAAAAAAAAAAAAAAADnBpdG0AAAAAAAEAAAA0aWxvYwAAAABEQAACAAEAAAAAAngAAQAAAAAAAAArAAIAAAAAAqMAAQAAAAAAAAAjAAAAOGlpbmYAAAAAAAIAAAAVaW5mZQIAAAAAAQAAaHZjMQAAAAAVaW5mZQIAAAAAAgAAaHZjMQAAAAGXaXBycAAAAXBpcGNvAAAAdmh2Y0MBA3AAAAAAAAAAAAAe8AD8/fj4AAAPA2AAAQAYQAEMAf//A3AAAAMAkAAAAwAAAwAeugJAYQABACpCAQEDcAAAAwCQAAADAAADAB6gIIEFluqumubgIaDAgAAAAwCAAAADAIRiAAEABkQBwXPBiQAAABRpc3BlAAAAAAAAAEAAAABAAAAAKGNsYXAAAAABAAAAAQAAAAEAAAAB////wQAAAAL////BAAAAAgAAABBwaXhpAAAAAAMICAgAAABxaHZjQwEECAAAAAAAAAAAAB7wAPz8+PgAAA8DYAABABdAAQwB//8ECAAAAwCf+AAAAwAAHroCQGEAAQAmQgEBBAgAAAMAn/gAAAMAAB7AggQWW6q6a5sCAAADAAIAAAMAAhBiAAEABkQBwXPBiQAAAA5waXhpAAAAAAEIAAAAJ2F1eEMAAAAAdXJuOm1wZWc6aGV2YzoyMDE1OmF1eGlkOjEAAAAAH2lwbWEAAAAAAAAAAgABBIECBIMAAgWFAgaHgwAAABppcmVmAAAAAAAAAA5hdXhsAAIAAQABAAAAVm1kYXQAAAAnKAGvEyExlvhOUKeW/WMCzQyVTFq5T6Vz3QpQk3J+uP7yh5PFYoLgAAAAHygBriZCSiTn1w3//h8LF2FVc1OybCBiRCkSgGP19K4=';
+      const fileId = crypto.randomUUID();
+
+      await expect(s3Client.uploadImage(base64Heif, fileId)).rejects.toThrow(
+        new UnsupportedMediaTypeError(
+          'image/heic images aren’t supported. Please provide one of: AVIF, GIF, JPEG, PNG, SVG, WebP',
+        ),
+      );
+    });
 
     it('should generate a unique file name when no file ID is provided', async () => {
       const spy = jest.spyOn(getUniqueFileNameModule, 'getUniqueFileName');
@@ -48,8 +68,27 @@ describe('S3Client', () => {
       expect(spy).not.toHaveBeenCalled();
     });
 
-    it('should abort if file already exists, if overwrite is prohibited', () => {});
+    it('should abort if file already exists, if overwrite is prohibited', async () => {
+      // Mock private instance method `S3Client#checkIfFileExists` to report a file conflict
+      jest.spyOn(S3Client.prototype as any, 'checkIfFileExists').mockResolvedValue(true);
 
-    it('should not abort file already exists and overwrite is allowed', () => {});
+      const fileId = crypto.randomUUID();
+      const expectedError = new ConflictError(
+        `File dev_uploads/images/${fileId}.gif already exists on S3, overwrite is not allowed`,
+      );
+
+      await expect(s3Client.uploadImage(base64Gif, fileId, false)).rejects.toThrow(expectedError);
+    });
+
+    it('should not abort if file already exists and overwrite is allowed', async () => {
+      // Mock private instance method `S3Client#checkIfFileExists` to report a file conflict
+      jest.spyOn(S3Client.prototype as any, 'checkIfFileExists').mockResolvedValue(true);
+
+      const fileId = crypto.randomUUID();
+
+      await expect(s3Client.uploadImage(base64Gif, fileId, true)).resolves.not.toThrow(
+        ConflictError,
+      );
+    });
   });
 });
