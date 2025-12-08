5a396044c7f020eace39d21443f7540044f58110
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-12-08 13:59:01 +1300

    🔄

--- packages/datatrak-web/src/layout/Header/SyncButton.tsx
+++ packages/datatrak-web/src/layout/Header/SyncButton.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
 import { IconButton, useTheme } from '@material-ui/core';
-import { RefreshCcw } from 'lucide-react';
+import { RefreshCw } from 'lucide-react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { RouterLink } from '@tupaia/ui-components';
-
 import { ROUTES } from '../../constants/url';
+import { useIsSyncing } from '../../sync/useIsSyncing';
 
 export const StyledSyncButton = styled(IconButton)<{
   component: React.ElementType;
   to: string;
 }>`
   padding: 0.5rem;
+
+  @keyframes --spin-sync-button {
+    to {
+      transform: rotate(1turn);
+    }
+  }
 `;
 
 export const SyncButton = () => {
   const secondaryColor = useTheme().palette.secondary.main;
+  const isSyncing = useIsSyncing();
 
   return (
     <StyledSyncButton to={ROUTES.SYNC} component={RouterLink}>
-      <RefreshCcw size={25} color={secondaryColor} />
+      <RefreshCw
+        size={25}
+        color={secondaryColor}
+        style={{ animation: isSyncing ? '--spin-sync-button 1150ms linear infinite' : undefined }}
+      />
     </StyledSyncButton>
   );
 };
