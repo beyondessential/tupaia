c0391e6bceaa63e7aaabb2e4adfb8d6bfffabdaf
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-12-11 10:50:37 +1300

    extract into separate hooks

--- packages/datatrak-web/src/views/Sync/LastSyncDate.tsx
+++ packages/datatrak-web/src/views/Sync/LastSyncDate.tsx
@@ -1,21 +1,40 @@
-import React, { HTMLAttributes } from 'react';
+import { formatDistance } from 'date-fns';
+import React, { HTMLAttributes, useEffect, useState } from 'react';
 import { SyncHeading } from './SyncHeading';
 import { SyncParagraph } from './SyncParagraph';
+
 interface LastSyncDateProps extends HTMLAttributes<HTMLDivElement> {
-  formattedLastSuccessfulSyncTime: React.ReactNode;
   lastSyncDate: Date | null;
 }
 
-export const LastSyncDate = ({
-  formattedLastSuccessfulSyncTime,
-  lastSyncDate,
-  ...props
-}: LastSyncDateProps) => {
+export function formatLastSuccessfulSyncTime(lastSuccessfulSyncTime: Date | null): React.ReactNode {
+  const formattedTimeString = lastSuccessfulSyncTime
+    ? formatDistance(lastSuccessfulSyncTime, new Date(), { addSuffix: true })
+    : '';
+
+  // Capitalize the first letter
+  return formattedTimeString.length > 0
+    ? formattedTimeString.charAt(0).toUpperCase() + formattedTimeString.slice(1)
+    : formattedTimeString;
+}
+
+export const LastSyncDate = ({ lastSyncDate, ...props }: LastSyncDateProps) => {
+  const [formattedDate, setFormattedDate] = useState<React.ReactNode>();
+
+  useEffect(() => {
+    // Refresh relatively formatted date
+    const interval = setInterval(
+      () => void setFormattedDate(formatLastSuccessfulSyncTime(lastSyncDate)),
+      1_000,
+    );
+    return () => void clearInterval(interval);
+  }, [lastSyncDate]);
+
   return (
     <div {...props}>
       <SyncHeading>Last successful sync</SyncHeading>
       <SyncParagraph>
-        <time dateTime={lastSyncDate?.toISOString()}>{formattedLastSuccessfulSyncTime}</time>
+        <time dateTime={lastSyncDate?.toISOString()}>{formattedDate}</time>
       </SyncParagraph>
     </div>
   );
