4cc260913db21043bbc2cce5b21711fffecd8603
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-12-15 11:23:27 +1300

    use updated object URL format

--- packages/server-utils/src/s3/constants.ts
+++ packages/server-utils/src/s3/constants.ts
@@ -1,7 +1,7 @@
 import { getIsProductionEnvironment } from '@tupaia/utils';
 
 export const S3_BUCKET_NAME = 'tupaia';
-export const S3_BUCKET_PATH = `https://s3-ap-southeast-2.amazonaws.com/${S3_BUCKET_NAME}/`;
+export const S3_BUCKET_PATH = `https://${S3_BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com/`;
 export const getS3ImageFilePath = () =>
   getIsProductionEnvironment() ? 'uploads/images/' : 'dev_uploads/images/';
 export const getS3UploadFilePath = () =>
