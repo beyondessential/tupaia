40650f43204b4468576433ccbd04809d52b8a804
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-12-18 12:45:32 +1300

    output directly

--- packages/devops/scripts/utility/getEC2TagValue.sh
+++ packages/devops/scripts/utility/getEC2TagValue.sh
@@ -8,10 +8,8 @@ instance_id=$(ec2metadata --instance-id)
 availability_zone=$(ec2metadata --availability-zone)
 region=${availability_zone::-1}
 
-tag_value=$(aws ec2 describe-tags \
+aws ec2 describe-tags \
 	--filters "Name=resource-id,Values=$instance_id" "Name=key,Values=$tag_name" \
 	--region "$region" \
 	--query Tags[0].Value \
-	--output=text)
-
-echo $tag_value
+	--output=text
