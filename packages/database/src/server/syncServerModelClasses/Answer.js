da4f328760783d039eb51714aee47eef6e5be0f8
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-01-13 11:10:42 +1300

    fix(database): missing `this` keyword on property access

--- packages/database/src/server/syncServerModelClasses/Answer.js
+++ packages/database/src/server/syncServerModelClasses/Answer.js
@@ -6,24 +6,24 @@ import { AnswerModel as BaseAnswerModel } from '../../core/modelClasses';
  * Extends from base AnswerModel to handle server-side image and file sync.
  */
 export class AnswerModel extends BaseAnswerModel {
-  s3Client;
+  #s3Client;
 
   constructor(...args) {
     super(...args);
-    this.s3Client = new S3Client(new S3());
+    this.#s3Client = new S3Client(new S3());
   }
 
   async incomingSyncHook(records) {
     const uploadPromises = records.map(async record => {
       if (record.data.type === QuestionType.Photo) {
-        const newAnswerText = await s3Client.uploadImage(record.data.text, record.data.id);
+        const newAnswerText = await this.#s3Client.uploadImage(record.data.text, record.data.id);
         return { ...record, data: { ...record.data, text: newAnswerText } };
       }
 
       if (record.data.type === QuestionType.File) {
         const { uniqueFileName, data } = JSON.parse(record.data.text);
         if (uniqueFileName && data) {
-          const newAnswerText = await s3Client.uploadFile(uniqueFileName, data);
+          const newAnswerText = await this.#s3Client.uploadFile(uniqueFileName, data);
           return { ...record, data: { ...record.data, text: newAnswerText } };
         }
       }
