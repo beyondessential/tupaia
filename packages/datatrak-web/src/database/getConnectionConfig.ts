92103d4c605ecedceabf167afac08e743eac28f6
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-01-27 14:24:44 +1300

    doc(datatrakWeb): remove dead comment

--- packages/datatrak-web/src/database/getConnectionConfig.ts
+++ packages/datatrak-web/src/database/getConnectionConfig.ts
@@ -6,7 +6,6 @@ export const getConnectionConfig = () => {
   const connectionString = getEnvVarOrDefault('PG_LITE_CONNECTION_STRING', 'idb://datatrak-db');
 
   if (process.env.NODE_ENV === 'production') {
-    // preallocate 128MB of memory for the pglite instance, otherwise it can cause out of memory issue when syncing a lot of changes while the browser is running a lot of tabs
     return {
       pglite: new PGlite(connectionString),
     };
