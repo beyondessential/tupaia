a8f5cc2b196db1a8f261aed29ca7ccd646d7d83f
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-01-30 12:49:55 +1300

    use offline

--- packages/datatrak-web/src/api/queries/useRecentSurveys.ts
+++ packages/datatrak-web/src/api/queries/useRecentSurveys.ts
@@ -1,14 +1,37 @@
-import { useQuery } from '@tanstack/react-query';
-import { DatatrakWebRecentSurveysRequest, Project, UserAccount } from '@tupaia/types';
-import { get } from '../api';
+import { camelcaseKeys, ensure } from '@tupaia/tsutils';
+import type { DatatrakWebRecentSurveysRequest, Project, UserAccount } from '@tupaia/types';
 import { useCurrentUserContext } from '../CurrentUserContext';
+import { get } from '../api';
+import { useIsOfflineFirst } from '../offlineFirst';
+import { useDatabaseQuery, type ContextualQueryFunctionContext } from './useDatabaseQuery';
+
+interface RecentSurveysQueryContext extends ContextualQueryFunctionContext {
+  projectId?: Project['id'];
+  userId?: UserAccount['id'];
+}
+
+interface RecentSurveysQueryFunction {
+  (context: RecentSurveysQueryContext): Promise<DatatrakWebRecentSurveysRequest.ResBody>;
+}
+
+const queryFunctions = {
+  local: async ({ models, projectId, userId }) => {
+    const surveyResponses = await models.user.getRecentSurveys(ensure(userId), projectId);
+    return camelcaseKeys(surveyResponses);
+  },
+  remote: async ({ projectId, userId }) =>
+    await get('recentSurveys', { params: { projectId, userId } }),
+} as const satisfies Record<'local' | 'remote', RecentSurveysQueryFunction>;
 
 export const useRecentSurveys = (userId?: UserAccount['id'], projectId?: Project['id']) => {
-  return useQuery(
+  const isOfflineFirst = useIsOfflineFirst();
+  return useDatabaseQuery<DatatrakWebRecentSurveysRequest.ResBody>(
     ['recentSurveys', userId, projectId],
-    (): Promise<DatatrakWebRecentSurveysRequest.ResBody> =>
-      get('recentSurveys', { params: { userId, projectId } }),
-    { enabled: !!userId && !!projectId },
+    isOfflineFirst ? queryFunctions.local : queryFunctions.remote,
+    {
+      enabled: !!userId && !!projectId,
+      localContext: { userId, projectId },
+    },
   );
 };
 
