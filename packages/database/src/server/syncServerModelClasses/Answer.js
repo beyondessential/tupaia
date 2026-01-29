dc9791185b3abc0b119a7759cfa892cadee751e2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-01-29 17:29:42 +1300

    fix

--- packages/database/src/server/syncServerModelClasses/Answer.js
+++ packages/database/src/server/syncServerModelClasses/Answer.js
@@ -1,7 +1,7 @@
 import { S3, S3Client } from '@tupaia/server-utils';
 import { QuestionType } from '@tupaia/types';
-
 import { AnswerModel as BaseAnswerModel } from '../../core/modelClasses';
+
 /**
  * Extends from base AnswerModel to handle server-side image and file sync.
  */
@@ -13,9 +13,11 @@ export class AnswerModel extends BaseAnswerModel {
     this.#s3Client = new S3Client(new S3());
   }
 
+  /** @satisfies {undefined | (records: SyncSnapshotAttributes[]) => Promise<{ inserts: SyncSnapshotAttributes[], updates: SyncSnapshotAttributes[] }>} */
   async incomingSyncHook(records) {
     const uploadPromises = records.map(async record => {
       if (record.data.type === QuestionType.Photo) {
+        // answer.text for Image questions store the image URL
         const newAnswerText = await this.#s3Client.uploadImage(record.data.text, record.data.id);
         return { ...record, data: { ...record.data, text: newAnswerText } };
       }
@@ -23,8 +25,9 @@ export class AnswerModel extends BaseAnswerModel {
       if (record.data.type === QuestionType.File) {
         const { uniqueFileName, data } = JSON.parse(record.data.text);
         if (uniqueFileName && data) {
-          const newAnswerText = await this.#s3Client.uploadFile(uniqueFileName, data);
-          return { ...record, data: { ...record.data, text: newAnswerText } };
+          // answer.text for File questions store the filename
+          void (await this.#s3Client.uploadFile(uniqueFileName, data));
+          return { ...record, data: { ...record.data, text: uniqueFileName } };
         }
       }
 
