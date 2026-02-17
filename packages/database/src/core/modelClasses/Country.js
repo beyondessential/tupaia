9956dfedaff522f1e9246d7b32a58080f53b0fd0
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-02-18 10:22:19 +1300

    chore(database): dedupe instance method

--- packages/database/src/core/modelClasses/Country.js
+++ packages/database/src/core/modelClasses/Country.js
@@ -15,10 +15,6 @@ export class CountryRecord extends DatabaseRecord {
 export class CountryModel extends DatabaseModel {
   static syncDirection = SyncDirections.PULL_FROM_CENTRAL;
 
-  async buildSyncLookupQueryDetails() {
-    return null;
-  }
-
   get DatabaseRecordClass() {
     return CountryRecord;
   }
