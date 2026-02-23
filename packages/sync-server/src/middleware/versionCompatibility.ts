f72b7ca1acf1ee5d6a0eac900d0c5a60f100fc0a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-02-23 17:21:01 +1300

    use semver

--- packages/sync-server/src/middleware/versionCompatibility.ts
+++ packages/sync-server/src/middleware/versionCompatibility.ts
@@ -1,6 +1,8 @@
 import type { NextFunction, Request, Response } from 'express';
+import semverNeq from 'semver/functions/neq';
+import semverValid from 'semver/functions/valid';
 
-import { version } from '../../package.json'; // with { type: 'json' };
+import { version } from '../../package.json';
 
 export const versionCompatibility = (req: Request, res: Response, next: NextFunction) => {
   const clientVersion = req.header('X-Client-Version');
@@ -10,12 +12,19 @@ export const versionCompatibility = (req: Request, res: Response, next: NextFunc
     return;
   }
 
-  // Simple inequality check because Tupaia can assume server version ≥ client version. If server
-  // version is different, then it is newer; and client is considered outdated.
-  if (clientVersion !== version) {
+  if (semverValid(clientVersion) === null) {
+    res.status(400).json({
+      error: `Malformed X-Client-Version header. “${clientVersion}” isn’t a valid semver number`,
+    });
+    return;
+  }
+
+  if (semverNeq(clientVersion, version)) {
+    // Simple inequality check because Tupaia can assume server version ≥ client version. If server
+    // version is different, then it is newer; and client is considered outdated.
     res.setHeader('X-Required-Client-Version', version);
     res.status(400).json({
-      error: `Please reload to get the latest version of Tupaia DataTrak (v${version}) before syncing.`,
+      error: `Please reload to get the latest version of Tupaia DataTrak (v${version}) before syncing`,
     });
     return;
   }
