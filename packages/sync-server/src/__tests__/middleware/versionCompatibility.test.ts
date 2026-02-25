b052024d941d29874f1dfafb8b6a8ad878834994
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-02-26 12:06:23 +1300

    test(syncServer): RN-1751: version compatibility test suite (#6647)
    * try
    
    * fix bad mock
    
    * simplify
    
    * delete stub test
    
    * use `it.each`
    
    * reorder
    
    * refactor
    
    * comment unrealistic cases
    
    * fix
    
    * typo
    
    * proper reset before each
    
    * mock next fn
    
    fix
    
    * less verbose
    
    * dedupe type casts
    
    * just mock it
    
    * remove exclusive test
    
    * fix test descriptions

---
+++ packages/sync-server/src/__tests__/middleware/versionCompatibility.test.ts
@@ -0,0 +1,93 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import { versionCompatibility } from '../../middleware/versionCompatibility';
+
+const mockRequest = (headers: Record<string, string> = {}) =>
+  ({
+    header: ((name: string) => headers[name] ?? undefined) as Request['header'],
+  }) as Partial<Request> as Request;
+
+const mockResponse = () =>
+  ({
+    json: jest.fn().mockReturnThis(),
+    status: jest.fn().mockReturnThis(),
+  }) as Partial<Response> as Response;
+
+const mockNextFunction = jest.fn() as jest.MockedFunction<NextFunction>;
+
+describe('versionCompatibility', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.resetAllMocks();
+  });
+
+  describe('malformed request header', () => {
+    it('should respond with 400 if `X-Client-Version` header is missing', () => {
+      const req = mockRequest();
+      const res = mockResponse();
+
+      versionCompatibility(req, res, mockNextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        error: 'Missing X-Client-Version header. This is required for sync.',
+      });
+      expect(mockNextFunction).not.toHaveBeenCalled();
+    });
+
+    it('should respond with 400 if `X-Client-Version` header is not a valid semver', () => {
+      const invalidVersion = 'not-a-version';
+      const req = mockRequest({ 'X-Client-Version': invalidVersion });
+      const res = mockResponse();
+
+      versionCompatibility(req, res, mockNextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        error: `Malformed X-Client-Version header. “${invalidVersion}” isn’t a valid semver number.`,
+      });
+      expect(mockNextFunction).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('version discrepancy between client and server', () => {
+    it.each([
+      ['client major version is older', '0.0.0', '1.0.0'],
+      ['client minor version is older', '0.0.0', '0.1.0'],
+      ['client patch version is older', '0.0.0', '0.0.1'],
+      // Cases below are never expected to occur in practice
+      ['client major version is newer', '1.0.0', '0.0.0'],
+      ['client minor version is newer', '0.1.0', '0.0.0'],
+      ['client patch version is newer', '0.0.1', '0.0.0'],
+    ])('should respond with 400 if %s', (_description, clientVersion, serverVersion) => {
+      jest.doMock('../../../package.json', () => ({ version: serverVersion }));
+      const { versionCompatibility } = require('../../middleware/versionCompatibility');
+
+      const req = mockRequest({ 'X-Client-Version': clientVersion });
+      const res = mockResponse();
+
+      versionCompatibility(req, res, mockNextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        error: `Please reload to get the latest version of Tupaia DataTrak (v${serverVersion}) before syncing`,
+      });
+      expect(mockNextFunction).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should not error if the versions match', () => {
+    const version = '10.10.10';
+    jest.doMock('../../../package.json', () => ({ version }));
+    const { versionCompatibility } = require('../../middleware/versionCompatibility');
+
+    const req = mockRequest({ 'X-Client-Version': version });
+    const res = mockResponse();
+
+    versionCompatibility(req, res, mockNextFunction);
+
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+    expect(mockNextFunction).toHaveBeenCalled();
+  });
+});
