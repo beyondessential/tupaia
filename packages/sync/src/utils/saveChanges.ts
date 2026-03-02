c7a301c01a8db29d307525265dbe0f654db38182
chris-bes <chris@beyondessential.com.au>
2026-03-02 15:01:47 +1100

    fix error

--- packages/sync/src/utils/saveChanges.ts
+++ packages/sync/src/utils/saveChanges.ts
@@ -11,18 +11,8 @@ export const saveCreates = async (
     const batch = records.slice(i, i + batchSize);
     try {
       await model.createMany(batch);
-    } catch (originalError: any) {
-      // try records individually, some may succeed and we want to capture the
-      // specific one with the error
-      await Promise.all(
-        batch.map(async row => {
-          try {
-            await model.create(row);
-          } catch (error: any) {
-            throw new Error(`Insert failed for record '${row.id}': ${originalError.message}`);
-          }
-        }),
-      );
+    } catch (error: any) {
+      throw new Error(`Batch insert failed for model ${model.databaseRecord}: ${error.message}`);
     }
 
     progressCallback?.(batch.length);
@@ -50,18 +40,8 @@ export const saveUpdates = async (
       } else {
         await bulkUpdateForClient(model, batch);
       }
-    } catch (originalError: any) {
-      // try records individually, some may succeed and we want to capture the
-      // specific one with the error
-      await Promise.all(
-        batch.map(async row => {
-          try {
-            await model.updateById(row.id, row);
-          } catch (error: any) {
-            throw new Error(`Update failed for record '${row.id}': ${originalError.message}`);
-          }
-        }),
-      );
+    } catch (error: any) {
+      throw new Error(`Batch update failed for model ${model.databaseRecord}: ${error.message}`);
     }
 
     progressCallback?.(batch.length);
@@ -105,18 +85,8 @@ export const saveDeletes = async (
     const batch = recordsForDelete.slice(i, i + batchSize);
     try {
       await model.delete({ id: batch.map(r => r.id) });
-    } catch (originalError: any) {
-      // try records individually, some may succeed and we want to capture the
-      // specific one with the error
-      await Promise.all(
-        batch.map(async row => {
-          try {
-            await model.delete({ id: row.id });
-          } catch (error: any) {
-            throw new Error(`Delete failed for record '${row.id}': ${originalError.message}`);
-          }
-        }),
-      );
+    } catch (error: any) {
+      throw new Error(`Batch delete failed for model ${model.databaseRecord}: ${error.message}`);
     }
 
     progressCallback?.(batch.length);
