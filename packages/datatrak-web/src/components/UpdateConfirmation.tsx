d36ebf9cc71062a59bd0882abf8fb17f5d8939e2
chris-bes <63621318+chris-bes@users.noreply.github.com>
2026-03-04 14:24:00 +1100

    fix(datatrak): RN-1717: Fix build (#6656)
    * fix build
    
    * increase size
    
    * test
    
    * add confirm when there is no internet

--- packages/datatrak-web/src/components/UpdateConfirmation.tsx
+++ packages/datatrak-web/src/components/UpdateConfirmation.tsx
@@ -3,6 +3,7 @@ import { Link } from '@mui/material';
 import styled from 'styled-components';
 
 import { BannerNotification } from '../views/BannerNotification';
+import { ConfirmationModal } from './ConfirmationModal';
 
 const StyledLink = styled(Link)`
   &.MuiLink-root {
@@ -22,6 +23,7 @@ export function setWaitingWorker(worker: ServiceWorker) {
 
 export const UpdateNotification = () => {
   const [updateAvailable, setUpdateAvailable] = useState(!!pendingWorker);
+  const [showOfflineModal, setShowOfflineModal] = useState(false);
 
   useEffect(() => {
     notifyComponent = () => setUpdateAvailable(true);
@@ -38,14 +40,30 @@ export const UpdateNotification = () => {
   if (!updateAvailable) return null;
 
   const handleClick = () => {
+    if (!window.navigator.onLine) {
+      setShowOfflineModal(true);
+      return;
+    }
+    setShowOfflineModal(false);
     pendingWorker?.postMessage({ type: 'SKIP_WAITING' });
     window.location.reload();
   };
 
   return (
-    <BannerNotification style={{ backgroundColor: '#002d47' }}>
-      A new version of DataTrak is now available,{' '}
-      <StyledLink onClick={handleClick}>click here</StyledLink> to get the latest version.
-    </BannerNotification>
+    <>
+      <BannerNotification style={{ backgroundColor: '#002d47' }}>
+        A new version of DataTrak is now available,{' '}
+        <StyledLink onClick={handleClick}>click here</StyledLink> to get the latest version.
+      </BannerNotification>
+      <ConfirmationModal
+        isOpen={showOfflineModal}
+        onClose={() => setShowOfflineModal(false)}
+        onConfirm={() => setShowOfflineModal(false)}
+        heading="No internet connection"
+        description="Updating requires an active internet connection. Please connect to the internet and try again."
+        confirmLabel="OK"
+        cancelLabel="Dismiss"
+      />
+    </>
   );
 };
