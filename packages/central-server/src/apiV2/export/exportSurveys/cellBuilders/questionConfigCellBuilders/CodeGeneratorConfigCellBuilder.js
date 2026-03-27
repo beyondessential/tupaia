3fd5fb2c429d8289923df01746669f1b50af7609
Tom Caiger <caigertom@gmail.com>
2026-03-27 15:38:55 +1300

    Update CodeGeneratorConfigCellBuilder.js

--- packages/central-server/src/apiV2/export/exportSurveys/cellBuilders/questionConfigCellBuilders/CodeGeneratorConfigCellBuilder.js
+++ packages/central-server/src/apiV2/export/exportSurveys/cellBuilders/questionConfigCellBuilders/CodeGeneratorConfigCellBuilder.js
@@ -1,65 +1,23 @@
 import { KeyValueCellBuilder } from '../KeyValueCellBuilder';
 
-const FIELD_TRANSLATION = {
-  'dynamicPrefix.questionId': 'dynamicPrefix',
-};
-
-const flattenObject = (value, field, flattenedObject = {}) => {
-  if (!(typeof value === 'object' && !Array.isArray(value) && value !== null)) {
-    // eslint-disable-next-line no-param-reassign
-    flattenedObject[field] = value;
-    return;
-  }
-
-  Object.entries(value).forEach(([subField, subValue]) => {
-    flattenObject(subValue, `${field}.${subField}`, flattenedObject);
-  });
-};
-
 export class CodeGeneratorConfigCellBuilder extends KeyValueCellBuilder {
-  async processValue(value, field) {
-    if (field === 'dynamicPrefix') {
-      return this.fetchQuestionCode({ questionId: value });
-    }
-    return value;
-  }
+  individualFieldProcessors = {
+    dynamicPrefix: async (models, dynamicPrefixObject = {}) => {
+      const processedEntries = await Promise.all(
+        Object.entries(dynamicPrefixObject).map(async ([subField, value]) => {
+          const field = `dynamicPrefix.${subField}`;
+          if (subField === 'questionId') {
+            const code = await this.fetchQuestionCode({ questionId: value });
+            return `dynamicPrefix: ${code}`;
+          }
+          return `${field}: ${value}`;
+        }),
+      );
+      return processedEntries.join('\r\n');
+    },
+  };
 
   extractRelevantObject({ codeGenerator }) {
     return codeGenerator;
-  }
-
-  async processField(rawValue, rawField) {
-    const flattenedFields = {};
-    flattenObject(rawValue, rawField, flattenedFields);
-
-    const processedFieldsAndValues = await Promise.all(
-      Object.entries(flattenedFields).map(async ([field, value]) => {
-        const translatedField = FIELD_TRANSLATION[field] || field;
-        const translatedValue = await this.processValue(value, translatedField);
-        return `${translatedField}: ${translatedValue}`;
-      }),
-    );
-    return processedFieldsAndValues;
-  }
-
-  async build(jsonStringOrObject) {
-    if (!jsonStringOrObject) {
-      return '';
-    }
-
-    const fullObject =
-      typeof jsonStringOrObject === 'string' ? JSON.parse(jsonStringOrObject) : jsonStringOrObject;
-    const object = this.extractRelevantObject(fullObject) || {};
-
-    const processedFieldsPromises = Object.entries(object).map(async ([field, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
-        return this.processField(value, field);
-      }
-      const processedValue = await this.processValue(value, field);
-      return `${field}: ${processedValue}`;
-    });
-
-    const processedFields = (await Promise.all(processedFieldsPromises)).flat();
-    return processedFields.join('\r\n');
   }
 }
