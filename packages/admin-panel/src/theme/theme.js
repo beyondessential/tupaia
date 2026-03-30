4426f450ae0c45eae3172e2d682965860f0e7699
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-03-30 14:03:13 +1300

    tweak: default to font-defined underline thickness, other CSS remedies (#6700)
    * `text-decoration-thickness: from-font`
    
    * and the rest
    
    * whitespace
    
    * `touch-action: manupulation`
    
    * balance headings
    
    * apply to `MuiLink-root`

--- packages/admin-panel/src/theme/theme.js
+++ packages/admin-panel/src/theme/theme.js
@@ -195,6 +195,18 @@ const overrides = {
   },
   MuiCssBaseline: {
     '@global': {
+      '*': {
+        textDecorationThickness: 'from-font',
+      },
+      html: {
+        textWrap: 'pretty',
+      },
+      'button, figcaption, h1, h2, h3, h4, h5, h6, input, label': {
+        textWrap: 'balance',
+      },
+      'button, input, textarea, select': {
+        touchAction: 'manipulation',
+      },
       label: {
         fontWeight: 500,
       },
@@ -211,6 +223,9 @@ const overrides = {
     root: {
       fontVariantNumeric: 'lining-nums slashed-zero tabular-nums',
     },
+  },
+  MuiLink: {
+    root: { textDecorationThickness: 'from-font' },
   },
   MuiSvgIcon: {
     root: {
