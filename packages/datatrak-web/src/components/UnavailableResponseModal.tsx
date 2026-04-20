fba4593b6995ed6b9bec125975941b1f11be288a
chris-bes <chris@beyondessential.com.au>
2026-04-21 09:51:09 +1000

    fix

--- packages/datatrak-web/src/components/UnavailableResponseModal.tsx
+++ packages/datatrak-web/src/components/UnavailableResponseModal.tsx
@@ -1,45 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Typography } from '@material-ui/core';
-import { Button } from './Button';
-
-import { Modal } from './Modal';
-
-const Heading = styled(Typography).attrs({
-  variant: 'h2',
-  align: 'center',
-})`
-  margin-bottom: 1rem;
-`;
-
-const Wrapper = styled.div`
-  max-inline-size: 27rem;
-  ${({ theme }) => theme.breakpoints.up('sm')} {
-    padding: 1rem 2rem;
-  }
-`;
-
-const ModalButton = styled(Button)`
-  &.MuiButtonBase-root.MuiButton-root {
-    flex: 1;
-    margin: 0;
-    white-space: nowrap;
-  }
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column-reverse;
-  gap: 1rem;
-  inline-size: 100%;
-  margin: 1.5rem auto 0;
-  max-inline-size: 20rem;
-
-  ${({ theme }) => theme.breakpoints.up('sm')} {
-    flex-direction: row;
-    justify-content: center;
-  }
-`;
+import { InfoMessageModal } from './InfoMessageModal';
 
 interface UnavailableResponseModalProps {
   isOpen: boolean;
@@ -47,18 +7,10 @@ interface UnavailableResponseModalProps {
 }
 
 export const UnavailableResponseModal = ({ isOpen, onClose }: UnavailableResponseModalProps) => (
-  <Modal open={isOpen} onClose={onClose}>
-    <Wrapper>
-      <Heading>Unavailable to view submission</Heading>
-      <Typography align="center">
-        This submission was completed on a different device and cannot be viewed while on this
-        device. Please use the web app version to view the submission.
-      </Typography>
-      <ButtonWrapper>
-        <ModalButton onClick={onClose} variant="outlined">
-          Close
-        </ModalButton>
-      </ButtonWrapper>
-    </Wrapper>
-  </Modal>
+  <InfoMessageModal
+    isOpen={isOpen}
+    onClose={onClose}
+    heading="Unavailable to view submission"
+    message="This submission was completed on a different device and therefore cannot be viewed. Please log into DataTrak on a web browser in order to view the submission."
+  />
 );
