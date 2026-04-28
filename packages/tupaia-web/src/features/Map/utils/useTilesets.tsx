f2e172684a03b02490a5ceff74afdfb8068719ad
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-29 09:40:35 +1200

    fix

--- packages/tupaia-web/src/features/Map/utils/useTilesets.tsx
+++ packages/tupaia-web/src/features/Map/utils/useTilesets.tsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import type { TileSet } from '@tupaia/ui-map-components';
 import { DEFAULT_TILESETS, getAutoTileSet } from '@tupaia/ui-map-components';
-import { CUSTOM_TILE_SETS } from '../../../constants';
+import { useEffect, useMemo, useState } from 'react';
+import { useParams } from 'react-router';
 import { useProject } from '../../../api/queries';
+import { CUSTOM_TILE_SETS } from '../../../constants';
 import { useGAEffect } from '../../../utils';
+
+const defaultTileSets = [DEFAULT_TILESETS.osm, DEFAULT_TILESETS.satellite];
 
 export const useTilesets = () => {
   const { projectCode } = useParams();
   const { data: project } = useProject(projectCode);
   const initialTileSet = getAutoTileSet();
-  const [activeTileSet, setActiveTileSet] = useState(initialTileSet);
-  const { tileSets = '' } = project?.config || {};
-  const customTilesetNames = tileSets.split(',');
-  const customTileSets = customTilesetNames
-    .map(
-      tileset =>
-        CUSTOM_TILE_SETS.find(({ key }) => key === tileset) as (typeof CUSTOM_TILE_SETS)[0],
-    )
-    .filter(item => item);
-  const defaultTileSets = [DEFAULT_TILESETS.osm, DEFAULT_TILESETS.satellite];
-  const availableTileSets = [...defaultTileSets, ...customTileSets];
+  const [activeTileSet, setActiveTileSet] = useState<TileSet>(initialTileSet);
+
+  const availableTileSets = useMemo(() => {
+    const { tileSets = '' } = project?.config || {};
+    const customTilesetNames = tileSets.split(',');
+    const customTileSets = customTilesetNames
+      .map(tileset => CUSTOM_TILE_SETS.find(({ key }) => key === tileset) as TileSet)
+      .filter(item => item);
+    return [...defaultTileSets, ...customTileSets];
+  }, [project?.config]);
 
   useGAEffect('Map', 'Change Tile Set', activeTileSet?.label);
 
   const onTileSetChange = (tileSetKey: string) => {
-    const newActiveTileSet = availableTileSets.find(
-      ({ key }) => key === tileSetKey,
-    ) as (typeof CUSTOM_TILE_SETS)[0];
+    const newActiveTileSet = availableTileSets.find(({ key }) => key === tileSetKey);
     setActiveTileSet(newActiveTileSet);
   };
 
@@ -38,7 +38,7 @@ export const useTilesets = () => {
     ) {
       setActiveTileSet(initialTileSet);
     }
-  }, [JSON.stringify(availableTileSets)]);
+  }, [availableTileSets]);
 
   return {
     availableTileSets,
