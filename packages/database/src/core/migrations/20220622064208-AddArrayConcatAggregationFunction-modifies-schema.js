f47d6a74bb3878121548c76b234373be44ddf8e3
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-05-05 12:31:00 +1200

    mutate migration

--- packages/database/src/core/migrations/20220622064208-AddArrayConcatAggregationFunction-modifies-schema.js
+++ packages/database/src/core/migrations/20220622064208-AddArrayConcatAggregationFunction-modifies-schema.js
@@ -14,21 +14,27 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
+/**
+ * @privateRemarks
+ * PostgreSQL 14 updated `array_cat()` to take `anycompatiblearray` (previously `anyarray`).
+ * When we upgraded Tupaia’s RDS engine from v13 to v18, this migration was edited to also take
+ * `anycompatiblearray` so this migration doesn’t error.
+ * @see https://www.postgresql.org/docs/release/14.0
+ */
 exports.up = async function (db) {
+  await db.runSql('DROP AGGREGATE IF EXISTS array_concat_agg(anyarray);');
   await db.runSql(`
-    CREATE OR REPLACE AGGREGATE array_concat_agg(anyarray) (
+    CREATE OR REPLACE AGGREGATE array_concat_agg(anycompatiblearray) (
       SFUNC = array_cat,
-      STYPE = anyarray
+      STYPE = anycompatiblearray
     );
   `);
 };
 
 exports.down = async function (db) {
-  await db.runSql('DROP AGGREGATE IF EXISTS array_concat_agg(anyarray);');
+  await db.runSql('DROP AGGREGATE IF EXISTS array_concat_agg(anycompatiblearray);');
 };
 
 exports._meta = {
   version: 1,
-  /* `array_concat_agg()` is used only by `createPermissionsBasedMeditrakSyncQueue` */
-  targets: ['server'],
 };
