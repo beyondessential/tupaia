752cd4a8a60b69db8725e6c8d4992b0ba998d15a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-05-11 18:03:48 +1200

    smush

--- packages/central-server/src/apiV2/export/exportOptionSet/exportOptionSet.js
+++ packages/central-server/src/apiV2/export/exportOptionSet/exportOptionSet.js
@@ -28,9 +28,7 @@ export async function exportOptionSet(req, res) {
   const worksheet = xlsx.utils.json_to_sheet(options);
   const sheetName = sanitizeWorksheetName(optionSetRecord.name);
 
-  // Upgrade xlsx to v0.20 or newer to do next two lines in one go
-  const workbook = xlsx.utils.book_new();
-  xlsx.utils.book_append_sheet(workbook, worksheet, sheetName);
+  const workbook = xlsx.utils.book_new(worksheet, sheetName);
 
   const dirname = getExportPathForUser(userId);
   const basename = toFilename(`Option Set - ${optionSetRecord.name}.xlsx`);
