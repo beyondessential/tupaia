cc866ca6f993af9b1971431ee53a15ed7819c83c
Tom Caiger <caigertom@gmail.com>
2026-05-19 14:04:12 +1200

    refactor: TUP-3156: DHIS2 push project scoping cleanup

--- packages/central-server/src/database/utilities/getEntityIdFromClinicId.js
+++ packages/central-server/src/database/utilities/getEntityIdFromClinicId.js
@@ -5,9 +5,6 @@ export async function getEntityIdFromClinicId(models, clinicId, projectId = null
     return null;
   }
   const clinic = await models.facility.findById(clinicId);
-  // TUP-3156: facility codes (clinics, hospitals) are sub-country and duplicated
-  // per project. Caller should pass the survey's project_id so we resolve to
-  // the right copy. Falls back to bare lookup if projectId is null.
   const entity = await models.entity.findOneByCodeInProject(clinic.code, projectId);
   if (!entity) {
     throw new Error(`Entity could not be found for clinic with code ${clinic.code}.`);
