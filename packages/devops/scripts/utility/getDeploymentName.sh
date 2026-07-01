a6699cc2567f4585e114ba1926c559748771364e
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-01 19:16:18 +1200

    fix dead code

--- packages/devops/scripts/utility/getDeploymentName.sh
+++ packages/devops/scripts/utility/getDeploymentName.sh
@@ -3,12 +3,15 @@ set -e +x
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+set +e
 deployment_name=$("$script_dir"/getEC2TagValue.sh DeploymentName)
+exit_code=$?
+set -e
 
 # Branch names should be lowercase (see /packages/devops/scripts/ci/validateBranchName.sh), but
 # this is only enforced when deployed by /packages/devops/scripts/ci/triggerRedeploy.sh, which is
 # guarded by CI. Nevertheless, it should be exceedingly unlikely for DeploymentName to be ‘None’.
-if (($? != 0)) || [[ $deployment_name = None ]]; then
+if ((exit_code != 0)) || [[ $deployment_name = None ]]; then
   source "$script_dir"/../../../../scripts/bash/ansiControlSequences.sh
   {
     echo -en "${BOLD}${YELLOW}getEC2TagValue error.${RESET} "
