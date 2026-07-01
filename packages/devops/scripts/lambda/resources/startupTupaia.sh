59a18d1163bacdba543bdf73537b3b417ddeb0a4
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-02 11:48:18 +1200

    this does it

--- packages/devops/scripts/lambda/resources/startupTupaia.sh
+++ packages/devops/scripts/lambda/resources/startupTupaia.sh
@@ -9,12 +9,12 @@
 
 set -o pipefail # fail pipe where scripts are e.g. piped out to deployment logs
 
-declare -i start_time="$SECONDS"
+declare -i start_time=$(date +%s)
+export start_time
 
 home_dir=/home/ubuntu
-tupaia_dir=$home_dir/tupaia
 logs_dir=$home_dir/logs
-deployment_scripts=$tupaia_dir/packages/devops/scripts/deployment-aws
+deployment_scripts=$home_dir/tupaia/packages/devops/scripts/deployment-aws
 
 # Add tag for CI/CD to use as a health check
 instance_id=$(ec2metadata --instance-id)
@@ -25,13 +25,12 @@ tag_errored() {
   aws ec2 create-tags --resources "$instance_id" --tags Key=StartupBuildProgress,Value=errored
   service nginx stop # stop nginx as an obvious sign the build has failed
 
-  declare -i duration=$((SECONDS - start_time))
+  declare -i duration=$(($(date +%s) - start_time))
   echo "Startup failed after $((duration / 60)) min $((duration % 60)) s"
 }
 trap tag_errored ERR
 
 deployment_name=$("$deployment_scripts"/../utility/getEC2TagValue.sh DeploymentName)
-branch=$("$deployment_scripts"/../utility/getEC2TagValue.sh Branch)
 
 # Set bash prompt to have deployment name in it
 set_prompt() {
@@ -66,65 +65,75 @@ set_prompt
 # Create a directory for logs to go
 mkdir -m 777 -p "$logs_dir"
 
-schedule_preaggregation_job() {
-  echo "10 13 * * * PATH=$PATH $home_dir/tupaia/packages/web-config-server/run_preaggregation.sh | while IFS= read -r line; do echo \"\$(date --iso-8601=seconds) │ \$line\"; done > $logs_dir/preaggregation.txt" >tmp.cron
-  crontab -l >>tmp.cron || echo >>tmp.cron
-  crontab tmp.cron
-  rm tmp.cron
-}
-
-fetch_latest_code() {
-  cd "$tupaia_dir"
-  if git ls-remote --exit-code --heads origin "$branch" &>/dev/null; then
-    echo "Fetching latest code from branch $branch..."
-    local branch_to_use=$branch
-  else
-    echo "Branch $branch doesn’t exist. Fetching latest code from dev..."
-    local branch_to_use=dev
-  fi
-  set -x
-  git remote set-branches --add origin "$branch_to_use"
-  git fetch --all --prune
-  git reset --hard # clear out any manual changes that have been made, which would cause checkout to fail
-  git switch "$branch_to_use"
-  git reset --hard origin/"$branch_to_use"
-  set +x
-}
-
 main() {
+  local home_dir=/home/ubuntu
+  local tupaia_dir=$home_dir/tupaia
+  local logs_dir=$home_dir/logs
+  local deployment_scripts=$tupaia_dir/packages/devops/scripts/deployment-aws
+  local deployment_name=$("$deployment_scripts"/../utility/getEC2TagValue.sh DeploymentName)
+  local branch=$("$deployment_scripts"/../utility/getEC2TagValue.sh Branch)
+  local instance_id=$(ec2metadata --instance-id)
+
+  schedule_preaggregation_job() {
+    echo "10 13 * * * PATH=$PATH $home_dir/tupaia/packages/web-config-server/run_preaggregation.sh | while IFS= read -r line; do echo \"\$(date --iso-8601=seconds) │ \$line\"; done > $logs_dir/preaggregation.txt" >tmp.cron
+    crontab -l >>tmp.cron || echo >>tmp.cron
+    crontab tmp.cron
+    rm tmp.cron
+  }
+
+  fetch_latest_code() {
+    cd "$tupaia_dir"
+    if git ls-remote --exit-code --heads origin "$branch" &>/dev/null; then
+      echo "Fetching latest code from branch $branch..."
+      local branch_to_use=$branch
+    else
+      echo "Branch $branch doesn’t exist. Fetching latest code from dev..."
+      local branch_to_use=dev
+    fi
+    set -x
+    git remote set-branches --add origin "$branch_to_use"
+    git fetch --all --prune
+    git reset --hard # clear out any manual changes that have been made, which would cause checkout to fail
+    git switch "$branch_to_use"
+    git reset --hard origin/"$branch_to_use"
+    set +x
+  }
+
   echo "Starting up $deployment_name ($branch)"
 
   # Turn on cloudwatch agent for prod and dev (can be turned on manually if needed on feature instances)
   # TODO currently broken
   # if [[ $deployment_name = production || $deployment_name = dev ]]; then
-  #   sudo -Hu ubuntu $deployment_scripts/startCloudwatchAgent.sh
+  #   $deployment_scripts/startCloudwatchAgent.sh
   # fi
 
-  \. "$home_dir/.nvm/nvm.sh" # Load nvm so node is available on $PATH
+  if [[ -s $NVM_DIR/nvm.sh ]]; then
+    source "$NVM_DIR/nvm.sh" # Load nvm so node is available on $PATH
+  fi
 
-  sudo -Hu ubuntu fetch_latest_code
+  fetch_latest_code
 
   if [[ $deployment_name = production ]]; then
-    sudo -u ubuntu schedule_preaggregation_job
+    schedule_preaggregation_job
   fi
 
   # central-server and data-table-server need Tailnet access for external database connections
-  sudo -Hu ubuntu DEPLOYMENT_NAME="$deployment_name" "$deployment_scripts"/connectTailscale.sh
+  DEPLOYMENT_NAME="$deployment_name" "$deployment_scripts"/connectTailscale.sh
   # Build each package, including injecting environment variables from Bitwarden
-  sudo -Hu ubuntu "$deployment_scripts"/buildDeployablePackages.sh "$deployment_name"
+  "$deployment_scripts"/buildDeployablePackages.sh "$deployment_name"
   # Deploy each package
-  sudo -Hu ubuntu "$deployment_scripts"/../deployment-common/startBackEnds.sh
+  "$deployment_scripts"/../deployment-common/startBackEnds.sh
   # Set nginx config and start the service running
   sudo -E DEPLOYMENT_NAME="$deployment_name" "$deployment_scripts"/configureNginx.sh
 
   # Tag as complete so CI/CD system can use the tag as a health check
   aws ec2 create-tags --resources "$instance_id" --tags Key=StartupBuildProgress,Value=complete
 
-  local -i duration=$((SECONDS - start_time))
+  declare -i duration=$(($(date +%s) - start_time))
   echo "Startup completed in $((duration / 60)) min $((duration % 60)) s"
 }
 
-main |&
+sudo -Hu ubuntu bash -leE -c "$(declare -f main); main" |&
   while IFS= read -r line; do
     echo "$(date --iso-8601=seconds) │ $line"
   done >>"$logs_dir"/deployment.log
