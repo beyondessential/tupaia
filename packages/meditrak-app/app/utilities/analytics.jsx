7a0ec2cdce5c848ebb8027baf63377c710c056b3
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-01 12:23:29 +1200

    simplify comment

--- packages/meditrak-app/app/utilities/analytics.jsx
+++ packages/meditrak-app/app/utilities/analytics.jsx
@@ -1,6 +1,4 @@
-// Analytics previously reported to Microsoft App Center, which has been retired.
-// These are now no-ops, but the API is preserved so call sites don't need to change.
-// If analytics are reinstated in future, wire these methods up to the new provider.
+/** Analytics previously reported to Visual Studio App Center, which was retired in 2025. */
 export const analytics = {
   trackEvent: () => {},
   trackButtonPress: () => {},
