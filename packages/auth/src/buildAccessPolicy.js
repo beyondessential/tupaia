3608b52c06981cd23cbcd76bbf48f6db1bd6bbdd
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-02 21:12:57 +1200

    sql

--- packages/auth/src/buildAccessPolicy.js
+++ packages/auth/src/buildAccessPolicy.js
@@ -11,37 +11,43 @@ const fetchPermissionGroupChildren = async (models, permissionGroupName) => {
  * { entityCode1: ['permissionGroup1', 'permissionGroup2'], entityCode2: ['permissionGroup1'] }
  */
 export const buildAccessPolicy = async (models, userId) => {
-  const permissionsByEntity = {};
-  const userEntityPermissions = await models.userEntityPermission.find({ user_id: userId });
+  console.time('buildAccessPolicy');
+  const [{ policy }] = await models.database.executeSql(
+    `
+      WITH RECURSIVE expanded_permissions AS (
+        SELECT
+          e.code AS entity_code,
+          pg.id AS permission_group_id,
+          pg.name AS permission_group_name
+        FROM user_entity_permission uep
+        JOIN entity e ON e.id = uep.entity_id
+        JOIN permission_group pg ON pg.id = uep.permission_group_id
+        WHERE uep.user_id = ?
 
-  // set up permission group children fetching, for flattening the permission hierarchy
-  const permissionGroupToChildren = {};
-  const getOrFetchPermissionGroupChildren = async permissionGroupName =>
-    permissionGroupToChildren[permissionGroupName] || // get from cache
-    fetchPermissionGroupChildren(models, permissionGroupName); // or fetch from db
+        UNION
 
-  // iterate through the user's permissions and build the permission groups per entity
-  await Promise.all(
-    userEntityPermissions.map(async userEntityPermission => {
-      const { permission_group_name: permissionGroupName, entity_code: entityCode } =
-        userEntityPermission;
-
-      permissionsByEntity[entityCode] = permissionsByEntity[entityCode] || [];
-      permissionsByEntity[entityCode].push(permissionGroupName);
-
-      // get all of the permission groups below this one in the hierarchy, as the user also has
-      // implied access for them, so they'll also be added to the access policy (which has a simple,
-      // flat structure, ignorant of the fact that permission groups exist in a hierarchy)
-      const permissionGroupChildren = await getOrFetchPermissionGroupChildren(permissionGroupName);
-      permissionsByEntity[entityCode] =
-        permissionsByEntity[entityCode].concat(permissionGroupChildren);
-    }),
+        SELECT
+          ep.entity_code,
+          child_pg.id AS permission_group_id,
+          child_pg.name AS permission_group_name
+        FROM expanded_permissions ep
+        JOIN permission_group child_pg ON child_pg.parent_id = ep.permission_group_id
+      )
+      SELECT jsonb_object_agg(entity_code, permission_groups) AS policy
+      FROM (
+        SELECT
+          entity_code,
+          jsonb_agg(permission_group_name ORDER BY permission_group_name) AS permission_groups
+        FROM (
+          SELECT DISTINCT entity_code, permission_group_name
+          FROM expanded_permissions
+        ) deduped
+        GROUP BY entity_code
+      ) grouped;
+    `,
+    userId,
   );
-
-  // policy is simply the permissions by entity, but de-duplicated
-  const policy = {};
-  Object.entries(permissionsByEntity).forEach(([entityCode, permissionGroups]) => {
-    policy[entityCode] = uniq(permissionGroups);
-  });
-  return policy;
+  console.timeEnd('buildAccessPolicy');
+  console.log(policy ?? {});
+  return policy ?? {};
 };
