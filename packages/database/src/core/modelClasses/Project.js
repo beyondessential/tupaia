3b29c3cc8b2e7c034eb5220c81e1879672006b87
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-08-05 10:35:32 +1200

    tweak(database): faster project details query

--- packages/database/src/core/modelClasses/Project.js
+++ packages/database/src/core/modelClasses/Project.js
@@ -136,6 +136,8 @@ export class ProjectModel extends DatabaseModel {
               json_agg(child_id) AS child_id
             FROM
               entity_relation er
+            WHERE
+              er.parent_id IN (SELECT entity_id FROM project)
             GROUP BY
               parent_id) sub ON p.entity_id = sub.parent_id
         WHERE ?;
