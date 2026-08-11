d376bc7441d99bf4806ef1f4c5ea6b1851037234
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-08-11 15:32:40 +1200

    also remote

--- packages/devops/scripts/deployment-common/setup.sh
+++ packages/devops/scripts/deployment-common/setup.sh
@@ -154,10 +154,10 @@ install_pm2() {
   # Ideally match version in root package.json, which devs use locally
   if ! command -v pm2 &>/dev/null; then
     echo 'PM2 not installed. Installing...'
-    npm install --global pm2@^6.0.8
-  elif (($(pm2 --version | cut -d . -f 1) != 6)); then
-    echo "PM2 $(pm2 --version) is installed. Replacing with ^6.0.8..."
-    npm install --global pm2@^6.0.8
+    npm install --global pm2@^7.0.3
+  elif (($(pm2 --version | cut -d . -f 1) != 7)); then
+    echo "PM2 $(pm2 --version) is installed. Replacing with ^7.0.3..."
+    npm install --global pm2@^7.0.3
   fi
   echo "PM2 $(pm2 --version) is installed"
 
