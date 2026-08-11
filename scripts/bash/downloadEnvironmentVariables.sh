937e8f9953eb21c132cb88a5981fa81b2781cd7a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-08-11 14:50:39 +1200

    style: lowercase local shell script parameters

--- scripts/bash/downloadEnvironmentVariables.sh
+++ scripts/bash/downloadEnvironmentVariables.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e +x # Do not output commands in this script, as some would show credentials in plain text
 
-DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if ! "$DIR"/requireCommands.sh yarn jq; then
+if ! "$dir"/requireCommands.sh yarn jq; then
     exit 1
 fi
 
-. "$DIR/ansiControlSequences.sh"
-DEPLOYMENT_NAME=$1
-REPO_ROOT=$(realpath "$DIR/../..")
+. "$dir/ansiControlSequences.sh"
+deployment_name=$1
+repo_root=$(realpath "$dir/../..")
 
 # Log into Bitwarden
 if ! yarn bw login --check &>/dev/null; then
@@ -49,7 +49,7 @@ cleanup() {
     # way that’s compatible to both (by not supplying a suffix for the -i flag), but this causes
     # macOS to generate backup files which we don’t need.
     if [[ $(uname) = 'Darwin' ]]; then
-        rm -f "$REPO_ROOT"/env/*.env-e "$REPO_ROOT"/packages/*/.env-e
+        rm -f "$repo_root"/env/*.env-e "$repo_root"/packages/*/.env-e
     fi
 }
 
@@ -63,20 +63,20 @@ fi
 eval "$(yarn bw unlock --passwordenv BW_PASSWORD | grep -o -m 1 'export BW_SESSION=.*$')"
 
 # Collection in BitWarden where .env vars are kept
-COLLECTION_PATH='Engineering/Tupaia General/Environment Variables'
-COLLECTION_ID=$(yarn bw get collection "$COLLECTION_PATH" | jq --raw-output .id)
+collection_path='Engineering/Tupaia General/Environment Variables'
+collection_id=$(yarn bw get collection "$collection_path" | jq --raw-output .id)
 
 # Fetch the whole collection up front, and search it locally from there on. This is much faster.
-COLLECTION_ITEMS=$(yarn bw list items --collectionid "$COLLECTION_ID")
+collection=$(yarn bw list items --collectionid "$collection_id")
 
-if [[ -z $COLLECTION_ITEMS || $COLLECTION_ITEMS = '[]' ]]; then
-    echo -e "${BOLD}${RED}Bitwarden collection is empty.${RESET} No items found in $COLLECTION_PATH" >&2
+if [[ -z $collection || $collection = '[]' ]]; then
+    echo -e "${BOLD}${RED}Bitwarden collection is empty.${RESET} No items found in $collection_path" >&2
     exit 1
 fi
 
-get_notes_for_items_named_like() {
+get_note() {
     local search_term=$1
-    printf '%s' "$COLLECTION_ITEMS" |
+    printf '%s' "$collection" |
         jq --raw-output --arg search "$search_term" \
             'map(select(.name | ascii_downcase | contains($search | ascii_downcase))) | .[] .notes'
 }
@@ -85,7 +85,7 @@ echo
 
 get_packages_with_env_files() {
     # All deployable packages depend on .env files...
-    readarray -t packages_with_env_files < <("$DIR"/getDeployablePackages.sh)
+    readarray -t packages_with_env_files < <("$dir"/getDeployablePackages.sh)
     # ...plus these
     packages_with_env_files+=(data-api viz-test-tool)
     printf '%s\n' "${packages_with_env_files[@]}"
@@ -93,75 +93,75 @@ get_packages_with_env_files() {
 
 # Can provide one or more packages as command line arguments, or will default to all
 if [[ -z $2 ]]; then
-    readarray -t PACKAGES < <(get_packages_with_env_files)
-    echo -e "${BLUE}==>️${RESET} ${BOLD}Fetching environment variables for all packages${RESET}"
+    readarray -t packages < <(get_packages_with_env_files)
+    echo -e "${BLUE}==>️${RESET} ${BOLD}Loading environment variables for all packages${RESET}"
 else
-    PACKAGES=("${@:2}")
-    echo -e "${BLUE}==>️${RESET} ${BOLD}Fetching environment variables for ${PACKAGES[*]}${RESET}"
+    packages=("${@:2}")
+    echo -e "${BLUE}==>️${RESET} ${BOLD}Loading environment variables for ${packages[*]}${RESET}"
 fi
 
 load_env_file_from_bw() {
-    FILE_NAME=$1
-    BASE_FILE_PATH=$2
-    NEW_FILE_NAME=$3
-    ENV_FILE_PATH=$BASE_FILE_PATH/$NEW_FILE_NAME.env
+    local file_name=$1
+    local base_file_path=$2
+    local new_file_name=$3
+    local env_file_path=$base_file_path/$new_file_name.env
 
-    echo -en "${YELLOW}🚚 Fetching variables for ${BOLD}${FILE_NAME}...${RESET}"
+    echo -en "${YELLOW}🚚 Loading variables for ${BOLD}${file_name}...${RESET}"
 
     # checkout deployment specific env vars, or dev as fallback
-    DEPLOYMENT_ENV_VARS=$(get_notes_for_items_named_like "$FILE_NAME.$DEPLOYMENT_NAME.env")
+    deployment_env_vars=$(get_note "$file_name.$deployment_name.env")
 
-    if [[ -n $DEPLOYMENT_ENV_VARS ]]; then
-        echo "$DEPLOYMENT_ENV_VARS" >"$ENV_FILE_PATH"
+    if [[ -n $deployment_env_vars ]]; then
+        echo "$deployment_env_vars" >"$env_file_path"
     else
-        DEV_ENV_VARS=$(get_notes_for_items_named_like "$FILE_NAME.dev.env")
-        echo "$DEV_ENV_VARS" >"$ENV_FILE_PATH"
+        local dev_env_vars=$(get_note "$file_name.dev.env")
+        echo "$dev_env_vars" >"$env_file_path"
 
-        if [[ -z $DEV_ENV_VARS ]]; then
+        if [[ -z $dev_env_vars ]]; then
             echo -en "$CLEAR_LINE"
-            echo -e "${YELLOW}⚠️ No item named ${BOLD}${FILE_NAME}.$DEPLOYMENT_NAME.env${RESET} or ${BOLD}${FILE_NAME}.dev.env${RESET}${YELLOW}. Wrote empty file to $ENV_FILE_PATH."
+            echo -e "${YELLOW}⚠️ No item named ${BOLD}${file_name}.$deployment_name.env${RESET} or ${BOLD}${file_name}.dev.env${RESET}${YELLOW}. Wrote empty file to $env_file_path."
             return
         fi
     fi
 
     # Replace any instances of the placeholder [deployment-name] in the .env file with the actual
     # deployment name (e.g. [deployment-name]-api.tupaia.org -> specific-deployment-api.tupaia.org)
-    sed -i -e "s/\[deployment-name\]/$DEPLOYMENT_NAME/g" "$ENV_FILE_PATH"
+    sed -i -e "s/\[deployment-name\]/$deployment_name/g" "$env_file_path"
 
     if [[ -v DOMAIN ]]; then
         # Replace the placeholder [domain]
-        sed -i -e "s/\[domain\]/$DOMAIN/g" "$ENV_FILE_PATH"
+        sed -i -e "s/\[domain\]/$DOMAIN/g" "$env_file_path"
     fi
 
-    if [[ $DEPLOYMENT_NAME = *-e2e || $DEPLOYMENT_NAME = e2e ]]; then
+    if [[ $deployment_name = *-e2e || $deployment_name = e2e ]]; then
         # Update e2e environment variables
-        if [[ $FILE_NAME = aggregation ]]; then
-            sed -i -e 's/^AGGREGATION_URL_PREFIX="?dev-"?$/AGGREGATION_URL_PREFIX=e2e-/g' "$ENV_FILE_PATH"
+        if [[ $file_name = aggregation ]]; then
+            sed -i -e 's/^AGGREGATION_URL_PREFIX="?dev-"?$/AGGREGATION_URL_PREFIX=e2e-/g' "$env_file_path"
         fi
     fi
 
-    if [[ $DEPLOYMENT_NAME = dev ]]; then
+    if [[ $deployment_name = dev ]]; then
         # Update dev specific environment variables
         # (removes ###DEV_ONLY### prefixes, leaving the key=value pair uncommented)
         # (after removing prefix, if there are duplicate keys, dotenv uses the last one in the file)
-        sed -i -e 's/^###DEV_ONLY###//g' "$ENV_FILE_PATH"
+        sed -i -e 's/^###DEV_ONLY###//g' "$env_file_path"
     fi
 
     echo -en "$CLEAR_LINE"
-    echo -e "${GREEN}✅ Downloaded variables for ${BOLD}${FILE_NAME}${RESET} → $ENV_FILE_PATH"
+    echo -e "${GREEN}✅ Wrote ${BOLD}${file_name}${RESET} → $env_file_path"
 }
 
-for PACKAGE in "${PACKAGES[@]}"; do
+for package_name in "${packages[@]}"; do
     # Only download the env file if there is an example file in the package. If there isn’t, this
     # means it is a package that doesn’t need env vars
-    if [[ -f $REPO_ROOT/packages/$PACKAGE/.env.example ]]; then
-        load_env_file_from_bw "$PACKAGE" "$REPO_ROOT/packages/$PACKAGE" ''
+    if [[ -f $repo_root/packages/$package_name/.env.example ]]; then
+        load_env_file_from_bw "$package_name" "$repo_root/packages/$package_name" ''
     fi
 done
 
 echo
-echo -e "${BLUE}==>️${RESET} ${BOLD}Fetching shared environment variables${RESET}"
-for file_name in "$REPO_ROOT"/env/*.env.example; do
+echo -e "${BLUE}==>️${RESET} ${BOLD}Loading shared environment variables${RESET}"
+for file_name in "$repo_root"/env/*.env.example; do
     package_name=$(basename "$file_name" '.env.example')
-    load_env_file_from_bw "$package_name" "$REPO_ROOT/env" "$package_name"
+    load_env_file_from_bw "$package_name" "$repo_root/env" "$package_name"
 done
